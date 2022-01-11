9c5b039d6f4c85b84534c7653ec51293a00d931d
9teMare <wangzihao139@126.com>
2022-01-11 16:31:23 +0800

    Renamed method, refactored code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
-    public static void greetings() {
+    public static void simpleTodo() {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
         input += scanner.nextLine();
         ArrayList<String> list = new ArrayList<>();
 
         while(true) {
-            //list all the tasks
-            if (input.equalsIgnoreCase("list")) {
+                //bye, shut down Cortana
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+                //list all the tasks
+            } else if (input.equalsIgnoreCase("list")) {
                 if (list.size() == 0) {
                     System.out.println("You are done for the day, or are you?");
                 } else {
@@ -19,11 +22,7 @@ public class Duke {
                         System.out.println(i + 1 + ". " + list.get(i));
                     }
                 }
-            //bye, shut down Cortana
-            } else if (input.equalsIgnoreCase("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            //valid command, add task
+                //valid command, add task
             } else {
                 if(list.contains(input)) {
                     System.out.println("Task already exists!");
@@ -47,6 +46,6 @@ public class Duke {
                 "  \\____|  \\___/  |_|     \\__|  \\__,_| |_| |_|  \\__,_|\n" +
                 "                                                     \n";
         System.out.println("Hello from\n" + logo + "My name is Cortana, what can I do for you, master?");
-        greetings();
+        simpleTodo();
     }
 }
