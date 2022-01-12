3c505f89cd69eb54b9b888ce82965f6a73ed3d60
tiffany <tiffanylinnn21@gmail.com>
2022-01-12 17:28:56 +0800

    Implemented skeletal version of Duke (Greet, Echo, Exit)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static String answer(String input) {
+        if (!input.equals("bye")) {
+            return input;
+        } else {
+            return "Bye. Hope to see you again soon!";
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke \n" + "What can I do for you?");
+        String userInput = "";
+
+        while (!userInput.equals("bye")) {
+            userInput = sc.nextLine();
+            System.out.println(answer(userInput));
+        }
+        sc.close();
+
     }
 }
