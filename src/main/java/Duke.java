c18b850da8c58e587be0883f2db8deb27b692ef3
ddx-510 <dai.tianle@dhs.sg>
2022-01-12 21:23:18 +0800

    Level-1 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static boolean isEnd = false;
+
+    public static void horizontalLine(){
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void greet(){
+        horizontalLine();
+        System.out.println("\tHello I am DDX");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println();
+        horizontalLine();
+    }
+
+    public static void echo(){
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String output = myObj.nextLine();
+        // detect bye
+        if (output.equals("bye")) {
+            Duke.isEnd = true;
+            output = "\tBye. Hope to see you again soon!";
+        }
+        horizontalLine();
+        System.out.println(output);
+        System.out.println();
+        horizontalLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        while (!isEnd) {
+            echo();
+        }
     }
 }
