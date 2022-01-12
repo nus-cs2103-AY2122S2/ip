5d4d4afa99e08886e3d357787767477e03854223
tiffany <tiffanylinnn21@gmail.com>
2022-01-12 18:12:08 +0800

    Updated skeletal duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
 
-    public static String answer(String input) {
-        if (!input.equals("bye")) {
-            return input;
-        } else {
+    public Duke(){}
+
+    public String answer(String input) {
+        if (input.equals("bye")) {
             return "Bye. Hope to see you again soon!";
+        } else {
+            return input;
         }
     }
 
     public static void main(String[] args) {
+        Duke duke = new Duke();
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Duke \n" + "What can I do for you?");
         String userInput = "";
 
         while (!userInput.equals("bye")) {
             userInput = sc.nextLine();
-            System.out.println(answer(userInput));
+            System.out.println(duke.answer(userInput));
         }
         sc.close();
 
     }
 }
+
