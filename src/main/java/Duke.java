7145aed60a40aa354ffa4a6c46e53b99f94d68d5
soloplxya <69782911+soloplxya@users.noreply.github.com>
2022-01-13 22:50:19 +0800

    Level 3: Mark As Done (With Extension)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,11 +2,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static ArrayList<String> tasks = new ArrayList<>();
+
+    private static final ArrayList<Task> tasks = new ArrayList<>();
+    private static final String divider = "\n______________________________________\n";
 
     public static void addTask(String task) {
         System.out.println("Added as per your request: " + task);
-        tasks.add(task);
+        tasks.add(new Task(task));
     }
 
     public static void listTasks() {
@@ -17,9 +19,26 @@ public class Duke {
         System.out.println(listOfTasks);
     }
 
+    public static void markTask(int taskId, boolean mark) {
+        Task toSet = tasks.get(taskId - 1);
+        if (mark) {
+            toSet.markIsDone();
+            tasks.set(taskId - 1, toSet);
+            System.out.println("Sugoi! I have marked this task as done!\n" + tasks.get(taskId - 1).toString());
+        } else {
+            if (toSet.isDone) {
+                toSet.markUndone();
+                tasks.set(taskId - 1, toSet);
+                System.out.println("No worries! I have unmarked this task for you!\n" + tasks.get(taskId - 1).toString());
+            } else {
+                System.out.println("This task has yet to be done!");
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
-        String divider = "\n______________________________________\n";
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -31,19 +50,61 @@ public class Duke {
 
         System.out.println(divider);
         System.out.println("What do you need help with?\n");
-        String userInput = scanner.nextLine();
+        String[] inputArray = scanner.nextLine().split(" ");
 
-        while (!userInput.equalsIgnoreCase("bye")) {
-            if (userInput.equalsIgnoreCase("list")) {
-                listTasks();
-            } else {
-                addTask(userInput);
+
+        while (!(inputArray[0].equalsIgnoreCase("bye") && inputArray.length == 1)) {
+
+            // single command
+            if (inputArray.length == 1) {
+               if (inputArray[0].equalsIgnoreCase("list")) {
+                   listTasks();
+               } else {
+                   addTask(inputArray[0]);
+               }
             }
+
+            // double command
+            if (inputArray.length > 1) {
+                if (inputArray[0].equalsIgnoreCase("mark")) {
+                    try {
+                        int taskId = Integer.parseInt(inputArray[1]);
+                        if (taskId > 0 && taskId < (tasks.size() + 1)) {
+                            markTask(taskId, true);
+                        } else {
+                            System.out.println("Task cannot be found within the task list! Please fix your machigai!");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Task ID needs to be an integer");
+                    }
+                } else if (inputArray[0].equalsIgnoreCase("unmark")) {
+                    try {
+                        int taskId = Integer.parseInt(inputArray[1]);
+                        if (taskId > 0 && taskId < tasks.size()) {
+                            markTask(taskId, false);
+                        } else {
+                            System.out.println("Task cannot be found within the task list! Please fix your machigai!");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Task ID has to be an integer!");
+                    }
+                }
+                else {
+                    StringBuilder input = new StringBuilder();
+                    for (String item: inputArray) {
+                        input.append(item).append(" ");
+                    }
+                    addTask(input.toString());
+                }
+            }
+
             System.out.println(divider);
-            userInput = scanner.nextLine();
+            inputArray = scanner.nextLine().split(" ");
         }
+
 
         System.out.println(divider);
         System.out.println("Sayonara! Hope to see you again soon!");
+        scanner.close();
     }
 }
