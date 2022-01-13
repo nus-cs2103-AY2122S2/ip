90567c204970f6bcab0804c98fb9564c826f2529
bakano98 <70756909+bakano98@users.noreply.github.com>
2022-01-13 09:39:52 +0800

    Level-1 : Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = "--------------------------------------------------";
+        String tabbedLine = "\t----------------------------------------------";
+        System.out.println(line);
+        System.out.println("Hello from Burp\n");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        while (true) {
+            String cmd = br.readLine();
+            if (cmd.equals("bye")) {
+                System.out.println(tabbedLine + "\n\tBye. Hope to see you again soon!\n" + tabbedLine);
+                break;
+            } else {
+                System.out.println(tabbedLine + "\n\t" + cmd + "\n" + tabbedLine);
+            }
+        }
     }
 }
