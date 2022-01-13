b8580b318ba9d12b6326da914fccf397aa3027d7
bakano98 <70756909+bakano98@users.noreply.github.com>
2022-01-13 12:20:36 +0800

    Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,12 +9,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Duke {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, DukeException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String tabbedLine = "\t----------------------------------------------";
         String cmd;
         List<Task> toDoList = new ArrayList<>();
-        String tabbedLine = "\t----------------------------------------------";
-        String welcome = tabbedLine + "\n\tHello from Burp\n\tWhat can I do for you?\n" + tabbedLine;
+        String welcome = formatMsg("Hello from Burp\n\tWhat can I do for you?");
         System.out.println(welcome);
         while (!(cmd = br.readLine()).equals("bye")) {
             String[] cmd_split = cmd.split(" ");
@@ -28,57 +28,68 @@ public class Duke {
                     System.out.println(tabbedLine);
                     break;
                 case "mark":
-                    int numberToMark = Integer.parseInt(cmd_split[1])-1; // convert to 0-based indexing
-                    if (toDoList.size() == 0 || numberToMark >= toDoList.size()) {
-                        System.out.println("Error. Tasks.Task does not exist");
-                        break;
+                    try {
+                        int numberToMark = Integer.parseInt(cmd_split[1])-1; // convert to 0-based indexing
+                        Task taskToMark = toDoList.get(numberToMark);
+                        taskToMark.mark();
+                        System.out.println(formatMsg("OK, I've marked this task as done:\n\t" + taskToMark));
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(new DukeException(formatMsg("☹ OOPS!!! Item to mark does not exist.")));
                     }
-                    Task taskToMark = toDoList.get(numberToMark);
-                    taskToMark.mark();
-                    System.out.println(tabbedLine + "\nNice! I've marked this task as done:");
-                    System.out.println(taskToMark);
-                    System.out.println(tabbedLine);
                     break;
                 case "unmark":
-                    int numberToUnmark = Integer.parseInt(cmd_split[1])-1; // convert to 0-based indexing
-                    if (toDoList.size() == 0 || numberToUnmark >= toDoList.size()) {
-                        System.out.println("Error. Tasks.Task does not exist");
-                        break;
+                    try {
+                        int numberToUnmark = Integer.parseInt(cmd_split[1])-1; // convert to 0-based indexing
+                        Task taskToUnmark = toDoList.get(numberToUnmark);
+                        taskToUnmark.unmark();
+                        System.out.println(formatMsg("OK, I've marked this task as not done yet:\n\t" + taskToUnmark));
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(new DukeException(formatMsg("☹ OOPS!!! Item to unmark does not exist.")));
                     }
-                    Task taskToUnmark = toDoList.get(numberToUnmark);
-                    taskToUnmark.unmark();
-                    System.out.println(tabbedLine + "\nOK, I've marked this task as not done yet:");
-                    System.out.println(taskToUnmark);
-                    System.out.println(tabbedLine);
                     break;
                 case "todo":
-                    ToDo newToDo = new ToDo(cmd.split("todo")[1], false);
-                    toDoList.add(newToDo);
-                    System.out.println(tabbedLine + "\n\tGot it. I've added this task:\n\t" + newToDo + "\n\tNow you have " + toDoList.size() + " tasks in the list.");
-                    System.out.println(tabbedLine);
+                    try {
+                        ToDo newToDo = new ToDo(cmd.split("todo")[1], false);
+                        toDoList.add(newToDo);
+                        System.out.println(formatMsg("Got it. I've added this task:\n\t" + newToDo + "\n\tNow you have " + toDoList.size() + " tasks in the list."));
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(new DukeException(formatMsg("☹ OOPS!!! The description of a todo cannot be empty.")));
+                    }
                     break;
                 case "deadline":
-                    String[] deadlineDetails = cmd.split("deadline")[1].split("/by");
-                    String deadlineName = deadlineDetails[0];
-                    String deadline = deadlineDetails[1];
-                    Deadline newDeadline = new Deadline(deadlineName, false, deadline);
-                    toDoList.add(newDeadline);
-                    System.out.println(tabbedLine + "\n\tGot it. I've added this task:\n\t" + newDeadline + "\n\tNow you have " + toDoList.size() + " tasks in the list.");
-                    System.out.println(tabbedLine);
+                    try {
+                        String[] deadlineDetails = cmd.split("deadline")[1].split("/by");
+                        String deadlineName = deadlineDetails[0];
+                        String deadline = deadlineDetails[1];
+                        Deadline newDeadline = new Deadline(deadlineName, false, deadline);
+                        toDoList.add(newDeadline);
+                        System.out.println(formatMsg("Got it. I've added this task:\n\t" + newDeadline + "\n\tNow you have " + toDoList.size() + " tasks in the list."));
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(new DukeException(formatMsg("☹ OOPS!!! The description of a deadline cannot be empty.")));
+                    }
                     break;
                 case "event":
-                    String[] eventDetails = cmd.split("event")[1].split("/at");
-                    String eventName = eventDetails[0];
-                    String eventDateTime = eventDetails[1];
-                    Event newEvent = new Event(eventName, false, eventDateTime);
-                    toDoList.add(newEvent);
-                    System.out.println(tabbedLine + "\n\tGot it. I've added this task:\n\t" + newEvent + "\n\tNow you have " + toDoList.size() + " tasks in the list.");
-                    System.out.println(tabbedLine);
+                    try {
+                        String[] eventDetails = cmd.split("event")[1].split("/at");
+                        String eventName = eventDetails[0];
+                        String eventDateTime = eventDetails[1];
+                        Event newEvent = new Event(eventName, false, eventDateTime);
+                        toDoList.add(newEvent);
+                        System.out.println(formatMsg("Got it. I've added this task:\n\t" + newEvent + "\n\tNow you have " + toDoList.size() + " tasks in the list."));
+                    } catch (IndexOutOfBoundsException e) {
+                        System.out.println(new DukeException(formatMsg("☹ OOPS!!! The description of an event cannot be empty.")));
+                    }
                     break;
                 default:
-                    System.out.println("Invalid");
+                    System.out.println(new DukeException(formatMsg("☹ OOPS!!! I'm sorry, but I don't know what that means :-(")));
+                    break;
             }
         }
-        System.out.println(tabbedLine + "\n\tBye. Hope to see you again soon!\n" + tabbedLine);
+        System.out.println(formatMsg("Bye. Hope to see you again soon!"));
+    }
+
+    public static String formatMsg(String msg) {
+        String tabbedLine = "\t----------------------------------------------";
+        return (tabbedLine + "\n\t" + msg + "\n" + tabbedLine);
     }
 }
