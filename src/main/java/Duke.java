3b7338c78f9ab142ee752d680a715b292128db03
soloplxya <69782911+soloplxya@users.noreply.github.com>
2022-01-13 20:43:36 +0800

    Level 1: Greet, Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String divider = "______________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Konnichiwassup from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println(divider);
+        System.out.println("What do you need help with?\n");
+        String userInput = scanner.nextLine();
+
+        while (!userInput.equalsIgnoreCase("bye")) {
+            System.out.println(userInput);
+            System.out.println(divider);
+            userInput = scanner.nextLine();
+        }
+
+        System.out.println(divider);
+        System.out.println("Sayonara! Hope to see you again soon!");
     }
 }
