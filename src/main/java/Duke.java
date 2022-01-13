3e5cbb3e1f23b1235763bd1cbbc5d75a9907d2c9
tiffany <tiffanylinnn21@gmail.com>
2022-01-13 11:42:23 +0800

    Implemented Mark as Done with extension A-Classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,34 +1,52 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Duke {
 
-    private ArrayList<String> arr;
-    private int numOfTasks;
+    private ArrayList<Task> arr;
 
     public Duke(){
-        this.arr = new ArrayList<String>(0);
-        this.numOfTasks = 0;
+        this.arr = new ArrayList<Task>();
     }
 
-    private void update(String s) {
-        this.arr.add(s);
+    private void addTask(Task t) {
+        this.arr.add(t);
     }
 
     private void answer(String input) {
-        String ans = "";
+        String ans = "\t";
         if (input.equals("bye")) {
-            ans = "Bye. Hope to see you again soon!";
+            ans += "Bye. Hope to see you again soon!";
         } else if (input.equals("list")) {
-            for (String s : arr) {
-                ans += s + ('\n');
+            ans += "Here are the tasks in your list: \n";
+            for (int i = 0; i < arr.size(); i++) {
+                Task t = arr.get(i);
+                if (i == arr.size() - 1) {
+                    ans += String.format("\t%d.%s", i + 1, t.toString());
+                } else {
+                    ans += String.format("\t%d.%s \n", i + 1, t.toString());
+                }
             }
-            ans = ans.trim();
+        } else if (Pattern.matches("mark \\d+|unmark \\d+", input)) {
+            String[] strArr = input.split(" ");
+            int index = Integer.valueOf(strArr[1]) - 1;
+            if (index >= 0 && index < arr.size()) {
+                Task t = arr.get(index);
+                if (strArr[0].equals("mark")) {
+                    t.markDone();
+                    ans += "Nice! I've marked this task as done: \n\t\t" + t.toString();
+                } else {
+                    t.markUndone();
+                    ans += "OK, I've marked this task as not done yet: \n\t\t" + t.toString();
+                }
+            } else {
+                ans = "\tInvalid index given!";
+            }
         } else {
-            numOfTasks++;
-            String task = numOfTasks + ". " + input;
-            update(task);
-            ans = "added: " + input;
+            Task task = new Task(input);
+            addTask(task);
+            ans += "added: " + input;
         }
         System.out.println(ans);
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task  {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
