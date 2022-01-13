14d102358bdfca7c29f73a3fc024b1e7bd167c28
bakano98 <70756909+bakano98@users.noreply.github.com>
2022-01-13 09:58:26 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,32 @@
 import java.io.BufferedReader;
+import java.util.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Duke {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = "--------------------------------------------------";
+        int itemNumber = 1; // start from 1
+        String cmd;
+        List<String> toDoList = new ArrayList<String>();
         String tabbedLine = "\t----------------------------------------------";
-        System.out.println(line);
-        System.out.println("Hello from Burp\n");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        while (true) {
-            String cmd = br.readLine();
-            if (cmd.equals("bye")) {
-                System.out.println(tabbedLine + "\n\tBye. Hope to see you again soon!\n" + tabbedLine);
-                break;
-            } else {
-                System.out.println(tabbedLine + "\n\t" + cmd + "\n" + tabbedLine);
+        String welcome = tabbedLine + "\n\tHello from Burp\n\tWhat can I do for you?\n" + tabbedLine;
+        System.out.println(welcome);
+        while (!(cmd = br.readLine()).equals("bye")) {
+            switch (cmd) {
+                case "list":
+                    // then list out the items
+                    System.out.println(tabbedLine);
+                    toDoList.forEach(item -> System.out.println(item));
+                    System.out.println(tabbedLine);
+                    break;
+                default:
+                    toDoList.add(itemNumber + ": " + cmd);
+                    itemNumber++;
+                    System.out.println(tabbedLine + "\n\tadded: " + cmd + "\n" + tabbedLine);
+                    break;
             }
         }
+        System.out.println(tabbedLine + "\n\tBye. Hope to see you again soon!\n" + tabbedLine);
     }
 }
