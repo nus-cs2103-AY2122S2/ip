caa75f0a7840687f11cd05a3a5d28923f5a129c0
jxt00 <e0556267@u.nus.edu>
2022-01-13 17:24:47 +0800

    Add basic functionalities to Duke.java
    Implement a skeletal version of Duke that
    * greets the user
    * echos commands entered by the user
    * exits when the user types bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String lineBreak = "____________________________________________________________\n";
+        String catFace = " =^_^=\n";
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                + "|  _ \\ _   _| | _____      /\\_/\\           ___\n"
+                + "| | | | | | | |/ / _      = o_o =_______    \\ \\  -Julie Rhodes-\\\n"
+                + "| |_| | |_| |   <  __/     __^      __(  \\.__) )\n"
+                + "|____/ \\__,_|_|\\_\\___| (@)<_____>__(_____)____/\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(lineBreak + "Meow! I'm Duke" + catFace + "What can I do for you?\n" + lineBreak);
+
+        String input = sc.nextLine().strip();
+
+        while (!input.equals("bye")) {
+            System.out.println(lineBreak + input + catFace + lineBreak);
+            input = sc.nextLine().strip();
+        }
+
+        System.out.println(lineBreak + "Bye. Hope to see you again soon!" + catFace + lineBreak);
+
+        sc.close();
     }
 }
