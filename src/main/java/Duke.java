709c45725c01b264f9db3243f29a58fdf94da013
DESKTOP-6G3IIBM\User <arcornior>
2022-01-14 19:45:49 +0800

    Duke.java - Added tracking for 3 types of tasks Task.java - Supports tracking of 3 types of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,11 +14,11 @@ public class Duke {
                 "|  __| '_ \\ / __| | |/ /\n" +
                 "| |__| | | | (__| |   < \n" +
                 "\\____/_| |_|\\___|_|_|\\_\\\n";
-        System.out.println("Good morning gennermen from\n" + logo);
+        System.out.println("GOOD MORNING GENNERMEN from\n" + logo);
 
         String border = "   ____________________________________________________________";
         String welcome =
-                "Good MORNING CHAO RECRUIT! YOU MAY CALL ME ENCIK ENCIK\n" +
+                "GOOD MORNING CHAO RECRUIT! YOU MAY CALL ME ENCIK ENCIK\n" +
                 "      WHAT YOU WANT?";
         format(welcome);
 
@@ -40,26 +40,46 @@ public class Duke {
                 }
                 format(str.toString());
             } else {
-                String[] temp = toEcho.split(" ");
+                //Split cmd into 2 parts, the type of task, and remaining text
+                String[] temp = toEcho.split(" ", 2);
                 String cmd = temp[0];
+                String rem = temp[1];
                 //Assuming input correct
                 if (cmd.equals("mark")) {
-                    int num = Integer.parseInt(temp[1]) - 1;
+                    int num = Integer.parseInt(rem) - 1;
                     tasks.get(num).mark();
                     String confirm =
                             "THIS ONE\n        " + tasks.get(num) +
                             "\n      FINISH ALREADY AH? SWEE CHAI BUTTERFLY RECRUIT!";
                     format(confirm);
                 } else if (cmd.equals("unmark")) {
-                    int num = Integer.parseInt(temp[1]) - 1;
+                    int num = Integer.parseInt(rem) - 1;
                     tasks.get(num).unmark();
                     String confirm =
                             "I THOUGHT THIS ONE\n        " + tasks.get(num) +
                             "\n      FINISH ALREADY? NEVER MIND THIS WEEKEND CONFINE!";
                     format(confirm);
-                } else {
-                    tasks.add(new Task(toEcho));
-                    format("added: " + toEcho);
+                } else if (cmd.equals("deadline")) {
+                    //deadline
+                    String task = rem.split("/by")[0];
+                    String dead = rem.split("/by")[1];
+                    Task tempTask = new Task(cmd, task, dead);
+                    tasks.add(tempTask);
+                    format("YOU BETTER FINISH THIS AH:\n        " + tempTask +
+                            "\n      YOU GOT " + tasks.size() + " TO DO AH BETTER NOT FORGET!");
+                } else if (cmd.equals("event")) {
+                    //event
+                    String task = rem.split("/at")[0];
+                    String dead = rem.split("/at")[1];
+                    Task tempTask = new Task(cmd, task, dead);
+                    tasks.add(tempTask);
+                    format("YOU BETTER REMEMBER THIS AH:\n        " + tempTask +
+                            "\n      YOU GOT " + tasks.size() + " TO DO AH BETTER NOT FORGET!");
+                } else if (cmd.equals("todo")) {
+                    //todo
+                    Task tempTask = new Task(cmd, rem);
+                    tasks.add(tempTask);
+                    format("YOU WANT TO DO THIS AH:\n        " + tempTask + "\n      VERY GOOD!");
                 }
             }
             toEcho = input.nextLine();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,19 @@
 public class Task {
     private String name;
-    boolean done;
+    private boolean done;
+    private String dead;
+    private String type;
 
-    public Task(String name) {
+    public Task(String type, String name) {
         this.name = name;
         this.done = false;
+        this.type = type;
+    }
+    public Task(String type, String name, String dead) {
+        this.name = name;
+        this.done = false;
+        this.type = type;
+        this.dead = dead;
     }
 
     public void mark() {
@@ -18,12 +27,26 @@ public class Task {
     @Override
     public String toString() {
         String str = "[";
+        //Type
+        if (this.type.equals("todo")) {
+            str += "T][";
+        } else if (this.type.equals("deadline")) {
+            str += "D][";
+        } else {
+            str += "E][";
+        }
+        //Completion
         if (this.done) {
             str += "X";
         } else {
             str += " ";
         }
         str += "] " + this.name;
+        if (this.type.equals("deadline")) {
+            str += "(by:" + this.dead + ")";
+        } else if (this.type.equals("event")){
+            str += "(at:" + this.dead + ")";
+        }
         return str;
     }
 }
