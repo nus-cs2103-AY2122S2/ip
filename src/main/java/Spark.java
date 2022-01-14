8fb5cd7ae3463c070af825698160ff25b2e335f5
James Yeap <jamesyeap.ys@gmail.com>
2022-01-14 20:42:03 +0800

    Add function to echo user commands and to exit when user's command is "bye".

--- src/main/java/Spark.java
+++ src/main/java/Spark.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Spark {
     public static void main(String[] args) {
-        System.out.println("Spark");
-        System.out.println("Hello human! What can I do for you?\n");
+        Scanner sc = new Scanner(System.in);
+
+        // print welcome message
+        System.out.println("-----------------------------------");
+        System.out.println("Hello! I'm Spark");
+        System.out.println("What can I do for you?");
+        System.out.println("-----------------------------------");
+
+        // get command from user
+        String command = sc.nextLine();
+
+        // process commands that are not "bye"
+        while (!command.equals("bye")) {
+            System.out.println("-----------------------------------");
+            System.out.println(command);
+            System.out.println("-----------------------------------");
+
+            command = sc.nextLine();
+        }
+
+        // print goodbye message
+        System.out.println("Cool, see you around!");
     }
 }
