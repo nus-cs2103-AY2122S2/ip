848f6b3f24dc800b1b4b6f04118390868c8e350f
James Yeap <jamesyeap.ys@gmail.com>
2022-01-14 20:28:26 +0800

    Rename Duke to Spark.

---
+++ src/main/java/Spark.java
@@ -0,0 +1,6 @@
+public class Spark {
+    public static void main(String[] args) {
+        System.out.println("Spark");
+        System.out.println("Hello human! What can I do for you?\n");
+    }
+}
