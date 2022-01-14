7fb5ff8fc64f74ae93512c6f22dede4fbde39a7a
yunbinmo <yunbinmo@gmail.com>
2022-01-14 20:31:32 +0800

    Implement level 2 - Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,25 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> textList = new ArrayList<>();
+
     public static void main(String[] args) {
         printMsg("Hello! I am Spike ⊂( ・ ̫・)⊃\nWhat can I do for you?");
 
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
-            String command = sc.next();
+            String command = sc.nextLine();
             if (command.equals("bye")) {
                 printMsg("See you soon! ﾍ(=￣∇￣)ﾉ");
                 sc.close();
                 return;
+            } else if (command.equals("list")) {
+                printList();
             } else {
-                printMsg(command);
+                String str = "added: " + command;
+                textList.add(command);
+                printMsg(str);
             }
         }
     }
@@ -23,6 +30,23 @@ public class Duke {
     public static void printMsg(String msg) {
         System.out.println("-------------------------------------------------\n"
                 + msg + "\n"
-                + "-------------------------------------------------\n");
+                + "-------------------------------------------------");
+    }
+
+    /**
+     * Print all items in the list
+     */
+    public static void printList() {
+        int i = 1;
+        String result = "";
+        for (String str : textList) {
+            if (i == textList.size()) {
+                result = result + i + ". " + str;
+            } else {
+                result = result + i + ". " + str + "\n";
+            }
+            i++;
+        }
+        printMsg(result);
     }
 }
