5c57728f5eb016a404f08d45177555c9e68fde82
raynerljm <raynerljm@gmail.com>
2022-01-14 17:35:40 +0800

    Implement level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duck\nWhat can I do for you? *quack*");
+
+        Scanner sc = new Scanner(System.in);
+        String command = "";
+        while (!command.equals("bye")) {
+            command = sc.nextLine();
+            System.out.println(String.format("You quacked: %s", command));
+        }
+        System.out.println("Aww. Hope to see you again soon! *quack*");
     }
 }
