94a5a6e445728a19a3a0822f87be3b3c5e4ee716
pnutzz-0207 <77202907+pnutzz-0207@users.noreply.github.com>
2022-01-14 17:52:28 +0800

    Update Duke.java
    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static String LINE = "*~*~*~*~*~*~*~*~*~*~*~*~*~*~*";
+
+
+    private static void messageProcess() {
+        Scanner user_input = new Scanner(System.in).useDelimiter("\n");
+        String user_message = user_input.next();
+
+        while (!user_message.equals("bye")) {
+            System.out.println(LINE + "\n" + user_message + "\n" + LINE);
+            user_message = user_input.next();
+        }
+    }
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "  *    ,---.    ,-----.  ,--.  ,--.   ,---.      *\n"
+                + "      '   .-'  '  .-.  ' |  ,'.|  |  /  O  \\    *\n"
+                + " *    `.  `-.  |  | |  | |  |' '  | |  .-.  |   \n"
+                + "    * .-'    | '  '-'  ' |  | `   | |  | |  |      *\n"
+                + "*     `-----'   `-----'  `--'  `--' `--' `--'   *";
         System.out.println("Hello from\n" + logo);
+        System.out.println("\nHi! You can call me Sona ^^~\nHow can I help you?\n");
+
+        messageProcess();
+
+
+
+        System.out.println(LINE + "\nBye-bye :')  Hope to see you soon! \n" + LINE);
     }
 }
