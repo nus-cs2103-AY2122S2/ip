504662d5dc15fc46a498fa29093f7b98b664d914
zhengzikang <zhengzikangzzz@gmail.com>
2022-01-15 02:21:19 +0800

    Implement Add and List tasks

--- src/main/java/Meep.java
+++ src/main/java/Meep.java
@@ -6,15 +6,25 @@ public class Meep {
         String userInput = "In", outputMsg = "Out";
         Scanner sc = new Scanner(System.in);
 
+        String[] tasks = new String[100];
+        int taskIndex = 0;
+
         while (true) {
             System.out.print("You: ");
             userInput = sc.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println("Meep: Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println("Meep: ");
+                for (int i = 0; i < taskIndex; i++) {
+                    System.out.println("     " + i + ".  " + tasks[i]);
+                }
             } else {
                 outputMsg = userInput;
-                System.out.println("Meep:"+outputMsg);
+                tasks[taskIndex] = userInput;
+                taskIndex++;
+                System.out.println("Meep: added: " + outputMsg);
             }
         }
         sc.close();
