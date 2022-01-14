7342e5e4d643585aeda0688454865d021c6096dd
DESKTOP-6G3IIBM\User <arcornior>
2022-01-14 17:30:57 +0800

    Edited Duke.java Duke, now known as Encik, will echo user input until bye is input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo =
+                " _____           _ _   \n" +
+                "|  ___|         (_) |   \n" +
+                "| |__ _ __   ___ _| | __\n" +
+                "|  __| '_ \\ / __| | |/ /\n" +
+                "| |__| | | | (__| |   < \n" +
+                "\\____/_| |_|\\___|_|_|\\_\\\n";
+        System.out.println("Good morning gennermen from\n" + logo);
+
+        String border = "   ____________________________________________________________";
+        System.out.println(border +
+                "\n      Good MORNING CHAO RECRUIT! YOU MAY CALL ME ENCIK ENCIK\n" +
+                "      WHAT YOU WANT?\n" + border + "\n");
+
+        Scanner input = new Scanner(System.in);
+        String toEcho = input.nextLine();
+        while(!toEcho.equals("bye")) {
+            System.out.println(toEcho);
+            toEcho = input.nextLine();
+        }
+        System.out.println(border +
+                "\n      BYE WHAT BYE? YOU GO DROP TWENTY THEN BYE! DOWN!\n" + border + "\n");
     }
 }
