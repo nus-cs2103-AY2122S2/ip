b4dc51c7ad4258d596f445b9ca27bc41b4caf2ce
Sean Leong <seanleongsr@gmail.com>
2022-01-14 21:56:25 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.*;
+import java.io.*;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Duke: Hello! I'm Duke\n      What can I do for you?");
+        String inp = "";
+        List<String> a = new ArrayList<String>();
+        while (!inp.equals("bye")) {
+            inp = s.nextLine();
+            if (inp.equals("bye")) {
+                System.out.println("\nDuke:  Bye. Hope to see you again soon!");
+            } else {
+                System.out.println("\nDuke:  " + inp + "\n");
+            }
+        }
     }
 }
