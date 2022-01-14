8da8b89edffa2b0d231658a6e26bb4a45f5907b6
tyanhan <tayyanhanw@gmail.com>
2022-01-15 01:24:40 +0800

    Add Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,7 +25,14 @@ public class Duke {
                 break;
             } else if (str.equals("list")) {
                 System.out.println(toDoList);
-            } else {
+            } else if (str.length() > 4 && str.substring(0,4).equals("mark")) {
+                ToDoItem task = toDoList.get(Integer.parseInt(str.substring(5)));
+                task.markAsDone();
+            } else if (str.length() > 6 && str.substring(0,6).equals("unmark")) {
+                ToDoItem task = toDoList.get(Integer.parseInt(str.substring(7)));
+                task.markAsUndone();
+            }
+            else {
                 toDoList.add(new ToDoItem(str));
                 System.out.println("added: " + str);
             }
@@ -35,10 +42,28 @@ public class Duke {
 }
 
 class ToDoItem {
-    String task;
+    protected String task;
+    protected boolean isDone;
 
     ToDoItem(String task) {
         this.task = task;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println("[X] " + this.task);
+    }
+
+    public void markAsUndone() {
+        isDone = false;
+        System.out.println(" OK, I've marked this task as not done yet: ");
+        System.out.println("[ ] " + this.task);
     }
 
     @Override
@@ -48,18 +73,22 @@ class ToDoItem {
 }
 
 class ToDoList {
-    ArrayList<ToDoItem> ls = new ArrayList<>();
+    protected ArrayList<ToDoItem> ls = new ArrayList<>();
 
     public void add(ToDoItem task) {
         ls.add(task);
     }
 
+    public ToDoItem get(int item) {
+        return ls.get(item - 1);
+    }
+
     @Override
     public String toString() {
         int counter = 1;
-        String output = "";
+        String output = "Here are the tasks in your list:\n";
         for (ToDoItem task : ls) {
-            output += counter + ". " + task + "\n";
+            output += counter + "." + "[" + task.getStatusIcon() + "] " + task + "\n";
             counter += 1;
         }
         return output.substring(0,output.length() - 1);
