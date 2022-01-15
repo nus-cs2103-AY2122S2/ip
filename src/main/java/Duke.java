eb07fd3b1a8e4b7936b09c221023f1d3304b8c66
YIHERNPC\leeyi <leeyihern96@gmail.com>
2022-01-16 00:07:20 +0800

    Implemented basic dialogue feature for Duke. Duke echoes user's input.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String userIn = ".";
+        while (!userIn.equals("bye")) {
+            userIn = sc.nextLine();
+            System.out.println(speech(userIn));
+        }
+    }
+
+    private static String speech(String text) {
+        return "____________________________\n" +
+                text +
+                "\n____________________________\n";
     }
 }
