cb37b5d32990f20d89fa5f532f44cbbf17057c04
RyanCheungJF <ryan9cheung@gmail.com>
2022-01-15 18:41:36 +0800

    Implemented Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.logging.Handler;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm Duke!");
+        System.out.println("What can I do for you?\n");
+
+        while (true) {
+            Scanner scn = new Scanner(System.in);
+            String input = scn.nextLine();
+            if (input.equals(Handlers.BYE.label)) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
---
+++ src/main/java/Handlers.java
@@ -0,0 +1,9 @@
+public enum Handlers {
+    BYE("bye");
+
+    public final String label;
+
+    private Handlers(String label) {
+        this.label = label;
+    }
+}
