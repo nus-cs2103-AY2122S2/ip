458bb6e57404913232d8f0b4618d8a7df39c7dbb
Alexander Er <aceszhenwei@yahoo.com>
2022-01-15 23:43:27 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
+    static String line = "------------------------------------";
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "/\\___ \\                            __           \n"
+                + "\\/__/\\ \\     __     _ __   __  __ /\\_\\    ____  \n"
+                + "   _\\ \\ \\  /'__`\\  /\\`'__\\/\\ \\/\\ \\\\/\\ \\  /',__\\ \n"
+                + "  /\\ \\_\\ \\/\\ \\L\\.\\_\\ \\ \\/ \\ \\ \\_/ |\\ \\ \\/\\__, `\\\n"
+                + "  \\ \\____/\\ \\__/.\\_\\\\ \\_\\  \\ \\___/  \\ \\_\\/\\____/\n"
+                + "   \\/___/  \\/__/\\/_/ \\/_/   \\/__/    \\/_/\\/___/ \n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("How may I help you today?");
+        System.out.println(line);
+
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye") || input.equals("Bye")) {
+                System.out.println(line);
+                System.out.println("Goodbye! I'll be here if you need anything else.");
+                System.out.println(line);
+                break;
+            }
+            else {
+                System.out.println(line);
+                System.out.println(input);
+                System.out.println(line);
+            }
+        }
     }
 }
+
+
