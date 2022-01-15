1589e7d85fa290adafc230bf4f7ca1a606bdeb94
Wei Howe <weihowe99@gmail.com>
2022-01-15 23:28:14 +0800

    Level 1 greet echo bye working

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,50 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static int level = 1;
+
+    public Duke() {}
+
+    public void run() {
+        welcome();
+        Scanner sc = new Scanner(System.in);
+        boolean end = false;
+        while (!end) {
+            String user_command = sc.nextLine();
+            if (user_command.equals("bye")){
+                exit();
+                end = true;
+            } else {
+                echo(user_command);
+            }
+        }
+    }
+
+    public void echo(String user_command) {
+        horizontal();
+        System.out.println(user_command);
+        horizontal();
+    }
+
+    public void exit() {
+        horizontal();
+        System.out.println("Bye nerd. Glad you'll be interacting with a real life human now! Haha...");
+        horizontal();
+    }
+
+    public void horizontal() {
+        System.out.println("-------------------------Level " + level + "-------------------------");
+    }
+
+    public void welcome(){
+        horizontal();
+        System.out.println("Greetings, NERD! I'm Duke");
+        System.out.println("Fine, I'm programmed to be nice today. What can I do for you? :)");
+        horizontal();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        new Duke().run();
     }
 }
