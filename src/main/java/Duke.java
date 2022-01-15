b21397750af0ed7a78e6c545db392328ef1d9485
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2022-01-15 15:06:48 +0800

    Added text to match expected output when calling list. Changed to print "added input" where input is the whole input and not only the first word. Simplified some logic in the program

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("    Hello! I'm Duke\n" +
-                "    What can I do for you?");
+        System.out.println("    Hello! I'm Duke\n" + "    What can I do for you?");
         Task[] tasks = new Task[100];
         int numOfTasks = 0;
         Scanner sc = new Scanner(System.in);
         boolean isExit = false;
-        while (isExit == false) {
+        while (!isExit) {
             String comm = sc.nextLine();
             String[] processedCommand = comm.split(" ", 2);
             switch (processedCommand[0]) {
@@ -17,6 +16,7 @@ public class Duke {
                     isExit = true;
                     break;
                 case "list":
+                    System.out.println("    Here are the tasks in your list:");
                     for (int i = 0; i < numOfTasks; i++) {
                         int indexToPrint = i + 1;
                         if (tasks[i].getIsDone()) {
@@ -30,20 +30,19 @@ public class Duke {
                     int taskIndex = Integer.parseInt(processedCommand[1]) - 1;
                     tasks[taskIndex].setIsDone(true);
                     System.out.println("    Nice! I've marked this task as done:" + "\n"
-                            + "      [X]" + tasks[taskIndex].getDescription() );
+                            + "      [X] " + tasks[taskIndex].getDescription());
                     break;
                 case "unmark":
                     taskIndex = Integer.parseInt(processedCommand[1]) - 1;
                     tasks[taskIndex].setIsDone(false);
                     System.out.println("    Ok! I've marked this task as not done yet:" + "\n"
-                            + "      [ ]" + tasks[taskIndex].getDescription());
+                            + "      [ ] " + tasks[taskIndex].getDescription());
                     break;
                 default:
-                    System.out.println("    added: " + processedCommand[0]);
+                    System.out.println("    added: " + comm);
                     Task newTask = new Task(comm);
                     tasks[numOfTasks] = newTask;
                     numOfTasks++;
-
             }
         }
     }
