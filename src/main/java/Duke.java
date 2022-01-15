91d9ffec1c2411291433abdba1e22528dcd546c5
Ho Lanzan (Leon) <ho.leon98@gmail.com>
2022-01-15 23:04:29 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,9 +10,21 @@ public class Duke {
         System.out.println("Woof, I am (supposed to look like) a dog bot. \n" + logo + "\n" +  "What do you want from me?\n");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        String[] strArr = new String[100];
+        int size = 1;
         while (!str.equals("bye")){
-            System.out.println(str);
-            str = sc.nextLine();
+            if (str.equals("list")){
+                for (int i = 1; i < size; i++) {
+                    System.out.println(i + ". " + strArr[i]);
+                }
+                str = sc.nextLine();
+            }
+            else {
+                System.out.println("added : " + str);
+                strArr[size] = str;
+                size++;
+                str = sc.nextLine();
+            }
         }
         System.out.println("Bye! Hope not to see you again :)");
         sc.close();
