f8c952ca84cae83fbc3efea9a877fe813231d4c8
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2022-01-15 21:25:48 +0800

    Added extensions to commands so as to accomodate new classes(i.e. Task, ToDo, Event and Deadline)
    processedCommand is further processed to obtain deadline and description which are used to create Event and/or Deadline instances
    Logic of printing whether task is marked or unmarked is now handle by the individual classes(Event,Deadline,ToDo,Task)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,24 +19,47 @@ public class Duke {
                     System.out.println("    Here are the tasks in your list:");
                     for (int i = 0; i < numOfTasks; i++) {
                         int indexToPrint = i + 1;
-                        if (tasks[i].getIsDone()) {
-                            System.out.println("    " + indexToPrint + ".[X] " + tasks[i].getDescription());
-                        } else {
-                            System.out.println("    " + indexToPrint + ".[ ] " + tasks[i].getDescription());
-                        }
+                        System.out.println("    " + indexToPrint + "." + tasks[i].identify());
                     }
                     break;
                 case "mark":
                     int taskIndex = Integer.parseInt(processedCommand[1]) - 1;
                     tasks[taskIndex].setIsDone(true);
                     System.out.println("    Nice! I've marked this task as done:" + "\n"
-                            + "      [X] " + tasks[taskIndex].getDescription());
+                            + "      " + tasks[taskIndex].identify());
                     break;
                 case "unmark":
                     taskIndex = Integer.parseInt(processedCommand[1]) - 1;
                     tasks[taskIndex].setIsDone(false);
                     System.out.println("    Ok! I've marked this task as not done yet:" + "\n"
-                            + "      [ ] " + tasks[taskIndex].getDescription());
+                            + "      " + tasks[taskIndex].identify());
+                    break;
+                case "deadline":
+                    String[] inputForDeadlineConstructor = processedCommand[1].split("/", 2);
+                    inputForDeadlineConstructor[0] = inputForDeadlineConstructor[0].subSequence(0,
+                            inputForDeadlineConstructor[0].length() - 1).toString();
+                    Deadline deadline = new Deadline(inputForDeadlineConstructor[0], inputForDeadlineConstructor[1]);
+                    System.out.println("    Got it. I've added this task: \n" + "      " + deadline.identify());
+                    tasks[numOfTasks] = deadline;
+                    numOfTasks++;
+                    System.out.println(String.format("    Now you have %d tasks in the list.", numOfTasks));
+                    break;
+                case "todo":
+                    ToDo todo = new ToDo(processedCommand[1]);
+                    System.out.println("    Got it. I've added this task: \n" + "      " + todo.identify());
+                    tasks[numOfTasks] = todo;
+                    numOfTasks++;
+                    System.out.println(String.format("    Now you have %d tasks in the list.", numOfTasks));
+                    break;
+                case "event":
+                    String[] inputForEventConstructor = processedCommand[1].split("/", 2);
+                    inputForEventConstructor[0] = inputForEventConstructor[0].subSequence(0,
+                            inputForEventConstructor[0].length() - 1).toString();
+                    Event event = new Event(inputForEventConstructor[0], inputForEventConstructor[1]);
+                    System.out.println("    Got it. I've added this task: \n" + "      " + event.identify());
+                    tasks[numOfTasks] = event;
+                    numOfTasks++;
+                    System.out.println(String.format("    Now you have %d tasks in the list.", numOfTasks));
                     break;
                 default:
                     System.out.println("    added: " + comm);
