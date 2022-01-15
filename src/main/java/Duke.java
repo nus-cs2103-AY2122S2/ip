58f6c6dd7fdae8a782d015cd80224c05e1b41b3d
Wang Jun Hong <e0544005@u.nus.edu>
2022-01-15 12:35:18 +0800

    Added initial functionality to echo messages sent by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Initialize a scanner object
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Hello I am Chi \nHow may I serve you today?");
+
+        // Stores the text input of user
+        String echo;
+        // Request for user input
+        echo = userInput.nextLine();
+        // Check if bye has been said
+        while (!echo.toLowerCase().equals("bye")) {
+            // If not, output their text and request for new message
+            System.out.println(echo);
+            echo = userInput.nextLine();
+        }
+        // Ending statement and close scanner
+        System.out.println("Bye, see you next time nyan~");
+        userInput.close();
     }
 }
