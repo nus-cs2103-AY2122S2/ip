6bf6891e8987ca9011cb7eda028aca4e745c8ef5
Lim Bing Sen <bingsenlim0806@gmail.com>
2022-01-15 20:50:30 +0800

    Add functionality to add task and list task.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     private static final String GREET_MESSAGE
@@ -6,14 +7,16 @@ public class Duke {
             "Which task shall we burn today?";
     private static final String BYE_MESSAGE = "Roarrr....Let's burn more tasks next time!";
     private static final int BORDER_LENGTH = 70;
+    private static ArrayList<Task> tasks;
 
     public static void main(String[] args) {
+        tasks = new ArrayList<Task>();
         greet();
         Scanner sc = new Scanner(System.in);
         System.out.println("What should Charizard do next?");
         String input = sc.nextLine();
         while (input.compareToIgnoreCase("bye") != 0) {
-            show(input);
+            processInput(input);
             System.out.println("What should Charizard do next?");
             input = sc.nextLine();
         }
@@ -22,9 +25,29 @@ public class Duke {
 
     public static void show(String message) {
         drawBorder(BORDER_LENGTH);
-        System.out.println("    " + message);
+        String[] lines = message.split("\n");
+        for (String line: lines) {
+            System.out.println("    " + line);
+        }
         drawBorder(BORDER_LENGTH);
         System.out.println();
+    }
+
+    public static void processInput(String input) {
+        if (input.equalsIgnoreCase("list")) {
+            String message = "Charizard's burning wish list:\n";
+            for (int i = 0; i < tasks.size(); i++) {
+                message += String.format("%d. %s", i, tasks.get(i).toString());
+                if (i < tasks.size() - 1) {
+                    message += "\n";
+                }
+            }
+            show(message);
+        } else {
+            Task newTask = new Task(input);
+            tasks.add(newTask);
+            show(String.format("Charizard is ready to burn task: %s", newTask.toString()));
+        }
     }
 
     public static void exit() {
