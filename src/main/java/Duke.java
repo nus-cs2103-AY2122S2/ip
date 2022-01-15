750c93851e300517c6bcc4ed90538a2c0c30c763
bloodofme <awesome.dragon.wizard@gmail.com>
2022-01-15 17:23:23 +0800

    completed level 2 tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,19 +3,38 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        Task[] myList;
+        myList = new Task[100];
+        int count = 0;
 
         String welcome = "Hello! I'm ChatCat\n" + "What can I do for you?\n";
         System.out.println(welcome);
 
-        String input = "placeholder";
+        String input = " ";
         while (!input.equals("bye")) {
-            input = sc.nextLine(); //reads string.
 
-           if (!input.equals("bye")) {
-               System.out.println(input + "\n");
-           } else {
-               System.out.println("Bye. Hope to see you again soon!");
-           }
+            input = sc.nextLine();
+            switch(input) {
+                case "list":
+                    if (count == 0) {
+                        System.out.println("empty list!");
+                    }
+                    for (int i = 0; i < count; i++) {
+
+
+                        System.out.println((i + 1) + ". " + myList[i].getTask());
+                    }
+                    System.out.println("\n");
+                    break;
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                default:
+                    Task task = new Task(input);
+                    System.out.println("added: " + input + "\n");
+                    myList[count] = task;
+                    count++;
+            }
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private String task;
+    private boolean isDone;
+    private String Symbol;
+    private final String doneSymbol = "[X]";
+    private final String undoneSymbol = "[ ]";
+
+    public Task(String task) {
+        this.task = task;
+        Symbol = undoneSymbol;
+        this.isDone = false;
+    }
+
+    public void setDone() {
+        isDone = true;
+    }
+
+    public void setUnDone() {
+        isDone = false;
+    }
+
+    public String getStatus() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+}
