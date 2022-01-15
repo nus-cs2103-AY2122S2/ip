1f302c88da4295ab800656c9ce4b5f997caa0b66
Teo Hao Yu <thstoneyes@gmail.com>
2022-01-15 15:42:24 +0800

    Added mark as done and mark as undone functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static int id = 0;
-    private static String[] tasks = new String[100];
+    private static final Task[] tasks = new Task[100];
 
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
@@ -24,20 +24,34 @@ public class Duke {
 
         while (userInput.hasNextLine()) {
             String request = userInput.nextLine();
+            String[] parsedReq = request.split(" ");
+
+            // Main response
+            System.out.print(divider);
             if (request.equals("bye")) {
-                System.out.println(divider + "    Goodbye! Till the next time we meet!\n" + divider);
+                System.out.println("    Goodbye! Till the next time we meet!\n");
                 break;
             } else if (request.equals("list")) {
-                System.out.print(divider);
+                System.out.println("    Your outstanding tasks as of now are as listed: ");
                 for (int i = 0; i < id; i++) {
-                    System.out.println(i + 1 + ". " + tasks[i]);
+                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
                 }
-                System.out.println(divider);
+            } else if (parsedReq[0].equals("mark")) {
+                Task t = tasks[Integer.parseInt(parsedReq[1]) - 1];
+                t.markAsDone();
+                System.out.println("    Great job! I've marked the task as completed: ");
+                System.out.println("    " + t);
+            } else if (parsedReq[0].equals("unmark")) {
+                Task t = tasks[Integer.parseInt(parsedReq[1]) - 1];
+                t.markAsUndone();
+                System.out.println("    No problem, I've marked the task as uncompleted: ");
+                System.out.println("    " + t);
             } else {
-                tasks[id] = request;
+                tasks[id] = new Task(request);
                 id++;
-                System.out.println(divider + "    added: " + request + "\n" + divider);
+                System.out.println("    added: " + request);
             }
+            System.out.print(divider);
         }
     }
 }
