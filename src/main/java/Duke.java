d20102545f6ab249f2f38ad84ad4ca8b69406665
Teo Hao Yu <thstoneyes@gmail.com>
2022-01-15 16:31:34 +0800

    Added ToDos, Events and Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,6 +25,7 @@ public class Duke {
         while (userInput.hasNextLine()) {
             String request = userInput.nextLine();
             String[] parsedReq = request.split(" ");
+            String reqType = parsedReq[0];
 
             // Main response
             System.out.print(divider);
@@ -36,23 +37,40 @@ public class Duke {
                 for (int i = 0; i < id; i++) {
                     System.out.println("    " + (i + 1) + ". " + tasks[i]);
                 }
-            } else if (parsedReq[0].equals("mark")) {
+            } else if (reqType.equals("mark")) {
                 Task t = tasks[Integer.parseInt(parsedReq[1]) - 1];
                 t.markAsDone();
                 System.out.println("    Great job! I've marked the task as completed: ");
                 System.out.println("    " + t);
-            } else if (parsedReq[0].equals("unmark")) {
+            } else if (reqType.equals("unmark")) {
                 Task t = tasks[Integer.parseInt(parsedReq[1]) - 1];
                 t.markAsUndone();
                 System.out.println("    No problem, I've marked the task as uncompleted: ");
                 System.out.println("    " + t);
-            } else {
-                tasks[id] = new Task(request);
+            } else if (reqType.equals("todo")) {
+                Task newTask = new Todo(request.substring(5));
+                tasks[id] = newTask;
                 id++;
-                System.out.println("    added: " + request);
+                System.out.println("    Task added: ");
+                System.out.println("    " + newTask);
+            } else if (reqType.equals("deadline")) {
+                int dIndex = request.indexOf("/by");
+                Task newTask = new Deadline(request.substring(9, dIndex), request.substring(dIndex + 4));
+                tasks[id] = newTask;
+                id++;
+                System.out.println("    Task added: ");
+                System.out.println("    " + newTask);
+            } else if (reqType.equals("event")) {
+                int eIndex = request.indexOf("/at");
+                Task newTask = new Event(request.substring(6, eIndex), request.substring(eIndex + 4));
+                tasks[id] = newTask;
+                id++;
+                System.out.println("    Task added: ");
+                System.out.println("    " + newTask);
             }
             System.out.print(divider);
         }
     }
 }
+
 
