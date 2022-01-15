16201a7479fe82cd441ffd52bf5a12d3ec87c95d
bloodofme <awesome.dragon.wizard@gmail.com>
2022-01-15 13:13:57 +0800

    completed Level 1 of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc= new Scanner(System.in);
+
+        String welcome = "Hello! I'm ChatCat\n" + "What can I do for you?\n";
+        System.out.println(welcome);
+
+        String input = "placeholder";
+        while (!input.equals("bye")) {
+            input = sc.nextLine(); //reads string.
+
+           if (!input.equals("bye")) {
+               System.out.println(input + "\n");
+           } else {
+               System.out.println("Bye. Hope to see you again soon!");
+           }
+        }
     }
 }
