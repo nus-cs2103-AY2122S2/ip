c7a50607f77fc5cc4be48397b2165a048f1c6460
Yong Zi Xin <zixinyong448@gmail.com>
2022-01-15 22:19:27 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner; //Import the Scanner class
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Oh hello dear, I'm Dukie, Zi Xin's favourite chattie box\n" +
+                            "Nice to meet you dear:>\n" +
+                            "I'll just repeat what you say for now");
+
+        Scanner myObj = new Scanner(System.in); //Create a Scanner object
+        String input; //declare a string variable to store input
+
+        while (!(input = myObj.nextLine()).equals("bye")) { //check input not "bye"
+            System.out.println(input);
+        }
+        System.out.println("Bye. Hope to see you again soon!"); //ending sentence
     }
 }
