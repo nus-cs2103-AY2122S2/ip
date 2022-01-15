9141ef8acf0884f8a4f7fe6c5fa1a40b4e528bde
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2022-01-15 11:26:44 +0800

    Unable to keep track of commands
    Keep track of commands so users can see past inputs
    
    Added array in Command class to keep track of past input and list when called
    
    list and bye command are not added in tracking of past commands

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,18 +1,31 @@
 public class Command {
+    private static String[] commands = new String[100];
+    private static int numOfCommands = 0;
+    private int indexOfCommand;
     private String command;
     private boolean isExit;
 
     public Command(String command) {
+        if (!command.equals("list") || !command.equals("bye")) {
+            indexOfCommand = numOfCommands;
+            numOfCommands++;
+            commands[indexOfCommand] = command;
+        }
         this.command = command.toLowerCase();
         isExit = false;
     }
 
     public boolean execute() {
         if (command.equals(new String ("bye"))) {
-            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("    Bye. Hope to see you again soon!");
             isExit = true;
+        } else if (command.equals(new String("list"))) {
+            for (int i = 0;i < numOfCommands; i++) {
+                int indexToPrint = i + 1;
+                System.out.println("    " + indexToPrint + ". " + commands[i]);
+            }
         } else {
-            System.out.println("    " + command);
+            System.out.println("    added: " + command);
         }
         return isExit;
     }
