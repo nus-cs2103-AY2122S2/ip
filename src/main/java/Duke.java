fadbc64dc37a3bf2a2ef9780def3f0731f1f5fbc
NatalieTanML <nat130613@gmail.com>
2022-01-15 17:40:17 +0800

    Implemented Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo =
+                  "      _          _          ____       __     __      ___       ____      \n"
+                + "     | |        / \\        |  _ \\      \\ \\   / /     |_ _|     / ___|     \n"
+                + "  _  | |       / _ \\       | |_) |      \\ \\ / /       | |      \\___ \\     \n"
+                + " | |_| |  _   / ___ \\   _  |  _ <   _    \\ V /    _   | |   _   ___) |  _ \n"
+                + "  \\___/  (_) /_/   \\_\\ (_) |_| \\_\\ (_)    \\_/    (_) |___| (_) |____/  (_)\n"
+                + "                                                                          \n";
+        System.out.println("Starting up...\n"
+                + "Online and ready.\n"
+                + logo
+                + "At your service.\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        while (!command.equals("bye"))
+        {
+            echo(command);
+            command = scanner.nextLine();
+        }
+
+        System.out.println("Goodbye. J.A.R.V.I.S. systems powering off...");
+
+    }
+
+    public static void echo(String str)
+    {
+        System.out.println("\t\t------------------------------");
+        System.out.println("\t\t" + str);
+        System.out.println("\t\t------------------------------");
     }
 }
