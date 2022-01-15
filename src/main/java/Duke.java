938d4df9e049897da3f7bbbb453c5702b2fbdb9d
blaketingyu <blaketantingyu@gmail.com>
2022-01-15 14:38:19 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,56 @@
+import java.util.Scanner;
+
 public class Duke {
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //Program's opening message
+        System.out.println("____________________________________________________________");
+        System.out.println("    Wassup! I'm Puke! \uD83E\uDD2E \n    What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        //Scanner initialization for user input
+        Scanner sc = new Scanner(System.in);
+        String userInput = "";
+
+        while (true) {
+            //User Input dictates what the program does
+            userInput = sc.nextLine();
+            //Case 1: Terminate the program
+            if (userInput.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("    Bye Bye \uD83D\uDC4B! Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                //Break to exit
+                break;
+            }
+            //Case 2: List the todolist
+            else if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("    list");
+                System.out.println("____________________________________________________________");
+            }
+
+            //Case 3: blah
+            else if (userInput.equals("blah")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("    blah");
+                System.out.println("____________________________________________________________");
+            }
+
+            //Case 4: If its anything but the commands
+            else {
+                System.out.println("____________________________________________________________");
+                System.out.println("    " + userInput + "?");
+                System.out.println("    What's that? I dont recognise that command\uD83E\uDD2E!\n    Try again! ");
+                System.out.println("____________________________________________________________");
+            }
+
+        }
+
+
+
+
+
     }
 }
