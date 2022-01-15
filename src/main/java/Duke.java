8ee664820b1349d02a6521d61ac53791db4f1867
Teo Hao Yu <thstoneyes@gmail.com>
2022-01-15 17:56:02 +0800

    Added minimal exception handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,65 @@ public class Duke {
     private static int id = 0;
     private static final Task[] tasks = new Task[100];
 
+    public static void listTasks() {
+        System.out.println("    Your outstanding tasks as of now are as listed:");
+        for (int i = 0; i < id; i++) {
+            System.out.println("    " + (i + 1) + ". " + tasks[i]);
+        }
+    }
+
+    public static void markTask(int i) {
+        Task t = tasks[i];
+        t.markAsDone();
+        System.out.println("    Great job! I've marked the task as completed:");
+        System.out.println("    " + t);
+    }
+
+    public static void unmarkTask(int i) {
+        Task t = tasks[i];
+        t.markAsUndone();
+        System.out.println("    No problem, I've marked the task as uncompleted:");
+        System.out.println("    " + t);
+    }
+
+    public static void createTodo(String request, String[] parsedReq) throws DukeException {
+        if (parsedReq.length == 1) {
+            throw new DukeException("The description of a todo cannot be empty.");
+        } else {
+            Task newTask = new Todo(request.substring(5));
+            tasks[id] = newTask;
+            id++;
+            System.out.println("    Task added:");
+            System.out.println("    " + newTask);
+        }
+    }
+
+    public static void createDeadline(String request, String[] parsedReq) throws DukeException {
+        if (parsedReq.length == 1) {
+            throw new DukeException("The description of a deadline cannot be empty.");
+        } else {
+            int dIndex = request.indexOf("/by");
+            Task newTask = new Deadline(request.substring(9, dIndex), request.substring(dIndex + 4));
+            tasks[id] = newTask;
+            id++;
+            System.out.println("    Task added:");
+            System.out.println("    " + newTask);
+        }
+    }
+
+    public static void createEvent(String request, String[] parsedReq) throws DukeException {
+        if (parsedReq.length == 1) {
+            throw new DukeException("The description of an event cannot be empty.");
+        } else {
+            int eIndex = request.indexOf("/at");
+            Task newTask = new Event(request.substring(6, eIndex), request.substring(eIndex + 4));
+            tasks[id] = newTask;
+            id++;
+            System.out.println("    Task added:");
+            System.out.println("    " + newTask);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         String poogie = "    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠉⠉⠉⠉⠉⠉⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
@@ -25,47 +84,29 @@ public class Duke {
         while (userInput.hasNextLine()) {
             String request = userInput.nextLine();
             String[] parsedReq = request.split(" ");
-            String reqType = parsedReq[0];
 
             // Main response
             System.out.print(divider);
-            if (request.equals("bye")) {
-                System.out.println("    Goodbye! Till the next time we meet!");
-            } else if (request.equals("list")) {
-                System.out.println("    Your outstanding tasks as of now are as listed:");
-                for (int i = 0; i < id; i++) {
-                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
+            try {
+                if (request.equals("bye")) {
+                    System.out.println("    Goodbye! Till the next time we meet!");
+                } else if (request.equals("list")) {
+                    listTasks();
+                } else if (request.startsWith("mark")) {
+                    markTask(Integer.parseInt(parsedReq[1]) - 1);
+                } else if (request.startsWith("unmark")) {
+                    unmarkTask(Integer.parseInt(parsedReq[1]) - 1);
+                } else if (request.startsWith("todo")) {
+                    createTodo(request, parsedReq);
+                } else if (request.startsWith("deadline")) {
+                    createDeadline(request, parsedReq);
+                } else if (request.startsWith("event")) {
+                    createEvent(request, parsedReq);
+                } else {
+                    throw new DukeException("My apologies, but it seems that I do not understand your request.");
                 }
-            } else if (reqType.equals("mark")) {
-                Task t = tasks[Integer.parseInt(parsedReq[1]) - 1];
-                t.markAsDone();
-                System.out.println("    Great job! I've marked the task as completed:");
-                System.out.println("    " + t);
-            } else if (reqType.equals("unmark")) {
-                Task t = tasks[Integer.parseInt(parsedReq[1]) - 1];
-                t.markAsUndone();
-                System.out.println("    No problem, I've marked the task as uncompleted:");
-                System.out.println("    " + t);
-            } else if (reqType.equals("todo")) {
-                Task newTask = new Todo(request.substring(5));
-                tasks[id] = newTask;
-                id++;
-                System.out.println("    Task added:");
-                System.out.println("    " + newTask);
-            } else if (reqType.equals("deadline")) {
-                int dIndex = request.indexOf("/by");
-                Task newTask = new Deadline(request.substring(9, dIndex), request.substring(dIndex + 4));
-                tasks[id] = newTask;
-                id++;
-                System.out.println("    Task added:");
-                System.out.println("    " + newTask);
-            } else if (reqType.equals("event")) {
-                int eIndex = request.indexOf("/at");
-                Task newTask = new Event(request.substring(6, eIndex), request.substring(eIndex + 4));
-                tasks[id] = newTask;
-                id++;
-                System.out.println("    Task added:");
-                System.out.println("    " + newTask);
+            } catch (DukeException e) {
+                System.out.println("    " + e.getMessage());
             }
             System.out.print(divider);
         }
