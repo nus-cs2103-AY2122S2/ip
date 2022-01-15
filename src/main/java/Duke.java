313864a6d77ed555be69f7dd220a0cca7a2552ad
zihaowrez <wuzihaohy@gmail.com>
2022-01-15 14:18:44 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,18 +10,29 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String frame = "_____________________________________________\n";
-        System.out.println(frame + "Hello! I\'m " + name + "\nWhat can I do for you?\n" + frame);
+        String frame = "_____________________________________________";
+        System.out.println(frame + "\nHello! I\'m " + name + "\nWhat can I do for you?\n" + frame + "\n");
 
         Scanner sc = new Scanner(System.in);
         String command;
+        List<String> tasks = new ArrayList<String>();
         while (true) {
             command = sc.nextLine();
             if (command.equals("bye")) {
-                System.out.println(frame + "Bye. Hope to see you again soon!\n" + frame);
+                System.out.println(frame + "\nBye. Hope to see you again soon!\n" + frame + "\n");
                 break;
+            } else if (command.equals("list")) {
+                System.out.println(frame);
+                if (tasks.size() == 0) {
+                    System.out.println("There are no tasks in your list~");
+                }
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i+1) + ". " + tasks.get(i));
+                }
+                System.out.println(frame + "\n");
             } else {
-                System.out.println(frame + command + "\n" + frame);
+                tasks.add(command);
+                System.out.println(frame + "\nadded: " + command + "\n" + frame + "\n");
             }
         }
     }
