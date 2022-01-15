0ad5ea53632e9621e95118bea4498d1faadfee53
NatalieTanML <nat130613@gmail.com>
2022-01-15 21:54:59 +0800

    Implemented Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int count = 1;
+
         String logo =
                   "      _          _          ____       __     __      ___       ____      \n"
                 + "     | |        / \\        |  _ \\      \\ \\   / /     |_ _|     / ___|     \n"
@@ -15,21 +18,35 @@ public class Duke {
                 + "At your service.\n");
 
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-        while (!command.equals("bye"))
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye"))
         {
-            echo(command);
-            command = scanner.nextLine();
+            if (input.equals("list"))
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 1; i < count; i++)
+                {
+                    sb.append(i).append(". ").append(tasks[i]).append(i + 1 == count ? "" : "\n");
+                }
+                echo(sb.toString());
+            }
+            else
+            {
+                tasks[count] = input;
+                count++;
+                echo("Added: " + input);
+            }
+            input = scanner.nextLine();
         }
 
         System.out.println("Goodbye. J.A.R.V.I.S. systems powering off...");
-
     }
 
     public static void echo(String str)
     {
-        System.out.println("\t\t------------------------------");
-        System.out.println("\t\t" + str);
-        System.out.println("\t\t------------------------------");
+        System.out.println("------------------------------");
+        System.out.println(str);
+        System.out.println("------------------------------");
     }
 }
