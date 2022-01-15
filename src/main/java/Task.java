29df7ccbf04fc49158e983e7f465dc375241774c
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2022-01-15 21:19:19 +0800

    added Task class
    To allow future classes to inherit from
    
    added way to identify type of task and indicate whether is completed
    
    to allow printing of output just by calling identity method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,4 +18,13 @@ public class Task {
     public String getDescription(){
         return description;
     }
+
+    public String identify(){
+        if (isDone) {
+            return String.format("[X] %s", description);
+        } else {
+            return String.format("[ ] %s", description);
+        }
+    }
+
 }
