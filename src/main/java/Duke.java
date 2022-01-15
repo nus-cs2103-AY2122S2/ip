2a4e767f1abdaf2c2694bc867c83e360ff59a9d6
Ho Lanzan (Leon) <ho.leon98@gmail.com>
2022-01-15 20:00:39 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "       __  \n"
+                + "(____()'`; \n"
+                + "/,    /` \n"
+                + "\\\\\"--\\\\\n";
+
+        System.out.println("Woof, I am (supposed to look like) a dog bot. \n" + logo + "\n" +  "What do you want from me?\n");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        while (!str.equals("bye")){
+            System.out.println(str);
+            str = sc.nextLine();
+        }
+        System.out.println("Bye! Hope not to see you again :)");
+        sc.close();
     }
 }
