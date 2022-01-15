6a65d1e999328e79ec515b20a1af774ff4e6cf58
zihaowrez <wuzihaohy@gmail.com>
2022-01-15 15:59:25 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Duke {
+
     public static void main(String[] args) {
         String name = "Enkel";
         String logo = " ____        _        \n"
@@ -15,9 +18,11 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String command;
-        List<String> tasks = new ArrayList<String>();
+        List<Task> tasks = new ArrayList<Task>();
+        Pattern p_mark = Pattern.compile(" *(un)?mark +\\d*([1-9]+\\d*) *", Pattern.CASE_INSENSITIVE);
         while (true) {
             command = sc.nextLine();
+            Matcher m_mark = p_mark.matcher(command);
             if (command.equals("bye")) {
                 System.out.println(frame + "\nBye. Hope to see you again soon!\n" + frame + "\n");
                 break;
@@ -25,15 +30,37 @@ public class Duke {
                 System.out.println(frame);
                 if (tasks.size() == 0) {
                     System.out.println("There are no tasks in your list~");
-                }
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i+1) + ". " + tasks.get(i));
+                } else {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
                 }
                 System.out.println(frame + "\n");
+            } else if (m_mark.matches()) {
+                int i = Integer.parseInt(m_mark.group(2));
+                if (m_mark.group(1) != null) {
+                    if (i > 0 && i <= tasks.size()) {
+                        tasks.get(i - 1).markNotDone();
+                        System.out.println(frame + "\nOK, I've marked this task as not done yet:\n"
+                                + tasks.get(i - 1) + "\n" + frame + "\n");
+                    } else {
+                        System.out.println(frame + "\nIndex is invalid\n" + frame + "\n");
+                    }
+                } else {
+                    if (i > 0 && i <= tasks.size()) {
+                        tasks.get(i - 1).markAsDone();
+                        System.out.println(frame + "\nNice! I've marked this task as done:\n"
+                                + tasks.get(i - 1) + "\n" + frame + "\n");
+                    } else {
+                        System.out.println("Index is invalid");
+                    }
+                }
             } else {
-                tasks.add(command);
+                tasks.add(new Task(command));
                 System.out.println(frame + "\nadded: " + command + "\n" + frame + "\n");
             }
         }
     }
+
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markNotDone() {
+        isDone = false;
+    }
+
+}
