8d127157a0d31db2d31be806651d3c558077c927
tzhan98 <77192480+tzhan98@users.noreply.github.com>
2022-01-15 14:44:22 +0800

    Level 1 Complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        boolean isBye = false;
+        while(!isBye){
+            isBye = echo(sc.nextLine());
+        }
+    }
+
+    public static boolean echo(String s){
+        if (s.equals("bye")){
+            System.out.println("Bye. I don't want to see you anytime soon! :)");
+            return true;
+        } else {
+            System.out.println(s);
+            return false;
+        }
     }
 }
