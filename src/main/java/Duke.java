e7be795a1c3db50910cdcc15e23847299b19398a
ZF <zifoongl@gmail.com>
2022-01-15 21:03:43 +0800

    Level-3 implemeneted

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,10 +11,10 @@ public class Duke {
         System.out.println("Howdy and welcome to\n" + logo + "\n" + "Feel free to tell duke any tasks you'd like!");
 
         String inputData;
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
 
         while(true) {
-            Scanner scan = new Scanner(System.in);
             inputData = scan.nextLine();
             if(inputData.equals("bye")) {
                 System.out.println("~BYE!~ Come back to Duke anytime");
@@ -22,11 +22,40 @@ public class Duke {
             }
             else if(inputData.equals("list")){
                 for(int i = 1; i <= list.size(); i++){
-                    String text = list.get(i-1);
-                    System.out.println(i + ": " + text);
+                    Task task = list.get(i-1);
+                    System.out.print(i + ": ");
+                    task.printTask();
                 }
-            }else{
-                list.add(inputData);
+            } else if(inputData.contains("unmark")){
+                String[] splitted = inputData.split("\\s+");
+                int index = Integer.parseInt(splitted[1]);
+                if(index >= list.size()){
+                    System.out.println("Index out of bounds, please try again");
+                } else {
+                    list.get(index).setDone(false);
+                    for (int i = 1; i <= list.size(); i++) {
+                        Task task = list.get(i - 1);
+                        System.out.print(i + ": ");
+                        task.printTask();
+                    }
+                }
+            } else if(inputData.contains("mark")){
+                String[] splitted = inputData.split("\\s+");
+                int index = Integer.parseInt(splitted[1]);
+                if(index >= list.size()){
+                    System.out.println("Index out of bounds, please try again");
+                } else {
+                    list.get(index).setDone(true);
+                    for (int i = 1; i <= list.size(); i++) {
+                        Task task = list.get(i - 1);
+                        System.out.print(i + ": ");
+                        task.printTask();
+                    }
+                }
+            }
+            else{
+                Task newTask = new Task(inputData);
+                list.add(newTask);
                 System.out.println("added: " + inputData);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    //Attributes of a task
+    String taskName;
+    boolean done;
+
+    public Task(String taskName){
+        this.taskName = taskName;
+        this.done = false;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public void printTask(){
+        if(this.done){
+            System.out.println("[X] " + this.taskName);
+        } else {
+            System.out.println("[ ] " + this.taskName);
+        }
+    }
+}
