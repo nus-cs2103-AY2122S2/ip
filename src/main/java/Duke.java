8066792e69ffa3cf0047bd1c112964ac58e8c0ed
blaketingyu <blaketantingyu@gmail.com>
2022-01-15 15:58:25 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+//Author: Tan Ting Yu
+//Student Number: A218235J
+
+
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
@@ -6,20 +11,26 @@ public class Duke {
     public static void main(String[] args) {
         //Program's opening message
         System.out.println("____________________________________________________________");
-        System.out.println("    Wassup! I'm Puke! \uD83E\uDD2E \n    What can I do for you?");
+        System.out.println("    Wassup! I'm Puke! \n    What can I do for you?");
         System.out.println("____________________________________________________________");
 
         //Scanner initialization for user input
         Scanner sc = new Scanner(System.in);
         String userInput = "";
 
+        //Task List
+        ArrayList<Task> taskList = new ArrayList<>();
+        //Counter for number of tasks in Task List
+        int numOfTasks = 0;
+
         while (true) {
             //User Input dictates what the program does
             userInput = sc.nextLine();
+
             //Case 1: Terminate the program
             if (userInput.equals("bye")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("    Bye Bye \uD83D\uDC4B! Hope to see you again soon!");
+                System.out.println("    Bye Bye! Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 //Break to exit
                 break;
@@ -27,23 +38,74 @@ public class Duke {
             //Case 2: List the todolist
             else if (userInput.equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("    list");
+                System.out.println("    Here are the tasks in your list:");
+                for (int i = 0; i < numOfTasks; i++)
+                    System.out.println("    " + (i + 1) + ". " + taskList.get(i));
                 System.out.println("____________________________________________________________");
             }
 
-            //Case 3: blah
-            else if (userInput.equals("blah")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("    blah");
-                System.out.println("____________________________________________________________");
+            //Case 3: mark or unmark the task
+            else if (userInput.contains("mark")) {
+                String[] splitString = userInput.split(" ");
+                //If no second input
+                if (splitString[1].equals(""))
+                    System.out.println("Please input a second input. Eg mark 1, unmark 2");
+
+                //Handle error if the second input is not an integer
+                try {
+                    //Gets the index of the task in the task list
+                    int index = Integer.parseInt(splitString[1]);
+                    //If index is out of range, throw illegal argument exception
+                    if (index < 0 || index > numOfTasks) {
+                        throw new IllegalArgumentException();
+                    }
+
+                    //If no errors, continue
+                    //Gets the task that we want to mark or unmark
+                    Task thatTask = taskList.get(index - 1);
+
+                    System.out.println("____________________________________________________________");
+                    String displayMessage = "";
+                    if (splitString[0].equals("mark")) {
+                        displayMessage = "    Nice! I've marked this task as done:";
+                        thatTask.markTask();
+                    } else {
+                        displayMessage = "  OK, I've marked this task as not done yet:";
+                        thatTask.unmarkTask();
+                    }
+
+                    System.out.println(displayMessage);
+                    System.out.println("    " + thatTask);
+                    System.out.println("____________________________________________________________");
+
+
+                //NumberFormatException is thrown if the second input is not an integer
+                } catch (NumberFormatException nfe) {
+                    System.out.println("    Second input has to be an integer! Eg mark 1, unmark 2");
+                    System.out.println("____________________________________________________________");
+
+                //IllegalArgumentException is thrown if the second input is out of range
+                } catch (IllegalArgumentException e) {
+                    System.out.println("    Out of range! Number has to be between 1 and " + numOfTasks);
+                    System.out.println("____________________________________________________________");
+                }
+
             }
 
-            //Case 4: If its anything but the commands
+
+            //Case 4: Add the task
             else {
-                System.out.println("____________________________________________________________");
-                System.out.println("    " + userInput + "?");
-                System.out.println("    What's that? I dont recognise that command\uD83E\uDD2E!\n    Try again! ");
-                System.out.println("____________________________________________________________");
+                if (userInput.equals("")) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("    Task cannot be empty!");
+                    System.out.println("____________________________________________________________");
+                } else {
+                    taskList.add(new Task(userInput));
+                    numOfTasks++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("    added: " + userInput);
+                    System.out.println("____________________________________________________________");
+                }
             }
 
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Task {
+import java.util.ArrayList;
+
+
+public class Task {
+    private String taskName;
+    private Boolean completed;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.completed = false;
+    }
+
+    public void markTask() {
+        this.completed = true;
+    }
+
+    public void unmarkTask() {
+        this.completed = false;
+    }
+
+
+
+    @Override
+    public String toString() {
+        String completedOrNah = this.completed? "X":"";
+        String display = "[" + completedOrNah + "] " + taskName;
+        return display;
+    }
+
+
 }
