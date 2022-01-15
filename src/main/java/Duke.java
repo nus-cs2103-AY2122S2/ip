a964cce195bdfcd5332e6dc6b37bebbe6dacaf55
Wei Howe <weihowe99@gmail.com>
2022-01-16 00:45:39 +0800

    mark unmark task done, to refactor code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,14 @@
+import main.java.Task;
 import java.util.Scanner;
 
 public class Duke {
 
     private static int level = 2;
-    private String task_arr[];
+    private Task task_arr[];
     private int curr_task;
 
     public Duke() {
-        this.task_arr = new String[100];
+        this.task_arr = new Task[100];
         this.curr_task = 0;
     }
 
@@ -27,20 +28,29 @@ public class Duke {
     }
 
     public void handle(String user_command) {
+        String words[] = user_command.split(" ", 2);
         horizontal();
         if (user_command.equals("list")) {
             if (curr_task == 0) {
                 System.out.println("No tasks added yet!");
             } else {
                 for (int i = 0; i < curr_task; i++) {
-                    System.out.println(i+1 + ". " + task_arr[i]);
+                    System.out.println(i+1 + "." + "[" + task_arr[i].getStatusIcon() + "] " + task_arr[i].getDescription());
                 }
             }
+        } else if (words[0].equals("mark")){
+            System.out.println("Cool! You seemed to have been productive just like me! I've marked this task as done: ");
+            task_arr[Integer.parseInt(words[1]) - 1].markDone();
+            System.out.println( "[" + task_arr[Integer.parseInt(words[1]) - 1].getStatusIcon() + "] " + task_arr[Integer.parseInt(words[1]) - 1].getDescription());
+        } else if (words[0].equals("unmark")){
+            System.out.println("Did you mess up something? Fine... I'll mark it as undone heh but I believe you can do it!: ");
+            task_arr[Integer.parseInt(words[1]) - 1].markUndone();
+            System.out.println( "[" + task_arr[Integer.parseInt(words[1]) - 1].getStatusIcon() + "] " + task_arr[Integer.parseInt(words[1]) - 1].getDescription());
         } else {
-            task_arr[curr_task] = user_command;
+            task_arr[curr_task] = new Task(user_command);
             System.out.println("added new task: "+ user_command);
             curr_task++;
-        }
+            }
         horizontal();
     }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+package main.java;
+
+// Credits: partial solution from
+// https://nus-cs2103-ay2122s2.github.io/website/schedule/week2/project.html
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markUndone() {
+        isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
