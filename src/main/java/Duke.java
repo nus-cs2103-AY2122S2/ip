a76ef2441254ecd89b28f9678e59edd2fe39f617
Wang Jun Hong <e0544005@u.nus.edu>
2022-01-15 14:13:17 +0800

    Added functionality to store and print messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import java.util.Scanner;
 
 public class Duke {
+    /** Stores the messages sent by the user. */
+    private static List<String> messages = new ArrayList<>();
+
     public static void main(String[] args) {
         // Initialize a scanner object
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Hello I am Chi \nHow may I serve you today?");
+        System.out.println("Hello master I am Chi \nHow may I serve you today nyan~?");
 
         // Stores the text input of user
         String echo;
@@ -12,12 +18,23 @@ public class Duke {
         echo = userInput.nextLine();
         // Check if bye has been said
         while (!echo.toLowerCase().equals("bye")) {
-            // If not, output their text and request for new message
+            if (echo.toLowerCase().equals("list")) {
+                int i = 1;
+                for (String s: messages) {
+                    System.out.printf("%d. %s\n",i, s);
+                    ++i;
+                }
+                echo = userInput.nextLine();
+                continue;
+            }
+            // Echo message sent by user
             System.out.println(echo);
+            // Store message
+            messages.add(echo);
             echo = userInput.nextLine();
         }
         // Ending statement and close scanner
-        System.out.println("Bye, see you next time nyan~");
+        System.out.println("Sayonara, see you next time nyan~");
         userInput.close();
     }
 }
