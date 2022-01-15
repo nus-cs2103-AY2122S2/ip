620e4c3b63f61a6fec3dec52a75a8094f0a56dfb
ChanWeiJie <chanweijie@ymail.com>
2022-01-15 12:19:09 +0800

    Completed Level 2. Add & List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String taskList[] = new String[100];
+    private static int numOfTask = 0;
+
+    public static void addToList(String s){
+        taskList[numOfTask] = s;
+        numOfTask++;
+        System.out.println("added: " + s);
+    }
+
+    public static void printList(){
+        for(int i = 0; i < numOfTask; i++){
+            String output = i + 1 + "." + " " + taskList[i];
+            System.out.println(output);
+        }
+    }
 
     public static void main(String[] args) {
         String greeting = " Hello! I'm TaskJamie \n What can i do for you?";
@@ -12,8 +27,11 @@ public class Duke {
             String input = sc.nextLine();
             if(input.equals("bye")){
                 break;
+            } else if(input.equals("list")) {
+                printList();
+            } else {
+                addToList(input);
             }
-            System.out.println(input);
         }
         System.out.println(ending);
     }
