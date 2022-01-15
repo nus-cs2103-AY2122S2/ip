299a65947a284c4ace7854381368eb2124f947f9
zhengzikang <zhengzikangzzz@gmail.com>
2022-01-15 17:31:04 +0800

    Implement MeepException and handle possible errors
    Exceptions provide the means to separate the details of what to do when
    something out of the ordinary happens from the main logic of a program.
    
    MeepException handles invalid command, invalid date/time format and
    invalid task number.

--- src/main/java/Meep.java
+++ src/main/java/Meep.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Meep {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Utils.printLogo();
         String userInput = "In";
         String outputMsg = "Out";
@@ -15,62 +14,57 @@ public class Meep {
         while (true) {
             System.out.print("You:");
             userInput = sc.nextLine();
-            String[] arr = userInput.split(" ", 2);
-            String userCommand = arr[0];
 
+            String[] parsedInput=null;
+            try{
+                parsedInput=Utils.parseUserInput(userInput,taskList);
+            }catch (InvalidInputException e){
+                System.out.print("Meep:");
+                System.out.println(e.getMessage());
+                continue;
+            }
+            String userCommand=parsedInput[0];
 
             if (userCommand.equals("bye")) {
                 System.out.println("Meep:Bye. Hope to see you again soon!");
                 break;
             } else if (userCommand.equals("list")) {
                 System.out.println("Meep:");
-                int i = 1;
-                for (Task task : taskList) {
-                    System.out.println("     " + i + ".  " + task.toString());
-                    i++;
-                }
+                Utils.printTaskList(taskList);
             } else if (userCommand.equals("mark")) {
-                String theRest = arr[1];
+                String theRest = parsedInput[1];
                 Task task = taskList.get(Integer.parseInt(theRest) - 1);
                 task.markDone();
-
                 System.out.println("Meep:Nice! I've marked this task as done:");
                 System.out.println("     " + task.toString());
             } else if (userCommand.equals("unmark")) {
-                String theRest = arr[1];
+                String theRest = parsedInput[1];
                 Task task = taskList.get(Integer.parseInt(theRest) - 1);
                 task.unmarkDone();
 
                 System.out.println("Meep:OK, I've marked this task as not done yet:");
                 System.out.println("     " + task.toString());
             } else if (userCommand.equals("deadline")) {
-                String[] output = arr[1].split("/", 2);
-                String[] newoutput = output[1].split(" ", 2);
-                Deadline deadline = new Deadline(output[0],newoutput[1]);
+                Deadline deadline = new Deadline(parsedInput[1], parsedInput[3]);
                 taskList.add(deadline);
 
                 System.out.println("Meep:Got it. I've added this task:");
                 System.out.println(deadline.toString());
-                System.out.println("Now you have "+ taskList.size()+" tasks in the list.");
+                System.out.println("Now you have " + taskList.size() + " tasks in the list.");
             } else if (userCommand.equals("event")) {
-                String[] output = arr[1].split("/", 2);
-                String[] newoutput = output[1].split(" ", 2);
-                Event event = new Event(output[0],newoutput[1]);
+                Event event = new Event(parsedInput[1], parsedInput[3]);
                 taskList.add(event);
 
                 System.out.println("Meep:Got it. I've added this task:");
                 System.out.println(event.toString());
-                System.out.println("Now you have "+ taskList.size()+" tasks in the list.");
+                System.out.println("Now you have " + taskList.size() + " tasks in the list.");
             } else if (userCommand.equals("todo")) {
-                outputMsg = arr[1];
-                ToDo todo = new ToDo(outputMsg);
+                ToDo todo = new ToDo(parsedInput[1]);
                 taskList.add(todo);
 
                 System.out.println("Meep:Got it. I've added this task:");
                 System.out.println(todo.toString());
-                System.out.println("Now you have "+ taskList.size()+" tasks in the list.");
-            } else {
-                System.out.println("Meep:else ");
+                System.out.println("Now you have " + taskList.size() + " tasks in the list.");
             }
         }
         sc.close();
--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.util.List;
+
 public final class Utils {
     public static void printLogo() {
         System.out.println("____________________________________________________________");
@@ -8,5 +11,64 @@ public final class Utils {
         System.out.println("*    *    *  ******  ******    *");
         System.out.println("Hello! I'm Meep\n" + "What can I do for you?");
         System.out.println("____________________________________________________________");
+    }
+
+    public static String[] parseUserInput(String userInput, List<Task> tasks) throws InvalidInputException {
+        // catch empty input
+        if (userInput.trim().length() == 0)
+            throw new InvalidInputException("Empty");
+        String[] arr = userInput.split(" ", 2);
+        // command might be "list" or "bye"
+        if (arr.length == 1) {
+            if (arr[0].equals("list") || arr[0].equals("bye"))
+                return arr;
+            else
+                throw new InvalidInputException("Invalid length 1 command. Please try 'list/bye'.");
+        } else {
+            if (arr[0].equals("mark") || arr[0].equals("unmark") || arr[0].equals("delete")) {
+                String theRest = arr[1];
+                int num;
+                try {
+                    num = Integer.parseInt(theRest);
+                    if (num < 0 || num > tasks.size())
+                        throw new InvalidInputException("Task number out of range");
+                    return arr;
+                } catch (NumberFormatException e) {
+                    throw new InvalidInputException("Invalid task number. Please enter a valid integer.");
+                }
+            } else if (arr[0].equals("deadline") || arr[0].equals("event")) {
+                String[] output = arr[1].split("/", 2);
+
+                if (output.length == 1)
+                    throw new InvalidInputException("Invalid " + arr[0] + " format. eg. deadline return book /by Sunday.");
+
+                String[] date = output[1].split(" ", 2);
+
+                // check Prepositions of Time/Date
+                if (!date[0].equals("on") && !date[0].equals("by") && !date[0].equals("at") && !date[0].equals("in"))
+                    throw new InvalidInputException("Invalid Prepositions of Time. Please use 'in' 'at' 'by' or 'on'.");
+
+                String[] result = new String[4];
+                result[0] = arr[0];
+                result[1] = output[0];
+                result[2] = date[0];
+                result[3] = date[1];
+
+                return result;
+            } else if (arr[0].equals("todo")) {
+                return arr;
+            } else {
+                throw new InvalidInputException("Invalid command. Please try 'mark/unmark/event/deadline/todo'. ");
+            }
+        }
+    }
+
+
+    public static void printTaskList(List<Task> taskList) {
+        int i = 1;
+        for (Task task : taskList) {
+            System.out.println("     " + i + ".  " + task.toString());
+            i++;
+        }
     }
 }
