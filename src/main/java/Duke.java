f777704e74a3ae862ddaac00606efbf22a33dcfe
Teo Hao Yu <thstoneyes@gmail.com>
2022-01-15 14:48:06 +0800

    Added add and list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static int id = 0;
+    private static String[] tasks = new String[100];
+
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         String poogie = "    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠉⠉⠉⠉⠉⠉⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n" +
@@ -24,8 +27,17 @@ public class Duke {
             if (request.equals("bye")) {
                 System.out.println(divider + "    Goodbye! Till the next time we meet!\n" + divider);
                 break;
+            } else if (request.equals("list")) {
+                System.out.print(divider);
+                for (int i = 0; i < id; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
+                System.out.println(divider);
+            } else {
+                tasks[id] = request;
+                id++;
+                System.out.println(divider + "    added: " + request + "\n" + divider);
             }
-            System.out.println(divider + "    added: " + request + "\n" + divider);
         }
     }
 }
