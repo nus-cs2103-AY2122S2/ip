1987c8faa2a0973927dc5af47a644037d4a64877
zihaowrez <wuzihaohy@gmail.com>
2022-01-16 19:32:54 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class Duke {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         String name = "Enkel";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,23 +17,21 @@ public class Duke {
         System.out.println(frame + "\nHello! I\'m " + name + "\nWhat can I do for you?\n" + frame + "\n");
 
         Scanner sc = new Scanner(System.in);
-        String command;
+        String input;
         List<Task> tasks = new ArrayList<Task>();
-        Pattern p_mark = Pattern.compile(" *(un)?mark +\\d*([1-9]+\\d*) *", Pattern.CASE_INSENSITIVE);
         while (true) {
-            command = sc.nextLine();
-            String[] type_desc = command.split(" ", 2);
-            String taskType = type_desc[0];
-            String desc = "";
-            if (type_desc.length == 2) {
-                desc = type_desc[1];
+            input = sc.nextLine();
+            String[] splited = input.split(" ", 2);
+            String firstWord = splited[0];
+            String remaining = "";
+            if (splited.length == 2) {
+                remaining = splited[1];
             }
-            Matcher m_mark = p_mark.matcher(command);
-            if (command.equals("bye")) {
-                System.out.println(frame + "\nBye. Hope to see you again soon!\n" + frame + "\n");
+            System.out.println(frame);
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n" + frame);
                 break;
-            } else if (command.equals("list")) {
-                System.out.println(frame);
+            } else if (input.equals("list")) {
                 if (tasks.size() == 0) {
                     System.out.println("There are no tasks in your list~");
                 } else {
@@ -42,57 +40,66 @@ public class Duke {
                         System.out.println((i + 1) + ". " + tasks.get(i));
                     }
                 }
-                System.out.println(frame + "\n");
-            } else if (m_mark.matches()) {
-                int i = Integer.parseInt(m_mark.group(2));
-                if (m_mark.group(1) != null) {
+            } else if (input.matches("(un)?mark \\d*([1-9]+\\d*)")) {
+                int i = Integer.parseInt(remaining);
+                if (firstWord.equals("unmark")) {
                     if (i > 0 && i <= tasks.size()) {
                         tasks.get(i - 1).markNotDone();
-                        System.out.println(frame + "\nOK, I've marked this task as not done yet:\n"
-                                + tasks.get(i - 1) + "\n" + frame + "\n");
+                        System.out.println("OK, I've marked this task as not done yet:\n"
+                                + tasks.get(i - 1));
                     } else {
-                        System.out.println(frame + "\nIndex is invalid\n" + frame + "\n");
+                        System.out.println("Index is invalid");
                     }
                 } else {
                     if (i > 0 && i <= tasks.size()) {
                         tasks.get(i - 1).markAsDone();
-                        System.out.println(frame + "\nNice! I've marked this task as done:\n"
-                                + tasks.get(i - 1) + "\n" + frame + "\n");
+                        System.out.println("Nice! I've marked this task as done:\n"
+                                + tasks.get(i - 1));
                     } else {
                         System.out.println("Index is invalid");
                     }
                 }
-            } else if (desc.equals("")) {
-                System.out.println(frame + "\nNo description entered\n" + frame + "\n");
-            } else if (taskType.equals("todo")) {
-                Task t = new Todo(desc);
-                tasks.add(t);
-                System.out.println(frame + "\nGot it. I've added this task:\n" + t
-                        + "\nNow you have " + tasks.size() + " tasks in your list.\n"
-                        + frame + "\n");
-            } else if (taskType.equals("deadline")) {
-                String[] desc_by = desc.split(" /by ", 2);
-                String by = "";
-                if (desc_by.length == 2) {
-                    by = desc_by[1];
+            } else if (firstWord.equals("todo")) {
+                if (remaining.equals("")) {
+                    System.out.println("The description of a todo cannot be empty");
+                } else {
+                    Task t = new Todo(remaining);
+                    tasks.add(t);
+                    System.out.println("Got it. I've added this task:\n" + t
+                            + "\nNow you have " + tasks.size() + " tasks in your list.");
                 }
-                Task t = new Deadline(desc_by[0], by);
-                tasks.add(t);
-                System.out.println(frame + "\nGot it. I've added this task:\n" + t
-                        + "\nNow you have " + tasks.size() + " tasks in your list.\n"
-                        + frame + "\n");
-            } else if (taskType.equals("event")) {
-                String[] desc_at = desc.split(" /at ", 2);
-                String at = "";
-                if (desc_at.length == 2) {
-                    at = desc_at[1];
+            } else if (firstWord.equals("deadline")) {
+                if (remaining.equals("")) {
+                    System.out.println("The description of a deadline cannot be empty");
+                } else {
+                    String[] desc_by = remaining.split(" */by *", 2);
+                    String by = "";
+                    if (desc_by.length == 2) {
+                        by = desc_by[1];
+                    }
+                    Task t = new Deadline(desc_by[0], by);
+                    tasks.add(t);
+                    System.out.println("Got it. I've added this task:\n" + t
+                            + "\nNow you have " + tasks.size() + " tasks in your list.");
                 }
-                Task t = new Event(desc_at[0], at);
-                tasks.add(t);
-                System.out.println(frame + "\nGot it. I've added this task:\n" + t
-                        + "\nNow you have " + tasks.size() + " tasks in your list.\n"
-                        + frame + "\n");
+            } else if (firstWord.equals("event")) {
+                if (remaining.equals("")) {
+                    System.out.println("The description of an event cannot be empty");
+                } else {
+                    String[] desc_at = remaining.split(" */at *", 2);
+                    String at = "";
+                    if (desc_at.length == 2) {
+                        at = desc_at[1];
+                    }
+                    Task t = new Event(desc_at[0], at);
+                    tasks.add(t);
+                    System.out.println("Got it. I've added this task:\n" + t
+                            + "\nNow you have " + tasks.size() + " tasks in your list.");
+                }
+            } else {
+                System.out.println("I'm sorry, but I don't know what that means :(");
             }
+            System.out.println(frame + "\n");
         }
     }
 
