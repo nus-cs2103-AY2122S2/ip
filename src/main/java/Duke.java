99ac83fc01ea41c666eb2f394cea7b5a73393783
Lai Mei Tin <e0550504@u.nus.edu>
2022-01-16 14:22:31 +0800

    Add greet and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
-    public static void main(String[] args) {
+    private static String delimiter = "*******************************************************";
+
+    private static void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+    private static void exit() {
+        System.out.println(delimiter);
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+        exit();
     }
 }
