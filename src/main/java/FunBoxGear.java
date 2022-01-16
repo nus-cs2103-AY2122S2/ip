4674a7da401995f398534f80aad8b7b7fe86eb69
junrong98 <e0544375@u.nus.edu>
2022-01-16 15:11:27 +0800

    Update FunBoxGear.java
    Update FunBoxGear.java to follow closely to the OOP principle.
    
    Functions such as isBye, isList etc. used to detect the type of
    message sent by the users has similar behaviour resulting in code
    duplication.
    
    The getCommands() function remove such duplication by combining the
    aforementioned functions together.
    
    FunBoxGear.java has also been updated to make use of Task.java.

--- src/main/java/FunBoxGear.java
+++ src/main/java/FunBoxGear.java
@@ -4,14 +4,14 @@
 public class FunBoxGear {
     public final String GREETING = "Yo! I am FunBox └[∵┌]└[ ∵ ]┘[┐∵]┘ \nWhat can I do for you?";
     private int noOfItems;
-    private String[] itemsList;
+    private Task[] tasksList;
 
     /**
      * Constructor for FunBoxGear
      */
     public FunBoxGear() {
         this.noOfItems = 0;
-        this.itemsList = new String[100];
+        this.tasksList = new Task[100];
     }
 
     /**
@@ -22,29 +22,30 @@ public class FunBoxGear {
     }
 
     /**
-     * Check if message written by the user is "bye"
+     * Check whether if user's input is a commands
      *
-     * @param message The message written by the users on the command prompt
-     * @return return true if the message is a "bye" otherwise return false
+     * @param message The user's input to the command prompt
+     * @return Return false if message is "bye", otherwise return true
      */
-    public boolean isBye(String message) {
-        if (message.equals("bye")) {
+    public boolean getCommands(String message) {
+        String[] formattedMsg = this.formatCommands(message);
+        switch (formattedMsg[0]) {
+        case "bye":
+            this.sayBye();
+            return false;
+        case "list":
+            this.showList();
+            return true;
+        case "mark":
+            this.markDone(formattedMsg);
+            return true;
+        case "unmark":
+            this.markUndone(formattedMsg);
+            return true;
+        default:
+            this.addToList(message);
             return true;
         }
-        return false;
-    }
-
-    /**
-     * Check if message written by the user is "list"
-     *
-     * @param message The message written by the users on the command prompt
-     * @return return true if the message is a "bye" otherwise return false
-     */
-    public boolean isList(String message) {
-        if (message.equals("list")) {
-            return true;
-        }
-        return false;
     }
 
     /**
@@ -52,8 +53,8 @@ public class FunBoxGear {
      *
      * @param message The message written by the users on the command prompt
      */
-    public void addToList(String message) {
-        this.itemsList[noOfItems] = message;
+    private void addToList(String message) {
+        this.tasksList[noOfItems] = new Task(message);
         noOfItems++;
         System.out.println("added: " + message);
     }
@@ -61,10 +62,46 @@ public class FunBoxGear {
     /**
      * Loop through a list of items and print out each item
      */
-    public void showList() {
+    private void showList() {
+        System.out.println("Here are the tasks in your list \uD83D\uDCF3:");
         for (int i = 0; i < this.noOfItems; i++) {
-            System.out.println((i + 1) + ". " + this.itemsList[i]);
+            System.out.println((i + 1) + "." + this.tasksList[i]);
         }
+    }
+
+    /**
+     * Format the user's message to be able to differentiate between special commands
+     *
+     * @param message The user's message to be formatted
+     * @return Return a String array which contains the split message. The first element is used to differentiate
+     * whether it's a message, command, or command which require special formatting
+     */
+    private String[] formatCommands(String message) {
+        // Split message by blank space
+        String[] messageArr = message.split(" ");
+        return messageArr;
+    }
+
+    /**
+     * Mark the item on the list as done
+     *
+     * @param messageArr The formatted message of the user, the second item of the array typically contains the
+     *                   taskNo to be mark as done
+     */
+    private void markDone(String[] messageArr) {
+        int taskNo = Integer.parseInt(messageArr[1]);
+        this.tasksList[taskNo - 1].setDone();
+    }
+
+    /**
+     * Mark the item on the list as not done
+     *
+     * @param messageArr The formatted message of the user, the second item of the array typically contains the
+     *                   taskNo to be mark as not done
+     */
+    private void markUndone(String[] messageArr) {
+        int taskNo = Integer.parseInt(messageArr[1]);
+        this.tasksList[taskNo - 1].setUndone();
     }
 
     /**
