f87d4a4609165d40a92231d9d21354b4e3c00079
nnmoq <77339343+nnmoq@users.noreply.github.com>
2022-01-16 16:12:31 +0800

    Finished Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println("========================");
+
+        while(true) {
+            System.out.print("Me   : ");
+            String message = sc.nextLine();
+            if (message.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.print("Duke : " + message + "\n");
+        }
+        sc.close();
     }
 }
