4117049e6fcb0f604ba75dc3c5fff93ffd1054d4
Samuel Tee <samuel_tee_gy@hotmail.com>
2022-01-16 13:19:53 +0800

    - Implemented Level-3: Mark as Done - Added Task Class - Added new checks for mark and unmark ISSUES: - Many invalid inputs can cause a crash

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,25 +12,38 @@ public class Duke {
         System.out.println(generateIntro());
         // Starting input loop
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> toDoList = new ArrayList<>();
-        boolean done = false;
-        while(!done) {
+        ArrayList<Task> toDoList = new ArrayList<>();
+        while (true) {
             String input = sc.nextLine();
-            if (input.equals("quit") || input.equals("exit")) {
-                done = true;
+            String[] inputArray = input.split(" ");
+            String command = inputArray[0];
+            if (command.equals("quit") || command.equals("exit")) {
                 System.out.println(generateGoodbye());
-            } else if (input.equals("list")) { // List
+                break;
+            } else if (command.equals("list")) { // List
                 String listOutput = "";
                 for (int i = 0; i < toDoList.size(); i++) {
                     if (i != 0) {
-                        listOutput += "\n" + (i + 1) + ". " + toDoList.get(i);
+                        listOutput += "\n" + (i + 1) + ". " + toDoList.get(i).getCurrentStatus();
                     } else {
-                        listOutput += (i + 1) + ". " + toDoList.get(i);
+                        listOutput += (i + 1) + ". " + toDoList.get(i).getCurrentStatus();
                     }
                 }
                 System.out.println(generateResponse(listOutput));
+            } else if (command.equals("mark")) {
+                int taskNo = Integer.parseInt(inputArray[1]) - 1;
+                Task currentTask = toDoList.get(taskNo);
+                currentTask.markAsDone();
+                String output = "Marked as done:\n" + currentTask.getCurrentStatus();
+                System.out.println(output);
+            } else if (command.equals("unmark")) {
+                int taskNo = Integer.parseInt(inputArray[1]) - 1;
+                Task currentTask = toDoList.get(taskNo);
+                currentTask.markAsUndone();
+                String output = "Marked as undone:\n" + currentTask.getCurrentStatus();
+                System.out.println(output);
             } else { // Add to list
-                toDoList.add(input);
+                toDoList.add(new Task(input));
                 String message = "added " + input;
                 System.out.println(generateResponse(message));
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    protected String taskName;
+    protected boolean done;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        return this.taskName;
+    }
+
+    public String getStatusIcon() {
+        return (done ? "x" : " ");
+    }
+
+    public void markAsDone() {
+        this.done = true;
+    }
+
+    public void markAsUndone() {
+        this.done = false;
+    }
+
+    public String getCurrentStatus() {
+        return "[" + getStatusIcon() + "] " + this.taskName;
+    }
+}
