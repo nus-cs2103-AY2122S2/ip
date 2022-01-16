d89db7f6841ea12479b103779196a0ded3ac471a
kwanhw <kwanhaowei@gmail.com>
2022-01-16 21:18:01 +0800

    Implemented Level-2: Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int index = 0;
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -20,14 +22,39 @@ public class Duke {
         while (canContinue) {
             inputTxt = input.nextLine();
 
-            if (inputTxt.equals("bye")) {
-                inputTxt = "Bye. Hope to see you again soon!";
-                canContinue = false;
+            switch (inputTxt) {
+                case "bye":
+                    inputTxt = "Bye. Hope to see you again soon!";
+                    canContinue = false;
+                    break;
+
+                case "list":
+                    inputTxt = listTasks(tasks,index);
+                    break;
+
+                default:
+                    tasks[index] = inputTxt;
+                    index++;
+                    inputTxt = String.format("added: %s",inputTxt);
+                    break;
             }
 
             System.out.println("____________________________________________________________");
             System.out.println(inputTxt);
             System.out.println("____________________________________________________________");
         }
+    }
+
+    public static String listTasks(String[] tasks, int toIndex) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < toIndex; i++) {
+            sb.append(String.format("%d. %s",i+1,tasks[i]));
+
+            if (i != toIndex - 1)  {
+                sb.append("\n");
+            }
+        }
+
+        return sb.toString();
     }
 }
