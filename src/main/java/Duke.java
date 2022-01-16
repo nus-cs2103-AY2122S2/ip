ab06d2f55f23425dadd03189fab76ba51a4fa251
kwanhw <kwanhaowei@gmail.com>
2022-01-16 23:10:54 +0800

    Implemented Level-4: ToDos,Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -39,14 +40,24 @@ public class Duke {
                     toDelete = Integer.valueOf(split[1]) - 1;
                     task = tasks[toDelete];
                     task.markAsDone();
-                    inputTxt = String.format("Nice! I marked this task as done:\n [%s] %s", task.getStatusIcon(), task);
+                    inputTxt = String.format("Nice! I marked this task as done:\n %s", task);
+                    break;
+
+                case "deadline":
+                case "event":
+                case "todo":
+                    String[] taskDetails = processTask(split);
+                    task = buildTask(split[0],taskDetails);
+                    tasks[index]  = task;
+                    index++;
+                    inputTxt = String.format("Got it. I've added this task:\n\t %s\n Now you have %d tasks in the list",task,index);
                     break;
 
                 case "unmark":
                     toDelete = Integer.valueOf(split[1]) - 1;
                     task = tasks[toDelete];
                     task.markAsUndone();
-                    inputTxt = String.format("OK, I've marked this task as not done yet:\n [%s] %s", task.getStatusIcon(), task);
+                    inputTxt = String.format("OK, I've marked this task as not done yet:\n %s", task);
                     break;
 
                 default:
@@ -63,15 +74,61 @@ public class Duke {
     }
 
     public static String listTasks(Task[] tasks, int toIndex) {
-        StringBuilder sb = new StringBuilder("Here are the tasks in your list:\n");
+        StringBuilder sb = new StringBuilder("Here are the tasks in your list:");
         for (int i = 0; i < toIndex; i++) {
-            sb.append(String.format("%d. [%s] %s",i+1, tasks[i].getStatusIcon() ,tasks[i]));
+            sb.append(String.format("\n%d.%s",i+1,tasks[i]));
 
-            if (i != toIndex - 1)  {
-                sb.append("\n");
-            }
+//            if (i != toIndex - 1)  {
+//                sb.append("\n");
+//            }
         }
 
         return sb.toString();
+    }
+
+    public static String[] processTask(String[] input) {
+        int index = 1;
+
+        String delimiter = getDelimiter(input[0]);
+        if (!delimiter.equals("")) {
+            for (int i = 1; i < input.length; i++) {
+                if (input[i].equals(delimiter)) {
+                    index = i;
+                    break;
+                }
+            }
+
+            String description = String.join(" ",Arrays.copyOfRange(input,1,index));
+            String time = String.join(" ",Arrays.copyOfRange(input,index+1,input.length));
+
+            String[] processedTask = {description,time};
+            return processedTask;
+        } else {
+            String[] processedTask = {String.join(" ",Arrays.copyOfRange(input,1,input.length)) };
+           return processedTask;
+        }
+
+    }
+
+    public static String getDelimiter(String action) {
+        switch (action) {
+            case "deadline":
+                return "/by";
+            case "event":
+                return "/at";
+            default:
+                return "";
+        }
+    }
+
+    public static Task buildTask(String action, String[] input) {
+        switch (action) {
+            case "deadline":
+                return new Deadline(input[0],input[1]);
+            case "event":
+                return new Event(input[0],input[1]);
+            default:
+                return new Todo(input[0]);
+        }
     }
 }
