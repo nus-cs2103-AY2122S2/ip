62c20aed5bdf0f745093f99f6eb478bbe98674d1
Tan Yu Tao <tanyutao913@gmail.com>
2022-01-16 18:39:32 +0800

    Setup skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import javax.sound.sampled.Line;
+import java.util.Scanner;
 public class Duke {
+    public static String LINE = "    ____________________________________________________________\n";
+    public static String INDENT = "     ";
     public static void main(String[] args) {
+        //intro messages
+        Scanner sc =  new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(LINE + INDENT + "Hello! I'm Duke\n"+ INDENT + "What can I do for you?\n" + LINE);
+        //main program loop
+        while(true){
+            String input = sc.nextLine();
+            if(input.equals("bye")){
+                break;
+            }
+            System.out.println(LINE + INDENT + input + "\n" + LINE);
+        }
+
+        System.out.println(LINE + INDENT + "Bye. Hope to see you again soon!\n" + LINE);
     }
 }
