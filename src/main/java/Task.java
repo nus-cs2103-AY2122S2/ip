fb969f7579fc9adb2e8dcdadf1bdd3de35cf9ac6
James Yeap <jamesyeap.ys@gmail.com>
2022-01-16 13:52:55 +0800

    Change Task toString() method to reflect its completion status and added setter-functions to mark Task as Done or Not-Done.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,5 +1,6 @@
 public class Task {
     private String name;
+    private boolean done;
 
     public Task(String name) {
         this.name = name;
@@ -9,8 +10,30 @@ public class Task {
         return name;
     }
 
+    public boolean isDone() {
+        return this.done;
+    }
+
+    public void markDone() {
+        this.done = true;
+
+        System.out.println("Awesome! I've marked this task as done:");
+        System.out.println(this);
+    }
+
+    public void markNotDone() {
+        this.done = false;
+
+        System.out.println("Okay, I've marked this task as not done yet:");
+        System.out.println(this);
+    }
+
+    public String getStatusIcon() {
+        return this.done ? "[X]" : "[]";
+    }
+
     @Override
     public String toString() {
-        return this.name;
+        return String.format("%s %s", this.getStatusIcon(), this.getName());
     }
 }
