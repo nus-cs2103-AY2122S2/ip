2f2264fd73e19639e4d6007d8043496e00be53b9
BrendonLau <brendonlcy@hotmail.com>
2022-01-16 22:50:50 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String INPUT_NAME = "You: ";
+    private static final String OUTPUT_NAME = "Duke: ";
+
     public static void main(String[] args) {
+        start();
+        Scanner sc = new Scanner(System.in);
+        String command;
+        do {
+            System.out.print(INPUT_NAME);
+            command = sc.nextLine();
+
+            // main
+            String output = command.equals("bye")
+                    ? "Why are you leaving me? :(" : command;
+            System.out.println(output);
+        } while (!command.equals("bye"));
+        sc.close();
+    }
+
+    public static void start() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String msg = "Hello! I am Duke. \n"
+                + "Your wish is my command.\n\n";
+        System.out.print(logo + msg);
     }
 }
