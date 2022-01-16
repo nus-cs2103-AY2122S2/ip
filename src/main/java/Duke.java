e49ff082461b155c50501b702baa99ff8149c51d
Dane Bagaoisan <dane0515@gmail.com>
2022-01-17 02:00:04 +0800

    Added Task class; list is now a list of tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> strings = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
+        String[] temp;
+        Task tempTask;
         String input;
 
         System.out.println("Hola! soy José\nQué puedo hacer por ti? UwU");
@@ -15,12 +17,24 @@ public class Duke {
                 System.out.println("Adiós. Espero volver a verte pronto!");
                 break;
             } else if (input.equals("list")) {
-                for (int i = 0; i < strings.size(); i++) {
-                    System.out.println(i + 1 + ": " + strings.get(i));
+                System.out.println("Aquí están las tareas en su lista:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i + 1 + ": " + tasks.get(i));
                 }
             } else {
-                System.out.println("added: " + input);
-                strings.add(input);
+                temp = input.split(" ");
+                if (temp[0].equals("mark")) {
+                    tempTask = tasks.get(Integer.parseInt(temp[1]) - 1);
+                    tempTask.mark();
+                    System.out.println("¡Bonito! He marcado esta tarea como hecha:\n" + tempTask);
+                } else if (temp[0].equals("unmark")) {
+                    tempTask = tasks.get(Integer.parseInt(temp[1]) - 1);
+                    tempTask.unmark();
+                    System.out.println("Bien, he marcado esta tarea como aún no realizada:\n" + tempTask);
+                } else {
+                    System.out.println("Agregado: " + input);
+                    tasks.add(new Task(input));
+                }                
             }
         }
 
