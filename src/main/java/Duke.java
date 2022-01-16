2ce2576da20d69ba6ee10b90f7202e585daa0ec8
NatalieTanML <nat130613@gmail.com>
2022-01-16 14:26:26 +0800

    Implemented support for tracking diff. task types
    Implemented Todos, Deadlines and Events in Duke
    
    Formatted/cleaned up code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,8 +6,13 @@ public class Duke {
     private static final String INPUT_MARK = "mark";
     private static final String INPUT_UNMARK = "unmark";
 
-    // set task at index 0 to be a dummy filler value
-    private static final ArrayList<Task> tasks = new ArrayList<>(List.of(new Task("")));
+    private static final String TASK_TODO = "todo";
+    private static final String TASK_DEADLINE = "deadline";
+    private static final String TASK_EVENT = "event";
+    private static final String TASK_BY = "/by";
+    private static final String TASK_AT = "/at";
+
+    private static final Task[] tasks = new Task[100];
     private static int count = 1;
     private static boolean processNext = true;
 
@@ -17,10 +20,11 @@ public class Duke {
         welcome();
 
         Scanner scanner = new Scanner(System.in);
-        String[] input = scanner.nextLine().split(" ");
+        String input = scanner.nextLine();
 
         while (processNext) {
-            switch (input[0]) {
+            String[] tokens = input.split(" ");
+            switch (tokens[0]) {
                 case INPUT_BYE:
                     processNext = false;
                     echo("Goodbye. J.A.R.V.I.S. systems powering off...");
@@ -29,17 +33,27 @@ public class Duke {
                     printTasks();
                     break;
                 case INPUT_MARK:
-                    markAsDone(input);
+                    markAsDone(tokens);
                     break;
                 case INPUT_UNMARK:
-                    markAsUndone(input);
+                    markAsUndone(tokens);
+                    break;
+                case TASK_TODO:
+                    addTodo(input);
+                    break;
+                case TASK_DEADLINE:
+                    addDeadline(input);
+                    break;
+                case TASK_EVENT:
+                    addEvent(input);
                     break;
                 default:
-                    addTask(input);
+                    // TODO: to be implemented in future levels
                     break;
             }
-            input = scanner.nextLine().split(" ");
+            input = scanner.nextLine();
         }
+        scanner.close();
     }
 
     private static void welcome() {
@@ -65,7 +79,7 @@ public class Duke {
     private static void printTasks() {
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i < count; i++) {
-            sb.append(i).append(". ").append(tasks.get(i));
+            sb.append(i).append(".\t").append(tasks[i]);
             if (i + 1 != count) {
                 sb.append("\n");
             }
@@ -73,24 +87,46 @@ public class Duke {
         echo(sb.toString());
     }
 
-    private static void markAsDone(String[] input) {
-        int num = Integer.parseInt(input[1]);
-        Task task = tasks.get(num);
+    private static void markAsDone(String[] tokens) {
+        int num = Integer.parseInt(tokens[1]);
+        Task task = tasks[num];
         task.markAsDone();
-        echo("I've marked the following task as completed:\n" + task);
+        echo("I've marked the following task as completed:\n\t" + task);
     }
 
-    private static void markAsUndone(String[] input) {
-        int num = Integer.parseInt(input[1]);
-        Task task = tasks.get(num);
+    private static void markAsUndone(String[] tokens) {
+        int num = Integer.parseInt(tokens[1]);
+        Task task = tasks[num];
         task.markAsUndone();
-        echo("Understood. I've marked the following task as incomplete:\n" + task);
+        echo("Understood. I've marked the following task as incomplete:\n\t" + task);
     }
 
-    private static void addTask(String[] input) {
-        String description = String.join(" ", input);
-        tasks.add(new Task(description));
+    private static void addTodo(String input) {
+        String description = input.substring(TASK_TODO.length() + 1);
+        tasks[count] = new Todo(description);
+        echo("Got it. I've added this task:\n\t"
+                + tasks[count] + "\n"
+                + "Now you have " + count + " task(s) in your list.");
         count++;
-        echo("Added: " + description);
+    }
+
+    private static void addDeadline(String input) {
+        String[] split = input.split(TASK_BY);
+        String description = split[0].substring(TASK_DEADLINE.length() + 1);
+        tasks[count] = new Deadline(description, split[1]);
+        echo("Got it. I've added this deadline:\n\t"
+                + tasks[count] + "\n"
+                + "Now you have " + count + " task(s) in your list.");
+        count++;
+    }
+
+    public static void addEvent(String input) {
+        String[] split = input.split(TASK_AT);
+        String description = split[0].substring(TASK_EVENT.length() + 1);
+        tasks[count] = new Event(description, split[1]);
+        echo("Got it. I've added this event:\n\t"
+                + tasks[count] + "\n"
+                + "Now you have " + count + " task(s) in your list.");
+        count++;
     }
 }
