2ebe6f9425a3fcc7eb2834432d6c4d6259d81bc3
gz <zhouguanzong@gmail.com>
2022-01-16 17:21:39 +0800

    add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) throws Exception {
@@ -9,17 +10,27 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String command = br.readLine();
+        ArrayList<String> list = new ArrayList<>();
         while (!command.equals("bye")) {
             switch(command) {
                 case "list":
-                    System.out.println("    ____________________________________________________________\n" +
-                            "     list\n" +
-                            "    ____________________________________________________________");
+                    System.out.println("    ____________________________________________________________");
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.printf("     %d. %s%n",i + 1, list.get(i));
+                    }
+                    System.out.println("    ____________________________________________________________");
                     command = br.readLine();
                     break;
                 case "blah":
                     System.out.println("    ____________________________________________________________\n" +
                             "     blah\n" +
+                            "    ____________________________________________________________");
+                    command = br.readLine();
+                    break;
+                default:
+                    list.add(command);
+                    System.out.println("    ____________________________________________________________\n" +
+                            "     added: " + command + "\n" +
                             "    ____________________________________________________________");
                     command = br.readLine();
                     break;
