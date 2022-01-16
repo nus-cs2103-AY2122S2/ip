7b4f0b87a574240225230ad8834a572df2d3b04d
kwanhw <kwanhaowei@gmail.com>
2022-01-16 21:54:41 +0800

    Implemented Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int index = 0;
 
         String logo = " ____        _        \n"
@@ -21,8 +21,11 @@ public class Duke {
         boolean canContinue = true;
         while (canContinue) {
             inputTxt = input.nextLine();
+            String[] split = inputTxt.split(" ");
 
-            switch (inputTxt) {
+            int toDelete;
+            Task task;
+            switch (split[0]) {
                 case "bye":
                     inputTxt = "Bye. Hope to see you again soon!";
                     canContinue = false;
@@ -32,8 +35,22 @@ public class Duke {
                     inputTxt = listTasks(tasks,index);
                     break;
 
+                case "mark":
+                    toDelete = Integer.valueOf(split[1]) - 1;
+                    task = tasks[toDelete];
+                    task.markAsDone();
+                    inputTxt = String.format("Nice! I marked this task as done:\n [%s] %s", task.getStatusIcon(), task);
+                    break;
+
+                case "unmark":
+                    toDelete = Integer.valueOf(split[1]) - 1;
+                    task = tasks[toDelete];
+                    task.markAsUndone();
+                    inputTxt = String.format("OK, I've marked this task as not done yet:\n [%s] %s", task.getStatusIcon(), task);
+                    break;
+
                 default:
-                    tasks[index] = inputTxt;
+                    tasks[index] = new Task(inputTxt);
                     index++;
                     inputTxt = String.format("added: %s",inputTxt);
                     break;
@@ -45,10 +62,10 @@ public class Duke {
         }
     }
 
-    public static String listTasks(String[] tasks, int toIndex) {
-        StringBuilder sb = new StringBuilder();
+    public static String listTasks(Task[] tasks, int toIndex) {
+        StringBuilder sb = new StringBuilder("Here are the tasks in your list:\n");
         for (int i = 0; i < toIndex; i++) {
-            sb.append(String.format("%d. %s",i+1,tasks[i]));
+            sb.append(String.format("%d. [%s] %s",i+1, tasks[i].getStatusIcon() ,tasks[i]));
 
             if (i != toIndex - 1)  {
                 sb.append("\n");
