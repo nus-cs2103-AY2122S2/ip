9270af1e0a2abf7d830143f1e61541567e1258c6
bloodofme <awesome.dragon.wizard@gmail.com>
2022-01-16 14:38:16 +0800

    completed level 6

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 import java.io.*;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Task[] Tasks;
-        Tasks = new Task[100];
+        ArrayList<Task> Tasks = new ArrayList<Task>();
         int count = 0;
 
         String welcome = "Hello! I'm ChatCat\n" + "What can I do for you?\n";
@@ -20,12 +20,12 @@ public class Duke {
 
                 switch(input) {
                     case "list":
-                        if (count == 0) {
+                        if (Tasks.size() == 0) {
                             System.out.println("empty list!");
                         } else {
                             System.out.println("Here are the tasks in your list:");
-                            for (int i = 0; i < count; i++) {
-                                System.out.println((i + 1) + ". " + Tasks[i]);
+                            for (int i = 0; i < Tasks.size(); i++) {
+                                System.out.println((i + 1) + ". " + Tasks.get(i));
                             }
                         }
                         System.out.println("");
@@ -39,36 +39,34 @@ public class Duke {
                         switch(splitInput[0]) {
                             case "mark":
                                 int taskID = Integer.parseInt(splitInput[1]) - 1;
-                                Tasks[taskID].setDone();
+                                Tasks.get(taskID).setDone();
                                 System.out.println("Nice! I've marked this task as done:\n" +
-                                        Tasks[taskID] + "\n");
+                                        Tasks.get(taskID) + "\n");
                                 break;
                             case "unmark":
                                 int taskIDu = Integer.parseInt(splitInput[1]) - 1;
-                                Tasks[taskIDu].setUnDone();
+                                Tasks.get(taskIDu).setUnDone();
                                 System.out.println("OK, I've marked this task as not done yet:\n" +
-                                        Tasks[taskIDu] + "\n");
+                                        Tasks.get(taskIDu) + "\n");
                                 break;
                             case "todo":
                                 if (len == 1) {
                                     throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
                                 }
                                 Todo todo = new Todo(input.substring(5));
-                                Tasks[count] = todo;
-                                System.out.println("Got it. I've added this task:\n" + Tasks[count]);
-                                count++;
-                                System.out.println("Now you have " + count + " tasks in the list." + "\n");
+                                Tasks.add(todo);
+                                System.out.println("Got it. I've added this task:\n" + todo);
+                                System.out.println("Now you have " + Tasks.size() + " tasks in the list." + "\n");
                                 break;
                             case "deadline":
                                 if (len == 1) {
                                     throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
                                 }
                                 String[] split = input.split("/by ");
-                                Deadline deadine = new Deadline(split[0].substring(9), split[1]);
-                                Tasks[count] = deadine;
-                                System.out.println("Got it. I've added this task:\n" + Tasks[count]);
-                                count++;
-                                System.out.println("Now you have " + count + " tasks in the list." + "\n");
+                                Deadline deadline = new Deadline(split[0].substring(9), split[1]);
+                                Tasks.add(deadline);
+                                System.out.println("Got it. I've added this task:\n" + deadline);
+                                System.out.println("Now you have " + Tasks.size() + " tasks in the list." + "\n");
                                 break;
                             case "event":
                                 if (len == 1) {
@@ -76,10 +74,15 @@ public class Duke {
                                 }
                                 split = input.split("/at ");
                                 Event event = new Event(split[0].substring(6), split[1]);
-                                Tasks[count] = event;
-                                System.out.println("Got it. I've added this task:\n" + Tasks[count]);
-                                count++;
-                                System.out.println("Now you have " + count + " tasks in the list." + "\n");
+                                Tasks.add(event);
+                                System.out.println("Got it. I've added this task:\n" + event);
+                                System.out.println("Now you have " + Tasks.size() + " tasks in the list." + "\n");
+                                break;
+                            case "delete":
+                                int toDelete = Integer.parseInt(splitInput[1]) - 1;
+                                Task removed = Tasks.remove(toDelete);
+                                System.out.println("Noted. I've removed this task:\n" + removed);
+                                System.out.println("Now you have " + Tasks.size() + " tasks in the list." + "\n");
                                 break;
                             default:
                                 throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means");
