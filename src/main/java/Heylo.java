5aed520bff450757e04b106c61524c515ece6509
Charisma Kausar <charisma.kausar@gmail.com>
2022-01-17 02:01:59 +0800

    Echo commands or exit program

--- src/main/java/Heylo.java
+++ src/main/java/Heylo.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Heylo {
     public static void main(String[] args) {
         String logo =
@@ -8,6 +10,21 @@ public class Heylo {
                         "|_| |_|  \\___|  \\__, | |_|  \\___/\n" +
                         "                |___/\n";
         System.out.println(logo);
-        System.out.println("Heylo! What can I do for you today? :)");
+        System.out.println("Heylo! What can I do for you today? :)\n");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String command = sc.nextLine();
+            System.out.printf(" ");
+
+            if(command.equals("Bye")) {
+                System.out.println("See you again! :)");
+                return;
+            } else {
+                System.out.println(command);
+            }
+            System.out.println("__________________________________________________________");
+        }
     }
 }
