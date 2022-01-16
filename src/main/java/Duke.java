7c3af5538df9edff2234d64b7b1447038778690b
hanqinilnix <ohanqin@gmail.com>
2022-01-16 19:11:43 +0800

    Added "add" and "list" functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,10 @@ public class Duke {
     private static int LINE_BREAK_LENGTH = 60; // length for line break
     private static String LINE_BREAK = "_".repeat(LINE_BREAK_LENGTH); // string for horizontal line break
 
+    private static int MAX_SIZE = 100;
+    private static int listSize = 0;
+    private static String[] toDoList = new String[MAX_SIZE];
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -27,7 +31,7 @@ public class Duke {
             cmd = scanner.nextLine().toLowerCase();
             if (cmd.equals("bye")) {
                 printWithTab(LINE_BREAK);
-                printWithTab("Have a nice day");
+                printWithTab("Have a nice day Master");
                 printWithTab(LINE_BREAK + "\n");
                 continue;
             } else {
@@ -43,7 +47,25 @@ public class Duke {
 
     public static void command(String command) {
         printWithTab(LINE_BREAK);
-        printWithTab(command);
+        if (command.equals("list")) {
+            if (listSize == 0) {
+                printWithTab("Dear Master, you have not added anything");
+            } else {
+                printWithTab("Dear Master, here is a list of things you have added:\n");
+                for (int i = 0; i < listSize; i++) {
+                    printWithTab(String.format("%d. %s", i + 1, toDoList[i]));
+                }
+            }
+        } else {
+            addTask(command);
+        }
         printWithTab(LINE_BREAK + "\n");
+    }
+
+    public static void addTask(String todo) {
+        toDoList[listSize] = todo;
+        listSize++;
+
+        printWithTab(String.format("Understood, I have added \"%s\" to the list", todo));
     }
 }
