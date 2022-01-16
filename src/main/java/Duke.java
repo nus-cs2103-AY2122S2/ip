5580481d6f4b32b5b96a8dce1038cac522692a56
Lai Mei Tin <e0550504@u.nus.edu>
2022-01-16 14:57:16 +0800

    Add echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     private static String delimiter = "*******************************************************";
 
@@ -11,13 +13,32 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
     }
+
     private static void exit() {
-        System.out.println(delimiter);
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void echo(String command) {
+        System.out.println(command);
+    }
+
+    private static void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println(delimiter);
+            System.out.print("Enter your command: ");
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            } else {
+                echo(command);
+            }
+        }
     }
 
     public static void main(String[] args) {
         greet();
+        run();
         exit();
     }
 }
