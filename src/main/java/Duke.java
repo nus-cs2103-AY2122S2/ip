6c945974cb4a5bb254d04f3025fff92bc7157270
Ashuh <ashuh223@gmail.com>
2022-01-16 17:47:49 +0800

    Implement add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,6 +12,9 @@ public class Duke {
     private static final String TEXT_GOODBYE = "Bye. Hope to see you again soon!";
     private static final String TEXT_DIVIDER = "____________________________________________________________";
     private static final String KEY_EXIT = "bye";
+    private static final String KEY_LIST = "list";
+
+    private final List<String> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         new Duke().run();
@@ -25,22 +30,36 @@ public class Duke {
             if (input.equals(KEY_EXIT)) {
                 sayGoodbye();
                 return;
+            } else if (input.equals(KEY_LIST)) {
+                listTasks();
             } else {
-                echo(input);
+                addTask(input);
             }
         }
     }
 
-    private void greet() {
-        System.out.println(indent(addSeparator(TEXT_LOGO + "\n\n" + TEXT_GREETING)));
+    private void addTask(String input) {
+        tasks.add(input);
+        System.out.println(indent(addSeparator("added: " + input)) + '\n');
     }
 
-    private void echo(String input) {
-        System.out.println(indent(addSeparator(input)));
+    private void listTasks() {
+        System.out.println(indent(TEXT_DIVIDER));
+
+        for (int i = 0; i < tasks.size(); i++) {
+            String entry = (i + 1) + ". " + tasks.get(i);
+            System.out.println(indent(entry));
+        }
+
+        System.out.println(indent(TEXT_DIVIDER) + '\n');
+    }
+
+    private void greet() {
+        System.out.println(indent(addSeparator(TEXT_LOGO + "\n\n" + TEXT_GREETING)) + '\n');
     }
 
     private void sayGoodbye() {
-        System.out.println(indent(addSeparator(TEXT_GOODBYE)));
+        System.out.println(indent(addSeparator(TEXT_GOODBYE)) + '\n');
     }
 
     private String addSeparator(String input) {
