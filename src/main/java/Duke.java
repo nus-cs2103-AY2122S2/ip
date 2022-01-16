cfec524d6c13803fd7f0a84f4f7dd656cf318015
Zechary <E0530207@u.nus.edu>
2022-01-16 10:21:16 +0800

    Add .class files to gitignore, add greet echo exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Introduction
+        String introduction = "____________________________________________________________\nHello! I'm Duke\nWhat can I do for you?\n____________________________________________________________";
+        System.out.println(introduction);
+
+        // Scanner for new input
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+            String[] splitInput = input.split("\\s");
+            switch(splitInput[0]) {
+                case "bye":
+                    System.out.println("____________________________________________________________\nBye! Hope to see you again\n____________________________________________________________");
+                    return;
+                default:
+                    System.out.println("____________________________________________________________\n" + input + "\n____________________________________________________________");
+            }
+        }
     }
 }
