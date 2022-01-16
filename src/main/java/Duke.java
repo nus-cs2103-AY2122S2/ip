146bc62b788d382a68882a050e3c4a83058c6c93
nicollegann <nicollegan@gmail.com>
2022-01-16 15:16:57 +0800

    add level 1: greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "     ____        _        \n"
+                + "    |  _ \\ _   _| | _____ \n"
+                + "    | | | | | | | |/ / _ \\\n"
+                + "    | |_| | |_| |   <  __/\n"
+                + "    |____/ \\__,_|_|\\_\\___|\n";
+
+        String intro = "    Hello! I'm Duke! \n"
+                + "    What can I do for you? \n";
+
+        String line = "    ―――――――――――――――――――――――――――――――――― \n";
+
+        String indent = "    ";
+
+        System.out.print(logo + "\n" + intro + line);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.print(line + indent + input + "\n" + line);
+            input = scanner.nextLine();
+        }
+
+        System.out.print(line + indent + "Bye. Hope to see you again soon!" + "\n" + line);
     }
 }
