7b5e4cad53ef7a21499515094bcbc129b89ad66f
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2022-01-16 11:04:25 +0800

    Modify Duke class
    Change print statements to easily identify what is printed
    Add exception handling for commands where exception may occur due to invalid input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("    Hello! I'm Duke\n" + "    What can I do for you?");
+        System.out.println("    Hello! I'm Duke\n    What can I do for you?");
         Task[] tasks = new Task[100];
         int numOfTasks = 0;
         Scanner sc = new Scanner(System.in);
@@ -10,6 +10,7 @@ public class Duke {
         while (!isExit) {
             String comm = sc.nextLine();
             String[] processedCommand = comm.split(" ", 2);
+            Integer taskIndex = null;
             switch (processedCommand[0]) {
                 case "bye":
                     System.out.println("    Bye. Hope to see you again soon!");
@@ -19,54 +20,115 @@ public class Duke {
                     System.out.println("    Here are the tasks in your list:");
                     for (int i = 0; i < numOfTasks; i++) {
                         int indexToPrint = i + 1;
-                        System.out.println("    " + indexToPrint + "." + tasks[i].identify());
+                        System.out.println(String.format(
+                                "    %d.%s", indexToPrint, tasks[i].identify()));
                     }
                     break;
                 case "mark":
-                    int taskIndex = Integer.parseInt(processedCommand[1]) - 1;
+                    //Exception catching
+                    if (processedCommand.length <= 1) {
+                        new DukeException();
+                        break;
+                    }
+                    try {
+                        taskIndex = Integer.parseInt(processedCommand[1]) - 1;
+                    } catch (NumberFormatException e) {
+                        new DukeException();
+                        break;
+                    }
+
+                    //Mark task
                     tasks[taskIndex].setIsDone(true);
-                    System.out.println("    Nice! I've marked this task as done:" + "\n"
-                            + "      " + tasks[taskIndex].identify());
+                    System.out.println(String.format(
+                            "    Nice! I've marked this task as done:\n    %s", tasks[taskIndex].identify()));
                     break;
                 case "unmark":
-                    taskIndex = Integer.parseInt(processedCommand[1]) - 1;
+                    //Exception catching
+                    if (processedCommand.length <= 1) {
+                        new DukeException();
+                        break;
+                    }
+                    try {
+                        taskIndex = Integer.parseInt(processedCommand[1]) - 1;
+                    } catch (NumberFormatException e) {
+                        new DukeException();
+                        break;
+                    }
+
+                    //Unmark task
                     tasks[taskIndex].setIsDone(false);
                     System.out.println("    Ok! I've marked this task as not done yet:" + "\n"
                             + "      " + tasks[taskIndex].identify());
                     break;
                 case "deadline":
+                    //Exception catching
+                    if (processedCommand.length <= 1) {
+                        new DukeException();
+                        break;
+                    } else if (!processedCommand[1].contains("/") || processedCommand[1].startsWith(("/"))) {
+                        new DukeException();
+                        break;
+                    }
+
+
+                    //Processing input
                     String[] inputForDeadlineConstructor = processedCommand[1].split("/", 2);
                     inputForDeadlineConstructor[0] = inputForDeadlineConstructor[0].subSequence(0,
                             inputForDeadlineConstructor[0].length() - 1).toString();
+
+                    //Create deadline
                     Deadline deadline = new Deadline(inputForDeadlineConstructor[0], inputForDeadlineConstructor[1]);
-                    System.out.println("    Got it. I've added this task: \n" + "      " + deadline.identify());
+                    System.out.println(String.format(
+                            "    Got it. I've added this task: \n    %s", deadline.identify()));
                     tasks[numOfTasks] = deadline;
                     numOfTasks++;
                     System.out.println(String.format("    Now you have %d tasks in the list.", numOfTasks));
                     break;
                 case "todo":
+                    //Exception catching
+                    if (processedCommand.length <= 1) {
+                        new DukeException();
+                        break;
+                    }
+
+                    //Create Todo
                     ToDo todo = new ToDo(processedCommand[1]);
-                    System.out.println("    Got it. I've added this task: \n" + "      " + todo.identify());
+                    System.out.println(String.format(
+                            "    Got it. I've added this task: \n      %s", todo.identify()));
                     tasks[numOfTasks] = todo;
                     numOfTasks++;
                     System.out.println(String.format("    Now you have %d tasks in the list.", numOfTasks));
                     break;
                 case "event":
+                    //Exception catching
+                    if (processedCommand.length <= 1) {
+                        new DukeException();
+                        break;
+                    } else if (!processedCommand[1].contains("/") || processedCommand[1].startsWith(("/"))) {
+                        new DukeException();
+                        break;
+                    }
+
+                    //Input processing
                     String[] inputForEventConstructor = processedCommand[1].split("/", 2);
                     inputForEventConstructor[0] = inputForEventConstructor[0].subSequence(0,
                             inputForEventConstructor[0].length() - 1).toString();
+
+                    //Create event
                     Event event = new Event(inputForEventConstructor[0], inputForEventConstructor[1]);
-                    System.out.println("    Got it. I've added this task: \n" + "      " + event.identify());
+                    System.out.println(String.format(
+                            "    Got it. I've added this task: \n      %s", event.identify()));
                     tasks[numOfTasks] = event;
                     numOfTasks++;
                     System.out.println(String.format("    Now you have %d tasks in the list.", numOfTasks));
                     break;
                 default:
-                    System.out.println("    added: " + comm);
+                    System.out.println(String.format("    added: %s", comm));
                     Task newTask = new Task(comm);
                     tasks[numOfTasks] = newTask;
                     numOfTasks++;
             }
         }
+        sc.close();
     }
 }
