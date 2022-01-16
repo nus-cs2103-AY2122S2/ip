d91d4886de9c0a7a5cad3856be0a0bfdad918c2f
Alexander Er <aceszhenwei@yahoo.com>
2022-01-16 13:35:08 +0800

    Level-4
    Made some adjustments to Task for further abstraction.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,21 @@
+import java.util.Arrays;
 import java.util.Scanner;
-
 
 public class Duke {
     static String line = "------------------------------------";
     static Scanner scanner = new Scanner(System.in);
     static Task[] arr = new Task[100];
 
+    public static <T> int getLength(T[] arr){
+        int count = 0;
+        for(T el : arr)
+            if (el != null)
+                ++count;
+        return count;
+    }
+
     public static void main(String[] args) {
-        int counter = 0;
+
         String logo = "/\\___ \\                            __           \n"
                 + "\\/__/\\ \\     __     _ __   __  __ /\\_\\    ____  \n"
                 + "   _\\ \\ \\  /'__`\\  /\\`'__\\/\\ \\/\\ \\\\/\\ \\  /',__\\ \n"
@@ -20,47 +28,91 @@ public class Duke {
 
         while(true) {
             String input = scanner.nextLine();
+
+            // Exit Feature
             if (input.equals("bye")) {
                 System.out.println(line);
                 System.out.println("Goodbye! I'll be here if you need anything else.");
                 System.out.println(line);
                 break;
             }
+
+            // List Feature
             else if (input.equals("list")) {
                 System.out.println(line);
                 for (int i = 0; i < arr.length; i++) {
                     if (arr[i] == null) {
                         break;
                     }
-                    System.out.println(i+1 + ".[" + arr[i].toggleDone() + "] " + arr[i].name +"\n");
+                    System.out.println(i+1 + "." + arr[i].toString());
                 }
                 System.out.println(line);
             }
+
+            // Mark & Unmark Feature
             else if (input.contains("mark") || input.contains("unmark")) {
                 System.out.println(line);
                 String s = input.replaceAll("\\D+","");
-                int clean = Integer.parseInt(s) - 1;
+                int clean = Integer.parseInt(s) - 1;  // Parse to find what number in list to toggle
+                // Edge Case
                 if (arr[clean] == null) {
                     System.out.println("Error! No tasked added");
                 }
+                // Mark
                 else if (input.toCharArray()[0] != 'u') {
-                    arr[clean].done = true;
+                    arr[clean].setMarked();
                     System.out.println("Nice! I've marked this task as done:\n " +
-                            "   ["  + arr[clean].toggleDone() + "] " + arr[clean].name);
+                            "   "  + arr[clean].toString());
                 }
+                // Unmark
                 else {
-                    arr[clean].done = false;
+                    arr[clean].setUnmarked();
                     System.out.println("OK, I've marked this task as not done yet:\n " +
-                            "   ["  + arr[clean].toggleDone() + "] " + arr[clean].name);
+                            "   "  + arr[clean].toString());
                 }
                 System.out.println(line);
             }
-            else {
+
+            // Deadline Feature
+            else if (input.contains("deadline")) {
                 System.out.println(line);
-                Task newTask = new Task(input);
-                System.out.println("added: " + newTask.name);
-                arr[counter] = newTask;
-                counter += 1;
+                String nondead = input.split("deadline ", 2)[1]; // Remove instruction
+                String task = nondead.split(" /by ", 2)[0]; // Split to task
+                String date = nondead.split(" /by ", 2)[1]; // Split to date
+                Deadline newDeadline = new Deadline(task, date);
+                int itemsInList = getLength(arr) + 1;
+                System.out.println("Got it! I've added this task: \n    " +
+                        newDeadline.toString() + "\n" +
+                        "Now you have " + itemsInList + " tasks in the list.");
+                arr[getLength(arr)] = newDeadline;
+                System.out.println(line);
+            }
+
+            // Event Feature
+            else if (input.contains("event")) {
+                System.out.println(line);
+                String nonevent = input.split("event ", 2)[1]; // Remove instruction
+                String task = nonevent.split(" /at ", 2)[0]; // Split to task
+                String date = nonevent.split(" /at ", 2)[1]; // Split to date
+                Event newEvent = new Event(task, date);
+                int itemsInList = getLength(arr) + 1;
+                System.out.println("Got it! I've added this task: \n    " +
+                        newEvent.toString() + "\n" +
+                        "Now you have " + itemsInList + " tasks in the list.");
+                arr[getLength(arr)] = newEvent;
+                System.out.println(line);
+            }
+
+            // To Do Feature
+            else if (input.contains("todo")) {
+                System.out.println(line);
+                String word = input.split(" ", 2)[1]; // Remove instruction
+                ToDo newToDo = new ToDo(word);
+                int itemsInList = getLength(arr) + 1;
+                System.out.println("Got it! I've added this task: \n    " +
+                        newToDo.toString() + "\n" +
+                        "Now you have " + itemsInList + " tasks in the list.");
+                arr[getLength(arr)] = newToDo;
                 System.out.println(line);
             }
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,18 +1,37 @@
 public class Task {
     protected String name;
     protected boolean done;
+    protected final String mark = "[X]";
+    protected final String unmarked = "[ ]";
 
     public Task(String name) {
         this.name = name;
         this.done = false;
     }
 
-    public String toggleDone() {
+    public String getName() {
+        return this.name;
+    }
+
+    public void setMarked() {
+        this.done = true;
+    }
+
+    public void setUnmarked() {
+        this.done = false;
+    }
+
+    public String isDone() {
         if (this.done) {
-            return "X";
+            return mark;
         }
         else {
-            return " ";
+            return unmarked;
         }
+    }
+
+    @Override
+    public String toString() {
+        return isDone() + " " + getName();
     }
 }
