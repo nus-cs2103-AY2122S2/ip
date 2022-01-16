d571cfc0ab6113a2fb028a7f83c2f00ee07c6566
DESKTOP-R830QT2\Jan <jan.alfenson.tan@gmail.com>
2022-01-17 00:32:32 +0800

    Rename Duke to Sana, Add Greet, Echo, Bye functionality

--- src/main/java/Sana.java
+++ src/main/java/Sana.java
@@ -1,10 +1,31 @@
-public class Duke {
+import java.util.Scanner; // Import the Scanner class
+
+/**
+ * Sana is a BIG program!
+ *
+ * @author  Jan Alfenson Tan
+ * @version 1.0
+ */
+public class Sana {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String border = "_____________________________________________";
+        System.out.println(border);
+        System.out.println("Hi! I'm BEEEEEEEG\nWhats up?");
+        System.out.println(border);
+
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            String input = userInput.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(border);
+                System.out.println("See you next time!");
+                System.out.println(border);
+                break;
+            } else {
+                System.out.println(border);
+                System.out.println(input);
+                System.out.println(border);
+            }
+        }
     }
 }
