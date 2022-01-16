bb6a352b45111d55d2f96af7122281156e20834c
Steven Chua <stevengkc714@protonmail.com>
2022-01-16 12:49:49 +0800

    Add Greeting, Echo and Exit functions to MNSKY

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void print_mnsky(String msg) {
+        System.out.print("MNSKY: ");
+        System.out.println(msg);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        print_mnsky("Hi, I'm");
+        print_mnsky("MM    MM  NN    NN   SSSSS  KK   KK  YY    YY");
+        print_mnsky("MMM  MMM  NNN   NN  SSS     KK  KK    YY  YY");
+        print_mnsky("MM MM MM  NN NN NN   SSSS   KKKKK       YY");
+        print_mnsky("MM    MM  NN   NNN     SSS  KK  KK      YY");
+        print_mnsky("MM    MM  NN    NN  SSSSS   KK   KK     YY");
+        print_mnsky("I can help!");
+
+        String input = "";
+        Scanner scan = new Scanner(System.in);
+
+        while (!input.equals("bye")) {
+            System.out.println();
+            System.out.print("> ");
+            input = scan.nextLine();
+
+            if (input.equals("bye")) {
+                print_mnsky("I can help!");
+                System.out.println("[MNSKY is shutting itself down...]");
+            } else {
+                System.out.println(input);
+                print_mnsky("I can help!");
+            }
+        }
     }
 }
