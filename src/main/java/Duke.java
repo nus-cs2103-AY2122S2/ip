2dac3903594f7d2e400e27886f8599589deb0fcb
hieunm1821 <hieunm1821@gmail.com>
2022-01-16 23:05:49 +0800

    Add Greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        final String HORIZONTAL_LINE = "\t" + "____________________________________________________________";
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("\t" + "Hello! I'm Duke");
+        System.out.println("\t" + "What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("\t" + "Bye. Hope to see you again soon!");
+                System.out.println(HORIZONTAL_LINE);
+                break;
+            } else {
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("\t" + userInput);
+                System.out.println(HORIZONTAL_LINE);
+            }
+        }
     }
 }
