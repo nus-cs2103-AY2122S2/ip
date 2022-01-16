ac47ec93571b058e859c69e5064b9b9b2b8c50e9
bakano98 <70756909+bakano98@users.noreply.github.com>
2022-01-16 18:59:49 +0800

    Level-8: Dates and Times

--- src/main/java/Tasks/Deadline.java
+++ src/main/java/Tasks/Deadline.java
@@ -1,16 +1,39 @@
 package Tasks;
 
+import java.time.LocalDate;
+
 public class Deadline extends Task {
-    String deadline; // (at: date time)
+    String time;
+    // Keeping this for now since we do not know what to use LocalDate date for...
+    String deadline;
+    LocalDate date;
+
     public Deadline(String task, boolean markStatus, String deadline) {
         super(task, markStatus);
         this.deadline = deadline;
+        this.dateFormatter(deadline);
     }
 
     @Override
     public String getStringCmd() {
         // mark status | type | descriptor | deadline
         return super.getMarkStatus() + "&D&" + super.getTask() + "&" +  this.deadline;
+    }
+
+    private void dateFormatter(String dateTime) {
+        // the string is of the form: 2/12/2019 1800
+        dateTime = dateTime.replace('/', '-');
+        String[] splitDateTime = dateTime.split(" ");
+        try {
+            this.date = LocalDate.parse(splitDateTime[0]);
+        } catch (Exception e) {
+            System.out.println("Wrong date format");
+        }
+        this.time = splitDateTime[1];
+    }
+
+    public String getDate() {
+        return date.toString();
     }
 
     public String getDeadline() {
