f0c941419f6444266d6be117ac81354c42f1aaac
domlimm <dominiclimkj3448@gmail.com>
2022-01-16 20:02:01 +0800

    - Update greeting - Change bot logo and name

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
+    private static final String BOT_ART =
+            "     /\\   | |   | |          \n" +
+            "    /  \\  | |__ | |__  _   _ \n" +
+            "   / /\\ \\ | '_ \\| '_ \\| | | |\n" +
+            "  / ____ \\| |_) | |_) | |_| |\n" +
+            " /_/    \\_\\_.__/|_.__/ \\__, |\n" +
+            "                        __/ |\n" +
+            "                       |___/ \n";
+    private static final String BOT_NAME = "Abby";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.printf("Hello! I'm %s\nWhat can I do for you?\n", BOT_NAME);
+        System.out.printf("Can I just show off my art?\n%s", BOT_ART);
     }
 }
