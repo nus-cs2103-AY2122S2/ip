a18f781e65798d6a9bba1e81b458fcdf5cb3eb14
Yong Gi <rygrobin@gmail.com>
2022-01-16 22:27:06 +0800

    greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String botName = "Angry Kitty";
+        printDivider();
+        System.out.println("    Hello, I'm " + botName + ".");
+        System.out.println("    What can I do for you?");
+        printDivider();
+        Scanner inputScanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("bye")) {
+            input = inputScanner.nextLine();
+            if (input.equals("bye")) {
+                printEndMessage();
+            } else {
+                echo(input);
+            }
+        }
     }
+
+    public static void printEndMessage() {
+        printDivider();
+        System.out.println("    Bye. Hope to see you again soon!");
+        printDivider();
+    }
+
+    public static void printDivider() {
+        String divider = "    ____________________________________________________________";
+        System.out.println(divider);
+    }
+
+    public static void echo(String input) {
+        printDivider();
+        System.out.println("    " + input);
+        printDivider();
+    }
+
 }
