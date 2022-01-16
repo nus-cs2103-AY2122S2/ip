a64adfd8053259fe869d8bb8bcb6f96e26ea68c6
Steven Chua <stevengkc714@protonmail.com>
2022-01-16 14:34:33 +0800

    Add list functionality

--- src/main/java/MNSKY.java
+++ src/main/java/MNSKY.java
@@ -1,34 +1,80 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class MNSKY {
-    public static void print_mnsky(String msg) {
+    private ArrayList<String> list;
+    private Scanner con;
+
+    public MNSKY() {
+         this.list = new ArrayList<>();
+         this.con = new Scanner(System.in);
+    }
+
+    /**
+     * Prints the greeting message for the chatbot.
+     */
+    public void greeting() {
+        this.printMNSKY("Hi, I'm");
+        this.printMNSKY("MM      MM  NN      NN   SSSSSSS   KK     KK  YY      YY");
+        this.printMNSKY("MMMM  MMMM  NNNN    NN  SSSS       KK   KK      YY  YY");
+        this.printMNSKY("MM  MM  MM  NN  NN  NN    SSSSS    KKKKK          YY");
+        this.printMNSKY("MM      MM  NN    NNNN       SSSS  KK   KK        YY");
+        this.printMNSKY("MM      MM  NN      NN  SSSSSSS    KK     KK      YY");
+        this.printMNSKY("I can help!");
+    }
+
+    /**
+     * Retrieves user input from stdin.
+     * @return The user's input
+     */
+    private String getInput() {
+        System.out.println();
+        System.out.print("> ");
+        return this.con.nextLine();
+    }
+
+    /**
+     * Prints the passed message with "MNSKY: " before it.
+     * @param msg The message that should be printed after "MNSKY: "
+     */
+    private void printMNSKY(String msg) {
         System.out.print("MNSKY: ");
         System.out.println(msg);
     }
-    public static void main(String[] args) {
-        print_mnsky("Hi, I'm");
-        print_mnsky("MM      MM  NN      NN   SSSSSSS   KK     KK  YY      YY");
-        print_mnsky("MMMM  MMMM  NNNN    NN  SSSS       KK   KK      YY  YY");
-        print_mnsky("MM  MM  MM  NN  NN  NN    SSSSS    KKKKK          YY");
-        print_mnsky("MM      MM  NN    NNNN       SSSS  KK   KK        YY");
-        print_mnsky("MM      MM  NN      NN  SSSSSSS    KK     KK      YY");
-        print_mnsky("I can help!");
 
-        String input = "";
-        Scanner scan = new Scanner(System.in);
+    /**
+     * Parses the input and executes the logic depending on the type of input.
+     * @return True if the user input "bye" and thus wants to stop talking to the chatbot.
+     *          False otherwise.
+     */
+    public boolean parseInput() {
+        String input = this.getInput();
 
-        while (!input.equals("bye")) {
-            System.out.println();
-            System.out.print("> ");
-            input = scan.nextLine();
-
-            if (input.equals("bye")) {
-                print_mnsky("I can help!");
+        switch (input) {
+            case "bye":
+                this.printMNSKY("I can help!");
                 System.out.println("[MNSKY is shutting itself down...]");
-            } else {
-                System.out.println(input);
-                print_mnsky("I can help!");
-            }
+                return false;
+
+            case "list":
+                for (int i = 0; i < this.list.size(); i++) {
+                    System.out.println(String.format("%d. %s", i, this.list.get(i)));
+                }
+                break;
+
+            default:
+                System.out.println(String.format("[Added %s]", input));
+                this.list.add(input);
         }
+
+        this.printMNSKY("I can help!");
+        return true;
+    }
+
+    public static void main(String[] args) {
+        MNSKY mnsky = new MNSKY();
+        mnsky.greeting();
+
+        while (mnsky.parseInput()) {}
     }
 }
