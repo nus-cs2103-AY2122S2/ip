9c567cd9a8ca66f7de90f99b36fd2f933bd89d36
Lim Bing Sen <bingsenlim0806@gmail.com>
2022-01-16 21:03:16 +0800

    Move all replies from Duke as final static String variables.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,20 @@ public class Duke {
             = "Roarrr.... I'm Burning Charizard, tasked to burnnn down your tasks.\n" +
             "Which task shall we burn today?";
     private static final String BYE_MESSAGE = "Roarrr....Let's burn more tasks next time!";
+    private static final String ADD_MESSAGE = "Charizard is ready to burn task:";
+    private static final String MARK_MESSAGE = "Charizard breathe out fire and burned the task.";
+    private static final String UNMARK_MESSAGE = "Oh no! The task was not burnt completely!";
+    private static final String QUESTION_MESSAGE = "What should Charizard do next?";
+    private static final String ERROR_NO_COMMAND = "Charizard does not know this move. Try a different command.";
+    private static final String ERROR_PARSE_INT = "Charizard's can only remember numbers. Try specifying a task number."
+            + "\n(Use \"list\" command to see the tasks and their corresponding task number).";
+    private static final String ERROR_INVALID_INDEX = "Task number does not exist. Charizard is confused..";
+    private static final String ERROR_TODO_NO_NAME = "Please specify the name of new task to be burnt";
+    private static final String ERROR_INVALID_NAME_OR_DATE
+            = "Charizard insists of knowing both name and time of the new task.";
+    private static final String ERROR_TOO_MANY_DATES = "Charizard can only remember one deadline per task.";
+    private static final String FORMAT_DEADLINE = "Try using \"deadline <task_name> /by <deadline>\".";
+    private static final String FORMAT_EVENT = "Try using \"event <task_name> /by <event_time>\".";
     private static final int BORDER_LENGTH = 80;
     private static ArrayList<Task> tasks;
 
@@ -13,11 +27,11 @@ public class Duke {
         tasks = new ArrayList<Task>();
         greet();
         Scanner sc = new Scanner(System.in);
-        System.out.println("What should Charizard do next?");
+        System.out.println(QUESTION_MESSAGE);
         String input = sc.nextLine();
         while (input.compareToIgnoreCase("bye") != 0) {
             processInput(input);
-            System.out.println("What should Charizard do next?");
+            System.out.println(QUESTION_MESSAGE);
             input = sc.nextLine();
         }
         exit();
@@ -61,7 +75,7 @@ public class Duke {
                 message = processEvent(inputWithoutCommand);
 
             } else {
-                throw new DukeException("Charizard does not know this move. Try a different command.");
+                throw new DukeException(ERROR_NO_COMMAND);
             }
         } catch (DukeException e) {
             message = "OOPS! " + e.getMessage();
@@ -75,62 +89,59 @@ public class Duke {
             tasks.get(taskId).mark(isDone);
             String message = "";
             if (isDone) {
-                message = String.format("Charizard breathe out fire and burned the task.");
+                message = MARK_MESSAGE;
             } else {
-                message = String.format("Oh no! The task was not burnt completely!");
+                message = String.format(UNMARK_MESSAGE);
             }
             message += String.format("\n  %s", tasks.get(taskId).toString());
             return message;
         } catch (NumberFormatException e) {
-            throw new DukeException("Charizard's can only remember numbers. Try specifying a task number."
-                + "\n(Use \"list\" command to see the tasks and their corresponding task number).");
+            throw new DukeException(ERROR_PARSE_INT);
         } catch (IndexOutOfBoundsException e) {
-            throw new DukeException("Task number does not exist. Charizard is confused..");
+            throw new DukeException(ERROR_INVALID_INDEX);
         }
     }
 
     public static String processTodo(String input) throws DukeException {
         if (input.matches("\\s*")) {
-            throw new DukeException("Please specify the name of new task to be burnt");
+            throw new DukeException(ERROR_TODO_NO_NAME);
         }
         ToDo newTodo = new ToDo(input);
         tasks.add(newTodo);
-        String message = String.format("Charizard is ready to burn task:\n  %s\nThere are %d tasks in the burning list.",
-                newTodo.toString(), tasks.size());
+        String message = String.format("%s\n  %s\nThere are %d tasks in the burning list.",
+                ADD_MESSAGE, newTodo.toString(), tasks.size());
         return message;
     }
 
     public static String processDeadline(String input) throws DukeException {
         String[] splitInput = input.split("/by");
         if (splitInput.length < 2 || splitInput[0].matches("\\s*") || splitInput[1].matches("\\s*")) {
-            throw new DukeException("Charizard insists of knowing both name and deadline of the new task."
-                + "\nTry using \"deadline <task_name> /by <deadline>\".");
+            throw new DukeException(ERROR_INVALID_NAME_OR_DATE + "\n" + FORMAT_DEADLINE);
         } else if (splitInput.length > 2) {
-            throw new DukeException("Charizard can only remember one deadline per task.");
+            throw new DukeException(ERROR_TOO_MANY_DATES);
         }
         String description = splitInput[0].trim();
         String deadline = splitInput[1].trim();
         Deadline newDeadline = new Deadline(description, deadline);
         tasks.add(newDeadline);
-        String message = String.format("Charizard is ready to burn task:\n  %s\nThere are %d tasks in the burning list.",
-                newDeadline.toString(), tasks.size());
+        String message = String.format("%s\n  %s\nThere are %d tasks in the burning list.",
+                ADD_MESSAGE, newDeadline.toString(), tasks.size());
         return message;
     }
 
     public static String processEvent(String input) throws DukeException {
         String[] splitInput = input.split("/at");
         if (splitInput.length < 2) {
-            throw new DukeException("Charizard insists of knowing both name and time of the new task."
-                    + "\nTry using \"event <task_name> /by <event_time>\".");
+            throw new DukeException(ERROR_INVALID_NAME_OR_DATE + "\n" + FORMAT_EVENT);
         } else if (splitInput.length > 2) {
-            throw new DukeException("Charizard can only remember one time per task.");
+            throw new DukeException(ERROR_TOO_MANY_DATES);
         }
         String description = splitInput[0].trim();
         String time = splitInput[1].trim();
         Event newEvent = new Event(description, time);
         tasks.add(newEvent);
-        String message = String.format("Charizard is ready to burn task:\n  %s\nThere are %d tasks in the burning list.",
-                newEvent.toString(), tasks.size());
+        String message = String.format("%s\n  %s\nThere are %d tasks in the burning list.",
+                ADD_MESSAGE, newEvent.toString(), tasks.size());
         return message;
     }
 
