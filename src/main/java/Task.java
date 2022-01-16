4f726b916c9075e5c56c3b9b40473417758731ca
DESKTOP-R830QT2\Jan <jan.alfenson.tan@gmail.com>
2022-01-17 01:18:11 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,54 @@
+
+/**
+ * This class represents the tasks Sana will remember
+ *
+ * @author  Jan Alfenson Tan
+ * @version 1.0
+ */
+public class Task {
+    /**
+     * true when task is done, false otherwise
+     */
+    private boolean done;
+
+    /**
+     * name of the task
+     */
+    private String taskName;
+
+    /**
+     * Constructor for the task object
+     *
+     * @param taskName  name of the task
+     */
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.done = false;
+    }
+
+    /**
+     * Returns the task name
+     *
+     * @return taskName
+     */
+    public String getTaskName() {
+        return taskName;
+    }
+
+    /**
+     * Returns whether the task is done
+     * @return  done
+     */
+    public boolean getDone() {
+        return done;
+    }
+
+    /**
+     * Sets the task to done or not
+     *
+     * @param done  sets the task's completion
+     */
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
