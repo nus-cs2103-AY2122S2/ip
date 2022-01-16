451626d2f849503209aa56e81807fbbbe16fad4a
Dane Bagaoisan <dane0515@gmail.com>
2022-01-17 01:27:17 +0800

    Added list feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hola! soy José\nQué puedo hacer por ti? UwU");
+        ArrayList<String> strings = new ArrayList<>();
         String input;
 
-        while (scanner.hasNextLine()) {
+        System.out.println("Hola! soy José\nQué puedo hacer por ti? UwU");
+
+        while (scanner.hasNext()) {
             input = scanner.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Adiós. Espero volver a verte pronto!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < strings.size(); i++) {
+                    System.out.println(i + 1 + ": " + strings.get(i));
+                }
             } else {
-                System.out.println(input);
+                System.out.println("added: " + input);
+                strings.add(input);
             }
         }
+
+        scanner.close();
     }
 }
