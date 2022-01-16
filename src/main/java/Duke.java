7bb1edb57bf2b3aab820c835c641d9bceca3f811
gz <zhouguanzong@gmail.com>
2022-01-17 02:48:11 +0800

    add mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,30 +10,49 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String command = br.readLine();
-        ArrayList<String> list = new ArrayList<>();
+        String keyWord;
+        ArrayList<Task> list = new ArrayList<>();
         while (!command.equals("bye")) {
-            switch(command) {
-                case "list":
-                    System.out.println("    ____________________________________________________________");
+            keyWord = command.split(" ")[0];
+            switch(keyWord) {
+                case "list": {
+                    System.out.println("    ____________________________________________________________\n" +
+                            "     Here are the tasks in your list:");
                     for (int i = 0; i < list.size(); i++) {
-                        System.out.printf("     %d. %s%n",i + 1, list.get(i));
+                        System.out.printf("     %d. %s%n", i + 1, list.get(i).toString());
                     }
                     System.out.println("    ____________________________________________________________");
                     command = br.readLine();
                     break;
-                case "blah":
+                }
+                case "mark": {
+                    int taskNo = Integer.parseInt(command.split(" ")[1]) - 1;
+                    list.get(taskNo).markDone();
                     System.out.println("    ____________________________________________________________\n" +
-                            "     blah\n" +
+                            "     Nice! I've marked this task as done: ");
+                    System.out.printf("       %s%n",list.get(taskNo).toString());
+                    System.out.println("    ____________________________________________________________");
+                    command = br.readLine();
+                    break;
+                }
+                case "unmark": {
+                    int taskNo = Integer.parseInt(command.split(" ")[1]) - 1;
+                    list.get(taskNo).markUndone();
+                    System.out.println("    ____________________________________________________________\n" +
+                            "     OK, I've marked this task as not done yet:");
+                    System.out.printf("       %s%n",list.get(taskNo).toString());
+                    System.out.println("    ____________________________________________________________");
+                    command = br.readLine();
+                    break;
+                }
+                default: {
+                    list.add(new Task(command));
+                    System.out.println("    ____________________________________________________________\n" +
+                            "     added: " + list.get(list.size()-1).toString() + "\n" +
                             "    ____________________________________________________________");
                     command = br.readLine();
                     break;
-                default:
-                    list.add(command);
-                    System.out.println("    ____________________________________________________________\n" +
-                            "     added: " + command + "\n" +
-                            "    ____________________________________________________________");
-                    command = br.readLine();
-                    break;
+                }
             }
         }
         System.out.println("    ____________________________________________________________\n" +
---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+    private String taskName;
+    private boolean status;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.status = false;
+    }
+
+    public void markDone() {
+        this.status = true;
+    }
+
+    public void markUndone() {
+        this.status = false;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public boolean getStatus() {
+        return this.status;
+    }
+
+    public String toString() {
+        String done = this.status ? "[X] " : "[ ] ";
+        return done + getTaskName();
+    }
+
+}
