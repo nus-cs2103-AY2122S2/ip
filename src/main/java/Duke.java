f0b1542205b41dc67ed91b46f9b0010b9546933b
hieunm1821 <hieunm1821@gmail.com>
2022-01-16 23:28:15 +0800

    Adding function, listing function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         final String HORIZONTAL_LINE = "\t" + "____________________________________________________________";
+        String[] storedText = new String[100];
+        int counting = 0;
+
         System.out.println(HORIZONTAL_LINE);
         System.out.println("\t" + "Hello! I'm Duke");
         System.out.println("\t" + "What can I do for you?");
@@ -17,9 +20,18 @@ public class Duke {
                 System.out.println("\t" + "Bye. Hope to see you again soon!");
                 System.out.println(HORIZONTAL_LINE);
                 break;
-            } else {
+            } else if (userInput.equals("list")) {
                 System.out.println(HORIZONTAL_LINE);
-                System.out.println("\t" + userInput);
+                for (int i = 0; i < counting; i++) {
+                    int index = i + 1;
+                    System.out.println("\t" + index + ". " + storedText[i]);
+                }
+                System.out.println(HORIZONTAL_LINE);
+            } else {
+                storedText[counting] = userInput;
+                counting++;
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("\t" + "added: " + userInput);
                 System.out.println(HORIZONTAL_LINE);
             }
         }
