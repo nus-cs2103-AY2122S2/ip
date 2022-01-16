d998b3f96b781af759aa03ffcc238fef9a9593e8
Charisma Kausar <charisma.kausar@gmail.com>
2022-01-17 01:30:30 +0800

    Customize chatbot name

---
+++ src/main/java/Heylo.java
@@ -0,0 +1,13 @@
+public class Heylo {
+    public static void main(String[] args) {
+        String logo =
+                " _   _                  _\n" +
+                        "| | | |   ___   _   _  | |   ___\n" +
+                        "| |_| |  / _ \\ | | | | | |  / _ \\ \n" +
+                        "|  _  | |  __/ | |_| | | | | (_) |\n" +
+                        "|_| |_|  \\___|  \\__, | |_|  \\___/\n" +
+                        "                |___/\n";
+        System.out.println(logo);
+        System.out.println("Heylo! What can I do for you today? :)");
+    }
+}
