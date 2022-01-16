c74a9bf0aeb80fba1782b8c39651efc9e143ca4e
junrong98 <e0544375@u.nus.edu>
2022-01-16 11:26:34 +0800

    Update Duke.java, README.md, runtest.bat
    Update the aforementioned files to better reflect on the current
    project. Any mentioned of "Duke" was changed to "FunBox".

---
+++ src/main/java/FunBox.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+/**
+ * The FunBox class is used as the outer shell of FunBoxGear,
+ * which contains the functionality of FunBox
+ */
+public class FunBox {
+    public static void main(String[] args) {
+        
+    }
+}
