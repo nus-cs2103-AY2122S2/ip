efe5bbbd4c23116a9eb3ee9ae78c66b41f90ca03
gz <zhouguanzong@gmail.com>
2022-01-16 17:01:03 +0800

    add greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.io.*;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+    public static void main(String[] args) throws Exception {
+        String logo = "    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n";
         System.out.println("Hello from\n" + logo);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String command = br.readLine();
+        while (!command.equals("bye")) {
+            switch(command) {
+                case "list":
+                    System.out.println("    ____________________________________________________________\n" +
+                            "     list\n" +
+                            "    ____________________________________________________________");
+                    command = br.readLine();
+                    break;
+                case "blah":
+                    System.out.println("    ____________________________________________________________\n" +
+                            "     blah\n" +
+                            "    ____________________________________________________________");
+                    command = br.readLine();
+                    break;
+            }
+        }
+        System.out.println("    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________");
+
+
     }
 }
