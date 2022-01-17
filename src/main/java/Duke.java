74811af3d5b213fb478ab20e67ee4b02372b6808
glennljw <glennljw7@gmail.com>
2022-01-17 17:39:13 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,29 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t Hello! I'm Duke\n\t What can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        // Initialise String array of tasks
+        String[] tasks = new String[100];
+        int count = 0;
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye"))
                 break;
-            System.out.println(input);
+            if (input.equals("list")) {
+                System.out.println("\t____________________________________________________________");
+                for (int taskCount = 0; taskCount < count; taskCount++) {
+                    System.out.println("\t " + (taskCount + 1) + ". " + tasks[taskCount]);
+                }
+                System.out.println("\t____________________________________________________________");
+                continue;
+            }
+            tasks[count] = input;
+            count++;
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t added: " + input);
+            System.out.println("\t____________________________________________________________");
         }
         System.out.println("Bye. Hope to see you again soon!");
         sc.close();
