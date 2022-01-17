f070d1a8920b5f09482bada665f8c230e2a74c04
Dane Bagaoisan <dane0515@gmail.com>
2022-01-17 22:45:59 +0800

    Added some exception handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,55 +5,60 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Task> tasks = new ArrayList<>();
-        String[] temp, temptemp;
+        String[] task, taskInfo;
         Task tempTask;
         String input, command;
 
         System.out.println("Hola! soy José\nQué puedo hacer por ti? UwU");
 
         while (scanner.hasNext()) {
-            input = scanner.nextLine();
-            System.out.println("-------------------------------------");
-            if (input.equals("bye")) {
-                System.out.println("Adiós. Espero volver a verte pronto!");
-                break;
-            } else if (input.equals("list")) {
-                System.out.println("Aquí están las tareas en su lista:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(i + 1 + ": " + tasks.get(i));
-                }
-            } else {
-                temp = input.split(" ", 2);
-                command = temp[0];
-
-                if (command.equals("mark")) {
-                    tempTask = tasks.get(Integer.parseInt(temp[1]) - 1);
-                    tempTask.mark();
-                    System.out.println("¡Bonito! He marcado esta tarea como hecha:\n" + tempTask);
-                } else if (command.equals("unmark")) {
-                    tempTask = tasks.get(Integer.parseInt(temp[1]) - 1);
-                    tempTask.unmark();
-                    System.out.println("Bien, he marcado esta tarea como aún no realizada:\n" + tempTask);
-                } else {
-                    if (command.equals("todo")) {
-                        tempTask = new ToDo(temp[1]);
-                        tasks.add(tempTask);
-                        System.out.println("Entendido. he añadido esta tarea:\n" + tempTask);
-                    } else if (command.equals("deadline")) {
-                        temptemp = temp[1].split(" /by ");
-                        tempTask = new Deadline(temptemp[0], temptemp[1]);
-                        tasks.add(tempTask);
-                        System.out.println("Entendido. he añadido esta tarea:\n" + tempTask);
-                    } else if (command.equals("event")) {
-                        temptemp = temp[1].split(" /at ");
-                        tempTask = new Event(temptemp[0], temptemp[1]);
-                        tasks.add(tempTask);
-                        System.out.println("Entendido. he añadido esta tarea:\n" + tempTask);
-                    } else {
-                        System.out.println("Error");
+            try {
+                input = scanner.nextLine();
+                System.out.println("-------------------------------------");
+                if (input.equals("bye")) {
+                    System.out.println("Adiós. Espero volver a verte pronto!");
+                    break;
+                } else if (input.equals("list")) {
+                    System.out.println("Aquí están las tareas en su lista:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(i + 1 + ": " + tasks.get(i));
                     }
-                    System.out.println("Ahora tienes " + tasks.size() + " tareas en la lista.");
+                } else {
+                    task = input.split(" ", 2);
+                    command = task[0];
+                    if (command.equals("mark")) {
+                        tempTask = tasks.get(Integer.parseInt(task[1]) - 1);
+                        tempTask.mark();
+                        System.out.println("¡Bonito! He marcado esta tarea como hecha:\n" + tempTask);
+                    } else if (command.equals("unmark")) {
+                        tempTask = tasks.get(Integer.parseInt(task[1]) - 1);
+                        tempTask.unmark();
+                        System.out.println("Bien, he marcado esta tarea como aún no realizada:\n" + tempTask);
+                    } else {
+                        if (command.equals("todo") || command.equals("deadline") || command.equals("event")) {
+                            if (task.length > 1) {
+                                if (command.equals("todo")) {
+                                    tempTask = new ToDo(task[1]);
+                                } else if (command.equals("deadline")) {
+                                    taskInfo = task[1].split(" /by ");
+                                    tempTask = new Deadline(taskInfo[0], taskInfo[1]);
+                                } else {
+                                    taskInfo = task[1].split(" /at ");
+                                    tempTask = new Event(taskInfo[0], taskInfo[1]);
+                                }
+                                tasks.add(tempTask);
+                                System.out.println("Entendido. he añadido esta tarea:\n" + tempTask);
+                            } else {
+                                throw new DukeException(command + " requires additional info");
+                            }
+                        } else {
+                            throw new DukeException("Nani?! No comprende por favor");
+                        }
+                        System.out.println("Ahora tienes " + tasks.size() + " tareas en la lista.");
+                    }
                 }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
             System.out.println("-------------------------------------");
         }
