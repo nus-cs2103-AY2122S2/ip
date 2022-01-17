d83ec19432faf944758a39c499d79baa6bb32800
Shivang211 <shivanggupta2110@gmail.com>
2022-01-17 23:22:26 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        boolean loop = true;
+        while (loop) {
+            String input = sc.nextLine();
+            if (input.equals("Bye")){
+                loop = false;
+                System.out.println("Bye. Hope to see you soon.");
+            } else {
+                System.out.println("--------------------------------------------------");;
+                System.out.println(input);
+            }
+        }
     }
 }
+
