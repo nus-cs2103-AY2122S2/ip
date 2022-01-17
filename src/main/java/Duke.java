262bc4528862f668ceb1e8f4611f75591f00bb24
putaojuice <33406416+putaojuice@users.noreply.github.com>
2022-01-17 16:16:31 +0800

    Duke Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,31 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean endProgram = false;
+        ArrayList<Task> list = new ArrayList<Task>(); // arraylist to keep track of task
+        boolean endProgram = false; // state to terminate the program
         welcomeMessage();
 
         while(!endProgram) {
-            String input = scanner.nextLine();
+            String input = scanner.nextLine(); // user input
+
+            // exit program when user input "bye"
             if(input.equals("bye")) {
                 endProgram();
                 endProgram = true;
                 break;
             }
-            drawDivider();
-            System.out.println("    " + input);
-            drawDivider();
+
+            // list out the tasks
+            if(input.equals("list")) {
+                listTask(list);
+                continue;
+            }
+
+            // add task
+            addTask(input, list);
         }
     }
 
@@ -38,6 +48,25 @@ public class Duke {
     private static void endProgram() {
         drawDivider();
         System.out.println("    " + "Bye. Hope to see you again soon!");
+        drawDivider();
+    }
+
+    private static void listTask(ArrayList<Task> list) {
+        drawDivider();
+        if(list.size() == 0) {
+            System.out.println("Nothing on your list!");
+        } else {
+            for(int i = 0; i < list.size(); i++) {
+                System.out.println(String.format("%d. %s", i + 1, list.get(i).getTaskName()));
+            }
+        }
+        drawDivider();
+    }
+
+    private static void addTask(String taskName, ArrayList<Task> list) {
+        drawDivider();
+        list.add(new Task(taskName));
+        System.out.println("    " + "added: " + taskName);
         drawDivider();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private final String name;
+
+    public Task(String taskName) {
+        this.name = taskName;
+    }
+
+    public String getTaskName() {
+        return this.name;
+    }
+}
