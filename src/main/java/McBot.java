9ee161a84dc0eb1c311315057680dbe269a76324
AD-NAP <>
2022-01-17 22:47:23 +0800

    Level 2. Added the ability to store text entered by the user and list it back.

--- src/main/java/McBot.java
+++ src/main/java/McBot.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class McBot {
     public static void main(String[] args) {
+            String frameLine = "==========================================";
             String logo = "\n" +
                     "___  ___    ______       _    \n" +
                     "|  \\/  |    | ___ \\     | |   \n" +
@@ -11,25 +13,39 @@ public class McBot {
                     "\\_|  |_/\\___\\____/ \\___/ \\__| \n" +
                     "                              \n" +
                     "                              \n";
-        System.out.println("Ahoy! Me name be" + logo);
+        System.out.println(logo);
+        System.out.println(frameLine);
+        System.out.println("Ahoy! Me name be McBot.\nTell me lad, what do you want?");
+        System.out.println(frameLine);
 
         Scanner sc = new Scanner(System.in);
         boolean running = true;
+        ArrayList<String> arrList = new ArrayList<>(100);
 
         while(running) {
             String command = sc.nextLine();
             switch(command) {
                 case "bye":
-                    System.out.println("Arghh! This ain't the last time ye see me -beep-");
+                    System.out.println(frameLine);
+                    System.out.println("Arghh! This ain't the last time ye see me lad");
+                    System.out.println(frameLine);
                     running = false;
                     break;
                 case "list":
-                    System.out.println("list");
-                    break;
-                case "blah":
-                    System.out.println("blah");
+                    int i = 1;
+                    System.out.println(frameLine);
+                    for (String task : arrList) {
+                        System.out.print(i + ". ");
+                        System.out.println(task);
+                        i++;
+                    }
+                    System.out.println(frameLine);
                     break;
                 default:
+                    arrList.add(command);
+                    System.out.println(frameLine);
+                    System.out.println("added: " + command);
+                    System.out.println(frameLine);
             }
         }
     }
