2c6b2f15d0eebb9c011b2a3e8a98e90399d9eb4b
jxt00 <e0556267@u.nus.edu>
2022-01-17 14:18:59 +0800

    DukeException class: remove unnecessary methods

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,26 +1,12 @@
 public class DukeException extends Exception {
-    protected ExceptionType eType;
-    protected String lineBreak = "____________________________________________________________\n";
+    protected String message;
 
-    public DukeException(ExceptionType eType) {
-        this.eType = eType;
+    public DukeException(String message) {
+        this.message = message;
     }
 
-    public void EmptyInputException() {
-        if (this.eType == ExceptionType.EMPTYINPUT) {
-            System.out.print(lineBreak + "Meow! Enter a valid command!\n" + lineBreak);
-        }
-    }
-
-    public void EmptyDescriptionException() {
-        if (this.eType == ExceptionType.EMPTYDESC) {
-            System.out.print(lineBreak + "Meow! A task needs a description!\n" + lineBreak);
-        }
-    }
-
-    public void InvalidDateException() {
-        if (this.eType == ExceptionType.INVALIDDATE) {
-            System.out.print(lineBreak + "Meow! A date is required!\n" + lineBreak);
-        }
+    @Override
+    public String toString() {
+        return message;
     }
 }
