35573221244238b48ff028dcf80a2b728674ee1a
Chua Bing Quan <chipneverdies@gmail.com>
2022-01-17 19:12:40 +0800

    Implement greet, echo, and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    private static final String INDENTATION = "    ";
+    private static final String DIVIDER = "____________________________________________________________";
+    private static final String GREETING = "Hello! I'm Duke\nWhat can I do for you?";
+    private static final String GOODBYE = "Bye. Hope to see you again soon!";
+
+
+    public static void main(String[] args) throws Exception {
+        final Scanner input = new Scanner(System.in);
+        System.out.println(Duke.constructResponse(Duke.GREETING));
+        try {
+            while (true) {
+                final String command = input.nextLine();
+                if (command.equals("bye")) {
+                    System.out.println(Duke.constructResponse(Duke.GOODBYE));
+                    break;
+                }
+                System.out.println(Duke.constructResponse(command));
+            }
+        } finally {
+            input.close();
+        }
+    }
+
+    private static String constructResponse(String content) {
+        final String divider = Duke.INDENTATION + Duke.DIVIDER + "\n";
+        final String response =
+                Duke.INDENTATION + " " + content.replaceAll("\n", "\n " + Duke.INDENTATION) + "\n";
+        return divider + response + divider;
     }
 }
