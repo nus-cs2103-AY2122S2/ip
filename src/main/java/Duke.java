4def4c4ff97969bcae7eb75b2ae5fc802ad51f99
iztanpy <isaactanpy2000@gmail.com>
2022-01-17 15:56:02 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -26,35 +26,59 @@ public class Duke {
                 for (int i = 0; i < tasks.size(); i++) {
                     Task currentTask = tasks.get(i);
                     int index = i + 1;
-                    System.out.println(index + "." + currentTask.getStatusIcon() + " " + currentTask.toString());
+                    System.out.println(index + "." + currentTask.toString());
                 }
             }
 
             // mark
-            else if (str.length() >= 4 && str.substring(0,4).equals("mark")) {
+            else if (str.length() >= 4 && str.startsWith("mark")) {
                 String[] markedTask = str.split(" ");
                 int indexMarked = Integer.parseInt(markedTask[1]) - 1;
                 Task currentTask = tasks.get(indexMarked);
                 currentTask.isDone = true;
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println(currentTask.getStatusIcon() + " " + currentTask.toString());
+                System.out.println(currentTask.toString())
+                ;
             }
 
 
             // unmark
-            else if (str.length() >= 6 && str.substring(0,6).equals("unmark")) {
+            else if (str.length() >= 6 && str.startsWith("unmark")) {
                 String[] markedTask = str.split(" ");
                 int indexMarked = Integer.parseInt(markedTask[1]) - 1;
                 Task currentTask = tasks.get(indexMarked);
                 currentTask.isDone = false;
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(currentTask.getStatusIcon() + " " + currentTask.toString());
+                System.out.println(currentTask.toString());
             }
 
-            // repeat what the user says and save to tasks list
+            // generate tasks
             else {
-                tasks.add(new Task(str));
-                System.out.println("added: " + str);
+                if (str.length() >= 4 && str.startsWith("todo")) {
+                    Todo currentTodo = new Todo(str.substring(5));
+                    tasks.add(currentTodo);
+                    System.out.println("added: " + currentTodo.toString());
+                }
+                else if (str.length() >= 8 && str.startsWith("deadline")) {
+                    String actualTask = str.substring(9);
+                    String[] segments = actualTask.split(" /by ");
+                    Deadline currentDeadline = new Deadline(segments[0], segments[1]);
+                    tasks.add(currentDeadline);
+                    System.out.println("added: " +  currentDeadline.toString());
+
+                }
+
+                else if (str.length() >= 5 && str.startsWith("event")) {
+                    String actualTask = str.substring(6);
+                    String[] segments = actualTask.split(" /at ");
+                    Event currentEvent = new Event(segments[0], segments[1]);
+                    tasks.add(currentEvent);
+                    System.out.println("added: " + currentEvent.toString());
+                }
+                else {
+                    tasks.add(new Task(str));
+                    System.out.println("added: " + str);
+                }
             }
         }
         System.out.println("Bye. Hope to see you again soon!\n");
