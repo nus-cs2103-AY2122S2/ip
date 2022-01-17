4b29dbdeb0280436ca8b74eb1cdaf45f25265b2b
Ong Kim Lai <kimlailfc@gmail.com>
2022-01-17 19:55:48 +0800

    changed name to Bobby

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
 public class Duke {
+    String tasks[] = new String[100];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " ____  _____ ____  ____ __  __\n"
+                + "|  _ \\|  _  |  _ \\|  _ \\\\ \\/ /\n"
+                + "| |_| | | | | |_| | |_| |\\  /\n"
+                + "| |_| | |_| | |_| | |_| ||  |\n"
+                + "|____/|_____|____/|____/ |__|\n";
         System.out.println("Hello from\n" + logo);
         String dash = "    ____________________________________\n";
-        System.out.println(dash + "    Hello! I\'m Duke\n    What can I do for you?\n" + dash);
+        System.out.println(dash + "    Hello! I\'m Bobby\n    What can I do for you?\n" + dash);
 
          while (true) {
              String input = sc.nextLine();
