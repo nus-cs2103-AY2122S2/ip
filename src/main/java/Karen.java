0340a0c7ca7e7070632aeb93bd05b90fb7cdccaf
onefangg <onefangg@gmail.com>
2022-01-17 14:57:41 +0800

    Implemented function to modify done status of item based on index

--- src/main/java/Karen.java
+++ src/main/java/Karen.java
@@ -1,51 +1,59 @@
 import java.util.Scanner;
 
 public class Karen {
-    String[] toDos;
+    Task[] toDos;
     int runningIndex;
 
     public Karen() {
-        this.toDos = new String[100];
+        this.toDos = new Task[100];
         this.runningIndex = 0;
     }
 
-    public void processCommand(String command) {
-        if (!command.equals("bye") && !command.equals("list") )  {
-            this.toDos[this.runningIndex] = command;
-            this.runningIndex++;
-        }
-        this.echo(command);
-    }
-
-    public void echo(String command, Boolean greet) {
+    public void echo(String statement) {
         System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-        System.out.println(command);
+        System.out.println(statement);
         System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*\n");
     }
 
-    public void echo(String command) {
+    public void processCommand(String command) {
         String output = command;
         // commands with extra functionality
         if (command.equals("bye")) {
             output = "Goodbye - I'll be seeing your manager's manager next.";
-        } else if (command.equals("list")) {
+            this.echo(output);
+            System.exit(0);
+        }
+        else if (command.equals("list")) {
             // formatting list
             int counter = 0;
             output = "";
-            for (String item: this.toDos) {
-                output = output.concat(String.format("%d: %s\n", counter+1, item));
+
+            for (Task item: this.toDos) {
+                output = output.concat(String.format("%d.%s\n", counter+1, item.toString()));
                 counter++;
                 if (counter >= this.runningIndex) {
                     break;
                 }
             }
-        } else {
-            output = String.format("added: %s", command);
         }
+        else if (command.matches("(un)?mark \\d+")) {
+            int index = Integer.valueOf(command.split(" ")[1]);
+            Task getTask = this.toDos[index-1];
 
-        System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-        System.out.println(output);
-        System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*\n");
+            if (command.startsWith("mark")) {
+                getTask.markDone();
+                output = String.format("This task is finally done:\n  %s",getTask.toString());
+            } else if (command.startsWith("unmark")) {
+                getTask.markUndone();
+                output = String.format("This task is now incomplete - unacceptable:\n  %s",getTask.toString());
+            }
+        }
+        else {
+            output = String.format("added: %s", command);
+            this.toDos[this.runningIndex] = new Task(command);
+            this.runningIndex++;
+        }
+        this.echo(output);
     }
 
     public static void main(String[] args) {
@@ -53,18 +61,12 @@ public class Karen {
         Karen manager = new Karen();
 
         // greet
-        manager.echo("Hello, my name is Karen.\nI'll be speaking (to) as your manager today.",true);
+        manager.echo("Hello, my name is Karen.\nI'll be speaking (to) as your manager today.");
 
         while (true) {
             // take in input as commands
             String cmd = scanner.nextLine();
             manager.processCommand(cmd);
-
-            // exit loop
-            if (cmd.equals("bye")) {
-                break;
-            }
         }
-        System.exit(0);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    public String description;
+    protected boolean done;
+
+    public Task(String description) {
+        this.description = description;
+        this.done = false;
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+
+    public void markUndone() {
+        this.done = false;
+    }
+
+    public String getDoneIcon () {
+        return (this.done) ? "X" : " ";
+    }
+
+    public String toString() {
+        String status = this.getDoneIcon();
+        return String.format("[%s] %s", status, this.description);
+    }
+}
