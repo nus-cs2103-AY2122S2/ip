896beb6e6b62570d29de2d27d89cb6a42b4d7e4f
junjunjieOng <e0544320@u.nus.edu>
2022-01-17 12:55:21 +0800

    Add add feature and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] taskList = new String[100];
+    private static int taskNum = 0;
+
     public static void main(String[] args) {
         String greet = "Hello! I'm Duke\n" + "What can I do for you?\n";
         String goodbye = "Bye. Hope to see you again soon!";
         System.out.println(greet);
-        while(true){
+
+        while(true) {
             Scanner sc = new Scanner(System.in);
-            String curr_word = sc.next();
+            String curr_word = sc.nextLine();
             if (curr_word.equals("bye")) {
                 System.out.println(goodbye);
                 break;
-            } else{
-                System.out.println(curr_word);
+            } else if (curr_word.equals("list")) {
+                if (taskNum == 0) {
+                    System.out.println("No task");
+                } else {
+                    for (int i = 1; i < taskNum + 1; i++) {
+                        String currTask = taskList[i - 1];
+                        System.out.println(i + ". " + currTask);
+                    }
+                }
+            } else {
+                taskList[taskNum] = curr_word;
+                taskNum++;
+                System.out.println("added: " + curr_word);
             }
         }
     }
