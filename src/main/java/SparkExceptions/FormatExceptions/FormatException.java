fea9a7e5bdd1314f427aa4f9209b5a0ab018c1ca
James Yeap <jamesyeap.ys@gmail.com>
2022-01-17 10:08:00 +0800

    Create an Exception class that represents all Exceptions arising from errors in formatting in user-inputs; ie missing Title when creating a new Todo.

---
+++ src/main/java/SparkExceptions/FormatExceptions/FormatException.java
@@ -0,0 +1,9 @@
+package SparkExceptions.FormatExceptions;
+
+import SparkExceptions.SparkException;
+
+public class FormatException extends SparkException {
+    public FormatException(String errorMessage) {
+        super(errorMessage);
+    }
+}
