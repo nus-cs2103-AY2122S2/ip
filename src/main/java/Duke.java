ebd1ae4d10446427ac8c144e443452ccbc1c68ab
iztanpy <isaactanpy2000@gmail.com>
2022-01-17 15:21:00 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,8 +11,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
 
-        ArrayList<String> tasks = new ArrayList<> ();
-
+        ArrayList<Task> tasks = new ArrayList<> ();
 
         while (true){
             String str= sc.nextLine();
@@ -22,15 +21,39 @@ public class Duke {
                 break;
             }
 
-            if (str.equals("list")) {
+            // list
+            else if (str.equals("list")) {
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(i + 1 + " " + tasks.get(i));
+                    Task currentTask = tasks.get(i);
+                    int index = i + 1;
+                    System.out.println(index + "." + currentTask.getStatusIcon() + " " + currentTask.toString());
                 }
+            }
+
+            // mark
+            else if (str.length() >= 4 && str.substring(0,4).equals("mark")) {
+                String[] markedTask = str.split(" ");
+                int indexMarked = Integer.parseInt(markedTask[1]) - 1;
+                Task currentTask = tasks.get(indexMarked);
+                currentTask.isDone = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(currentTask.getStatusIcon() + " " + currentTask.toString());
+            }
+
+
+            // unmark
+            else if (str.length() >= 6 && str.substring(0,6).equals("unmark")) {
+                String[] markedTask = str.split(" ");
+                int indexMarked = Integer.parseInt(markedTask[1]) - 1;
+                Task currentTask = tasks.get(indexMarked);
+                currentTask.isDone = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(currentTask.getStatusIcon() + " " + currentTask.toString());
             }
 
             // repeat what the user says and save to tasks list
             else {
-                tasks.add(str);
+                tasks.add(new Task(str));
                 System.out.println("added: " + str);
             }
         }
