e05b0f00b16b36892fc4277d5157bede6780abdf
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-01-17 21:50:53 +0800

    Implemented Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,33 +1,48 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         String greeting = "    ____________________________________________________________\n"
-                + "     Hello! I'm Duke\n"
-                + "     What can I do for you?\n"
-                + "    ____________________________________________________________";
+                        + "     Hello! I'm Duke\n"
+                        + "     What can I do for you?\n"
+                        + "    ____________________________________________________________";
         System.out.println(greeting);
 
         Scanner sc = new Scanner(System.in);
 
         String userInput = sc.nextLine();
+        ArrayList<String> listOfTasks = new ArrayList<>();
 
         // Repeat till bye is entered
         while (!userInput.equals("bye")) {
 
-            // Echo current input
-            String toPrint = "    ____________________________________________________________\n"
-                + "    " + userInput + "\n"
-                + "    ____________________________________________________________";
-            System.out.println(toPrint);
+            if (userInput.equals("list")) {
+                String toPrint = "    ____________________________________________________________";
+                System.out.println(toPrint);
+                for (int index = 1; index < listOfTasks.size() + 1; index++) {
+                    System.out.println("     " + index + ". " + listOfTasks.get(index - 1));
+                }
+                System.out.println(toPrint);
+            } else {
+                // Echo current input
+                String toPrint = "    ____________________________________________________________\n"
+                               + "     added: " + userInput + "\n"
+                               + "    ____________________________________________________________";
+
+                // Add the user input to list of tasks
+                listOfTasks.add(userInput);
+
+                System.out.println(toPrint);
+            }
 
             // Get next input
             userInput = sc.nextLine();
         }
 
         System.out.println("    ____________________________________________________________\n"
-                + "     Bye. Hope to see you again soon!\n"
-                + "    ____________________________________________________________");
+                         + "     Bye. Hope to see you again soon!\n"
+                         + "    ____________________________________________________________");
 
     }
 }
