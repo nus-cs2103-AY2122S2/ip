f989ebe1a8fec744087ee132d80d84af2ec937e1
dionegoh <e0439283@u.nus.edu>
2022-01-17 17:25:20 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+
+        String line = "_________________________________";
+        String indentation = "     ";
+
+        System.out.println(indentation + line + "\n" + indentation +
+                "Hello! I'm PikaBot " + "\n" + indentation +
+                "What can I do for you? \n" + indentation +
+                line);
+
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(indentation + line + "\n" + indentation +
+                    input + "\n" + indentation + line);
+            input = sc.nextLine();
+        }
+
+        System.out.println(indentation + line + "\n" + indentation +
+                "Bye. Hope to see you again!" + "\n" + indentation +
+                line);
+
+        sc.close();
     }
 }
