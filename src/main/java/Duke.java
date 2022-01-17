393626eaac1e7657ab48174c53f39fb6e88bb9e2
Kevin <thamooie@yahoo.com.sg>
2022-01-18 01:51:58 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " _       _   _   __  __   _   _ \n"
+                    + "| |     | | | | |  \\/  | | | | |\n"
+                    + "| |     | | | | | |\\/| | | | | |\n"
+                    + "| |___  | |_| | | |  | | | |_| |\n"
+                    + "|_____|  \\___/  |_|  |_|  \\___/ \n";
+
+        String linebreak = "\n－－－－－－－－－－（・Ａ・）－－－－－－－－－\n";
+
+        System.out.println(linebreak + "Hello I'm\n" + logo + linebreak);
+        System.out.println("What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String str = sc.nextLine();
+
+            if (str.compareToIgnoreCase("bye") == 0) {
+                System.out.println("LUMU: Goodbye. Hope to see you again soon");
+                System.out.println(linebreak);
+                sc.close();
+                return;
+            } else {
+                System.out.println("LUMU: " + str);
+                System.out.println(linebreak);
+            }
+        }
     }
 }
