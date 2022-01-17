d8968ab8b040dd3f79facba33b2b6377fba83baa
Bryan Ong <bryan.o@u.nus.edu>
2022-01-18 02:51:27 +0800

    Implemented level 1 - Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| |_| | | | | |/ / _ \\\n"
+                    + "| |__/| |_| |   <  __/\n"
+                    + "|_|    \\__,_|_|\\_\\___|\n";
+
+        System.out.printf("____________________________________________________________\n"
+                    + logo + "Hello! I'm Puke, your friendly neighbourhood chatbot!\n"
+                    + "What do you want to do?\n"
+                    + "____________________________________________________________\n");
+
+        while (true) {
+            System.out.printf("> ");
+            String input = sc.nextLine();
+            if (input.equals("bye")) break;
+
+            System.out.println(input + "\n____________________________________________________________");
+        }
+        System.out.println("Goodbye. Come back by anytime!\n____________________________________________________________");
     }
 }
