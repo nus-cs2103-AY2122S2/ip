7bcf55b13bfc2f2cbda26a5138051180f40279c2
Btaykb <bentay728@gmail.com>
2022-01-17 16:48:26 +0800

    completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String bar = "-------------------------------------------------"; //Reusable horizontal bar display
+        String indent = "      "; //Reusable indentation display 
+        System.out.println(bar);
+        System.out.println("Hi I'm Zen!\n" +
+                "How can I help ?");
+        System.out.println(bar);
+        System.out.println();
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(bar);
+            System.out.println(indent + input);
+            System.out.println(bar);
+            System.out.println();
+            input = sc.nextLine();
+        }
+        System.out.println(bar);
+        System.out.println(indent + "Bye! See you soon !");
+        System.out.println(bar);
     }
 }
