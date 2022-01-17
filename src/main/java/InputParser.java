c2da42d9ddbb438d8d8f76bfff6dfef7c3cf3fd1
HappyPear <ongweixin@hotmail.com>
2022-01-17 22:23:07 +0800

    Error Handling for Task Creation

--- src/main/java/InputParser.java
+++ src/main/java/InputParser.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class InputParser {
     public int run(String ss, Printer p, ArrayList<Task> arr) throws DukeException {
@@ -9,9 +10,10 @@ public class InputParser {
                 p.print(" Bye. Hope to see you again soon!");
                 return -1;
             } else if (action.equals("list")) {
+                // Empty List handled by Printer
                 p.print(arr);
             } else if (action.equals("mark")) {
-                //===========ERROR HANDLING============================
+
                 if(args.length == 1)
                     throw new DukeException("Task ID must be provided!");
                 try {
@@ -23,11 +25,11 @@ public class InputParser {
                     throw new DukeException("Task List is empty!");
                 if(Integer.parseInt(args[1]) > arr.size())
                     throw new DukeException("Task ID out of range!");
-                //=======================================================
+
                 arr.get(Integer.parseInt(args[1]) - 1).mark();
                 p.print("Nice! I've marked this task as done: ", " " + arr.get(Integer.parseInt(args[1]) - 1).toString());
             } else if (action.equals("unmark")) {
-                //===========ERROR HANDLING============================
+
                 if(args.length == 1)
                     throw new DukeException("Task ID must be provided!");
                 try {
@@ -39,18 +41,41 @@ public class InputParser {
                     throw new DukeException("Task List is empty!");
                 if(Integer.parseInt(args[1]) > arr.size())
                     throw new DukeException("Task ID out of range!");
-                //=======================================================
+
                 arr.get(Integer.parseInt(args[1]) - 1).unmark();
                 p.print("OK, I've marked this task as not done yet: "," " + arr.get(Integer.parseInt(args[1]) - 1).toString());
             } else if (action.equals("todo")) {
+                if(args.length == 1)
+                    throw new DukeException("Task Name must be provided!");
+
                 ToDoTask newTask = new ToDoTask((ss).substring(5, ss.length()));
                 arr.add(newTask);
                 p.print("Added Task: ", " " + newTask.toString(), String.format("There are now %d task(s) in the list.", arr.size()));
             } else if (action.equals("deadline")) {
+
+                if(args.length == 1)
+                    throw new DukeException("Task Name must be provided!");
+                if(!Arrays.stream(args).anyMatch("/by"::equals))
+                    throw new DukeException("/by flag not detected. Please specify deadline date using /by!");
+                if(ss.trim().split("/by").length == 1)
+                    throw new DukeException("Please specify deadline date after /by!");
+                if (ss.substring(9, ss.length()).split("/by")[0].trim().equals(""))
+                    throw new DukeException("Task Name must be provided!");
+
                 DeadlineTask newTask = new DeadlineTask(ss.substring(9, ss.length()).split("/by")[0], ss.split("/by")[1]);
                 arr.add(newTask);
                 p.print("Added Task: ", " " + newTask.toString(), String.format("There are now %d task(s) in the list.", arr.size()));
             } else if (action.equals("event")) {
+
+                if(args.length == 1)
+                    throw new DukeException("Task Name must be provided!");
+                if(!Arrays.stream(args).anyMatch("/at"::equals))
+                    throw new DukeException("/at flag not detected. Please specify event location using /at!");
+                if(ss.trim().split("/at").length == 1)
+                    throw new DukeException("Please specify deadline date after /at!");
+                if (ss.substring(6, ss.length()).split("/at")[0].trim().equals(""))
+                    throw new DukeException("Task Name must be provided!");
+
                 EventTask newTask = new EventTask(ss.substring(6, ss.length()).split("/at")[0], ss.split("/at")[1]);
                 arr.add(newTask);
                 p.print("Added Task: ", " " + newTask.toString(), String.format("There are now %d task(s) in the list.", arr.size()));
