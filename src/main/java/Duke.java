56b314e0b3218ad38f6c18f2f6b834127b8d8c6c
Ryan Walter Lee <88182652+ryanwalterlee@users.noreply.github.com>
2022-01-17 12:06:02 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -6,10 +8,19 @@ public class Duke {
         String intro = "Hello! I'm Duke\n" +
                 "What can I do for you?";
         System.out.println(intro);
+        ArrayList<String> tasks = new ArrayList<>();
         String cmd = sc.nextLine();
         while (!cmd.equals("bye")) {
-            System.out.println(cmd);
+            if (cmd.equals("list")) {
+                for (int i = 1; i <= tasks.size(); i++) {
+                    System.out.println(i + ". " + tasks.get(i - 1));
+                }
+            } else {
+                tasks.add(cmd);
+                System.out.println("added: " + cmd);
+            }
             cmd = sc.nextLine();
+
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
