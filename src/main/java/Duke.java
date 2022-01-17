017872f00bda64cb3aefca842609be3ec345e7ea
Chua Bing Quan <chipneverdies@gmail.com>
2022-01-17 23:07:02 +0800

    Refactor duke commands to correspond to relevant methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,32 +4,49 @@ public class Duke {
     private static final String INDENTATION = "    ";
     private static final String DIVIDER = "____________________________________________________________";
     private static final String GREETING = "Hello! I'm Duke\nWhat can I do for you?";
-    private static final String GOODBYE = "Bye. Hope to see you again soon!";
+    private static final String FAREWELL = "Bye. Hope to see you again soon!";
+    private static final Task[] tasks = new Task[100];
+    private static int nextItemIndex = 0;
 
     public static void main(String[] args) throws Exception {
         final Scanner input = new Scanner(System.in);
-        final Task[] tasks = new Task[100];
-        int nextItemIndex = 0;
+        Duke.greet();
 
-        System.out.println(Duke.constructResponse(Duke.GREETING));
         try {
             while (true) {
                 final String command = input.nextLine();
                 switch (command) {
                     case "bye":
-                        System.out.println(Duke.constructResponse(Duke.GOODBYE));
+                        Duke.bidFarewell();
                         return;
                     case "list":
-                        System.out.println(Duke.constructResponse(Duke.constructTextList(tasks)));
+                        Duke.handleList();
                         break;
                     default:
-                        tasks[nextItemIndex++] = new Task(command);
-                        System.out.println(Duke.constructResponse("added: " + command));
+                        Duke.handleAdd(new Task(command));
                 }
             }
         } finally {
             input.close();
         }
+    }
+
+    private static void greet() {
+        System.out.println(Duke.constructResponse(Duke.GREETING));
+    }
+
+    private static void bidFarewell() {
+        System.out.println(Duke.constructResponse(Duke.FAREWELL));
+    }
+
+    private static void handleList() {
+        final String response = Duke.constructResponse(Duke.constructTextList(Duke.tasks));
+        System.out.println(response);
+    }
+
+    private static void handleAdd(Task t) {
+        Duke.tasks[Duke.nextItemIndex++] = t;
+        System.out.println(Duke.constructResponse("added: " + t));
     }
 
     private static String constructResponse(String content) {
