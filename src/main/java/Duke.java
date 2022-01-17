c7a2826c562540e7d869390a72cce387c9e3eeda
sbhbenjamin <sbhbenjamin@gmail.com>
2022-01-17 09:53:34 +0800

    Add level-2 features: add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
+    private ArrayList<Task> taskList;
+    private Scanner scanner;
+
+    public void greet() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,20 +13,56 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
-        Scanner sc = new Scanner(System.in);
-        String input = "";
+    }
 
-        while (true) {
-            input = sc.nextLine();
-            if (input.equals("bye")) {
-                break;
-            }
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
+    public void echo(String input) {
+
+        System.out.println(input);
+    }
+
+    public void addTask(String input) {
+        Task newTask = new Task(input);
+        System.out.println("____________________________________________________________");
+        System.out.printf("added: %s%n", input);
+        System.out.println("____________________________________________________________");
+        taskList.add(newTask);
+    }
+
+    public void listTasks() {
+        System.out.println("____________________________________________________________");
+        for (int i = 1; i < taskList.size() + 1; i++) {
+            System.out.printf("%d. %s%n", i, taskList.get(i - 1));
         }
         System.out.println("____________________________________________________________");
+    }
+
+    public void exit() {
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
-        sc.close();
+        scanner.close();
+    }
+
+    public void start() {
+        this.greet();
+        this.taskList = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
+
+        String input = "";
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                this.listTasks();
+            } else {
+                this.addTask(input);
+            }
+        }
+        this.exit();
+    }
+
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.start();
     }
 }
