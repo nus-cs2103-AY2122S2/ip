2513480ad3d20ab13af0b665b0c28ea4565a1a3d
Korochii <yanlong21@live.com.sg>
2022-01-18 01:13:20 +0800

    Update javadocs for the Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,15 +2,31 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Initializes a new instance of task, according to the task description.
+     * By default, the new task is set to "not done".
+     *
+     * @param description Describes what needs to be done in this task
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Retrieves the status icon according to the current status (isDone).
+     *
+     * @return "X" if task is already done, " " if task is not done.
+     */
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (this.isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Retrieves the task description.
+     *
+     * @return A string that represents the task description.
+     */
     public String getDescription() {
         return this.description;
     }
