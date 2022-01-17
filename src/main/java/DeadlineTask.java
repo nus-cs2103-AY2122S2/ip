1d525f96d3b36388869e3c4eb0c66dc02ea94701
HappyPear <ongweixin@hotmail.com>
2022-01-17 21:43:11 +0800

    DeadlineTask Creation and updated DeadlineTask toString method

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,13 +1,14 @@
 public class DeadlineTask extends Task{
     private String by;
 
-    DeadlineTask(String ss) {
+    DeadlineTask(String ss, String by) {
         this.taskName = ss;
         this.isDone = false;
+        this.by = by;
     }
 
     @Override
     public String toString() {
-        return String.format("[D][%s] %s", this.isDone?"X":" ", this.taskName);
+        return String.format("[D][%s] %s (by:%s)", this.isDone?"X":" ", this.taskName, this.by);
     }
 }
