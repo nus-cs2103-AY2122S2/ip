f0790a6a920d7b2afa718ac6ab90c1176f7f03f3
aaron-ljx <e0556043@u.nus.edu.sg>
2022-01-17 19:33:21 +0800

    Level-1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("\nHow may I help you today?\n");
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (Objects.equals(input.toLowerCase(), "bye")) {
+                System.out.println("\nGoodbye.\nHave a nice day!");
+                break;
+            } else {
+                System.out.println(input + "\n");
+            }
+        }
     }
 }
