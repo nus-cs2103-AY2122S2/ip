6e53a34d38862c3b152f8b55062a44739af4a12b
Aryan Sarswat <65006917+AryanSarswat@users.noreply.github.com>
2022-01-17 21:31:49 +0800

    Implemented Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "    ____________________________________________________________\n"
+                + "     Hello! I'm Duke\n"
+                + "     What can I do for you?\n"
+                + "    ____________________________________________________________";
+        System.out.println(greeting);
+
+        Scanner sc = new Scanner(System.in);
+
+        String userInput = sc.nextLine();
+
+        // Repeat till bye is entered
+        while (!userInput.equals("bye")) {
+
+            // Echo current input
+            String toPrint = "    ____________________________________________________________\n"
+                + "    " + userInput + "\n"
+                + "    ____________________________________________________________";
+            System.out.println(toPrint);
+
+            // Get next input
+            userInput = sc.nextLine();
+        }
+
+        System.out.println("    ____________________________________________________________\n"
+                + "     Bye. Hope to see you again soon!\n"
+                + "    ____________________________________________________________");
+
     }
 }
