17eff43acdd4c78a44ee1fc52669130f81183fb2
eman-kom <emmanuelmok92@gmail.com>
2022-01-17 12:25:49 +0800

    Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
 
-    public static final String greeting = "\tHello! I'm Duke\n\tWhat can I do for you?\n";
     public static final String goodbye = "\tBye. Hope to see you again soon!";
+    public static final String greeting = "\tHello! I'm Duke\n\tWhat can I do for you?\n";
 
     public static void main(String[] args) {
 
@@ -16,9 +17,9 @@ public class Duke {
 
         while (!input.equals("bye")) {
 
-            String[] arr = input.split(" ", 2);
+            String[] arr = input.trim().split(" ", 2);
             String cmd = arr[0];
-            String var = arr.length > 1 ? arr[1] : "";
+            String var = arr.length < 2 ? "" : arr[1];
 
             switch (cmd) {
 
@@ -31,45 +32,112 @@ public class Duke {
                     break;
 
                 case "mark":
-                    int mark = Integer.parseInt(var) - 1;
-                    taskList.get(mark).setCompleted(true);
-                    System.out.println("\tNice! I've marked this task as done:\n\t\t" + var + ". " + taskList.get(mark).toString() + "\n");
+                    marking(taskList, var, true);
                     break;
 
                 case "unmark":
-                    int unmark = Integer.parseInt(var) - 1;
-                    taskList.get(unmark).setCompleted(false);
-                    System.out.println("\tOK, I've marked this task as not done yet:\n\t\t" + var + ". " + taskList.get(unmark).toString() + "\n");
+                    marking(taskList, var, false);
                     break;
 
                 case "event":
-                    String[] e = var.split("/at", 2);
-                    Event newEvent = new Event(e[0], e[1]);
-                    taskList.add(newEvent);
-                    System.out.println("\tGot it. I've added this task:\n\t\t" + newEvent + "\n\tNow you have "+ taskList.size() +" tasks in the list.\n");
+                    String[] e = var.trim().split("/at", 2);
+
+                    try {
+                        checkInput(e, "/at");
+                    }
+
+                    catch (DukeException ex) {
+                        System.out.println("\t" + ex + "\n");
+                        input = sc.nextLine();
+                        continue;
+                    }
+
+                    createNewTask(taskList, new Event(e[0], e[1]));
                     break;
 
                 case "deadline":
-                    String[] d = var.split("/by", 2);
-                    Deadline newDeadline = new Deadline(d[0], d[1]);
-                    taskList.add(newDeadline);
-                    System.out.println("\tGot it. I've added this task:\n\t\t" + newDeadline + "\n\tNow you have "+ taskList.size() +" tasks in the list.\n");
+                    String[] d = var.trim().split("/by", 2);
+
+                    try {
+                        checkInput(d, "/by");
+                    }
+
+                    catch (DukeException ex) {
+                        System.out.println("\t" + ex + "\n");
+                        input = sc.nextLine();
+                        continue;
+                    }
+
+                    createNewTask(taskList, new Deadline(d[0], d[1]));
                     break;
 
                 case "todo":
-                    ToDo newTodo = new ToDo(var);
-                    taskList.add(newTodo);
-                    System.out.println("\tGot it. I've added this task:\n\t\t" + newTodo + "\n\tNow you have "+ taskList.size() +" tasks in the list.\n");
+                    if (var.equals("")) {
+                        System.out.println("\tNo task to do\n");
+                    }
+
+                    else {
+                        createNewTask(taskList, new ToDo(var));
+                    }
+
                     break;
 
                 default:
-                    taskList.add(new Task(input));
-                    System.out.println("\tadded: " + input + "\n");
+                    System.out.println("\tWrong command\n");
             }
 
             input = sc.nextLine();
         }
 
         System.out.println(goodbye);
+    }
+
+    private static void createNewTask(ArrayList<Task> taskList, Task task) {
+        taskList.add(task);
+        System.out.println("\tGot it. I've added this task:");
+        System.out.println("\t\t" + task);
+        System.out.println("\tNow you have "+ taskList.size() +" tasks in the list.\n");
+    }
+
+    private static void marking(ArrayList<Task> taskList, String idx, boolean state) {
+
+        try {
+            int i = Integer.parseInt(idx) - 1;
+            taskList.get(i).setCompleted(state);
+            System.out.println(state ? "\tNice! I've marked this task as done:" : "\tOK, I've marked this task as not done yet:");
+            System.out.println("\t\t" + idx + ". " + taskList.get(i).toString() + "\n");
+        }
+
+        catch (NumberFormatException e) {
+            System.out.println("\tThere is no a number\n");
+        }
+
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("\tTask does not exist\n");
+        }
+    }
+
+    private static void checkInput(String[] str, String type) throws DukeException {
+
+        switch (str.length) {
+            case 0:
+                throw new DukeException("No task in instruction");
+
+            case 1:
+                throw new DukeException(type + " not found");
+
+            case 2:
+                if (str[0].equals("") && str[1].equals("")) {
+                    throw new DukeException("No task in instruction");
+                }
+
+                else if (str[0].equals("")) {
+                    throw new DukeException("Description of task not found");
+                }
+
+                else if (str[1].equals("")) {
+                    throw new DukeException("Datetime of task not found");
+                }
+        }
     }
 }
