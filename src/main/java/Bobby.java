10e1c561d302a343d67e639bf21512d528643663
YiFei2 <fongyifei1999@gmail.com>
2022-01-18 06:38:20 +0800

    completed level-2

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Bobby {
     public static void main(String[] args) {
+        ArrayList<String> stringArray = new ArrayList<String>();
         System.out.println("Bobby greets you. Bobby is here to help.");
 
         while(true) {
@@ -10,8 +12,14 @@ public class Bobby {
             if (userInput.equals("bye")) {
                 System.out.println("Bobby bids you farewell.");
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println("Here is what you told Bobby:");
+                for (int i = 0; i < stringArray.size(); i++) {
+                    int count = i + 1;
+                    System.out.println(count + ". " + stringArray.get(i));
+                }
             } else {
-                System.out.println(userInput);
+                stringArray.add(userInput);
             }
         }
     }
