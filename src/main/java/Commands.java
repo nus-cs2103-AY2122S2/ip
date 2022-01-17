0e0350b6f1e26b22dc6fcf0eca2f46109e5ef18f
blaketingyu <blaketantingyu@gmail.com>
2022-01-18 00:49:37 +0800

    Moved all the if else cases to Commands file

---
+++ src/main/java/Commands.java
@@ -0,0 +1,157 @@
+public class Commands {
+    private String command;
+
+    public Commands(String command) {
+        this.command = command;
+    }
+    
+    //Default case 
+    public void invalidCommand() {
+        System.out.println("\n" + this.command + "?");
+        System.out.println("What are you on about?");
+        System.out.println("Type list if you want to know the commands, kid!");
+    }
+
+    //List of commands
+    public void listOfCommand() {
+        System.out.println("____________________________________________________________");
+        System.out.println("\nCommands: ");
+        System.out.println("    List                                    -list out all your current tasks");
+        System.out.println("    todo <task name>                        -Add a todo task without any deadline specified");
+        System.out.println("    deadline <task name> /by <deadline>     -Adds a task that has to be done before the specified deadline");
+        System.out.println("    event <task name> /at <deadline>        -Adds a task that occurs at the specified deadline");
+        System.out.println("    mark <task number>                      -marks task as completed");
+        System.out.println("    unmark <task number>                    -marks a completed task as uncompleted");
+        System.out.println("    bye                                     -exits the program :'( ");
+        System.out.println("____________________________________________________________");
+    }
+
+
+    public void performCommand(TaskList tasklist) {
+        String splitString[] = this.command.split(" ");
+        String firstWord = splitString[0];
+        
+         if (firstWord.equals("list")) {
+                tasklist.list();
+         } 
+         
+        else if (firstWord.equals("mark") || firstWord.equals("unmark") || firstWord.equals("delete")) {
+            String deleted = "";
+             //Handle the case of having no second input
+             if (splitString.length == 1 ) {
+                 System.out.println("What?! You are to enter only 2 inputs. Eg mark 1, unmark 2, delete 3\n");
+             }  else if (splitString.length > 2) {
+                 //Handle the case of having more than 2 inputs
+                 System.out.println("What?! You are to enter only 2 inputs. Eg mark 1, unmark 2, delete 3\n");
+             } else {
+                 try {
+                     //Handle error if the second input is not an integer
+                     //Gets the index of the task in the task list
+                     int index = Integer.parseInt(splitString[1]);
+                     //If index is out of range, throw illegal argument exception
+                     if (index <= 0 || index > tasklist.currentSize) {
+                         throw new DukeExceptions("  Index is out of task range");
+                     }
+                     //If no errors, continue
+                       //Task that we are going to mark/unmark/delete
+                    Task thatTask = tasklist.get(index - 1);
+                     String displayMessage = "";
+                     if (firstWord.equals("mark")) {
+                         displayMessage = "Aye! I've marked this task as completed:\n";
+                         tasklist.mark(index - 1);
+                     } else if (firstWord.equals("unmark")){
+                         displayMessage = "Aye kid! I've marked this task as uncompleted:\n";
+                         tasklist.unmark(index - 1);
+                     } else {
+                         tasklist.delete(index - 1);
+                         displayMessage = "YES! I've removed this task and soon I'll remove you as well!:\n";
+                         deleted = "Now you have " + tasklist.currentSize + " tasks in the list";
+                     }
+                    
+                     System.out.println(displayMessage);
+                     System.out.println("    " + thatTask.toString());
+                     System.out.println(deleted);
+                    
+                     //NumberFormatException is thrown if the second input is not an integer
+                 } catch (NumberFormatException nfe) {
+                     System.out.println("What? Second input has to be an integer! Eg mark 1, unmark 2\n");
+                     //Out of task range is thrown if the second input is out of range
+                 } catch (DukeExceptions e) {
+                     System.out.println("BRAT ! Your index is out of range! Number has to in the range of the list");
+                 }
+
+             }
+         }  
+         
+         else if (firstWord.equals("todo") || firstWord.equals("deadline") || firstWord.equals("event")) {
+            //Pass to check whether task is to be created
+            boolean created = false;
+
+            //Handle the case of having no second input
+
+            if (splitString.length == 1 ) {
+                System.out.println("What?! Task description/name cannot be empty. Eg todo eat, deadline eat food /by 12pm, event concert /at 8pm\n");
+            } else {
+                String taskName = command.substring(command.indexOf(" "));
+                Task newTask = new Task("");
+                if (firstWord.equals("todo")) {
+                    newTask = new TodoTask(taskName);
+                    created = true;
+                } else {
+                    //Handle the case of having no "/" to specify deadline or time of occurrences for deadline and event tasks
+                    if (!taskName.contains("/")) {
+                        System.out.println("Quit fooling around kid!");
+                        System.out.println("Deadline and event tasks require /by and /at to specify the deadline or time of occurrence.\n");
+                        System.out.println("Eg Deadline eat food /by 12pm, event concert /at 8pm\n");
+                    } else {
+                        String temp = taskName.substring(taskName.indexOf("/"));
+                        String taskNameWithoutBack = taskName.substring(0, taskName.indexOf("/"));
+                        //String that states by ... or at....
+                        if (firstWord.equals("deadline")) {
+                            //Handle the case of deadline task having no /by
+                            if (!temp.contains("by")) {
+                                System.out.println("Quit fooling around kid!");
+                                System.out.println("Deadline tasks require /by specify the deadline.");
+                                System.out.println("Eg deadline eat food /by 12pm");
+                            } else {
+                                String timeOfOccurrence = temp.substring(temp.indexOf(" ") + 1);
+                                newTask = new DeadlineTask(taskNameWithoutBack, timeOfOccurrence);
+                                created = true;
+                            }
+                        } else {
+                            //Handle the case of event task having no /at
+                            if (!temp.contains("at")) {
+                                System.out.println("Quit fooling around kid!");
+                                System.out.println("Event tasks require /at specify the time of occurrence.");
+                                System.out.println("music concert /at 8pm\n");
+                            } else {
+                                String timeOfOccurrence = temp.substring(temp.indexOf(" ") + 1);
+                                newTask = new EventTask(taskNameWithoutBack, timeOfOccurrence);
+                                created = true;
+                            }
+                        }
+                    }
+                }
+
+                //Only tasks that pass the check can be created
+                if (created) {
+                    tasklist.add(newTask);
+                    System.out.println("Quit ordering me around!");
+                    System.out.println("I've added this task to our list:");
+                    System.out.println("    " + newTask);
+                    System.out.println("    Now you have " + tasklist.currentSize + " tasks in the list.\n");
+                }
+
+            }
+        }
+
+        else if (firstWord.equals("help")) {
+          listOfCommand();
+        }
+
+        else {
+           invalidCommand();
+        }
+           
+    }
+}
