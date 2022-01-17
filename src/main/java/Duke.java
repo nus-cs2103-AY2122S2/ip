4cd524419df748b94b68f97195d82a0313d3fa66
DESKTOP-6G3IIBM\User <arcornior>
2022-01-17 17:18:55 +0800

    Replaced hardcoded spaces with final values instead

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,19 +7,21 @@ public class Duke {
     }
 
     public static void main(String[] args) {
+        final String SPACE = "\n      ";
+        final String TASKSPACE = "\n        ";
         String logo =
                 " _____           _ _   \n" +
-                "|  ___|         (_) |   \n" +
-                "| |__ _ __   ___ _| | __\n" +
-                "|  __| '_ \\ / __| | |/ /\n" +
-                "| |__| | | | (__| |   < \n" +
-                "\\____/_| |_|\\___|_|_|\\_\\\n";
+                        "|  ___|         (_) |   \n" +
+                        "| |__ _ __   ___ _| | __\n" +
+                        "|  __| '_ \\ / __| | |/ /\n" +
+                        "| |__| | | | (__| |   < \n" +
+                        "\\____/_| |_|\\___|_|_|\\_\\\n";
         System.out.println("GOOD MORNING GENNERMEN from\n" + logo);
 
         String border = "   ____________________________________________________________";
         String welcome =
                 "GOOD MORNING CHAO RECRUIT! YOU MAY CALL ME ENCIK ENCIK\n" +
-                "      WHAT YOU WANT?";
+                        "      WHAT YOU WANT?";
         format(welcome);
 
         //Create ArrayList to store tasks
@@ -27,7 +29,7 @@ public class Duke {
 
         Scanner input = new Scanner(System.in);
         String toEcho = input.nextLine();
-        while(!toEcho.equals("bye")) {
+        while (!toEcho.equals("bye")) {
             //Add name of task to str for easy printing
             StringBuilder str = new StringBuilder();
             str.append("NEED ME TO REMIND YOU AH?\n      ");
@@ -50,15 +52,15 @@ public class Duke {
                     int num = Integer.parseInt(rem) - 1;
                     tasks.get(num).mark();
                     String confirm =
-                            "THIS ONE\n        " + tasks.get(num) +
-                            "\n      FINISH ALREADY AH? SWEE CHAI BUTTERFLY RECRUIT!";
+                            "THIS ONE" + TASKSPACE + tasks.get(num) + SPACE +
+                                    "FINISH ALREADY AH? SWEE CHAI BUTTERFLY RECRUIT!";
                     format(confirm);
                 } else if (cmd.equals("unmark")) {
                     int num = Integer.parseInt(rem) - 1;
                     tasks.get(num).unmark();
                     String confirm =
-                            "I THOUGHT THIS ONE\n        " + tasks.get(num) +
-                            "\n      FINISH ALREADY? NEVER MIND THIS WEEKEND CONFINE!";
+                            "I THOUGHT THIS ONE" + TASKSPACE + tasks.get(num) + SPACE +
+                                    "FINISH ALREADY? NEVER MIND THIS WEEKEND CONFINE!";
                     format(confirm);
                 } else if (cmd.equals("deadline")) {
                     //deadline
@@ -67,9 +69,9 @@ public class Duke {
                     Task tempTask = new Deadline(task, dead);
                     tasks.add(tempTask);
                     String confirm =
-                            "YOU BETTER FINISH THIS AH:\n        " + tempTask +
-                            "\n      YOU STILL GOT " + tasks.size() +
-                            " THINGS TO DO AH BETTER NOT FORGET!";
+                            "YOU BETTER FINISH THIS AH:" + TASKSPACE + tempTask + SPACE +
+                                    "YOU STILL GOT " + tasks.size() +
+                                    " THINGS TO DO AH BETTER NOT FORGET!";
                     format(confirm);
                 } else if (cmd.equals("event")) {
                     //event
@@ -78,18 +80,18 @@ public class Duke {
                     Task tempTask = new Event(task, dead);
                     tasks.add(tempTask);
                     String confirm =
-                            "YOU BETTER REMEMBER THIS AH:\n        " + tempTask +
-                            "\n      YOU STILL GOT " + tasks.size() +
-                            " THINGS TO DO AH BETTER NOT FORGET!";
+                            "YOU BETTER REMEMBER THIS AH:" + TASKSPACE + tempTask + SPACE +
+                                    "YOU STILL GOT " + tasks.size() +
+                                    " THINGS TO DO AH BETTER NOT FORGET!";
                     format(confirm);
                 } else if (cmd.equals("todo")) {
                     //todo
                     Task tempTask = new ToDo(rem);
                     tasks.add(tempTask);
                     String confirm =
-                            "YOU WANT TO DO THIS AH:\n        " + tempTask +
-                            "\n      VERY GOOD! YOU STILL GOT " + tasks.size() +
-                            " THINGS TO DO AH BETTER NOT FORGET!";
+                            "YOU WANT TO DO THIS AH:" + TASKSPACE + tempTask + SPACE +
+                                    "VERY GOOD! YOU STILL GOT " + tasks.size() +
+                                    " THINGS TO DO AH BETTER NOT FORGET!";
                     format(confirm);
                 }
             }
