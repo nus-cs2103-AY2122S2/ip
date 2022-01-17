90fa9d8a6f5a28369cf9c418c44c50daf903b90e
Kevin <thamooie@yahoo.com.sg>
2022-01-18 02:52:31 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,68 @@
 import java.util.Scanner;
 
 public class Duke {
+    private int repeatListSize = 0;
+    private String[] repeatList = new String[100];
+    private Scanner scanner;
+
+    public Duke(Scanner sc) {
+        this.scanner = sc;
+    }
+
     public static void main(String[] args) {
-        String logo = " _       _   _   __  __   _   _ \n"
-                    + "| |     | | | | |  \\/  | | | | |\n"
-                    + "| |     | | | | | |\\/| | | | | |\n"
-                    + "| |___  | |_| | | |  | | | |_| |\n"
-                    + "|_____|  \\___/  |_|  |_|  \\___/ \n";
-
-        String linebreak = "\n－－－－－－－－－－（・Ａ・）－－－－－－－－－\n";
-
-        System.out.println(linebreak + "Hello I'm\n" + logo + linebreak);
-        System.out.println("What can I do for you?");
-
         Scanner sc = new Scanner(System.in);
+        Duke Lumu = new Duke(sc);
 
+        Lumu.botInitialize();
+        Lumu.botResponse();
+    }
+
+    private String lineBreak() {
+        return "\n－－－－－－－－－－（・Ａ・）－－－－－－－－－\n";
+    }
+
+    private void botInitialize() {
+        String logo = " _       _   _   __  __   _   _ \n"
+            + "| |     | | | | |  \\/  | | | | |\n"
+            + "| |     | | | | | |\\/| | | | | |\n"
+            + "| |___  | |_| | | |  | | | |_| |\n"
+            + "|_____|  \\___/  |_|  |_|  \\___/ \n";
+
+        System.out.println(lineBreak() + "Hello I'm\n" + logo);
+        System.out.println("What can I do for you?" + lineBreak());
+    }
+
+    private void botResponse() {
         while (true) {
-            String str = sc.nextLine();
+            String str = scanner.nextLine();
 
             if (str.compareToIgnoreCase("bye") == 0) {
                 System.out.println("LUMU: Goodbye. Hope to see you again soon");
-                System.out.println(linebreak);
-                sc.close();
+                System.out.println(lineBreak());
+                scanner.close();
                 return;
+            } else if (str.compareToIgnoreCase("list") == 0) {
+                displayList();
+                System.out.println(lineBreak());
             } else {
-                System.out.println("LUMU: " + str);
-                System.out.println(linebreak);
+                System.out.println("added: " + str);
+                addToList(str);
+                System.out.println(lineBreak());
+            }
+        }
+    }
+
+    private void addToList(String str) {
+        repeatList[repeatListSize] = str;
+        repeatListSize++;
+    }
+
+    private void displayList() {
+        if (repeatListSize == 0) {
+            System.out.println("LUMU: Your list is empty!");
+        } else {
+            for (int i = 0; i < repeatList.length && i < repeatListSize; i++) {
+                System.out.println(String.valueOf(i + 1) + ". " + repeatList[i]);
             }
         }
     }
