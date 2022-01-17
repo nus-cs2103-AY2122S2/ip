af4fd35be141a7a723fcb1eefaea78ea4f724a2d
Ryan Walter Lee <88182652+ryanwalterlee@users.noreply.github.com>
2022-01-17 13:05:14 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -8,18 +7,31 @@ public class Duke {
         String intro = "Hello! I'm Duke\n" +
                 "What can I do for you?";
         System.out.println(intro);
-        ArrayList<String> tasks = new ArrayList<>();
-        String cmd = sc.nextLine();
+        ArrayList<Task> tasks = new ArrayList<>();
+        String cmd = sc.next();
         while (!cmd.equals("bye")) {
             if (cmd.equals("list")) {
                 for (int i = 1; i <= tasks.size(); i++) {
                     System.out.println(i + ". " + tasks.get(i - 1));
                 }
-            } else {
-                tasks.add(cmd);
-                System.out.println("added: " + cmd);
+            } else if (cmd.equals("mark")) {
+                int taskNumber = sc.nextInt();
+                tasks.get(taskNumber - 1).mark();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks.get(taskNumber - 1));
+            } else if (cmd.equals("unmark")) {
+                int taskNumber = sc.nextInt();
+                tasks.get(taskNumber - 1).unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks.get(taskNumber - 1));
+            } else if (cmd.equals("add")){
+                System.out.println("What task do you want to add? ");
+                sc.nextLine();
+                String task = sc.nextLine();
+                tasks.add(new Task(task));
+                System.out.println("added: " + task);
             }
-            cmd = sc.nextLine();
+            cmd = sc.next();
 
         }
         System.out.println("Bye. Hope to see you again soon!");
