81626775363af587ad833d5ceca302ed0fa19c3c
aaron-ljx <e0556043@u.nus.edu.sg>
2022-01-17 19:46:34 +0800

    Level-2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -11,14 +12,20 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("\nHow may I help you today?\n");
 
+        ArrayList<String> inputList = new ArrayList<>();
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             if (Objects.equals(input.toLowerCase(), "bye")) {
                 System.out.println("\nGoodbye.\nHave a nice day!");
                 break;
+            } else if (Objects.equals(input.toLowerCase(), "list")) {
+                if (!inputList.isEmpty()) {
+                    inputList.forEach(System.out::println);
+                }
             } else {
-                System.out.println(input + "\n");
+                System.out.println("added: " + input + "\n");
+                inputList.add(input);
             }
         }
     }
