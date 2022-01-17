1e21ee7b45967410cd0c903fb2b1496263bbb096
HappyPear <ongweixin@hotmail.com>
2022-01-17 21:23:01 +0800

    unmark functionality

--- src/main/java/InputParser.java
+++ src/main/java/InputParser.java
@@ -13,7 +13,8 @@ public class InputParser {
             arr.get(Integer.parseInt(args[1]) - 1).mark();
             p.print("Nice! I've marked this task as done: "," " + arr.get(Integer.parseInt(args[1]) - 1).toString());
         } else if (action.equals("unmark")) {
-
+            arr.get(Integer.parseInt(args[1]) - 1).unmark();
+            p.print("OK, I've marked this task as not done yet: "," " + arr.get(Integer.parseInt(args[1]) - 1).toString());
         } else {
             arr.add(new Task(ss));
             p.print(String.format("added: %s", ss));
