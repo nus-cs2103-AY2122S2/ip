213d920b43457fb458bfef74e404e011b5cc538a
Ryan Walter Lee <88182652+ryanwalterlee@users.noreply.github.com>
2022-01-17 11:02:17 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String intro = "Hello! I'm Duke\n" +
+                "What can I do for you?";
+        System.out.println(intro);
+        String cmd = sc.nextLine();
+        while (!cmd.equals("bye")) {
+            System.out.println(cmd);
+            cmd = sc.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
