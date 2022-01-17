ce1d60fe9f33a7c566f77d931a14b31efa062eab
Ye Yint Paing Oo <e0543517@u.nus.edu>
2022-01-17 20:02:18 +0800

    Implement an initial skeletal version of the Duke that simply greets the user and exits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,8 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm\n" + logo + "I am at your service.\n");
+        System.out.println("Ciao! Hope to see you again!");
+
     }
 }
