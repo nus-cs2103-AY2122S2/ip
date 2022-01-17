b9d7517bb5068a7db6be5b9241a5261ab2febfcc
HappyPear <ongweixin@hotmail.com>
2022-01-17 21:46:12 +0800

    EventTas Creation and updated EventTask toString method

--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -1,14 +1,15 @@
 public class EventTask extends Task{
     private String location;
 
-    EventTask(String ss) {
+    EventTask(String ss, String at) {
         this.taskName = ss;
         this.isDone = false;
+        this.location = at;
     }
 
     @Override
     public String toString() {
-        return String.format("[E][%s] %s", this.isDone?"X":" ", this.taskName);
+        return String.format("[E][%s] %s (at:%s)", this.isDone?"X":" ", this.taskName, this.location);
     }
 
 }
