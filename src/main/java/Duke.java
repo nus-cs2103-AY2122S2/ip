a14039fc00f4b806238c44568681ae8fd8281536
Joshua <e0544333@u.nus.edu>
2022-01-18 00:53:23 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String bot = "Hello! I'm Duke\nWhat can I do for you?", response = "";
+        StringBuilder ListResponse = new StringBuilder();
 
         System.out.println(appendLines(bot));
 
@@ -13,8 +17,12 @@ public class Duke {
             if (response.compareTo("bye") == 0) {
                 System.out.println(appendLines("Bye. Hope to see you again soon!"));
                 break;
+            } else if (response.compareTo("list") == 0) {
+                System.out.println(appendLines(ListResponse.toString()));
+            } else {
+                System.out.println(appendLines("added: " + response));
+                ListResponse.append(response + "\n");
             }
-            System.out.println(appendLines(response));
         }
     }
 
