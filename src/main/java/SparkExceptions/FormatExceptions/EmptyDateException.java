98d57c3a52ebfd6cad21b02f3d39b494de0ed00e
James Yeap <jamesyeap.ys@gmail.com>
2022-01-17 10:08:44 +0800

    Create Exceptions that are thrown when user adds an invalid Task.

--- src/main/java/SparkExceptions/FormatExceptions/EmptyDateException.java
+++ src/main/java/SparkExceptions/FormatExceptions/EmptyDateException.java
@@ -1,8 +1,10 @@
+package SparkExceptions.FormatExceptions;
+
 /**
  * An exception thrown when the user attempts to create a new
- * Deadline or Event with no name.
+ * Tasks.Deadline or Tasks.Event with no name.
  */
-public class EmptyDateException extends Exception {
+public class EmptyDateException extends FormatException {
     public EmptyDateException() {
         super("OOPS!!! You have to give your task a date!");
     }
--- src/main/java/SparkExceptions/FormatExceptions/EmptyTitleException.java
+++ src/main/java/SparkExceptions/FormatExceptions/EmptyTitleException.java
@@ -1,8 +1,10 @@
+package SparkExceptions.FormatExceptions;
+
 /**
  * An exception thrown when the user attempts to create a new
- * Task with no name.
+ * Tasks.Task with no name.
  */
-public class EmptyTitleException extends Exception {
+public class EmptyTitleException extends FormatException {
     public EmptyTitleException() {
         super("OOPS!!! You have to give your task a title!");
     }
