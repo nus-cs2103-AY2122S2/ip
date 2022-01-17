7f2de0d4b14e4804b08220ab1b7778f995175e09
honganhcs <e0560140@u.nus.edu>
2022-01-17 23:16:16 +0800

    Added the ability to store text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,31 @@
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Duke {
+
+    private ArrayList<String> list;
+    private int numItems;
+
+    private Duke() {
+        this.list = new ArrayList<>();
+        this.numItems = 0;
+    }
+
+    private void addItem(String item) {
+        this.list.add(item);
+        numItems++;
+        System.out.println("added: " + item);
+    }
+
+    private void printItems() {
+        for(int i = 0; i < this.numItems; i++) {
+            System.out.println(String.valueOf(i + 1) + ". " + list.get(i));
+        }
+    }
+
     public static void main(String[] args) {
+        Duke duke = new Duke();
         String greetings = "Greetings from Ann \n" + "What can I do for you?";
         System.out.println(greetings);
 
@@ -13,8 +36,10 @@ public class Duke {
             if(input.toLowerCase().equals("bye")) {
                 System.out.println("Sad to see you go :( See you again soon!");
                 break;
+            } else if (input.toLowerCase().equals("list")){
+                duke.printItems();
             } else {
-                System.out.println(input);
+                duke.addItem(input);
             }
         }
     }
