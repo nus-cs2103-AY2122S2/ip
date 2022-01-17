01f2bfb0ece53365ae5969bd83499b5275e1ef87
junjunjieOng <e0544320@u.nus.edu>
2022-01-17 12:27:32 +0800

    duke chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greet = "Hello! I'm Duke\n" + "What can I do for you?\n";
+        String goodbye = "Bye. Hope to see you again soon!";
+        System.out.println(greet);
+        while(true){
+            Scanner sc = new Scanner(System.in);
+            String curr_word = sc.next();
+            if (curr_word.equals("bye")) {
+                System.out.println(goodbye);
+                break;
+            } else{
+                System.out.println(curr_word);
+            }
+        }
     }
 }
