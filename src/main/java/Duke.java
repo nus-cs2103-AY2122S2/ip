4d23605f22e5bdfb94e7978747a81f864f7eaf8f
HappyPear <ongweixin@hotmail.com>
2022-01-17 20:38:14 +0800

    Printer.print uses varargs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,13 +9,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Printer p = new Printer();
+        p.print("Hello! I'm Duke," , "What can I do for you?");
+
+
     }
+
+
+
 }
 
 class Printer {
-    public void print(String ss) {
+    public void print(String ... args) {
         System.out.println(String.format("    %s", "=========================================="));
-        System.out.println(String.format("    %s", ss));
+        Arrays.asList(args).forEach((x) -> { System.out.println(String.format("    %s", x.toString())); } );
         System.out.println(String.format("    %s", "=========================================="));
     }
 }
