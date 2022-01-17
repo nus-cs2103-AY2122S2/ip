d7d23566af0c1c07f40a507ecc9d387bcfa72c39
sharpstorm <chimchar17@gmail.com>
2022-01-17 17:20:38 +0800

    Add User Input Functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,47 @@
+import java.util.Scanner;
+import java.util.function.Consumer;
+
 public class Duke {
-    private static final String SEPARATOR = "------------------------------------";
+    private static final String SEPARATOR = "\t------------------------------------";
     private static final String BOT_NAME = "Megumin";
 
     public static void main(String[] args) {
         greet();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = readInput(sc);
+            processCommand(command);
+            System.out.println();
+        }
     }
 
     private static void greet() {
+        printBlock((linePrinter) -> {
+            linePrinter.accept("Hi! I'm " + BOT_NAME);
+            linePrinter.accept("What do you need?");
+        });
+        System.out.println();
+    }
+
+    private static String readInput(Scanner sc) {
+        System.out.println("Enter a Command:");
+        String line = sc.nextLine();
+
+        return line;
+    }
+
+    private static void processCommand(String command) {
+        printBlock((linePrinter) -> {
+            linePrinter.accept("\t" + command);
+        });
+    }
+
+    private static void printBlock(Consumer<Consumer<String>> action) {
         System.out.println(SEPARATOR);
-        System.out.println("Hi! I'm " + BOT_NAME);
-        System.out.println("What do you need?");
+        action.accept((line) -> {
+            System.out.println("\t" + line);
+        });
         System.out.println(SEPARATOR);
     }
 }
