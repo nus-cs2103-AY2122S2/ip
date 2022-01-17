7c3b1ea3de7ce65e3f69d4f1c7ad535b204985c3
Joshua <e0544333@u.nus.edu>
2022-01-18 00:43:03 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String bot = "Hello! I'm Duke\nWhat can I do for you?", response = "";
+
+        System.out.println(appendLines(bot));
+
+        while(response.compareTo("bye") != 0) {
+            response = reader.readLine();
+            if (response.compareTo("bye") == 0) {
+                System.out.println(appendLines("Bye. Hope to see you again soon!"));
+                break;
+            }
+            System.out.println(appendLines(response));
+        }
+    }
+
+    public static String appendLines(String s) {
+        String line = "____________________________________________________________";
+        return line + "\n" + s + "\n" + line;
     }
 }
