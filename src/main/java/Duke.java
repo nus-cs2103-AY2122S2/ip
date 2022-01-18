91ab3f73b0baa789dde496b4224dceedf3cfa756
xzzz3 <xingzheng@u.nus.edu>
2022-01-18 22:49:03 +0800

    Cleaning up code with helper functions and switch statements.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static final ArrayList<String> toDoList = new ArrayList<String>(100);
+    private static final ArrayList<Task> toDoList = new ArrayList<Task>(100);
 
     public static void main(String[] args) {
         String logo = "__  __ ____________\n" +
@@ -13,34 +13,44 @@ public class Duke {
         System.out.println("Hello uwu! I'm xzzz,");
         System.out.println("You can check your schedwle here (ɔ◔‿◔)ɔ ♥!");
 
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            String command = sc.nextLine();
-            if (command.equals("bye")) {
-                break;
-            }
-
-            respond(command);
-        }
+        respond();
 
         System.out.println("Cya later~ ≧◉◡◉≦");
     }
 
-    private static void respond(String command) {
-        if (command.equals("list")) {
-            displayList();
-        } else if (command.equals("blah")) {
-            System.out.println("blah");
-        } else {
-            toDoList.add(command);
-            System.out.println("okie!! (✿◠‿◠)  i have added: " + command);
+    private static void respond() {
+        Scanner sc = new Scanner(System.in);
+        label:
+        while (sc.hasNext()) {
+            String command = sc.nextLine();
+            switch (command) {
+                case "bye":
+                    break label;
+                case "list":
+                    displayList();
+                    break;
+                case "blah":
+                    System.out.println("blah");
+                    break;
+                case "mark":
+                    mark(sc.nextInt());
+                    break;
+                default:
+                    toDoList.add(new Task(command));
+                    System.out.println("okie!! (✿◠‿◠)  i have added: " + command);
+                    break;
+            }
         }
     }
 
     private static void displayList() {
         int number = 1;
-        for (String item : toDoList) {
-            System.out.println(number + ". " + item);
+        for (Task item : toDoList) {
+            System.out.println(number + ". " + item.toString());
         }
+    }
+
+    private static void mark(int idx) {
+        toDoList.set(idx - 1, toDoList.get(idx - 1).mark()); // minus 1 to offset the indexing
     }
 }
