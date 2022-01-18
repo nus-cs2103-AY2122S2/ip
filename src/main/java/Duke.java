1e46ecd652df7b9fac82882f61130d331ef4ae3e
kwanhw <kwanhaowei@gmail.com>
2022-01-18 21:20:44 +0800

    Refactored input processing and initialisation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,20 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+    public static final String BYE = "bye";
+    public static final String LIST = "list";
+    public static final String MARK = "mark";
+    public static final String UNMARK = "unmark";
+    public static final String MAKE_TODO = "todo";
+    public static final String MAKE_EVENT = "event";
+    public static final String MAKE_DEADLINE = "deadline";
+
+    private static Task[] tasks;
+    private static int index;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int index = 0;
+        init();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -19,116 +28,136 @@ public class Duke {
         System.out.println("____________________________________________________________");
 
         String inputTxt;
-        boolean canContinue = true;
-        while (canContinue) {
+        while (input.hasNext()) {
             inputTxt = input.nextLine();
-            String[] split = inputTxt.split(" ");
+            processInput(inputTxt);
 
-            int toDelete;
-            Task task;
-            switch (split[0]) {
-                case "bye":
-                    inputTxt = "Bye. Hope to see you again soon!";
-                    canContinue = false;
+            if (inputTxt.startsWith("bye")) {
+                break;
+            }
+        }
+    }
+
+    private static void init() {
+        index = 0;
+        tasks = new Task[100];
+    }
+
+    public static void processInput(String inputTxt) {
+        String[] split = inputTxt.split(" ");
+        String command = split[0].toLowerCase();
+        String commandArgs = inputTxt.substring(command.length()).trim();
+        int toDelete;
+        Task task;
+        try {
+            switch (command) {
+                case BYE:
+                    printMessage("Bye. Hope to see you again soon!");
                     break;
 
-                case "list":
-                    inputTxt = listTasks(tasks,index);
+                case LIST:
+                    printMessage(listTasks(tasks, index));
                     break;
 
-                case "mark":
-                    toDelete = Integer.valueOf(split[1]) - 1;
+                case MARK:
+                    if (index == 0) {
+                        throw new DukeException("☹ OOPS!!! Please make sure you have something in the list before marking a task as done!");
+                    }
+
+                    if (split.length == 1) {
+                        throw new DukeException("☹ OOPS!!! Please make sure you did specify which item I should be marking as done.");
+                    }
+                    toDelete = Integer.valueOf(commandArgs) - 1;
                     task = tasks[toDelete];
                     task.markAsDone();
-                    inputTxt = String.format("Nice! I marked this task as done:\n %s", task);
+                    printMessage( String.format("Nice! I marked this task as done:\n %s", task));
                     break;
 
-                case "deadline":
-                case "event":
-                case "todo":
-                    String[] taskDetails = processTask(split);
-                    task = buildTask(split[0],taskDetails);
-                    tasks[index]  = task;
-                    index++;
-                    inputTxt = String.format("Got it. I've added this task:\n\t %s\n Now you have %d tasks in the list",task,index);
-                    break;
+                case UNMARK:
+                    if (index == 0) {
+                        throw new DukeException("☹ OOPS!!! Please make sure you have something in the list before marking a task as undone!");
+                    }
 
-                case "unmark":
-                    toDelete = Integer.valueOf(split[1]) - 1;
+                    if (split.length == 1) {
+                        throw new DukeException("☹ OOPS!!! Please make sure you did specify which item I should be marking as undone.");
+                    }
+                    toDelete = Integer.valueOf(commandArgs) - 1;
                     task = tasks[toDelete];
                     task.markAsUndone();
-                    inputTxt = String.format("OK, I've marked this task as not done yet:\n %s", task);
+                    printMessage( String.format("OK, I've marked this task as not done yet:\n %s", task));
+                    break;
+
+                case MAKE_DEADLINE:
+                case MAKE_EVENT:
+                case MAKE_TODO:
+                    task = processTask(command, commandArgs);
+                    tasks[index] = task;
+                    index++;
+                    printMessage(String.format("Got it. I've added this task:\n\t %s\n Now you have %d tasks in the list",
+                            task,
+                            index));
                     break;
 
                 default:
-                    tasks[index] = new Task(inputTxt);
-                    index++;
-                    inputTxt = String.format("added: %s",inputTxt);
-                    break;
+                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
-
-            System.out.println("____________________________________________________________");
-            System.out.println(inputTxt);
-            System.out.println("____________________________________________________________");
+        } catch (DukeException e) {
+            printMessage(e.getMessage());
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            printMessage("☹ OOPS!!! I don't think you gave me a valid number.");
+        } catch (NullPointerException e) {
+            printMessage("☹ OOPS!!! I think you may have given me something that's out of range.");
         }
     }
 
     public static String listTasks(Task[] tasks, int toIndex) {
         StringBuilder sb = new StringBuilder("Here are the tasks in your list:");
         for (int i = 0; i < toIndex; i++) {
-            sb.append(String.format("\n%d.%s",i+1,tasks[i]));
-
-//            if (i != toIndex - 1)  {
-//                sb.append("\n");
-//            }
+            sb.append(String.format("\n%d.%s", i + 1, tasks[i]));
         }
 
         return sb.toString();
     }
 
-    public static String[] processTask(String[] input) {
-        int index = 1;
+    public static Task processTask(String command, String args) throws DukeException {
+        if (command.equals(MAKE_TODO)) {
+            if (args.equals("")) {
+                throw new DukeException("☹ OOPS!!! Make sure the task is not empty!");
+            }
+            return new Todo(args);
+        } else {
+            String delimiter = getDelimiter(command);
+            String[] taskParams = args.split(delimiter);
 
-        String delimiter = getDelimiter(input[0]);
-        if (!delimiter.equals("")) {
-            for (int i = 1; i < input.length; i++) {
-                if (input[i].equals(delimiter)) {
-                    index = i;
-                    break;
-                }
+//            Checks for syntax correctness
+            if (taskParams.length == 1) {
+                String errorMsg = String.format("☹ OOPS!!! Make sure your command follows this format: %s <task>%s<time>",command,delimiter);
+                throw new DukeException(errorMsg);
             }
 
-            String description = String.join(" ",Arrays.copyOfRange(input,1,index));
-            String time = String.join(" ",Arrays.copyOfRange(input,index+1,input.length));
 
-            String[] processedTask = {description,time};
-            return processedTask;
-        } else {
-            String[] processedTask = {String.join(" ",Arrays.copyOfRange(input,1,input.length)) };
-           return processedTask;
-        }
-
-    }
-
-    public static String getDelimiter(String action) {
-        switch (action) {
-            case "deadline":
-                return "/by";
-            case "event":
-                return "/at";
-            default:
-                return "";
+            if (command.equals(MAKE_DEADLINE)) {
+                return new Deadline(taskParams[0], taskParams[1]);
+            } else {
+                return new Event(taskParams[0], taskParams[1]);
+            }
         }
     }
 
-    public static Task buildTask(String action, String[] input) {
+    public static String getDelimiter(String action) throws DukeException {
         switch (action) {
-            case "deadline":
-                return new Deadline(input[0],input[1]);
-            case "event":
-                return new Event(input[0],input[1]);
+            case MAKE_DEADLINE:
+                return " /by ";
+            case MAKE_EVENT:
+                return " /at ";
             default:
-                return new Todo(input[0]);
+                throw new DukeException("☹ OOPS!!! I'm sorry, but I don't think there's a delimiter for that..");
         }
+    }
+
+    public static void printMessage(String inputTxt) {
+        System.out.println("____________________________________________________________");
+        System.out.println(inputTxt);
+        System.out.println("____________________________________________________________");
     }
 }
