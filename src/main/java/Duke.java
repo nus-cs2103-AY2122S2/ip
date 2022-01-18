b3e1b06fe71c33f1aef54c857d3d0e495cbe339e
yokester98 <yokeyue.loy@u.nus.edu>
2022-01-18 20:45:48 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    // Attributes
     public static String LINE = "____________________________________________________________";
+    public static ArrayList<String> LIST = new ArrayList<>();
 
     public static void greet() {
         System.out.println(LINE + "\n Hello! I'm Duke\n What can I do for you?\n" + LINE);
     }
 
-    public static void echo(String s) {
-        System.out.println(LINE + "\n " + s + "\n" + LINE);
+    public static void add(String s) {
+        LIST.add(s);
+        System.out.println(LINE + "\n added: " + s + "\n" + LINE);
+    }
+
+    public static void list() {
+        System.out.println(LINE);
+        for (int i = 1; i <= LIST.size(); i++) {
+            System.out.println(String.format(" %d. %s", i, LIST.get(i - 1)));
+        }
+        System.out.println(LINE);
     }
 
     public static void exit() {
@@ -18,13 +30,15 @@ public class Duke {
     public static void main(String[] args) {
         greet();
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            String usrInput = sc.next();
+        while (sc.hasNextLine()) {
+            String usrInput = sc.nextLine();
             if (usrInput.equals("bye")) {
                 exit();
                 break;
+            } else if (usrInput.equals("list")) {
+                list();
             } else {
-                echo(usrInput);
+                add(usrInput);
             }
         }
     }
