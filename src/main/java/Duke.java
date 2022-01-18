e25c2ea05585f294948be2b94ef58c877a5f240a
domlimm <dominiclimkj3448@gmail.com>
2022-01-18 12:28:26 +0800

    - Add exception handling for all possible common cases - Add Duke's exception class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,6 +11,14 @@ public class Duke {
             "                        __/ |\n" +
             "                       |___/ ";
     private static final String BOT_NAME = "Abby";
+    private static final String UNKNOWN_MSG =
+            "OOPS!!! I'm sorry, but I don't know what that means :-(";
+    private static final String INVALID_INDEX_MSG =
+            "OOPS!!! I can't find that in your task list :-(";
+    private static final String BROKEN_MSG =
+            "YOU BROKE ME :-(";
+    private static final String INVALID_MARK_MSG =
+            "OOPS!!! I need a number to find that task :-(";
 
     private ArrayList<Task> tasks;
     
@@ -31,29 +39,65 @@ public class Duke {
     }
 
     public void add(String task) {
-        String[] taskArr = task.split(" ", 2);
-        String type = taskArr[0];
+        String[] taskArr = null;
+        String type = "";
         String result = "Got it. I've added this task:\n";
 
-        if (type.equalsIgnoreCase("deadline")) {
-            String[] taskData = taskArr[1].split(" /by ");
+        try {
+            taskArr = task.split(" ", 2);
+            type = taskArr[0];
 
-            this.tasks.add(new Deadline(taskData[0], taskData[1]));
-        } else if (type.equalsIgnoreCase("event")) {
-            String[] taskData = taskArr[1].split(" /at ");
+            if (type.equalsIgnoreCase("deadline")) {
+                String[] taskData = taskArr[1].split(" /by ");
 
-            this.tasks.add(new Event(taskData[0], taskData[1]));
-        } else {
-            this.tasks.add(new Todo(taskArr[1]));
+                this.tasks.add(new Deadline(taskData[0], taskData[1]));
+            } else if (type.equalsIgnoreCase("event")) {
+                String[] taskData = taskArr[1].split(" /at ");
+
+                this.tasks.add(new Event(taskData[0], taskData[1]));
+            } else if (type.equalsIgnoreCase("todo")) {
+                this.tasks.add(new Todo(taskArr[1]));
+            } else {
+                throw new IndexOutOfBoundsException();
+            }
+
+            int noOfTasks = this.tasks.size();
+            String pluralTask = noOfTasks > 1 ? "tasks" : "task";
+
+            result += "  " + this.tasks.get(noOfTasks - 1).toString() + "\n";
+            result += "Now you have " + noOfTasks + " " + pluralTask + " in the list.";
+
+            output(result);
+        } catch (IndexOutOfBoundsException e) {
+            if (isValidType(type)) {
+                if (isMissingData(taskArr)) {
+                    output("OOPS!!! Some data of your " + type + " task is missing. :-(");
+                    return;
+                }
+
+                output("OOPS!!! The description of a " + type + " cannot be empty. :-(");
+            } else {
+                output(UNKNOWN_MSG);
+            }
+        }
+    }
+
+    public boolean isValidType(String type) {
+        if (type.equalsIgnoreCase("todo") ||
+                type.equalsIgnoreCase("event") ||
+                type.equalsIgnoreCase("deadline")) {
+            return true;
         }
 
-        int noOfTasks = this.tasks.size();
-        String pluralTask = noOfTasks > 1 ? "tasks" : "task";
+        return false;
+    }
 
-        result += "  " + this.tasks.get(noOfTasks - 1).toString() + "\n";
-        result += "Now you have " + noOfTasks + " " + pluralTask + " in the list.";
+    public boolean isMissingData(String[] taskArr) {
+        if (taskArr.length < 3 && !taskArr[0].equalsIgnoreCase("todo")) {
+            return true;
+        }
 
-        output(result);
+        return false;
     }
 
     public void list() {
@@ -79,14 +123,18 @@ public class Duke {
     }
 
     public void toggleCompleted(boolean isMark, int index) {
-        this.tasks.get(--index).setCompleted(isMark);
+        try {
+            this.tasks.get(--index).setCompleted(isMark);
 
-        String output = isMark ?
-                "Nice! I've marked this task as done:\n" :
-                "OK, I've marked this task as not done yet:\n";
-        String task = "  " + this.tasks.get(index).toString();
+            String output = isMark ?
+                    "Nice! I've marked this task as done:\n" :
+                    "OK, I've marked this task as not done yet:\n";
+            String task = "  " + this.tasks.get(index).toString();
 
-        output(output + task);
+            output(output + task);
+        } catch (IndexOutOfBoundsException e) {
+            output(INVALID_INDEX_MSG);
+        }
     }
 
     public void start() {
@@ -96,38 +144,49 @@ public class Duke {
         output(BOT_ART + "\nHello! I'm " + BOT_NAME + "\nWhat can I do for you?\n");
             
         while (true) {
-            input = new Input(sc.nextLine());
+            try {
+                input = new Input(sc.nextLine());
 
-            switch (input.getStates()) {
-                case ECHO:
-                    output(input.getInput());
+                switch (input.getStates()) {
+                    case ECHO:
+                        output(input.getInput());
 
+                        break;
+                    case BYE:
+                        output(bye());
+
+                        break;
+                    case ADD:
+                        add(input.getInput());
+
+                        break;
+                    case LIST:
+                        list();
+
+                        break;
+                    case TOGGLE:
+                        boolean isMark =
+                                input.getInput().split(" ")[0].equalsIgnoreCase("mark");
+
+                        if (input.getArgs().length() == 0) {
+                            output(INVALID_MARK_MSG);
+                            continue;
+                        }
+
+                        toggleCompleted(isMark, Integer.parseInt(input.getArgs()));
+
+                        break;
+                    default:
+                        throw new DukeException(UNKNOWN_MSG);
+                }
+
+                if (input.getStates() == Input.States.BYE) {
                     break;
-                case BYE:
-                    output(bye());
-
-                    break;
-                case ADD:
-                    add(input.getInput());
-
-                    break;
-                case LIST:
-                    list();
-
-                    break;
-                case TOGGLE:
-                    boolean isMark =
-                            input.getInput().split(" ")[0].equalsIgnoreCase("mark");
-
-                    toggleCompleted(isMark, Integer.parseInt(input.getArgs()));
-
-                    break;
-                default:
-                    break;
-            }
-
-            if (input.getStates() == Input.States.BYE) {
-                break;
+                }
+            } catch (DukeException e) {
+                output(UNKNOWN_MSG);
+            } catch (IllegalArgumentException e) {
+                output(BROKEN_MSG + " " + e.getMessage());
             }
         }
 
