5e17c225c6c2439c5d3873b0ce1a749a2c7a2ee4
junjieteoh <45454752+junjieteoh@users.noreply.github.com>
2022-01-18 20:47:38 +0800

    Updated data structure of arrayOfList to a Task array

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,23 +1,27 @@
 import java.util.*;
 
 public class Task {
-    private String name;
-    private static String[] arrayOfTask = new String[100];
+    protected String name;
+    protected boolean isDone;
+    protected int id;
+
+    private static Task[] arrayOfTask = new Task[100];
     private static int counter = 0;
 
     Task(String name) {
         this.name = name;
-        arrayOfTask[counter] = name;
+        this.id = counter + 1;
+        arrayOfTask[counter] = this;
         counter++;
     }
 
     public static String printArray() {
         int arrayLength = arrayOfTask.length;
-        String output = "";
+        String output = "       This is the list of all tasks :D \n";
 
         for(int i = 0; i < arrayLength; i++) {
             if(arrayOfTask[i] != null) {
-                output += "       " + Integer.toString(i+1) + ". " + arrayOfTask[i];
+                output += "       " + arrayOfTask[i];
                 if(i != arrayLength - 1) {
                     output += "\n";
                 }
@@ -26,6 +30,10 @@ public class Task {
         }
 
         return output;
+    }
+
+    public String toString() {
+        return Integer.toString(this.id) + ". " + arrayOfTask[this.id - 1].name;
     }
 
 }
