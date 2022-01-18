39361e59a85513dee580c8a24c9e533a9f8b682e
Daniel Lee <minqiang8194@gmail.com>
2022-01-18 15:05:15 +0800

    Added marking and unmarking functionality to Tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,26 +2,36 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    private static final String COMMAND_BYE = "bye";
+    private static final String COMMAND_LIST = "list";
+    private static final String COMMAND_MARK = "mark";
+    private static final String COMMAND_UNMARK = "unmark";
+
     public static void main(String[] args) {
         printContent("Hello! I'm Duke\n     What can I do for you?");
         ArrayList<Task> tasks = new ArrayList<Task>();
         Scanner sc = new Scanner(System.in);
         while (true){
-            String query = sc.nextLine();
-            if (query.compareTo("bye") == 0){
+            String line = sc.nextLine();
+            String[] splitted = line.split("\\s+");
+            String query = splitted[0];
+            if (query.compareTo(COMMAND_BYE) == 0){
                 printContent("Bye. Hope to see you again soon!");
                 break;
-            } else if (query.compareTo("list") == 0){
-                String list = "";
-                for (int i = 0; i < tasks.size(); i++){
-                    list += (i+1) + ". " + tasks.get(i).getTitle();
-                    if (i != tasks.size()-1)
-                        list += "\n     ";
-                }
-                printContent(list);
+            } else if (query.compareTo(COMMAND_LIST) == 0){
+                processList(tasks);
+            } else if (query.compareTo(COMMAND_MARK) == 0){
+                Task thisTask = tasks.get(Integer.parseInt(splitted[1])-1);
+                thisTask.markAsDone();
+                printContent("Nice! I've marked this task as done:\n       [" + thisTask.getStatusIcon() + "] " + thisTask.getTitle());
+            } else if (query.compareTo(COMMAND_UNMARK) == 0){
+                Task thisTask = tasks.get(Integer.parseInt(splitted[1])-1);
+                thisTask.markAsUndone();
+                printContent("OK, I've marked this task as nto done yet:\n       [" + thisTask.getStatusIcon() + "] " + thisTask.getTitle());
             } else{
-                printContent("added: " + query);
-                tasks.add(new Task(query));
+                printContent("added: " + line);
+                tasks.add(new Task(line));
             }
         }
         sc.close();
@@ -37,5 +47,15 @@ public class Duke {
         System.out.println(spacing + text);
         printLine();
         System.out.println();
+    }
+
+    public static void processList(ArrayList<Task> tasks){
+        String list = "Here are the tasks in your list:\n     ";
+        for (int i = 0; i < tasks.size(); i++){
+            list += (i+1) + ". " + "[" + tasks.get(i).getStatusIcon() + "] " + tasks.get(i).getTitle();
+            if (i != tasks.size()-1)
+                list += "\n     ";
+        }
+        printContent(list);
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,30 @@
 public class Task {
-    private String title;
+    protected String title;
+    protected Boolean isDone;
 
     public Task(String title){
         this.title = title;
+        this.isDone = false;
+    }
+    
+    public Task(String title, Boolean isDone){
+        this.title = title;
+        this.isDone = isDone;
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
+    }
+
+    public void markAsUndone(){
+        this.isDone = false;
     }
 
     public String getTitle(){
         return this.title;
+    }
+
+    public String getStatusIcon(){
+        return (isDone ? "X" : " ");
     }
 }
