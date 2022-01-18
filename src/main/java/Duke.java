0f9bfc99381ebb0045ee8eaf9b879e4fd0db6440
Joshua <e0544333@u.nus.edu>
2022-01-18 18:20:47 +0800

    ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,50 +8,101 @@ public class Duke {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String bot = "Hello! I'm Duke\nWhat can I do for you?", response = "";
-        ArrayList<Task> ListResponse = new ArrayList<Task>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
-        System.out.println(appendLines(bot));
-
-        int digit = 0;
+        printLines();
+        System.out.println(bot);
+        printLines();
 
         while(response.compareTo("bye") != 0) {
             response = reader.readLine();
             if (response.compareTo("bye") == 0) {
-                System.out.println(appendLines("Bye. Hope to see you again soon!"));
+                printLines();
+                System.out.println("Bye. Hope to see you again soon!");
+                printLines();
                 break;
             } else if (response.compareTo("list") == 0) {
-                System.out.println("Here are the tasks in your list:\n");
-                for(int i = 0; i < ListResponse.size(); i++) {
-                    System.out.println(i + 1 + ". " + ListResponse.get(i).toString());
-                }
-            } else if (response.contains("mark")) {
+                printLines();
+                System.out.println("Here are the tasks in your list:");
+                listAllTasks(tasks);
+                printLines();
+//                Handle Mark command
+            } else {
+                printLines();
                 String[] split = response.split("\\s+");
-                digit = Integer.parseInt(split[1]);
-                if (digit > 0 && digit <= ListResponse.size()) {
-                    switch (split[0]) {
-                        case "mark":
-                            System.out.println("Nice! I've marked this task as done:\n");
-                            break;
-                        case "unmark":
-                            System.out.println("OK, I've marked this task as not done yet:\n");
-                            break;
+                if (split[0].contains("mark")) {
+                    int digit = Integer.parseInt(split[1]);
+                    if (digit > 0 && digit <= tasks.size()) {
+                        handleResponse(split[0]);
+                        Task t = tasks.get(digit - 1);
+                        t.unmark();
+                        System.out.println(t.toString());
+                    } else {
+                        System.out.println("Invalid range! Try again.");
                     }
-                    Task t = ListResponse.get(digit - 1);
-                    t.unmark();
-                    System.out.println(t.toString());
                 } else {
-                    System.out.println("Invalid range! Try again.\n");
+                    handleResponse(split[0]);
+                    Task newTask = null;
+                    boolean valid = true;
+                    switch (split[0]) {
+                        case "todo":
+                            newTask = new Todo(removeSubString(response, "todo "));
+                            break;
+                        case "deadline":
+                            String[] deadlineSplit = response.split(" /by ");
+                            newTask = new Deadline(deadlineSplit[0].replace("deadline ", ""), deadlineSplit[1]);
+                            break;
+                        case "event":
+                            String[] eventSplit = response.split(" /at ");
+                            newTask = new Event(eventSplit[0].replace("event ", ""), eventSplit[1]);
+                            break;
+                        default:
+                            valid = false;
+                    }
+                    if (valid == true) {
+                        tasks.add(newTask);
+                        System.out.println(newTask.toString());
+                        System.out.println("Now you have " + tasks.size() +  " tasks in the list.");
+                    } else {
+                        System.out.println("Invalid command! Please try again.");
+                    }
                 }
-            }
-            else {
-                System.out.println(appendLines("added: " + response));
-                ListResponse.add(new Task(response));
+                printLines();
             }
         }
     }
 
-    public static String appendLines(String s) {
-        String line = "____________________________________________________________";
-        return line + "\n" + s + "\n" + line;
+// ____________________________________________________________
+//                  START OF HELPER METHODS
+// ____________________________________________________________
+    public static void printLines() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void listAllTasks(ArrayList<Task> tasks) {
+        for(int i = 0; i < tasks.size(); i++) {
+            System.out.println(i + 1 + ". " + tasks.get(i).toString());
+        }
+    }
+
+    public static String removeSubString(String response, String word) {
+        return response.replace(word, "");
+    }
+
+// ____________________________________________________________
+//                  START OF COMMAND HANDLER
+// ____________________________________________________________
+    public static void handleResponse(String res) {
+        switch (res) {
+            case "mark":
+                System.out.println("Nice! I've marked this task as done:");
+                break;
+            case "unmark":
+                System.out.println("OK, I've marked this task as not done yet:");
+                break;
+            case "todo":
+                System.out.println("Got it. I've added this task:");
+        }
     }
 }
+
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -15,6 +15,7 @@ public class Task {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + this.description;
     }
