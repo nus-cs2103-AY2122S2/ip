20968ea710939be79a9c28c3648a6515917dc615
Wong Chee Hong <c.h.wong2606@gmail.com>
2022-01-19 00:32:31 +0800

    Implement `list` and `add`
    as per Duke Level 2 specifications.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,36 +5,27 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         boolean isPolling = true;
-        System.out.println(chatBox(greetingMessage()));
+        DukeEngine dukeEngine = new DukeEngine();
+
+        System.out.println(chatBox(dukeEngine.greetingMessage()));
 
         while(isPolling) {
             String command = sc.nextLine();
-            String replyMessage;
+            String replyMessage = "";
             if (command.equals("bye")) {
                 isPolling = false;
-                replyMessage = byeMessage();
+                replyMessage = dukeEngine.byeMessage();
+
+            } else if (command.equals("list")){
+                replyMessage = dukeEngine.listItems();
             } else {
-                replyMessage = echoCommand(command);
+                replyMessage = dukeEngine.addText(command);
             }
 
             System.out.println(chatBox(replyMessage));
         }
-    }
 
-    public static String greetingMessage() {
-        StringBuilder greeting = new StringBuilder();
-        greeting.append("Wow! Hello! I'm Duke.\n");
-        greeting.append("What can I do for you?\n");
-
-        return greeting.toString();
-    }
-
-    public static String echoCommand(String msg) {
-        return msg + "\n";
-    }
-
-    public static String byeMessage() {
-        return "Bye. Hope to see you again soon!\n";
+        sc.close();
     }
 
     //wraps a given text in a box to be printed
@@ -42,9 +33,12 @@ public class Duke {
         StringBuilder box = new StringBuilder();
         box.append("----------------------------------------\n");
         box.append(text);
+
+        //appends a newline if provided text does not have one
+        if (text.charAt(text.length() - 1) != '\n') box.append("\n");
+
         box.append("----------------------------------------\n");
 
         return box.toString();
     }
-
 }
---
+++ src/main/java/DukeEngine.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class DukeEngine {
+    
+    ArrayList<String> itemList = new ArrayList<>();
+
+    public String addText(String text) {
+        itemList.add(text);
+        return "added: " + text;
+    }
+
+    public String listItems() {
+        StringBuilder sb = new StringBuilder();
+        if (itemList.isEmpty()) {
+            sb.append("There is nothing in the list!");
+        }
+        
+        for (int i = 1; i <= itemList.size(); i++) {
+            sb.append(i + ". ").append(itemList.get(i - 1)).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String greetingMessage() {
+        StringBuilder greeting = new StringBuilder();
+        greeting.append("Wow! Hello! I'm Duke.\n");
+        greeting.append("What can I do for you?");
+
+        return greeting.toString();
+    }
+
+    public String echoCommand(String msg) {
+        return msg;
+    }
+
+    public String byeMessage() {
+        return "Bye. Hope to see you again soon!";
+    }
+
+}
