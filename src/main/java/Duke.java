03f27e1548f53cfde71ccc0f74f7dc63259325de
Daffa Fathani Adila <daffa26@gmail.com>
2022-01-18 10:52:49 +0800

    implemented duke basic commands, greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+    private static final String INDENT = "    ";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        Duke.greet();
+
+        while (true) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                Duke.bye();
+                break;
+            } else {
+                Duke.echo(input);
+            }
+        }
+    }
+
+    public static void greet() {
+        System.out.println(Duke.INDENT + Duke.HORIZONTAL_LINE);
+        System.out.println(Duke.INDENT + "Hello there, I am Giga Chad Duke");
+        System.out.println(Duke.INDENT + "How can I help you?");
+        System.out.println(Duke.INDENT + Duke.HORIZONTAL_LINE);
+    }
+
+    public static void bye() {
+        System.out.println(Duke.INDENT + Duke.HORIZONTAL_LINE);
+        System.out.println(Duke.INDENT + "Bye, hope to see you again soon!");
+        System.out.println(Duke.INDENT + Duke.HORIZONTAL_LINE);
+    }
+
+    public static void echo(String args) {
+        System.out.println(Duke.INDENT + Duke.HORIZONTAL_LINE);
+        System.out.println(Duke.INDENT + args);
+        System.out.println(Duke.INDENT + Duke.HORIZONTAL_LINE);
     }
 }
