0db4323f0ed1b2746ad8ba2e121da06cd9bdba58
Rahul Prasad <e0638893@u.nus.edu>
2022-01-19 01:32:17 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,26 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        Scanner sc= new Scanner(System.in);
+        String str = sc.next();
+
+        while(!str.equals("bye")) {
+            System.out.println(str); 
+            str = sc.next();
+            
+        }
+        
+            
+            System.out.println("Bye. Hope to see you again soon!");
+            sc.close();
+            
+        
+    }
+}
     }
 }
