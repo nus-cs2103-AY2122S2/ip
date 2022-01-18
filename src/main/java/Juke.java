302ce61d47b6ddca56680c1a0782fd92b5ca754c
Spyobird <elliotlimzy@gmail.com>
2022-01-19 00:01:14 +0800

    Juke.java: add functionality for add, display task list

--- src/main/java/Juke.java
+++ src/main/java/Juke.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Juke {
     private static final Juke INSTANCE = new Juke();
     
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        INSTANCE.run(input);
-        
-        input.close();
+    private String[] taskList;
+    private final int taskListSize = 100;
+    private int index;
+    
+    public Juke() {
+        this.taskList = new String[this.taskListSize];
+        this.index = 0;
     }
     
     private void run(Scanner input) {
@@ -20,18 +22,28 @@ public class Juke {
                 case "bye":
                     this.bye();
                     break;
+                case "list":
+                    this.displayTaskList();
+                    break;
                 default:
-                    this.echo(cmd);
+                    this.addToTaskList(cmd);
             }
         }
     }
     
     private void greet() {
-        String logo = " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "                                                             _____   \n" +
+            "          _____  ______   _____     ______   _______    _____\\    \\  \n" +
+            "         |\\    \\_\\     \\  \\    \\   |\\     \\  \\      \\  /    / |    | \n" +
+            "         \\ \\     \\\\    |  |    |    \\\\     \\  |     /|/    /  /___/| \n" +
+            "          \\|      ||   |  |    |     \\|     |/     //|    |__ |___|/ \n" +
+            "           |      ||    \\_/   /|      |     |_____// |       \\       \n" +
+            "   ______  |      ||\\         \\|      |     |\\     \\ |     __/ __    \n" +
+            "  /     / /      /|| \\         \\__   /     /|\\|     ||\\    \\  /  \\   \n" +
+            " |      |/______/ | \\ \\_____/\\    \\ /_____/ |/_____/|| \\____\\/    |  \n" +
+            " |\\_____\\      | /   \\ |    |/___/||     | / |    | || |    |____/|  \n" +
+            " | |     |_____|/     \\|____|   | ||_____|/  |____|/  \\|____|   | |  \n" +
+            "  \\|_____|                  |___|/                          |___|/   \n";
         System.out.println("Hello from\n" + logo);
     }
     
@@ -44,11 +56,42 @@ public class Juke {
         this.formattedPrint(text);
     }
     
+    private void addToTaskList(String text) {
+        if (index < this.taskListSize && index >= 0) {
+            this.taskList[index] = text;
+            this.formattedPrint("Task added: " + text);
+            index++;
+            return;
+        }
+        this.formattedPrint("Task list is full, task could not be added.");
+    }
+    
+    private void displayTaskList() {
+        if (index > 0) {
+            String str = "";
+            for (int i = 0; i < index; i++) {
+                str += (i + 1) + ". " + this.taskList[i] + "\n";
+            }
+            this.formattedPrint(str.stripTrailing());
+            return;
+        }
+        this.formattedPrint("Task list is empty.");
+    }
+    
     private void formattedPrint(String text) {
-        System.out.println(text + "\n");
+        System.out.println("____________________________________________________________");
+        System.out.println(text);
+        System.out.println("____________________________________________________________\n");
     }
     
     private void printMarker() {
-        System.out.print("> ");
+        System.out.print("\u232c ");
+    }
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        INSTANCE.run(input);
+        
+        input.close();
     }
 }
