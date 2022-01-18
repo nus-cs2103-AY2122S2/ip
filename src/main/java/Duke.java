8cb03b08e98a76facde419070913ac0f610e9b2e
Tan Kel Zin <kelzzin2@gmail.com>
2022-01-18 16:24:40 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,53 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Duke {
+
+    private final static Scanner ioIn = new Scanner(System.in);
+    private final static PrintStream ioOut = System.out;
+
+    private static void printLine() {
+        String line = "\n~._.~\"~._.~\"~._.~\"~._.~\"~._.~\"~._.~\"" +
+                "~._.~\"~._.~\"~._.~\"~._.~\"~._.~\"~._.~\"~._.~\n";
+        ioOut.println(line);
+    }
+
+    private static void printMenu() {
+        String art = "                      .-.\n" +
+                "         heehee      /aa \\_\n" +
+                "                   __\\-  / )                 .-.\n" +
+                "         .-.      (__/    /        haha    _/oo \\\n" +
+                "       _/ ..\\       /     \\               ( \\v  /__\n" +
+                "      ( \\  u/__    /       \\__             \\/   ___)\n" +
+                "       \\    \\__)   \\_.-._._   )  .-.       /     \\\n" +
+                "       /     \\             `-`  / ee\\_    /       \\_\n" +
+                "    __/       \\               __\\  o/ )   \\_.-.__   )\n" +
+                "   (   _._.-._/     hoho     (___   \\/           '-'\n" +
+                "jgs '-'                        /     \\\n" +
+                "                             _/       \\    teehee\n" +
+                "                            (   __.-._/\n";
+        String greet = "Heee hooo I'm Tsohg! How can I help you?";
+        ioOut.println(art);
+        ioOut.println(greet);
+    }
+
+    private static boolean takeInput() {
+        printLine();
+        String input = ioIn.nextLine();
+        printLine();
+        if (input.equals("bye")) {
+            ioOut.println("Seeeee youuuu sooon...");
+            return false;
+        }
+        ioOut.println(input);
+        return true;
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printMenu();
+        while (true) {
+            if (!takeInput()) break;
+        }
+        printLine();
     }
 }
