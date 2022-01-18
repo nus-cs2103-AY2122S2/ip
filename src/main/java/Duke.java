e33a33baccf4e911853aae2ba038c91509ae7a96
kaiyichen <chenkaiyi01@gmail.com>
2022-01-18 16:09:53 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        System.out.println(separation + greeting + separation);
+        System.out.println(separation +list + separation);
+        System.out.println(separation +blah+ separation);
+        System.out.println(separation +bye + separation);
+
     }
+
+    public static String greeting = "Hello! I'm Duke\nWhat can I do for you?";
+    public static String list = "list";
+    public static String blah = "blah";
+    public static String bye = "Bye, Hope to see you again soon!";
+    public static String separation = "\n******************************\n";
 }
