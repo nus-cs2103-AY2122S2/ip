8ea9850eacf5a774458af737557cebfe6de39dbe
xzzz3 <xingzheng@u.nus.edu>
2022-01-18 22:22:18 +0800

    Initialized for Level-1, added basic customization and responses.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,46 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+    private static final ArrayList<String> toDoList = new ArrayList<String>(100);
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "__  __ ____________\n" +
+                "\\ \\/ /|_  /_  /_  /\n" +
+                " >  <  / / / / / / \n" +
+                "/_/\\_\\/___/___/___|";
+        System.out.println(logo);
+        System.out.println("Hello uwu! I'm xzzz,");
+        System.out.println("You can check your schedwle here (ɔ◔‿◔)ɔ ♥!");
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+
+            respond(command);
+        }
+
+        System.out.println("Cya later~ ≧◉◡◉≦");
+    }
+
+    private static void respond(String command) {
+        if (command.equals("list")) {
+            System.out.println("list");;
+        } else if (command.equals("blah")) {
+            System.out.println("blah");
+        } else {
+            toDoList.add(command);
+            System.out.println("added: " + command);
+        }
+    }
+
+    private static void displayList() {
+        int number = 1;
+        for (String item : toDoList) {
+            System.out.println(number + ". " + item);
+        }
     }
 }
