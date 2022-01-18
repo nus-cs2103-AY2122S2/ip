5436caedffb6cdcf2332bdd9affaf7466e9e1a6a
zxgoh <gohzixuan123@gmail.com>
2022-01-18 10:05:57 +0800

    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,3 @@
-import java.net.StandardSocketOptions;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -15,26 +13,39 @@ public class Duke {
                 "     Hello! I'm Duke\n" +
                 "     What can I do for you?\n" +
                 "    ____________________________________________________________");
-        ArrayList<String> newArray = new ArrayList<>();
+        ArrayList<Task> itemList = new ArrayList<>();
         while(true) {
             Scanner sc = new Scanner(System.in);
             String reply = sc.nextLine();
-            if (reply.equals("bye")) {
+            if (reply.split(" ")[0].equals("mark") && reply.split(" ").length == 2) {
+                int index = Integer.valueOf(reply.split(" ")[1]);
+                itemList.get(index - 1).markAsDone();
+                System.out.println("    ____________________________________________________________\n" +
+                        "     Nice! I've marked this task as done: \n" +
+                        "       [X] " + itemList.get(index - 1).description + "\n" +
+                        "    ____________________________________________________________\n");
+        } else if (reply.split(" ")[0].equals("unmark") && reply.split(" ").length == 2) {
+                int index = Integer.valueOf(reply.split(" ")[1]);
+                itemList.get(index - 1).markAsUndone();
+                System.out.println("    ____________________________________________________________\n" +
+                        "     OK, I've marked this task as not done yet: \n" +
+                        "       [ ] " + itemList.get(index - 1).description + "\n" +
+                        "    ____________________________________________________________\n");
+            } else if (reply.equals("bye")) {
                 System.out.println("    ____________________________________________________________\n" +
                         "     Bye. Hope to see you again soon!\n" +
                         "    ____________________________________________________________");
                 break;
             } else if (reply.equals("list")) {
-                String totalString = "    ____________________________________________________________\n";
-                for(int i = 0; i < newArray.size(); i++) {
-                    totalString += "    " + (i + 1) + ": " + newArray.get(i) + "\n";
+                String totalString = "    ____________________________________________________________\n" +
+                        "    Here are the tasks in your list:\n";
+                for(int i = 0; i < itemList.size(); i++) {
+                    totalString += "    " + (i + 1) + ". " + "[" + itemList.get(i).getStatusIcon() + "] " + itemList.get(i).description + "\n";
                 }
                 totalString += "    ____________________________________________________________\n";
                 System.out.println(totalString);
-            }
-
-            else {
-                newArray.add(reply);
+            } else {
+                itemList.add(new Task(reply));
                 System.out.println("    ____________________________________________________________\n" +
                         "     " + "added: " + reply + "\n" +
                         "    ____________________________________________________________");
