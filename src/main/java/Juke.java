67f5a189f4f48c90f11b476a603313cc3ab05023
Spyobird <elliotlimzy@gmail.com>
2022-01-19 02:21:49 +0800

    Juke.java: Add token/delimitting of input; Add mark/unmark for task list

--- src/main/java/Juke.java
+++ src/main/java/Juke.java
@@ -1,14 +1,15 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Juke {
     private static final Juke INSTANCE = new Juke();
     
-    private String[] taskList;
+    private Task[] taskList;
     private final int taskListSize = 100;
     private int index;
     
     public Juke() {
-        this.taskList = new String[this.taskListSize];
+        this.taskList = new Task[this.taskListSize];
         this.index = 0;
     }
     
@@ -16,17 +17,43 @@ public class Juke {
         this.greet();
         while(true) {
             this.printMarker();
-            String cmd = input.nextLine();
             
-            switch(cmd) {
-                case "bye":
-                    this.bye();
-                    break;
-                case "list":
-                    this.displayTaskList();
-                    break;
-                default:
-                    this.addToTaskList(cmd);
+            String[] args = input.nextLine().stripLeading().stripTrailing().split("\\s+");
+            if (args.length > 0) {
+                switch (args[0]) {
+                    case "":
+                        break;
+                    case "bye":
+                        this.bye();
+                        break;
+                    case "list":
+                        this.displayTaskList();
+                        break;
+                    case "mark":
+                        if (args.length > 1) {
+                            try {
+                                this.markTaskAsDone(Integer.parseInt(args[1]));
+                            } catch (NumberFormatException e) {
+                                this.formattedPrint("Invalid argument for mark.");
+                            }
+                        } else {
+                            this.formattedPrint("Please specify an argument for mark.");
+                        }
+                        break;
+                    case "unmark":
+                        if (args.length > 1) {
+                            try {
+                                this.markTaskAsNotDone(Integer.parseInt(args[1]));
+                            } catch (NumberFormatException e) {
+                                this.formattedPrint("Invalid argument for unmark.");
+                            }
+                        } else {
+                            this.formattedPrint("Please specify an argument for unmark.");
+                        }
+                        break;
+                    default:
+                        this.addToTaskList(String.join(" ", args));
+                }
             }
         }
     }
@@ -58,7 +85,7 @@ public class Juke {
     
     private void addToTaskList(String text) {
         if (index < this.taskListSize && index >= 0) {
-            this.taskList[index] = text;
+            this.taskList[index] = new Task(text);
             this.formattedPrint("Task added: " + text);
             index++;
             return;
@@ -70,12 +97,30 @@ public class Juke {
         if (index > 0) {
             String str = "";
             for (int i = 0; i < index; i++) {
-                str += (i + 1) + ". " + this.taskList[i] + "\n";
+                str += (i + 1) + ". " + this.taskList[i].fancyPrint() + "\n";
             }
             this.formattedPrint(str.stripTrailing());
             return;
         }
         this.formattedPrint("Task list is empty.");
+    }
+    
+    private void markTaskAsDone(int arg) {
+        if (arg <= 0 || arg > this.index) {
+            this.formattedPrint("Invalid index for mark.");
+            return;
+        }
+        this.taskList[arg - 1].markAsDone();
+        this.formattedPrint("Marked task \u00ab" + this.taskList[arg - 1].toString() + "\u00bb as done.");
+    }
+    
+    private void markTaskAsNotDone(int arg) {
+        if (arg <= 0 || arg > this.index) {
+            this.formattedPrint("Invalid index for mark.");
+            return;
+        }
+        this.taskList[arg - 1].markAsNotDone();
+        this.formattedPrint("Marked task \u00ab" + this.taskList[arg - 1].toString() + "\u00bb as not done.");
     }
     
     private void formattedPrint(String text) {
