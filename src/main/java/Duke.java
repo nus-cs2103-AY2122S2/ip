1b35d7d2ffa7745863f479c9961595b12efefe75
zxgoh <gohzixuan123@gmail.com>
2022-01-18 08:53:16 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.net.StandardSocketOptions;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -13,18 +15,28 @@ public class Duke {
                 "     Hello! I'm Duke\n" +
                 "     What can I do for you?\n" +
                 "    ____________________________________________________________");
+        ArrayList<String> newArray = new ArrayList<>();
         while(true) {
             Scanner sc = new Scanner(System.in);
-            String reply = sc.next();
+            String reply = sc.nextLine();
             if (reply.equals("bye")) {
                 System.out.println("    ____________________________________________________________\n" +
                         "     Bye. Hope to see you again soon!\n" +
                         "    ____________________________________________________________");
                 break;
+            } else if (reply.equals("list")) {
+                String totalString = "    ____________________________________________________________\n";
+                for(int i = 0; i < newArray.size(); i++) {
+                    totalString += "    " + (i + 1) + ": " + newArray.get(i) + "\n";
+                }
+                totalString += "    ____________________________________________________________\n";
+                System.out.println(totalString);
             }
+
             else {
+                newArray.add(reply);
                 System.out.println("    ____________________________________________________________\n" +
-                        "     " + reply + "\n" +
+                        "     " + "added: " + reply + "\n" +
                         "    ____________________________________________________________");
             }
         }
