b863f0433a094e2ea82d28cdfe75945dd95cc5a6
leeyiheng12 <leeyiheng12@gmail.com>
2022-01-19 00:57:07 +0800

    add functionality to mark task as done or undone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,34 @@ import java.util.*;
 
 public class Duke {
 
+    public static class Task {
+        public boolean done = false;
+        public String name;
+
+        public Task(String taskName) {
+            this.name = taskName;
+        }
+
+        public void setDone(boolean newDone) {
+            this.done = newDone;
+        }
+
+        public boolean isDone() {
+            return this.done;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[%s] %s", this.done ? "X" : " ", this.name);
+        }
+    }
+
     public static String indent = "    ";
     public static String separator = "--------------------------------------------";
     public static String[] openingMessage = new String[] {"Hello! I'm Duke", "What can I do for you?"};
     public static String closingMessage = "Bye. Hope to see you again soon!";
 
-    public static ArrayList<String> allTasks = new ArrayList<>();
+    public static ArrayList<Task> allTasks = new ArrayList<>();
 
     public static void printIndent(String s) {
         System.out.println(indent + s);
@@ -27,15 +49,29 @@ public class Duke {
 
     public static void displayTasks() {
         printIndent(separator);
+        printIndent("Here are the tasks in your list:");
         for (int i = 0; i < allTasks.size(); i++) {
             printIndent(String.format("%d. %s", i+1, allTasks.get(i)));
         }
         printIndent(separator + "\n");
     }
 
-    public static void addTask(String task) {
-        allTasks.add(task);
-        prettyPrint(String.format("added: %s", task));
+    public static void addTask(String taskName) {
+        Task t = new Task(taskName);
+        allTasks.add(t);
+        prettyPrint(String.format("added: %s", taskName));
+    }
+
+    public static void markTask(int taskNum) {
+        Task thisTask = allTasks.get(taskNum);
+        thisTask.setDone(true);
+        prettyPrint(new String[] {"Nice! I've marked this task as done:", thisTask.toString()});
+    }
+
+    public static void unmarkTask(int taskNum) {
+        Task thisTask = allTasks.get(taskNum);
+        thisTask.setDone(false);
+        prettyPrint(new String[] {"Ok, I've marked this task as not done yet:", thisTask.toString()});
     }
 
     public static void main(String[] args) {
@@ -56,17 +92,23 @@ public class Duke {
         while (!finished) {
             userInput = sc.nextLine();
 
-            switch (userInput) {
-                case "bye":
-                    prettyPrint(closingMessage);
-                    finished = true;
-                    break;
-                case "list":
-                    displayTasks();
-                    break;
-                default:
-                    addTask(userInput);
+            if (userInput.equals("bye")) {
+                prettyPrint(closingMessage);
+                finished = true;
+            } else if (userInput.equals("list")) {
+                displayTasks();
+            } else if (userInput.startsWith("mark")) {
+                char lastChar = userInput.charAt(userInput.length() - 1);
+                int taskToMark = Character.getNumericValue(lastChar);
+                markTask(taskToMark - 1);
+            } else if (userInput.startsWith("unmark")) {
+                char lastChar = userInput.charAt(userInput.length() - 1);
+                int taskToUnmark = Character.getNumericValue(lastChar);
+                unmarkTask(taskToUnmark - 1);
+            } else {
+                addTask(userInput);
             }
+
         }
     }
 }
