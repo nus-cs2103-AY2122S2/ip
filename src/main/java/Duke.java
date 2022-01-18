8750e07bf47a9a7f458639244e4bbf16ac1f63fc
thikhinab <thikhinab@gmail.com>
2022-01-18 15:35:20 +0800

    Implemented the skeletal version of Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,61 @@
+import java.util.Scanner;
+
+/**
+ *  Runs a Personal Assistant Chatbot which helps users
+ *  keep track of tasks and other things. The current
+ *  iteration of the Chatbot is named Luca.
+ */
 public class Duke {
+
+   public static final String logo = "888      888     888  .d8888b.         d8888 \n"
+           + "888      888     888 d88P  Y88b       d88888 \n"
+           + "888      888     888 888    888      d88P888 \n"
+           + "888      888     888 888            d88P 888 \n"
+           + "888      888     888 888           d88P  888 \n"
+           + "888      888     888 888    888   d88P   888 \n"
+           + "888      Y88b. .d88P Y88b  d88P  d8888888888 \n"
+           + "88888888  \"Y88888P\"   \"Y8888P\"  d88P     888 \n";
+
+
+    private static final String dividerLine = "___________________"
+            + "_________________________________________\n";
+
+    /**
+     * Formats the string by indenting with 4 spaces
+     * and wrapping the string with divider lines.
+     *
+     * @param input string to be formatted
+     * @return indented string with divider lines.
+     */
+    private static String formatString(String input) {
+        String temp = dividerLine + input + "\n" + dividerLine;
+        String indented = temp.replaceAll("(?m)^", "    ");
+        return indented;
+    }
+
+    /**
+     * Runs the Personal Assistant Chatbot where
+     * the user can input commands.
+     *
+     * @param args initial arguments
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String startPrompt = "Hi! I'm Luca\n"
+                + "How may I help you?\n";
+
+        System.out.println(Duke.formatString("\n" + logo + "\n"
+                + startPrompt));
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        // Exit loop only after the user enters "Bye"
+        while (!input.equals("bye")) {
+            System.out.println(Duke.formatString(input));
+            input = scanner.nextLine();
+        }
+
+        System.out.println(Duke.formatString("Bye. See you again soon!"));
     }
 }
