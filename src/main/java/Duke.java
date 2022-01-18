e6cbc77c729a5630fe215bb73c3201d8aa48da10
Daniel Lee <minqiang8194@gmail.com>
2022-01-18 13:46:51 +0800

    Update Duke with Level 1 requirements

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printContent("Hello! I'm Duke\n     What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            String query = sc.nextLine();
+            if (query.compareTo("bye") == 0){
+                printContent("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                printContent(query);
+            }
+        }
+        sc.close();
+    }
+
+    public static void printLine(){
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void printContent(String text){
+        String spacing = "     ";
+        printLine();
+        System.out.println(spacing + text);
+        printLine();
+        System.out.println();
     }
 }
