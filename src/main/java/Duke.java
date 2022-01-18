2333e31543e05a9c525602abafb8714477526ded
joeietao <77208712+joeietao@users.noreply.github.com>
2022-01-19 03:51:07 +0800

    completed level 1: greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner; //Import the Scanner class
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        //Greetings
+        System.out.println("Greetings! I'm Duke" + "\n" + "What can I do for you today?\n" + logo);
+
+        //create a scanner object
+        Scanner scanned = new Scanner(System.in);
+        System.out.println("");
+
+        //read the user input
+        String input = scanned.nextLine();
+
+        //continue the scanning if user does not say bye, else continue repeating what the user says
+        while (!input.equals("bye")) {
+            System.out.println(input);
+
+            //create a scanner object which contains user input
+            scanned = new Scanner(System.in);
+            System.out.println("");
+
+            //read the new user input
+            input = scanned.nextLine();
+        }
+
+        //goodbye msg
+        System.out.println("Sad to see you leave, come back soon!");
     }
 }
