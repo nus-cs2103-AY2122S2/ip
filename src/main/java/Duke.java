eff5ad04a2ad077e57e1b1242a6f564e71d8b2ff
DannyDakota <51451719+DannyDakota@users.noreply.github.com>
2022-01-18 22:01:48 +0800

    Level-3 added Mark and Unmark feature, Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,30 +4,69 @@ import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        ArrayList<String> objList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
         System.out.println("Hello! I'm Whey\n" + "What can I do for you today?");
         Scanner userInput = new Scanner(System.in);
         try {
             while (true) {
                 String nextLine = userInput.nextLine();
+                int counter = 1;
+                //Exiting the code when bye is inputted
                 if (nextLine.equals("bye")) {
                     System.out.println("  " + "Bye beautiful! hope to see you again hehe");
                     break;
-                } else if (nextLine.equals("list")) {
-                    int counter = 1;
-                    for (String s : objList) {
-                        System.out.println("  " + counter + ". " + s);
+                //display the list of tasks
+                } else if ((nextLine.replaceAll("\\s+", "")).equals("list")) {
+                   System.out.println("Here are the tasks in your list:\n");
+                    for (Task s : taskList) {
+                        System.out.println("  " + counter + "." + s);
                         counter++;
                     }
+                //nothing inputted into system.in
                 } else if (nextLine.isBlank()) {
                     System.out.println("You didn't key in anything!! Feed me with a task!");
+                //unmarking a task
+                } else if (nextLine.startsWith("unmark")) {
+                    int taskNumber = intSearch(nextLine) - 1;
+                    if (taskNumber <= taskList.size()) {
+                        Task intendedTask = taskList.get(taskNumber);
+                        intendedTask.setDone(false);
+                        System.out.println("  " + "Ok! I've marked this task as not done yet:\n"
+                                + "    " + intendedTask);
+                    } else {
+                        System.out.println("Task does not exist! Check again hehe");
+                    }
+                //marking a task
+                } else if (nextLine.startsWith("mark")) {
+                    int taskNumber = intSearch(nextLine) - 1;
+                    if (taskNumber <= taskList.size()) {
+                        Task intendedTask = taskList.get(taskNumber);
+                        intendedTask.setDone(true);
+                        System.out.println("  " + "Nice! I've marked this task as done:\n"
+                                + "    " + intendedTask);
+                    } else {
+                        System.out.println("Task does not exist! Check again hehe");
+                    }
+                //adding a task to the list
                 } else {
-                    objList.add(nextLine);
+                    Task currentTask = new Task(nextLine);
+                    taskList.add(currentTask);
                     System.out.println("  " + "added: " + nextLine);
                 }
             }
         } catch (IllegalStateException | NoSuchElementException e) {
             System.out.println("Error detected, Whey is shutting down!");
         }
+    }
+
+    public static int intSearch(String text) {
+        //replace non digit number with empty space
+        text = text.replaceAll("[^\\d]", "");
+        //replace every white space with empty space
+        text = text.replaceAll(" +", "");
+        if (text.equals("")) {
+            return -1;
+        }
+        return Integer.parseInt(text);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+
+    private String description;
+    private boolean done;
+
+    public Task(String description) {
+        this.description = description;
+        this.done = false;
+    }
+
+    //getter for done
+    public boolean getDone(boolean bool) {
+        return done;
+    }
+
+    //setter for done
+    public void setDone(boolean bool) {
+        this.done = bool;
+    }
+    @Override
+    public String toString() {
+        return "[" + (this.done ? "X" : " ") + "] "
+        + this.description;
+    }
+}
