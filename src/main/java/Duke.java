b5d6cdfb70e094eec8cc8e2079e07f6f0ac6c616
chriswengqi <chris_wengqi@hotmail.com>
2022-01-18 14:38:09 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,42 @@
+import javax.swing.plaf.basic.BasicSplitPaneUI;
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(chatBox("Hello! I'm Duke\n    What can I do for you?"));
+
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println(chatBox(command));
+            System.out.println();
+
+            command = sc.nextLine();
+        }
+
+        System.out.println(chatBox("Bye. Hope to see you again soon!"));
+
+        sc.close();
+    }
+
+    private static String chatBox(String command){
+        String tab = "    ";
+        String horizontalLines = tab + "___________________________________";
+
+        return horizontalLines + "\n" + tab + command + "\n" + horizontalLines;
+
     }
 }
+
+
+
