fecc1877bbeb2681a523a983d323246520c60ba5
xMashedxTomatox <xMashedxTomatox@gmail.com>
2022-01-18 16:55:19 +0800

    Add function greet, echo and bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,108 @@
+import java.io.*;
+import java.util.*;
+
 public class Duke {
+
+    public static class FastIO extends PrintWriter
+    {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastIO()
+        {
+            super(new BufferedOutputStream(System.out));
+            br = new BufferedReader(new
+                    InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException  e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        */
+
+        FastIO io = new FastIO();
+
+        greet();
+        boolean exited = false;
+
+        while (!exited) {
+            String input = io.nextLine();
+            if (input.equals("bye")) {
+                exited = true;
+            } else {
+                echo(input);
+            }
+        }
+
+        bye();
+    }
+
+    public static void greet() {
+        System.out.println("    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n");
+    }
+
+    public static void echo(String input) {
+        System.out.println("    ____________________________________________________________\n" +
+                "     " + input + "\n" +
+                "    ____________________________________________________________\n");
+    }
+
+    public static void bye() {
+        System.out.println("    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________");
     }
 }
