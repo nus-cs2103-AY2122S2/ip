0297c1e86be0c503bcb282097edf4246715f41e1
Rahul Prasad <e0638893@u.nus.edu>
2022-01-19 01:37:51 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
-public class Duke {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Duke {
     public static void main(String[] args) {
+        
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,12 +15,25 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         Scanner sc= new Scanner(System.in);
-        String str = sc.next();
+        String str = sc.nextLine();
+
+        ArrayList<String> list = new ArrayList<String>();
+        
+    
 
         while(!str.equals("bye")) {
-            System.out.println(str); 
-            str = sc.next();
-            
+            if(!str.equals("list")) {
+                list.add(str);
+                System.out.println("added: " + str);
+                
+
+            } else {
+                for(int i = 0; i < list.size(); i++) {
+                    System.out.println((i+1) + ". " + list.get(i));
+                }
+
+            }
+            str = sc.nextLine();
         }
         
             
@@ -24,7 +41,5 @@ public class Duke {
             sc.close();
             
         
-    }
-}
     }
 }
