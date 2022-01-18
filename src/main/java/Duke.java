74855f666ef7ee44ec23eaa9203d6ca0ae5118c3
yokester98 <yokeyue.loy@u.nus.edu>
2022-01-18 21:52:11 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,22 +4,36 @@ import java.util.ArrayList;
 public class Duke {
     // Attributes
     public static String LINE = "____________________________________________________________";
-    public static ArrayList<String> LIST = new ArrayList<>();
+    public static ArrayList<Task> LIST = new ArrayList<>();
 
     public static void greet() {
         System.out.println(LINE + "\n Hello! I'm Duke\n What can I do for you?\n" + LINE);
     }
 
     public static void add(String s) {
-        LIST.add(s);
+        LIST.add(new Task(s));
         System.out.println(LINE + "\n added: " + s + "\n" + LINE);
     }
 
     public static void list() {
-        System.out.println(LINE);
+        System.out.println(LINE + "\n Here are the tasks in your list:");
         for (int i = 1; i <= LIST.size(); i++) {
-            System.out.println(String.format(" %d. %s", i, LIST.get(i - 1)));
+            System.out.printf(" %d.[%s] %s%n", i, LIST.get(i - 1).getStatusIcon(), LIST.get(i - 1).getDescription());
         }
+        System.out.println(LINE);
+    }
+
+    public static void mark(int i) {
+        System.out.println(LINE + "\n Nice! I've marked this task as done: ");
+        LIST.get(i - 1).setDone();
+        System.out.printf("   [%s] %s%n", LIST.get(i - 1).getStatusIcon(), LIST.get(i - 1).getDescription());
+        System.out.println(LINE);
+    }
+
+    public static void unmark(int i) {
+        System.out.println(LINE + "\n OK, I've marked this task as not done yet:");
+        LIST.get(i - 1).setNotDone();
+        System.out.printf("   [%s] %s%n", LIST.get(i - 1).getStatusIcon(), LIST.get(i - 1).getDescription());
         System.out.println(LINE);
     }
 
@@ -30,15 +44,30 @@ public class Duke {
     public static void main(String[] args) {
         greet();
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNextLine()) {
-            String usrInput = sc.nextLine();
-            if (usrInput.equals("bye")) {
-                exit();
-                break;
-            } else if (usrInput.equals("list")) {
-                list();
-            } else {
-                add(usrInput);
+        label:
+        while (sc.hasNext()) {
+            String usrInput = sc.next();
+            switch (usrInput) {
+                case "bye":
+                    exit();
+                    break label;
+                case "list":
+                    list();
+                    break;
+                case "mark": {
+                    int taskNum = Integer.parseInt(sc.next());
+                    mark(taskNum);
+                    break;
+                }
+                case "unmark": {
+                    int taskNum = Integer.parseInt(sc.next());
+                    unmark(taskNum);
+                    break;
+                }
+                default:
+                    usrInput += sc.nextLine();
+                    add(usrInput);
+                    break;
             }
         }
     }
