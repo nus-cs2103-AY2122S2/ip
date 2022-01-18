3cf77a31e603678b46acaa8bb9885cbaf08358c7
Shivang211 <shivanggupta2110@gmail.com>
2022-01-18 14:42:21 +0800

    Added mark and unmark

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,22 +4,17 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private ArrayList<String> dukeList = new ArrayList<String>();
+    private ArrayList<Task> dukeList = new ArrayList<Task>();
     Scanner sc = new Scanner(System.in);
 
     public void startDuke() {
         displayHelloMessage();
-        
-        boolean loop = true;
-        while (loop) {
-            String input = sc.nextLine();
-            if (input.equals("Bye")) {
-                displayByeMessage();
-                loop = false;
-            } else {
-                executeCommand(input);
-            }
+        String command = sc.nextLine();
+        while(!command.equals("Bye")) {
+            executeCommand(command);
+            command = sc.nextLine();
         }
+        displayByeMessage();
     }
 
     public void displayHelloMessage() {
@@ -43,12 +38,14 @@ public class Duke {
         displayLine();
     }
 
-    public void addToList(String command) {
-        dukeList.add(command);
+    public void addTaskToList(String command) {
+        Task newTask = new Task(command);
+        dukeList.add(newTask);
     }
 
     public void displayList() {
         displayLine();
+        System.out.println("Here are the tasks in your list:");
         for (int i = 1;i <= dukeList.size(); i++) {
             System.out.println( (i) + ". " + dukeList.get(i-1));
         }
@@ -56,13 +53,39 @@ public class Duke {
     }
 
     void executeCommand(String command) {
-        if(!command.equals("list") && !command.equals("Bye")) {
-            addToList(command);
-            displayMessage(command);
-        } else if (command.equals("list")) {
+        if (command.equals("list")) {
             displayList();
-            
+        } else if (command.length() > 4 && command.substring(0,5).equals("mark ")) {
+            int taskIndex = Integer.parseInt(command.substring(5)) - 1;
+            Task toBeCompleted = dukeList.get(taskIndex);
+            toBeCompleted.isDone = true;
+            toBeCompleted.isComplete();
+            displayTaskCompletion(toBeCompleted);
+        } else if (command.length() > 6 && command.substring(0,7).equals("unmark ")) {
+            int taskIndex = Integer.parseInt(command.substring(7)) - 1;
+            Task toBeCompleted = dukeList.get(taskIndex);
+            toBeCompleted.isDone = false;
+            //toBeCompleted.isComplete();
+            displayIncompleteTask(toBeCompleted);
+        } else {
+            displayMessage(command);
+            addTaskToList(command);
         }
+
+    }
+
+    void displayIncompleteTask(Task complete){
+        displayLine();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(complete);
+        displayLine();
+    }
+
+    void displayTaskCompletion(Task incomplete) {
+        displayLine();
+        System.out.println("Nice! I've marked this task as done: ");
+        System.out.println(incomplete);
+        displayLine();
     }
     
     public static void main(String[] args) {
