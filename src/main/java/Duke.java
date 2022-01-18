1d8209ee147f0c87502186c28d05987fc95f3ea6
kaiyichen <chenkaiyi01@gmail.com>
2022-01-18 17:25:57 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,45 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.* ;
 
 public class Duke {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String greeting = "Hello! I'm Duke\nWhat can I do for you?";
 
         String separation = "\n******************************\n";
         System.out.println(separation + greeting + separation);
-        List list_of_inputs = new ArrayList<String>();
+        ArrayList<Task> list_of_inputs = new ArrayList<>();
+
         while(true) {
-            Scanner input = new Scanner(System.in);
-            String inp = input.nextLine();
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String inp = br.readLine();
+            String[] temp = inp.split(" ");
 
             if (inp.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (inp.equals("list")) {
                 for (int i = 0; i < list_of_inputs.size(); i++) {
-                    System.out.println(Integer.toString(i+1) + ". " + list_of_inputs.get(i));
+
+                    System.out.println((i+1) + ". [" +  (list_of_inputs.get(i)).getStatusIcon() + "] " + list_of_inputs.get(i).description);
                 }
+            } else if (temp[0].equals("mark")) {
+                int curr_no = Integer.parseInt(temp[1]) - 1;
+                Task task_done = list_of_inputs.get(curr_no);
+                task_done.markedDone();
+                System.out.println ("[" + task_done.getStatusIcon() + "]" + task_done.description );
+            } else if (temp[0].equals("unmark")) {
+                int curr_no = Integer.parseInt(temp[1]) - 1;
+                Task task_undone = list_of_inputs.get(curr_no);
+                task_undone.markedUndone();
+                System.out.println ("[" + task_undone.getStatusIcon() + "] " + task_undone.description );
             } else {
                 System.out.println("added:" + inp);
-                list_of_inputs.add(inp);
+                list_of_inputs.add(new Task(inp));
             }
         }
 
