2bf13a118462a3ba72f9f65066c35e045d058456
yokester98 <yokeyue.loy@u.nus.edu>
2022-01-18 20:30:34 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String LINE = "____________________________________________________________";
+
+    public static void greet() {
+        System.out.println(LINE + "\n Hello! I'm Duke\n What can I do for you?\n" + LINE);
+    }
+
+    public static void echo(String s) {
+        System.out.println(LINE + "\n " + s + "\n" + LINE);
+    }
+
+    public static void exit() {
+        System.out.println(LINE + "\n Bye. Hope to see you again soon!\n" + LINE);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String usrInput = sc.next();
+            if (usrInput.equals("bye")) {
+                exit();
+                break;
+            } else {
+                echo(usrInput);
+            }
+        }
     }
 }
