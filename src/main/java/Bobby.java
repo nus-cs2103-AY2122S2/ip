607dc75e6d0630d9c2a3611bb4eb95aa6a1f6ae9
YiFei2 <fongyifei1999@gmail.com>
2022-01-18 16:51:50 +0800

    Implemented task.java, completed Level-3

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -3,24 +3,39 @@ import java.util.ArrayList;
 
 public class Bobby {
     public static void main(String[] args) {
-        ArrayList<String> stringArray = new ArrayList<String>();
+        ArrayList<Task> stringArray = new ArrayList<Task>();
         System.out.println("Bobby greets you. Bobby is here to help.");
 
         while(true) {
             Scanner scannerObj = new Scanner(System.in);
             String userInput = scannerObj.nextLine();
+            String[] inputs = userInput.split(" ");
+            String command = inputs[0];
+
             if (userInput.equals("bye")) {
                 System.out.println("Bobby bids you farewell.");
                 break;
             } else if (userInput.equals("list")) {
                 System.out.println("Here is what you told Bobby:");
                 for (int i = 0; i < stringArray.size(); i++) {
+                    Task t = stringArray.get(i);
                     int count = i + 1;
-                    System.out.println(count + ". " + stringArray.get(i));
+                    System.out.println(count + ".[" + t.getStatusIcon() + "] " + t.description);
                 }
+            } else if (command.equals("mark")) {
+                Task t = stringArray.get(Integer.parseInt(inputs[1]) - 1);
+                t.markAsDone();
+                System.out.println("Bobby applauds you. This task is done:\n"
+                    + "[" + t.getStatusIcon() + "] " + t.description);
+            } else if (command.equals("unmark")) {
+                Task t = stringArray.get(Integer.parseInt(inputs[1]) - 1);
+                t.unmarkAsDone();
+                System.out.println("Bobby will remember that this task is not yet done:\n"
+                        + "[" + t.getStatusIcon() + "] " + t.description);
             } else {
-                System.out.println("Bobby heard: " + userInput);
-                stringArray.add(userInput);
+                Task newTask = new Task(userInput);
+                System.out.println("Bobby heard: " + newTask.description);
+                stringArray.add(newTask);
             }
         }
     }
