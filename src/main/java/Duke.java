2a067d946512dfcdbd7ba00c8d0f2ceb3ec0bc57
kaiyichen <chenkaiyi01@gmail.com>
2022-01-18 16:29:41 +0800

    Level-1 corrected

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
         System.out.println(separation + greeting + separation);
-        System.out.println(separation +list + separation);
-        System.out.println(separation +blah+ separation);
-        System.out.println(separation +bye + separation);
+
+
+        while(true) {
+            Scanner input = new Scanner(System.in);
+            String inp = input.nextLine();
+            if (inp.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(separation+ inp + separation);
+            }
+        }
 
     }
 
     public static String greeting = "Hello! I'm Duke\nWhat can I do for you?";
-    public static String list = "list";
-    public static String blah = "blah";
-    public static String bye = "Bye, Hope to see you again soon!";
+
     public static String separation = "\n******************************\n";
 }
