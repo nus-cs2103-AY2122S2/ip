e40e6b7c4e5f68cf12be58729a0ba1162147746b
janald99 <e0543657@u.nus.edu>
2022-01-18 22:22:42 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 public class Duke {
     private static final String botName = "Feline";
-    private String[] list;
+    private Task[] list;
     private int listIndex;
 
     public Duke() {
-        this.list = new String[100];
+        this.list = new Task[100];
         this.listIndex = 1;
     }
     private static void greet() {
@@ -16,15 +16,71 @@ public class Duke {
     private static void farewell() {
         System.out.println("See you next time!\n");
     }
-    private void addToList(String str) {
+    private void addToList(String str) {    //adds task to list
         System.out.println("added: " + str + "\n");
-        this.list[this.listIndex] = str;
+        this.list[this.listIndex] = new Task(str);
         this.listIndex ++;
+    }
+    private String getTaskDescription(int i) {
+        return this.list[i].description;
+    }
+    private boolean getTaskStatus(int i) {
+        return this.list[i].isDone;
+    }
+    private String getTaskStatement(int i) {
+        //[X] read book, or [ ] do sth
+        return "[" + this.list[i].getStatusIcon() + "] " + this.getTaskDescription(i) + "\n";
     }
     private void printList() {
         for (int i = 1; i < this.listIndex; i++) {
-            System.out.println(i + ". " + this.list[i] + "\n");
+            System.out.println(i + "." + this.getTaskStatement(i));
         }
+    }
+    private static String getFirstWord(String input) {
+        int index = input.indexOf(' ');
+        if (index > -1) {
+            // more than one word
+            return input.substring(0, index).trim();   // Extract first word
+        } else {
+            // there is only one word
+            return input;
+        }
+    }
+
+    private boolean isMarkAction(String action, String input) {
+        if (!action.equals("mark")) return false;
+        // also need to make sure there is only 1 space, and the second "word" is an int character
+//        int wordCount = 0;
+//        String trim = input.trim();
+//        System.out.println("trim " + trim);
+//        wordCount = trim.split("\\s+").length;
+//        System.out.println("word count " + wordCount);
+//        if (wordCount != 2) return false;
+
+        return true;
+    }
+
+    private boolean isUnMarkAction(String action, String input) {
+        if (!action.equals("unmark")) return false;
+        return true;
+    }
+
+    private void mark(String input) {
+        System.out.println("Nice! I've marked this task as done:\n");
+        String[] words = input.split(" ", 2);
+        // the second word is expected to be a number for now
+        int taskNumber = Integer.parseInt(words[1]);
+        this.list[taskNumber].markAsDone();
+        System.out.println(this.getTaskStatement(taskNumber) + "\n");
+    }
+
+    private void unmark(String input) {
+        System.out.println("OK, I've marked this task as not done yet:\n");
+        String[] words = input.split(" ", 2);
+        // the second word is expected to be a number for now
+        int taskNumber = Integer.parseInt(words[1]);
+        this.list[taskNumber].markAsUndone();
+        System.out.println(this.getTaskStatement(taskNumber) + "\n");
     }
     public static void main(String[] args) {
 
@@ -32,11 +88,18 @@ public class Duke {
         Duke.greet();
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        while (!input.equals("bye")) {
+        while (!input.equals("bye")) {  //terminates system when user says bye
             if (input.equals("list")) {
                 duke.printList();
             } else {
-                duke.addToList(input);
+                String action = Duke.getFirstWord(input);
+                if (duke.isMarkAction(action, input)) {
+                    duke.mark(input);
+                } else if (duke.isUnMarkAction(action, input)) {
+                    duke.unmark(input);
+                } else {
+                    duke.addToList(input);
+                }
             }
             input = sc.nextLine();
         }
