ea68bac73cfb65466977aa08e06e6c2a7e12df15
YiFei2 <fongyifei1999@gmail.com>
2022-01-18 20:20:41 +0800

    completed Level-4

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 
 public class Bobby {
     public static void main(String[] args) {
-        ArrayList<Task> stringArray = new ArrayList<Task>();
+        ArrayList<Task> taskArray = new ArrayList<Task>();
         System.out.println("Bobby greets you. Bobby is here to help.");
 
         while(true) {
             Scanner scannerObj = new Scanner(System.in);
             String userInput = scannerObj.nextLine();
-            String[] inputs = userInput.split(" ");
+            String[] inputs = userInput.split(" ", 2);
             String command = inputs[0];
 
             if (userInput.equals("bye")) {
@@ -17,23 +17,44 @@ public class Bobby {
                 break;
             } else if (userInput.equals("list")) {
                 System.out.println("Here is what you told Bobby:");
-                for (int i = 0; i < stringArray.size(); i++) {
-                    Task t = stringArray.get(i);
+                for (int i = 0; i < taskArray.size(); i++) {
+                    Task t = taskArray.get(i);
                     int count = i + 1;
                     System.out.println(count + "." + t);
                 }
             } else if (command.equals("mark")) {
-                Task t = stringArray.get(Integer.parseInt(inputs[1]) - 1);
+                Task t = taskArray.get(Integer.parseInt(inputs[1]) - 1);
                 t.markAsDone();
                 System.out.println("Bobby applauds you. This task is done:\n" + t);
             } else if (command.equals("unmark")) {
-                Task t = stringArray.get(Integer.parseInt(inputs[1]) - 1);
+                Task t = taskArray.get(Integer.parseInt(inputs[1]) - 1);
                 t.unmarkAsDone();
                 System.out.println("Bobby will remember that this task is not yet done:\n" + t);
+            } else if (command.equals("todo")) {
+                Todo newTodo = new Todo(inputs[1]);
+                System.out.println("Bobby heard: " + newTodo);
+                taskArray.add(newTodo);
+                System.out.println("Bobby remembers " + taskArray.size() + " tasks.");
+            } else if (command.equals("deadline")) {
+                String[] splitInputs = inputs[1].split(" /by ", 2);
+                String description = splitInputs[0];
+                String by = splitInputs[1];
+                Deadline newDeadline = new Deadline(description, by);
+                System.out.println("Bobby heard: " + newDeadline);
+                taskArray.add(newDeadline);
+                System.out.println("Bobby remembers " + taskArray.size() + " tasks.");
+            } else if (command.equals("event")) {
+                String[] splitInputs = inputs[1].split(" /at ", 2);
+                String description = splitInputs[0];
+                String at = splitInputs[1];
+                Event newEvent = new Event(description, at);
+                System.out.println("Bobby heard: " + newEvent);
+                taskArray.add(newEvent);
+                System.out.println("Bobby remembers " + taskArray.size() + " task(s).");
             } else {
                 Task newTask = new Task(userInput);
                 System.out.println("Bobby heard: " + newTask.description);
-                stringArray.add(newTask);
+                taskArray.add(newTask);
             }
         }
     }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,14 +1,11 @@
 public class Todo extends Task {
 
-    protected String by;
-
     public Todo(String description) {
         super(description);
-        this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: ";
+        return "[T]" + super.toString();
     }
 }
