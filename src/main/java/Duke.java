fc1b865a97f8d6908078c66817773cd880bf988e
Melvin <theemptyonelvsyx@gmail.com>
2022-01-18 21:05:34 +0800

    Added Greet, Echo, Exit to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,37 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+
+        String greeting = "     ____________________________________________________________\n"
+                + "     Hello! I'm Duke\n"
+                + "     What can I do for you?\n"
+                + "     ____________________________________________________________";
+
+        System.out.println(greeting);
+
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext()) {
+
+            String input = sc.next();
+            String output = "";
+            String line = "     ____________________________________________________________\n";
+
+            if (input.equals("bye"))
+                output = "Bye. Hope to see you again soon!";
+            else
+                output = input;
+
+            System.out.println(line
+                    + "     "
+                    + output
+                    + "\n"
+                    + line);
+
+            if (!output.equals(input))
+                break;
+        }
+
+        sc.close();
     }
-}
