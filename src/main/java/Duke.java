e2a4e85b3e7a2b8d8325e565012749811cb8eecf
DannyDakota <51451719+DannyDakota@users.noreply.github.com>
2022-01-18 09:48:13 +0800

    Level 1 - Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Whey\n" + "What can I do for you today?");
+        Scanner userInput = new Scanner(System.in);
+        try {
+            while (true) {
+                String nextLine = userInput.nextLine();
+                if (nextLine.equals("bye")) {
+                    System.out.println("  " + "Bye beautiful! hope to see you again hehe");
+                    break;
+                } else {
+                    System.out.println("  " + nextLine);
+                }
+            }
+        } catch (IllegalStateException | NoSuchElementException e) {
+            System.out.println("Error detected, Whey is shutting down!");
+        }
     }
 }
