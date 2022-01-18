5af774af2981c56d15fa01af3e41bf2075700ca8
Vishwanth2210 <vishwanth.s22@u.nus.edu>
2022-01-19 02:39:11 +0800

    Added Greet, Echo and Exit feature

---
+++ src/main/java/Arthur.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Arthur {
+
+    /**
+     * Adds design above and below the given text.
+     * @param text The string to be placed between the design
+     */
+    public static void printFormat(String text) {
+        String design = "_";
+        System.out.println(design.repeat(50) + "\n"
+                + text + "\n" + design.repeat(50));
+    }
+
+    public static void main(String[] args) {
+        BufferedReader io = new BufferedReader(new InputStreamReader(System.in));
+
+        // Greetings
+        String logo = " / \\   _ |_ |_  | |  _  \n"
+                + "/---\\ |  |_ | | |_| |  ";
+        printFormat("Greetings from\n" + logo
+                + "\n" + "How may I assist you today?");
+
+        // Echo and Exit Feature
+        String input;
+        try {
+            input = io.readLine();
+            // Checks for exit input 'bye'
+            while (!input.equals("bye")) {
+                printFormat(input);
+                input = io.readLine();
+            }
+            printFormat("Bye!" + "\n" + "Have a great day!");
+        } catch (IOException e) {
+            System.out.println("Sorry, there seems to be an Issue."
+                    + "\n" + "Please restart and try again");
+        }
+    }
+}
