e468bf88e08a561c8fa71e78ab27f924b1254c67
Lai Mei Tin <e0550504@u.nus.edu>
2022-01-18 22:54:53 +0800

    Add some error handling mechanisms

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -61,41 +61,40 @@ public class Duke {
         System.out.println(tasks[taskNumber - 1]);
     }
 
-    private static void execute(String command) {
-        String[] commandTokens = command.split(" ");
+    private static void execute(String command) throws DukeException, 
+            NumberFormatException, ArrayIndexOutOfBoundsException {
+        String[] commandTokens = command.split(" ", 2);
         switch (commandTokens[0]) {
         case "list":
             listTask();
             break;
         case "mark":
-            mark(Integer.parseInt(commandTokens[1]));
+            int indexOfTaskToMark = Integer.parseInt(commandTokens[1]);
+            if (!(1 <= indexOfTaskToMark && indexOfTaskToMark <= numberOfTasks)) {
+                throw new DukeException("The index of task to be marked is invalid!!");
+            }
+            mark(indexOfTaskToMark);
             break;
         case "unmark":
-            unmark(Integer.parseInt(commandTokens[1]));
+            int indexOfTaskToUnmark = Integer.parseInt(commandTokens[1]);
+            if (!(1 <= indexOfTaskToUnmark && indexOfTaskToUnmark <= numberOfTasks)) {
+                throw new DukeException("The index of task to be unmarked is invalid!!");
+            }
+            unmark(indexOfTaskToUnmark);
             break;
         case "todo":
-            //fallthrough
+            addTask("todo", commandTokens[1], "");
+            break;
         case "deadline":
-            //fallthrough
+            String[] deadlineTokens = commandTokens[1].split(" /by ");
+            addTask("deadline", deadlineTokens[0], deadlineTokens[1]);
+            break;
         case "event":
-            String taskName = "";
-            String dateTime = "";
-            boolean isTaskNameTurn = true;
-            for (int i = 1; i < commandTokens.length; i++) {
-                String currentToken = commandTokens[i];
-                if (currentToken.equals("/by") || currentToken.equals("/at")) {
-                    isTaskNameTurn = false;
-                    continue;
-                }
-                if (isTaskNameTurn) {
-                    taskName += (taskName.equals("") ? "" : " ") + currentToken;
-                } else {
-                    dateTime += (dateTime.equals("") ? "" : " ") + currentToken;
-                }
-            }
-            addTask(commandTokens[0], taskName, dateTime);
+            String[] eventTokens = commandTokens[1].split(" /at ");
+            addTask("event", eventTokens[0], eventTokens[1]);
+            break;
         default:
-            ;
+            throw new DukeException("I'm sorry, but I don't know what that means :(");
         }
     }
 
@@ -110,7 +109,15 @@ public class Duke {
             if (command.equals("bye")) {
                 isQuitting = true;
             } else {
-                execute(command);
+                try {
+                    execute(command);
+                } catch (DukeException exception) {
+                    System.out.println("Uh oh! " + exception.getMessage());
+                } catch (NumberFormatException exception) {
+                    System.out.println("Please use index of task to indicate which task to mark/unmark");
+                } catch (ArrayIndexOutOfBoundsException exception) {
+                    System.out.println("Seems like the command is incomplete -_-");
+                }
             }
         }
     }
