179d1f34e2a03d904749dd8c134dfd09d20055f4
leeyiheng12 <leeyiheng12@gmail.com>
2022-01-18 19:40:56 +0800

    lvl1: made the bot echo user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,50 @@
+import java.util.*;
+
 public class Duke {
+
+    public static String indent = "    ";
+    public static String separator = "--------------------------------------------";
+    public static String[] openingMessage = new String[] {"Hello! I'm Duke", "What can I do for you?"};
+    public static String closingMessage = "Bye. Hope to see you again soon!";
+
+    public static void printIndent(String s) {
+        System.out.println(indent + s);
+    }
+
+    public static void printWithSeparator(String message) {
+        printIndent(separator);
+        printIndent(message);
+        printIndent(separator + "\n");
+    }
+
+    public static void printWithSeparator(String[] messages) {
+        printIndent(separator);
+        for (String message : messages) printIndent(message);
+        printIndent(separator + "\n");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = indent + " ____        _        \n"
+                + indent + "|  _ \\ _   _| | _____ \n"
+                + indent + "| | | | | | | |/ / _ \\\n"
+                + indent + "| |_| | |_| |   <  __/\n"
+                + indent + "|____/ \\__,_|_|\\_\\___|\n";
+
+        // introduction messages
+        System.out.println(logo);
+        printWithSeparator(openingMessage);
+
+        // read input
+        Scanner sc = new Scanner(System.in);
+        String userInput;
+        while (true) {
+            userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                printWithSeparator(closingMessage);
+                break;
+            }
+            printWithSeparator(userInput);
+        }
+
     }
 }
