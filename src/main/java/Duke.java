1721366479388b206ef51e90a91edb8f1a49d7e3
Jessica Jacelyn <jessicajacelyn@gmail.com>
2022-01-18 17:54:09 +0800

    Level 3 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,32 +4,47 @@ import java.util.*;
 public class Duke {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = "";
-        ArrayList<String> items = new ArrayList<>();
-
+        ArrayList<Task> tasks = new ArrayList<>();
         System.out.println("Hello!!! I'm jBot\nWhat can I do for you? :3");
-        s = br.readLine();
+        String full = br.readLine();
+        String[] s = full.split(" ");
 
         while (true) {
             // "bye" to end the program
-            if (s.equals("bye")) {
+            if (s[0].equals("bye")) {
                 System.out.println("Bye!! See you again soon!!");
                 break;
             }
-            // if user requests to list the items
-            else if (s.equals("list")){
+            // if user requests to list their tasks
+            else if (s[0].equals("list")){
+                System.out.println("Here are the tasks in your lists:\n");
                 int t = 1;
-                for(String item : items) {
-                    System.out.println(t + ". " + item);
+                for(Task item : tasks) {
+                    System.out.println(t + ". [" + item.getDone() + "] " + item.desc);
                     t++;
                 }
             }
+            else if (s[0].equals("mark")) {
+                int n = Integer.parseInt(s[1]) - 1;
+                tasks.set(n, tasks.get(n).mark());
+                Task temp = tasks.get(n);
+                System.out.println("Alright! I've marked this as done:\n  [" + temp.getDone() + "] " + temp.desc);
+
+            }
+            else if (s[0].equals("unmark")) {
+                int n = Integer.parseInt(s[1]) - 1;
+                tasks.set(n, tasks.get(n).unmark());
+                Task temp = tasks.get(n);
+                System.out.println("Alright! I've marked this as not done:\n  [" + temp.getDone() + "] " + temp.desc);
+
+            }
             // user add items to list
             else {
-                System.out.println("     added: " + s);
-                items.add(s);
+                System.out.println("     added: " + full);
+                tasks.add(new Task(full));
             }
-            s = br.readLine();
+            full = br.readLine();
+            s = full.split(" ");
         }
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Task {
+public class Task {
+
+    public boolean done;
+    public String desc;
+
+    public Task(String desc) {
+        this.desc = desc;
+        this.done = false;
+    }
+    public Task(String desc, boolean done) {
+        this.desc = desc;
+        this.done = done;
+    }
+    public String getDone() {
+        return (this.done ? "X" : " "); // mark done task with X
+    }
+//    public String getDesc() {
+//        return this.desc;
+//    }
+    public Task mark() {
+        return new Task(this.desc, true);
+    }
+    public Task unmark() {
+        return new Task(this.desc);
+    }
 }
