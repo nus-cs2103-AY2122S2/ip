2de421fcb3252efc302c24d37c8c0b1660283079
Jessica Jacelyn <jessicajacelyn@gmail.com>
2022-01-18 17:30:51 +0800

    Level 2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,22 +3,34 @@ import java.util.*;
 
 public class Duke {
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello!!! I'm jBot\nWhat can I do for you? :3");
-        String s = "";
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = "";
+        ArrayList<String> items = new ArrayList<>();
+
+        System.out.println("Hello!!! I'm jBot\nWhat can I do for you? :3");
+        s = br.readLine();
+
         while (true) {
+            // "bye" to end the program
             if (s.equals("bye")) {
                 System.out.println("Bye!! See you again soon!!");
                 break;
-            } else
-                System.out.println(s);
+            }
+            // if user requests to list the items
+            else if (s.equals("list")){
+                int t = 1;
+                for(String item : items) {
+                    System.out.println(t + ". " + item);
+                    t++;
+                }
+            }
+            // user add items to list
+            else {
+                System.out.println("     added: " + s);
+                items.add(s);
+            }
             s = br.readLine();
         }
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo)
     }
 }
+
