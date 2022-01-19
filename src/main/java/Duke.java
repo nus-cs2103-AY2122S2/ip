a47fac8a5f0989e4b50c312f52120e933604b1d9
Keith <e0544450@u.nus.edu>
2022-01-19 17:55:44 +0800

    Level 2 commit, Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,14 +10,26 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
 
+        String[] strings = new String[100];
+        int counter = 0;
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 System.out.println(lines + "\nBye. Hope to see you again soon!\n" + lines);
                 break;
             }
+            else if (input.equals("list")) {
+                System.out.println(lines);
+                for (int i = 0;i < counter;i++) {
+                    System.out.println(Integer.toString(i + 1) + ". " + strings[i]);
+                }
+                System.out.println(lines);
+
+            }
             else {
-                System.out.println(lines + "\n" + input + "\n" + lines);
+                System.out.println(lines + "\nadded: " + input + "\n" + lines);
+                strings[counter++] = input;
             }
         }
     }
