6f450be8b8fd4917e2c9beb06d976974924c9e91
Huang <72971889+hqhqhq1@users.noreply.github.com>
2022-01-20 03:25:06 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+    static Scanner sc = new Scanner(System.in);
+    static String line = "\n_______________________^_^__________________________________\n";
+
+    public static void echo() {
+        String input;
+        while(true) {
+            input = sc.next();
+            if (input.equals("bye")) {
+                System.out.println(line + "Bye. Hope to see you again soon!\n" + line);
+                break;
+            } else {
+                System.out.println(line + input + line);
+            }
+        }
+    }
+
     public static void main(String[] args) {
+
+
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(line + "Hello! I'm Duke\n" +
+                "What can I do for you?\n" + line);
+
+        Duke.echo();//level-1
+
+
     }
+
 }
