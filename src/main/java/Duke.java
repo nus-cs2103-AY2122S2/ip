1e5002b7e0b99925167156eaa03b5f8d61a8853d
kashing555 <kashing555@gmail.com>
2022-01-19 17:03:01 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,14 +11,22 @@ public class Duke {
 
         Scanner input = new Scanner(System.in);
         String text = "";
+        String[] list = new String[100];
+        int tasks = 0;
 
         while (!text.equals("bye")) {
             text = input.next();
+            tasks++;
             System.out.println("------------------------------------------------------------");
             if (text.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
+            } else if (text.equals("list")) {
+                for (int i = 1; i < tasks; i++) {
+                    System.out.println(i + ". " + list[i - 1]);
+                }
             } else {
-                System.out.println(text);
+                list[tasks - 1] = text;
+                System.out.println("added: " + text);
             }
             System.out.println("------------------------------------------------------------");
         }
