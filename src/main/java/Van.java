3fa32c10a6fb2fdb2612e201526059bdee2dfbfa
howardwhw2 <howardwhw2@gmail.com>
2022-01-19 21:52:42 +0800

    added level 2

--- src/main/java/Van.java
+++ src/main/java/Van.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class Van {
     public static void main(String[] args) {
+        int counter = 0;
         String command = null;
+        String[] tasks = new String[100];
         Scanner input = new Scanner(System.in);
         String divide = "-------------------------------------------";
         System.out.println("Hello I am Van");
@@ -11,8 +13,19 @@ public class Van {
         command = input.nextLine();
         System.out.println(divide);
         while (!command.equalsIgnoreCase("bye")) {
-            System.out.println(command);
-            System.out.println(divide);
+            switch(command.toLowerCase()) {
+                case "list":
+                    for (int i = 0; i < counter; i++) {
+                        System.out.println(i+1 + ". " + tasks[i]);
+                    }
+                    System.out.println(divide);
+                    break;
+                default:
+                    tasks[counter] = command;
+                    counter++;
+                    System.out.println("added: " + command);
+                    System.out.println(divide);
+            }
             command = input.nextLine();
             System.out.println(divide);
         }
