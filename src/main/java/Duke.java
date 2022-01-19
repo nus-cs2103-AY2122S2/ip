b95a266ac2f33eb87b9b5d4ddfe3bf3a463d0b4a
cheekean5848 <loo.ck5848@outlook.com>
2022-01-19 20:43:01 +0800

    Complete Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,25 +5,36 @@ import java.util.Objects;
 
 public class Duke {
     public static void main(String[] args) throws IOException {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "\t ____        _        \n"
+                + "\t|  _ \\ _   _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| |   <  __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|\n";
 
         String line = "\t_______________________________________________\n";
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(logo + line + "\t Hello! I'm Duke\n" + "\t What can I do for you?\n" + line);
+        String items[] = new String[100];
+        int count = 0;
+
+        System.out.println(line + logo + "\t Hello! I'm Duke\n" + "\t What can I do for you?\n" + line);
 
         while (true) {
             String input = reader.readLine();
+            System.out.println(line);
             if (input.equals("bye")) {
-                System.out.println(line + "\t Bye. Hope to see you again soon!\n" + line);
+                System.out.println("\t Bye. Hope to see you again soon!\n" + line);
                 break;
+            } else if (input.equals("list")) {
+                for (int i=0; i<count; i++) {
+                    System.out.println("\t " + i + ". " + items[i]);
+                }
             } else {
-                System.out.println(line + "\t " + input + "\n" + line);
+                items[count] = input;
+                count++;
+                System.out.println("\t added: " + input);
             }
+            System.out.println(line);
         }
     }
 }
