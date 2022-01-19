2f079ed1169af66f9341897fd30a82b7d85a5f35
Pun Hui Min <punhuimin99@gmail.com>
2022-01-19 21:12:12 +0800

    Updated Duke with list handler
    Ducky, previously known as Duke, can now store the text handed to it and
    display the text back to the user when they request for it.
    
    To enhance Ducky, next step is to add the ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,18 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
 
-
-        System.out.println("Hello! I'm Ducky! \nWhat can I do for you?" +
+        ArrayList<String> tasks = new ArrayList<String>();
+        System.out.println("Hello! I'm Ducky! :) \nWhat can I do for you?" +
                 "\n-----------------");
 
         while(true){
@@ -18,72 +24,16 @@ public class Duke {
                 System.out.println(byeResponse);
                 break;
             }
+            else if(response.equals("list")){
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i+1 +". " + tasks.get(i));
+                }
+            }
             else {
-                System.out.println(response + "\n-----------------");
+                tasks.add(response);
+                System.out.println("added: " + response + "\n-----------------");
             }
         }
     }
 }
 
-
-/**
- * Fast I/O
- * @source https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
- */
-class FastIO extends PrintWriter
-{
-    BufferedReader br;
-    StringTokenizer st;
-
-    public FastIO()
-    {
-        super(new BufferedOutputStream(System.out));
-        br = new BufferedReader(new
-                InputStreamReader(System.in));
-    }
-
-    String next()
-    {
-        while (st == null || !st.hasMoreElements())
-        {
-            try
-            {
-                st = new StringTokenizer(br.readLine());
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
-        return st.nextToken();
-    }
-
-    int nextInt()
-    {
-        return Integer.parseInt(next());
-    }
-
-    long nextLong()
-    {
-        return Long.parseLong(next());
-    }
-
-    double nextDouble()
-    {
-        return Double.parseDouble(next());
-    }
-
-    String nextLine()
-    {
-        String str = "";
-        try
-        {
-            str = br.readLine();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        return str;
-    }
-}
