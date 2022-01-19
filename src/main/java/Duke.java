ab5e4893f8a0abb185312cea158fc589dc4b469d
LapisRaider <angielimenqi7@gmail.com>
2022-01-19 20:16:11 +0800

    - cleaned up Duke code to use member functions instead of static member functions - seperated some of duke variable to become member variables

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,23 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
+    private boolean isRunning;
+    private ArrayList<Task> userTexts;
+
+    public Duke() {
+        this.userTexts = new ArrayList<Task>();
+        this.isRunning = true;
+    }
+
     public static void main(String[] args) {
+        Duke duke = new Duke();
+
+        duke.startGreeting();
+        duke.runDuke();
+    }
+
+    /* Initial greeting for Duke */
+    public void startGreeting() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -10,33 +26,45 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> userTexts = new ArrayList<String>();
-
         printDukeResponse("Sup! Name's Duke \nHow can I help you today?");
+    }
 
-        boolean isRunning = true;
-        while (isRunning) {
-            String userResponse = scanner.nextLine();
+    /* Run Duke default behavior */
+    public void runDuke() {
+        Scanner sc = new Scanner(System.in);
 
-            if (userResponse.equals("bye")) { // quit application
-                printDukeResponse("See ya!");
-                isRunning = false;
-                continue;
-            } else if (userResponse.equals("list")) {
-                printDukeResponse(getListStr(userTexts)); // just copy user response
+        while (this.isRunning) {
+            String userResponse = sc.nextLine();
+
+            if (commandsParsed(userResponse)) {
                 continue;
             }
 
-            userTexts.add(userResponse);
+            this.userTexts.add(new Task(userResponse));
             printDukeResponse("added: " + userResponse);
         }
 
-        scanner.close();
+        sc.close();
+    }
+
+    /* Return true if command is successfully parsed */
+    public boolean commandsParsed(String commands) {
+        if (commands.equals("bye")) { // quit application
+            printDukeResponse("See ya!");
+            this.isRunning = false;
+            return true;
+        }
+
+        if (commands.equals("list")) {
+            printDukeResponse(getListStr(userTexts)); // just copy user response
+            return true;
+        }
+
+        return false;
     }
 
     /* Print in the Duke response format */
-    public static void printDukeResponse(String response) {
+    public void printDukeResponse(String response) {
         System.out.println(
                 "\n--------------------------------------------------------------------------------------------");
         System.out.println("Duke Speaking:\n");
@@ -45,7 +73,7 @@ public class Duke {
                 "--------------------------------------------------------------------------------------------\n");
     }
 
-    public static String getListStr(ArrayList<? extends Object> list) {
+    public String getListStr(ArrayList<? extends Object> list) {
         StringBuilder sb = new StringBuilder("");
 
         for (int i = 0; i < list.size(); ++i) {
