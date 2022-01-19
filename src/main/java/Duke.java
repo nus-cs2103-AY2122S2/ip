12200d76b6d69e2b4ba8f9780ce0d6f38a54182a
Raja Dishafnya Achadi <uno.highclass2@gmail.com>
2022-01-20 04:21:03 +0800

    Added Echo class and added echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Heya! I'm Duke!\n" +
+                "What can I do for ya?\n\n");
+
+        // Create new variables
+        String tempString;
+        Echo tempEcho = new Echo();
+
+        // While loop
+        while(true) {
+
+            tempString = scanner.nextLine();
+
+            // Breaks the loop
+            if (tempString.equals("bye")) {
+                break;
+            }
+
+            tempEcho.EchoStr(tempString);
+        }
+
+        // The last thing Duke says
+        System.out.println("  > Ok then, see ya!");
     }
 }
---
+++ src/main/java/Echo.java
@@ -0,0 +1,10 @@
+// Echo class for Level-1
+public class Echo {
+    public Echo() {}
+
+    // Echoes a string
+    public void EchoStr (String temp) {
+        System.out.println("  > " + temp + "\n\n");
+    }
+
+}
