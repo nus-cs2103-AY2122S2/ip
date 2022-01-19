3d561ff6080259f25d60aa1ffebb9eb881dcc26a
declanleeler <declanlee-ler@hotmail.com>
2022-01-19 21:22:21 +0800

    Add functionality to store user input
    Storing the input allows us to print all the items entered back so that the user is reminded of what they need. Using an arraylist is beneficial compared to an array as we are unsure of how long the users input will be.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,30 @@
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("Hello! I'm Duke \n" + "What do you need me to note down for you? Type it below!");
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
         while(true) {
             String input = sc.nextLine();
+
             if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("Bye. Hope your memory gets better!!");
                 break;
-            } else {
-                System.out.println(input);
+            }
+            else if (input.equals("list")) {
+                int count = 0;
+                while (count < list.size()) {
+                    int t = count + 1;
+                    System.out.println(t + ". " + list.get(count));
+                    count++;
+                }
+            }
+            else {
+                list.add(input);
+                System.out.println("NOTED DOWN: " + input);
             }
         }
     }
