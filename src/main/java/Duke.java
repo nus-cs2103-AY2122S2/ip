01f882fe3c3eae5d6de466d19b573851bbb8a983
joelwongjy <joelwongjy@gmail.com>
2022-01-19 17:07:22 +0800

    Add mark as done
    Add the ability to mark tasks as done.
    Add the ability to change the status back to not done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,42 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Duke {
-    public static List<String> tasks = new ArrayList<>();
+    public static List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hey there! I'm Mickey, your personal mouse assistant.\nWhat can I do for you today?\n");
+        System.out.println("Hey there! I'm Mickey, your Mouse assistant.\nWhat can I do for you today?\n");
         for (String in = sc.next(); !in.equals("bye"); in = sc.next()) {
-            if (in.equals("list")) {
-                System.out.println("\tOh boy! Here are your tasks:");
-
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("\t\t" + (i + 1) + ". " + tasks.get(i));
-                }
-            } else {
-                tasks.add(in);
-                System.out.println("\tadded: " + in);
+            switch (in) {
+                case "list":
+                    System.out.println("\tOh boy! Here are your tasks:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println("\t\t" + (i + 1) + ". " + tasks.get(i));
+                    }
+                    break;
+                case "mark":
+                    System.out.println("\tThat is swell! You have completed this task:");
+                    System.out.println("\t\t" + markAsDone(sc.nextInt()));
+                    break;
+                case "unmark":
+                    System.out.println("\tHot dog! Complete this soon:");
+                    System.out.println("\t\t" + markAsDone(sc.nextInt()));
+                    break;
+                default:
+                    Task newTask = new Task(in);
+                    tasks.add(newTask);
+                    System.out.println("\tAw, gee! New task: " + in);
+                    break;
             }
         }
         System.out.println("\tToodles! See ya real soon!");
+        tasks.clear();
+    }
+
+    public static Task markAsDone(int index) {
+        tasks.get(index - 1).markAsDone();
+        return tasks.get(index - 1);
     }
 }
