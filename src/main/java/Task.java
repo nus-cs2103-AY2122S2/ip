c6f5254dc6e4e45c3080f6d9dfe5fda3b94f29a1
Charisma Kausar <charisma.kausar@gmail.com>
2022-01-19 13:05:00 +0800

    Remove getDescription function

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -9,32 +9,28 @@ public class Task {
         this.isDone = false;
     }
 
-    public String getDescription() {
-        return this.description;
-    }
-
     public String getStatusIcon() {
         return this.isDone ? "[✓]" : "[ ]";
     }
 
     public static void markAsDone(int index) {
         taskList[index].isDone = true;
-        System.out.println(taskList[index].getStatusIcon() + " " + taskList[index].getDescription());
+        System.out.println(taskList[index].getStatusIcon() + " " + taskList[index].description);
     }
 
     public static void markAsNotDone(int index) {
         taskList[index].isDone = false;
-        System.out.println(taskList[index].getStatusIcon() + " " + taskList[index].getDescription());
+        System.out.println(taskList[index].getStatusIcon() + " " + taskList[index].description);
     }
 
     public static void addToList(Task task) {
         taskList[taskCount++] = task;
-        System.out.println(" added: " + task.getDescription());
+        System.out.println(" added: " + task.description);
     }
 
     public static void printTasks() {
         for (int i = 0; i < taskCount; i++) {
-            System.out.println(" " + (i + 1) + "." + taskList[i].getStatusIcon() + " " + taskList[i].getDescription());
+            System.out.println(" " + (i + 1) + "." + taskList[i].getStatusIcon() + " " + taskList[i].description);
         }
     }
 }
