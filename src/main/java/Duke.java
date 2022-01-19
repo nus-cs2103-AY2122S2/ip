cd0131fbdaa675ed2341f910498ea233a6f786f0
jaegarpoon <jaegarpoon@gmail.com>
2022-01-20 01:33:45 +0800

    Added Greet, Echo, Exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Ron\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        String input = sc.nextLine();
+        String bye = "bye";
+        while (!input.equals(bye)) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            input = sc.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
