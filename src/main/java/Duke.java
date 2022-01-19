b8c801b887e23415b232aa52a410abe0f26857d4
lchokhoe <chokhoeefps335f@gmail.com>
2022-01-19 13:38:03 +0800

    Renamed Duke to DukeLCH and changed default Greet response

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,28 @@
+import java.util.Scanner; // Imported Scanner class
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "_______________________________________________________\n"
-                + " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n"
-                + "Hello! I'm Duke\n"
-                + "What can I do for you?\n"
+                + " ____        _         _    ____ _   _ \n"
+                + "|  _ \\ _   _| | _____ | | /  ___| | | |\n"
+                + "| | | | | | | |/ / _ \\| | | |   | |_| |\n"
+                + "| |_| | |_| |   <  __/| |_| |___|  _  |\n"
+                + "|____/ \\__,_|_|\\_\\___||___|\\____|_| |_|\n"
+                + "Hello! I'm DukeLCH\n"
+                + "How can I be of service?\n"
                 + "_______________________________________________________\n";
         System.out.println(logo);
+        Commands cmd = new Commands();
+        Scanner io = new Scanner(System.in); // Scanner object created
+
+        while(true) {
+            String input = io.nextLine();
+            if (input.equals("bye")) {
+                cmd.bye();
+                break;
+            } else {
+                cmd.echoInput(input);
+            }
+        }
     }
 }
