8ef7b703c42c0f51a4eda2dd43b8fa7342782e00
TypeDefinition <lnxterry@gmail.com>
2022-01-19 22:50:08 +0800

    Finished Level-2.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -8,17 +9,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-
+        ArrayList<String> list = new ArrayList<String>();
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (input.toLowerCase().equals("bye")) {
-                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println("   " + input);
+
+            if (input.toLowerCase().equals("list")) {
+                for (int i = 0; i < list.size(); ++i) {
+                    System.out.println(String.format("%d. %s", i+1, list.get(i)));
+                }
+                continue;
+            }
+
+            list.add(input);
+            System.out.println("added: " + input);
         }
     }
 }
