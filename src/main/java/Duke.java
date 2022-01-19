30ec03b6b9a71e0c4753c8fb8440aacb1a9e86a4
aidanaden <aidan@DESKTOP-73NEEJC.localdomain>
2022-01-19 16:52:13 +0800

    added greet, echo, exit with base styling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.text.Format;
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    public static String FormatMsg(String msg) {
+        String startFormat = "####################\n";
+        String endFormat = "\n####################";
+        return startFormat + msg + endFormat;
+    }
+
+    public static void DisplayWelcomeMsg() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String welcomeMsg = "Hello! I'm Duke\n"
+                + "What can I do for you?";
+        System.out.println(FormatMsg(welcomeMsg));
+    }
+
+    public static void main(String[] args) {
+        DisplayWelcomeMsg();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                String exitMsg = FormatMsg("Bye. Hope to see you again soon!");
+                System.out.println(exitMsg);
+                return;
+            } else {
+                System.out.println(FormatMsg(command));
+            }
+        }
     }
 }
