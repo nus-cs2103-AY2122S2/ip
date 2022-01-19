bdb10880830310feefb6e141b8e9477f08402a82
cwnm <phuajungheng@gmail.com>
2022-01-20 00:22:42 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,54 @@
 import java.util.Scanner;
 import java.util.*;
+import myPackage.*;
 
 public class Duke {
     public static void main(String[] args) {
+        //Task c = new Task("HELLO");
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         int listCount = 0;
-        String[] list = new String[100];
+        Task[] list = new Task[100];
+        //String[] list = new String[100];
         while (true) {
             Scanner input = new Scanner(System.in);
             String userInput = input.nextLine();
-            if (userInput.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.exit(0);
-            } else if (userInput.equals("list")) {
-                for (int i = 0; i < listCount; i++) {
-                    System.out.println(String.format("%d. %s", i + 1, list[i]));
+            String[] userInputSplit = userInput.split(" ");
+            String keyword = userInputSplit[0];
+            int lenSplit = userInputSplit.length;
+            switch (keyword) {
+                case "bye": {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.exit(0);
+                    break;
                 }
-            } else {
-                System.out.println("added: " + userInput);
-                list[listCount] = userInput;
-                listCount++;
+                case "list": {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < listCount; i++) {
+                        System.out.printf("%d.[%s] %s%n", i + 1,list[i].getStatusIcon(), list[i].getDescription());
+                    }
+                    break;
+                }
+                case "mark": {
+                    if (lenSplit > 1) {
+                        list[Integer.parseInt(userInputSplit[1]) - 1].markAsDone();
+                    }
+                    break;
+                }
+                case "unmark": {
+                    if (lenSplit > 1) {
+                        list[Integer.parseInt(userInputSplit[1]) - 1].unmarkAsDone();
+                    }
+                    break;
+                }
+                default: {
+                    System.out.println("added: " + userInput);
+                    list[listCount] = new Task(userInput);
+                    listCount++;
+                }
             }
+
+
+
         }
 
     }
---
+++ src/main/java/myPackage/Task.java
@@ -0,0 +1,33 @@
+package myPackage;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.printf("[%s] %s%n", this.getStatusIcon(), this.description);
+    }
+
+    public void unmarkAsDone() {
+        this.isDone = false;
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.printf("[%s] %s%n", this.getStatusIcon(), this.description);
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
+    //Task t = new Task("read book");
+    //t.markAsDone();
