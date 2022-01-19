c9eb14cf81dd4f6cecfa4340d3b54ea27a324a17
HanJiyao <hanjiyao0@gmail.com>
2022-01-19 20:52:56 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -7,21 +9,40 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String line = "    ____________________________________________________________";
         System.out.println(logo);
-        System.out.println(line + "\n    Hello! I'm Duke by A0221330A.\n    What can I do for you?\n" + line);
+
+        System.out.println(output("Hello! I'm Duke by A0221330A.\n    What can I do for you?"));
+
+        List<Task> list = new ArrayList<>();
 
         boolean exit = false;
         while (!exit) {
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println(line + "\n    Bye. Hope to see you again soon!\n" + line);
+                System.out.println(output("Bye. Hope to see you again soon!"));
                 exit = true;
+            } else if (input.equals("list")) {
+                StringBuilder text = new StringBuilder();
+                for (int i=0; i<list.size();i++){
+                    if (i == 0) {
+                        text.append(i + 1).append(". ").append(list.get(i).getTask()).append("\n");
+                    } else if (i == list.size() - 1){
+                        text.append("    ").append(i + 1).append(". ").append(list.get(i).getTask());
+                    } else{
+                        text.append("    ").append(i + 1).append(". ").append(list.get(i).getTask()).append("\n");
+                    }
+                }
+                System.out.println(output(text.toString()));
             } else {
-                System.out.println(line + "\n    " + input + "\n" + line);
+                list.add(new Task(input));
+                System.out.println(output("added: " + input));
             }
         }
+    }
 
+    public static String output(String text) {
+        String line = "    ____________________________________________________________";
+        return line + "\n    " + text + "\n" + line;
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private final String task;
+
+    public Task(String task) {
+        this.task = task;
+    }
+
+    public String getTask() {
+        return task;
+    }
+}
