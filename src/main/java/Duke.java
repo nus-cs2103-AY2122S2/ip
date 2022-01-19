6feaa1baa8071569070a78b6605919727c9bb65e
whoisjunhong <weejunhong@gmail.com>
2022-01-19 14:47:40 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,28 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> arr = new ArrayList<>();
+
+        System.out.println("____________________________________________________________" + '\n'
+                + "Hello! I'm Duke" + '\n'
+                + "What can I do for you?" + '\n'
+                + "____________________________________________________________");
 
         while (sc.hasNext()) {
-            String curr = sc.next();
+            String curr = sc.nextLine();
 
-            if (curr.equals("bye")) {
+            if (curr.equals("list")) {
+                for (int i = 0; i < arr.size(); i++) {
+                    System.out.println(i + 1 + ". " + arr.get(i));
+                }
+            } else if (curr.equals("bye")) {
                 break;
             } else {
-                System.out.println("____________________________________________________________" + '\n'
-                        + curr + '\n'
-                        + "____________________________________________________________");
+                arr.add(curr);
+                System.out.println("added: " + curr);
             }
         }
         System.out.println("____________________________________________________________" + '\n'
