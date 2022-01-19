152a505a2d3ee1c14b30078317d9ef8cb45a14db
Spyobird <elliotlimzy@gmail.com>
2022-01-19 15:53:29 +0800

    Add Deadline.java; Add Event.java; Add Todo.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,22 @@
+public class Deadline extends Task {
+    private String time;
+    
+    public Deadline(String description, String time) {
+        super(description);
+        this.time = time;
+    }
+    
+    public String getTime() {
+        return this.time;
+    }
+    
+    @Override
+    public String getTaskIcon() {
+        return "[D]";
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + " (by: " + this.getTime() + ")";
+    }
+}
---
+++ src/main/java/Event.java
@@ -0,0 +1,21 @@
+public class Event extends Task {
+    private String time;
+    public Event(String description, String time) {
+        super(description);
+        this.time = time;
+    }
+    
+    public String getTime() {
+        return this.time;
+    }
+    
+    @Override
+    String getTaskIcon() {
+        return "[E]";
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + " (at: " + this.getTime() + ")";
+    }
+}
