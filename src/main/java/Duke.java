2204fecd5f008e8045c2c1677950418b7b0dcdc0
lchokhoe <chokhoeefps335f@gmail.com>
2022-01-19 13:05:01 +0800

    Greet added to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        String logo = "_______________________________________________________\n"
+                + " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "_______________________________________________________\n";
+        System.out.println(logo);
     }
 }
