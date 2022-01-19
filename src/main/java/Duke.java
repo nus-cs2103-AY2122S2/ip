458a361478c9594165b649566744129b31e1e801
DESKTOP-MDM8KAA\lx <bianyuqi142857@gmail.com>
2022-01-20 00:51:53 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "Hello! I'm Duke\n" + "What can I do for you?";
+        System.out.println(greeting);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = null;
+        String bye = "bye";
+        while(true) {
+            input = scanner.nextLine();
+            if(input.equals(bye)) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            }
+            System.out.println(input);
+        }
     }
 }
