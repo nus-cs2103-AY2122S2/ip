37a1f66cc186e963d4a464fb150b89f4ae352417
Vikrant Prakash <vikrantprakash2605@gmail.com>
2022-01-20 05:00:15 +0800

    Added list functionality

--- src/main/java/Johnny.java
+++ src/main/java/Johnny.java
@@ -4,13 +4,25 @@ public class Johnny {
         System.out.println("Hello! I'm Johnny \n" + "What can I do for you?");
         Scanner sc = new Scanner(System.in);
         String input;
+        String[] list = new String[100];
+        int count = 0;
         while(true) {
             input = sc.nextLine();
             if(input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(input);
+            else if(input.equals("list")) {
+                for(int i = 0; i < count; i++) {
+                    System.out.print(i + 1);
+                    System.out.println(". " + list[i]);
+                }
+            }
+            else {
+                System.out.println("added: " + input);
+                list[count] = input;
+                count++;
+            }
         }
     }
 }
