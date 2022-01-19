4fdc33780771d3a070c6f8272f42c126715c0002
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-01-19 18:17:09 +0800

    feat: added greet, botResponse, and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
 
-    private final static String divPadding = "    ";
+//    private final static String divPadding = "    ";
     private final static String strPadding = "      ";
+    private static boolean exitFlag = false;
 
     public static void main(String[] args) {
-        initialiseBot();
+        Scanner sc = new Scanner(System.in);
+
+        greet();
+
+        while(!exitFlag) {
+            echoInput(sc.nextLine());
+        }
+
+        sc.close();
     }
 
-    private static void initialiseBot() {
-        String divString = "-----------------------------------------";
+    private static void greet() {
         String botName = "Baymax";
+        String greeting = "Greetings, I am " + botName + ".\n" +
+                strPadding + "What can I do you for?";
 
-        System.out.println(divPadding + divString);
-        System.out.println(strPadding + "Greetings, I am " + botName + ".");
-        System.out.println(strPadding + "What can I do you for?");
-        System.out.println(divPadding + divString);
+        botResponse(greeting);
+    }
 
+    private static void botResponse(String resString) {
+        String divString = "    -----------------------------------------";
+
+        System.out.println(divString);
+        System.out.println(strPadding + resString);
+        System.out.println(divString);
+    }
+
+    private static void echoInput(String input) {
+        if (input.equals("exit")) {
+            botResponse("Pleasure to be of service, see you soon!");
+            exitFlag = true;
+        } else {
+            botResponse(input);
+        }
     }
 }
