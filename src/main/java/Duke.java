52b4125451ebc056fca97c4ce7bc53175eae8d05
medajet <jettaneekiat@gmail.com>
2022-01-19 22:11:03 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LOGO =
+            "   ___      _  ______       _   \n" +
+                    "  |_  |    | | | ___ \\     | |  \n" +
+                    "    | | ___| |_| |_/ / ___ | |_ \n" +
+                    "    | |/ _ \\ __| ___ \\/ _ \\| __|\n" +
+                    "/\\__/ /  __/ |_| |_/ / (_) | |_ \n" +
+                    "\\____/ \\___|\\__\\____/ \\___/ \\__|\n";
+    private static final String BORDER = "________________________________\n";
+
+    /**
+     * Greets the user.
+     */
+    private static void greet() {
+        System.out.println(BORDER + LOGO + BORDER);
+        System.out.println("How may I help you today? Please enter your command:");
+    }
+
+    /**
+     * Exits the program.
+     */
+    private static void exit() {
+        System.out.println(BORDER + "Bye!\n" + BORDER);
+        System.exit(0);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        Scanner s = new Scanner(System.in);
+        boolean end = false;
+        while (!end) {
+            String input = s.nextLine().toLowerCase();
+            if (input.equals("bye")) {
+                end = true;
+                exit();
+            } else {
+                System.out.println(BORDER + input + "\n" + BORDER);
+            }
+        }
     }
 }
