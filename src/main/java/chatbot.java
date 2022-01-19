9639f2b3b4cfe273f47edc90e11f0931f2e2f4d6
Halpfrog <62001923+Halpfrog@users.noreply.github.com>
2022-01-20 00:14:10 +0800

    Level 2

--- src/main/java/chatbot.java
+++ src/main/java/chatbot.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 public class chatbot {
     public static void main(String[] args) {
         String intro = "Hello! I'm Duke \n" +
                     "What can I do for you? \n";
     System.out.println(intro);
     
+    List<String> ls = new ArrayList<>();
     while(true) {
         Scanner sc = new Scanner(System.in);
         String n = sc.next();
         if(n.compareToIgnoreCase("bye") == 0) {
             break;
+        } else if (n.compareToIgnoreCase("list") != 0) {
+            System.out.println("added: " + n);
+            ls.add(n);
         } else {
-            System.out.println(n);
+            for(int i = 1; i <= ls.size(); i++) {
+                System.out.println(i + " " + ls.get(i -1));
+            }
         }
     } 
     System.out.println("Bye. Hope to see you again");
