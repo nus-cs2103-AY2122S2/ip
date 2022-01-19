5998c315da3b44c5b70c77aec95a8a9b3673616a
simjunheng <sjh_98@hotmail.com>
2022-01-19 19:51:51 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,7 +12,7 @@ public class Duke {
     /**
      * The main method of this program
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // Greet
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -20,60 +20,97 @@ public class Duke {
         // Scanner Object
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-
-        // Add different types of Task, List, Mark
-        while (!input.equals("bye")) {
-            int num = 1;
-            String[] temp = input.split(" ");
-            if (temp[0].equals("mark")) {
-                int index = Integer.parseInt(temp[1]) - 1;
-                Task task = list.get(index);
-                task.setAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(task.getTask());
-            } else if (temp[0].equals("unmark")) {
-                int index = Integer.parseInt(temp[1]) - 1;
-                Task task = list.get(index);
-                task.setAsNotDone();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(task.getTask());
-            } else if (temp[0].equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (Task task : list) {
-                    System.out.println(num + "." + task.getTask());
-                    num++;
-                }
-            } else {
-                if (temp[0].equals("todo")) {
-                    Task newTask = new ToDo(Duke.rmType(input));
-                    list.add(newTask);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(newTask.getTask());
-                    System.out.println("Now you have "+ list.size() +" tasks in the list.");
+        try {
+            // Add different types of Task, List, Mark
+            while (!input.equals("bye")) {
+                String[] temp = input.split(" ");
+                if (temp[0].equals("mark")) {
+                    int index = Integer.parseInt(temp[1]) - 1;
+                    Duke.markTask(index);
+                } else if (temp[0].equals("unmark")) {
+                    int index = Integer.parseInt(temp[1]) - 1;
+                    Duke.unMarkTask(index);
+                } else if (temp[0].equals("list")) {
+                    Duke.list();
+                } else if (temp[0].equals("todo")) {
+                    Duke.addToDo(input);
                 } else if (temp[0].equals("deadline")) {
-                    String adjInput = Duke.rmType(input);
-                    Task newTask = new Deadline(Duke.extractDateORTask(adjInput, 2),
-                            Duke.extractDateORTask(adjInput, 1));
-                    list.add(newTask);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(newTask.getTask());
-                    System.out.println("Now you have "+ list.size() +" tasks in the list.");
+                    Duke.addDeadline(input);
                 } else if (temp[0].equals("event")) {
-                    String adjInput = Duke.rmType(input);
-                    Task newTask = new Event(Duke.extractDateORTask(adjInput, 2),
-                            Duke.extractDateORTask(adjInput, 1));
-                    list.add(newTask);
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(newTask.getTask());
-                    System.out.println("Now you have "+ list.size() +" tasks in the list.");
+                    Duke.addEvent(input);
                 } else {
-                    System.out.println("Invalid input. Please try again.");
+                    Duke.notSpecified();
                 }
+                input = scanner.nextLine();
             }
-            input = scanner.nextLine();
+            //Exit
+            System.out.println("Bye. Hope to see you again soon!");
+        } catch (DukeException ex) {
+            System.out.println(ex.getMessage());
         }
-        //Exit
-        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void markTask(int index) throws DukeException {
+        if (index >= list.size()) {
+            throw new DukeException("☹ OOPS!!! I'm sorry, can't find task :-(");
+        } else {
+            Task task = list.get(index);
+            task.setAsDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(task.getTask());
+        }
+    }
+
+    public static void unMarkTask(int index) throws DukeException {
+        if (index >= list.size()) {
+            throw new DukeException("☹ OOPS!!! I'm sorry, can't find task :-(");
+        } else {
+            Task task = list.get(index);
+            task.setAsNotDone();
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println(task.getTask());
+        }
+    }
+
+    public static void list() {
+        int num = 1;
+        System.out.println("Here are the tasks in your list:");
+        for (Task task : list) {
+            System.out.println(num + "." + task.getTask());
+            num++;
+        }
+    }
+
+    public static void addToDo(String input) throws DukeException {
+        Task newTask = new ToDo(Duke.rmType(input));
+        list.add(newTask);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(newTask.getTask());
+        System.out.println("Now you have "+ list.size() +" tasks in the list.");
+    }
+
+    public static void addDeadline(String input) throws DukeException {
+        String adjInput = Duke.rmType(input);
+        Task newTask = new Deadline(Duke.extractDateORTask(adjInput, 2),
+                Duke.extractDateORTask(adjInput, 1));
+        list.add(newTask);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(newTask.getTask());
+        System.out.println("Now you have "+ list.size() +" tasks in the list.");
+    }
+
+    public static void addEvent(String input) throws DukeException {
+        String adjInput = Duke.rmType(input);
+        Task newTask = new Event(Duke.extractDateORTask(adjInput, 2),
+                Duke.extractDateORTask(adjInput, 1));
+        list.add(newTask);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(newTask.getTask());
+        System.out.println("Now you have "+ list.size() +" tasks in the list.");
+    }
+
+    public static void notSpecified() throws DukeException {
+        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
     }
 
     /**
@@ -82,8 +119,11 @@ public class Duke {
      * @param input string given by the user
      * @return remaining string after task type is removed.
      */
-    public static String rmType(String input) {
+    public static String rmType(String input) throws DukeException {
         String[] temp = input.split(" ");
+        if (temp.length == 1) {
+            throw new DukeException("☹ OOPS!!! The description of a " +  temp[0] +  " cannot be empty.");
+        }
         String[] newTemp = new String[temp.length - 1];
         for (int i = 1; i < temp.length; i++) {
             newTemp[i - 1] = temp[i];
@@ -101,8 +141,11 @@ public class Duke {
      * @param input string given by the user
      * @return remaining string after task type is removed.
      */
-    public static String extractDateORTask(String input, int integer) {
+    public static String extractDateORTask(String input, int integer) throws DukeException {
         String[] temp = input.split(" /");
+        if (temp.length == 1) {
+            throw new DukeException("☹ OOPS!!! Remember to specify your date");
+        }
         if (integer == 1) {
             return temp[1];
         } else {
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,11 @@
+/**
+ * This class supports exception belonging to the Duke program.
+ * @author Sim Jun Heng
+ * @version CS2103T AY21/22 Sem 2
+ */
+
+public class DukeException extends Exception {
+  public DukeException(String message) {
+    super(message);
+  }
+}
