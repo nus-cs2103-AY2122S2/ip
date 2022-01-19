24c6d0ae8961278ec8f9b9db33d4d6fbafdb15d1
cwq2326 <cwq2326@gmail.com>
2022-01-19 23:54:38 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,9 @@ public class Duke {
     static Scanner scanner = new Scanner(System.in);
     static final String welcome_message = "Hello! I'm Duke\n" +
             "What can I do for you?";
+    static final int SIZE = 100;
+    static String[] taskList = new String[SIZE];
+    static int pointer = 0;
 
     public static void main(String[] args) {
 
@@ -14,11 +17,23 @@ public class Duke {
             String input = scanner.nextLine();
 
             switch (input) {
+                case "list":
+                    if (pointer == 0) {
+                        System.out.println("You have not yet added any tasks!");
+                        break;
+                    }
+                    else {
+                        for (int i = 0; i < pointer; i++) {
+                            System.out.println(i + 1 + ". " + taskList[i]);
+                        }
+                    }
+                    break;
                 case "bye":
                     System.out.println("Bye. Hope to see you again soon!");
                     System.exit(0);
                 default:
-                    System.out.println(input);
+                    taskList[pointer++] = input;
+                    System.out.println("added: " + input);
             }
         }
     }
