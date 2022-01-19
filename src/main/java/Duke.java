dcee4f39419839d56d58ef7accb1f4289475fe2c
April Chan <chanyijuan@gmail.com>
2022-01-20 04:46:23 +0800

    Level-1: Implemented with while-loop and Scanner class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.lang.*;
+import java.util.*;
+
 public class Duke {
+    static String indent = "    ";
+    static String line = "    ____________________________________________________________";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(line);
+        System.out.println(indent+"Hello! I'm Duke\n"+
+                indent+
+                "What can I do for you?");
+        System.out.println(line);
+
+        Scanner in = new Scanner(System.in);
+        String cmd = in.nextLine();
+        while (!cmd.equals("bye")) {
+            System.out.println(line);
+            System.out.println(indent+cmd);
+            System.out.println(line);
+            cmd = in.nextLine();
+        }
+
+        System.out.println(line);
+        System.out.println(indent+"Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
