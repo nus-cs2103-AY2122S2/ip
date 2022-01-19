2714517ab405bb507699b763575c6b59ed01be8b
teominghuang <bennyteomhh@hotmail.com>
2022-01-19 20:51:00 +0800

    Level 1 - Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,16 +9,16 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
 
-        String hyphenate = "____________________________________________________________";
-        System.out.println(hyphenate + "\nHello! I'm Duke\nWhat can I do for you?\n" + hyphenate);
+        String hyphenate = "    ____________________________________________________________";
+        System.out.println(hyphenate + "\n    Hello! I'm DockerHawker\n    I am your personal assistance! what can I do for you?\n" + hyphenate);
 
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
         while (!command.equals("bye")){
-            System.out.println(hyphenate + "\n" + command + "\n" + hyphenate);
+            System.out.println(hyphenate + "\n    " + command + "\n" + hyphenate);
             command = sc.nextLine();
         }
-        
+        System.out.println(hyphenate + "\n    Bye. Let's play some video games next time!\n" + hyphenate);
         sc.close();
     }
 }
