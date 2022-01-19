b1c7b3949a4e2d1644a8ba8162a302f5c4f3cbcc
honganhcs <e0560140@u.nus.edu>
2022-01-19 19:02:55 +0800

    1. Added the ability to delete tasks from the list. 2. Updated the test case to test for the delete function.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,11 +10,9 @@ public class Duke {
     private class Item {
         private String content;
         private Boolean done;
-        private int index;
-        private Item(String content, int index) {
+        private Item(String content) {
             this.content = content;
             this.done = false;
-            this.index = index;
         }
         private void mark() {
             this.done = true;
@@ -24,53 +22,44 @@ public class Duke {
         }
         @Override
         public String toString() {
-            return String.valueOf(this.index) + ". " + this.noIndexToString();
-        }
-        public String noIndexToString() {
             return "[T]" + (this.done? "[X] " : "[ ] ") + this.content;
         }
     }
     private class Deadline extends Item{
         private String deadline;
 
-        private Deadline(String content, int index) {
-            super(content, index);
+        private Deadline(String content) {
+            super(content);
         }
-        private Deadline(String content, int index, String deadline) {
-            super(content, index);
+        private Deadline(String content, String deadline) {
+            super(content);
             this.deadline = deadline;
         }
         private void addDeadline(String deadline) {
             this.deadline = deadline;
         }
+
         @Override
         public String toString() {
-            return String.valueOf(super.index) + ". " + this.noIndexToString();
-        }
-        @Override
-        public String noIndexToString() {
             return "[D]" + (super.done? "[X] " : "[ ] ") + super.content + " (by " + this.deadline + ")";
         }
     }
     private class Event extends Item {
         private String time;
 
-        private Event(String content, int index) {
-            super(content, index);
+        private Event(String content) {
+            super(content);
         }
-        private Event(String content, int index, String time) {
-            super(content, index);
+        private Event(String content, String time) {
+            super(content);
             this.time = time;
         }
         private void addTime(String time) {
             this.time = time;
         }
+
         @Override
         public String toString() {
-            return String.valueOf(super.index) + ". " + this.noIndexToString();
-        }
-        @Override
-        public String noIndexToString() {
             return "[E]" + (super.done? "[X] " : "[ ] ") + super.content + " (at " + this.time + ")";
         }
     }
@@ -103,10 +92,10 @@ public class Duke {
             System.out.println("Oops! Please add a description for the todo!");
         } else {
             this.numItems++;
-            Item item = new Item (input.substring(5), this.numItems);
+            Item item = new Item (input.substring(5));
             this.list.add(item);
             System.out.println("Alright! I've added this task:");
-            System.out.println(item.noIndexToString());
+            System.out.println(item.toString());
             System.out.println(numItems == 1 ? "Now there is 1 task in your list."
                     :"Now there are " + numItems + " tasks in your list.");
         }
@@ -127,10 +116,10 @@ public class Duke {
                 System.out.println("Oops! Please add a deadline for the deadline!");
             } else {
                 this.numItems++;
-                Item item = new Deadline(components[0], this.numItems, components[1]);
+                Item item = new Deadline(components[0], components[1]);
                 this.list.add(item);
                 System.out.println("Alright! I've added this task:");
-                System.out.println(item.noIndexToString());
+                System.out.println(item.toString());
                 System.out.println(numItems == 1 ? "Now there is 1 task in your list."
                         :"Now there are " + numItems + " tasks in your list.");
             }
@@ -152,45 +141,13 @@ public class Duke {
                 System.out.println("Oops! Please add a time for the event!");
             } else {
                 this.numItems++;
-                Item item = new Event(components[0], this.numItems, components[1]);
+                Item item = new Event(components[0], components[1]);
                 this.list.add(item);
                 System.out.println("Alright! I've added this task:");
-                System.out.println(item.noIndexToString());
+                System.out.println(item.toString());
                 System.out.println(numItems == 1 ? "Now there is 1 task in your list."
                         : "Now there are " + numItems + " tasks in your list.");
             }
-        }
-    }
-
-    private void mark(String index) {
-        try {
-            int i = Integer.parseInt(index);
-            if (i == 0 || i > numItems) {
-                System.out.println("Oops! Please enter a number between 1 and " + numItems +"!");
-            } else {
-                Item item = this.list.get(i - 1);
-                item.mark();
-                System.out.println("Alright! I've marked this task as done:");
-                System.out.println(item.noIndexToString());
-            }
-        } catch (NumberFormatException nfe) {
-            System.out.println("Oops! Please enter a valid task number!");
-        }
-    }
-
-    private void unmark(String index) {
-        try {
-            int i = Integer.parseInt(index);
-            if (i == 0 || i > numItems) {
-                System.out.println("Oops! Please enter a number between 1 and " + numItems + "!");
-            } else {
-                Item item = this.list.get(i - 1);
-                item.unmark();
-                System.out.println("Alright! I've marked this task as not done yet:");
-                System.out.println(item.noIndexToString());
-            }
-        } catch (NumberFormatException nfe) {
-            System.out.println("Oops! Please enter a valid task number!");
         }
     }
 
@@ -199,8 +156,84 @@ public class Duke {
             System.out.println("Currently, you have no tasks :)");
         } else {
             System.out.println("Here are the tasks in your list:");
-            for (Item i : this.list) {
-                System.out.println(i.toString());
+            int i = 1;
+            for (Item item : this.list) {
+                System.out.println(i + ". " + item.toString());
+                i++;
+            }
+        }
+    }
+
+    private void mark(String index) {
+        if(index.isBlank()) {
+            System.out.println("Oops! Please use the following format:");
+            System.out.println("mark [task number]");
+        } else if (numItems == 0) {
+            System.out.println("Oops! Currently, you have no tasks :)");
+        } else {
+            try {
+                int i = Integer.parseInt(index.substring(1));
+                if (i <= 0 || i > numItems) {
+                    System.out.println("Oops! Please enter a number between 1 and " + numItems + "!");
+                } else {
+                    Item item = this.list.get(i - 1);
+                    item.mark();
+                    System.out.println("Alright! I've marked this task as done:");
+                    System.out.println(item.toString());
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("Oops! Please use the following format:");
+                System.out.println("mark [task number]");
+            }
+        }
+    }
+
+    private void unmark(String index) {
+        if(index.isBlank()) {
+            System.out.println("Oops! Please use the following format:");
+            System.out.println("unmark [task number]");
+        } else if (numItems == 0) {
+            System.out.println("Oops! Currently, you have no tasks :)");
+        } else {
+            try {
+                int i = Integer.parseInt(index.substring(1));
+                if (i <= 0 || i > numItems) {
+                    System.out.println("Oops! Please enter a number between 1 and " + numItems + "!");
+                } else {
+                    Item item = this.list.get(i - 1);
+                    item.unmark();
+                    System.out.println("Alright! I've marked this task as not done yet:");
+                    System.out.println(item.toString());
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("Oops! Please use the following format:");
+                System.out.println("unmark [task number]");
+            }
+        }
+    }
+
+    private void delete(String index) {
+        if(index.isBlank()) {
+            System.out.println("Oops! Please use the following format:");
+            System.out.println("delete [task number]");
+        } else if (numItems == 0) {
+            System.out.println("Oops! Currently, you have no tasks :)");
+        } else {
+            try {
+                int i = Integer.parseInt(index.substring(1));
+                if (i <= 0 || i > numItems) {
+                    System.out.println("Oops! Please enter a number between 1 and " + numItems + "!");
+                } else {
+                    Item item = this.list.get(i - 1);
+                    this.list.remove(i - 1);
+                    this.numItems--;
+                    System.out.println("Alright! I've removed this task:");
+                    System.out.println(item.toString());
+                    System.out.println("Now you have " + this.numItems + " tasks in your list.");
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("Oops! Please use the following format:");
+                System.out.println("delete [task number]");
             }
         }
     }
@@ -214,11 +247,13 @@ public class Duke {
         } else if (input.length() < 4) {
             this.addItem(input);
         } else if(input.substring(0,4).toLowerCase().equals("mark")) {
-            this.mark(input.substring(5));
+            this.mark(input.substring(4));
         } else if(input.length() < 6) {
             this.addItem(input);
         } else if(input.substring(0,6).toLowerCase().equals("unmark")) {
-            this.unmark(input.substring(7));
+            this.unmark(input.substring(6));
+        } else if(input.substring(0,6).toLowerCase().equals("delete")) {
+            this.delete(input.substring(6));
         } else {
             this.addItem(input);
         }
