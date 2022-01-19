5f4548c0706a5713a5777a452ee340bdb50a6f8e
joelwongjy <joelwongjy@gmail.com>
2022-01-19 16:47:12 +0800

    Add add and list
    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Duke {
+    public static List<String> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hey there! I'm Mickey, your personal mouse assistant.\nWhat can I do for you today?\n");
         for (String in = sc.next(); !in.equals("bye"); in = sc.next()) {
-                System.out.println("\t" + in);
+            if (in.equals("list")) {
+                System.out.println("\tOh boy! Here are your tasks:");
+
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("\t\t" + (i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(in);
+                System.out.println("\tadded: " + in);
+            }
         }
         System.out.println("\tToodles! See ya real soon!");
     }
