9709c35e4152df31adefd41dc6829cc499305ba0
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-01-19 21:49:22 +0800

    feat: added display list, changed echo function to add to list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
 
+
+    private static <T> boolean sizeCheck(List<T> inputList) {
+        return inputList.size() < 3;
+    }
+
     private static void run() {
         Scanner sc = new Scanner(System.in);
         boolean exitFlag = false;
+        List<String> strList = new ArrayList<>();
 
         while(!exitFlag) {
             String input = sc.nextLine();
-            if (input.equals("exit")) {
-                botResponse("Pleasure to be of service, see you soon!");
-                exitFlag = true;
-            } else {
-                botResponse(input);
+
+            switch (input) {
+                case "exit": botResponse("Pleasure to be of service, see you soon!");
+                             exitFlag = true;
+                             break;
+                case "list": botResponse(strList);
+                             break;
+                default:
+                    if (sizeCheck(strList)) {
+                        strList.add(input);
+                        botResponse("added: " + input);
+                    } else {
+                        botResponse("List is full, sorry! :(");
+                    }
             }
         }
         sc.close();
+    }
+
+    private static <T> void botResponse(List<T> inputList) {
+        String divString = "    ---------------------------------------------";
+        String strPadding = "      ";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < inputList.size(); i++) {
+            sb.append(String.format(strPadding + "%02d." + inputList.get(i), i + 1));
+            if (i != inputList.size() - 1) { sb.append("\n"); }
+
+        }
+        System.out.println(divString);
+        System.out.println(sb.toString());
+        System.out.println(divString);
     }
 
     private static void botResponse(String resString) {
