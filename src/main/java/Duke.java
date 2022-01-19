79c287fde30019215428bbd617bda6162188dfae
cashewnade <e0559400@u.nus.edu>
2022-01-19 17:56:05 +0800

    Level-2
    Completed Level-2. Changed the greeting and farewell message, as well as renamed the chatbot.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    private static final String GREETING_MESSAGE = "Hey, Duke here! How can I help you today?";
-    private static final String FAREWELL_MESSAGE = "Adios! Take care and see you soon!";
+    private static final String GREETING_MESSAGE = "Hey, Aeromon here! Fancy a cup of tea?";
+    private static final String FAREWELL_MESSAGE = "Buai Buai! Ciao for now!";
+    private static final String ADD_MESSAGE = "Nicely! I've added for you: ";
+    private static final List<String> toDoList = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println(GREETING_MESSAGE);
 
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
+        while (sc.hasNextLine()) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println(FAREWELL_MESSAGE);
                 break;
+            } else if (input.equals("list")) {
+                int count = 1;
+                for (String item : toDoList) {
+                    System.out.println(String.format("%d: %s", count, item));
+                    count++;
+                }
+            } else {
+                toDoList.add(input);
+                System.out.println(ADD_MESSAGE + input);
             }
-            System.out.println(input);
         }
     }
 }
