1a608cb90f2fbeb0be023a0806717a51d4ad5805
Seah Zhi Xuan <e0659847@u.nus.edu>
2022-01-19 18:21:38 +0800

    Duke Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Duke {
+    static String indent = "    ";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+
+        hLineBreak();
+        printlnWithIndent("Hello! I'm YourBoss.\n" +
+                indent +
+                "What can you do for me?");
+        hLineBreak();
+
+        String userInput = "";
+
+        while(userInput != "bye" || userInput != "Bye") {
+            userInput = scanner.nextLine();
+            hLineBreak();
+            printlnWithIndent(userInput);
+            hLineBreak();
+        }
+
+        hLineBreak();
+        printlnWithIndent("Bye. Hope I never see you again!");
+        hLineBreak();
+
+        scanner.close();
+    }
+
+    static void hLineBreak() {
+        System.out.println(indent+"____________________________________________________________");
+    }
+
+    static void printlnWithIndent(String input) {
+        System.out.print(indent);
+        System.out.println(input);
     }
 }
