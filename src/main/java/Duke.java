e6ec3b8e001562307377fb900377d1a150717f2d
cwnm <phuajungheng@gmail.com>
2022-01-19 17:06:54 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
+        int listCount = 0;
+        String[] list = new String[100];
         while (true) {
             Scanner input = new Scanner(System.in);
-            String s = input.next();
-            if (s.equals("bye")) {
+            String userInput = input.nextLine();
+            if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.exit(0);
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < listCount; i++) {
+                    System.out.println(String.format("%d. %s", i + 1, list[i]));
+                }
+            } else {
+                System.out.println("added: " + userInput);
+                list[listCount] = userInput;
+                listCount++;
             }
-            System.out.println(s);
+            System.out.println(userInput);
         }
 
     }
