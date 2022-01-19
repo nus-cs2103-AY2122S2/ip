38269b8bcf0bd7dd97ac515df97307f8c9bcb175
Keith <e0544450@u.nus.edu>
2022-01-19 17:44:09 +0800

    Level-1 commit, greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String lines = "____________________________________________________________";
+
+        System.out.println(lines);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        System.out.println(lines);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(lines + "\nBye. Hope to see you again soon!\n" + lines);
+                break;
+            }
+            else {
+                System.out.println(lines + "\n" + input + "\n" + lines);
+            }
+        }
     }
 }
