b322f0091cd90236cede2c9f434095c010816d7d
Shem Maleriado <shem.maleriado@gmail.com>
2022-01-19 20:12:30 +0800

    feat: todos, events, deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,16 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
-
 
 public class Duke {
     private static ArrayList<Task> tasks = new ArrayList<>();
+
+    private static String isPlural(int n) {
+        if (n > 1) {
+            return "s ";
+        }
+        return " ";
+    }
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -23,61 +30,66 @@ public class Duke {
         while (!command.equals("bye")) {
             if (command.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
+
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.printf("%d.[%s] %s%n", i + 1, tasks.get(i).getStatusIcon(), tasks.get(i).description);
+                    System.out.printf("%d.%s%n", i + 1, tasks.get(i));
                 }
-            } else if (isMarkCommand(command)) {
-                String[] commandArr = command.split(" ", 2);
-
-                Task task = tasks.get(Integer.parseInt(commandArr[1]) - 1);
-                task.markAsDone();
-
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.printf("[%s] %s%n", task.getStatusIcon(), task.description);
-            } else if (isUnmarkCommand(command)) {
-                String[] commandArr = command.split(" ", 2);
-
-                Task task = tasks.get(Integer.parseInt(commandArr[1]) - 1);
-                task.markAsNotDone();
-
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.printf("[%s] %s%n", task.getStatusIcon(), task.description);
             } else {
-                tasks.add(new Task(command));
-                System.out.println("added: " + command);
+                String[] commandArr = command.split(" ");
+                String type = commandArr[0];
+
+                if (type.equals("mark")) {
+                    Task task = tasks.get(Integer.parseInt(commandArr[1]) - 1);
+                    task.markAsDone();
+
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(task);
+                } else if (type.equals("unmark")) {
+                    Task task = tasks.get(Integer.parseInt(commandArr[1]) - 1);
+                    task.markAsNotDone();
+
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(task);
+                } else {
+                    String[] descriptionArr = Arrays.copyOfRange(commandArr, 1, commandArr.length);
+                    String description = String.join(" ", descriptionArr);
+
+                    if (type.equals("todo")) {
+                        Task todo = new ToDo(description);
+                        tasks.add(todo);
+
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(todo);
+                    } else if (type.equals("event")) {
+                        String[] eventArr = description.split(" /at ");
+                        String name = eventArr[0];
+                        String time = eventArr[1];
+
+                        Task event = new Event(name, time);
+                        tasks.add(event);
+
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(event);
+                    } else if (type.equals("deadline")) {
+                        String[] deadlineArr = description.split(" /by ");
+                        String name = deadlineArr[0];
+                        String time = deadlineArr[1];
+
+                        Task deadline = new Deadline(name, time);
+                        tasks.add(deadline);
+
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println(deadline);
+                    } else {
+                        tasks.add(new Task(command));
+                        System.out.println("Added: " + command);
+                    }
+                }
+                System.out.println("Now you have " + tasks.size() + " task" + isPlural(tasks.size()) + "in the list.");
             }
             command = sc.nextLine();
         }
-
         sc.close();
         System.out.println("Bye. Hope to see you again soon!");
-    }
-
-    public static boolean isMarkCommand(String command) {
-        String[] commandArr = command.split(" ", 2);
-        if (commandArr.length < 2) {
-            return false;
-        }
-        String description = commandArr[0];
-        String digit = commandArr[1];
-        if (digit.matches("\\d+")) {
-            int index = Integer.parseInt(digit);
-            return description.equals("mark") && index > 0 && index <= tasks.size();
-        }
-        return false;
-    }
-
-    public static boolean isUnmarkCommand(String command) {
-        String[] commandArr = command.split(" ", 2);
-        if (commandArr.length < 2) {
-            return false;
-        }
-        String description = commandArr[0];
-        String digit = commandArr[1];
-        if (digit.matches("\\d+")) {
-            int index = Integer.parseInt(digit);
-            return description.equals("unmark") && index > 0 && index <= tasks.size();
-        }
-        return false;
     }
 }
