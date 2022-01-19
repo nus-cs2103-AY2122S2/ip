d18692d74a866da7d94810e96dcfc13b1b196e26
onefangg <onefangg@gmail.com>
2022-01-19 15:03:00 +0800

    Implemented validation checks and rudimentary exception handling on input params from cli

--- src/main/java/Karen.java
+++ src/main/java/Karen.java
@@ -30,21 +30,79 @@ public class Karen {
                 item.toString(), this.tasksLength);
     }
 
+    public boolean validateCommand(String command){
+        boolean flag = false;
+        try {
+            flag = this.validateHelper(command);
+        } catch (KarenException err) {
+            this.echo(err.toString());
+        }
+        return flag;
+    }
+
+    protected boolean validateHelper(String command) throws KarenException {
+        System.out.println(command);
+        if (command.equals("list") | command.equals("bye")) {
+            return true;
+        }
+        else if (command.matches("(un)?mark.*")) {
+            if (command.matches("(un)?mark \\d+")) {
+                return true;
+            }
+            throw new KarenException("You can only mark/unmark with an integer index");
+        }
+        else if (command.matches("todo.*")) {
+            if (command.matches("todo (.*)")) {
+                return true;
+            } else {
+                throw new KarenException("I can't understand what todo you want to add.");
+            }
+        }
+        else if (command.matches("deadline.*")) {
+            if (command.matches("deadline (.*) \\/by (.*)")) {
+                return true;
+            }
+            else if (command.matches("^((?!\\/by).)*$")) {
+                throw new KarenException("You're missing an /by flag needed to add an deadline");
+            }
+            else {
+                throw new KarenException("I can't understand what deadline you want to add.");
+            }
+        }
+        else if (command.matches("event.*")) {
+            if (command.matches("event (.*) \\/at (.*)")) {
+                return true;
+            }
+            else if (command.matches("^((?!\\/at).)*$")) {
+                throw new KarenException("You're missing an /at flag needed to add an event");
+            }
+            else {
+                throw new KarenException("I can't understand what event you want to add.");
+            }
+        }
+        throw new KarenException("I don't understand anything - I want to speak with your manager");
+    }
+
     public void processCommand(String command) {
         String output = command;
-        // commands with extra functionality
+        if (!validateCommand(command)) {
+            return;
+        }
+
+        // commands that don't depend on user input/extra params
         if (command.equals("bye")) {
             output = "Goodbye - I'll be seeing your manager's manager next.";
             this.echo(output);
             System.exit(0);
         }
         else if (command.equals("list")) {
-            if (this.tasksLength==0) {
-                output = "";
-            } else {
+            if (this.tasksLength==0)
+            {
+                output = "Nothing is even added yet.";
+            } else
+            {
                 int counter = 0;
                 output = "";
-
                 for (Task item: this.toDos) {
                     output = output.concat(String.format("%d.%s\n", counter+1, item.toString()));
                     counter++;
@@ -55,10 +113,10 @@ public class Karen {
             }
 
         }
-        else if (command.matches("(un)?mark \\d+")) {
+        //  commands that require params
+        else if (command.matches("(un)?mark .*")) {
             int index = Integer.valueOf(command.split(" ")[1]);
             Task getTask = this.toDos[index-1];
-
             if (command.startsWith("mark")) {
                 getTask.markDone();
                 output = String.format("This task is finally done:\n  %s",getTask.toString());
@@ -67,8 +125,7 @@ public class Karen {
                 output = String.format("This task is now incomplete - unacceptable:\n  %s",getTask.toString());
             }
         }
-        else if (command.matches("todo (.*)"))
-        {
+        else if (command.matches("todo (.*)"))  {
             String item_descriptor = command.split(" ", 2)[1];
             ToDo item = new ToDo(item_descriptor);
             this.addTask(item);
@@ -89,8 +146,6 @@ public class Karen {
             Event item = new Event(m.group(1), m.group(2));
             this.addTask(item);
             output = this.formatAddTask(item);
-        } else {
-            output = String.format("You gave me nothing I understood. Check again: [%s]", command);
         }
         this.echo(output);
     }
@@ -101,7 +156,6 @@ public class Karen {
 
         // greet
         manager.echo("Hello, my name is Karen.\nI'll be speaking (to) as your manager today.");
-
         while (true) {
             // take in input as commands
             String cmd = scanner.nextLine();
---
+++ src/main/java/KarenException.java
@@ -0,0 +1,13 @@
+public class KarenException extends Exception {
+    String msg;
+
+    public KarenException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return this.msg;
+    }
+}
