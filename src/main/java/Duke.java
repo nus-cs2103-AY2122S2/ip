487213cece3e4879e7a973b1526ef5ab3846184b
brian16600 <brian974902@gmail.com>
2022-01-20 03:22:36 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner; //import Scanner
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,27 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String dukeGreeting = "Hello! I'm Duke \nWhat can I do for you?";
+        boolean endChat = false;
+        String endMessage = "Bye. Hope to see you again soon!";
+        Scanner sc = new Scanner(System.in);
+        System.out.println(dukeGreeting);
+        while (!endChat) {
+            String userResponse = sc.nextLine();
+            endChat = levelOneRespond(userResponse);
+        }
+        System.out.println(endMessage);
+
+
+    }
+    public static boolean levelOneRespond (String input) {
+        String bye = "bye";
+        String dukeMessage = "Duke: ";
+        if (!input.equals(bye)) {
+            System.out.println(dukeMessage + input);
+            return false;
+        } else {
+            return true;
+        }
     }
 }
