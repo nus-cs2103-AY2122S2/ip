9903ebb68201a7486a5382585deace4c1996778f
lchokhoe <chokhoeefps335f@gmail.com>
2022-01-19 14:06:24 +0800

    Fixed a bug where count wasn't being incremented with each iteration in printAll() method

--- src/main/java/InputHistory.java
+++ src/main/java/InputHistory.java
@@ -16,6 +16,7 @@ public class InputHistory {
         int count = 1;
         while (iterator.hasNext()) {
             System.out.println(count + ". " + iterator.next() + "\n");
+            count++;
         }
     }
 }
