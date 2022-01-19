32eee7c973ad32f041d5cafc0655e3f35dc02a0f
kxshxsh <kashish.v.varshney@gmail.com>
2022-01-19 13:35:52 +0530

    Implementing adding to and reading the tasklist

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class Duke {
+    public static ArrayList <String > manager = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        ArrayList<String> manager = new ArrayList<String>();
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -15,14 +21,15 @@ public class Duke {
         greet();
         addLineBreak();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             String instruct = br.readLine();
             if (instruct.equals("bye")) {
                 endSession();
                 break;
+            } else if (instruct.equals("list")) {
+                reportList();
             } else {
-                System.out.println(instruct);
+                addTask(instruct);
             }
             addLineBreak();
         }
@@ -44,5 +51,17 @@ public class Duke {
 
     public static void addLineBreak() {
         System.out.println("---------------------xx-------------------------");
+    }
+
+    public static void reportList() {
+        int len = manager.size();
+        for (int i = 0; i < len; i++) {
+            System.out.println((i + 1) + ": " + manager.get(i));
+        }
+    }
+
+    public static void addTask(String instruct) {
+        manager.add(instruct);
+        System.out.println("added: " + instruct);
     }
 }
