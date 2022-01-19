3f8564aa2db888288056d8ce1c91c4a4b4fa442a
louisdavinlie <louisdavinlie@gmail.com>
2022-01-20 00:04:16 +0800

    Implement greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.*;
+import java.io.*;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    public static final String hl = "------------------------------------------------------------------------";
+
+    public static void greetings() {
+        String logo = "";
+        System.out.println(hl + "\nHi! I'm Duke\nWhat can I do for you?");
+    }
+
+    public static void chat() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.println(hl);
+            System.out.print("> ");
+            String input = br.readLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(hl);
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        greetings();
+
+        chat();
     }
 }
