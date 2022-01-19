abc723660fa802a51a9d115a06bd12a7724268f9
muraddurrani <77261055+muraddurrani@users.noreply.github.com>
2022-01-19 19:33:34 +0800

    Implement increment Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,7 +11,7 @@ public class Duke {
                 "/_/  |_\\____/_____/   \n" +
                 "                      \n";
         Scanner reader = new Scanner(System.in);
-        TaskList taskList = new TaskList();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         printAce(logo);
         printAce("Hey, I'm Ace. What can I help you with?");
@@ -18,14 +19,61 @@ public class Duke {
         String userInput = reader.nextLine();
 
         while(!userInput.equals("bye")) {
-            switch(userInput) {
-                case "list":
-                    String tasks = taskList.getAllTasks();
-                    printAce(tasks);
-                    break;
-                default:
-                    taskList.addTask((userInput));
-                    printAce("added: " + userInput);
+
+            if (userInput.equals("list")) {
+                StringBuilder strBuilder = new StringBuilder("Here are the tasks in your list:\n");
+                for (int i = 0; i < taskList.size(); i++) {
+                    String task = (i + 1 + ". " + taskList.get(i) + "\n");
+                    strBuilder.append(task);
+                }
+                printAce(strBuilder.toString());
+
+            } else if (userInput.startsWith("mark ")) {
+
+                if (userInput.length() < 6) {
+                    printAce("Please provide the number of the task you'd like to mark. Type 'list' to view your tasks.");
+                } else {
+                    String taskNumber = userInput.substring(5);
+                    try {
+                        int taskNum = Integer.parseInt(taskNumber);
+
+                        if (taskNum <= taskList.size() & 0 < taskNum) {
+                            Task selected = taskList.get(taskNum - 1);
+                            selected.markAsComplete();
+                            printAce("I've marked the following task as complete:\n" + selected);
+                        } else {
+                            printAce("You don't have a task number " + taskNum + " on your list. Type 'list' to view your tasks.");
+                        }
+
+                    } catch (NumberFormatException e) {
+                        printAce("Please provide the number of the task you'd like to mark. Type 'list' to view your tasks.");
+                    }
+                }
+
+            } else if(userInput.startsWith("unmark ")) {
+                if (userInput.length() < 8) {
+                    printAce("Please provide the number of the task you'd like to unmark. Type 'list' to view your tasks.");
+                } else {
+                    String taskNumber = userInput.substring(7);
+                    try {
+                        int taskNum = Integer.parseInt(taskNumber);
+
+                        if (taskNum <= taskList.size() & 0 < taskNum) {
+                            Task selected = taskList.get(taskNum - 1);
+                            selected.markAsIncomplete();
+                            printAce("I've unmarked the following task as complete:\n" + selected);
+                        } else {
+                            printAce("You don't have a task number " + taskNum + " on your list. Type 'list' to view your tasks.");
+                        }
+
+                    } catch (NumberFormatException e) {
+                        printAce("Please provide the number of the task you'd like to unmark. Type 'list' to view your tasks.");
+                    }
+                }
+            } else {
+                Task newTask = new Task(userInput);
+                taskList.add(newTask);
+                printAce("I've added the following task:\n" + newTask);
             }
             userInput = reader.nextLine();
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,26 @@
 public class Task {
-    private final String taskName;
+    protected final String taskName;
+    protected boolean taskComplete;
 
     public Task(String taskName) {
         this.taskName = taskName;
+        this.taskComplete = false;
+    }
+
+    public String getStatusIcon() {
+        return (taskComplete ? "X" : " ");
+    }
+
+    public void markAsComplete() {
+        this.taskComplete = true;
+    }
+
+    public void markAsIncomplete() {
+        this.taskComplete = false;
     }
 
     @Override
     public String toString() {
-        return this.taskName;
+        return "[" + this.getStatusIcon() + "] " + this.taskName;
     }
 }
