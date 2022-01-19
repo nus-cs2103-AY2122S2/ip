b90465ccbcb274c064260208cca5f61e07de78bf
Seah Zhi Xuan <e0659847@u.nus.edu>
2022-01-20 01:00:46 +0800

    Added Exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,66 +13,71 @@ public class Duke {
                 indent +
                 " What can you do for me?");
 
-        String userInput = scanner.nextLine();
-
         while(true) {
+            String userInput = scanner.nextLine();
             String[] splitUserInput = userInput.split(" ",2);
             String firstWord = splitUserInput[0];
 
-            if (userInput.equals("bye")) { // exit loop
-                dukeOutput(" Bye. Hope I never see you again!");
-                break;
-            } else if (userInput.equals("list")) { // list out everything in list
-                StringBuilder tempOut = new StringBuilder(taskList.get(0).toString());
-                for (int i = 1; i<taskList.size();i++) {
-                    tempOut.append(" " + indent).append(taskList.get(i).toString());
-                }
-                hLineBreak();
-                printlnWithIndent(" Here are the tasks in your list:");
-                System.out.print(indent + " " + tempOut); // newline in tempOut
-                hLineBreak();
+            try {
+                if (userInput.equals("bye")) { // exit loop
+                    dukeOutput(" Bye. Hope I never see you again!");
+                    break;
+                } else if (userInput.equals("list")) { // list out everything in list
+                    StringBuilder tempOut = new StringBuilder(taskList.get(0).toString());
+                    for (int i = 1; i < taskList.size(); i++) {
+                        tempOut.append(" " + indent).append(taskList.get(i).toString());
+                    }
+                    hLineBreak();
+                    printlnWithIndent(" Here are the tasks in your list:");
+                    System.out.print(indent + " " + tempOut); // newline in tempOut
+                    hLineBreak();
 
-            } else if (firstWord.equals("mark") || firstWord.equals("unmark")) {
-                int taskIndex = -1;
-                try {
+                } else if (firstWord.equals("mark") || firstWord.equals("unmark")) {
+                    int taskIndex = -1;
                     taskIndex = Integer.parseInt(splitUserInput[1]) - 1;
-                } catch (NumberFormatException ex) {
-                    System.out.println("Argument after mark/unmark not an integer!");
+
+                    Task currTask = taskList.get(taskIndex);
+
+                    if (firstWord.equals("mark")) {
+                        dukeOutput(currTask.markAsDone(true));
+                    } else { // unmark case
+                        dukeOutput(currTask.markAsDone(false));
+                    }
+
+                } else { // add task to list, todo, event or deadline
+                    String remainingUserInput = "";
+                    remainingUserInput = splitUserInput[1];
+
+                    if (firstWord.equals("todo")) {
+                        ToDo newToDo = new ToDo(userInput);
+                        taskList.add(newToDo);
+                        dukeAddTaskOutput(newToDo);
+                    } else if (firstWord.equals("deadline")) {
+                        String taskName = remainingUserInput.substring(0, remainingUserInput.indexOf("/by"));
+                        String timeBy = remainingUserInput.substring(remainingUserInput.indexOf("/by") + 4);
+                        Deadline newDeadline = new Deadline(taskName, timeBy);
+                        taskList.add(newDeadline);
+                        dukeAddTaskOutput(newDeadline);
+                    } else if (firstWord.equals("event")) {
+                        String taskName = remainingUserInput.substring(0, remainingUserInput.indexOf("/at"));
+                        String timeRange = remainingUserInput.substring(remainingUserInput.indexOf("/at") + 4);
+                        Event newEvent = new Event(taskName, timeRange);
+                        taskList.add(newEvent);
+                        dukeAddTaskOutput(newEvent);
+                    } else { // if none of the above tasks
+                        throw new DukeException(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    }
                 }
-
-                Task currTask = taskList.get(taskIndex);
-
-                if (firstWord.equals("mark")) {
-                    dukeOutput(currTask.markAsDone(true));
-                } else { // unmark case
-                    dukeOutput(currTask.markAsDone(false));
-                }
-
-            } else { // add task to list, todo, event or deadline
-
-                String remainingUserInput = splitUserInput[1];
-
-                if (firstWord.equals("todo")) {
-                    ToDo newToDo = new ToDo(userInput);
-                    taskList.add(newToDo);
-                    dukeAddTaskOutput(newToDo);
-                } else if (firstWord.equals("deadline")) {
-                    String taskName = remainingUserInput.substring(0,remainingUserInput.indexOf("/by"));
-                    String timeBy = remainingUserInput.substring(remainingUserInput.indexOf("/by") + 4);
-                    Deadline newDeadline = new Deadline(taskName,timeBy);
-                    taskList.add(newDeadline);
-                    dukeAddTaskOutput(newDeadline);
-                } else if (firstWord.equals("event")) {
-                    String taskName = remainingUserInput.substring(0,remainingUserInput.indexOf("/at"));
-                    String timeRange = remainingUserInput.substring(remainingUserInput.indexOf("/at") + 4);
-                    Event newEvent = new Event(taskName,timeRange);
-                    taskList.add(newEvent);
-                    dukeAddTaskOutput(newEvent);
-                } else { // if none of the above tasks
-
-                }
+            } catch (StringIndexOutOfBoundsException ex) {
+                dukeOutput(" ☹ OOPS!!! Argument after missing /at or /by!!!");
+                continue;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                dukeOutput(" ☹ OOPS!!! I think you're missing some arguments!");
+                continue;
+            } catch (DukeException e) {
+                dukeOutput(e.getMessage());
+                continue;
             }
-            userInput = scanner.nextLine();
         }
 
         scanner.close();
