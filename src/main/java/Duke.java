50133ac196be7f8723f3aeaab495340b1bd0b214
Gerald Tan <tanjunweigerald1997@gmail.com>
2022-01-19 15:15:19 +0800

    We now only have a very skeleton of the Duke class.
    This needs to be changed to be able to utilise good software engineering principles by creating new classes
    
    Extracting out common behaviours into a separate method & creating new classes for abstraction allows us to employ good swe principles
    
    Let's edit Duke class & create new quote class (Additional random feature)
    
    Duke class:
    * Employed abstraction principles for print methods so that changes can be easily made
    * Instantiated new Quote class which generates a Quote of the Day
    * Utilise Scanner to get user input
    
    Quote class: Abstracted out the generation of quotes to a separate class
    * Utilise Arraylist and random number generator to randomly generate a quote

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public void print(String str){
+        System.out.println("\t" + str);
+        System.out.println("_".repeat(100));
+    }
+
+    public void printBye(){
+        this.print("Bye. Hope I've been helpful todayyy, and see you again!");
+    }
+
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        Duke duke = new Duke();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        duke.print("Hello, My Dear Friend... I'm Duke!");
+        Quote quoteOfTheDay = new Quote();
+        duke.print(quoteOfTheDay.generateQuote());
+        duke.print("What can i do for you today?");
+
+        while (true){
+            String userInput = sc.nextLine();
+            if (userInput.equalsIgnoreCase("bye")){
+                duke.printBye();
+                break;
+            } else {
+                duke.print(userInput);
+            }
+        }
     }
 }
