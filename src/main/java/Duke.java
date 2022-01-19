cc1734038eaf1bc54fc46022fbb6226e9e3bf08d
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-01-19 17:43:39 +0800

    feat: changed greeting message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
+
+    private final static String paddingStr = "    ";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        initialiseBot();
+    }
+
+    private static void initialiseBot() {
+        String divString = "-----------------------------------------";
+        String botName = "Baymax";
+
+        System.out.println(paddingStr + divString);
+        System.out.println(paddingStr + paddingStr + "Greetings, I am " + botName + ".");
+        System.out.println(paddingStr + paddingStr + "What can I do you for?");
+        System.out.println(paddingStr + divString);
+
     }
 }
