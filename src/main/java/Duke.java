4837d84efc6d09f374bee683f96bb902ae41e997
Oddcorner <90017908+Oddcorner@users.noreply.github.com>
2022-01-19 16:34:41 +0800

    Add list feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,25 +1,47 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         String lineBreak = "★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★\n";
         String welcomeMessage =  lineBreak
                                  + "Hello! I'm Duke\n"
                                  + "What can I do for you?\n"
                                  + lineBreak;
         String goodbyeMessage = "Bye. Hope to see you again soon!";
+
         System.out.println(welcomeMessage);
+
         while(true){
             String input = sc.nextLine();
+
+            // ignores empty lines
+            if(input.equals("")) {
+                continue;
+            }
+
             System.out.println(lineBreak);
+
             if(input.equals("bye")){
                 System.out.println(goodbyeMessage);
                 System.out.println(lineBreak);
                 break;
             }
 
-            // Echo user input
-            System.out.println(input);
+            // List out list
+            if(input.equals("list")){
+                for(int i = 0; i < list.size(); i++){
+                    System.out.println(String.valueOf(i + 1) + ". " + list.get(i));
+                }
+                continue;
+            }
+
+            // add user input to  list
+            list.add(input);
+
+            // print message
+            System.out.println("added: " + input);
             System.out.println(lineBreak);
         }
         sc.close();
