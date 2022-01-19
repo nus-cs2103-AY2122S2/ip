2b8b5fd344f2ecf226f9eefde680a75eed13fbd1
Oddcorner <90017908+Oddcorner@users.noreply.github.com>
2022-01-19 16:05:31 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        String lineBreak = "★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★\n";
+        String welcomeMessage =  lineBreak
+                                 + "Hello! I'm Duke\n"
+                                 + "What can I do for you?\n"
+                                 + lineBreak;
+        String goodbyeMessage = "Bye. Hope to see you again soon!";
+        System.out.println(welcomeMessage);
+        while(true){
+            String input = sc.nextLine();
+            System.out.println(lineBreak);
+            if(input.equals("bye")){
+                System.out.println(goodbyeMessage);
+                System.out.println(lineBreak);
+                break;
+            }
+
+            // Echo user input
+            System.out.println(input);
+            System.out.println(lineBreak);
+        }
+        sc.close();
     }
 }
