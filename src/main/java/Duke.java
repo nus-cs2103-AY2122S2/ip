0d9ed7d2d7c83a52e0f78313340d8ac7b0358e78
cheekean5848 <loo.ck5848@outlook.com>
2022-01-19 20:10:25 +0800

    Complete Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String line = "\t_______________________________________________\n";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(logo + line + "\t Hello! I'm Duke\n" + "\t What can I do for you?\n" + line);
+
+        while (true) {
+            String input = reader.readLine();
+            if (input.equals("bye")) {
+                System.out.println(line + "\t Bye. Hope to see you again soon!\n" + line);
+                break;
+            } else {
+                System.out.println(line + "\t " + input + "\n" + line);
+            }
+        }
     }
 }
