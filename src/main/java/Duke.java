9f8f715c13ef951076b5c126b6d0389246367dd1
Kai Qi <173136u@mymail.nyp.edu.sg>
2022-01-19 16:14:00 +0800

    Update Duke.java
    Update Duke.java to greet, echo and exit when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        String welcome = "Hi! I'm Ruby, How can I help you?";
+        printMsg(welcome);
+        String command = "";
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            command = sc.next();
+            if (command.equals("bye")) {
+                break;
+            }
+            printMsg(command);
+        }
+        printMsg("Okay, bye! Hope to see you again :)");
+    }
+
+    /**
+     * Prints the message that is parsed into this method.
+     *
+     * @param msg a String containing the message to be printed.
+     */
+    public static void printMsg(String msg) {
+        String divider = "---------------------------------------------------------";
+        System.out.println(divider);
+        System.out.println(msg);
+        System.out.println(divider);
     }
 }
