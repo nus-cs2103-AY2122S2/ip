2d80631f870dba68f7707137ad35df7adf25ab15
muraddurrani <77261055+muraddurrani@users.noreply.github.com>
2022-01-19 17:08:24 +0800

    Implement increment Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "\n" +
+                "    ___   ____________\n" +
+                "   /   | / ____/ ____/\n" +
+                "  / /| |/ /   / __/   \n" +
+                " / ___ / /___/ /___   \n" +
+                "/_/  |_\\____/_____/   \n" +
+                "                      \n";
+        Scanner reader = new Scanner(System.in);
+
+        printAce(logo);
+        printAce("Hey, I'm Ace. What can I help you with?");
+
+        String userInput = reader.nextLine();
+
+        while(!userInput.equals("bye")) {
+            printAce(userInput);
+            userInput = reader.nextLine();
+        }
+        printAce("See you later!");
+        reader.close();
+    }
+
+    private static void printAce(String str) {
+        System.out.println("________\n" + str.indent(4));
     }
 }
