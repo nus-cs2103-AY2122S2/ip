0dda7e776383c627dc2df725da29cdc4a6c6150e
lamwj98 <evanlam@Evans-MacBook-Pro.local>
2022-01-19 20:37:20 +0800

    Level1 v2

--- src/main/java/Siri.java
+++ src/main/java/Siri.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Siri {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Siri!");
+        String logo = "   -----      O    -----      O \n" +
+                      " /   _   \\   __   |       \\   __\n" + 
+                      " |  | |__|  |  |  |   O   |  |  |\n" +
+                      " |   ----\\  |  |  |       /  |  |\n" +
+                      "  \\ __   |  |  |  |   ---    |  |\n" +
+                      " |---|   |  |  |  |       \\  |  |\n" +
+                      "  \\______/  |__|  |___|\\___\\ |__|\n";
+        System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
