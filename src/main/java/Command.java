ed4337c4bb0a3d2366081db3ac9e8a4c687fcc17
juayhee <e0524722@u.nus.edu>
2022-01-19 18:14:13 +0800

    Command.java: Added Command class

---
+++ src/main/java/Command.java
@@ -0,0 +1,14 @@
+public class Command {
+    private String cmd;
+
+    public Command(String input) {
+        this.cmd = input;
+    }
+
+    /**
+     * Executes the command string stored in cmd
+     */
+    public void execute() {
+        
+    }
+}
