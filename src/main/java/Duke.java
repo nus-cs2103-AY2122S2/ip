018dbe4b42d89b18391238ea833b5c4c53c3bb17
Seah Zhi Xuan <e0659847@u.nus.edu>
2022-01-19 18:55:02 +0800

    Duke Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,36 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     static String indent = "    ";
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        ArrayList<String> list = new ArrayList<String>(); // arraylist to hold inputs
 
         dukeOutput("Hello! I'm YourBoss.\n" +
                 indent +
                 "What can you do for me?");
 
-        String userInput = "";
-        userInput = scanner.nextLine();
+        String userInput = scanner.nextLine();
 
-        while(!userInput.equals("bye")) {
-            dukeOutput(userInput);
+        while(true) {
+            if (userInput.equals("bye")) { // exit loop
+                dukeOutput("Bye. Hope I never see you again!");
+                break;
+            } else if (userInput.equals("list")) { // list out everything in list
+                String tempOut = "1. " + list.get(0);
+                for (int i = 1; i<list.size();i++) {
+                    tempOut = tempOut + "\n" + indent + (i+1) + ". " + list.get(i);
+                }
+                dukeOutput(tempOut);
+
+            } else { // add input to list
+                list.add(userInput);
+                dukeOutput("added: " + userInput);
+            }
             userInput = scanner.nextLine();
         }
-
-        dukeOutput("Bye. Hope I never see you again!");
 
         scanner.close();
     }
