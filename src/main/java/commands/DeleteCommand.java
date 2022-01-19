c5cb2dc0fc993d556e96a9716dfb4bf0d7b9492c
Tan Yu Tao <tanyutao913@gmail.com>
2022-01-19 14:17:19 +0800

    Completed DeleteCommand

--- src/main/java/commands/DeleteCommand.java
+++ src/main/java/commands/DeleteCommand.java
@@ -5,9 +5,22 @@ import tasks.*;
 public class DeleteCommand extends Command{
     private Task[] tasklist;
     private int size;
+    private int deleteIndex;
+    private Task deleted;
 
-    public DeleteCommand(Task[] tasklist, int size, String input){
-
+    public DeleteCommand(Task[] tasklist, int size, int deleteIndex){
+        this.deleteIndex = deleteIndex - 1;
+        this.size = size;
+        this.tasklist = tasklist;
+        this.deleted = tasklist[this.deleteIndex];
+        for(int i = this.deleteIndex; i < size; i++) {
+            if(i == 99){
+                this.tasklist[i] = null;
+                break;
+            }
+            this.tasklist[i] = this.tasklist[i+1];
+        }
+        this.size--;
     }
 
     @Override
@@ -21,5 +34,10 @@ public class DeleteCommand extends Command{
     }
 
     @Override
-    public void execute() {}
+    public void execute() {
+        printFormatted(new String[]{
+                "Noted. I've removed this task:",
+                "  "+ deleted,
+                "Now you have " + size + " tasks in the list"});
+    }
 }
