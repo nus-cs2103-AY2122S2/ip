0125a8b03b12be949fc6ed2c58112aa77787bec9
Gerald Tan <tanjunweigerald1997@gmail.com>
2022-01-19 16:08:59 +0800

    Add functionality to store and display user tasks
    Right now, we perform all commands through Duke's main method. This can be abstracted out into another method for simplicity.
    
    Lets abstract this code out into a method performCommand which performs the user task! This method will call the necessary functions within the Duke class from now on.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    public ArrayList<String> userTasks;
+
+    public Duke(){
+        userTasks = new ArrayList<String>();
+    }
 
     public void print(String str){
         System.out.println("\t" + str);
@@ -8,8 +14,35 @@ public class Duke {
     }
 
     public void printBye(){
-        this.print("Bye. Hope I've motivated you as much as I could have!, and SMILE :D");
+        this.print("Bye. Hope I've motivated you as much as I could have, and SMILE :D");
     }
+
+    public void addTask(String userTask){
+        this.userTasks.add(userTask);
+    }
+
+    public void printUserTasks(){
+        for (int i = 1;i<=this.userTasks.size();i++) {
+            System.out.println((String.valueOf(i) + ": " + this.userTasks.get(i-1)));
+        }
+        System.out.println("These are all your tasks! Complete them and be soooo satisfied :D");
+        System.out.println("_".repeat(100));
+    }
+
+    public void performCommand(String userTask){
+            if (userTask.equalsIgnoreCase("bye")){
+                this.printBye();
+                System.exit(0);
+            } else if (userTask.length() == 0){
+                this.print("You have not keyed in any task! Maybe you missed something? Don't worry, just try again!");
+            } else if (userTask.equalsIgnoreCase("list")){
+                this.printUserTasks();
+            } else{
+                this.addTask(userTask);
+                this.print("Wonderful! You've added a task and are one step closer to your goals!\n" +
+                        "Added your task: " + userTask);
+            }
+        }
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
@@ -18,15 +51,9 @@ public class Duke {
         Quote quoteOfTheDay = new Quote();
         duke.print(quoteOfTheDay.generateQuote());
         duke.print("What can i do for you today?");
-
-        while (true){
-            String userInput = sc.nextLine();
-            if (userInput.equalsIgnoreCase("bye")){
-                duke.printBye();
-                break;
-            } else {
-                duke.print(userInput);
-            }
+        while (true) {
+            String userTask = sc.nextLine();
+            duke.performCommand(userTask);
         }
     }
 }
