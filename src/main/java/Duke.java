6f7bf949adeab9b696f8f4d6eaa7ab9918d28b7a
LapisRaider <angielimenqi7@gmail.com>
2022-01-19 23:25:36 +0800

    Place in exceptions for duke - description of task cant be empty - if forgot /by or /at for deadline and event - if mark and unmark give an invlid task number - if no valid command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -37,16 +37,19 @@ public class Duke {
         while (this.isRunning) {
             String userResponse = sc.nextLine();
 
-            if (commandsParsed(userResponse)) {
-                continue;
+            try {
+                commandsParsed(userResponse);
+            } catch (DukeException error) {
+                printDukeResponse(error.getMessage());
             }
+
         }
 
         sc.close();
     }
 
     /* Return true if command is successfully parsed */
-    public boolean commandsParsed(String input) {
+    public void commandsParsed(String input) throws DukeException {
         StringTokenizer st = new StringTokenizer(input, " ");
         String firstCommand = st.nextToken();
 
@@ -54,23 +57,28 @@ public class Duke {
         if (firstCommand.equals("bye")) {
             printDukeResponse("See ya!");
             this.isRunning = false;
-            return true;
+            return;
         }
 
         // print task list
         if (firstCommand.equals("list")) {
             printDukeResponse("Here are the tasks in your list: \n" + getListStr(userTexts));
-            return true;
+            return;
         }
 
         // mark task
         if (firstCommand.equals("mark") || firstCommand.equals("unmark")) {
             boolean markTask = firstCommand.equals("mark");
-            int taskIndex = Integer.parseInt(st.nextToken()) - 1;
+            int taskIndex = 0;
+
+            try {
+                taskIndex = Integer.parseInt(st.nextToken()) - 1;
+            } catch (NumberFormatException error) {
+                throw new DukeException("Invalid input, you need to give a number/integer");
+            }
 
             if (taskIndex >= userTexts.size()) {
-                printDukeResponse("Yo yo yo, this task don't exist.");
-                return true;
+                throw new DukeException("Yo yo yo, this task don't exist. Give a legitimate task number.");
             }
 
             Task task = userTexts.get(taskIndex);
@@ -79,22 +87,39 @@ public class Duke {
                     : "Alright, I've unmarked the task: \n ";
 
             printDukeResponse(cmdDescription + task.toString());
-            return true;
+            return;
         }
 
         // for adding the diff types of tasks
         if (firstCommand.equals("todo") || firstCommand.equals("deadline") || firstCommand.equals("event")) {
             Task newTask = null;
 
-            String taskDescription = input.substring(input.indexOf(firstCommand) + firstCommand.length() + 1);
-            if (firstCommand.equals("deadline")) {
-                newTask = new Deadline(taskDescription.substring(0, taskDescription.indexOf(" /by")),
-                        taskDescription.substring(taskDescription.indexOf("/by") + 4));
-            } else if (firstCommand.equals("event")) {
-                newTask = new Event(taskDescription.substring(0, taskDescription.indexOf(" /at")),
-                        taskDescription.substring(taskDescription.indexOf("/at") + 4));
-            } else {
-                newTask = new Todo(taskDescription);
+            String exceptionErrPrint = "The description of your task cannot be empty.";
+            try {
+                String taskDescription = input.substring(input.indexOf(firstCommand) + firstCommand.length() + 1);
+
+                if (taskDescription.length() == 0) {
+                    throw new DukeException(exceptionErrPrint);
+                }
+
+                // init the correct task type
+                if (firstCommand.equals("deadline")) {
+                    exceptionErrPrint = "Did you remember to put in the deadline after /by? Or did u remember to add /by?";
+                    newTask = new Deadline(taskDescription.substring(0, taskDescription.indexOf(" /by")),
+                            taskDescription.substring(taskDescription.indexOf("/by") + 4));
+
+                } else if (firstCommand.equals("event")) {
+                    exceptionErrPrint = "Did you remember to put in the timing after /at? Or did u remember to add /at?";
+                    newTask = new Event(taskDescription.substring(0, taskDescription.indexOf(" /at")),
+                            taskDescription.substring(taskDescription.indexOf("/at") + 4));
+
+                } else {
+                    newTask = new Todo(taskDescription);
+                }
+
+            } catch (StringIndexOutOfBoundsException error) {
+                throw new DukeException(
+                        "Something is wrong. " + exceptionErrPrint);
             }
 
             this.userTexts.add(newTask);
@@ -102,10 +127,10 @@ public class Duke {
                     + "\nNow you have " + String.valueOf(this.userTexts.size()) + " tasks in your list.";
 
             printDukeResponse(printStr);
-            return true;
+            return;
         }
 
-        return false;
+        throw new DukeException("HEY! I don't know what this mean, command doesn't exist.");
     }
 
     /* Print in the Duke response format */
