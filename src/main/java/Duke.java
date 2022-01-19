1b0a909e99bf1679a5b79bc67f04dc6fa8d30816
joelwongjy <joelwongjy@gmail.com>
2022-01-19 16:39:17 +0800

    Add Greet, Echo, Exit
    Implement skeletal version of Mickey that starts by greeting the user, echos commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hey there! I'm Mickey, your personal mouse assistant.\nWhat can I do for you today?\n");
+        for (String in = sc.next(); !in.equals("bye"); in = sc.next()) {
+                System.out.println("\t" + in);
+        }
+        System.out.println("\tToodles! See ya real soon!");
     }
 }
