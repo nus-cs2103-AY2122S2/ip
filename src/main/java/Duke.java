2c486e9babec5aeea49d33d84324f248fc421e21
Kevin Kuo <63287696+kevinkuo0320@users.noreply.github.com>
2022-01-20 02:59:16 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,27 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    public static String[] strArray = new String[100];
+    public static int index = 0;
+
     public static void main(String[] args) {
-        System.out.println("hello im duke wassup");
+
+        System.out.println("hello im duke wassup \n what can I help you?");
         Scanner sc = new Scanner(System.in);
         String echo = sc.nextLine();
-        while(!echo.equals( "bye")){
-            System.out.println(echo);
+        while(!echo.equals( "bye") ){
+            if(echo.equals("list")){
+
+                for(int i = 0; i < index; i++){
+                    int num = i + 1;
+                    System.out.println(num + " " + strArray[i]);
+                }
+            } else {
+                strArray[index] = echo;
+                System.out.println("added " + echo);
+                index++;
+            }
             echo = sc.nextLine();
         }
 
