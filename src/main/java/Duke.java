8e720ba69d50b9ed4d8db29fda3dede983cb63d4
ivorcmx <e0544423@u.nus.edu>
2022-01-19 14:29:00 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,7 @@ public class Duke {
         System.out.println("What do you wish for today? Your wish is my command");
         System.out.println("*-**-**-**-**-**-**-**-**-**-****-**-****-**-****-**");
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> storeList = new ArrayList<>();
+        ArrayList<Task> storeList = new ArrayList<>();
         while (sc.hasNextLine()) {
             String command = sc.nextLine();
             if (command.equals("bye")) {
@@ -19,10 +19,24 @@ public class Duke {
                 int sizeOfList = storeList.size();
                 System.out.println("Everything in my blue brain now: ");
                 for (int i = 1; i <= sizeOfList; i++) {
-                    System.out.println(i + "."+ " " + storeList.get(i -1));
+                    Task t = storeList.get(i - 1);
+                    System.out.println(i + "." + "[" + t.getStatusIcon() + "] " + t.getDescription());
                 }
+            } else if (command.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(command.substring(5));
+                Task tobeMark = storeList.get(taskNumber - 1);
+                tobeMark.setMark();
+                System.out.println("You could have gotten me to help you. This task has been marked done");
+                System.out.println("[X] " + tobeMark.getDescription());
+
+            } else if (command.startsWith("unmark ")) {
+                int taskNumber1 = Integer.parseInt(command.substring(7));
+                Task tobeUnmark = storeList.get(taskNumber1 - 1);
+                tobeUnmark.setUnmark();
+                System.out.println("You probably need more genies to help you. This task has been marked as not done");
+                System.out.println("[ ] " + tobeUnmark.getDescription());
             } else {
-                storeList.add(command);
+                storeList.add(new Task(command));
                 System.out.println("stored in my blue brain: " + command);
                 System.out.println("*-**-**-**-**-**-**-**-**-**-****-**-****-**-****-**");
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+
+    }
+
+    public void setMark() {
+        this.isDone = true;
+    }
+
+    public void setUnmark() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
