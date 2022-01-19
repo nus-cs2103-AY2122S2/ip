5142ce651d6467f0008b2ec43a2bd256cd061e12
LapisRaider <angielimenqi7@gmail.com>
2022-01-19 18:33:15 +0800

    Finish Level 1. Able to Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,32 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner scanner = new Scanner(System.in);
+
+        printDukeResponse("Sup! Name's Duke \nHow can I help you today?");
+
+        boolean isRunning = true;
+        while (isRunning) {
+            String userResponse = scanner.nextLine();
+
+            if (userResponse.equals("bye")) { // quit application
+                printDukeResponse("See ya!");
+                isRunning = false;
+                continue;
+            }
+
+            printDukeResponse(userResponse); // just copy user response
+        }
+    }
+
+    /* Print in the Duke response format */
+    public static void printDukeResponse(String response) {
+        System.out.println(
+                "\n--------------------------------------------------------------------------------------------");
+        System.out.println("Duke Speaking:\n");
+        System.out.println(response);
+        System.out.println(
+                "--------------------------------------------------------------------------------------------\n");
     }
 }
