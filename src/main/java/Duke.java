20a7b39cd30f5ef366ad8f1c50d67febe98b0d34
dineshraj555 <e0652525@u.nus.edu>
2022-01-19 18:57:22 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+        String logo =
+                  "||======||==||======|| !!!! ||====||    //===//======||   \n"
+                + "||                  || !!!! ||    ||   //   //       ||\n"
+                + "||  ||==||  ||==||  ||======||    ||==||   //  ||====||\n"
+                + "||  ||  ||  ||  ||  ||      ||            |||  ||====|| \n"
+                + "||  ||  ||  ||  ||  ||      ||    ||==||   \\\\  ||====|| \n"
+                + "||  ||  ||  ||  ||  ||      ||    ||   \\\\   \\\\       || \n"
+                + "||==||  ||==||  ||==||======||====||    \\\\===\\\\======|| \n";
+
+        System.out.println("\n" +logo + line);
+        System.out.println("Greetings! Mike here! \nHow can I help you?" + line);
+        String cmd = br.readLine();
+        while(!cmd.equals("bye")){
+            System.out.println(line + cmd + line);
+            cmd = br.readLine();
+        }
+        System.out.println(line + "Bye yo. Hope I helped you!" + line);
     }
 }
