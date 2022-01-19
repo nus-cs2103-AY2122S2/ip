29cee3e81b14d341037adfdd7c32b6df5ef83bac
aidanaden <aidan@DESKTOP-73NEEJC.localdomain>
2022-01-20 02:17:53 +0800

    Added level-3, Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,7 @@ public class Duke {
      * @param msg the message to be styled
      * @return    the styled message
      */
-    public static String FormatMsg(String msg) {
+    public static String formatMsg(String msg) {
         String startFormat = "####################\n";
         String endFormat = "####################";
         return startFormat + msg + endFormat;
@@ -17,7 +17,7 @@ public class Duke {
     /**
      * Returns a styled welcome message upon launch
      */
-    public static void DisplayWelcomeMsg() {
+    public static void displayWelcomeMsg() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -27,30 +27,30 @@ public class Duke {
 
         String welcomeMsg = "Hello! I'm Duke\n"
                 + "What can I do for you?\n";
-        System.out.println(FormatMsg(welcomeMsg));
+        System.out.println(formatMsg(welcomeMsg));
     }
 
     /**
      * Returns a styled exit message on exit
      */
-    public static void DisplayExitMsg() {
-        System.out.println(FormatMsg("Bye. Hope to see you again soon!\n"));
+    public static void displayExitMsg() {
+        System.out.println(formatMsg("Bye. Hope to see you again soon!\n"));
     }
 
     /**
      * Runs Level 1 version of the app, Greet/Echo/Exit
      */
-    public static void LevelOne() {
-        DisplayWelcomeMsg();
+    public static void levelOne() {
+        displayWelcomeMsg();
         Scanner sc = new Scanner(System.in);
 
         while (true) {
             String command = sc.nextLine();
             if (command.equals("bye")) {
-                DisplayExitMsg();
+                displayExitMsg();
                 return;
             } else {
-                System.out.println(FormatMsg(command));
+                System.out.println(formatMsg(command));
             }
         }
     }
@@ -61,7 +61,7 @@ public class Duke {
      * @param data arrayList of string data
      * @return     data as a list in a single string
      */
-    public static String RenderList(ArrayList<String> data) {
+    public static String renderList(ArrayList<String> data) {
         String renderStr = "";
 
         for (int i = 0; i < data.size(); i++) {
@@ -75,33 +75,107 @@ public class Duke {
     }
 
     /**
-     * Runs Level 2 version of the app, Add/List
+     * Returns string of stored data as an indexed list
+     *
+     * @param data arrayList of Task data
+     * @return     data as a list in a single string
      */
-    public static void LevelTwo() {
-        DisplayWelcomeMsg();
+    public static String renderTaskList(ArrayList<Task> data) {
+        String renderStr = "";
+
+        for (int i = 0; i < data.size(); i++) {
+            String dataStr = String.format("%d. ", i+1)
+                    + data.get(i)
+                    + " \n";
+            renderStr += dataStr;
+        }
+
+        return renderStr;
+    }
+
+    /**
+     * Runs Level 2 version of the app, Mark as done
+     */
+    public static void levelTwo() {
+        displayWelcomeMsg();
         Scanner sc = new Scanner(System.in);
         ArrayList<String> data = new ArrayList<>();
-        int size = 0;
 
         for (int i = 0; i < 100; i++) {
             String input = sc.nextLine();
 
             if (input.equals("bye")) {
-                DisplayExitMsg();
+                displayExitMsg();
                 return;
             }
 
             if (input.equals("list")) {
-                System.out.println(FormatMsg(RenderList(data)));
+                System.out.println(formatMsg(renderList(data)));
             } else {
                 data.add(input);
                 String output = " added: " + input + "\n";
-                System.out.println(FormatMsg(output));
+                System.out.println(formatMsg(output));
             }
         }
     }
 
+    public static void displayMarkMsg(String task) {
+        String markMsg = "Nice! I've marked this task as done:\n"
+                + task + "\n";
+        System.out.println(formatMsg(markMsg));
+    }
+
+    public static void displayUnmarkMsg(String task) {
+        String unmarkMsg = "Nice! I've marked this task as NOT done:\n"
+                + task + "\n";
+        System.out.println(formatMsg(unmarkMsg));
+    }
+
+    /**
+     * Runs Level 3 version of the app, Mark as done
+     */
+    public static void levelThree() {
+        displayWelcomeMsg();
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Task> data = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                displayExitMsg();
+                return;
+            }
+
+            if (input.equals("list")) {
+                System.out.println(formatMsg(renderTaskList(data)));
+                continue;
+            }
+
+            if (input.contains("mark")) {
+                int itemIndex = Integer.parseInt(input.split(" ")[1]);
+
+                if (itemIndex <= data.size()) {
+                    Task selectedTask = data.get(itemIndex - 1);
+                    if (input.contains("unmark")) {
+                        selectedTask.markAsIncomplete();
+                        displayUnmarkMsg(selectedTask.toString());
+                    } else {
+                        selectedTask.markAsComplete();
+                        displayMarkMsg(selectedTask.toString());
+                    }
+                }
+                continue;
+            }
+
+            Task newTask = new Task(input);
+            data.add(newTask);
+            String output = " added: " + input + "\n";
+            System.out.println(formatMsg(output));
+        }
+    }
+
     public static void main(String[] args) {
-        LevelTwo();
+        levelThree();
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,27 @@
 public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] "
+                + description;
+    }
+
+    public void markAsComplete() {
+        isDone = true;
+    }
+
+    public void markAsIncomplete() {
+        isDone = false;
+    }
 }
