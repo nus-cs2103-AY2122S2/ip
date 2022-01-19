c53afa5cc31c05341a3bbac79236257161b38f12
Shem Maleriado <shem.maleriado@gmail.com>
2022-01-19 14:32:27 +0800

    feat: greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What can I do for you?");
+        String command = sc.nextLine();
+
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = sc.nextLine();
+        }
+
+        sc.close();
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
