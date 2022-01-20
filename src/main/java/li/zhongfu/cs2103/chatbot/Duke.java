78e89efc90706bdb0a4f47e0fba38e5ff227726c
zhongfu <me@zhongfu.li>
2022-01-20 16:56:45 +0800

    style: fix formatting

--- src/main/java/li/zhongfu/cs2103/chatbot/Duke.java
+++ src/main/java/li/zhongfu/cs2103/chatbot/Duke.java
@@ -12,19 +12,21 @@ public class Duke {
 
     private static void dialog(String[] lines) {
         System.out.println("    " + HLINE);
-        for (String line: lines) {
+        for (String line : lines) {
             System.out.println("    " + line);
         }
         System.out.println("    " + HLINE + "\n");
     }
-    
+
     /**
-     * Takes a list of items and returns a list of strings representing the items in an (1-indexed) ordered list.
+     * Takes a list of items and returns a list of strings representing the items in
+     * an (1-indexed) ordered list.
      * 
-     * Each string in the list is of the form {@code n. foo}, where {@code n} is the index of the item (starting from 1)
+     * Each string in the list is of the form {@code n. foo}, where {@code n} is the
+     * index of the item (starting from 1)
      * and {@code foo} is the string representation of the item.
      * 
-     * @param <T> the type of items in {@code items}
+     * @param <T>   the type of items in {@code items}
      * @param items the list of items to be enumerated
      * @return a list of strings representing the items in an 1-indexed ordered list
      */
@@ -39,39 +41,39 @@ public class Duke {
 
     public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
-                    + "| | | | | | | |/ / _ \\\n"
-                    + "| |_| | |_| |   <  __/\n"
-                    + "|____/ \\__,_|_|\\_\\___|\n";
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        dialog(new String[]{
-            String.format("Hello! I'm %s", BOT_NAME),
-            "How can I help you?"
+        dialog(new String[] {
+                String.format("Hello! I'm %s", BOT_NAME),
+                "How can I help you?"
         });
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         List<Task> tasks = new ArrayList<>();
         while (true) {
-           String input = br.readLine();
-           switch (input) {
-               case "list":
-                   dialog(enumerateList(tasks).toArray(String[]::new));
-                   break;
+            String input = br.readLine();
+            switch (input) {
+                case "list":
+                    dialog(enumerateList(tasks).toArray(String[]::new));
+                    break;
 
-               case "bye":
-                   dialog(new String[] {
-                           "Bye. Hope to see you again soon!"
-                   });
-                   return; // to shut the linter up
+                case "bye":
+                    dialog(new String[] {
+                            "Bye. Hope to see you again soon!"
+                    });
+                    return; // to shut the linter up
 
-               default:
-                   Task task = new Task(input);
-                   tasks.add(task);
-                   dialog(new String[] {
-                           String.format("added: %s", task)
-                        });
-           }
+                default:
+                    Task task = new Task(input);
+                    tasks.add(task);
+                    dialog(new String[] {
+                            String.format("added: %s", task)
+                    });
+            }
         }
     }
 }
