b926a7ba718bce188f239cc630cea7a3d1f32f92
DESKTOP-MDM8KAA\lx <bianyuqi142857@gmail.com>
2022-01-20 13:55:06 +0800

    added event, deadline and todo subclass

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,15 +2,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    public static boolean findKeyword(String[] nextLineArr, String keyword){
+        for(int i = 0; i < nextLineArr.length; i++) {
+            if(nextLineArr[i].equals(keyword)) {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         String greeting = "Hello! I'm Duke\n" + "What can I do for you?";
         System.out.println(greeting);
 
         ArrayList<Task> taskList = new ArrayList<>();
 
+
         Scanner scanner = new Scanner(System.in);
         String input = null;
         String bye = "bye";
+
 
 
         while(scanner.hasNextLine()) {
@@ -22,9 +33,10 @@ public class Duke {
                 return;
             }
             if(command.equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 1; i <= taskList.size(); i++){
                     int index = i - 1;
-                    System.out.println(i + ". " + taskList.get(index).getStatusIcon()+  taskList.get(index).toString());
+                    System.out.println(i + ". " +taskList.get(index).toString());
                 }
                 continue;
             }
@@ -32,13 +44,45 @@ public class Duke {
                 int index_andone =  Integer.parseInt(strArr[1]);
                 Task temp = taskList.get(index_andone - 1);
                 temp.isDone = true;
-                System.out.println("Nice! I've marked this task as done: \n" + temp.getStatusIcon() + temp.toString());
+                System.out.println("Nice! I've marked this task as done: \n"+ temp.toString());
                 continue;
             }
 
-            Task task = new Task(input);
-            taskList.add(task);
-            System.out.println("added: " + input);
+            if(command.equals("todo")) {
+                String title = input.substring("todo".length() + 1);
+                System.out.println("Got it. I've added this task:");
+                Task task = new Todo(title);
+                taskList.add(task);
+                System.out.println("   " + task.toString() + "\n"+ "Now you have " + taskList.size() + " tasks in the list.");
+            }
+
+            if(command.equals("deadline")) {
+                System.out.println("test");
+                if(findKeyword(strArr, "/by")) {
+                    System.out.println("test1");
+                    String[] splitArr = input.split("/by", 2);
+                    String title = splitArr[0].substring("deadline".length() + 1);
+                    String time = splitArr[1];
+                    System.out.println("Got it. I've added this task:");
+                    Task task = new Deadline(title, time);
+                    taskList.add(task);
+                    System.out.println("   " + task.toString() + "\n"+ "Now you have " + taskList.size() + " tasks in the list.");
+                }
+            }
+
+            if(command.equals("event")) {
+                if(findKeyword(strArr, "/at")) {
+                    String[] splitArr = input.split("/at", 2);
+                    String title = splitArr[0].substring("event".length() + 1);
+                    String time = splitArr[1];
+                    System.out.println("Got it. I've added this task:");
+                    Task task = new Event(title, time);
+                    taskList.add(task);
+                    System.out.println("   " + task.toString() + "\n"+  "Now you have " + taskList.size() + "tasks in the list.");
+                }
+            }
+
+
         }
     }
 }
