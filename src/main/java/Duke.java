53e69678b45f00de225b99bcd8a5517b009958dc
j4ck990 <jackng990@gmail.com>
2022-01-21 01:48:41 +0800

    Implement mark and unmark command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,8 +7,8 @@ public class Duke {
     private static int numCmd;
 
     public static void msgWrap(String msg) {
-        System.out.println("\t________________________________________________________________________\n\t" +
-                msg.replace("\n","\n\t") +
+        System.out.println("\t________________________________________________________________________\n\t " +
+                msg.replace("\n","\n\t ") +
                 "\n\t________________________________________________________________________\n");
     }
 
@@ -18,20 +18,35 @@ public class Duke {
 
     private static void runCmd() {
         String cmd = readCmd();
-        switch (cmd.toLowerCase()) {
+        switch (cmd.toLowerCase().split(" ")[0]) {
             case "":
                 runCmd();
-                return;
-            case "thank you":
-                msgWrap("You're very much welcome. \n" +
+                break;
+            case "bye":
+                msgWrap("See you next time. \n" +
                         "I am always available when you need me. ");
-                return;
+                break;
             case "list":
                 msgWrap(String.join("\n", list));
                 runCmd();
-                return;
+                break;
+            case "mark": case "unmark":
+                int n = Integer.parseInt(cmd.split(" ")[1]);
+                if (n > list.size()) {
+                    msgWrap("Please add more items first. ");
+                } else if (cmd.startsWith("mark")) {
+                    list.set(n-1, list.get(n-1).substring(0,3)+'X'+list.get(n-1).substring(4));
+                    msgWrap(String.format("Alright I've marked this task as done: \n" +
+                            "  %s", list.get(n-1).substring(2)));
+                } else {
+                    list.set(n-1, list.get(n-1).substring(0,3)+' '+list.get(n-1).substring(4));
+                    msgWrap(String.format("Alright I've marked this task as not done yet: \n" +
+                            "  %s", list.get(n-1).substring(2)));
+                }
+                runCmd();
+                break;
             default:
-                list.add(String.format("%d. %s", numCmd, cmd));
+                list.add(String.format("%d.[ ] %s", numCmd, cmd));
                 numCmd++;
                 msgWrap("added: " + cmd);
                 runCmd();
