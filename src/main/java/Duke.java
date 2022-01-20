3312c0682211fc30e5558db33b1e4b6b7e752267
Shi Hong <68313416+chuashihong@users.noreply.github.com>
2022-01-21 01:11:20 +0800

    Level 2 and Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
@@ -7,19 +9,73 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String lines = "____________________________________________________________";
+        String endline = "____________________________________________________________\n";
+        System.out.println(lines);
         System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
+        ArrayList<Task> allTasks = new ArrayList<>();
+        System.out.println("How may I serve you?");
+        System.out.println(endline);
         while (true) {
-            System.out.println("How may I serve you?");
+
             String input = sc.nextLine();
+            String[] parts = input.split(" ");
 
             //level-1 feature: for exit
             if(input.equals("bye")) {
+                System.out.println(lines);
                 System.out.println("Bye! See you again");
+                System.out.println(endline);
                 break;
             }
 
-            System.out.println(input);
+            if(input.equals("list")) {
+                System.out.println(lines);
+                System.out.println("Here are all your tasks:");
+                for (int i = 0; i < allTasks.size(); i++) {
+                    System.out.println((i + 1) + "." + allTasks.get(i).getStatusIcon() + allTasks.get(i).getDescription());
+                }
+//                for(Task t :allTasks) {
+//                    System.out.println("1. " + t.getDescription());
+//                }
+                System.out.println(lines + "\n");
+                continue;
+            }
+            if(parts[0].equals("mark")) {
+                Task markTask = allTasks.get(Integer.parseInt(parts[1]) - 1);
+                System.out.println(lines);
+                if (markTask.isDone == true) {
+                    System.out.println("You have already done this task!");
+                    System.out.println(markTask.markAsDone());
+                    continue;
+                }
+                System.out.println("Good Job! You have marked this task as done!");
+                System.out.println(markTask.markAsDone());
+                System.out.println(endline);
+                continue;
+            }
+            if(parts[0].equals("unmark")) {
+                Task markTask = allTasks.get(Integer.parseInt(parts[1])-1);
+                System.out.println(lines);
+                if (markTask.isDone == false) {
+                    System.out.println("This task is already in undone status");
+                    System.out.println(markTask.markAsUndone());
+                    continue;
+                }
+                System.out.println("OK, I have marked this as not done yet:");
+                System.out.println(markTask.markAsUndone());
+                System.out.println(endline);
+                continue;
+            }
+
+            //
+            System.out.println(lines);
+            allTasks.add(new Task(input));
+            System.out.println("added: " + input);
+            System.out.println(endline);
+
+//            System.out.println(input);
 
         }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X] " : "[ ] "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String markAsDone() {
+        this.isDone = true;
+        return this.getStatusIcon() + this.description;
+    }
+
+    public String markAsUndone() {
+        this.isDone = false;
+        return this.getStatusIcon() + this.description;
+    }
+
+}
