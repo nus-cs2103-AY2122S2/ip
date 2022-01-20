0a58e0462f75e8b6966d43a8e415ccdb34b8f4ae
Justinnje <e0543778@u.nus.edu>
2022-01-20 20:06:29 +0800

    Updated Duke for Level 1. Parrots command back. only with command "bye" will the program end.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public Duke() {
+        String start =
+                "________________________________\n"
+                        + "Hello! I am Duke.\n"
+                        + "Your Personal Assistant.\n"
+                        + "What can I do for you?\n"
+                        + "________________________________";
+        System.out.println(start);
     }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Duke duke = new Duke();
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                String bye = "GoodBye! I hope to see you again!";
+                System.out.println(bye);
+                break;
+            } else {
+                System.out.println(command);
+            }
+            System.out.println("________________________________");
+        }
+    }
+//    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+//    }
 }
