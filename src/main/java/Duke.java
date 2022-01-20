cc5699db1e65a651e7195d7eee24b9975e7590fc
EvaderFati <evaderfati@gmail.com>
2022-01-21 00:31:07 +0800

    Level-1 implementation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String br = "\t____________________________________________________________\n";
+
+    private static void greeting() {
+        String sentences = "\tHello! I'm Duke\n\tWhat can I do for you?\n";
+        System.out.println(Duke.br + sentences + Duke.br + "\n");
+    }
+
+    private static void bye() {
+        String sentences = "\tBye. Hope to see you again soon!\n";
+        System.out.println(Duke.br + sentences + Duke.br + "\n");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc= new Scanner(System.in);
+        String input;
+
+        Duke.greeting();
+        input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(Duke.br + "\t" + input + "\n" + Duke.br + "\n");
+            input = sc.nextLine();
+        }
+        Duke.bye();
     }
 }
