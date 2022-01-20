edb016ea941aba44c7ee3fb32cd03fdeb41b8c79
Rye-Catcher <2427367956@qq.com>
2022-01-20 22:21:35 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -78,18 +78,17 @@ public class Duke {
     /**
      * The main body of the chat box. Will receive commands and do things accordingly
      **/
+    static private ArrayList<Task> todo_list = new ArrayList<Task>();
+    static final String farewell_words = "Bye. Hope to see you again soon!";
+
     private static class processIO {
-        static void run() {
+        static boolean run() throws EmptyDescriptionException,  EmptyTimeException, InvalidCommandException, InvalidNumberException {
             Scanner scanner = new Scanner(System.in);
-            String farewell_words = "Bye. Hope to see you again soon!";
 
-            ArrayList<Task> todo_list = new ArrayList<Task>();
-
-            while(true) {
                 String command = scanner.nextLine();
                 if (command.equals("bye") || command.equals("Bye")) {
                     System.out.print(outputChatBox(farewell_words));
-                    break;
+                    return false;
                 } else if (command.equals("list")){
                     String output_list = "";
                     if (todo_list.isEmpty()) {
@@ -105,26 +104,44 @@ public class Duke {
                 } else {
                     String task_type = command.split(" ")[0];
                     if (task_type.equals("mark")) {
-                        Task temp = todo_list.get(Integer.parseInt(command.split(" ")[1]) - 1);
+                        int task_id = Integer.parseInt(command.split(" ")[1]) - 1;
+                        if (task_id < 0 || task_id >= todo_list.size()) {
+                            throw new InvalidNumberException();
+                        }
+                        Task temp = todo_list.get(task_id);
                         temp.setStatus(true);
                         String output_list = "Nice! I've marked this task as done: \n  " + temp.toString();
 
                         System.out.print(outputChatBox(output_list));
                     } else if (task_type.equals("unmark")) {
-                        Task temp = todo_list.get(Integer.parseInt(command.split(" ")[1]) - 1);
+                        int task_id = Integer.parseInt(command.split(" ")[1]) - 1;
+                        if (task_id < 0 || task_id >= todo_list.size()) {
+                            throw new InvalidNumberException();
+                        }
+                        Task temp = todo_list.get(task_id);
                         temp.setStatus(false);
                         String output_list = "OK, I've marked this task as not done yet: \n  " + temp.toString();
 
                         System.out.print(outputChatBox(output_list));
                     } else if (task_type.equals("todo")){
-                        ToDo temp = new ToDo(command.substring(task_type.length()));
+                        String description = command.substring(task_type.length());
+                        if (description.length() == 0) {
+                            throw new EmptyDescriptionException();
+                        }
+                        ToDo temp = new ToDo(description);
                         todo_list.add(temp);
                         String output_text = "Got it. I've added this task:\n    " + temp.toString()
                                 + "\nNow you have " + todo_list.size() + " tasks in the list.";
                         System.out.print(outputChatBox(output_text));
                     } else if (task_type.equals("deadline")) {
                         String description = command.substring(task_type.length()).split("/")[0];
+                        if (description.length() == 0) {
+                            throw new EmptyDescriptionException();
+                        }
                         String task_by = command.substring(task_type.length()).split("/")[1].substring(3);
+                        if (task_by.length() == 0) {
+                            throw new EmptyTimeException();
+                        }
                         Deadline temp = new Deadline(description, task_by);
                         todo_list.add(temp);
                         String output_text = "Got it. I've added this task:\n    " + temp.toString()
@@ -132,7 +149,13 @@ public class Duke {
                         System.out.print(outputChatBox(output_text));
                     } else if (task_type.equals("event")) {
                         String description = command.substring(task_type.length()).split("/")[0];
+                        if (description.length() == 0) {
+                            throw new EmptyDescriptionException();
+                        }
                         String task_time = command.substring(task_type.length()).split("/")[1].substring(3);
+                        if (task_time.length() == 0) {
+                            throw new EmptyTimeException();
+                        }
                         Event temp = new Event(description, task_time);
                         todo_list.add(temp);
                         String output_text = "Got it. I've added this task:\n    " + temp.toString()
@@ -140,10 +163,8 @@ public class Duke {
                         System.out.print(outputChatBox(output_text));
                     }
                 }
+                return true;
             }
-
-            return ;
-        }
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -153,6 +174,19 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String greeting = "Hello! I'm Pyke\n" + "What can I do for you?";
         System.out.print(outputChatBox(greeting));
-        processIO.run();
+        while(true) {
+            try {
+                if(!processIO.run()) break;
+            } catch (EmptyDescriptionException e) {
+                System.out.println(outputChatBox("OOPS!!! The description cannot be empty. :-("));
+            } catch (InvalidCommandException e) {
+                System.out.println(outputChatBox("OOPS!!! I'm sorry, but I don't know what that means :-("));
+            } catch (EmptyTimeException e) {
+                System.out.println(outputChatBox("OOPS!!! The time cannot be empty. :-("));
+            } catch (InvalidNumberException e) {
+                System.out.println(outputChatBox("OOPS!!! Seems like this is a invalid number :-("));
+            }
+        }
     }
 }
+
