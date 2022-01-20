2fc28cbefb99216f04ac5fe1befd63e738db2869
joey-chance <joey.chance@u.nus.edu>
2022-01-20 09:44:55 +0800

    level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String logo =
+                " ------    -------    -------   ---        ---   ---------    -------\n" +
+                        "|____  |  |   _   |  |   _   |  \\  \\      /  /  |___   ___|  |   ____|\n" +
+                        "     | |  |  |_|  |  |  |_|  |   \\  \\    /  /       | |      |  |____ \n" +
+                        " _   | |  |   _   |  |      _|    \\  \\  /  /        | |      |_____  |\n" +
+                        "| |__| |  |  | |  |  |  |\\  \\      \\  \\/  /      ___| |___    ____|  |\n" +
+                        "|______|  |__| |__|  |__| \\__\\      \\____/      |_________|  |_______|\n"
+                ;
+        System.out.println("Welcome home, sir.\n" + logo);
+        echo();
+    }
+    public static void echo() {
+        String input;
+        Scanner sc = new Scanner(System.in);
+        input = sc.nextLine();
+        while (!input.equalsIgnoreCase("bye")) {
+            System.out.println("___________________________________________________________");
+            System.out.println(input.toLowerCase());
+            System.out.println("___________________________________________________________");
+            input = sc.nextLine();
+        }
+        System.out.println("___________________________________________________________");
+        System.out.println("Good bye, sir.");
+        System.out.println("___________________________________________________________");
+        sc.close();
     }
 }
+
