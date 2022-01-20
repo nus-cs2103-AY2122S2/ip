63b73848fb4f40c66524d6d108021c2dbd1a3108
brian16600 <brian974902@gmail.com>
2022-01-20 20:03:45 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,10 +13,10 @@ public class Duke {
         String endMessage = "Bye. Hope to see you again soon!";
 
         //arraylist to store the list of tasks
-        ArrayList<String> arrayLst = new ArrayList<>();
+        ArrayList<Task> arrayLst = new ArrayList<>();
 
         System.out.println(dukeGreeting);
-        levelTwoRespond(arrayLst);
+        levelThreeRespond(arrayLst);
         System.out.println(endMessage);
 
 
@@ -38,27 +38,91 @@ public class Duke {
     }
 
     /**
-     * Method that handles the response for Level-2. Includes Add and List
-     * @param arrayLst arraylist that stores the String entries for add and can be listed out.
+     * Method that handles the response for Level-3. Building on level 2, it adds the ability to mark and unmark tasks as well as different representation for Tasks. Furthermore,
+     * the arraylist is changed to contain Task objects
+     * @param arrayLst arraylist that stores the Task entries for add and can be listed out.
      */
-    public static void levelTwoRespond (ArrayList<String> arrayLst) {
+    public static void levelThreeRespond (ArrayList<Task> arrayLst) {
         Scanner sc = new Scanner(System.in);
         String bye = "bye";
         String lst = "list";
+        String markCommand = "mark";
+        String unmarkCommand = "unmark";
+        String tasksInList = "Here are the tasks in your list:\n";
         String input = sc.nextLine();
-        if (input.equals(bye));
+
+        if (input.equals(bye)); //ends recursive loop, causing the bye statement in main to be executed
         else if (input.equals(lst)) {
+            System.out.println(tasksInList);
             int i = 0;
-            for (String item : arrayLst) {
+            for (Task item : arrayLst) {
                 i += 1;
-                System.out.println(i + ". " + item);
+                if (item.isMark()) {
+                    System.out.println(i + ". " + item);
+                } else {
+                    System.out.println(i + ". " + item);
+                }
             }
-            levelTwoRespond(arrayLst);
+            levelThreeRespond(arrayLst);
+
+        } else if (input.contains(unmarkCommand)) {
+            String stringIdx = input.split(" ")[1];
+            int  idx = Integer.parseInt(stringIdx) - 1;
+            Task unmarkTask = arrayLst.get(idx);
+            unmarkTask.unmarkTask();
+            levelThreeRespond(arrayLst);
+
+        } else if (input.contains(markCommand)){
+            String stringIdx = input.split(" ")[1];
+            int idx = Integer.parseInt(stringIdx) - 1;
+            Task markTask = arrayLst.get(idx);
+            markTask.markTask();
+            levelThreeRespond(arrayLst);
+
         } else {
             String added = "added: ";
-            arrayLst.add(input);
+            Task newTask = new Task(input);
+            arrayLst.add(newTask);
             System.out.println(added + input);
-            levelTwoRespond(arrayLst);
+            levelThreeRespond(arrayLst);
+        }
+    }
+
+    /**
+     * Represents a Task. Contains a Task constructor, two methods to mark and unmark tasks, toString() method as well as a isMark() method to check if Task is marked
+     */
+
+    public static class Task {
+        private boolean mark;
+        public String name;
+
+        public Task (String name) {
+            this.name = name;
+            this.mark = false;
+        }
+
+        public void markTask () {
+            String markedMessage = "Nice! I've marked this task as done:\n";
+            this.mark = true;
+            System.out.println(markedMessage + "  " + this);
+        }
+        public void unmarkTask() {
+            String unmarkedMessage = "OK, I've marked this task as not done yet:\n";
+            this.mark = false;
+            System.out.println(unmarkedMessage + "  " + this);
+        }
+        public boolean isMark() {
+            return this.mark;
+        }
+        public String toString() {
+            if (this.mark) {
+                String marked = "[X] ";
+                return marked + this.name;
+            } else {
+                String unmarked = "[ ] ";
+                return unmarked + this.name;
+            }
         }
     }
 }
+
