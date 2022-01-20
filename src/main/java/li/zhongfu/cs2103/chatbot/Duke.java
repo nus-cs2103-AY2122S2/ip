ce71cf138332eebb9fb5b6dc1150df0661006be8
zhongfu <me@zhongfu.li>
2022-01-20 19:05:48 +0800

    feat: add generic DukeException for... unexpected input?

--- src/main/java/li/zhongfu/cs2103/chatbot/Duke.java
+++ src/main/java/li/zhongfu/cs2103/chatbot/Duke.java
@@ -94,83 +94,98 @@ public class Duke {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         List<Task> tasks = new ArrayList<>();
         while (true) {
-            String input = br.readLine();
-            String[] parts = input.split("\\s", 2); // split into command and args
-            String inCmd = parts[0];
-            String inArgs = parts.length == 2 ? parts[1] : "";
-            switch (inCmd) {
-                case "mark":
-                case "unmark":
-                    try {
-                        int idx = Integer.parseInt(inArgs) - 1; // list is 1-indexed
-                        Task task = tasks.get(idx);
-                        boolean done = inCmd.equals("mark"); // set as done if cmd == "mark", else (e.g. cmd == "unmark") set as undone
-                        task.setDone(done); 
-                        dialog(new String[] {
-                            String.format("Task marked as %sdone:", done ? "" : "un"),
-                            String.format(" %s", task)
-                        });
-                    } catch (NumberFormatException e) {
-                        dialog(String.format("Hmm, that doesn't seem like a valid task number! Try '%s <number>'.", inCmd));
-                    } catch (IndexOutOfBoundsException e) {
-                        dialog("That task doesn't exist!");
-                    }
-                    break;
-
-                case "list":
-                    if (tasks.isEmpty()) {
-                        dialog("You have no tasks in your task list!");
-                    } else {
-                        List<String> output = new ArrayList<>();
-                        output.add("Here are the tasks in your list:");
-                        output.addAll(enumerateList(tasks));
-                        dialog(output.toArray(String[]::new));
-                    }
-                    break;
-
-                case "bye":
-                    dialog("Bye. Hope to see you again soon!");
-                    return; // to shut the linter up
-
-                case "todo":
-                case "deadline":
-                case "event":
-                    Task task;
-                    Map<String, String> taskArgs = parseArgString(inArgs);
-                    if (taskArgs.get("").isBlank()) { // includes strings with only whitespaces
-                        dialog("Task name cannot be empty!");
+            try {
+                String input = br.readLine();
+                String[] parts = input.split("\\s", 2); // split into command and args
+                String inCmd = parts[0];
+                String inArgs = parts.length == 2 ? parts[1] : "";
+                switch (inCmd) {
+                    case "mark":
+                    case "unmark":
+                        try {
+                            int idx = Integer.parseInt(inArgs) - 1; // list is 1-indexed
+                            Task task = tasks.get(idx);
+                            boolean done = inCmd.equals("mark"); // set as done if cmd == "mark", else (e.g. cmd == "unmark") set as undone
+                            task.setDone(done); 
+                            dialog(new String[] {
+                                String.format("Task marked as %sdone:", done ? "" : "un"),
+                                String.format(" %s", task)
+                            });
+                        } catch (NumberFormatException e) {
+                            throw new DukeException(String.format("Hmm, that doesn't seem like a valid task number! Try '%s <number>'.", inCmd), e);
+                        } catch (IndexOutOfBoundsException e) {
+                            throw new DukeException("That task doesn't exist!", e);
+                        }
                         break;
-                    }
 
-                    if (inCmd.equals("todo")) {
-                        task = new ToDo(taskArgs.get(""));
-                    } else if (inCmd.equals("deadline")) {
-                        if (!taskArgs.containsKey("by") || taskArgs.get("by").isEmpty()) {
-                            dialog(String.format("You need a time for your deadline! Try: deadline %s /by next Sunday", taskArgs.get("")));
-                            break;
+                    case "list":
+                        if (tasks.isEmpty()) {
+                            dialog("You have no tasks in your task list!");
+                        } else {
+                            List<String> output = new ArrayList<>();
+                            output.add("Here are the tasks in your list:");
+                            output.addAll(enumerateList(tasks));
+                            dialog(output.toArray(String[]::new));
                         }
-                        task = new Deadline(taskArgs.get(""), taskArgs.get("by"));
-                    } else if (inCmd.equals("event")) {
-                        if (!taskArgs.containsKey("at") || taskArgs.get("at").isEmpty()) {
-                            dialog(String.format("You need a time for your event! Try: event %s /at next Sunday", taskArgs.get("")));
-                            break;
-                        }
-                        task = new Event(taskArgs.get(""), taskArgs.get("at"));
-                    } else {
-                        throw new RuntimeException(String.format("Got unexpected command here: %s", inCmd));
-                    }
-                    
-                    tasks.add(task);
-                    dialog(new String[] {
-                        "New task added:",
-                        String.format(" %s", task),
-                        String.format("You now have %d tasks in your task list.", tasks.size())
-                    });
-                    break;
+                        break;
 
-                default:
-                    dialog("I don't know what that means! Try: todo, deadline, event, list, mark, unmark, bye");
+                    case "bye":
+                        dialog("Bye. Hope to see you again soon!");
+                        return; // to shut the linter up
+
+                    case "todo":
+                    case "deadline":
+                    case "event":
+                        Task task;
+                        Map<String, String> taskArgs = parseArgString(inArgs);
+                        if (taskArgs.get("").isBlank()) { // includes strings with only whitespaces
+                            throw new DukeException("Task name cannot be empty!");
+                        }
+
+                        if (inCmd.equals("todo")) {
+                            task = new ToDo(taskArgs.get(""));
+                        } else if (inCmd.equals("deadline")) {
+                            if (!taskArgs.containsKey("by") || taskArgs.get("by").isEmpty()) {
+                                throw new DukeException(String.format("You need a time for your deadline! Try: deadline %s /by next Sunday", taskArgs.get("")));
+                            }
+                            task = new Deadline(taskArgs.get(""), taskArgs.get("by"));
+                        } else if (inCmd.equals("event")) {
+                            if (!taskArgs.containsKey("at") || taskArgs.get("at").isEmpty()) {
+                                throw new DukeException(String.format("You need a time for your event! Try: event %s /at next Sunday", taskArgs.get("")));
+                            }
+                            task = new Event(taskArgs.get(""), taskArgs.get("at"));
+                        } else {
+                            throw new RuntimeException(String.format("Got unexpected command here: %s", inCmd));
+                        }
+                        
+                        tasks.add(task);
+                        dialog(new String[] {
+                            "New task added:",
+                            String.format(" %s", task),
+                            String.format("You now have %d tasks in your task list.", tasks.size())
+                        });
+                        break;
+
+                    default:
+                        throw new DukeException("I don't know what that means! Try: todo, deadline, event, list, mark, unmark, bye");
+                }
+            } catch (DukeException e) {
+                dialog(e.getMessage().split("\n"));
             }
         }
+    }
+}
+
+class DukeException extends Exception {
+    DukeException() {
+
+    }
+
+    DukeException(String message) {
+        super(message);
+    }
+
+    DukeException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
