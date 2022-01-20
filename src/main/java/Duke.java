21bed869948c478460cb3749a507d9f245073aa8
joey-chance <joey.chance@u.nus.edu>
2022-01-20 11:14:39 +0800

    level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,6 +25,7 @@ public class Duke {
             int id;
             switch(input) {
                 case "list" :
+                    System.out.println("Your tasks list, sir:");
                     for (int i = 1; i <= tasks.size(); i++) {
                         System.out.println(i + "." + tasks.get(i - 1));
                     }
@@ -41,11 +42,33 @@ public class Duke {
                     System.out.println("Slacking off, sir? This task is marked not done:");
                     System.out.println("  " + tasks.get(id - 1));
                     break;
+                case "todo" :
+                    sc.reset();
+                    input = sc.nextLine().strip();
+                    tasks.add(new Todo(input));
+                    System.out.println("I've added this to your tasks sir: " + tasks.getLast());
+                    System.out.println("There are " + tasks.size() + " tasks on your list.");
+                    break;
+                case "deadline" :
+                    sc.reset();
+                    input = sc.nextLine().strip();
+                    String[] deadline = input.split(" /by ");
+                    tasks.add(new Deadline(deadline[0], deadline[1]));
+                    System.out.println("I've added this to your tasks sir: " + tasks.getLast());
+                    System.out.println("There are " + tasks.size() + " tasks on your list.");
+                    break;
+                case "event" :
+                    sc.reset();
+                    input = sc.nextLine().strip();
+                    String[] event = input.split(" /at ");
+                    tasks.add(new Deadline(event[0], event[1]));
+                    System.out.println("I've added this to your tasks sir: " + tasks.getLast());
+                    System.out.println("There are " + tasks.size() + " tasks on your list.");
+                    break;
                 default :
                     sc.reset();
-                    input += sc.nextLine();
-                    tasks.add(new Task(input));
-                    System.out.println("added: " + input);
+                    sc.nextLine();
+                    System.out.println("Unknown command, sir.");
             }
             System.out.println("___________________________________________________________");
             input = sc.next();
