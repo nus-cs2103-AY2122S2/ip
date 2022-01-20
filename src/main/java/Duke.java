78e03ea3c086b7b6de9074cac701632ecd8d9563
Tan De Shao <e0544387@u.nus.edu>
2022-01-20 17:33:19 +0800

    level-2 code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Duke {
+    private static ArrayList<String> ls = new ArrayList<>();
+
+
     public static void main(String[] args) {
         initDuke();
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
             String userInput = sc.nextLine();
-            if (userInput.equals("bye")) {
+            if (userInput.equals("list")) {
+                printList();
+            } else if (userInput.equals("bye")) {
                 dukePrint("Bye. Hope to see you again soon!");
                 break;
             } else {
-                dukePrint(userInput);
+                dukePrint("added: " + addInput(userInput));
             }
         }
         sc.close();
+    }
+
+    private static String addInput(String input) {
+        ls.add(input);
+        return input;
+    }
+
+    private static void printList() {
+        for (int i = 0; i < ls.size(); i++) {
+            String num = String.format("%d. ", i + 1);
+            System.out.println(num + ls.get(i));
+        }
+        printHorizontalLine();
     }
 
     private static void dukePrint(String input) {
@@ -32,4 +52,7 @@ public class Duke {
         System.out.println(" What can I do for you?");
         printHorizontalLine();
     }
+
+
+
 }
