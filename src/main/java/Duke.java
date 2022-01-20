3217b0bf49e43fad4b703e0ae78ec3d5ace94111
Tan Jie Wei <>
2022-01-20 16:04:35 +0800

    Added level 2 functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) throws Exception {
@@ -9,14 +10,24 @@ public class Duke {
                 + "What can I do for you?\n";
         System.out.println(greeting);
 
+        ArrayList<String> list = new ArrayList<String>();
         String input = "";
+
         while (!input.equals("bye")) {
+
             input = br.readLine();
+
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+            } else {
+                list.add(input);
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
         }
     }
 }
