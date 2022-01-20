4e24ee35a9581ea4007d127c0335514deb290b45
j4ck990 <jackng990@gmail.com>
2022-01-21 01:16:12 +0800

    Implement add and list commands, abstract out welcome code
    Welcome code(daytime, logo) unrelated to Apollo's function.
    
    Abstract out to make code more readable.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,80 +1,48 @@
-import java.time.LocalTime;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    private static String msgWrap(String msg) {
-        return "\t________________________________________________________________________\n\t" +
+    static ArrayList<String> list;
+    private static Scanner sc;
+    private static int numCmd;
+
+    public static void msgWrap(String msg) {
+        System.out.println("\t________________________________________________________________________\n\t" +
                 msg.replace("\n","\n\t") +
-                "\n\t________________________________________________________________________\n";
-    }
-
-    private static void welcome() {
-        String daytime = dayTime();
-        String msg = String.format("Good %s Sir, I am Apollo. \n" +
-                "How can I help you on this wonderful %s? ", daytime, daytime.toLowerCase());
-        System.out.println(msgWrap(msg));
-    }
-
-    private static String dayTime() {
-        LocalTime now = LocalTime.now();
-        // define the border values
-        LocalTime eleven = LocalTime.of(11, 0);
-        LocalTime four = LocalTime.of(4, 0);
-        LocalTime fifteen = LocalTime.of(15, 0);
-        LocalTime eighteenThirty = LocalTime.of(18, 30);
-
-        // check if the time is after four and either before or exactly eleven
-        if (now.isAfter(four) &&
-                (now.isBefore(eleven) || now.equals(eleven)))
-            return "Morning";
-            // check if the time is after eleven and either before or exactly fifteen
-        else if (now.isAfter(eleven) &&
-                (now.isBefore(fifteen) || now.equals(fifteen)))
-            return "Afternoon";
-            // check if the time is after fifteen and either before or exactly eighteen thirty
-        else if (now.isAfter(fifteen) &&
-                (now.isBefore(eighteenThirty) || now.equals(eighteenThirty)))
-            return "Evening";
-            // otherwise, it's night
-        else return "Night";
+                "\n\t________________________________________________________________________\n");
     }
 
     private static String readCmd() {
-        Scanner sc = new Scanner(System.in);
-        String cmd = sc.nextLine();
-        return cmd.toLowerCase();
+        return sc.nextLine();
     }
 
     private static void runCmd() {
         String cmd = readCmd();
-        switch (cmd) {
+        switch (cmd.toLowerCase()) {
+            case "":
+                runCmd();
+                return;
             case "thank you":
-                System.out.println(msgWrap("You're very much welcome. \n" +
-                        "I am always available when you need me. "));
+                msgWrap("You're very much welcome. \n" +
+                        "I am always available when you need me. ");
+                return;
+            case "list":
+                msgWrap(String.join("\n", list));
+                runCmd();
                 return;
             default:
-                System.out.println(msgWrap(cmd));
+                list.add(String.format("%d. %s", numCmd, cmd));
+                numCmd++;
+                msgWrap("added: " + cmd);
                 runCmd();
         }
     }
 
     public static void main(String[] args) {
-        String logo = "       @@@%      @@@@@@*.      :@@@@@.    %@@@@@+    @@@@@@        #@@@@=       \n" +
-                        "       @@@@=     @@@@@@@@@    @@@@@@@@#   @@@@@@*    @@@@@@.     -@@@@@@@@      \n" +
-                        "       @@@@@     @@@@@@@@@*  %@@@@@@@@@+  @@@@@@*    @@@@@@.    .@@@@@@@@@@     \n" +
-                        "      #@@@@@     +@@@@+@@@@  @@@@%+%@@@@   @@@@       @@@@      @@@@@**@@@@=    \n" +
-                        "      @@@@@@+     %@@@  @@@ =@@@#   @@@@:  @@@@       @@@@      @@@@    @@@@    \n" +
-                        "      @@@.@@@     #@@@ @@@@ %@@@     @@@#  @@@@       @@@%      @@@*    @@@@    \n" +
-                        "     .@@@ @@@     *@@@@@@@= @@@@     @@@@  @@@@       @@@#      @@@-    @@@@    \n" +
-                        "     =@@@@@@@*    *@@@@@@%  @@@@     @@@@  @@@@       @@@*      @@@-    @@@@    \n" +
-                        "     @@@@@@@@@    +@@@@%    #@@@.    @@@+  @@@@  @@%  @@@+  @@* @@@@    @@@@    \n" +
-                        "     @@@-:-@@@.   +@@@      -@@@@   @@@@   @@@@  @@@  @@@=  @@% @@@@*  *@@@%    \n" +
-                        "     @@@.  @@@#   +@@@       @@@@@@@@@@@  -@@@@-=@@@ -@@@*-*@@% #@@@@@@@@@@.    \n" +
-                        "    @@@@@:@@@@@@ @@@@@@@     =@@@@@@@@@   @@@@@@@@@@ @@@@@@@@@%  @@@@@@@@@%     \n" +
-                        "    @@@@@-@@@@@@ @@@@@@@      =@@@@@@@-   @@@@@@@@@@ @@@@@@@@@%   @@@@@@@@      \n" +
-                        "    @%#*+:+%#*+= -=+++++        =%@%=     --======== --=======-    :*@@*.       \n";
-        System.out.println("Hello from\n" + logo);
-        welcome();
+        list = new ArrayList<>();
+        sc = new Scanner(System.in);
+        numCmd = 1;
+        Banner.welcomeMsg();
         runCmd();
     }
 }
