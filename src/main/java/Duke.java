f015065b70ebb08c37ac8bc9730cbefafd6f5e05
fantablack <fantablack@users.noreply.github.com>
2022-01-20 23:06:13 +0800

    added task adding

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,6 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        // KattSystem.out System.out = new KattSystem.out(System.in, System.out);
-
-        // String logo = " ____ _ \n"
-        // + "| _ \\ _ _| | _____ \n"
-        // + "| | | | | | | |/ / _ \\\n"
-        // + "| |_| | |_| | < __/\n"
-        // + "|____/ \\__,_|_|\\_\\___|\n";
-        // System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
 
         String greeting = "Hello! I'm Kizer\nWhat can I do for you?";
@@ -19,27 +11,30 @@ public class Duke {
         System.out.println(greeting);
         System.out.println(bar);
         ArrayList<String> storageList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         while (true) {
-            // String input = io.getWord();
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println(bar);
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(bar);
                 sc.close();
-                // System.out.close();
                 break;
             } else if (input.equals("list")) {
                 System.out.println(bar);
-                for (int i = 1; i <= storageList.size(); i++) {
-                    System.out.println(i + ". " + storageList.get(i - 1));
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 1; i <= taskList.size(); i++) {
+                    Task curr = taskList.get(i - 1);
+                    System.out.println(i + "." + "[" + curr.getStatusIcon() + "] " + curr);
                 }
                 System.out.println(bar);
             } else {
                 System.out.println(bar);
-                storageList.add(input);
-                System.out.println("added: " + input);
+                // storageList.add(input);
+                Task curr = new Task(input);
+                taskList.add(curr);
+                System.out.println("added: " + curr);
                 System.out.println(bar);
             }
 
