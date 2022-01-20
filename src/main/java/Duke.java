9f2b35fe2438150de694c982d698b3d7b1fdd7cd
Brandon <e0540875@u.nus.edu>
2022-01-20 14:43:49 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.*;
 public class Duke {
+    static Scanner sc = new Scanner(System.in);
+    static String line = "\n---------------------\n";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " ____         _   _     \n"
+                    + "|  _ \\       | | | |\n"
+                    + "| |_| |      | |-| |\n"
+                    + "| |_| |  _   | |-| |\n"
+                    + "|____/  |_|  |_| |_|\n";
+        System.out.println("Hello, I am B.H. How can I help you?\n" + logo + line);
+
+        while(true) {
+            String input = sc.nextLine();
+            if (input.equals("bye") || input.equals("Bye")) {
+                System.out.println(line + "GoodBye! Thanks for using B.H!" + line);
+                break;
+            } else {
+                System.out.println(line + input + line);
+            }
+        }
     }
 }
