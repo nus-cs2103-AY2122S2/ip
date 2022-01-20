29d922b48b22f035458160651c308f967b755a6e
garethkoh <e0533656@u.nus.edu>
2022-01-20 19:26:32 +0800

    Level 3: Mark as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Duke {
+public class Duke
+{
+
+    public static boolean isInteger(String input)
+    {
+        try {
+            Integer.parseInt(input);
+            return true;
+        }
+        catch( Exception e ) {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> toDoList = new ArrayList<>();
+        ArrayList<Task> toDoList = new ArrayList<>();
+
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,23 +29,81 @@ public class Duke {
         System.out.println("enter a command");
 
         String userInput = sc.nextLine();
+        String[] split = userInput.split(" ");
+
         while (!(userInput.equals("bye")))
         {
             if (userInput.equals("list"))
             {
                 for (int i = 0; i < toDoList.size(); i++)
                 {
-                    System.out.printf("%d. " + toDoList.get(i) +"\n", i + 1);
+                    Task t = toDoList.get(i);
+                    if (t.getStatus() == 1)
+                    {
+                        System.out.printf("%d. [X] " + t.getName() + "\n", i + 1);
+                    }
+                    else
+                    {
+                        System.out.printf("%d. [ ] " + t.getName() + "\n", i + 1);
+                    }
                 }
-                userInput = sc.nextLine();
+            }
+            else if (split[0].equals("unmark") && split.length > 1 && isInteger(split[1]))
+            {
+                int item = Integer.parseInt(split[1]);
+                Task t = toDoList.get(item - 1);
+                t.unmark();
+                System.out.printf("Boo! more work to do: %s\n", t.getName());
+            }
+            else if (split[0].equals("mark") && split.length > 1 && isInteger(split[1]))
+            {
+                int item = Integer.parseInt(split[1]);
+                Task t = toDoList.get(item - 1);
+                t.mark();
+                System.out.printf("great job! I've marked this task as done: %s\n", t.getName());
             }
             else
             {
                 System.out.println("task added: " + userInput);
-                toDoList.add(userInput);
-                userInput = sc.nextLine();
+                toDoList.add(new Task(userInput));
             }
+            userInput = sc.nextLine();
+            split = userInput.split(" ");
         }
         System.out.println("goodbye!");
+    }
+
+
+}
+
+class Task
+{
+    private int status; // 1 indicates done and 0 indicates not done
+    private String name;
+
+    public Task(String name)
+    {
+        this.status = 0;
+        this.name = name;
+    }
+
+    public int getStatus()
+    {
+        return status;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void mark()
+    {
+        this.status = 1;
+    }
+
+    public void unmark()
+    {
+        this.status = 0;
     }
 }
