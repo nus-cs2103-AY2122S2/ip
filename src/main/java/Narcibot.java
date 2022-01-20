9a5aabbd0e4ac0e85e931873da001df459fee5de
redpelican2108 <xinwei2108@gmail.com>
2022-01-20 13:00:24 +0800

    Handle Errors

--- src/main/java/Narcibot.java
+++ src/main/java/Narcibot.java
@@ -19,45 +19,85 @@ public class Narcibot {
     private static boolean command(String command) {
         boolean end = false;
         String[] tokens = command.split(" ",2);
-        switch(tokens[0]) {
-            case "bye":
-                System.out.println("So you finally decided to leave. Goodbye! Not that I really care.");
-                end = true;
-                break;
-            case "list":
-                System.out.println("Do I have to remind you again?");
-                for(int i = 0; i < index; i++) {
-                    System.out.println((i+1) + "." + list[i].getStatus());
-                }
-                break;
-            case "mark":
-                System.out.println("You finally did something? I'll mark it for you then.");
-                list[Integer.parseInt((tokens[1])) - 1].markDone();
-                break;
-            case "unmark":
-                System.out.println("As expected... another task that wasn't finished at all.");
-                list[Integer.parseInt((tokens[1])) - 1].markNotDone();
-                break;
-            case "todo":
-                System.out.println("I have added this task cause you won't remember it.");
-                list[index++] = new ToDo(tokens[1]);
-                System.out.println("You now have "+index+ " tasks");
-                break;
-            case "deadline":
-                System.out.println("I have added this task cause you won't remember it.");
-                String[] tokens2 = tokens[1].split("/by",2);
-                list[index++] = new Deadline(tokens2[0],tokens2[1]);
-                System.out.println("You now have "+index+ " tasks");
-                break;
-            case "event" :
-                System.out.println("I have added this task cause you won't remember it.");
-                String[] tokens3 = tokens[1].split("/at",2);
-                list[index++] = new Event(tokens3[0],tokens3[1]);
-                System.out.println("You now have "+index+ " tasks");
-                break;
-            default:
-                System.out.println("added: " + command);
-                list[index++] = new Task(command);
+        try {
+            switch (tokens[0]) {
+                case "bye":
+                    if(tokens.length != 1) {
+                        throw new IncorrectFormatException("Please enter only one word for this command");
+                    }
+                    System.out.println("So you finally decided to leave. Goodbye! Not that I really care.");
+                    end = true;
+                    break;
+                case "list":
+                    if(tokens.length != 1) {
+                        throw new IncorrectFormatException("Please enter only one word for this command.");
+                    }
+                    System.out.println("Do I have to remind you again?");
+                    for (int i = 0; i < index; i++) {
+                        System.out.println((i + 1) + "." + list[i].getStatus());
+                    }
+                    break;
+                case "mark":
+                    if(tokens.length != 2) {
+                        throw new IncorrectFormatException("Please enter mark followed by a number for this command. Example: mark 8");
+                    }
+                    int index1 = Integer.parseInt((tokens[1]));
+                    if(index1 < 1 || index1 > index) {
+                        throw new IncorrectFormatException("Please enter a valid task number.");
+                    }
+                    System.out.println("You finally did something? I'll mark it for you then.");
+                    list[index1 - 1].markDone();
+                    break;
+                case "unmark":
+                    if(tokens.length != 2) {
+                        throw new IncorrectFormatException("Please enter unmark followed by a number for this command. Example: unmark 7");
+                    }
+                    int index2 = Integer.parseInt((tokens[1]));
+                    if(index2 < 1 || index2 > index) {
+                        throw new IncorrectFormatException("Please enter a valid task number.");
+                    }
+                    System.out.println("As expected... another task that wasn't finished at all.");
+                    list[Integer.parseInt((tokens[1])) - 1].markNotDone();
+                    break;
+                case "todo":
+                    if(tokens.length != 2) {
+                        throw new IncorrectFormatException("You want me to remind you of something but you won't tell me of what it is?");
+                    }
+                    list[index++] = new ToDo(tokens[1]);
+                    System.out.println("I have added this task cause you won't remember it.");
+                    System.out.println("You now have " + index + " tasks");
+                    break;
+                case "deadline":
+                    if(tokens.length != 2) {
+                        throw new IncorrectFormatException("You want me to remind you of something but you won't tell me of what it is? The format is deadline (task) /by (time)");
+                    }
+                    String[] tokens2 = tokens[1].split("/by", 2);
+                    if(tokens2.length != 2 || tokens2[1].equals("")) {
+                        throw new IncorrectFormatException("Please tell me the deadline time using /by. Format: deadline (task) /by (time)");
+                    }
+                    list[index++] = new Deadline(tokens2[0], tokens2[1]);
+                    System.out.println("I have added this task cause you won't remember it.");
+                    System.out.println("You now have " + index + " tasks");
+                    break;
+                case "event":
+                    if(tokens.length != 2) {
+                        throw new IncorrectFormatException("You want me to remind you of something but you won't tell me of what it is? The format is event (task) /at (time)");
+                    }
+                    String[] tokens3 = tokens[1].split("/at", 2);
+                    if(tokens3.length != 2 || tokens3[1].equals("")) {
+                        throw new IncorrectFormatException("Please tell me the time of the event using /at. Format: event (task) /at (time)");
+                    }
+                    list[index++] = new Event(tokens3[0], tokens3[1]);
+                    System.out.println("I have added this task cause you won't remember it.");
+                    System.out.println("You now have " + index + " tasks");
+                    break;
+                default:
+                    System.out.println("Could you please talk some sense? I can't seem to comprehend what you're saying.");
+            }
+        }  catch (NumberFormatException e) {
+            System.out.println("Are you even trying to specify a task? Please enter in digits if you're wondering.");
+        } catch (IncorrectFormatException e) {
+            System.out.println(e.getMessage());
         }
         return end;
     }
