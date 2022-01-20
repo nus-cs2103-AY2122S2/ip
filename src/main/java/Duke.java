3b35d28b06a6238421e3f9aa6405762cb615106c
Tan Jie Wei <>
2022-01-20 15:04:37 +0800

    Added level 1 functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.io.*;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String greeting = "Hello! I'm Duke\n"
+                + "What can I do for you?\n";
+        System.out.println(greeting);
+
+        String input = "";
+        while (!input.equals("bye")) {
+            input = br.readLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+        }
     }
 }
