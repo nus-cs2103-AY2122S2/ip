5663ff82d058b3178f2d7a584f4a6d11a4469395
Oddcorner <90017908+Oddcorner@users.noreply.github.com>
2022-01-20 23:50:01 +0800

    Refactor Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,101 +1,103 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Duke {
-    private static ArrayList<Task> list = new ArrayList<>();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String lineBreak = "-------------------------------\n";
-        String welcomeMessage =  lineBreak
-                                 + "Hello! I'm Duke\n"
-                                 + "What can I do for you?\n"
-                                 + lineBreak;
-        String goodbyeMessage = "Bye. Hope to see you again soon!";
+    private ArrayList<Task> list = new ArrayList<>();
+    private String lineBreak = "-------------------------------\n";
+    private String welcomeMessage =  lineBreak
+            + "Hello! I'm Duke\n"
+            + "What can I do for you?\n"
+            + lineBreak;
+    private String goodbyeMessage = "Bye. Hope to see you again soon!";
+    public boolean isListening = true;
 
-        System.out.println(welcomeMessage);
+    public void ask(String input){
+        // Format input to remove leading and trailing spaces
+        input = input.trim();
 
-        while(true){
-            String input = sc.nextLine().trim();
-
-            // Ignores empty lines
-            if(input.equals("")) {
-                continue;
-            }
-
-            System.out.println(lineBreak);
-
-            // Exits program
-            if(input.equals("bye")){
-                System.out.println(goodbyeMessage);
-                System.out.println(lineBreak);
-                break;
-            }
-
-            // List out list
-            if(input.equals("list")){
-                System.out.println("Here are the tasks in your list:");
-                for(int i = 0; i < list.size(); i++){
-                    System.out.println(String.valueOf(i + 1) + ". " + list.get(i));
-                }
-                System.out.println(lineBreak);
-                continue;
-            }
-
-            // Check for keywords
-            String[] strArr = input.split(" ",2);
-            String[] result;
-            String keyword = strArr[0];
-            Task task = null;
-
-            try {
-                switch (keyword) {
-                    case "mark":
-                        System.out.println(mark(strArr[1]));
-                        break;
-                    case "unmark":
-                        System.out.println(unmark(strArr[1]));
-                        break;
-                    case "todo":
-                        task = new Todo((strArr[1]));
-                        break;
-                    case "deadline":
-                        result = strArr[1].split("/by", 2);
-                        task = new Deadline(result[0], result[1]);
-                        break;
-                    case "event":
-                        result = strArr[1].split("/at", 2);
-                        task = new Event(result[0], result[1]);
-                        break;
-                    case "delete":
-                        System.out.println(delete(strArr[1]));
-                        break;
-                    default:
-                        // Unknown keyword
-                        System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                        break;
-                }
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Not enough fields, please check your inputs and try again.");
-            }
-
-            if(task != null){
-                list.add(task);
-                System.out.println(taskAddedMessage(task));
-            }
-            System.out.println(lineBreak);
+        // Ignore empty input
+        if(input.equals("")){
+           return;
         }
-        sc.close();
+
+        System.out.println(lineBreak);
+
+        // Exits the program
+        if (input.equals("bye")){
+            goodbye();
+            return;
+        }
+
+        // List out the current list
+        if(input.equals("list")){
+            list();
+            return;
+        }
+
+        // Check for keywords
+        String[] strArr = input.split(" ",2);
+        String[] result;
+        String keyword = strArr[0];
+        Task task = null;
+
+        try {
+            switch (keyword) {
+                case "mark":
+                    System.out.println(mark(strArr[1]));
+                    break;
+                case "unmark":
+                    System.out.println(unmark(strArr[1]));
+                    break;
+                case "todo":
+                    task = new Todo((strArr[1]));
+                    break;
+                case "deadline":
+                    result = strArr[1].split("/by", 2);
+                    task = new Deadline(result[0], result[1]);
+                    break;
+                case "event":
+                    result = strArr[1].split("/at", 2);
+                    task = new Event(result[0], result[1]);
+                    break;
+                case "delete":
+                    System.out.println(delete(strArr[1]));
+                    break;
+                default:
+                    // Unknown keyword
+                    System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    break;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Not enough fields, please check your inputs and try again.");
+        }
+
+        if(task != null){
+            list.add(task);
+            System.out.println(taskAddedMessage(task));
+        }
+
+        System.out.println(lineBreak);
     }
 
-    private static String mark(String input){
-        int index;
+    public void introduce(){
+        System.out.println(welcomeMessage);
+    }
 
-        // Try to convert String input as index in list
-        try {
-            index = Integer.parseInt(input) - 1;
-        } catch (NumberFormatException e) {
-            index = -1;
+    public void goodbye(){
+        this.isListening = false;
+        System.out.println(goodbyeMessage);
+        System.out.println(lineBreak);
+    }
+
+    public void list(){
+        System.out.println("Here are the tasks in your list:");
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(String.valueOf(i + 1) + ". " + list.get(i));
         }
+        System.out.println(lineBreak);
+    }
 
+    private String mark(String input){
+        int index = parseInt(input) - 1;
         // Check out of bounds
         if(index < 0 || index >= list.size()) {
             return "This is an invalid index, please try again!";
@@ -105,16 +107,8 @@ public class Duke {
         return "Nice! I've marked this task as done:\n" + task;
     }
 
-    private static String unmark(String input){
-        int index;
-
-        // Try to convert String input as index in list
-        try {
-            index = Integer.parseInt(input) - 1;
-        } catch (NumberFormatException e) {
-            index = -1;
-        }
-
+    private String unmark(String input){
+        int index = parseInt(input) - 1;
         // Check out of bounds
         if(index < 0 || index >= list.size()) {
             return "This is an invalid index, please try again!";
@@ -124,22 +118,14 @@ public class Duke {
         return "OK! I've marked this task as not done yet:\n" + task;
     }
 
-    private static String taskAddedMessage(Task task){
+    private String taskAddedMessage(Task task){
         return "Got it. I've added this task:\n"
                 + task.toString()
                 + "\nNow you have " + list.size() + " tasks in the list.";
     }
 
-    private static String delete(String input){
-        int index;
-
-        // Try to convert String input as index in list
-        try {
-            index = Integer.parseInt(input) - 1;
-        } catch (NumberFormatException e) {
-            index = -1;
-        }
-
+    private String delete(String input){
+        int index = parseInt(input) - 1;
         // Check out of bounds
         if(index < 0 || index >= list.size()) {
             return "This is an invalid index, please try again!";
@@ -147,5 +133,27 @@ public class Duke {
         Task task = list.remove(index);
         return  "OK! I've removed this task:\n" + task
                     + "\nNow you have " + list.size() + " tasks in the list.";
+    }
+
+    private int parseInt(String s) {
+        int index;
+        try {
+            index = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            index = -1;
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Duke chatbot = new Duke();
+
+        chatbot.introduce();
+        while(chatbot.isListening){
+            String input = sc.nextLine();
+            chatbot.ask(input);
+        }
+        sc.close();
     }
 }
