7fd40621d4d150bd7d8e55ce734b48d55b93286c
juayhee <e0524722@u.nus.edu>
2022-01-20 12:10:21 +0800

    ListCommand.java: Added formatted taskList printing.

--- src/main/java/ListCommand.java
+++ src/main/java/ListCommand.java
@@ -1,10 +1,46 @@
+import java.util.ArrayList;
+
 /**
  * Prints the current list of tasks
  */
 public class ListCommand extends Command {
     @Override
     public void execute() {
-        System.out.println(TaskManager.taskList);
+        ArrayList<String> taskList = TaskManager.taskList;
+
+        // Formatting task list to be printed
+        String formattedTaskList = formatTaskList(taskList);
+        String formattedAnswer = Duke.formatAnswer(formattedTaskList);
+        System.out.println(formattedAnswer);
     }
+
+    /**
+     * Formats the task list for readability
+     * @param taskList ArrayList to be formatted
+     * @return formattedTaskList Formatted task list
+     */
+    private String formatTaskList(ArrayList<String> taskList) {
+        int indexCounter = 1;
+        String formattedTaskList = "";
+        int taskListSize = taskList.size();
+
+        for (String i : taskList) {
+            String item = String.valueOf(indexCounter) + ". " + i;
+
+            // Correcting formatting caused by Duke.formatAnswer()
+            if (indexCounter == 1) {
+                formattedTaskList += (item + "\n");
+            } else if (indexCounter == taskListSize) {
+                formattedTaskList += ("\t" + item);
+            } else {
+                formattedTaskList += ("\t" + item + "\n");
+            }
+
+            indexCounter += 1;
+        }
+
+        return formattedTaskList;
+    }
+
 
 }
