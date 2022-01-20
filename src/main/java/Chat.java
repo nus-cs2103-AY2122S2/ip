da87b487bc5585a873859ed50ce167d8aca19f76
imrajsingh <e0543951@u.nus.edu>
2022-01-20 14:14:34 +0800

    level 1

---
+++ src/main/java/Chat.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+public class Chat {
+
+    /**
+     * This Chat version is just supposed to take in the commands,
+     * and return them, thats all.
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("Wassup buddddyyyyyy! How you doin? \nWhat can I do you for fine sire?");
+
+        // takes in the incoming prompt
+        Scanner sc = new Scanner(System.in);
+
+        // String input = sc.next();
+
+        // Outputs
+        while (true) {
+
+            String input = sc.next();
+
+            if (input.equals("bye")) {
+                System.out.println("----------");
+                System.out.println(" " + "Bye, have a great time!");
+                System.out.println("----------");
+                break;
+            } else {
+                System.out.println("----------");
+                System.out.println(" " + input);
+                System.out.println("----------");
+            }
+        }
+    }
+
+}
+
+// import java.util.*;
+
+// public class Chat {
+
+// /**
+// * This Chat version is just supposed to take in the commands,
+// * and return them, thats all.
+// */
+
+// public static String LINE_BREAK = "---------------";
+// public static String GREETING = "Wassup buddddyyyyyy! How you doin? \nWhat
+// can I do you for fine sire?";
+// public static String BYE = LINE_BREAK + "\n Byeeeee, come back again ah!\n" +
+// LINE_BREAK;
+
+// public static void main(String[] args) {
+
+// String[] list = new String[100];
+// int counter = 0;
+
+// System.out.println(GREETING);
+
+// // takes in the incoming prompt
+// Scanner sc = new Scanner(System.in);
+
+// // Outputs
+// while (true) {
+
+// String input = sc.nextLine();
+
+// if (input.equals("bye")) {
+// System.out.println(BYE);
+// break;
+// } else if (input.equals("list")) {
+
+// System.out.println(LINE_BREAK);
+// int internalCounter = 1;
+
+// // iterate through the list
+// for (String item : list) {
+// if (item != null) {
+// System.out.println(" " + internalCounter + ". " + item);
+// ++internalCounter;
+// } else {
+// break;
+// }
+// }
+// System.out.println(LINE_BREAK);
+// } else {
+// list[counter] = input;
+// System.out.println(LINE_BREAK);
+// System.out.println(" ok added alr bro: " + input);
+// System.out.println(LINE_BREAK);
+// ++counter;
+// }
+// }
+// }
+
+// }
