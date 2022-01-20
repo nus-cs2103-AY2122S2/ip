60cc214fa127b3b2d26c26f8434de1500f579122
Pun Hui Min <punhuimin99@gmail.com>
2022-01-20 19:35:27 +0800

    Added DukeException, Updated Duke, and Added test cases
    Added documentation to Duke to showcase the params the methods take and
    what it outputs.Exceptions handled include wrong date input format, no
    description, no date provided, no proper instructions, and calling an
    empty list.
    
    Added more test cases to check on whether he correct exceptions will be
    thrown.
    
    Date validity should be checked in each Event and Deadline. Error
    should be thrown there to keep Duke code to a minimum.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.io.*;
+import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Duke {
@@ -26,20 +28,26 @@ public class Duke {
 
     public void help() {
         String helpResponse = "> Type 'list' to see what you have in your task list\n"+
-                "> Type todo <message> to put a todo in your list\n"+
-                "> Type deadline <message> /by <deadline> to put a deadline in your list"+
-                "\n> Type event <message> /at <date> to put an event in your list"+
+                "> Type 'todo <message>' to put a todo in your list\n"+
+                "> Type 'deadline <message> /by <deadline>' to put a deadline in your list." +
+                "\n\t - Deadline must be in 'DD/MM/YYYY' format"+
+                "\n> Type 'event <message> /at <date>' to put an event in your list"+
+                "\n\t - Date must be in 'DD/MM/YYYY' format" +
                 "\n> Type 'mark <x>' to mark a task in your list" +
                 "\n> Type 'unmark <x>' to unmark a task in your list";;
         System.out.println(helpResponse);
     }
 
-    public void printTask() {
-        System.out.println("Here are your tasks:");
-        for (int i = 0; i < this.tasks.size(); i++) {
-            Task currentTask = this.tasks.get(i);
-            String message = currentTask.getTask();
-            System.out.println(i + 1 + ". " + message);
+    public void printTasks() throws DukeException {
+        if (this.tasks.size() ==0) {
+            throw new DukeException("Please add some tasks first *(^.^)*");
+        } else {
+            System.out.println("Here are your tasks:");
+            for (int i = 0; i < this.tasks.size(); i++) {
+                Task currentTask = this.tasks.get(i);
+                String message = currentTask.getTask();
+                System.out.println(i + 1 + ". " + message);
+            }
         }
     }
 
@@ -48,45 +56,104 @@ public class Duke {
         System.out.println(byeResponse);
     }
 
-    public void addTodo(String description){
-        Todo entry = new Todo(description);
-        this.tasks.add(entry);
-        String message = entry.getTask();
-        System.out.println("I have added the following todo:\n" + message);
-        System.out.println("You now have " + this.tasks.size() + " tasks");
+    /*
+     * @param description which is the task description from user
+     */
+    public void addTodo(String description) throws DukeException{
+        if (description.length() == 0){
+            throw new DukeException("Oops! \\(@.@)/ You have not keyed in a description for the task!\n" +
+                    "Let's try again ~(^.^)~\n" +
+                    "Type 'help' if you need to know how to use this command");
+        }
+        else {
+            Todo entry = new Todo(description);
+            this.tasks.add(entry);
+            String message = entry.getTask();
+            System.out.println("I have added the following todo:\n" + message);
+            System.out.println("You now have " + this.tasks.size() + " tasks");
+        }
     }
 
-    public void addDeadline(String description, String time) {
-        Deadline entry = new Deadline(description, time);
-        this.tasks.add(entry);
-        String message = entry.getTask();
-        System.out.println("I have added the following deadline:\n" + message);
-        System.out.println("You now have " + this.tasks.size() + " tasks");
+    /*
+     * @param description which is the task description from user
+     * @param time which is the deadline provided by user
+     */
+    public void addDeadline(String description, String time) throws DukeException {
+        if (description.length() == 0){
+            throw new DukeException("Oops! \\(@.@)/ You have not keyed in a description for the task!\n" +
+                    "Let's try again ~(^.^)~\n" +
+                    "Type 'help' if you need to know how to use this command");
+        }
+        if (time.length() == 0){
+            throw new DukeException("Oops! \\(@.@)/ You have not keyed in a due date for the task!\n" +
+                    "Let's try again ~(^.^)~\n" +
+                    "Type 'help' if you need to know how to use this command");
+        }
+        else{
+            Deadline entry = new Deadline(description, time);
+            this.tasks.add(entry);
+            String message = entry.getTask();
+            System.out.println("I have added the following deadline:\n" + message);
+            System.out.println("You now have " + this.tasks.size() + " tasks");
+        }
     }
 
-    public void addEvent(String description, String time) {
-        Event entry = new Event(description, time);
-        this.tasks.add(entry);
-        String message = entry.getTask();
-        System.out.println("I have added the following event:\n" + message);
-        System.out.println("You now have " + this.tasks.size() + " tasks");
+    /*
+     * @param description which is the task description from user
+     * @param time which is the event time provided by user
+     */
+    public void addEvent(String description, String time) throws DukeException {
+        if (description.length() == 0){
+            throw new DukeException("Oops! \\(@.@)/ You have not keyed in a description for the task!\n" +
+                    "Let's try again ~(^.^)~\n" +
+                    "Type 'help' if you need to know how to use this command");
+        }
+        else {
+            Event entry = new Event(description, time);
+            this.tasks.add(entry);
+            String message = entry.getTask();
+            System.out.println("I have added the following event:\n" + message);
+            System.out.println("You now have " + this.tasks.size() + " tasks");
+        }
     }
 
+    /*
+     * @param strID which is the ID number of the task in the list
+     */
     public void unmarkItem(String strID) {
-        Task currentTask = this.tasks.get(Integer.parseInt(strID)-1);
-        currentTask.setNotDone();
-        String message = currentTask.getTask();
-        System.out.println("Sure, I have unmarked the following task:\n" + message);
+        try {
+            Task currentTask = this.tasks.get(Integer.parseInt(strID) - 1);
+            currentTask.setNotDone();
+            String message = currentTask.getTask();
+            System.out.println("Sure, I have unmarked the following task:\n" + message);
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println("Sorry, I could not find that \\(T.T)/\n" +
+                    "Please type 'list' to view your current entries.");
+        }
     }
 
+    /*
+     * @param strID which is the ID number of the task in the list
+     */
     public void markItem(String strID) {
-        Task currentTask = this.tasks.get(Integer.parseInt(strID)-1);
-        currentTask.setDone();
-        String message = currentTask.getTask();
-        System.out.println("Ok, I have marked the following task:\n" + message);
+        try {
+            Task currentTask = this.tasks.get(Integer.parseInt(strID) - 1);
+            currentTask.setDone();
+            String message = currentTask.getTask();
+            System.out.println("Ok, I have marked the following task:\n" + message);
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println("Sorry, I could not find the item \\(T.T)/\n" +
+                    "Please type 'list' to view your current entries.");
+        }
     }
 
-    public void check(Duke ducky, Scanner myObj) {
+    /*
+    * @param Takes in a Duke instance
+    * @param Takes in a Scanner object
+    * */
+    public void check(Duke ducky, Scanner myObj) throws DukeException {
         String response = myObj.nextLine();
         String [] textEntered = response.split(" ", 2);
         String command = textEntered[0];
@@ -97,22 +164,58 @@ public class Duke {
         } else if(command.equals("help")){
             ducky.help();
         } else if(command.equals("list")){
-            ducky.printTask();
+            ducky.printTasks();
         } else if(command.equals("todo")) {
-            String description = textEntered[1];
-            ducky.addTodo(description);
+            try {
+                String description = textEntered[1];
+                ducky.addTodo(description);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Oops! \\(@.@)/ You have not keyed in a description for the task!\n" +
+                        "Let's try again ~(^.^)~\n" +
+                        "Type 'help' if you need to know how to use this command");
+            }
         } else if(command.equals("deadline")) {
-            String text = textEntered[1];
-            String [] textArr = text.split("/by ");
-            String description = textArr[0];
-            String time = textArr[1];
-            ducky.addDeadline(description, time);
+            try {
+                String text = textEntered[1];
+                String[] textArr = text.split("/by ");
+                String description = textArr[0];
+                if (textArr.length == 1) {
+                    throw new DukeException("Oops, please specify a date!");
+                }
+                String time = textArr[1];
+                SimpleDateFormat date = new java.text.SimpleDateFormat("dd/MM/yyyy");
+                date.parse(time);
+                ducky.addDeadline(description, time);
+            } catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Oops! \\(@.@)/ You have not keyed in a description for the task!\n" +
+                        "Let's try again ~(^.^)~\n" +
+                        "Type 'help' if you need to know how to use this command");
+            } catch (ParseException e) {
+                System.out.println("Oops, please put a valid time format!\n"+
+                        "Let's try again ~(^.^)~\n" +
+                        "Type 'help' if you need to know how to use this command");
+            }
         } else if(command.equals("event")) {
-            String text = textEntered[1];
-            String [] textArr = text.split("/at ");
-            String description = textArr[0];
-            String time = textArr[1];
-            ducky.addEvent(description, time);
+            try {
+                String text = textEntered[1];
+                String [] textArr = text.split("/at ");
+                String description = textArr[0];
+                if (textArr.length == 1) {
+                    throw new DukeException("Oops, please specify a date!");
+                }
+                String time = textArr[1];
+                SimpleDateFormat date = new java.text.SimpleDateFormat("dd/MM/yyyy");
+                date.parse(time);
+                ducky.addEvent(description, time);
+            } catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Oops! \\(@.@)/ You have not keyed in a description for the task!\n" +
+                        "Let's try again ~(^.^)~\n" +
+                        "Type 'help' if you need to know how to use this command");
+            } catch (ParseException e) {
+                System.out.println("Oops, please put a valid time format!\n"+
+                        "Let's try again ~(^.^)~\n" +
+                        "Type 'help' if you need to know how to use this command");
+            }
         } else if(command.equals("unmark")) {
             String strID = textEntered[1];
             ducky.unmarkItem(strID);
@@ -120,19 +223,24 @@ public class Duke {
             String strID = textEntered[1];
             ducky.markItem(strID);
         } else {
-            ducky.help();
+            throw new DukeException("Sorry, I did not catch that! \\(T.T)/\n" +
+                    "Please type 'help' to see all commands I can help with.");
         }
         System.out.println("~--~~--~~--~~(~v~)~~--~~--~~--~");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         Duke ducky = new Duke();
         ducky.welcome();
 
         Scanner myObj = new Scanner(System.in);
 
         while(true){
-            ducky.check(ducky, myObj);
+            try{
+                ducky.check(ducky, myObj);
+            } catch (DukeException e){
+                System.out.println(e.toString()+ "\n~--~~--~~--~~(~v~)~~--~~--~~--~") ;
+            }
         }
     }
 }
---
+++ src/main/java/DukeException.java
@@ -0,0 +1,12 @@
+public class DukeException extends Exception {
+    String message;
+    public DukeException(String errorMessage) {
+        super(errorMessage);
+        this.message = errorMessage;
+    }
+
+    @Override
+    public String toString(){
+        return this.message;
+    }
+}
