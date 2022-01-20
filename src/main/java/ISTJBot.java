473f427b4e111fa66078d7449960b118b56b7eb4
jbkim1999 <jungbae.kim@u.nus.edu>
2022-01-20 19:01:32 +0800

    Implemented Level-3

--- src/main/java/ISTJBot.java
+++ src/main/java/ISTJBot.java
@@ -7,14 +7,24 @@ public class ISTJBot {
 
         boolean doneChatting = false;
         ArrayList<Task> tasks = new ArrayList<>();
-        String list = "";
+        String list = "Here are the tasks in your list:\n";
 
         String welcomeMessage = "Hello! I'm ISTJBot. \n" + "What can I do for you?";
         printResponse(welcomeMessage);
 
         while (!doneChatting) {
             String request = sc.nextLine();
-            if (request.equals("list")) {
+            String[] requestInfo = request.split(" ");
+
+            if (requestInfo[0].equals("mark")) {
+                int taskNumber = Integer.parseInt(requestInfo[1]);
+                tasks.get(taskNumber - 1).mark();
+                printResponse("Good. I've marked this task as done: \n" + tasks.get(taskNumber - 1).toString());
+            } else if (requestInfo[0].equals("unmark")) {
+                int taskNumber = Integer.parseInt(requestInfo[1]);
+                tasks.get(taskNumber - 1).unmark();
+                printResponse("I've marked this task as not done yet: \n" + tasks.get(taskNumber - 1).toString());
+            } else if (requestInfo[0].equals("list")) {
                 for (int i = 1; i <= tasks.size(); i++) {
                     if (i == tasks.size()) {
                         list += i + ". " + tasks.get(i - 1).toString();
@@ -23,15 +33,16 @@ public class ISTJBot {
                     }
                 }
                 printResponse(list);
+                list = "Here are the tasks in your list:\n";
             }
-            else if (request.equals("bye")) {
+            else if (requestInfo[0].equals("bye")) {
                 doneChatting = true;
                 printResponse("Bye, I'll be organizing your tasks until you come back.");
                 sc.close();
             } else {
                 Task TaskAdded = new Task(request);
                 tasks.add(TaskAdded);
-                printResponse("added: " + TaskAdded.toString());
+                printResponse("added: " + request);
             }
         }
     }
