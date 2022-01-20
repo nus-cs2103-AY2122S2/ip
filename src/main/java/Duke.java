6241dd8894c8269d76be038e37537d29d02e4981
dannytayjy <danny_tayjunyang@hotmail.com>
2022-01-20 21:26:04 +0800

    Implement Level-5
    * Add DukeException.java to handle any exceptions during program
    execution
    * Add Ui.java to handle input and output interactions
    * Update Duke.java to handle errors via exception handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,155 +1,321 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Duke {
     private static final List<Task> tasks = new ArrayList<>();
+
+    private final Ui ui;
+
+    public Duke() {
+        ui = new Ui();
+    }
 
     public static void main(String[] args) {
         new Duke().run();
     }
 
     public void run() {
-        greet();
+        ui.displayWelcome();
 
-        Scanner sc = new Scanner(System.in);
         String commandLine = "";
 
         while (!commandLine.equals("bye")) {
-            System.out.println("Enter command:");
-            commandLine = sc.nextLine().trim();
+            commandLine = ui.readCommand();
 
-            String[] commandLineParts = commandLine.split(" ", 2);
-
-            int taskNum;
-            Task task;
-            String taskDescription;
-            String taskInfo;
-
-            switch (commandLineParts[0].toLowerCase()) {
-            case "bye":
-                exit();
-                break;
-            case "list":
-                listTasks();
-                break;
-            case "mark":
-                taskNum = Integer.parseInt(commandLineParts[1]);
-                markTask(taskNum);
-                break;
-            case "unmark":
-                taskNum = Integer.parseInt(commandLineParts[1]);
-                unmarkTask(taskNum);
-                break;
-            case "todo":
-                taskDescription = commandLineParts[1];
-                task = new ToDo(taskDescription);
-                addTask(task);
-                break;
-            case "deadline":
-                taskInfo = commandLineParts[1];
-                String[] deadlineTaskDetails = taskInfo.split("/", 2);
-                task = new Deadline(deadlineTaskDetails[0], deadlineTaskDetails[1]);
-                addTask(task);
-                break;
-            case "event":
-                taskInfo = commandLineParts[1];
-                String[] eventTaskDetails = taskInfo.split("/", 2);
-                task = new Event(eventTaskDetails[0], eventTaskDetails[1]);
-                addTask(task);
-                break;
-            default:
-                break;
+            try {
+                processCommand(commandLine);
+            } catch (DukeException e) {
+                ui.displayError(e.getMessage());
+            } finally {
+                ui.displayLine();
             }
         }
     }
 
-    public void greet() {
-        String logo = "\t" + " ____        _        " + System.lineSeparator()
-                + "\t" + "|  _ \\ _   _| | _____" + System.lineSeparator()
-                + "\t" + "| | | | | | | |/ / _" + System.lineSeparator()
-                + "\t" + "| |_| | |_| |   <  __/" + System.lineSeparator()
-                + "\t" + "|____/ \\__,_|_|\\_\\___|" + System.lineSeparator();
+    public void processCommand(String commandLine) throws DukeException {
+        String[] commandLineParts = commandLine.split(" ", 2);
 
-        String welcomeMessage = "\t" + "Hello! I'm Duke, your Personal Assistant ChatBot."
-                /* + System.lineSeparator()
-                + logo */
-                + System.lineSeparator()
-                + "\t" + "What can I do for you?";
+        String commandType;
+        String commandInfo;
 
-        printResponse(welcomeMessage);
+        if (commandLineParts.length == 2) {
+            commandType = commandLineParts[0];
+            commandInfo = commandLineParts[1];
+
+            // If user inputs extra text after "bye" or "list" commands
+            if ((commandType.equals("bye") || commandType.equals("list")) && !commandInfo.equals("")) {
+                throw new DukeException("INVALID COMMAND. Please try again!");
+            }
+        } else { // for "bye" and "list" commands
+            commandType = commandLineParts[0];
+            commandInfo = "";
+        }
+
+        executeCommand(commandType, commandInfo);
     }
 
-    public void exit() {
-        String exitMessage = "\t" + "Bye. Hope to see you again soon!";
-        printResponse(exitMessage);
+    public void executeCommand(String commandType, String commandInfo) throws DukeException {
+        int taskNum;
+        Task task;
+        String taskDescription;
+        String taskInfo;
+
+        switch (commandType.toLowerCase()) {
+        case "bye":
+            ui.displayExit();
+            break;
+        case "list":
+            displayTasks();
+            break;
+        case "mark":
+            taskNum = getTaskNumFromMarkCommand(commandInfo);
+            markTask(taskNum);
+            break;
+        case "unmark":
+            taskNum = getTaskNumFromUnmarkCommand(commandInfo);
+            unmarkTask(taskNum);
+            break;
+        case "todo":
+            taskDescription = getTaskDescriptionFromToDoCommand(commandInfo);
+            task = new ToDo(taskDescription);
+            addTask(task);
+            break;
+        case "deadline":
+            taskInfo = processTaskInfoFromDeadlineCommand(commandInfo);
+            String[] deadlineTaskDetails = taskInfo.split("/", 2);
+            task = new Deadline(deadlineTaskDetails[0], deadlineTaskDetails[1]);
+            addTask(task);
+            break;
+        case "event":
+            taskInfo = processTaskInfoFromEventCommand(commandInfo);
+            String[] eventTaskDetails = taskInfo.split("/", 2);
+            task = new Event(eventTaskDetails[0], eventTaskDetails[1]);
+            addTask(task);
+            break;
+        default:
+            throw new DukeException("INVALID COMMAND. Please try again!");
+        }
     }
 
-    public void echo(String message) {
-        printResponse(message);
+    private boolean isCommandInfoPresent(String commandInfo) {
+        return commandInfo.trim().length() > 0;
     }
 
-    public void printResponse(String message) {
-        printLine();
-        System.out.println(message);
-        printLine();
+    private boolean isTaskDescriptionPresent(String taskDescription) {
+        return taskDescription.trim().length() > 0;
     }
 
-    public void printLine() {
-        String horizontalLine = "\t" + "________________________________________________________";
-        System.out.println(horizontalLine);
+    private int getTaskNumFromMarkCommand(String commandInfo) throws DukeException {
+        if (isCommandInfoPresent(commandInfo)) {
+            try {
+                return Integer.parseInt(commandInfo);
+            } catch (NumberFormatException e) {
+                throw new DukeException("Please enter a valid task number to be marked as done!");
+            }
+        } else {
+            throw new DukeException("Please enter a task number to be marked as done!");
+        }
+    }
+
+    private int getTaskNumFromUnmarkCommand(String commandInfo) throws DukeException {
+        if (isCommandInfoPresent(commandInfo)) {
+            try {
+                return Integer.parseInt(commandInfo);
+            } catch (NumberFormatException e) {
+                throw new DukeException("Please enter a valid task number to be marked as not done yet!");
+            }
+        } else {
+            throw new DukeException("Please enter a task number to be marked as not done yet!");
+        }
+    }
+
+    private String getTaskDescriptionFromToDoCommand(String commandInfo) throws DukeException {
+        if (isCommandInfoPresent(commandInfo)) {
+            return commandInfo;
+        } else {
+            throw new DukeException("The description of a todo cannot be empty!");
+        }
+    }
+
+    private String processTaskInfoFromDeadlineCommand(String commandInfo) throws DukeException {
+        if (!isCommandInfoPresent(commandInfo)) {
+            throw new DukeException("INCOMPLETE COMMAND"
+                    + System.lineSeparator() + "\t"
+                    + "The description and date/time of a deadline cannot be empty!"
+                    + System.lineSeparator() + "\t"
+                    + "[Note: Enter /by before specifying the date/time]");
+        } else {
+            String[] taskInfoParts = commandInfo.split("/", 2);
+
+            if (taskInfoParts.length != 2) {
+                throw new DukeException("INCOMPLETE COMMAND"
+                        + System.lineSeparator() + "\t"
+                        + "The date/time of a deadline cannot be empty!"
+                        + System.lineSeparator() + "\t"
+                        + "[Note: Enter /by before specifying the date/time]");
+            } else {
+                return getTaskInfoFromDeadlineCommand(commandInfo, taskInfoParts);
+            }
+        }
+    }
+
+    private String getTaskInfoFromDeadlineCommand(String commandInfo, String[] taskInfoParts) throws DukeException {
+        String taskDescription = taskInfoParts[0];
+        String taskDateTime = taskInfoParts[1];
+
+        if (taskDateTime.startsWith("by")) {
+            String dateTime = taskDateTime.trim();
+            String[] dateTimeParts = dateTime.split("\\s+", 2);
+
+            if (dateTimeParts.length == 2) {
+                if (isTaskDescriptionPresent(taskDescription)) {
+                    return commandInfo;
+                } else {
+                    throw new DukeException("INCOMPLETE COMMAND"
+                            + System.lineSeparator() + "\t"
+                            + "The description of a deadline cannot be empty!");
+                }
+            } else {
+                if (isTaskDescriptionPresent(taskDescription)) {
+                    throw new DukeException("INCOMPLETE COMMAND"
+                            + System.lineSeparator() + "\t"
+                            + "The date/time of a deadline cannot be empty!"
+                            + System.lineSeparator() + "\t"
+                            + "[Note: Enter /by before specifying the date/time]");
+                } else {
+                    throw new DukeException("INCOMPLETE COMMAND"
+                            + System.lineSeparator() + "\t"
+                            + "The description of a deadline cannot be empty!"
+                            + System.lineSeparator() + "\t"
+                            + "The date/time of a deadline cannot be empty!"
+                            + System.lineSeparator() + "\t"
+                            + "[Note: Enter /by before specifying the date/time]");
+                }
+            }
+        } else {
+            if (isTaskDescriptionPresent(taskDescription)) {
+                throw new DukeException("WRONG COMMAND"
+                        + System.lineSeparator() + "\t"
+                        + "Enter /by before specifying the date/time!");
+            } else {
+                throw new DukeException("INCOMPLETE & WRONG COMMAND"
+                        + System.lineSeparator() + "\t"
+                        + "The description of a deadline cannot be empty!"
+                        + System.lineSeparator() + "\t"
+                        + "Enter /by before specifying the date/time!");
+            }
+        }
+    }
+
+    private String processTaskInfoFromEventCommand(String commandInfo) throws DukeException {
+        if (!isCommandInfoPresent(commandInfo)) {
+            throw new DukeException("INCOMPLETE COMMAND"
+                    + System.lineSeparator() + "\t"
+                    + "The description and date/time of an event cannot be empty!"
+                    + System.lineSeparator() + "\t"
+                    + "[Note: Enter /at before specifying the date/time]");
+        } else {
+            String[] taskInfoParts = commandInfo.split("/", 2);
+
+            if (taskInfoParts.length != 2) {
+                throw new DukeException("INCOMPLETE COMMAND"
+                        + System.lineSeparator() + "\t"
+                        + "The date/time of an event cannot be empty!"
+                        + System.lineSeparator() + "\t"
+                        + "[Note: Enter /at before specifying the date/time]");
+            } else {
+                return getTaskInfoFromEventCommand(commandInfo, taskInfoParts);
+            }
+        }
+    }
+
+    private String getTaskInfoFromEventCommand(String commandInfo, String[] taskInfoParts) throws DukeException {
+        String taskDescription = taskInfoParts[0];
+        String taskDateTime = taskInfoParts[1];
+
+        if (taskDateTime.startsWith("at")) {
+            String dateTime = taskDateTime.trim();
+            String[] dateTimeParts = dateTime.split("\\s+", 2);
+
+            if (dateTimeParts.length == 2) {
+                if (isTaskDescriptionPresent(taskDescription)) {
+                    return commandInfo;
+                } else {
+                    throw new DukeException("INCOMPLETE COMMAND"
+                            + System.lineSeparator() + "\t"
+                            + "The description of an event cannot be empty!");
+                }
+            } else {
+                if (isTaskDescriptionPresent(taskDescription)) {
+                    throw new DukeException("INCOMPLETE COMMAND"
+                            + System.lineSeparator() + "\t"
+                            + "The date/time of an event cannot be empty!"
+                            + System.lineSeparator() + "\t"
+                            + "[Note: Enter /at before specifying the date/time]");
+                } else {
+                    throw new DukeException("INCOMPLETE COMMAND"
+                            + System.lineSeparator() + "\t"
+                            + "The description of an event cannot be empty!"
+                            + System.lineSeparator() + "\t"
+                            + "The date/time of an event cannot be empty!"
+                            + System.lineSeparator() + "\t"
+                            + "[Note: Enter /at before specifying the date/time]");
+                }
+            }
+        } else {
+            if (isTaskDescriptionPresent(taskDescription)) {
+                throw new DukeException("WRONG COMMAND"
+                        + System.lineSeparator() + "\t"
+                        + "Enter /at before specifying the date/time!");
+            } else {
+                throw new DukeException("INCOMPLETE & WRONG COMMAND"
+                        + System.lineSeparator() + "\t"
+                        + "The description of an event cannot be empty!"
+                        + System.lineSeparator() + "\t"
+                        + "Enter /at before specifying the date/time!");
+            }
+        }
     }
 
     public void addTask(Task task) {
         tasks.add(task);
 
-        String response = "\t" + "Got it. I've added this task:"
+        String response = ui.taskAddedMessage(task)
                 + System.lineSeparator()
-                + "\t\t" + task
-                + System.lineSeparator()
-                + "\t" + "Now you have " + tasks.size() + (tasks.size() > 1 ? " tasks" : " task") + " in the list.";
-        echo(response);
+                + ui.numOfTasksInListMessage(tasks);
+        ui.displayResponse(response);
     }
 
-    public void listTasks() {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("\t")
-                .append(tasks.size() > 1 ? "Here are the tasks in your list:" : "Here is the task in your list:")
-                .append(System.lineSeparator())
-                .append("\t")
-                .append("[Legend: T = todo, D = deadline, E = event]")
-                .append(System.lineSeparator());
-
-        for (int i = 0; i < tasks.size(); i++) {
-            if (i != 0) {
-                sb.append(System.lineSeparator());
-            }
-
-            sb.append("\t")
-                    .append(i + 1)
-                    .append(". ")
-                    .append(tasks.get(i));
+    public void displayTasks() throws DukeException {
+        if (tasks.size() != 0) {
+            String response = ui.tasksInListMessage(tasks);
+            ui.displayResponse(response);
+        } else {
+            throw new DukeException("There are no tasks in your list!");
         }
-
-        echo(sb.toString());
     }
 
-    public void markTask(int taskNum) {
-        Task task = tasks.get(taskNum - 1);
-        task.setDone();
+    public void markTask(int taskNum) throws DukeException {
+        if (taskNum > 0 && taskNum <= tasks.size()) {
+            Task task = tasks.get(taskNum - 1);
+            task.setDone();
 
-        String message = "\t" + "Nice! I've marked this task as done:"
-                + System.lineSeparator() + "\t\t" + task;
-        echo(message);
+            String message = ui.taskDoneMessage(task);
+            ui.displayResponse(message);
+        } else {
+            throw new DukeException("Task not found. Please try again!");
+        }
     }
 
-    public void unmarkTask(int taskNum) {
-        Task task = tasks.get(taskNum - 1);
-        task.setNotDone();
+    public void unmarkTask(int taskNum) throws DukeException {
+        if (taskNum > 0 && taskNum <= tasks.size()) {
+            Task task = tasks.get(taskNum - 1);
+            task.setNotDone();
 
-        String message = "\t" + "OK, I've marked this task as not done yet:"
-                + System.lineSeparator() + "\t\t" + task;
-        echo(message);
+            String message = ui.taskNotDoneMessage(task);
+            ui.displayResponse(message);
+        } else {
+            throw new DukeException("Task not found. Please try again!");
+        }
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,155 @@
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * A class that handles the interactions with the user.
+ */
+public class Ui {
+    /**
+     * Constructor to initialize an instance of Ui class.
+     */
+    public Ui() {
+    }
+
+    /**
+     * Reads the command line input from the user.
+     *
+     * @return The command line that was entered
+     */
+    public String readCommand() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER COMMAND:");
+        System.out.print("\t");
+        return sc.nextLine().trim();
+    }
+
+    /**
+     * Displays the welcome message.
+     */
+    public void displayWelcome() {
+        String logo = "\t" + " ____        _        " + System.lineSeparator()
+                + "\t" + "|  _ \\ _   _| | _____" + System.lineSeparator()
+                + "\t" + "| | | | | | | |/ / _" + System.lineSeparator()
+                + "\t" + "| |_| | |_| |   <  __/" + System.lineSeparator()
+                + "\t" + "|____/ \\__,_|_|\\_\\___|" + System.lineSeparator();
+
+        String welcomeMessage = "\t" + "Hello! I'm Duke, your Personal Assistant ChatBot."
+                /* + System.lineSeparator()
+                + logo */
+                + System.lineSeparator()
+                + "\t" + "What can I do for you?";
+
+        displayResponse(welcomeMessage);
+        displayLine();
+    }
+
+    /**
+     * Displays the goodbye message on exiting.
+     */
+    public void displayExit() {
+        String exitMessage = "\t" + "Bye. Hope to see you again soon!";
+        displayResponse(exitMessage);
+    }
+
+    /**
+     * Displays the response message with proper formatting.
+     *
+     * @param message Response message to be displayed
+     */
+    public void displayResponse(String message) {
+        displayLine();
+        System.out.println(message);
+    }
+
+    /**
+     * Displays the error message with proper formatting.
+     *
+     * @param message Error message to be displayed
+     */
+    public void displayError(String message) {
+        displayLine();
+        System.out.println("ERROR MESSAGE:");
+        System.out.println("\t" + "☹ " + message);
+    }
+
+    /**
+     * Displays a line to separate different parts of a message.
+     */
+    public void displayLine() {
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine);
+    }
+
+    /**
+     * Returns the message when the task is added.
+     *
+     * @param task Task that is added
+     * @return The string representation of the message
+     */
+    public String taskAddedMessage(Task task) {
+        return "\t" + "Got it. I've added this task:"
+                + System.lineSeparator() + "\t\t" + task;
+    }
+
+    /**
+     * Returns the message when the task is marked as done.
+     *
+     * @param task Task that is marked as done
+     * @return The string representation of the message
+     */
+    public String taskDoneMessage(Task task) {
+        return "\t" + "Nice! I've marked this task as done:"
+                + System.lineSeparator() + "\t\t" + task;
+    }
+
+
+    /**
+     * Returns the message when the task is marked as not done yet.
+     *
+     * @param task Task that is marked as not done yet
+     * @return The string representation of the message
+     */
+    public String taskNotDoneMessage(Task task) {
+        return "\t" + "OK, I've marked this task as not done yet:"
+                + System.lineSeparator() + "\t\t" + task;
+    }
+
+    /**
+     * Returns the message with the number of tasks in the task list.
+     *
+     * @param tasks List containing all the tasks
+     * @return The string representation of the message
+     */
+    public String numOfTasksInListMessage(List<Task> tasks) {
+        return "\t" + "Now you have " + tasks.size()
+                + (tasks.size() > 1 ? " tasks" : " task")
+                + " in the list.";
+    }
+
+    /**
+     * Returns the message with all the tasks in the task list.
+     *
+     * @param tasks List containing all the tasks
+     * @return The string representation of the message
+     */
+    public String tasksInListMessage(List<Task> tasks) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\t")
+                .append(tasks.size() > 1 ? "Here are the tasks in your list:" : "Here is the task in your list:")
+                .append(System.lineSeparator())
+                .append("\t")
+                .append("[Legend: T = todo, D = deadline, E = event]")
+                .append(System.lineSeparator());
+
+        for (int i = 0; i < tasks.size(); i++) {
+            if (i != 0) {
+                sb.append(System.lineSeparator());
+            }
+
+            sb.append("\t").append(i + 1).append(". ").append(tasks.get(i));
+        }
+
+        return sb.toString();
+    }
+}
