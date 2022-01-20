4b0cb6f47a3d39fdda12600c8327142697cbaf65
ian-from-dover <ian09hong@gmail.com>
2022-01-20 21:26:43 +0800

    Made Lily be able to Add and List Todos

--- src/main/java/Lily.java
+++ src/main/java/Lily.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 public class Lily {
     public static void main(String[] args) {
         boolean listening = true;
+        String[] list = new String[100];
+        int listIdx = 0;
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼");
@@ -19,24 +22,41 @@ public class Lily {
                 "    Need help with something?\n");
         System.out.println("    Commands you can type");
         System.out.println("    > bye: stop talking with Lily");
-        System.out.println("\n    If you type anything else, I'll repeat it back to you.");
+        System.out.println("\n    If you type anything else, I'll repeat it back to you."); // update this when new functionality is added
         System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n");
 
         // case statements
-        while (listening) {
+        userInteracting: while (listening) {
             String action = sc.nextLine();
-            if (action.equals("bye")) {
-                listening = false;
-                System.out.println("");
-                System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼");
-                System.out.println("    see ya.");
-                System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n");
-                break;
-            } else {
-                System.out.println("");
-                System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼");
-                System.out.println("    " + action);
-                System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n");
+            switch(action) {
+                case "bye":
+                    listening = false;
+                    System.out.println("");
+                    System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼");
+                    System.out.println("    see ya.");
+                    // System.out.println("    hmph.");
+                    System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n");
+                    break userInteracting;
+                case "list":
+                    String todo = "";
+                    for (int i = 0; i < listIdx; i++) {
+                        todo += ("    " + (i + 1) + ". " + list[i]);
+                        todo += i == (listIdx - 1) ? "" : "\n";
+                    }
+                    System.out.println("");
+                    System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼");
+                    System.out.println("    You told me you had to");
+                    System.out.println(todo);
+                    System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n");
+                    break;
+                default:
+                    list[listIdx] = action;
+                    listIdx++;
+                    System.out.println("");
+                    System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼");
+                    System.out.println("    i've dumped your \"" + action + "\" into your list");
+                    System.out.println("    you happy?");
+                    System.out.println("    ▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n");
             }
         }
         sc.close();
