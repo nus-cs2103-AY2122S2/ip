032cef60e5c77d2f314f413844a4d27bf17a4e10
dineshraj555 <e0652525@u.nus.edu>
2022-01-20 08:57:04 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,6 +7,12 @@ public class Duke {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String line = "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+        String start = "              Greetings! Mike here! \n" +
+                       "               How can I help you? \n" ;
+        String gotit = "Got it. I've added this task:\n ";
+        String mark = "Nice! I've marked this task as";
+        String now1 = "Now you have ";
+        String now2 = " tasks in the list.";
         String logo =
                   "||======||==||======|| !!!! ||====||    //===//======||   \n"
                 + "||                  || !!!! ||    ||   //   //       ||\n"
@@ -17,7 +23,7 @@ public class Duke {
                 + "||==||  ||==||  ||==||======||====||    \\\\===\\\\======|| \n";
 
         System.out.println("\n" +logo + line);
-        System.out.println("Greetings! Mike here! \nHow can I help you?" + line);
+        System.out.println(start+ line);
 
         Task list[] = new Task[100];
         int n = 0;
@@ -38,14 +44,24 @@ public class Duke {
             } else if (c[0].equals("mark")) {
                 int no = Integer.parseInt(c[1]) - 1;
                 list[no].markAsDone();
-                System.out.println(line + "Nice! I've marked this task as done:\n " + list[no].toString() + line);
+                System.out.println(line + mark + " as done:\n " + list[no].toString() + line);
             } else if (c[0].equals("unmark")) {
                 int no = Integer.parseInt(c[1]) - 1;
                 list[no].markAsUnDone();
-                System.out.println(line + "Nice! I've marked this task as not done yet:\n " + list[no].toString() + line);
-            } else {
-                System.out.println(line + "added: " + cmd + line);
-                list[n] = new Task(cmd);
+                System.out.println(line + mark + " as not done yet:\n " + list[no].toString() + line);
+            } else if (c[0].equals("todo")) {
+                list[n] = new Todo(cmd.substring(4));
+                System.out.println(line + gotit + list[n].toString() + "\n" + now1 + (n+1) + now2 + line);
+                n+=1;
+            } else if (c[0].equals("deadline")) {
+                String[] x = cmd.substring(8).split("/by ");
+                list[n] = new Deadline(x[0],x[1]);
+                System.out.println(line + gotit + list[n].toString() + "\n" + now1 + (n+1) + now2 + line);
+                n+=1;
+            } else if (c[0].equals("event")) {
+                String[] x = cmd.substring(5).split("/at ");
+                list[n] = new Event(x[0],x[1]);
+                System.out.println(line + gotit + list[n].toString() + "\n" + now1 + (n+1) + now2 + line);
                 n+=1;
             }
             cmd = br.readLine();
