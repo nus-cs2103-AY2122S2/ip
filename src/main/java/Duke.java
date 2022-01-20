f6510e2307dcf3b7a357f0b24f7b99b1b5914536
MontyPython28 <samyusubs@gmail.com>
2022-01-20 22:09:00 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,15 +8,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        System.out.println("Hello from\n" + logo);
-
         Scanner sc = new Scanner(System.in);
+        String[] lst = new String[100];
+        int currIndex = 0;
         System.out.println("____________________________________________________________\nHello! I'm Duke\nWhat can I do for you?\n____________________________________________________________");
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println("____________________________________________________________\n" +
-                    input + "\n" +
-                    "____________________________________________________________");
+            if (input.equals("list")) {
+                System.out.println("____________________________________________________________\n");
+                for (int i=0; i < currIndex; i++ ) {
+                    System.out.println(String.valueOf(i+1) + ". " + lst[i]);
+                }
+                System.out.println("____________________________________________________________\n");
+            }
+            else {
+                System.out.println("____________________________________________________________\n" +
+                        "added: " + input + "\n" +
+                        "____________________________________________________________");
+                lst[currIndex] = input;
+                currIndex += 1;
+            }
             input = sc.nextLine();
         }
         System.out.println("____________________________________________________________\n" +
