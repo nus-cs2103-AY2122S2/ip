36793f8814c042fd62907f0676d9c0bd08fa0f19
louisdavinlie <louisdavinlie@gmail.com>
2022-01-20 20:27:54 +0800

    Implement exceptions
    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,9 +24,6 @@ public class Duke {
             String[] taskAndTime = instructions[1].split("/at");
             task = new Event(taskAndTime[0], taskList.size() + 1, taskAndTime[1]);
             System.out.println("Added an event.");
-        } else {
-            task = new Task(String.join(" ", instructions), taskList.size() + 1);
-            System.out.println("Added: " + String.join(" ", instructions));
         }
         if (task != null) {
             taskList.add(task);
@@ -65,6 +62,41 @@ public class Duke {
         System.out.println(hl);
     }
 
+    public static void checkCommand(String[] instructions) throws DukeException {
+        if (instructions.length == 0) {
+            throw new DukeException("Empty input.");
+        } else {
+            String command = instructions[0];
+            if (command.equals("bye") || command.equals("list")) {
+                if (instructions.length > 1) {
+                    throw new DukeException("There's too many input!");
+                }
+            } else if (command.equals("mark") || command.equals("unmark")) {
+                if (instructions.length == 1) {
+                    throw new DukeException("Please specify the task number to " + command + ".");
+                }
+                int taskNumber = Integer.parseInt(instructions[1]);
+                if (taskNumber > taskList.size() || taskNumber <= 0) {
+                    throw new DukeException("Invalid task number! You have " + taskList.size() + " task(s).");
+                }
+            } else if (command.equals("todo")) {
+                if (instructions.length == 1) {
+                    throw new DukeException("Please specify the task you want to do.");
+                }
+            } else if (command.equals("deadline") || command.equals("event")) {
+                if (instructions.length == 1) {
+                    throw new DukeException("Please specify the description and time of the " + command + " you want to add.");
+                }
+                String[] taskAndTime = instructions[1].split("/");
+                if (taskAndTime.length < 2 || taskAndTime[0].equals("") || taskAndTime[1].length() < 3) {
+                    throw new DukeException("Invalid input. Please specify the description/time of the " + command + ".");
+                }
+            } else {
+                throw new DukeException("I don't understand your query.");
+            }
+        }
+    }
+
     public static void chat() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -73,26 +105,30 @@ public class Duke {
             System.out.print("> ");
             String userInput = br.readLine();
             String[] instructions = userInput.split(" ", 2);
-            if (instructions[0].equals("bye")) {
-                bye();
-                break;
-            } else if (instructions[0].equals("list")) {
-                list();
-            } else if (instructions[0].equals("mark")) {
-                int taskNumber = Integer.parseInt(instructions[1]);
-                mark(taskNumber);
-            } else if (instructions[0].equals("unmark")) {
-                int taskNumber = Integer.parseInt(instructions[1]);
-                unmark(taskNumber);
-            } else {
-                add(instructions);
+            try {
+                checkCommand(instructions);
+                if (instructions[0].equals("bye")) {
+                    break;
+                } else if (instructions[0].equals("list")) {
+                    list();
+                } else if (instructions[0].equals("mark")) {
+                    int taskNumber = Integer.parseInt(instructions[1]);
+                    mark(taskNumber);
+                } else if (instructions[0].equals("unmark")) {
+                    int taskNumber = Integer.parseInt(instructions[1]);
+                    unmark(taskNumber);
+                } else {
+                    add(instructions);
+                }
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
 
     public static void main(String[] args) throws IOException {
         greetings();
-
         chat();
+        bye();
     }
 }
