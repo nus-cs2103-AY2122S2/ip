c7c7185b69386158adf43a9c2bff0a67daf8f31d
joey-chance <joey.chance@u.nus.edu>
2022-01-20 10:37:41 +0800

    level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,22 +16,39 @@ public class Duke {
         read();
     }
     public static void read() {
-        LinkedList<String> tasks = new LinkedList<>();
+        LinkedList<Task> tasks = new LinkedList<>();
         String input;
         Scanner sc = new Scanner(System.in);
-        input = sc.nextLine();
+        input = sc.next();
         while (!input.equalsIgnoreCase("bye")) {
             System.out.println("___________________________________________________________");
-            if (input.equalsIgnoreCase("list")) {
-                for (int i = 1; i <= tasks.size(); i++) {
-                    System.out.println(i + ". " + tasks.get(i - 1));
-                }
-            } else {
-                tasks.add(input);
-                System.out.println("added: " + input);
+            int id;
+            switch(input) {
+                case "list" :
+                    for (int i = 1; i <= tasks.size(); i++) {
+                        System.out.println(i + "." + tasks.get(i - 1));
+                    }
+                    break;
+                case "mark" :
+                    id = sc.nextInt();
+                    tasks.get(id - 1).markAsDone();
+                    System.out.println("Good job sir, this task is marked done:");
+                    System.out.println("  " + tasks.get(id - 1));
+                    break;
+                case "unmark" :
+                    id = sc.nextInt();
+                    tasks.get(id - 1).markAsNotDone();
+                    System.out.println("Slacking off, sir? This task is marked not done:");
+                    System.out.println("  " + tasks.get(id - 1));
+                    break;
+                default :
+                    sc.reset();
+                    input += sc.nextLine();
+                    tasks.add(new Task(input));
+                    System.out.println("added: " + input);
             }
             System.out.println("___________________________________________________________");
-            input = sc.nextLine();
+            input = sc.next();
         }
         System.out.println("___________________________________________________________");
         System.out.println("Good bye, sir.");
