5c01105a3f04e941da56ef15737995ba3b1015a3
Justinnje <e0543778@u.nus.edu>
2022-01-20 22:47:29 +0800

    cleaned up the Main file.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,36 +4,37 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-//        try {
-            Scanner scanner = new Scanner(System.in);
-            Control control = new Control();
-            while (true) {
-                String command = scanner.nextLine();
-                String commandApp = command + " ";
-                String[] commandArr = commandApp.split(" ");
-                String firstWord = commandArr[0];
-                if (firstWord.equals("bye")) {
-                    control.bye();
-                    break;
-                }else if (firstWord.equals("list")) {
-                    control.list();
-                } else if (firstWord.equals("mark") || firstWord.equals("unmark")) {
-                    control.taskCheck(command);
-                } else if (firstWord.equals("todo")) {
-                    control.todo(command);
-                } else if (firstWord.equals("deadline")) {
-                    control.deadline(command);
-                } else if (firstWord.equals("event")) {
-                    control.event(command);
-                } else {
-                    System.out.println(firstWord + " is a invalid Task");
-                }
-                System.out.println("________________________________");
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        Control control = new Control();
+        while (true) {
+            String command = scanner.nextLine();
+            String commandApp = command + " ";
+            String[] commandArr = commandApp.split(" ");
+            String firstWord = commandArr[0];
+            if (firstWord.equals("bye")) {
+                control.bye();
+                break;
+            }else if (firstWord.equals("list")) {
+                control.list();
+            } else if (firstWord.equals("mark") || firstWord.equals("unmark")) {
+                control.taskCheck(command);
+            } else if (firstWord.equals("todo")) {
+                control.todo(command);
+            } else if (firstWord.equals("deadline")) {
+                control.deadline(command);
+            } else if (firstWord.equals("event")) {
+                control.event(command);
+            } else {
+                System.out.println(firstWord + " is a invalid Task");
             }
-//        } catch (NumberFormatException e) {
-//            // System.out.println("An invalid task index has been inputted");
-//        }
-
+            System.out.println("________________________________");
+        }
     }
 //    public static void main(String[] args) {
 //        String logo = " ____        _        \n"
