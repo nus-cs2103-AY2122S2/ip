248b30507720e76d86854adc7a25505aeb4a2ab7
juayhee <e0524722@u.nus.edu>
2022-01-20 14:39:10 +0800

    Task inheritance * Updated Task.java to become a parent class of more specific subtpyes of classes.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,8 @@
 public  class Task {
     private String input;
     private boolean isComplete = false;
+    private String type;
+    private String description;
     
     public Task(String input) {
         this.input = input;
@@ -14,16 +16,49 @@ public  class Task {
         this.isComplete = false;
     }
 
+    /**
+     * Gets the symbol for whether this task is complete or not
+     * @return A string symbol
+     */
+    private String getDoneSymbol() {
+        if (this.isComplete) {
+            return "[X]";
+        } else {
+            return "[ ]";
+        }
+    }
+
+    /**
+     * Returns description of task
+     * @return Description string
+     */
+    private String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Gets the symbol for the type of this task
+     * @return A string symbol
+     */
+    private String getTaskTypeSymbol() {
+        if (this.type.equals("todo")) {
+            return "[T]";
+        } else if (this.type.equals("deadline")) {
+            return "[D]";
+        } else if (this.type.equals("event")) {
+            return "[E]";
+        } else return null;
+    }
+
     @Override
     public String toString() {
-        String returnString;
-        if (this.isComplete) {
-            returnString = "[X] " + input;
-        } else {
-            returnString  = "[ ] " + input;
-        }
+        String doneSymbol = this.getDoneSymbol();
+        String taskTypeSymbol = this.getTaskTypeSymbol();
+        String description = this.getDescription();
 
-        return returnString;
+        String output = doneSymbol + taskTypeSymbol + " " + description;
+
+        return output;
     }
 
 }
