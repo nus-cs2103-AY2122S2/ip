8009280d3ebac38454bcf49d71b11f5938268270
SimJM <SimJM@users.noreply.github.com>
2022-01-20 16:38:46 +0800

    Level-2: added readBook(), returnBook()

--- src/main/java/Action.java
+++ src/main/java/Action.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Action {
 
     Action() {}
@@ -11,15 +14,41 @@ public class Action {
 
     void echo(String phrase) {
         String dash = "____________________________________________________________\n";
-        String output = String.format("%s%s%s", dash, phrase, dash);
+        String output = String.format("%s%s\n%s", dash, phrase, dash);
         System.out.println(output);
     }
 
     void bye() {
         String dash = "____________________________________________________________\n";
-        String bye = "Bye. Hope to see you again soon!";
+        String bye = "Bye. Hope to see you again soon!\n";
         String output = String.format("%s%s%s", dash, bye, dash);
         System.out.println(output);
+    }
+
+    void readBook(ArrayList<String> arrlst) {
+        arrlst.add("read book");
+        String dash = "____________________________________________________________\n";
+        String readBook = "added: read book\n";
+        String output = String.format("%s%s%s", dash, readBook, dash);
+        System.out.println(output);
+    }
+
+    void returnBook(ArrayList<String> arrlst) {
+        arrlst.add("return book");
+        String dash = "____________________________________________________________\n";
+        String returnBook = "added: return book\n";
+        String output = String.format("%s%s%s", dash, returnBook, dash);
+        System.out.println(output);
+    }
+
+    void list(ArrayList<String> arrlst) {
+        String dash = "____________________________________________________________\n";
+        System.out.println(dash);
+        for (int i =  0; i < arrlst.size(); i++) {
+            String output = String.format("%d. %s\n", i + 1, arrlst.get(i));
+            System.out.println(output);
+        }
+        System.out.println(dash);
     }
 
     @Override
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
+        int counter = 0;
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> arrlst = new ArrayList<>();
         String[] lst = new String[100];
         Action act = new Action();
 
         act.greet();
 
         while (sc.hasNext()) {
-            String word = sc.next();
+            String word = sc.nextLine();
             if (word.equals("bye")) {
                 act.bye();
                 break;
+            } else if (word.equals("list")) {
+                act.list(arrlst);
+            } else if (word.equals("read book")) {
+                act.readBook(arrlst);
+                counter++;
+            } else if (word.equals("return book")) {
+                act.returnBook(arrlst);
+                counter++;
             } else {
                 act.echo(word);
             }
