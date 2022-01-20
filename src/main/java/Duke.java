f793428191bc5077ecd74b75df980edaa2151b99
Thomas Cherian <tomc2407@gmail.com>
2022-01-20 23:02:32 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,19 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String newline = "----------------------------------------------------\n";
+        System.out.println("\n" + newline + newline + logo);
+        System.out.println("Hello there! I'm Duke, your very own Personal Assistant ChatBot.\nHow may I help you?");
+        System.out.println(newline);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(newline + "Goodbye! See you soon. :)\n" + newline);
+                break;
+            } else {
+                System.out.println(newline + input + "\n" + newline);
+            }
+        }
     }
 }
