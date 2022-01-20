0ae915c5bc333165d329645c0c4f5a1d12519b50
Justine Koh Yong Tai <>
2022-01-20 16:47:28 +0800

    Added Task class and updated chatbot to utilize Tasks. Added Mark and Unmark functionalities.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,20 +11,42 @@ public class Duke {
         System.out.println(line +
                 "    Hello! I'm Bob!\n    What can I do for you?\n" + line);
         Scanner sc = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
         String cmd = sc.nextLine();
 
         while (!cmd.equals("bye")) {
+
+            // list functionality
             if (cmd.equals("list")) {
                 int count = 1;
-                System.out.print(line);
-                for (String entry : list) {
-                    System.out.println("    " + count++ + ". " + entry);
+                System.out.println(line + "    Here are the tasks in your list:");
+                for (Task entry : list) {
+                    System.out.println("    " + count++ + "." + entry);
                 }
                 System.out.println(line);
-            } else {
-                list.add(cmd);
+            }
+            // mark functionality
+            else if (cmd.split(" ")[0].equals("mark")) {
+                int idx = Integer.parseInt(cmd.split(" ")[1]);
+                Task currTask = list.get(idx - 1);
+                currTask.toggleDone();
+
+                System.out.println(line + "    Nice! I've marked this task as done:\n      " + currTask);
+                System.out.println(line);
+            }
+            // unmark functionality
+            else if (cmd.split(" ")[0].equals("unmark")) {
+                int idx = Integer.parseInt(cmd.split(" ")[1]);
+                Task currTask = list.get(idx - 1);
+                currTask.toggleNotDone();
+
+                System.out.println(line + "    OK, I've marked this task as not done yet:\n      " + currTask);
+                System.out.println(line);
+            }
+            // add functionality
+            else {
+                list.add(new Task(cmd));
                 System.out.println(line + "    added: " + cmd + "\n" + line);
             }
             cmd = sc.nextLine();
@@ -32,3 +54,4 @@ public class Duke {
         System.out.println(line + "    Bye. Hope to see you again soon!\n" + line);
     }
 }
+
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " ");
+    }
+
+    public void toggleStatus() {
+        this.isDone = !this.isDone;
+    }
+
+    public void toggleDone() {
+        this.isDone = true;
+    }
+
+    public void toggleNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
