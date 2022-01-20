6a2ea629345ecd5a11005ec0e3aa35cd1c542d44
markbiju <markbiju16@gmail.com>
2022-01-20 18:02:03 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
+        int counter = 0;
+        String[] items = new String[100];
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
@@ -11,8 +14,14 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")){
+                for(int i = 0; i < counter; i++) {
+                    System.out.println(Integer.toString(i + 1) +". " + items[i]);
+                }
             } else {
-                System.out.println(input);
+                items[counter] = input;
+                counter = counter + 1;
+                System.out.println("added: " + input);
             }
         }
     }
