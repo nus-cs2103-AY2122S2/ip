43a8be6e250fdfdbd600d8ca42bf3ef026877e4a
Gerald Tan <tanjunweigerald1997@gmail.com>
2022-01-20 12:14:31 +0800

    Error Handling through creation of DukeException class
    Explanations for each error provided in code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,7 +9,7 @@ public class Duke {
         userTasks = new ArrayList<Task>();
     }
 
-    public void print(String str){
+    public void print(Object str){
         System.out.println(str);
         System.out.println("_".repeat(100));
     }
@@ -27,13 +27,13 @@ public class Duke {
     public void addDeadline(String userDeadlineString, String deadline){
         Deadline userDeadline = new Deadline(userDeadlineString, deadline);
         this.userTasks.add(userDeadline);
-        this.print("Perfect! You have added this Deadline Item:\n" + userDeadline + "\n Remember to stick to your objective date!");
+        this.print("Perfect! You have added this Deadline Item:\n" + userDeadline + "\nRemember to stick to your objective date!");
     }
 
     public void addEvent(String userEventString, String timing){
         Event userEvent = new Event(userEventString, timing);
         this.userTasks.add(userEvent);
-        this.print("Perfect! You have added this Event Item:\n" + userEvent + "\n Remember to be there 5 minutes early!");
+        this.print("Perfect! You have added this Event Item:\n" + userEvent + "\nRemember to be there 5 minutes early!");
     }
 
     public void printUserTasks(){
@@ -67,74 +67,109 @@ public class Duke {
         }
     }
 
-    public void markTaskDone(int taskIndex){ //This is the actual index for arraylist checking (minus one already)
+    public void markTaskDone(int taskIndex) throws DukeException { //This is the actual index for arraylist checking (minus one already)
         if (checkValidTask(taskIndex)) {
             Task task = this.userTasks.get(taskIndex);
             task.markAsDone();
             this.print("Congratulations on completing the task!\n" + task);
         } else {
-            this.print("There is no such task! Maybe you entered the wrong task?");
+            throw new DukeException("There is no such task! Maybe you entered the wrong task?");
         }
     }
 
-    public void markTaskNotDone(int taskIndex){ //This is the actual index for arraylist checking (minus one already)
+    public void markTaskNotDone(int taskIndex) throws DukeException{ //This is the actual index for arraylist checking (minus one already)
         if (checkValidTask(taskIndex)) {
             Task task = this.userTasks.get(taskIndex);
             task.markAsNotDone();
             this.print("I have un-marked this task!\n" + task);
         } else {
-            this.print("There is no such task! Maybe you entered the wrong task?");
+            throw new DukeException("There is no such task! Maybe you entered the wrong task?");
         }
     }
 
-    public void performCommand(String userTaskString){
+    public void performCommand(String userTaskString) throws DukeException{
             if (userTaskString.equalsIgnoreCase("bye")){
                 this.printBye();
                 System.exit(0);
             } else if (userTaskString.length() == 0){
-                this.print("You have not keyed in any task! Maybe you missed something? Don't worry, just try again!");
+                throw new DukeException("You have not keyed in any task! Maybe you missed something? Don't worry, just try again!");
             } else if (userTaskString.equalsIgnoreCase("list")){
                 this.printUserTasks();
             } else if (userTaskString.matches("^[markMARK]{4}.*")){
                 Pattern pattern = Pattern.compile("^[markMARK]{4}");
                 Matcher matcher = pattern.matcher(userTaskString);
                 if (matcher.find()) { // Finds first match (finds a match for each time .find() is run)
-                    this.markTaskDone(Integer.parseInt(userTaskString.substring(matcher.end()+1))-1);
+                    try {
+                        this.markTaskDone(Integer.parseInt(userTaskString.substring(matcher.end()+1))-1);
+                    } catch (StringIndexOutOfBoundsException err) { // For cases like "mark" without a number
+                        throw new DukeException("Please enter a task number to mark!");
+                     }catch (NumberFormatException err) { // For cases like "mark1" without a space in between
+                        throw new DukeException("Please mark a task in the following format:\n" +
+                                "mark [number]");
+                    }
                 }
             } else if (userTaskString.matches("^[unmarkUNMARK]{6}.*")){
                 Pattern pattern = Pattern.compile("^[unmarkUNMARK]{6}");
                 Matcher matcher = pattern.matcher(userTaskString);
                 if (matcher.find()) { // Finds first match (finds a match for each time .find() is run)
-                    this.markTaskNotDone(Integer.parseInt(userTaskString.substring(matcher.end()+1))-1);
+                    try {
+                        this.markTaskNotDone(Integer.parseInt(userTaskString.substring(matcher.end()+1))-1);
+                    } catch (StringIndexOutOfBoundsException err) { // For cases like "unmark" without a number
+                        throw new DukeException("Please enter a task number to unmark!");
+                    } catch (NumberFormatException err) { // For cases like "unmark1" without a space in between
+                        throw new DukeException("Please unmark a task in the following format:\n" +
+                                "unmark [number]");
+                    }
                 }
             } else if (userTaskString.matches("^[todTOD]{4}.*")){
                 Pattern pattern = Pattern.compile("^[todTOD]{4}");
                 Matcher matcher = pattern.matcher(userTaskString);
                 if (matcher.find()) { // Finds first match (finds a match for each time .find() is run)
-                    this.addTodo(userTaskString.substring(matcher.end()+1));
+                    try {
+                        this.addTodo(userTaskString.substring(matcher.end()+1));
+                    } catch (StringIndexOutOfBoundsException err) { //For cases like "todo" without any further description
+                        throw new DukeException("Please enter a description!");
+                    }
                 }
             } else if (userTaskString.matches("^[dealinDEALIN]{8}.*")){
                 Pattern pattern = Pattern.compile("^[dealinDEALIN]{8}");
                 Matcher matcher = pattern.matcher(userTaskString);
                 if (matcher.find()) { // Finds first match (finds a match for each time .find() is run)
-                    String userTaskStringSliced = userTaskString.substring(matcher.end()+1);
-                    String[] descriptionDeadlineSplit = userTaskStringSliced.split(" /by ", 2);
-                    this.addDeadline(descriptionDeadlineSplit[0], descriptionDeadlineSplit[1]);
+                    try {
+                        String userTaskStringSliced = userTaskString.substring(matcher.end()+1);
+                        String[] descriptionDeadlineSplit = userTaskStringSliced.split(" /by ", 2);
+                        this.addDeadline(descriptionDeadlineSplit[0], descriptionDeadlineSplit[1]);
+                    } catch (StringIndexOutOfBoundsException err) { // For cases like "deadline" without any other information
+                        throw new DukeException("Please enter a Deadline in the following format:\n" +
+                                "deadline [description] /by [datetime]");
+                    } catch (ArrayIndexOutOfBoundsException err) { //For cases like "deadline homework" without a "/by"
+                        throw new DukeException("Please enter a Deadline in the following format:\n" +
+                                "deadline [description] /by [datetime]");
+                    }
+
                 }
             } else if (userTaskString.matches("^[evntEVNT]{5}.*")){
                 Pattern pattern = Pattern.compile("^[evntEVNT]{5}");
                 Matcher matcher = pattern.matcher(userTaskString);
                 if (matcher.find()) { // Finds first match (finds a match for each time .find() is run)
-                    String userTaskStringSliced = userTaskString.substring(matcher.end()+1);
-                    String[] descriptionTimingSplit = userTaskStringSliced.split(" /at ", 2);
-                    this.addEvent(descriptionTimingSplit[0], descriptionTimingSplit[1]);
+                    try {
+                        String userTaskStringSliced = userTaskString.substring(matcher.end()+1);
+                        String[] descriptionTimingSplit = userTaskStringSliced.split(" /at ", 2);
+                        this.addEvent(descriptionTimingSplit[0], descriptionTimingSplit[1]);
+                    } catch (StringIndexOutOfBoundsException err) { // For cases like "event" without any other information
+                        throw new DukeException("Please enter an Event in the following format:\n" +
+                                "event [description] /at [datetime]");
+                    } catch (ArrayIndexOutOfBoundsException err) { //For cases like "event meeting" without a "/at"
+                        throw new DukeException("Please enter an Event in the following format:\n" +
+                                "event [description] /at [datetime]");
+                    }
                 }
-            } else { // If a user is adding tasks
-                this.print("Invalid Input! Please either add in a Todo, Deadline or Event!");
+            } else { // If a user does not specify a todo, deadline or event
+                throw new DukeException("Invalid Input! Please either add in a Todo, Deadline or Event!");
             }
         }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException{
         Scanner sc= new Scanner(System.in);
         Duke duke = new Duke();
         duke.print("Hello, My Dear Friend... I'm Duke, your personal motivator!");
@@ -143,7 +178,11 @@ public class Duke {
         duke.print("What can i do for you today?");
         while (true) {
             String userTaskString = sc.nextLine();
-            duke.performCommand(userTaskString);
+            try {
+                duke.performCommand(userTaskString);
+            } catch (DukeException exception){
+                duke.print(exception);
+            }
         }
     }
 }
