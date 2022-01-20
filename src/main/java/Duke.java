ea84b25461c35b61fb9277e59c2b181b5c52e7e8
Yong Rui <tanyongruifenway@gmail.com>
2022-01-20 17:30:40 +0800

    Level-6: added delete function, tidied up code, rearranged strings, added helper functions.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,18 +2,26 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import chatbot.*;
 public class Duke {
-    protected static String straightLine = "____________________________________________________________";
-    
+    // strings to use
+    public static String straightLine = "____________________________________________________________";
+    public static String introductionMessage = "Good day Sir. My name is Dook. \n How may I be of assistance?";
+    public static String byeMessage = "Farewell Sir. May you have a wonderful day.";
+    public static String noSuchTask = "I'm very sorry Sir, there is no such task you mentioned.";
+    public static String notValidNumber = "Please enter a valid number Sir.";
+    public static String commandRequiresNumber = "Sorry Sir, this command requires a number.";
+    public static String unrecognizedCommand = "Sorry Sir, I do not understand that command.";
+
+    ////////////////////////////////////////////////////////////////
+    // Main Methods
+
     // introduction message
     public static void displayGreeting() {
-        String introductionMessage = "Good day Sir. My name is Dook. \n How may I be of assistance?";
-        System.out.println(straightLine + "\n " + introductionMessage + "\n" + straightLine);
+        System.out.println(createReply(introductionMessage));
     }
 
     // farewell message
     public static void displayFarewell() {
-        String byeMessage = "Farewell Sir. May you have a wonderful day.";
-        System.out.println(straightLine + "\n " + byeMessage + "\n" + straightLine);
+        System.out.println(createReply(byeMessage));
     }
 
     // insert new Task
@@ -67,7 +75,7 @@ public class Duke {
         reply += "Very well Sir. I have added this task:";
         reply += "\n   " + newTask.toString();
         reply += "\n You now have " + taskListLength + " tasks in the agenda Sir.";
-        System.out.println(straightLine + "\n " + reply + "\n" + straightLine);
+        System.out.println(createReply(reply));
     }
 
     // display task list
@@ -80,45 +88,90 @@ public class Duke {
             counter++;
         }
         String listHeader = "This is your agenda of tasks Sir:";
-        System.out.println(straightLine + "\n " + listHeader + reply + "\n" + straightLine);
+        System.out.println(createReply(listHeader + reply));
     }
 
     // mark / unmark command
-    public static void markCommand(String[] inputStringArray, ArrayList<Task> taskList) throws DukeException {
+    public static void markCommand(String[] inputStringArray, ArrayList<Task> taskList, String type) throws DukeException {
         // check command
         boolean markAsDone = false;
-        if (inputStringArray[0].equals("mark")) {
+        if (type.equals("mark")) {
             markAsDone = true;
-        } else if (inputStringArray[0].equals("unmark")) {
+        } else if (type.equals("unmark")) {
             markAsDone = false;
-        } else { // not a mark / unmark command
-            throw new DukeException("Sorry Sir, I do not understand that command.");
+        } else { 
+            
         }
         
         // check if there is an integer after the text command input
-        String inputNumberString = inputStringArray[1];
         try {
+            String inputNumberString = inputStringArray[1];
             Integer inputNumberInteger = Integer.parseInt(inputNumberString);
             String reply = "";
+            
             int taskIndex = inputNumberInteger - 1;
             if (taskIndex >= taskList.size()) { // invalid integer input
-                reply = "I'm very sorry Sir, there is no such task you mentioned.";
-            } else {
-                Task task = taskList.get(taskIndex);
-                task.markTask(markAsDone);
-                
-                reply = "Very well Sir, I have marked this task as incomplete: \n";
-                if (markAsDone) {
-                    reply = "Very well Sir, I have marked this task as complete: \n";
-                }
-                reply += "   " + task.toString();
-            }
-            System.out.println(straightLine + "\n " + reply + "\n" + straightLine);
+                throw new DukeException(noSuchTask);
+            } 
 
-        } catch (NumberFormatException exception) { // not a number, hence not a mark / unmark command
-            throw new DukeException("Please enter a valid number Sir.");
+            Task task = taskList.get(taskIndex);
+            task.markTask(markAsDone);
+            
+            if (markAsDone) {
+                reply += "Very well Sir, I have marked this task as complete: ";
+            } else {
+                reply += "Very well Sir, I have marked this task as incomplete: ";
+            }
+            reply += "\n   " + task.toString();
+
+            System.out.println(createReply(reply));
+
+        } catch (NumberFormatException exception) { // not a number
+            throw new DukeException(notValidNumber);
+        } catch (ArrayIndexOutOfBoundsException exception) { // no input together with command
+            throw new DukeException(commandRequiresNumber);
         }
     }
+
+    // delete command
+    public static void deleteTask(String[] inputStringArray, ArrayList<Task> taskList) throws DukeException {
+        
+        // check if there is an integer after the text command input
+        try {
+            String inputNumberString = inputStringArray[1];
+            Integer inputNumberInteger = Integer.parseInt(inputNumberString);
+            String reply = "";
+            
+            int taskIndex = inputNumberInteger - 1;
+            if (taskIndex >= taskList.size()) { // invalid integer input
+                throw new DukeException(noSuchTask);
+            } 
+            
+            Task taskToRemove = taskList.get(taskIndex);
+            taskList.remove(taskToRemove);
+            String taskListLength = Integer.toString(taskList.size());
+
+            reply += "Very well Sir. I have removed this task:";
+            reply += "\n   " + taskToRemove.toString();
+            reply += "\n You now have " + taskListLength + " tasks in the agenda Sir.";
+            
+            System.out.println(createReply(reply));
+
+        } catch (NumberFormatException exception) { // not a number
+            throw new DukeException(notValidNumber);
+        } catch (ArrayIndexOutOfBoundsException exception) { // no input together with command
+            throw new DukeException(commandRequiresNumber);
+        } 
+    }
+
+    ////////////////////////////////////////////////////////////////
+    // Helper Methods
+
+    // inserts and formats the straight lines into the reply
+    public static String createReply(String reply) {
+        return straightLine + "\n " + reply + "\n" + straightLine;
+    }
+
     public static void main(String[] args) {
         // String logo = " ____        _        \n"
         //         + "|  _ \\ _   _| | _____ \n"
@@ -145,19 +198,22 @@ public class Duke {
                 } else if (inputText.equals("list")) { // list command
                     displayTaskList(taskList);
 
-                } else if (inputStringArray[0].contains("mark")) { // mark / unmark command
-                    markCommand(inputStringArray, taskList);
+                } else if (inputStringArray[0].equals("mark") || inputStringArray[0].equals("unmark")) { // mark / unmark command
+                    markCommand(inputStringArray, taskList, inputStringArray[0]);
 
                 } else if (inputStringArray[0].equals("todo") 
                 || inputStringArray[0].equals("deadline") 
                 || inputStringArray[0].equals("event")){
                     insertNewTask(inputStringArray[0], inputText, taskList);
 
+                } else if (inputStringArray[0].equals("delete")) {
+                    deleteTask(inputStringArray, taskList);
+
                 } else { // other text input
-                    throw new DukeException("Sorry Sir, I do not understand that command.");
+                    throw new DukeException(unrecognizedCommand);
                 }
             } catch (DukeException dukeException) {
-                System.out.println(straightLine + "\n " + dukeException.toString() + "\n" + straightLine);
+                System.out.println(createReply(dukeException.toString()));
             }
         }
     }
