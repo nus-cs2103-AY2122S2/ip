f60aebe8214dcc420d90f3f68717dc573ae62f6e
kerby <kerbysoh@gmail.com>
2022-01-20 15:04:34 +0800

    Implement Duke Level 8:
    - Created a new DateParser class to handle parsing of dates, from date to string, and from string to date formats.
    
    - Store Deadline and Event dates as java LocalDate objects, and print them in a different format.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,31 @@
+import java.time.LocalDate;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
 public class Deadline extends Task {
     private String deadlineDate;
+    private LocalDate date;
 
     Deadline(String description, String deadlineDate) {
         super(description);
         this.deadlineDate = deadlineDate;
+        try {
+            new SimpleDateFormat("yyyy-mm-dd").parse(deadlineDate);
+            this.date = DateParser.parseDate(deadlineDate);
+        } catch (ParseException e) {
+            this.date = null;
+        }
     }
 
     Deadline(String description, String deadlineDate, Boolean completed) {
         super(description, completed);
         this.deadlineDate = deadlineDate;
+        try {
+            new SimpleDateFormat("yyyy-mm-dd").parse(deadlineDate);
+            this.date = DateParser.parseDate(deadlineDate);
+        } catch (ParseException e) {
+            this.date = null;
+        }
     }
 
     @Override
@@ -22,6 +39,12 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.deadlineDate + ")";
+        String printedDate;
+        if (date != null) {
+            printedDate = DateParser.dateToString(this.date);
+        } else {
+            printedDate = this.deadlineDate;
+        }
+        return "[D]" + super.toString() + " (by: " + printedDate + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,31 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
 public class Event extends Task{
     private String eventTime;
+    private LocalDate date;
 
     Event(String description, String eventTime, Boolean completed) {
         super(description, completed);
         this.eventTime = eventTime;
+        try {
+            new SimpleDateFormat("yyyy-mm-dd").parse(eventTime);
+            this.date = DateParser.parseDate(eventTime);
+        } catch (ParseException e) {
+            this.date = null;
+        }
     }
 
     Event(String description, String eventTime) {
         super(description);
         this.eventTime = eventTime;
+        try {
+            new SimpleDateFormat("yyyy-mm-dd").parse(eventTime);
+            this.date = DateParser.parseDate(eventTime);
+        } catch (ParseException e) {
+            this.date = null;
+        }
     }
 
     @Override
@@ -22,6 +39,12 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + this.eventTime + ")";
+        String printedDate;
+        if (date != null) {
+            printedDate = DateParser.dateToString(this.date);
+        } else {
+            printedDate = this.eventTime;
+        }
+        return "[E]" + super.toString() + " (at: " + printedDate + ")";
     }
 }
