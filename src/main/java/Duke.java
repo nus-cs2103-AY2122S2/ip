3a058cdb24df39bb2916617f752ad0ab913a3c4a
manu2002g <gimhan.mg@gmail.com>
2022-01-20 15:03:08 +0530

    completed Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
 public class Duke {
+
+    public static String handle(String command) {
+        if (command.equals("bye")) {
+            return "See you later alligator :)\n";
+        } else {
+            return command;
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String prince = "______       _\n"
+                +  "| ___ \\     (_)\n"
+                + "| |_/ /_ __  _  _ __    ___  ___\n"
+                + "|  __/| '__|| || '_ \\  / __|/ _ \\ \n"
+                + "| |   | |   | || | | || (__|  __/\n"
+                + "\\_|   |_|   |_||_| |_| \\___|\\___|\n";
+
+        String divider = "~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+        System.out.println("Hello I'm\n" + prince);
+        System.out.println("How can I help you today?");
+        System.out.println(divider);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String command = sc.nextLine();
+            System.out.println(handle(command) + "\n");
+            if (command.equals("bye")) {
+                break;
+            }
+        }
     }
 }
