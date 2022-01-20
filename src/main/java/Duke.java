e7a5afc3d0b6214caefc363045fbbe689eea53d4
Rye-Catcher <2427367956@qq.com>
2022-01-20 22:18:12 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -32,11 +32,49 @@ public class Duke {
             this.isDone = new_status;
         }
 
+        @Override
         public String toString() {
             return "[" + this.getStatusIcon() + "] " + this.description;
         }
     }
-    
+
+    public static class ToDo extends Task {
+        public ToDo(String description) {
+            super(description);
+        }
+
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
+    public static class Deadline extends Task {
+        protected String by;
+
+        public Deadline(String description, String by) {
+            super(description);
+            this.by = by;
+        }
+
+        @Override
+        public String toString() {
+            return "[D]" + super.toString() + " (by: " + by + ")";
+        }
+    }
+
+    public static class Event extends Task {
+        protected String event_time;
+
+        public Event(String description, String event_time) {
+            super(description);
+            this.event_time = event_time;
+        }
+
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + " (at: " + event_time + ")";
+        }
+    }
     /**
      * The main body of the chat box. Will receive commands and do things accordingly
      **/
@@ -64,22 +102,43 @@ public class Duke {
                     }
 
                     System.out.print(outputChatBox(output_list));
-                } else if (command.split(" ")[0].equals("mark")) {
-                    Task temp = todo_list.get(Integer.parseInt(command.split(" ")[1]) - 1);
-                    temp.setStatus(true);
-                    String output_list = "Nice! I've marked this task as done: \n  " + temp.toString();
-
-                    System.out.print(outputChatBox(output_list));
-                } else if (command.split(" ")[0].equals("unmark")) {
-                    Task temp = todo_list.get(Integer.parseInt(command.split(" ")[1]) - 1);
-                    temp.setStatus(false);
-                    String output_list = "OK, I've marked this task as not done yet: \n  " + temp.toString();
-
-                    System.out.print(outputChatBox(output_list));
                 } else {
-                    todo_list.add(new Task(command));
+                    String task_type = command.split(" ")[0];
+                    if (task_type.equals("mark")) {
+                        Task temp = todo_list.get(Integer.parseInt(command.split(" ")[1]) - 1);
+                        temp.setStatus(true);
+                        String output_list = "Nice! I've marked this task as done: \n  " + temp.toString();
 
-                    System.out.print(outputChatBox("added: " + command));
+                        System.out.print(outputChatBox(output_list));
+                    } else if (task_type.equals("unmark")) {
+                        Task temp = todo_list.get(Integer.parseInt(command.split(" ")[1]) - 1);
+                        temp.setStatus(false);
+                        String output_list = "OK, I've marked this task as not done yet: \n  " + temp.toString();
+
+                        System.out.print(outputChatBox(output_list));
+                    } else if (task_type.equals("todo")){
+                        ToDo temp = new ToDo(command.substring(task_type.length()));
+                        todo_list.add(temp);
+                        String output_text = "Got it. I've added this task:\n    " + temp.toString()
+                                + "\nNow you have " + todo_list.size() + " tasks in the list.";
+                        System.out.print(outputChatBox(output_text));
+                    } else if (task_type.equals("deadline")) {
+                        String description = command.substring(task_type.length()).split("/")[0];
+                        String task_by = command.substring(task_type.length()).split("/")[1].substring(3);
+                        Deadline temp = new Deadline(description, task_by);
+                        todo_list.add(temp);
+                        String output_text = "Got it. I've added this task:\n    " + temp.toString()
+                                + "\nNow you have " + todo_list.size() + " tasks in the list.";
+                        System.out.print(outputChatBox(output_text));
+                    } else if (task_type.equals("event")) {
+                        String description = command.substring(task_type.length()).split("/")[0];
+                        String task_time = command.substring(task_type.length()).split("/")[1].substring(3);
+                        Event temp = new Event(description, task_time);
+                        todo_list.add(temp);
+                        String output_text = "Got it. I've added this task:\n    " + temp.toString()
+                                + "\nNow you have " + todo_list.size() + " tasks in the list.";
+                        System.out.print(outputChatBox(output_text));
+                    }
                 }
             }
 
