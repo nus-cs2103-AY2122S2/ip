aa0a752f26d107a277c7a202186530329c5c0c4d
mradhyaf <mradhyaf@gmail.com>
2022-01-20 21:18:53 +0700

    Implement add and list commands

--- src/main/java/Kenobi.java
+++ src/main/java/Kenobi.java
@@ -1,31 +1,40 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Kenobi {
-    public static void main(String[] args) {
-        String logo = " __   __\n"
-                + "|  | /  / _____  _    _  _____  _____    __\n"
-                + "|  |/  / | ____|| \\  | ||  _  ||  _  |  |  |\n"
-                + "|     /  | |___ |  \\ | || | | || |_| |_ |  |\n"
-                + "|     \\  | ____|| | \\| || | | ||  ___  ||  |\n"
-                + "|  |\\  \\ | |___ | |\\ | || |_| || |___| ||  |\n"
-                + "|__| \\__\\|_____||_| \\__||_____||_______||__|";
-        System.out.println(logo);
-        System.out.println("\n    Hello there, how may I help you?");
+  static ArrayList<String> taskList = new ArrayList<>();
 
-        Scanner sc = new Scanner(System.in);
-        
-        String inString;
-        while (true) {
-          inString = sc.nextLine();
+  public static void main(String[] args) {
+    String logo = " __   __\n"
+        + "|  | /  / _____  _    _  _____  _____    __\n"
+        + "|  |/  / | ____|| \\  | ||  _  ||  _  |  |  |\n"
+        + "|     /  | |___ |  \\ | || | | || |_| |_ |  |\n"
+        + "|     \\  | ____|| | \\| || | | ||  ___  ||  |\n"
+        + "|  |\\  \\ | |___ | |\\ | || |_| || |___| ||  |\n"
+        + "|__| \\__\\|_____||_| \\__||_____||_______||__|";
+    System.out.println(logo);
+    System.out.println("\n    Hello there, how may I help you?");
 
-          if (inString.equals("bye")) {
-            System.out.println("    Goodbye, old friend");
-            break;
-          }
+    Scanner sc = new Scanner(System.in);
 
-          System.out.println(inString);
+    String inString;
+    while (true) {
+      inString = sc.nextLine();
+
+      if (inString.equals("bye")) {
+        System.out.println("    Goodbye, old friend");
+        break;
+      } else if (inString.equals("list")) {
+        for (int i = 0; i < taskList.size(); i++) {
+          String str = String.format("    %d. %s", i + 1, taskList.get(i));
+          System.out.println(str);
         }
-
-        sc.close();
+      } else {
+        taskList.add(inString);
+        System.out.println("    added: " + inString);
+      }
     }
+
+    sc.close();
+  }
 }
