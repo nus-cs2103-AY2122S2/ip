9a731967389de23eac246be74cc86e8fcf994c51
khoahre123 <e0550384@u.nus.edu>
2022-01-21 06:19:21 +0800

    Replace some common string by enum

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,35 @@ import java.util.Scanner;
 
 public class Duke {
 
+    public enum TypicalString {
+        LONG_LINE {
+            public String toString() {
+                return "____________________________________________________________";
+            }
+        },
+        ADDED_TASK {
+            public String toString() {
+                return " Got it. I've added this task:";
+            }
+        },
+        HELLO {
+            public String toString() {
+                return " Hello! I'm Duke";
+            }
+        },
+        GOODBYE {
+            public String toString() {
+                return " Bye. Hope to see you again soon!";
+            }
+        }
+    }
+
     private static void addTask(ArrayList<TaskStorage> taskStorages, int numTask) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Got it. I've added this task:");
+        System.out.println(TypicalString.LONG_LINE);
+        System.out.println(TypicalString.ADDED_TASK);
         System.out.println("  " + taskStorages.get(taskStorages.size() - 1));
         System.out.println(" Now you have " + numTask +  " tasks in the list.");
-        System.out.println("____________________________________________________________");
+        System.out.println(TypicalString.LONG_LINE);
     }
 
     private static boolean checkNumeric(String string) {
@@ -22,10 +45,10 @@ public class Duke {
         //        + "| |_| | |_| |   <  __/\n"
         //        + "|____/ \\__,_|_|\\_\\___|\n";
         //System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke");
+        System.out.println(TypicalString.LONG_LINE);
+        System.out.println(TypicalString.HELLO);
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(TypicalString.LONG_LINE);
 
         Scanner sc = new Scanner(System.in);
         String nextWord = sc.nextLine();
@@ -46,22 +69,22 @@ public class Duke {
 
         while (! nextWord.equals("bye")) {
             if (commandWord.equals("list")) {
-                System.out.println("____________________________________________________________");
+                System.out.println(TypicalString.LONG_LINE);
                 System.out.println(" Here are the tasks in your list:");
                 for (int i = 1; i <= storingList.size(); i++) {
                     System.out.println(" " + i + "." + storingList.get(i - 1));
                 }
-                System.out.println("____________________________________________________________");
+                System.out.println(TypicalString.LONG_LINE);
             } else if (commandWord.equals("mark")) {
                 if (! checkNumeric(restWord)) {
                     exception.notNumeric("mark");
                 } else {
                     TaskStorage temp = storingList.get(Integer.parseInt(restWord) - 1);
                     temp.taskDone();
-                    System.out.println("____________________________________________________________");
+                    System.out.println(TypicalString.LONG_LINE);
                     System.out.println(" Nice! I've marked this task as done: ");
                     System.out.println("  " + temp);
-                    System.out.println("____________________________________________________________");
+                    System.out.println(TypicalString.LONG_LINE);
                 }
             } else if (commandWord.equals("unmark")) {
                 if (! checkNumeric(restWord)) {
@@ -69,10 +92,10 @@ public class Duke {
                 } else {
                     TaskStorage temp = storingList.get(Integer.parseInt(restWord) - 1);
                     temp.taskUndone();
-                    System.out.println("____________________________________________________________");
+                    System.out.println(TypicalString.LONG_LINE);
                     System.out.println(" OK, I've marked this task as not done yet: ");
                     System.out.println("  " + temp);
-                    System.out.println("____________________________________________________________");
+                    System.out.println(TypicalString.LONG_LINE);
                 }
             } else if (commandWord.equals("todo")) {
                 if (nextWord.length() == 4) {
@@ -103,12 +126,12 @@ public class Duke {
                     exception.notNumeric("delete");
                 } else {
                     TaskStorage temp = storingList.remove( Integer.parseInt(restWord) - 1);
-                    System.out.println("____________________________________________________________");
+                    System.out.println(TypicalString.LONG_LINE);
                     System.out.println(" Noted. I've removed this task: ");
                     System.out.println("  " + temp);
                     taskNumber -= 1;
                     System.out.println(" Now you have " + taskNumber + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
+                    System.out.println(TypicalString.LONG_LINE);
                 }
             } else {
                 exception.wrongSyntax();
@@ -123,8 +146,8 @@ public class Duke {
                 restWord = nextWord.substring(commandIndex + 1);
             }
         }
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(TypicalString.LONG_LINE);
+        System.out.println(TypicalString.GOODBYE);
+        System.out.println(TypicalString.LONG_LINE);
     }
 }
