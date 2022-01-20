76b765fe3737db04ed997d9d45c4660beeceef40
kxshxsh <kashish.v.varshney@gmail.com>
2022-01-20 12:57:55 +0530

    Implemented custom exceptions to handle errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,23 +22,33 @@ public class Duke {
 
         while (true) {
             String instruct = br.readLine();
-            if (instruct.equals("bye")) {
-                endSession();
-                break;
-            } else if (instruct.equals("list")) {
-                reportList();
-            } else {
-                String[] details = instruct.split(" ");
-                if (details[0].equals("mark")) {
-                    markAsDone(Integer.parseInt(details[1]));
-                } else if (details[0].equals("unmark")) {
-                    markNotDone(Integer.parseInt(details[1]));
+            try {
+                if (instruct.equals("bye")) {
+                    endSession();
+                    break;
+                } else if (instruct.equals("list")) {
+                    reportList();
                 } else {
-                    String taskType = details[0];
-                    addTask(taskType, instruct);
+                    String[] details = instruct.split(" ");
+                    if (details[0].equals("mark")) {
+                        markAsDone(Integer.parseInt(details[1]));
+                    } else if (details[0].equals("unmark")) {
+                        markNotDone(Integer.parseInt(details[1]));
+                    } else {
+                        String taskType = details[0];
+                        if (!(taskType.equals("todo") || taskType.equals("deadline")
+                                || taskType.equals("event"))) {
+                            throw new CustomException("sorry, this isn't a valid command yet!");
+                        } else {
+                            addTask(taskType, instruct);
+                        }
+                    }
                 }
+                addLineBreak();
+            } catch (CustomException e) {
+                System.out.println(e.getMessage());
+                addLineBreak();
             }
-            addLineBreak();
         }
 
         br.close();
@@ -60,52 +70,81 @@ public class Duke {
         System.out.println("---------------------xx-------------------------");
     }
 
-    public static void reportList() {
+    public static void reportList() throws CustomException {
         int len = manager.size();
-        for (int i = 0; i < len; i++) {
-            System.out.println((i + 1) + ". " + manager.get(i).toString());
+        if (len != 0) {
+            for (int i = 0; i < len; i++) {
+                System.out.println((i + 1) + ". " + manager.get(i).toString());
+            }
+        } else {
+            throw new CustomException("your task list is currently empty. " +
+                    "Add a task first:)");
         }
     }
 
-    public static void addTask(String taskType, String instruction) {
+    public static void addTask(String taskType, String instruction) throws CustomException {
         Task toAdd = new Task("empty task");
         if (taskType.equals("todo")) {
-            toAdd = new Todo(instruction.substring(5));
+            String description = instruction.substring(4);
+            if (description.isBlank()) {
+                throw new CustomException("todo description cannot be blank! Please add task details");
+            } else {
+                toAdd = new Todo(description.substring(1));
+            }
         } else if (taskType.equals("deadline")) {
             int index = instruction.indexOf("/by");
-            String description = instruction.substring(9, index);
-            String time = instruction.substring(index + 4);
-            toAdd = new Deadline(description, time);
+            if (index != -1) {
+                String description = instruction.substring(9, index);
+                String time = instruction.substring(index + 4);
+                toAdd = new Deadline(description, time);
+            } else {
+                throw new CustomException("Incorrect time format: ensure to prefix time with '/by'");
+            }
         } else {
             int index = instruction.indexOf("/at");
-            String description = instruction.substring(6, index);
-            String time = instruction.substring(index + 4);
-            toAdd = new Event(description, time);
-        }
+            if (index != -1) {
+                String description = instruction.substring(6, index);
+                String time = instruction.substring(index + 4);
+                toAdd = new Event(description, time);
+            } else {
+                throw new CustomException("Incorrect time format: ensure to prefix time with '/at'");
+            }
 
+        }
         manager.add(toAdd);
         System.out.println("Rodger that! Task item added:");
         System.out.println(toAdd);
     }
 
-    public static Task findTask(int num) {
+    public static Task findTask(int num) throws CustomException{
         Task desiredTask = new Task("empty task");
-        for (int i = 1; i <= manager.size(); i++) {
-            if (i == num) {
-                desiredTask = manager.get(i - 1);
+        int len = manager.size();
+
+        if ((num <= len) && (len > 0) && (num > 0)) {
+            for (int i = 1; i <= len; i++) {
+                if (i == num) {
+                    desiredTask = manager.get(i - 1);
+                }
             }
+            return desiredTask;
+        } else if (num < 0) {
+            throw new CustomException("Invalid task ID: number must be a positive integer:)");
+        } else if (num > len) {
+            throw new CustomException("Invalid task ID: this task number does not exist as of now.");
+        } else { // length of task list == 0
+            throw new CustomException("your task list is currently empty. " +
+                    "Add a task first:)");
         }
-        return desiredTask;
     }
 
-    public static void markAsDone(int num) {
+    public static void markAsDone(int num) throws CustomException {
         Task t = findTask(num);
         t.markDone();
         System.out.println("Congrats! Keep going:)");
         System.out.println(t);
     }
 
-    public static void markNotDone(int num) {
+    public static void markNotDone(int num) throws CustomException {
         Task t = findTask(num);
         t.undo();
         System.out.println("No worries:) Stay motivated!");
