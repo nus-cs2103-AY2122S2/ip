6c7a37a35dae7835128b43d042421ccff54550a9
Xu Peizhe <e0492499@u.nus.edu>
2022-01-20 18:02:29 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+    private final static String line =
+            "____________________________________________________________\n";
+    private final Scanner s = new Scanner(System.in);
+    private boolean finished = false;
+
+    public boolean isFinished() {
+        return this.finished;
+    }
+
+    private String read() {
+        String input = s.nextLine();
+        if (input.equals("bye")) this.finished = true;
+        return input;
+    }
+
+    private void respond(String respond) {
+        if (this.finished) respond = line + "Bye. Hope to see you again soon!\n" + line;
+        else respond = line + respond + "\n" + line;
+        System.out.println(respond);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(line);
+        System.out.println("Hello! I am xpz\nWhat can I do for you?\n" + line);
+        Duke xpz = new Duke();
+        while (!xpz.isFinished()) xpz.respond(xpz.read());
     }
 }
