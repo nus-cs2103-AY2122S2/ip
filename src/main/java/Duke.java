5d267d13ec16e9475ad0a09ebc9b2b2e70ba84fa
cashewnade <e0559400@u.nus.edu>
2022-01-20 17:00:57 +0800

    Level-4
    Completed Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Duke {
     private static final String GREETING_MESSAGE = "Hey, Aeromon here! Fancy a cup of tea?";
     private static final String FAREWELL_MESSAGE = "Buai Buai! Ciao for now!";
-    private static final String ADD_MESSAGE = "Nicely! I've added for you: ";
+    private static final String ADD_MESSAGE = "Nicely! I've added for you: \n";
     private static final String MARK_MESSAGE = "Naisu! You've completed: \n";
     private static final String UNMARK_MESSAGE = "OI! What happened to completing: \n";
     private static final List<Task> toDoList = new ArrayList<>();
@@ -33,13 +33,32 @@ public class Duke {
                 System.out.println(MARK_MESSAGE + temp);
             } else if (input.startsWith("unmark")) {
                 String intValue = input.replaceAll("[^0-9]", "");
-                int index = Integer.parseInt(intValue);
+                int index = Integer.parseInt(intValue) - 1;
                 Task temp = toDoList.get(index);
                 temp.markAsNotDone();
                 System.out.println(UNMARK_MESSAGE + temp);
-            } else {
-                toDoList.add(new Task(input));
-                System.out.println(ADD_MESSAGE + input);
+            } else if (input.startsWith("todo")) {
+                String taskName = input.substring(5);
+                Task task = new ToDo(taskName);
+                toDoList.add(task);
+                System.out.println(ADD_MESSAGE + task);
+                System.out.println(String.format("Aiyo, now you have %d tasks in the list, jiayous!", toDoList.size()));
+            } else if (input.startsWith("deadline")) {
+                String[] splitString = input.split("/by ");
+                String taskName = splitString[0].substring(9);
+                String by = splitString[1];
+                Task task = new Deadline(taskName, by);
+                toDoList.add(task);
+                System.out.println(ADD_MESSAGE + task);
+                System.out.println(String.format("Aiyo, now you have %d tasks in the list, jiayous!", toDoList.size()));
+            } else if (input.startsWith("event")) {
+                String[] splitString = input.split("/at ");
+                String taskName = splitString[0].substring(6);
+                String at = splitString[1];
+                Task task = new Event(taskName, at);
+                toDoList.add(task);
+                System.out.println(ADD_MESSAGE + task);
+                System.out.println(String.format("Aiyo, now you have %d tasks in the list, jiayous!", toDoList.size()));
             }
         }
     }
