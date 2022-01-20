7b397088e7a94f0da43cdf2d3aaa62fc7de3afa0
Albert Sutiono <albertsutz123@gmail.com>
2022-01-20 20:47:55 +0800

    Implemented basic Functionalities (greet, echo, exit) for Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String instruction = sc.nextLine();
+            switch(instruction){
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!\n");
+                    return;
+
+                default:
+                    System.out.println(instruction);
+            }
+            System.out.println("");
+        }
     }
 }
