13e8deb582632b9f490a1e83cbace1c845acce4e
joey-chance <joey.chance@u.nus.edu>
2022-01-20 09:57:44 +0800

    level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Duke {
@@ -12,15 +13,23 @@ public class Duke {
                         "|______|  |__| |__|  |__| \\__\\      \\____/      |_________|  |_______|\n"
                 ;
         System.out.println("Welcome home, sir.\n" + logo);
-        echo();
+        read();
     }
-    public static void echo() {
+    public static void read() {
+        LinkedList<String> tasks = new LinkedList<>();
         String input;
         Scanner sc = new Scanner(System.in);
         input = sc.nextLine();
         while (!input.equalsIgnoreCase("bye")) {
             System.out.println("___________________________________________________________");
-            System.out.println(input.toLowerCase());
+            if (input.equalsIgnoreCase("list")) {
+                for (int i = 1; i <= tasks.size(); i++) {
+                    System.out.println(i + ". " + tasks.get(i - 1));
+                }
+            } else {
+                tasks.add(input);
+                System.out.println("added: " + input);
+            }
             System.out.println("___________________________________________________________");
             input = sc.nextLine();
         }
