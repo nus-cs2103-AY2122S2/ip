ebfe6eb9d715c5f54c85cf24cec1825a1353bb3a
Albert Sutiono <albertsutz123@gmail.com>
2022-01-20 21:01:37 +0800

    Enables user add task and list them (Level 2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -10,6 +12,8 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         Scanner sc = new Scanner(System.in);
 
+        List<String> tasks = new ArrayList<String>();
+
         while (true) {
             String instruction = sc.nextLine();
             switch(instruction){
@@ -17,8 +21,15 @@ public class Duke {
                     System.out.println("Bye. Hope to see you again soon!\n");
                     return;
 
+                case "list":
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(i + 1 + ". " + tasks.get(i));
+                    }
+                    break;
+
                 default:
-                    System.out.println(instruction);
+                    tasks.add(instruction);
+                    System.out.println("added: " + instruction);
             }
             System.out.println("");
         }
