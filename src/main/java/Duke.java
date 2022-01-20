fd48ffb7c707d6baff3dfdbc362f302ed67145be
brandonrhan <e0540875@u.nus.edu>
2022-01-20 20:25:16 +0800

    add to lower case for first word

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,19 @@ import java.util.*;
 public class Duke {
     static Scanner sc = new Scanner(System.in);
     static String line = "\n---------------------\n";
+
+    enum functions {
+        todo,
+        event,
+        deadline,
+        list,
+        mark,
+        unmark,
+        delete,
+        bye
+    }
+
+
     public static void main(String[] args) throws DukeException {
         String logo = " ____         _   _     \n"
                     + "|  _ \\       | | | |\n"
@@ -16,28 +29,29 @@ public class Duke {
             while (true) {
                 String input = sc.nextLine();
                 String[] inputArray = input.split(" ", 2);
-                if (inputArray[0].equals("bye")) {
+                String s1 = inputArray[0].toLowerCase();
+                if (s1.equals("bye")) {
                     System.out.println(line + "GoodBye! Thanks for using B.H!" + line);
                     break;
-                } else if (inputArray[0].equals("list")) {
+                } else if (s1.equals("list")) {
                     System.out.println(line + bh.getList() + line);
-                } else if (inputArray[0].equals("mark") && inputArray.length > 1) {
+                } else if (s1.equals("mark") && inputArray.length > 1) {
                     int index = Integer.parseInt(inputArray[1]) - 1;
                     if (index < bh.getListSize()) {
                         System.out.println(line + "Well done! \n" + bh.mark(index) + line);
                     } else {
                         System.out.println("Index out of range");
                     }
-                } else if (inputArray[0].equals("unmark") && inputArray.length > 1) {
+                } else if (s1.equals("unmark") && inputArray.length > 1) {
                     int index = Integer.parseInt(inputArray[1]) - 1;
                     System.out.println("Oh no! \n" + bh.unmark(index));
-                } else if (inputArray[0].equals("todo")) {
+                } else if (s1.equals("todo")) {
                     String task = inputArray[1];
                     Task newtask = new Todo(task);
                     bh.addToList(newtask);
                     System.out.println(line + "Task added: " + newtask.toString() + "\n" +
                             "Now you have " + bh.getListSize() + " tasks in the list" + line);
-                } else if (inputArray[0].equals("deadline")) {
+                } else if (s1.equals("deadline")) {
                     String s = inputArray[1];
                     String[] arr = s.split("/by");
                     String task = arr[0];
@@ -46,7 +60,7 @@ public class Duke {
                     bh.addToList(newtask);
                     System.out.println(line + "Task added:" + newtask.toString() + "\n" +
                             "Now you have " + bh.getListSize() + " tasks in the list" + line);
-                } else if (inputArray[0].equals("event")) {
+                } else if (s1.equals("event")) {
                     String s = inputArray[1];
                     String[] arr = s.split("/at");
                     String task = arr[0];
@@ -55,7 +69,7 @@ public class Duke {
                     bh.addToList(newtask);
                     System.out.println(line + "Task added:" + newtask.toString() + "\n" +
                             "Now you have " + bh.getListSize() + " tasks in the list" + line);
-                } else if (inputArray[0].equals("delete")) {
+                } else if (s1.equals("delete")) {
                     int index = Integer.parseInt(inputArray[1]) - 1;
                     System.out.println(line + "Okay, I have remove this task:\n" +
                             bh.deleteTask(index) + line);
