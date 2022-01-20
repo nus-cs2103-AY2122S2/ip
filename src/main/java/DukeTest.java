c1797666fd5d91d6a6baa802bb7b1631637d9321
Tham Jin Lin <tham.jinlin@dhs.sg>
2022-01-21 03:47:24 +0800

    added level-2 requirements

--- src/main/java/DukeTest.java
+++ src/main/java/DukeTest.java
@@ -1,14 +1,27 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DukeTest {
     private static String exitTrigger = "bye";
+    private static ArrayList<String> store = new ArrayList<>();
 
-    public static void echo(String str) {
+    private static void echo(String str) {
         System.out.println(str);
     }
 
-    public static void exit() {
+    private static void exit() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void addToStore(String str) {
+        store.add(str);
+        System.out.println("added: " + str);
+    }
+
+    private static void list() {
+        for (int i = 1; i <= store.size(); i++) {
+            System.out.println(i + ". " + store.get(i - 1));
+        }
     }
 
     public static void main(String[] args) {
@@ -18,7 +31,15 @@ public class DukeTest {
         String input = scanner.nextLine();
 
         while (!(input.equals(exitTrigger))) {
-            echo(input);
+            switch (input) {
+                case "list":
+                    list();
+                    break;
+                default:
+                    addToStore(input);
+                    break;
+            }
+
             input = scanner.nextLine();
         }
 
