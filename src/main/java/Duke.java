ab9433a9fac004adcc9171c37c2be3570ac6a8ae
aditi2313 <guptaaditi2313@gmail.com>
2022-01-20 22:43:38 +0800

    Level 1, 2, 3, 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Duke {
 
     public static ArrayList<Task> list = new ArrayList<Task>();
-    public static ArrayList<String> type = new ArrayList<String>();
 
     public static void main(String[] args) {
         System.out.println("Hello! I'm Bob :) \nWhat can I do for you? :D");
@@ -35,8 +34,8 @@ public class Duke {
     public static void displaylist() {
         String returnstring = "";
         for (int i = 0; i < list.size(); i++) {
-            returnstring = returnstring + (i + 1) + ". [" + type.get(i) + "][" + list.get(i).getStatusIcon() + "] "
-                    + list.get(i) + "\n";
+            returnstring = returnstring + (i + 1) + ". [" + list.get(i).symbol() + "][" + list.get(i).getStatusIcon() +
+                    "] " + list.get(i) + "\n";
         }
         System.out.println(returnstring);
     }
@@ -45,21 +44,20 @@ public class Duke {
         if (stringtoadd.length < 2) {
             System.out.println("OOPS!! The description of a " + stringtoadd[0] + " cannot be empty.");
         } else {
-            String initial = "";
+            Task task;
             String returnstring = "";
             for (int i = 1; i < stringtoadd.length; i++) {
                 returnstring = returnstring + stringtoadd[i] + " ";
             }
             if (stringtoadd[0].equals("todo")) {
-                initial = "T";
+                task = new Todo(returnstring);
             } else if (stringtoadd[0].equals("deadline")) {
-                initial = "D";
+                task = new Deadline(returnstring);
             } else {
-                initial = "E";
+                task = new Event(returnstring);
             }
-            type.add(initial);
-            list.add(new Task(returnstring));
-            System.out.println("Got it!! :D I've added this task:\n" + " [" + initial + "][] " + returnstring +
+            list.add(task);
+            System.out.println("Got it!! :D I've added this task:\n" + " [" + task.symbol() + "][] " + returnstring +
                     "\nNow you have " + list.size() + " tasks in the list.");
         }
     }
@@ -67,23 +65,25 @@ public class Duke {
     public static void mark(String number) {
         int num = Integer.parseInt(number);
         list.get(num - 1).setAsDone();
-        System.out.println("Nice! :P I've marked this tast as done:\n [" + type.get(num - 1) + "][" +
-                list.get(num - 1).getStatusIcon() + "] " + list.get(num - 1));
+        Task temp = list.get(num - 1);
+        System.out.println("Nice! :P I've marked this tast as done:\n [" + temp.symbol() + "][" +
+                temp.getStatusIcon() + "] " + temp);
     }
 
     public static void unmark(String number) {
         int num = Integer.parseInt(number);
         list.get(num - 1).setAsNotDone();
-        System.out.println("OK ._. , I've marked this task as not done yet:\n [" + type.get(num - 1) + "][" +
-                list.get(num - 1).getStatusIcon() + "] " + list.get(num - 1));
+        Task temp = list.get(num - 1);
+        System.out.println("OK ._. , I've marked this task as not done yet:\n [" + temp.symbol() + "][" +
+                temp.getStatusIcon() + "] " + temp);
     }
 
     public static void delete(String number) {
         int num = Integer.parseInt(number);
-        System.out.println("Noted. I've removed this task:\n [" + type.get(num - 1) + "][" +
-                list.get(num - 1).getStatusIcon() + "] " + list.get(num - 1) +
+        Task temp = list.get(num - 1);
+        System.out.println("Noted. I've removed this task:\n [" + temp.symbol() + "][" +
+                temp.getStatusIcon() + "] " + temp +
                     "\nNow you have " + (list.size() - 1) + " tasks left in this list");
         list.remove(num - 1);
-        type.remove(num - 1);
     }
 }
