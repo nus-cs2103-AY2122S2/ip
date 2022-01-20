3cf225d7a8017a62d880cc567491e79d71a25af3
juayhee <e0524722@u.nus.edu>
2022-01-20 14:17:34 +0800

    Mark/Unmark formatting update * Mark/Unmark commands now provide feedback in the console

--- src/main/java/UnmarkCommand.java
+++ src/main/java/UnmarkCommand.java
@@ -6,15 +6,35 @@ import java.util.ArrayList;
 public class UnmarkCommand extends Command {
     private String input; // Parameters: 1
     public ArrayList<Task> taskList = TaskManager.taskList;
+    private int index; // Index of target task
 
     public UnmarkCommand(String i) {
         this.input = i;
+        index = Integer.parseInt(input) - 1; // -1 because list starts at 1 while indexes start at 0
+    }
+
+    private String formatOutput() {
+        String output = "";
+        String taskString = taskList.get(index).toString();
+
+        output += Duke.line();
+        output += Duke.newLine(Duke.indent("Task(s) unmarked as done: ",1));
+        output += Duke.indent("", 2);
+        output += Duke.newLine(taskString);
+        output += Duke.line();
+
+        return output;
+
     }
 
     @Override
     public void execute() {
         int index = Integer.parseInt(input) - 1; // -1 because list starts at 1 while indexes start at 0
         taskList.get(index).unmarkAsDone();
+
+        // Console prints
+        String output = formatOutput();
+        System.out.println(output);
 
     }
 }
