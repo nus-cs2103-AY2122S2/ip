3df64f5e9f070683ac11423ca256203519bab3c8
kavxya <e0550269@u.nus.edu>
2022-01-20 16:17:18 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
@@ -6,14 +8,24 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
 
+        int counter = 0;
+
+        List list = new ArrayList<String>();
+
         while(sc.hasNext()) {
             String value = sc.nextLine();
             if (value.equals("bye")){
                 System.out.println(" Bye. Hope to see you again soon!");
                 break;
             }
+            else if (value.equals("list")) {
+                for (int i=0; i<list.size(); i++) {
+                    System.out.println(i+1 + ". " + list.get(i));
+                }
+            }
             else {
-                System.out.println(value);
+                System.out.println("added: "+ value);
+                list.add(value);
             }
         }
 
