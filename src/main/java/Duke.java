7abb474f7ad91d1ad74963e3f2a553cad19f1e47
jetrz <joshuaemmanuelteo@gmail.com>
2022-01-20 15:33:09 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,62 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        
+        System.out.printf("                                             ,-.\n");
+        System.out.printf("                                          _.|  '\n");
+        System.out.printf("                                        .'  | /\n");
+        System.out.printf("                                      ,'    |'\n");
+        System.out.printf("                                     /      /\n");
+        System.out.printf("                       _..----\"\"---.'      /\n");
+        System.out.printf(" _.....---------...,-\"\"                  ,'\n");
+        System.out.printf(" `-._  \\                                /\n");
+        System.out.printf("     `-.+_            __           ,--. .\n");
+        System.out.printf("          `-.._     .:  ).        (`--\"| \\\n");
+        System.out.printf("               7    | `\" |         `...'  \\\n");
+        System.out.printf("               |     `--'     '+\"        ,\". ,\"\"-\n");
+        System.out.printf("               |   _...        .____     | |/    '\n");
+        System.out.printf("          _.   |  .    `.  '--\"   /      `./     j            Pika Pika!\n");
+        System.out.printf("         \\' `-.|  '     |   `.   /        /     /             Pikachu says hi!\n");
+        System.out.printf("         '     `-. `---\"      `-\"        /     /\n");
+        System.out.printf("          \\       `.                  _,'     /\n");
+        System.out.printf("           \\        `                        .\n");
+        System.out.printf("            \\                                j\n");
+        System.out.printf("             \\                              /\n");
+        System.out.printf("              `.                           .\n");
+        System.out.printf("                +                          \\\n");
+        System.out.printf("                |                           L\n");
+        System.out.printf("                |                           |\n");
+        System.out.printf("                |  _ /,                     |\n");
+        System.out.printf("                | | L)'..                   |\n");
+        System.out.printf("                | .    | `                  |\n");
+        System.out.printf("                '  \\'   L                   '\n");
+        System.out.printf("                 \\  \\   |                  j\n");
+        System.out.printf("                  `. `__'                 /\n");
+        System.out.printf("                _,.--.---........__      /\n");
+        System.out.printf("               ---.,'---`         |   -j\"\n");
+        System.out.printf("                .-'  '....__      L    |\n");
+        System.out.printf("              \"\"--..    _,-'       \\ l||\n");
+        System.out.printf("                  ,-'  .....------. `||'\n");
+        System.out.printf("               _,'                /\n");
+        System.out.printf("             ,'                  /\n");
+        System.out.printf("            '---------+-        /\n");
+        System.out.printf("                     /         /\n");
+        System.out.printf("                   .'         /\n");
+        System.out.printf("                 .'          /\n");
+        System.out.printf("               ,'           /\n");
+        System.out.printf("             _'....----\"\"\"\"\" \n");
+
+        Scanner sc = new Scanner(System.in);
+        String currInput = "";
+
+        while (true) {
+            currInput = sc.nextLine().toLowerCase();
+            if (currInput.equals("bye")) break;
+            System.out.println("    ________________________________________________________________");
+            System.out.println("    Pikachu says "+currInput);
+            System.out.println("    ________________________________________________________________");
+        }
+        System.out.println("Pika pika! \nPikachu says bye!");
     }
 }
