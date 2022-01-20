5a3a7c72991a7b315b65af3837ad06064777fabf
Tan Wei En <tanweien8382@gmail.com>
2022-01-20 19:40:15 +0800

    Level 5, added exception handling for tasks and markings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -24,7 +24,7 @@ public class Duke {
 
             String command = sc.nextLine().toLowerCase();
             String firstWord;
-            int itemIndex = 0;
+            int itemIndex = -1;
 
             int index = command.indexOf(' ');
             if (index > -1) {
@@ -50,50 +50,82 @@ public class Duke {
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
             } else if (firstWord.equals("mark")) {
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                taskList.mark(itemIndex - 1);
-                System.out.println("Nice! I've marked this task as done: ");
-                System.out.println(taskList.getTask(itemIndex - 1).toString());
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                if (itemIndex > taskList.length()) {
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("Out of bounds!");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                } else {
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    taskList.mark(itemIndex - 1);
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println(taskList.getTask(itemIndex - 1).toString());
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                }
 
             } else if (firstWord.equals("unmark")) {
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                taskList.unmark(itemIndex - 1);
-                System.out.println("OK, I've marked this task as not done yet: ");
-                System.out.println(taskList.getTask(itemIndex - 1).toString());
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                if (itemIndex > taskList.length()) {
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("Out of bounds!");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                } else {
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    taskList.unmark(itemIndex - 1);
+                    System.out.println("OK, I've marked this task as not done yet: ");
+                    System.out.println(taskList.getTask(itemIndex - 1).toString());
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                }
+
 
             } else if (firstWord.equals("delete")) {
                 System.out.println("mark");
 
             } else if (firstWord.equals("todo")){
-                String toDoItem = command.substring(index + 1);
-                ToDo toDo = new ToDo(toDoItem);
-                taskList.add(toDo);
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                System.out.println("Got it, I have added this task:" );
-                System.out.println(toDo.toString());
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                if (index == -1) {
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("OOPS!!! The description of a todo cannot be empty.");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                } else {
+                    String toDoItem = command.substring(index + 1);
+                    ToDo toDo = new ToDo(toDoItem);
+                    taskList.add(toDo);
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("Got it, I have added this task:" );
+                    System.out.println(toDo.toString());
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                }
 
             } else if (firstWord.equals("deadline")){
-                String deadlineItem = command.substring(index + 1, command.indexOf("/by"));
-                String time = command.substring(command.indexOf("/by") + 1);
-                Deadline deadline = new Deadline(deadlineItem, time);
-                taskList.add(deadline);
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                System.out.println("Got it, I have added this task:" );
-                System.out.println(deadline.toString());
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                if (index == -1) {
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("OOPS!!! The description of a deadline cannot be empty.");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                } else {
+                    String deadlineItem = command.substring(index + 1, command.indexOf("/by"));
+                    String time = command.substring(command.indexOf("/by") + 1);
+                    Deadline deadline = new Deadline(deadlineItem, time);
+                    taskList.add(deadline);
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("Got it, I have added this task:" );
+                    System.out.println(deadline.toString());
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                }
 
             } else if (firstWord.equals("event")) {
-                String eventItem = command.substring(index + 1, command.indexOf("/at"));
-                String time = command.substring(command.indexOf("/at") + 1);
-                Event event = new Event(eventItem, time);
-                taskList.add(event);
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                System.out.println("Got it, I have added this task:" );
-                System.out.println(event.toString());
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                if (index == -1) {
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("OOPS!!! The description of an event cannot be empty.");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                } else {
+                    String eventItem = command.substring(index + 1, command.indexOf("/at"));
+                    String time = command.substring(command.indexOf("/at") + 1);
+                    Event event = new Event(eventItem, time);
+                    taskList.add(event);
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("Got it, I have added this task:" );
+                    System.out.println(event.toString());
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                }
+
             }
         }
     }
