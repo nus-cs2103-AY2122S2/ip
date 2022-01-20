5399805c1f3db1368f885c3f79634d3e7ae1455f
albertsutz <albertsutz123@gmail.com>
2022-01-20 22:50:28 +0800

    Integrated basic Exceptions with Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,66 +16,84 @@ public class Duke {
         List<Task> tasks = new ArrayList<Task>();
 
         while (true) {
-            String[] instruction = sc.nextLine().split(" ");
-            switch(instruction[0]){
-                case "bye": {
-                    System.out.println("Bye. Hope to see you again soon!\n");
-                    return;
-                }
-
-                case "list": {
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println(i + 1 + ". " + tasks.get(i));
+            try {
+                String[] instruction = sc.nextLine().split(" ");
+                switch(instruction[0]){
+                    case "bye": {
+                        System.out.println("Bye. Hope to see you again soon!\n");
+                        return;
                     }
-                    break;
-                }
 
-                case "mark": {
-                    int taskNum = Integer.parseInt(instruction[1]);
-                    tasks.get(taskNum - 1).markAsDone();
-                    System.out.println(String.format("Nice!, I have marked this task as done: \n %s", tasks.get(taskNum - 1)));
-                    break;
-                }
+                    case "list": {
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println(i + 1 + ". " + tasks.get(i));
+                        }
+                        break;
+                    }
 
-                case "unmark": {
-                    int taskNum = Integer.parseInt(instruction[1]);
-                    tasks.get(taskNum - 1).markAsNotDone();
-                    System.out.println(String.format("Ok, I have marked this task as not done: \n %s", tasks.get(taskNum - 1)));
-                    break;
-                }
+                    case "mark": {
+                        int taskNum = Integer.parseInt(instruction[1]);
+                        tasks.get(taskNum - 1).markAsDone();
+                        System.out.println(String.format("Nice!, I have marked this task as done: \n %s", tasks.get(taskNum - 1)));
+                        break;
+                    }
 
-                case "todo": {
-                    String instructionName = String.join(" ", Arrays.copyOfRange(instruction, 1, instruction.length));
-                    Task newTodo = new Todo(instructionName);
-                    tasks.add(newTodo);
-                    System.out.println("Got it. I've added this task\n" + newTodo);
-                    System.out.println(String.format("You have %d tasks in the list", tasks.size()));
-                    break;
-                }
+                    case "unmark": {
+                        int taskNum = Integer.parseInt(instruction[1]);
+                        tasks.get(taskNum - 1).markAsNotDone();
+                        System.out.println(String.format("Ok, I have marked this task as not done: \n %s", tasks.get(taskNum - 1)));
+                        break;
+                    }
 
-                case "event": {
-                    int index = Arrays.asList(instruction).indexOf("/at");
-                    String instructionName = String.join(" ", Arrays.copyOfRange(instruction, 1, index));
-                    String time = String.join(" ", Arrays.copyOfRange(instruction, index + 1, instruction.length));
-                    Task newEvent = new Event(instructionName, time);
-                    tasks.add(newEvent);
-                    System.out.println("Got it. I've added this task\n" + newEvent);
-                    System.out.println(String.format("You have %d tasks in the list", tasks.size()));
-                    break;
-                }
+                    case "todo": {
+                        if(instruction.length != 2) {
+                            throw new InvalidArgumentException();
+                        }
+                        String instructionName = String.join(" ", Arrays.copyOfRange(instruction, 1, instruction.length));
+                        Task newTodo = new Todo(instructionName);
+                        tasks.add(newTodo);
+                        System.out.println("Got it. I've added this task\n" + newTodo);
+                        System.out.println(String.format("You have %d tasks in the list", tasks.size()));
+                        break;
+                    }
 
-                case "deadline": {
-                    int index = Arrays.asList(instruction).indexOf("/by");
-                    String instructionName = String.join(" ", Arrays.copyOfRange(instruction, 1, index));
-                    String time = String.join(" ", Arrays.copyOfRange(instruction, index + 1, instruction.length));
-                    Task newDeadline = new Deadline(instructionName, time);
-                    tasks.add(newDeadline);
-                    System.out.println("Got it. I've added this task\n" + newDeadline);
-                    System.out.println(String.format("You have %d tasks in the list", tasks.size()));
-                    break;
+                    case "event": {
+                        if(instruction.length == 1) {
+                            throw new InvalidArgumentException();
+                        }
+                        int index = Arrays.asList(instruction).indexOf("/at");
+                        String instructionName = String.join(" ", Arrays.copyOfRange(instruction, 1, index));
+                        String time = String.join(" ", Arrays.copyOfRange(instruction, index + 1, instruction.length));
+                        Task newEvent = new Event(instructionName, time);
+                        tasks.add(newEvent);
+                        System.out.println("Got it. I've added this task\n" + newEvent);
+                        System.out.println(String.format("You have %d tasks in the list", tasks.size()));
+                        break;
+                    }
+
+                    case "deadline": {
+                        if(instruction.length == 1) {
+                            throw new InvalidArgumentException();
+                        }
+                        int index = Arrays.asList(instruction).indexOf("/by");
+                        String instructionName = String.join(" ", Arrays.copyOfRange(instruction, 1, index));
+                        String time = String.join(" ", Arrays.copyOfRange(instruction, index + 1, instruction.length));
+                        Task newDeadline = new Deadline(instructionName, time);
+                        tasks.add(newDeadline);
+                        System.out.println("Got it. I've added this task\n" + newDeadline);
+                        System.out.println(String.format("You have %d tasks in the list", tasks.size()));
+                        break;
+                    }
+
+                    default: {
+                        throw new InvalidCommandException();
+                    }
                 }
+                System.out.println("");
             }
-            System.out.println("");
+            catch (Exception e){
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
