9ac9e687caf4e19bb6e2a53feafc4af61b0780ee
Justine Koh Yong Tai <>
2022-01-20 15:11:23 +0800

    Change scanner.next to scanner.nextLine

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
+@edu.umd.cs.findbugs.annotations.SuppressFBWarnings("DM_DEFAULT_ENCODING")
 public class Duke {
     public static void main(String[] args) {
         String line = "    ______________________________\n";
         System.out.println(line +
                 "    Hello! I'm Bob!\n    What can I do for you?\n" + line);
         Scanner sc = new Scanner(System.in);
-        String cmd = sc.next();
+        String cmd = sc.nextLine();
         while (!cmd.equals("bye")) {
-            cmd = sc.next();
+            cmd = sc.nextLine();
             System.out.println(line + "    " + cmd + "\n" + line);
         }
-        ;
         System.out.println(line + "    Bye. Hope to see you again soon!\n" + line);
     }
 }
