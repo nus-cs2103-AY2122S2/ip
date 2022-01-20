8577e28e397917959c64069e24865fc0321bc195
Albert Sutiono <albertsutz123@gmail.com>
2022-01-20 21:38:47 +0800

    Implement class Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+    private final String name;
+    private boolean isDone;
+    //can make isDone final for good practice
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public void switchStatus() {
+        this.isDone = !this.isDone;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone? "X" : " ");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s\n", this.getStatusIcon(), this.name);
+    }
+}
