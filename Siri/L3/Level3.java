37ab3af5df46a5649224405078561b9a4c5c4980
Ryan Chang Jia Jie <>
2022-01-21 19:56:57 +0800

    add mark logic to Level3.java

--- Siri/L3/Level3.java
+++ Siri/L3/Level3.java
@@ -8,21 +8,35 @@ public class Level3 {
         String line = "    __________________________________________________\n"; //print a line before greeting message
         System.out.println(line + "     Hi there. I'm Siri \n" + "     How may I help you? \n" + line);
 
-        while(true) { //will keep querying user for input until user inputs "bye" into system
-            String word = sc.nextLine(); //scans entire line as user's input
+        while (true) { //will keep querying user for input until user inputs "bye" into system
+            String word = sc.next();
             if (word.equals("bye")) { //print goodbye text
                 System.out.println(line + "     Goodbye.\n" + line);
                 break; //end while loop
             } else if (word.equals("list")) { //print all elements in a list with index in front
                 System.out.print(line);
                 int s = arrList.size();
-                for(int i = 0; i < s; i++) {
+                for (int i = 0; i < s; i++) {
+                    String currTask = arrList.get(i).toString();
+                    System.out.println("     " + (i + 1) + "." + currTask);
+                }
+                System.out.println(line);
+            } else if (word.equals("mark")) {
+                int num = sc.nextInt();
+                int index = num - 1;
+                Task t = arrList.get(index);
+                t.markAsDone();
+                arrList.set(index, t);
+                System.out.print(line);
+                int s = arrList.size();
+                for (int i = 0; i < s; i++) {
                     String currTask = arrList.get(i).toString();
                     System.out.println("     " + (i + 1) + "." + currTask);
                 }
                 System.out.println(line);
             } else { //print whatever user inputed with "added" infront
-                Task t = new Task(word);
+                String taskToDo = word + sc.nextLine();
+                Task t = new Task(taskToDo);
                 arrList.add(t);
                 System.out.println(line + "     added: " + word + "\n" + line);
             }
