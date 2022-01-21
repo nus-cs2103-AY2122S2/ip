e19e3617b9c3399950bfc3795f9af6b4f6a23a4b
MontyPython28 <samyusubs@gmail.com>
2022-01-21 21:23:58 +0800

    Level-7: Save

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -2,7 +2,9 @@ public class Todo extends Task {
     public Todo (String task, boolean done) {
         super(task, done);
     }
-
+    public String fileFormat() {
+        return String.format("T | %s | %s", this.done ? "X" : " ", this.task);
+    }
     public String toString() {
         return String.format("[T][%s] %s", this.done ? "X" : " ", this.task);
     }
