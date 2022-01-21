097711d221d4ac91dc92a319dac49caa09d43f1a
Daffa Fathani Adila <daffa26@gmail.com>
2022-01-21 23:21:39 +0800

    implement date parsing (minimal)

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private final String deadline;
+    private final LocalDate deadline;
     public Deadline(String taskName, String deadline) {
         super(taskName);
-        this.deadline = deadline;
+        this.deadline = LocalDate.parse(deadline);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + String.format(" (by: %s)", this.deadline);
+        return "[D]" + super.toString() + String.format(" (by: %s)",
+                this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    private final String time;
+    private final LocalDate time;
 
     public Event(String taskName, String time) {
         super(taskName);
-        this.time = time;
+        this.time = LocalDate.parse(time);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + String.format(" (at: %s)", this.time);
+        return "[E]" + super.toString() + String.format(" (at: %s)",
+                this.time.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 }
