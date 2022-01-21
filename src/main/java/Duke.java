181226238caccbee43ade417ad8b8405b0173fc5
KristopherPTaslim <e0424950@u.nus.edu>
2022-01-21 11:56:13 +0800

    Finish Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        ArrayList<String> taskArray = new ArrayList<String>();
+        ArrayList<Task> taskArray = new ArrayList<Task>();
 
         String beginning = "Hello! I'm Duke\n" +
                            "What can I do for you?";
@@ -13,20 +13,27 @@ public class Duke {
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-
+            String[] checkMarking = input.split(" ");
+            
             if (input.equals("list")) {
-
                 if (taskArray.isEmpty()) {
                     System.out.println("List is empty");
                 } else {
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskArray.size(); i++) {
                         String index = String.valueOf(i + 1);
-                        System.out.println( index + ". " + taskArray.get(i));
+                        System.out.println(index + "." + taskArray.get(i));
                     }
                 }
+            } else if (checkMarking[0].equals("mark") || checkMarking[0].equals("unmark")) { //this is to check whether it goes through marking
+                int index = Integer.parseInt(checkMarking[1]) - 1;
+                Task tasks = taskArray.get(index);
+                System.out.println(tasks.marking(checkMarking[0]));
+                taskArray.set(index, tasks);
             } else {
                 ChatBot chatBot = new ChatBot(input);
-                taskArray.add(input);
+                Task tasks = new Task(input);
+                taskArray.add(tasks);
                 System.out.println(chatBot.toString());
             }
             input = sc.nextLine();
@@ -39,3 +46,7 @@ public class Duke {
         sc.close();
     }
 }
+
+
+
+
---
+++ src/main/java/Task.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Task {
+    protected String description;
+    protected ArrayList<Boolean> isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = new ArrayList<Boolean>();
+        isDone.add(false);
+    }
+
+    public String getStatusIcon() {
+        return (isDone.get(0) ? "[X]" : "[ ]"); // mark done task with X // if done is "X" then " " 
+    }
+
+    public void setAsDone() {
+        isDone.set(0, true); 
+    }
+
+    public void setAsUndone() {
+        isDone.set(0, false);
+    }
+
+    public String marking(String mark) {
+        if (mark.equals("mark")) {
+            setAsDone();
+            String messageMarked = "Nice! I've marked this task as done: \n" + 
+                                    this.toString();
+            return messageMarked; 
+        } else {
+            setAsUndone();
+            String messageUnmarked = "OK, I've marked this task as not done yet: \n" +
+                                    this.toString();
+            return messageUnmarked;
+
+        }
+    }
+
+    public String toString() {
+        return getStatusIcon() + " " + this.description;
+    }
+}
