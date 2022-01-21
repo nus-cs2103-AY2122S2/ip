88726adbcfa9c369cec0a150e386dfa56f225da0
pnutzz-0207 <77202907+pnutzz-0207@users.noreply.github.com>
2022-01-21 15:22:25 +0800

    branch-Level-8

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
+import java.time.DateTimeException;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Duke {
 
@@ -92,7 +97,7 @@ public class Duke {
         }
     }
 
-    private static void message_deadline(String user_message) throws DukeException {
+    private static void message_deadline(String user_message) throws DukeException, DateTimeParseException {
         //empty body
         if (user_message.substring(8).replaceAll(" ", "").equals("")) {
             throw new DukeException(LINE + "\nOh no..did you forget to put what you need to do?\n" + LINE);
@@ -112,10 +117,42 @@ public class Duke {
                     + LINE);
         }
 
-        tasks.add(new Deadline(taskDetails[0], taskDetails[1]));
-        System.out.println((LINE + "\nWokay! I've added this task:\n"
-                + tasks.get(tasksAdded_index).toString()
-                + "\nNow you have " + (tasksAdded_index + 1) + " task(s) in the list\n" + LINE));
+        //processing date & time
+        String dateTime = taskDetails[1].replaceAll(" ", "");
+        LocalDateTime dateTimeComplete;
+        boolean timeEntered = false;
+
+        if (dateTime.length() == 14) { //date & time
+            LocalDate date = LocalDate.parse(dateTime.substring(0, 10));
+            int hour = Integer.parseInt(dateTime.substring(10, 12));
+            int min = Integer.parseInt(dateTime.substring(12));
+            dateTimeComplete = date.atTime(hour, min);
+            timeEntered = true;
+
+        } else if (dateTime.length() == 10) { //date only
+            LocalDate date = LocalDate.parse(dateTime.substring(0, 10));
+            dateTimeComplete = date.atTime(23, 59);
+
+        //date & time invalid
+        } else {
+            throw new DukeException(LINE
+                    + "\nOh no... the format or the date/time you entered is wrong! The correct format should be YYYY-MM-DD HHMM\n"
+                    + "It is not necessary to put in time!\n"
+                    + LINE);
+        }
+
+        tasks.add(new Deadline(taskDetails[0], dateTimeComplete));
+
+        if (timeEntered) {
+            System.out.println((LINE + "\nWokay! I've added this task:\n"
+                    + tasks.get(tasksAdded_index).toString()
+                    + "\nNow you have " + (tasksAdded_index + 1) + " task(s) in the list\n" + LINE));
+        } else {
+            System.out.println((LINE + "\nSince you did not enter the due time for this task, I will help you set it at 23:59!\n"
+                    + tasks.get(tasksAdded_index).toString()
+                    + "\nNow you have " + (tasksAdded_index + 1) + " task(s) in the list\n" + LINE));
+        }
+
         tasksAdded_index++;
     }
 
@@ -139,10 +176,42 @@ public class Duke {
                     + LINE);
         }
 
-        tasks.add(new Event(taskDetails[0], taskDetails[1]));
-        System.out.println((LINE + "\nWokay! I've added this task:\n"
-                + tasks.get(tasksAdded_index).toString()
-                + "\nNow you have " + (tasksAdded_index + 1) + " task(s) in the list\n" + LINE));
+        //processing date & time
+        String dateTime = taskDetails[1].replaceAll(" ", "");
+        LocalDateTime dateTimeComplete;
+        boolean timeEntered = false;
+
+        if (dateTime.length() == 14) { //date & time
+            LocalDate date = LocalDate.parse(dateTime.substring(0, 10));
+            int hour = Integer.parseInt(dateTime.substring(10, 12));
+            int min = Integer.parseInt(dateTime.substring(12));
+            dateTimeComplete = date.atTime(hour, min);
+            timeEntered = true;
+
+        } else if (dateTime.length() == 10) { //date only
+            LocalDate date = LocalDate.parse(dateTime.substring(0, 10));
+            dateTimeComplete = date.atTime(0, 0);
+
+            //date & time invalid
+        } else {
+            throw new DukeException(LINE
+                    + "\nOh no... the format or the date/time you entered is wrong! The correct format should be YYYY-MM-DD HHMM\n"
+                    + "It is not necessary to put in time!\n"
+                    + LINE);
+        }
+
+        tasks.add(new Event(taskDetails[0], dateTimeComplete));
+
+        if (timeEntered) {
+            System.out.println((LINE + "\nWokay! I've added this task:\n"
+                    + tasks.get(tasksAdded_index).toString()
+                    + "\nNow you have " + (tasksAdded_index + 1) + " task(s) in the list\n" + LINE));
+        } else {
+            System.out.println((LINE + "\nSince you did not enter what time is this event happening, I will help you set it at 00:00!\n"
+                    + tasks.get(tasksAdded_index).toString()
+                    + "\nNow you have " + (tasksAdded_index + 1) + " task(s) in the list\n" + LINE));
+        }
+
         tasksAdded_index++;
     }
 
@@ -175,6 +244,53 @@ public class Duke {
         tasksAdded_index--;
     }
 
+    private static void message_schedule(String user_message) throws DukeException {
+        //list is empty
+        if (list_isEmpty()) {
+            throw new DukeException(LINE
+                    + "\nThere is nothing happening on this day! Hooray~\n" + LINE);
+        }
+
+        String dateString = user_message.substring(8).replaceAll(" ", "");
+        //no date given
+        if (dateString.equals("")) {
+            throw new DukeException(LINE
+                    + "\nWhich day would you like to check? Input format: schedule YYYY-MM-DD\n" + LINE);
+        }
+
+        //date is invalid
+        LocalDate date = LocalDate.parse(dateString);
+        ArrayList<Task> scheduleList = new ArrayList<>(100);
+
+        for (int i = 0; i < tasksAdded_index; i++) {
+            if (tasks.get(i) instanceof Event) {
+                Event task = (Event) tasks.get(i);
+                if (date.isEqual(task.getAt().toLocalDate())) {
+                    scheduleList.add(task);
+                }
+            }
+            if (tasks.get(i) instanceof Deadline) {
+                Deadline task = (Deadline) tasks.get(i);
+                if (date.isEqual((task.getBy().toLocalDate()))) {
+                    scheduleList.add(task);
+                }
+            }
+        }
+
+        if (scheduleList.isEmpty()) {
+            System.out.println(LINE
+                    +  "\nThere is nothing happening on this day! Hooray~\n" + LINE);
+        } else {
+            System.out.println(LINE
+                    + "\nThese are the events/deadlines happening on " + date.toString() + ":");
+            for (int i = 0; i < scheduleList.size(); i++) {
+                System.out.println((i + 1) + "." + scheduleList.get(i).toString());
+            }
+            System.out.println(LINE);
+        }
+
+    }
+
 
     private static void messageProcess() {
         Scanner user_input = new Scanner(System.in).useDelimiter("\n");
@@ -204,11 +320,25 @@ public class Duke {
                 } else if (user_message.startsWith("delete") && split[0].equals("delete")) {
                     message_delete(user_message);
 
-                } else {
+                }  else if (user_message.startsWith("schedule") && split[0].equals("schedule")) {
+                    message_schedule(user_message);
+                }
+
+
+                else {
                     System.out.println(LINE + "\n" + user_message + "? Sorry, I don't understand what that means.. :(\n" + LINE);
                 }
             } catch (DukeException err) {
                 System.out.println(err.getMessage());
+            } catch (DateTimeParseException err) {
+                System.out.println(LINE
+                        + "\nOh no... the format or the date/time you entered is wrong! The correct format should be YYYY-MM-DD HHMM\n"
+                        + "It is not necessary to put in time!\n"
+                        + LINE);
+            } catch (DateTimeException err) {
+                System.out.println(LINE
+                        + "\nOops... did you put in the wrong date or time? :(\n"
+                        + LINE);
             }
 
             user_message = user_input.next();
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,41 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Event extends Task{
 
-    protected String at;
+    protected LocalDateTime at;
 
-    public Event(String description, String at) {
+    public Event(String description, LocalDateTime at) {
         super(description);
         this.at = at;
     }
 
+    public LocalDateTime getAt() {
+        return at;
+    }
+
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + at + ")";
+        int year = at.getYear();
+        String month = super.months[at.getMonthValue() - 1];
+        int day = at.getDayOfMonth();
+
+        String hour;
+        if (at.getHour() < 10) {
+            hour = "0" + at.getHour();
+        } else {
+            hour = String.valueOf(at.getHour());
+        }
+
+        String min;
+        if (at.getMinute() < 10) {
+            min = "0" + at.getMinute();
+        } else {
+            min = String.valueOf(at.getMinute());
+        }
+
+        return "[E]"
+                + super.toString()
+                + " (at: " + month + " " + day + " " + year + " " + hour + ":" + min + ")";
     }
 }
