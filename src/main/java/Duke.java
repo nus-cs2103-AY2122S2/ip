beb8f9a04f014cb7d5db740ec303fc8490b2d447
Gernene <wallie988@gmail.com>
2022-01-21 14:59:22 +0800

    Complete Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String str = sc.nextLine();
+            if (str.equals("bye")) {
+                System.out.println("Bye for now!");
+                return;
+            }
+            System.out.println(str);
+        }
     }
 }
