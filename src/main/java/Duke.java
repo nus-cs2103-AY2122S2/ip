721857549882e33f34c8209e8b3c20ea69baf878
SethCKL <chongkokleong1998@gmail.com>
2022-01-21 14:04:04 +0800

    Level-1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello, this is Belvedere, your virtual assistant!");
+        printLine();
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (true) {
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+            printLine();
+            input = scanner.nextLine();
+        }
+    }
+    // Static method to print a line in the terminal to seperate calls
+    public static void printLine() {
+        System.out.println("------------------------------");
     }
 }
