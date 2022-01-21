277c43ff56682054fec76cd85ec64f2c7f1f5aa7
KristopherPTaslim <e0424950@u.nus.edu>
2022-01-21 14:44:17 +0800

    Finish Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,7 +13,9 @@ public class Duke {
         String input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            String[] checkMarking = input.split(" ");
+
+            String[] checkType = input.split(" ");
+            String messageTask = "Got it. I've added this task: \n";
             
             if (input.equals("list")) {
                 if (taskArray.isEmpty()) {
@@ -25,11 +27,39 @@ public class Duke {
                         System.out.println(index + "." + taskArray.get(i));
                     }
                 }
-            } else if (checkMarking[0].equals("mark") || checkMarking[0].equals("unmark")) { //this is to check whether it goes through marking
-                int index = Integer.parseInt(checkMarking[1]) - 1;
+            } else if (checkType[0].equals("mark") || checkType[0].equals("unmark")) { //this is to check whether it goes through marking
+                int index = Integer.parseInt(checkType[1]) - 1;
                 Task tasks = taskArray.get(index);
-                System.out.println(tasks.marking(checkMarking[0]));
+                System.out.println(tasks.marking(checkType[0]));
                 taskArray.set(index, tasks);
+            } else if (checkType[0].equals("todo")) { //check todo
+                String stringSliced = input.substring(5,input.length());
+                Todo todoTask = new Todo(stringSliced);
+                taskArray.add(todoTask);
+                String noOfTask = String.valueOf(taskArray.size());
+                System.out.println(messageTask + todoTask.toString() + "\n"
+                                   + "Now you have " + noOfTask + " tasks in the list.");
+
+            } else if (checkType[0].equals("deadline")) { //check deadline
+                String deadlineCondition = "/by "; 
+                int indexOfTime = input.indexOf(deadlineCondition); //to find /
+                String dateTime = input.substring(indexOfTime + deadlineCondition.length(), input.length()); // the date and time for by
+                String stringSliced = input.substring(9, indexOfTime); // after deadline
+                Deadline deadlineTask = new Deadline(stringSliced, dateTime);
+                taskArray.add(deadlineTask);
+                String noOfTask = String.valueOf(taskArray.size());
+                System.out.println(messageTask + deadlineTask.toString() + "\n"
+                                    + "Now you have " + noOfTask + " tasks in the list.");
+            } else if (checkType[0].equals("event")) { // check event
+                String eventCondition = "/at "; 
+                int indexOfTime = input.indexOf(eventCondition); //to find /
+                String dateTime = input.substring(indexOfTime + eventCondition.length(), input.length()); // the date and time for by
+                String stringSliced = input.substring(6, indexOfTime); // after deadline
+                Event eventTask = new Event(stringSliced, dateTime);
+                taskArray.add(eventTask);
+                String noOfTask = String.valueOf(taskArray.size());
+                System.out.println(messageTask + eventTask.toString() + "\n"
+                                    + "Now you have " + noOfTask + " tasks in the list.");
             } else {
                 ChatBot chatBot = new ChatBot(input);
                 Task tasks = new Task(input);
