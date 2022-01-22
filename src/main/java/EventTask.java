be0bc5df6c7fbbbcc04487bdd49b178a7a434e10
hanqinilnix <ohanqin@gmail.com>
2022-01-22 22:04:10 +0800

    Teach Ari how to understand date

--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -1,17 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class EventTask extends Task {
     protected String time;
+    protected LocalDate nowTime;
 
-    public EventTask(String message) throws EmptyMessageException, DateFormatException {
+
+    public EventTask(String message) throws EmptyMessageException, DateFormatException, DateTimeParseException {
         String[] taskArray = message.split("/at");
+        LocalDate nowTime;
 
         if (taskArray[0].equals("")) {
             throw new EmptyMessageException();
         } else if (taskArray.length == 1) {
             throw new DateFormatException("Sorry Master, when is the due? (include /at)");
+        } else {
+            nowTime = LocalDate.parse(taskArray[1].stripLeading().stripTrailing());
         }
 
         super.taskMessage = taskArray[0].stripLeading().stripTrailing();
-        time = taskArray[1].stripLeading().stripTrailing();
+        this.nowTime = nowTime;
+        time = nowTime.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
     }
 
     public EventTask(String message, String when) {
