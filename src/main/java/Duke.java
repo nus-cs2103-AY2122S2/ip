abde661b5fb9a65487684b224c900724ea8ba311
AdrianOngJJ <adrianong360@gmail.com>
2022-01-22 19:40:29 +0800

    Add ability to mark oand unmark

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,23 +10,29 @@ import java.util.ArrayList;
  * while applying as many Java and SE techniques as possible along the way.
  *
  * @author AdrianOngJJ
+ * @version 0.1
+ * @since 22/1/2022
  */
 public class Duke {
     private static final String LINE_BREAK = "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-    private static final ArrayList<String> masterList = new ArrayList<>();
+    private static final ArrayList<Task> masterList = new ArrayList<>();
     /**
-     * Prints line break
+     * Prints line break.
      * @return void
      */
     private static final void printLineBreak() {
         System.out.println(LINE_BREAK);
     }
 
+    /**
+     * Prints Master List
+     * @param bw BufferedWriter from main to print out the Master List.
+     * @throws java.io.IOException If an I/O error occurs. Only takes in string.
+     */
     private static final void printList(BufferedWriter bw) throws Exception {
-        Object[] masterListArr = masterList.toArray();
-        int x = 1;
-        for (Object s : masterListArr) {
-            bw.write(x++ + ". " + s);
+        for(int i = 0; i < masterList.size(); i++) {
+            Task curr = masterList.get(i);
+            bw.write((i + 1) + "." + curr.toString());
             bw.newLine();
         }
         bw.flush();
@@ -44,24 +50,45 @@ public class Duke {
         printLineBreak();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String inputCommand = "";
+        String[] inputCommandArr = new String[100];
         boolean ifBye = false;
         do {
             printLineBreak();
             System.out.println();
             inputCommand = br.readLine();
+            inputCommandArr = inputCommand.split(" ");
             printLineBreak();
-            switch (inputCommand) {
+            switch (inputCommandArr[0]) {
                 case "bye":
                     ifBye = true;
                     System.out.println("Bye. Hope to see you again soon!");
                     break;
 
                 case "list":
+                    bw.write("Here are the tasks in your list:\n");
                     printList(bw);
                     break;
 
+                case "mark":
+                    bw.write("Nice! I've marked this task as done:\n");
+                    int index = Integer.parseInt(inputCommandArr[1]) - 1;
+                    Task curr = masterList.get(index); // task to be marked
+                    masterList.set(index, curr.markAsDone());
+                    bw.write(masterList.get(index).toString());
+                    bw.flush();
+                    break;
+
+                case "unmark":
+                    bw.write("OK, I've marked this task as not done yet:\n");
+                    int indexUnmark = Integer.parseInt(inputCommandArr[1]) - 1;
+                    Task currUnmark = masterList.get(indexUnmark); // task to be unmarked
+                    masterList.set(indexUnmark, currUnmark.unmarkItem());
+                    bw.write(masterList.get(indexUnmark).toString());
+                    bw.flush();
+                    break;
+
                 default:
-                    masterList.add(inputCommand);
+                    masterList.add(new Task(inputCommand));
                     System.out.println("added: " + inputCommand);
             }
         } while (!ifBye);
