529e8e42038e3b8f15e95385a8e224faa1f8688f
swiftguy1 <rocklikeatornado@gmail.com>
2022-01-22 20:35:52 +0800

    added level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Duke {
     private static final String lines = "____________________________________________________________";
     public static void main(String[] args) {
@@ -7,9 +10,22 @@ public class Duke {
         System.out.println("What do you wish for me to do?" + "\n" + lines);
 
         String echo = sc.nextLine();
+        ArrayList<String> arr = new ArrayList<>();
+
         while(!echo.equals("bye")){
-            System.out.println(lines + "\n" + echo + "\n" + lines);
-            echo = sc.nextLine();
+            if (!echo.equals("list")) {
+                arr.add(echo);
+                System.out.println(lines + "\n" + "added: " + echo + "\n" + lines);
+                echo = sc.nextLine();
+            } else {
+                int index = 1;
+                System.out.println(lines);
+                for(String str : arr){
+                    System.out.println(index +". " + str);
+                    index++;
+                }
+                echo = sc.nextLine();
+            }
         }
 
         System.out.println(lines + "\n" + "Bye. Hope to see you again soon!" + "\n" + lines);
