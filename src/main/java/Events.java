4cda252c15c768d676cea69a76326b11ad285a16
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-01-22 15:57:31 +0800

    Add date functionality in events and deadlines
    Tasks are able to recognize dates and print them as dates.

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,16 +1,31 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Events extends Task{
 
     private String time;
+    LocalDate date;
 
 
     Events(String description, String time) {
         super(description);
         this.time = time;
+        try {
+            date = LocalDate.parse(time);
+        } catch (DateTimeParseException e) {
+            date = null;
+        }
     }
 
     public void print() {
         System.out.print("[E]");
         System.out.print("[" + (this.completed ? "x" : " ") +  "] " + this.description);
-        System.out.println(" (at: " + this.time + ")");
+        if (date == null) {
+            System.out.println(" (at: " + this.time + ")");
+        } else {
+            System.out.println(" (at: " + this.date.format(DateTimeFormatter.ofPattern("dd MMM yyyy")) + ")");
+        }
+
     }
 }
