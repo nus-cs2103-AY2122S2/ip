6cc42e08b199cdca0c90613e275252951935ac07
Ho Lanzan (Leon) <ho.leon98@gmail.com>
2022-01-22 17:31:54 +0800

    Level-8: Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,14 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
     public Deadline(String description) throws DukeException {
         super(description);
         try {
-            String[] strArr = description.split("/at");
-            this.description = strArr[0] + "(" + "at:" + strArr[1] + ")";
+            String[] strArr = description.split("/by ");
+            String[] DateAndTime = strArr[1].split(" ");
+            LocalDate date = LocalDate.parse(DateAndTime[0]);
+            this.description = strArr[0] + "(" + "by: " + date.format(DateTimeFormatter.ofPattern("MMM d YYYY")) + ")";;
         }
         catch (ArrayIndexOutOfBoundsException e){
             throw new DukeException();
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,10 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
 
     public Event(String description) throws DukeException {
         super(description);
         try {
-            String[] strArr = description.split("/at");
-            this.description = strArr[0] + "(" + "at:" + strArr[1] + ")";
+            String[] strArr = description.split("/at ");
+            String[] DateAndTime = strArr[1].split(" ");
+            LocalDate date = LocalDate.parse(DateAndTime[0]);
+            this.description = strArr[0] + "(" + "at: " + date.format(DateTimeFormatter.ofPattern("MMM d YYYY")) + ")";
         }
         catch (ArrayIndexOutOfBoundsException e){
             throw new DukeException();
