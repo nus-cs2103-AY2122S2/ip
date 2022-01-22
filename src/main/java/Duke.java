dcc799ee0510d2d8d98f22d5f845a6225a18b2dc
AdrianOngJJ <adrianong360@gmail.com>
2022-01-22 17:20:19 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,24 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+/**
+ * Project Duke is a educational software project designed to take you
+ * through the steps of building a small software incrementally,
+ * while applying as many Java and SE techniques as possible along the way.
+ *
+ * @author AdrianOngJJ
+ */
 public class Duke {
+    private static final String LINE_BREAK = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+
+    /**
+     * Prints line break
+     * @return void
+     */
+    private static final void printLineBreak() {
+        System.out.println(LINE_BREAK);
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -13,9 +30,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String s = br.readLine();
-        bw.write(s);
-        bw.flush();
+        printLineBreak();
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        printLineBreak();
+        String inputCommand = "";
+        boolean ifBye = false;
+        do {
+            inputCommand = br.readLine();
+            switch (inputCommand) {
+                case "bye":
+                    ifBye = true;
+                    bw.write("Bye. Hope to see you again soon!");
+                    bw.close();
+                    break;
+
+                default:
+                    System.out.println(inputCommand);
+            }
+        } while (!ifBye);
 
     }
 }
