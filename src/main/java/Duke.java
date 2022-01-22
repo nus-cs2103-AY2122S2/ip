e7aae12a90c03638dd51f1cf2a0bffd7ee893df1
kuanjr <77227003+kuanjr@users.noreply.github.com>
2022-01-23 02:55:36 +0800

    added Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static String[] itemArray = new String[100];
+    public static int numberOfItems = 0;
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -22,15 +24,33 @@ public class Duke {
             userInput = dukeScan.nextLine();
             if (userInput.equals("bye")){
                 break;
+            } else if (userInput.equals("list")) {
+                listItem();
+            } else {
+                addItem(userInput);
             }
-            System.out.println("__________________________________");
-            System.out.println(userInput);
-            System.out.println("__________________________________");
         }
         System.out.println("__________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("__________________________________");
+    }
 
+    public static void addItem(String item) {
+        itemArray[numberOfItems] = item;
+        numberOfItems++;
+        System.out.println("__________________________________");
+        System.out.println("added: " + item);
+        System.out.println("__________________________________");
+    }
 
+    public static void listItem() {
+        System.out.println("__________________________________");
+        for (int i = 0; i < 100; i++) {
+            if (itemArray[i] == null) {
+                break;
+            }
+            System.out.printf("%d. " + itemArray[i] + "\n", i+1);
+        }
+        System.out.println("__________________________________");
     }
 }
