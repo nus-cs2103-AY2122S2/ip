fa48c36781be4fb7a92ba74c03501beb442e49cc
AdrianOngJJ <adrianong360@gmail.com>
2022-01-22 20:00:01 +0800

    Add Task class

---
+++ src/main/java/ToDos.java
@@ -0,0 +1,13 @@
+/**
+ * To Dos are tasks without any date/time attached to it
+ */
+public class ToDos extends Task {
+    public ToDos(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
