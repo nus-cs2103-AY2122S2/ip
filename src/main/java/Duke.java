adbd4f13a07b1155443d2c3c6866da779d29a8a8
MontyPython28 <samyusubs@gmail.com>
2022-01-22 23:06:14 +0800

    A-MoreOOP

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,46 +27,12 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
         ui.sayHello();
-        String input = ui.getCommand();
 
-        String[] splitted = input.split(" ", 2);
+        String input = ui.getCommand();
         while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                Command c = new ListCommand();
-                c.execute(ui, storage, taskList);
-            } else if (splitted[0].equals("delete")) {
-                int serialNumber = Integer.parseInt(splitted[1]);
-                Command c = new DeleteCommand(serialNumber);
-                c.execute(ui, storage, taskList);
-            } else if (splitted[0].equals("todo")) {
-                if (input.equals("todo") || input.equals("todo ")) {
-                    Ui.outputError("Please describe your todo :-(");
-                } else {
-                    Command c = new TodoCommand(splitted[1]);
-                    c.execute(ui, storage, taskList);
-                }
-            } else if (splitted[0].equals("deadline")) {
-                String[] time = splitted[1].split("/by ", 2);
-                Command c = new DeadlineCommand(time[0], time[1]);
-                c.execute(ui, storage, taskList);
-            } else if (splitted[0].equals("event")) {
-                String[] time = splitted[1].split("/at ");
-                Command c = new EventCommand(time[0], time[1]);
-                c.execute(ui, storage, taskList);
-            } else if (splitted[0].equals("mark")) {
-                int serialNumber = Integer.parseInt(splitted[1]);
-                Command c = new MarkCommand(serialNumber);
-                c.execute(ui, storage, taskList);
-            } else if (splitted[0].equals("unmark")) {
-                int serialNumber = Integer.parseInt(splitted[1]);
-                Command c = new UnmarkCommand(serialNumber);
-                c.execute(ui, storage, taskList);
-            } else {
-                // throw new DukeException("I'm sorry, but I don't know what that means :-(");
-                Ui.outputError(" I'm sorry, but I don't know what that means :-(");
-            }
+            Command c = Parser.parse(input);
+            c.execute(ui, storage, taskList);
             input = ui.getCommand();
-            splitted = input.split(" ", 2);
         }
 
         ui.exit();
