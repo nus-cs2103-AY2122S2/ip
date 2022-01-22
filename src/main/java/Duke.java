a5ebb87a478c2a1aae046d140249b512615e018d
garethkoh <e0533656@u.nus.edu>
2022-01-22 17:05:58 +0800

    Level 5. Handle errors. all errors handled.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -26,7 +27,8 @@ public class Duke
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("enter a command");
+        System.out.println("enter a command\n" +
+                "use command 'help' to see list of commands");
 
         String userInput = sc.nextLine();
         String[] split = userInput.split(" ");
@@ -41,69 +43,155 @@ public class Duke
                     System.out.printf("%d. " + t.toString(), i + 1);
                 }
             }
-            else if (split[0].equals("unmark") && split.length > 1 && isInteger(split[1]))
+            else if (split[0].equals("help"))
             {
-                int item = Integer.parseInt(split[1]);
-                Task t = toDoList.get(item - 1);
-                t.unmark();
-                System.out.printf("Boo! more work to do: %s\n", t.getName());
+                System.out.println("try the following commands:\n" +
+                        "   list (lists out all tasks in your todo list)\n" +
+                        "   todo <task> (adds a basic task to your todo list)\n" +
+                        "   deadline <task> /by <date> (adds a task with a deadline to your todo list)\n" +
+                        "   event <task> /at <date> (adds a new event with its corresponding date to your todo list)\n" +
+                        "   mark <task index> (marks that specific task as completed)\n" +
+                        "   unmark <task index> (removes mark from specific task)\n" +
+                        "   delete <task index> (removes task from todo list)\n" +
+                        "   bye (closes application)");
             }
-            else if (split[0].equals("mark") && split.length > 1 && isInteger(split[1]))
+            else if (split[0].equals("unmark"))
             {
-                int item = Integer.parseInt(split[1]);
-                Task t = toDoList.get(item - 1);
-                t.mark();
-                System.out.printf("great job! I've marked this task as done: %s\n", t.getName());
-            }
-            else if (split[0].equals("todo") && split.length > 1)
-            {
-                String name = userInput.substring(5);
-                ToDo t = new ToDo(name);
-                toDoList.add(t);
-                System.out.printf("task added:\n%s", t);
-                System.out.printf("you now have %d tasks\n",toDoList.size());
+                try{
+                    int item = Integer.parseInt(split[1]);
+                    try{
+                        Task t = toDoList.get(item - 1);
+                        t.unmark();
+                        System.out.printf("Boo! more work to do: %s\n", t.getName());
+                    }
+                    catch (IndexOutOfBoundsException e){
+                        System.out.println(("the index you have entered does not exist!"));
+                    }
+                }
+                catch (IndexOutOfBoundsException e){
+                    System.out.println("unmark description cannot be empty!");
+                }
+                catch (NumberFormatException e){
+                    System.out.println("unmark description must be an integer!");
+                }
+
 
             }
-            else if (split[0].equals("deadline") && split.length > 1 && userInput.contains("/"))
+            else if (split[0].equals("mark"))
             {
-                String item = userInput.substring(9);
-                String[] divide = item.split("/");
-                String name = divide[0];
-                String dueDate = divide[1];
-                Deadline d = new Deadline(name, dueDate.substring(3));
-                toDoList.add(d);
-                System.out.printf("task added:\n%s", d.toString());
-                System.out.printf("you now have %d tasks\n", toDoList.size());
+                try{
+                    int item = Integer.parseInt(split[1]);
+                    try{
+                        Task t = toDoList.get(item - 1);
+                        t.mark();
+                        System.out.printf("great job! I've marked this task as done: %s\n", t.getName());
+                    }
+                    catch (IndexOutOfBoundsException e){
+                        System.out.println(("the index you have entered does not exist!"));
+                    }
+                }
+                catch (IndexOutOfBoundsException e){
+                    System.out.println("mark description cannot be empty!");
+                }
+                catch (NumberFormatException e){
+                    System.out.println("mark description must be an integer!");
+                }
 
             }
-            else if (split[0].equals("event") && split.length > 1 && userInput.contains("/"))
+            else if (split[0].equals("todo"))
             {
-                String item = userInput.substring(6);
-                String[] divide = item.split("/");
-                String name = divide[0];
-                String time = divide[1];
-                Event e = new Event(name, time.substring(3));
-                toDoList.add(e);
-                System.out.printf("task added:\n%s", e.toString());
-                System.out.printf("you now have %d tasks\n", toDoList.size());
+                try{
+                    String check = split[1];
+                    String name = userInput.substring(5);
+                    ToDo t = new ToDo(name);
+                    toDoList.add(t);
+                    System.out.printf("task added:\n%s", t);
+                    System.out.printf("you now have %d tasks\n",toDoList.size());
+                }
+                catch (Exception e){
+                    System.out.println("the description of a todo cannot be empty!");
+                }
             }
-            else if (split[0].equals("delete") && split.length > 1 && isInteger(split[1]))
+            else if (split[0].equals("deadline"))
             {
-                int toDelete = Integer.parseInt(split[1]);
-                Task t = toDoList.get(toDelete - 1);
-                toDoList.remove(toDelete - 1);
-                System.out.printf("task removed:\n%s", t.toString());
-                System.out.printf("you now have %d tasks\n", toDoList.size());
+                try{
+                    String check = split[1];
+                    try{
+                        String item = userInput.substring(9);
+                        String[] divide = item.split("/");
+                        String name = divide[0];
+                        String dueDate = divide[1];
+                        Deadline d = new Deadline(name, dueDate.substring(3));
+                        toDoList.add(d);
+                        System.out.printf("task added:\n%s", d.toString());
+                        System.out.printf("you now have %d tasks\n", toDoList.size());
+                    }
+                    catch(IndexOutOfBoundsException e){
+                        System.out.println("deadline description must contain a date!");
+                    }
+
+                }
+                catch (IndexOutOfBoundsException e){
+                    System.out.println("deadline description cannot be empty!");
+                }
+
+
+            }
+            else if (split[0].equals("event"))
+            {
+                try{
+                    String check = split[1];
+                    try{
+                        String item = userInput.substring(6);
+                        String[] divide = item.split("/");
+                        String name = divide[0];
+                        String time = divide[1];
+                        Event e = new Event(name, time.substring(3));
+                        toDoList.add(e);
+                        System.out.printf("task added:\n%s", e.toString());
+                        System.out.printf("you now have %d tasks\n", toDoList.size());
+                    }
+                    catch(IndexOutOfBoundsException e){
+                        System.out.println("event description must contain a date!");
+                    }
+                }
+                catch (IndexOutOfBoundsException e){
+                    System.out.println("event description cannot be empty!");
+                }
+
+            }
+            else if (split[0].equals("delete"))
+            {
+                try{
+                    int toDelete = Integer.parseInt(split[1]);
+                    try{
+                        Task t = toDoList.get(toDelete - 1);
+                        toDoList.remove(toDelete - 1);
+                        System.out.printf("task removed:\n%s", t.toString());
+                        System.out.printf("you now have %d tasks\n", toDoList.size());
+                    }
+                    catch (IndexOutOfBoundsException e){
+                        System.out.println(("the index you have entered does not exist!"));
+                    }
+                }
+                catch (IndexOutOfBoundsException e){
+                    System.out.println("delete description cannot be empty!");
+                }
+                catch (NumberFormatException e){
+                    System.out.println("delete description must be an integer!");
+                }
+
+
             }
             else
             {
-                System.out.println("task added: " + userInput);
-                toDoList.add(new Task(userInput));
+                System.out.println("invalid command! try 'help' for list of commands");
             }
             userInput = sc.nextLine();
             split = userInput.split(" ");
         }
-        System.out.println("goodbye!\n");
+        System.out.println("goodbye!");
+        sc.close();
     }
 
 
