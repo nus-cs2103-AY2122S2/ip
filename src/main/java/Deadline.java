68f7ab54d3d20368ddc7e4f031428f364711d07d
Ho Lanzan (Leon) <ho.leon98@gmail.com>
2022-01-22 23:18:45 +0800

    Modify output format for tasks

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,7 +8,13 @@ public class Deadline extends Task{
             String[] strArr = description.split("/by ");
             String[] DateAndTime = strArr[1].split(" ");
             LocalDate date = LocalDate.parse(DateAndTime[0]);
-            this.description = strArr[0] + "(" + "by: " + date.format(DateTimeFormatter.ofPattern("MMM d YYYY")) + ")";;
+            if (DateAndTime[1] != null) {
+                this.description = strArr[0] + "| " + "by: " + date.format(DateTimeFormatter.ofPattern("MMM d YYYY"))
+                        + " " + DateAndTime[1];
+            }
+            else{
+                this.description = strArr[0] + "| " + "by: " + date.format(DateTimeFormatter.ofPattern("MMM d YYYY"));
+            }
         }
         catch (ArrayIndexOutOfBoundsException e){
             throw new DukeException();
