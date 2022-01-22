0eaf0749445ee29c4fbcbf7c9c06b4826789f293
markbiju <markbiju16@gmail.com>
2022-01-23 02:13:33 +0800

    Level-4 upd

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -26,19 +26,26 @@ public class Duke {
                 currentTask.markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(currentTask);
+
             } else if(input.substring(0,6).equals("unmark")){
                 int toBeUnmarked = Integer.parseInt(input.substring(7));
                 Task currentTask = tasks[toBeUnmarked - 1];
                 currentTask.markAsUndone();
                 System.out.println("OK, I've marked this task as not done yet: ");
                 System.out.println(currentTask);
+
             } else if(input.substring(0, 4).equals("todo")) {
                 String[] arrOfStr = input.split(" ", 2);
                 tasks[counter] = new Todo(arrOfStr[1]);
                 counter = counter + 1;
                 System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[counter]);
-                System.out.println("Now you have " + String.valueOf(counter + 1) + " tasks in the list.");
+                System.out.println(tasks[counter - 1]);
+                if(counter == 1){
+                    System.out.println("Now you have " + String.valueOf(counter) + " task in the list.");
+                } else {
+                    System.out.println("Now you have " + String.valueOf(counter) + " tasks in the list.");
+                }
+
             } else if(input.substring(0, 8).equals("deadline")) {
                 String[] arrOfStr = input.split(" ", 2);
                 String[] arrOfStr2 = arrOfStr[1].split("/", 2);
@@ -47,8 +54,27 @@ public class Duke {
                 tasks[counter] = new Deadline(arrOfStr2[0], arrOfStr3[1]);
                 counter = counter + 1;
                 System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[counter]);
-                System.out.println("Now you have " + String.valueOf(counter + 1) + " tasks in the list.");
+                System.out.println(tasks[counter - 1]);
+                if(counter == 1){
+                    System.out.println("Now you have " + String.valueOf(counter) + " task in the list.");
+                } else {
+                    System.out.println("Now you have " + String.valueOf(counter) + " tasks in the list.");
+                }
+            } else if(input.substring(0, 5).equals("event")) {
+                String[] arrOfStr = input.split(" ", 2);
+                String[] arrOfStr2 = arrOfStr[1].split("/", 2);
+                String[] arrOfStr3 = arrOfStr2[1].split(" ", 2);
+
+                tasks[counter] = new Event(arrOfStr2[0], arrOfStr3[1]);
+                counter = counter + 1;
+                System.out.println("Got it. I've added this task:");
+                System.out.println(tasks[counter - 1]);
+                if(counter == 1){
+                    System.out.println("Now you have " + String.valueOf(counter) + " task in the list.");
+                } else {
+                    System.out.println("Now you have " + String.valueOf(counter) + " tasks in the list.");
+                }
+
 
             } else {
                 tasks[counter] = new Task(input);
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,14 @@
-public class Event {
+public class Event extends Task {
+
+    protected String at;
+
+    public Event(String description, String by) {
+        super(description);
+        this.at = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + at + ")";
+    }
 }
