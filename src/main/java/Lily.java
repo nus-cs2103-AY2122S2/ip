40730b2ba476d4f5cf1f0b4f782196d78af18038
ian-from-dover <ian09hong@gmail.com>
2022-01-23 19:50:52 +0800

    Implemented Delete and Remove functionality

--- src/main/java/Lily.java
+++ src/main/java/Lily.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class Lily {
     private static final String indent = "    ";
     private static LinkedList<Task> list;
+
     public static void main(String[] args) {
         list = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
@@ -25,11 +26,11 @@ public class Lily {
 
         String welcomeMessage = logo;
         welcomeMessage += indent + "hey.\n"
-        + indent + "i don't really wanna track your tasks,\n"
-        + indent + "but i guess i have no choice (っ◞‸◟c)\n"
-        + indent + "need help with something?\n"
-        + "\n"
-        + indent + "Things you can type\n" + listCommands();
+                + indent + "i don't really wanna track your tasks,\n"
+                + indent + "but i guess i have no choice (っ◞‸◟c)\n"
+                + indent + "need help with something?\n"
+                + "\n"
+                + indent + "Things you can type\n" + listCommands();
         prettyPrint(welcomeMessage);
 
         // Lily accepts inputs from user
@@ -37,56 +38,48 @@ public class Lily {
             String sentence = sc.nextLine();
             String[] parsedSentence = sentence.split(" ");
             String action = parsedSentence[0];
-            switch(action) {
+            switch (action) {
                 // throw error if input doesn't match enums later on
                 case "bye":
                     prettyPrint("finally. what took you so long? (´-ω-`)");
                     break userInteracting;
 
                 case "list":
+                    // throw error instead
                     if (list.isEmpty()) {
                         prettyPrint("there's nothing in the list bro");
                     } else {
-                        String listMsg = "";
-                        // list the items in the list to the string
-                        int i = 1;
-                        for (Task t : list) {
-                            listMsg += indent + i + "."
-                            + t.toString()
-                            + (i == list.size() ? "" : "\n");
-                            i++;
-                        }
-                        prettyPrint("you told me you had to\n" + listMsg);
+                        prettyPrint("you told me you had to\n" + printList());
                     }
                     break;
 
                 case "mark":
                     int addIdx = Integer.parseInt(parsedSentence[1]) - 1;
                     /*
-                    if (list.isEmpty())
-                        throw new Error("you cant mark something that isn't there");
-                    else if (already marked)
-                        throw new error you've already finished this
-                        */
+                     * if (list.isEmpty())
+                     * throw new Error("you cant mark something that isn't there");
+                     * else if (already marked)
+                     * throw new error you've already finished this
+                     */
                     list.get(addIdx).mark();
                     String markMsg = "oh. you've finished it. okay\n"
-                    + indent + (addIdx + 1) + "." 
-                    + list.get(addIdx).toString();
+                            + indent + (addIdx + 1) + "."
+                            + list.get(addIdx).toString();
                     prettyPrint(markMsg);
                     break;
 
                 case "unmark":
                     int delIdx = Integer.parseInt(parsedSentence[1]) - 1;
                     /*
-                    if (list.isEmpty())
-                        throw new Error("you can't unmark something thaj isn't there");
-                    else if (not marked yet)
-                        throw new error you havent done this
-                        */
+                     * if (list.isEmpty())
+                     * throw new Error("you can't unmark something thaj isn't there");
+                     * else if (not marked yet)
+                     * throw new error you havent done this
+                     */
                     list.get(delIdx).unmark();
                     String unmarkMsg = "hey, you gotta get it done later, okay?\n"
-                    + indent + (delIdx + 1) + "." 
-                    + list.get(delIdx).toString();
+                            + indent + (delIdx + 1) + "."
+                            + list.get(delIdx).toString();
                     prettyPrint(unmarkMsg);
                     break;
 
@@ -118,10 +111,22 @@ public class Lily {
                     }
                     break;
 
+                case "delete":
+                case "remove":
+                    try {
+                        prettyPrint("hmph. then why did you make me track your\n"
+                        + indent + list.remove(Integer.parseInt(parsedSentence[1]) - 1)
+                        + "\n"
+                        + indent + "anyway, now you're left with\n" + printList());
+                    } catch (IndexOutOfBoundsException e) { // new error: catch no int input also
+                        errorPretty("eh bro your list is shorter than that");
+                    }
+                    break;
+
                 default:
                     errorPretty("sorry i don't understand what you meant by\n\n"
-                    + indent + sentence + "\n\n"
-                    + indent + "you can try these instead:\n" + listCommands());
+                            + indent + sentence + "\n\n"
+                            + indent + "you can try these instead:\n" + listCommands());
             }
         }
         sc.close();
@@ -129,32 +134,44 @@ public class Lily {
 
     protected static void prettyPrint(String s) {
         System.out.println("\n"
-        + indent + "▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n"
-        + indent + s + "\n"
-        + indent + "▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n");
+                + indent + "▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n"
+                + indent + s + "\n"
+                + indent + "▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n");
+    }
+
+    protected static String printList() {
+        String listMsg = "";
+        int i = 1;
+        for (Task t : list) {
+            listMsg += indent + i + "."
+                    + t.toString()
+                    + (i == list.size() ? "" : "\n");
+            i++;
+        }
+        return listMsg;
     }
 
     protected static void errorPretty(String s) {
         System.err.println("\n"
-        + indent + "▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n"
-        + indent + s + "\n"
-        + indent + "▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n");
+                + indent + "▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n"
+                + indent + s + "\n"
+                + indent + "▼＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝▼\n");
     }
 
     protected static void taskAddedMsg(Task t) {
         String no = list.size() == 1 ? " task " : " tasks ";
         prettyPrint("i've dumped this into your list:\n"
-            + indent + t.toString() + "\n"
-            + indent + "so now you have " + list.size() + no + "happening. hope you're happy");
+                + indent + t.toString() + "\n"
+                + indent + "so now you have " + list.size() + no + "happening. hope you're happy");
     }
 
     protected static String listCommands() {
         return indent + "> todo: record a task which has no date\n"
-             + indent + "> event: note an event with its date after /at\n"
-             + indent + "> deadline: note something with its date after /by\n"
-             + indent + "> list: show what you have to do\n"
-             + indent + "> mark: indicate which numbered task you completed\n"
-             + indent + "> unmark: indicate which task you havent completed\n"
-             + indent + "> bye: stop talking with Lily";
+                + indent + "> event: note an event with its date after /at\n"
+                + indent + "> deadline: note something with its date after /by\n"
+                + indent + "> list: show what you have to do\n"
+                + indent + "> mark: indicate which numbered task you completed\n"
+                + indent + "> unmark: indicate which task you havent completed\n"
+                + indent + "> bye: stop talking with Lily";
     }
 }
