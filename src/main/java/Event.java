e8b7d8efb5750e45ee8a2bb089ad13aa8ec78578
eman-kom <emmanuelmok92@gmail.com>
2022-01-23 20:05:00 +0800

    Implement Dates and Times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
     public String duration;
+    public LocalDate date;
+    public String time;
 
     public Event(String description, String duration) {
         super(description);
         this.duration = duration.trim();
+        String[] datetime = duration.trim().split(" ");
+        this.date = LocalDate.parse(datetime[0].trim());
+        this.time = datetime[1].trim();
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (by: " + this.duration +  ")";
+        return "[E]" + super.toString() + " (at: " + this.date.format(DateTimeFormatter.ofPattern("MMM dd yyyy")) + " " + this.time + ")";
     }
 }
