e71d35064b9b50eaac62ea8726c058a036f84030
waffledood <itshaikalhere@hotmail.sg>
2022-01-23 18:20:50 +0800

    Refactoring Duke logic to use switch

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,23 +3,30 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        String splitter = "______________________________________";
-        String indentationBase = "\t";
-        String indentationText = "\t  ";
-        String indentationTaskStatus = "\t    ";
-        ArrayList<Task> taskList = new ArrayList<>();
-        boolean firstUserChat = true;
 
-        String logo =   indentationText +
-                        "    _                  _     \n" + indentationText +
-                        "   |_|                |_|    \n" + indentationText +
-                        "    _  __ _ _ ____   ___ ___ \n" + indentationText +
-                        "   | |/ _` | '__\\ \\ / / / __|\n" + indentationText +
-                        "   | | (_| | |   \\ V /| \\__ \\\n" + indentationText +
-                        "   | |\\__,_|_|    \\_/ |_|___/\n" + indentationText +
-                        "  _/ |                       \n" + indentationText +
-                        " |__/                        \n";
+    String splitter = "______________________________________";
+    String indentationBase = "\t";
+    String indentationText = "\t  ";
+    String indentationTaskStatus = "\t    ";
+    ArrayList<Task> taskList = new ArrayList<>();
+    boolean firstUserChat = true;
+
+    String logo =   indentationText +
+                    "    _                  _     \n" + indentationText +
+                    "   |_|                |_|    \n" + indentationText +
+                    "    _  __ _ _ ____   ___ ___ \n" + indentationText +
+                    "   | |/ _` | '__\\ \\ / / / __|\n" + indentationText +
+                    "   | | (_| | |   \\ V /| \\__ \\\n" + indentationText +
+                    "   | |\\__,_|_|    \\_/ |_|___/\n" + indentationText +
+                    "  _/ |                       \n" + indentationText +
+                    " |__/                        \n"; 
+
+    public static void main(String[] args) {
+        new Duke().run();
+    }
+
+    public void run() {
+        
         System.out.println(String.format("%sHello, I'm\n%s%syour personal assistant.", indentationText, logo, indentationText));
 
         Scanner sc = new Scanner(System.in);
@@ -27,7 +34,9 @@ public class Duke {
         String userName = sc.nextLine();
         System.out.println(String.format("%sSplendid! My pleasure to serve you %s ", indentationText, userName));
 
-        while(true) {
+        boolean active = true;
+
+        while(active) {
             try {
                 System.out.println(String.format("%sWhat %smay I assist you with today, %s? \n\t%s", indentationText, (firstUserChat ? "" : "else "), userName, splitter));
                 firstUserChat = (firstUserChat==true) ? false : firstUserChat;
@@ -35,143 +44,88 @@ public class Duke {
                 String userInput = sc.nextLine();
                 userInput = userInput.trim();
 
+                String[] userInputString = userInput.split(" ", 2);
+                String command = userInputString[0];
+                String description = userInputString.length > 1 ? userInputString[1] : "" ;
+
                 // Start of Duke's text block 
                 System.out.println(String.format("%s%s", indentationBase, splitter));
 
-                // Exit - "bye", exits the program 
-                if (userInput.toLowerCase().equals("bye")) {
-                    System.out.println(String.format("%sGoodbye for now. \n", indentationText));
-                    break;
-                }
-
-                // List - "list", lists all of the user's inputs 
-                else if (userInput.toLowerCase().equals("list")) {
-                    if (taskList.size() == 0) System.out.println(String.format("%sYour list is empty %s", indentationText, userName));
-                    else {
-                        for (int i = 0; i < taskList.size(); i++) {
-                            Task task = taskList.get(i);
-                            System.out.println(String.format("%s%d. %s", indentationText, i+1, task)); 
-                        }
-                    }
-                }
-
-                // Mark, Unmark - "mark itemIndexNumber", "unmark itemIndexNumber", marks an item as done/undone accordingly 
-                else if (userInput.toLowerCase().matches("^mark \\d+|^mark") || userInput.toLowerCase().matches("^unmark \\d+|^unmark")) {
+                switch (command) {
                     
-                    // when the user input has the "mark/unmark" command & an integer 
-                    if (userInput.toLowerCase().matches("^mark \\d+") || userInput.toLowerCase().matches("^unmark \\d+")) {
+                    // Exit - "bye", exits the program 
+                    case "bye":
+                        System.out.println(String.format("%sGoodbye for now. \n", indentationText));
+                        active = false;
+                        break;
 
-                        try {
-                            String[] stringArray = userInput.toLowerCase().split(" ");
-                            int taskIndex = Integer.valueOf(stringArray[1]) - 1;
-                            
+                    // List - "list", lists all of the user's inputs 
+                    case "list":
+                        if (taskList.size() == 0) System.out.println(String.format("%sYour list is empty %s", indentationText, userName));
+                        else {
+                            for (int i = 0; i < taskList.size(); i++) {
+                                Task task = taskList.get(i);
+                                System.out.println(String.format("%s%d. %s", indentationText, i+1, task)); 
+                            }
+                        }
+                        break;
+                    
+                    // Mark, "mark itemIndexNumber", marks an item as done 
+                    // ^mark \\d+|^mark
+                    case "mark":
+                        markTask(description);
+                        break;
+                    
+                    // Unmark, "unmark itemIndexNumber", marks an item as undone 
+                    // ^unmark \\d+|^unmark
+                    case "unmark":
+                        unmarkTask(description);
+                        break;
+
+                    // Delete - removes Tasks from the list 
+                    // "^delete \\d+|^delete"
+                    case "delete":
+                        // when the user input has the "delete" command & an integer 
+                        if (userInput.matches("^delete \\d+")) {
+                            String[] textStrings = userInput.split(" "); 
+                            int taskIndex = Integer.valueOf(textStrings[1]) - 1;
+
                             // if user-specified task index is out of the list 
                             if (taskIndex >= taskList.size() || taskIndex < 0) {
                                 throw new DukeException("I'm sorry, you're referencing a task that does not exist!");
                             }
 
-                            // Mark 
-                            if (stringArray[0].equals("mark")) {
-                                
-                                Task task = taskList.get(taskIndex);
-                                task.markAsDone();
-                                System.out.println(String.format("%sOkay, marking this task as done:", indentationText));
-                                System.out.println(String.format("%s%s", indentationTaskStatus, task));
-                            
-                            // Unmark 
-                            } else if (stringArray[0].equals("unmark")) {
-                                
-                                // taskList.get(taskIndex).markAsUndone();
-                                Task task = taskList.get(taskIndex);
-                                task.markAsDone();
-                                System.out.println(String.format("%sOkay, marking this task as not done yet:", indentationText));
-                                System.out.println(String.format("%s%s", indentationTaskStatus, task));
-                            }
-                        } catch (DukeException e) {
-                            System.out.println(String.format("%s%s", indentationText, e.getMessage())); 
+                            Task task = taskList.remove(taskIndex);
+                            System.out.println(String.format("%sOkay, I've deleted this task:", indentationText));
+                            System.out.println(String.format("%s%s", indentationTaskStatus, task));
+                        } else {
+                            throw new DukeException("I'm sorry, you missed out the task index");
                         }
-                    } else {
-                        throw new DukeException("I'm sorry, you missed out the task index");
-                    }
-                }
+                        break;
 
-                // Delete - removes Tasks from the list 
-                else if (userInput.matches("^delete \\d+|^delete")) {
-                    // when the user input has the "delete" command & an integer 
-                    if (userInput.matches("^delete \\d+")) {
-                        String[] textStrings = userInput.split(" "); 
-                        int taskIndex = Integer.valueOf(textStrings[1]) - 1;
+                    // "^todo .*|^todo"
+                    case "todo":
+                        //
+                        addTodo(description);
+                        break;
 
-                        // if user-specified task index is out of the list 
-                        if (taskIndex >= taskList.size() || taskIndex < 0) {
-                            throw new DukeException("I'm sorry, you're referencing a task that does not exist!");
-                        }
+                    // "^event .*|^event", "/at"
+                    case "event":
+                        //
+                        addEvent(description, "event");
+                        break;
 
-                        Task task = taskList.remove(taskIndex);
-                        System.out.println(String.format("%sOkay, I've deleted this task:", indentationText));
-                        System.out.println(String.format("%s%s", indentationTaskStatus, task));
-                    } else {
-                        throw new DukeException("I'm sorry, you missed out the task index");
-                    }
+                    // "^deadline .*|^deadline", "/by"
+                    case "deadline":
+                        //
+                        addDeadline(description, "deadline");
+                        break;
+
+                    default:
+                        throw new DukeException("I'm sorry, you've input a command I don't recognize. Please try again.");
+                        // break;
                 }
                 
-                // Add - adds Task to list 
-                // 3 types of Tasks: ToDo's, Events, Deadlines 
-                else if (userInput.matches("^todo .*|^todo") || userInput.toLowerCase().matches("^event .*|^event") || userInput.toLowerCase().matches("^deadline .*|^deadline")) {
-                    String[] textStrings = userInput.split(" "); 
-                    String taskType = textStrings[0].toLowerCase(); 
-
-                    if (userInput.toLowerCase().matches("^todo .+") || userInput.toLowerCase().matches("^event .+") || userInput.toLowerCase().matches("^deadline .+")) {
-                        
-                        String taskDescription = String.join(" ", Arrays.copyOfRange(textStrings, 1, textStrings.length));
-                        Task task;
-                        
-                        // ToDo's 
-                        if (taskType.equals("todo")) {
-
-                            task = new Todo(taskDescription); 
-                            taskList.add(task);
-                            System.out.println(String.format("%sNoted. I've added this task:", indentationText));
-                            System.out.println(String.format("%s%s", indentationTaskStatus, task));
-                        
-                        // Events & Deadlines 
-                        } else {
-
-                            if (!taskDescription.matches(".+ /by .+|.+ /at .+")) {
-                                throw new DukeException("Your " + taskType + " command is incorrectly formatted. Please try again.");
-                            }
-
-                            String[] taskDescriptionStrings = taskDescription.split("/");
-                            String taskDescriptionText = taskDescriptionStrings[0].strip();
-                            String[] taskDescriptionTimeStrings = taskDescriptionStrings[1].split(" ");
-                            String taskDescriptionTime = String.join(" ", Arrays.copyOfRange(taskDescriptionTimeStrings, 1, taskDescriptionTimeStrings.length)).strip();
-
-                            // Events 
-                            if (taskType.equals("event")) { 
-
-                                task = new Event(taskDescriptionText, taskDescriptionTime);
-                                taskList.add(task);
-                                System.out.println(String.format("%sNoted. I've added this task:", indentationText));
-                                System.out.println(String.format("%s%s", indentationTaskStatus, task));
-                            
-                            // Deadlines 
-                            } else if (taskType.equals("deadline")) {
-
-                                task = new Deadline(taskDescriptionText, taskDescriptionTime);
-                                taskList.add(task);
-                                System.out.println(String.format("%sNoted. I've added this task:", indentationText));
-                                System.out.println(String.format("%s%s", indentationTaskStatus, task));
-                            }
-                        }
-
-                        System.out.println(String.format("%sNow you have %d tasks in the list.", indentationText, taskList.size()));
-                    } else {
-                        throw new DukeException("I'm sorry, your " + taskType + " command is missing a task description. Please try again.");
-                    }
-                } else {
-                    throw new DukeException("I'm sorry, you've input a command I don't recognize. Please try again.");
-                }
-
             }
             
             catch(DukeException e) {
@@ -181,4 +135,104 @@ public class Duke {
 
         sc.close();
     }
+
+    private void addTodo(String taskDescription) {
+        //
+        Task task = new Todo(taskDescription); 
+        taskList.add(task);
+        System.out.println(String.format("%sNoted. I've added this task:", indentationText));
+        System.out.println(String.format("%s%s", indentationTaskStatus, task));
+    }
+
+    private void addEvent(String taskDescription, String taskType) throws DukeException {
+        //
+        if (!taskDescription.matches(".+ /at .+")) {
+            throw new DukeException("Your " + taskType + " command is incorrectly formatted. Please try again.");
+        }
+
+        String[] taskDescriptionStrings = taskDescription.split("/");
+        String taskDescriptionText = taskDescriptionStrings[0].strip();
+        String[] taskDescriptionTimeStrings = taskDescriptionStrings[1].split(" ");
+        String taskDescriptionTime = String.join(" ", Arrays.copyOfRange(taskDescriptionTimeStrings, 1, taskDescriptionTimeStrings.length)).strip();
+
+        // Events 
+        Task task = new Event(taskDescriptionText, taskDescriptionTime);
+        taskList.add(task);
+        System.out.println(String.format("%sNoted. I've added this task:", indentationText));
+        System.out.println(String.format("%s%s", indentationTaskStatus, task));
+
+        // throw new DukeException("I'm sorry, your " + taskType + " command is missing a task description. Please try again.");
+    }
+
+    private void addDeadline(String taskDescription, String taskType) throws DukeException {
+        //
+        //
+        if (!taskDescription.matches(".+ /by .+")) {
+            throw new DukeException("Your " + taskType + " command is incorrectly formatted. Please try again.");
+        }
+
+        String[] taskDescriptionStrings = taskDescription.split("/");
+        String taskDescriptionText = taskDescriptionStrings[0].strip();
+        String[] taskDescriptionTimeStrings = taskDescriptionStrings[1].split(" ");
+        String taskDescriptionTime = String.join(" ", Arrays.copyOfRange(taskDescriptionTimeStrings, 1, taskDescriptionTimeStrings.length)).strip();
+        
+        // Deadline 
+        Task task = new Deadline(taskDescriptionText, taskDescriptionTime);
+        taskList.add(task);
+        System.out.println(String.format("%sNoted. I've added this task:", indentationText));
+        System.out.println(String.format("%s%s", indentationTaskStatus, task));
+
+        // throw new DukeException("I'm sorry, your " + taskType + " command is missing a task description. Please try again.");
+    }
+
+
+    private void markTask(String id) throws DukeException { 
+        // when the user input has the "mark" command & an integer 
+        if (id.toLowerCase().matches("\\d+")) {
+            try {
+                int taskIndex = Integer.valueOf(id) - 1;
+                
+                // if user-specified task index is out of the list 
+                if (taskIndex >= taskList.size() || taskIndex < 0) {
+                    throw new DukeException("I'm sorry, you're referencing a task that does not exist!");
+                }
+
+                Task task = taskList.get(taskIndex);
+                task.markAsDone();
+                System.out.println(String.format("%sOkay, marking this task as done:", indentationText));
+                System.out.println(String.format("%s%s", indentationTaskStatus, task));
+                
+            } catch (DukeException e) {
+                System.out.println(String.format("%s%s", indentationText, e.getMessage())); 
+            }
+        } else {
+            throw new DukeException("I'm sorry, you missed out the task index");
+        }
+    }
+
+    private void unmarkTask(String id) throws DukeException {
+        
+        // when the user input has the "unmark" command & an integer 
+        if (id.toLowerCase().matches("\\d+")) {
+            try {
+                int taskIndex = Integer.valueOf(id) - 1;
+                
+                // if user-specified task index is out of the list 
+                if (taskIndex >= taskList.size() || taskIndex < 0) {
+                    throw new DukeException("I'm sorry, you're referencing a task that does not exist!");
+                }
+
+                Task task = taskList.get(taskIndex);
+                task.markAsDone();
+                System.out.println(String.format("%sOkay, marking this task as not done yet:", indentationText));
+                System.out.println(String.format("%s%s", indentationTaskStatus, task));
+                
+            } catch (DukeException e) {
+                System.out.println(String.format("%s%s", indentationText, e.getMessage())); 
+            }
+        } else {
+            throw new DukeException("I'm sorry, you missed out the task index");
+        }
+    }
+
 }
