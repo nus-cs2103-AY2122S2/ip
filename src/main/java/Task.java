303a3e2c5e709dd9f49e1a33ad189eba4f6b46e7
Benjykoh <benjaminkoh0909@gmail.com>
2022-01-23 22:19:53 +0800

    After Level-2

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,13 @@
 public class Task {
-    private String taskName;
+    protected String taskName;
+    protected boolean isDone;
 
     public Task(String taskName) {
         this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
     }
 }
