29491b6c6419f3a83dd6beca9002b32cf5a1adc6
Jiaaa-yang <lyejy99@gmail.com>
2022-01-23 11:44:45 +0800

    Add date parsing for deadlines
    Allow users to pass in deadline in specified format,
    with error checking. Also displays it in a more readable
    way in task list.

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,20 +1,44 @@
 package task;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Deadline extends Task {
 
+    final static SimpleDateFormat inputDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    final static SimpleDateFormat outputDateFormat = new SimpleDateFormat("dd MMM");
     /**
-     * Deadline for current task in string
+     * Deadline for current task
      */
-    private final String deadline;
+    private final Date deadline;
 
     public Deadline(String name, String deadline) {
         super(name);
-        this.deadline = deadline;
+        this.deadline = parseDeadline(deadline);
+    }
+
+    /**
+     * Parse given date string to Date object, where
+     * string must be in the format of DD/MM/YYYY
+     *
+     * @param dateString date string to parse
+     * @return Date object corresponding to dateString
+     * @throws IllegalArgumentException if the date string is not of DD/MM/YYYY format
+     */
+    private static Date parseDeadline(String dateString) throws IllegalArgumentException {
+        try {
+            Date date = inputDateFormat.parse(dateString);
+            return date;
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("Invalid date format: Dates should be in DD/MM/YYYY");
+        }
     }
 
     @Override
     public String getDescription() {
+        String formattedDeadline = outputDateFormat.format(this.deadline);
         return String.format("[D]%s %s (by: %s)",
-                super.getDoneStatusCheckbox(), super.getName(), this.deadline);
+                super.getDoneStatusCheckbox(), super.getName(), formattedDeadline);
     }
 }
