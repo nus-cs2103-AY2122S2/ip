d32c9c0a3de58f660b815f19157ce50aa367ae8b
cwq2326 <cwq2326@gmail.com>
2022-01-23 15:47:07 +0800

    Update coding standard for switch case statement

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -19,44 +19,44 @@ public class Duke {
                 TaskFunctions.checkInputValidity(input);
 
                 switch (input_split[0]) {
-                    case "commands":
-                        TaskFunctions.getCommands();
-                        break;
-                    case "delete":
-                        int delete_position = Integer.parseInt(input_split[1]);
-                        TaskFunctions.deleteTask(delete_position);
-                        break;
-                    case "todo":
-                        String todo_description = input.split("todo ", 2)[1];
-                        TaskFunctions.addToList(new ToDo(todo_description));
-                        break;
-                    case "deadline":
-                        String deadline_description = input.split("deadline ", 2)[1].split("/by ")[0];
-                        String deadline_date = input.split("deadline ", 2)[1].split("/by ")[1];
-                        TaskFunctions.addToList(new Deadline(deadline_description, deadline_date));
-                        break;
-                    case "event":
-                        String event_description = input.split("event ", 2)[1].split("/at ")[0];
-                        String event_date = input.split("event ", 2)[1].split("/at ")[1];
-                        TaskFunctions.addToList(new Deadline(event_description, event_date));
-                        break;
-                    case "mark":
-                        TaskFunctions.markTask(Integer.parseInt(input_split[1]));
-                        break;
-                    case "unmark":
-                        TaskFunctions.unmarkTask(Integer.parseInt(input_split[1]));
-                        break;
-                    case "list":
-                        TaskFunctions.showTaskList();
-                        break;
-                    case "bye":
-                        System.out.println("Goodbye sir" +
-                                "\nQuote of the day: Success starts with SU!");
-                        System.exit(0);
-                    default:
-                        DukeException dukeException = new DukeException("I'm so very sorry," +
-                                " please make sure you enter a valid Ekud command");
-                        System.err.println(dukeException);
+                case "commands":
+                    TaskFunctions.getCommands();
+                    break;
+                case "delete":
+                    int delete_position = Integer.parseInt(input_split[1]);
+                    TaskFunctions.deleteTask(delete_position);
+                    break;
+                case "todo":
+                    String todo_description = input.split("todo ", 2)[1];
+                    TaskFunctions.addToList(new ToDo(todo_description));
+                    break;
+                case "deadline":
+                    String deadline_description = input.split("deadline ", 2)[1].split("/by ")[0];
+                    String deadline_date = input.split("deadline ", 2)[1].split("/by ")[1];
+                    TaskFunctions.addToList(new Deadline(deadline_description, deadline_date));
+                    break;
+                case "event":
+                    String event_description = input.split("event ", 2)[1].split("/at ")[0];
+                    String event_date = input.split("event ", 2)[1].split("/at ")[1];
+                    TaskFunctions.addToList(new Deadline(event_description, event_date));
+                    break;
+                case "mark":
+                    TaskFunctions.markTask(Integer.parseInt(input_split[1]));
+                    break;
+                case "unmark":
+                    TaskFunctions.unmarkTask(Integer.parseInt(input_split[1]));
+                    break;
+                case "list":
+                    TaskFunctions.showTaskList();
+                    break;
+                case "bye":
+                    System.out.println("Goodbye sir" +
+                            "\nQuote of the day: Success starts with SU!");
+                    System.exit(0);
+                default:
+                    DukeException dukeException = new DukeException("I'm so very sorry," +
+                            " please make sure you enter a valid Ekud command");
+                    System.err.println(dukeException);
                 }
             } catch (DukeException err) {
                 System.err.println(err);
