356f7d26b810c175c65988a7a3a9135e358acfa2
TeddYE <weiye98@gmail.com>
2022-01-23 17:30:22 +0800

    Duke class: implement level 1 functionalities
    Let's implement a skeletal version of Duke that starts by greeting
    the user, simply echoes commands entered by the user, and exits when
    the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static final String WELCOME_MESSAGE = "Hello! I'm Duke\nWhat can I do for you";
+    public static final String GOODBYE_MESSAGE = "Bye. Hope to see you again soon!";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        boolean isExit = false;
+
+        System.out.println(WELCOME_MESSAGE);
+        while (true) {
+            String userInput = sc.nextLine();
+            switch (userInput) {
+            case "bye":
+                isExit = true;
+                System.out.println(GOODBYE_MESSAGE);
+                break;
+            default:
+                System.out.println(userInput);
+                break;
+            }
+            if (isExit) {
+                break;
+            }
+        }
     }
 }
