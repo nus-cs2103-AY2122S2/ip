231d286643044ee984175ee44f9c28538660eb6d
ChanWeiJie <chanweijie@ymail.com>
2022-01-23 18:08:50 +0800

    Complete A-Junit
    Add TaskTest class
    
    Add ParseTest class

---
+++ src/test/java/duke/task/TaskTest.java
@@ -0,0 +1,29 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+
+    @Test
+    public void testStringConversion() {
+        assertEquals("[ ] read book", new Task("read book").toString());
+    }
+
+    @Test
+    public void testGetters() {
+        Task temp = new Task("read book");
+        assertEquals("read book", temp.getDescription());
+        assertEquals(" ", temp.getStatusIcon());
+    }
+
+    @Test
+    public void testSetters() {
+        Task temp = new Task("run");
+        temp.setTaskDone();
+        assertEquals("X", temp.getStatusIcon());
+        temp.setTaskNotDone();
+        assertEquals(" ", temp.getStatusIcon());
+    }
+}
