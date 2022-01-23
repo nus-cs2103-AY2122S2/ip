0dcb5056786cf2b2ecacfecc2ff7b1d96100cd0c
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2022-01-23 14:02:13 +0800

    Shift all classes to DukeClasses package

--- src/main/java/DukeException.java
+++
@@ -1,9 +0,0 @@
-import java.lang.Exception;
-
-public class DukeException extends Exception {
-
-    public DukeException(){
-        super("    Invalid input detected. Please check your input");
-    }
-
-}
