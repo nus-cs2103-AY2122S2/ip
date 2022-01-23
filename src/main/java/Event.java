5a10f2ee1e6694741590de8d6189eafb7511b470
Lim Bing Sen <bingsenlim0806@gmail.com>
2022-01-24 00:07:12 +0800

    Duke file: Save tasks to harddrive
    Tasks only exists throughout the lifetime of programme.
    
    Non-persistent tasks does not allow users to retrieve data stored during
    previous execution of the programme.
    
    Let's,
    * initialize the task list from a file on program load
    * save the task list to a file on exit
    * use relative path to track the location of the save file
    
    Using relative path will allow our programme to be platform independent
    and be able to run on different machines regardless of where the program
    is downloaded.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -9,4 +9,9 @@ public class Event extends Task {
     public String toString() {
         return String.format("%s (at: %s)", super.toString(), this.time);
     }
+
+    @Override
+    public String getFileSaveFormat() {
+        return String.format("%s | %s", super.getFileSaveFormat(), time);
+    }
 }
