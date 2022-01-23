31e91d35c61f3ec904b47eac0b2914750d14fd33
nicollegann <nicollegan@gmail.com>
2022-01-23 14:49:08 +0800

    Update code to follow coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.*;
+
+import java.util.ArrayList;
 
 public class Duke {
     public static String listStatus(int totalTasks) {
@@ -26,9 +27,11 @@ public class Duke {
         String bottomLine = "    ――――――――――――――――――――――――――――――――――\n";
         String indent = "    ";
 
+        // Print introduction
         System.out.print(logo + "\n" + intro + bottomLine);
 
-        ArrayList<Task> taskArr = new ArrayList<>();
+        // Create a new task list
+        ArrayList<Task> tasks = new ArrayList<>();
         int totalTasks = 0;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -42,94 +45,93 @@ public class Duke {
                 Command command = Command.valueOf(c);
 
                 switch (command) {
-                    case LIST: // list out all added tasks
-                        if (totalTasks == 0) {
-                            System.out.print(topLine + "There are no tasks in your list.\n" + bottomLine);
-                        } else {
-                            System.out.print(topLine + "Here are the tasks in your list:\n");
-                            int index = 1;
-                            for (int n = 0; n < totalTasks; n++) {
-                                Task t = taskArr.get(n);
-                                System.out.println(indent + index + "." + t.toString());
-                                index++;
+                case LIST:
+                    if (totalTasks == 0) {
+                        System.out.print(topLine + "There are no tasks in your list.\n" + bottomLine);
+                    } else {
+                        System.out.print(topLine + "Here are the tasks in your list:\n");
+                        int index = 1;
+                        for (int n = 0; n < totalTasks; n++) {
+                            Task t = tasks.get(n);
+                            System.out.println(indent + index + "." + t.toString());
+                            index++;
+                        }
+                        System.out.print(bottomLine);
+                    }
+                    break;
+                case MARK:
+                    // Fallthrough
+                case UNMARK:
+                    if (inputArr.length == 1) {
+                        throw new DukeException(topLine
+                                + "Sorry, I don't know which task you would like to mark :(\n"
+                                + bottomLine);
+                    } else {
+                        int taskNum = Integer.parseInt(inputArr[1]);
+                        Task t = tasks.get(taskNum - 1);
+
+                        if (command.equals(Command.MARK)) { // mark task as done
+                            System.out.print(topLine + "Nice! You've completed this task:\n  ");
+                            t.markAsDone();
+                        } else { // unmark task
+                            System.out.print(topLine + "Okay, I've marked this task as undone:\n  ");
+                            t.markAsUndone();
+                        }
+                        System.out.print(indent + t + "\n" + bottomLine);
+                    }
+                    break;
+                case DELETE:
+                    if (inputArr.length == 1) {
+                        throw new DukeException(topLine
+                                + "Sorry, I don't know which task you would like to delete :(\n"
+                                + bottomLine);
+                    } else {
+                        int taskNum = Integer.parseInt(inputArr[1]);
+                        Task t = tasks.remove(taskNum - 1);
+                        totalTasks--;
+                        System.out.print(topLine + "Okay, I've deleted this task:\n  "
+                                + indent + t + "\n" + indent + listStatus(totalTasks) + bottomLine);
+                    }
+                    break;
+                case TODO:
+                    // Fallthrough
+                case DEADLINE:
+                    // Fallthrough
+                case EVENT:
+                    if (inputArr.length == 1) {
+                        throw new DukeException(topLine + "Oops, the task needs a description!\n" + bottomLine);
+                    } else {
+                        if (command.equals(Command.TODO)) {
+                            String str = input.substring(5);
+                            tasks.add(new Todo(str));
+
+                        } else if (command.equals(Command.DEADLINE)) {
+                            String str = input.substring(9);
+                            String[] strArr = str.split(" /by ");
+                            if (strArr.length == 1) {
+                                throw new DukeException(topLine + "Oops, please set a date/time for this task!\n"
+                                        + bottomLine);
+                            } else {
+                                tasks.add(new Deadline(strArr[0], strArr[1]));
                             }
-                            System.out.print(bottomLine);
-                        }
-                        break;
 
-                    case MARK:
-                    case UNMARK:
-                        if (inputArr.length == 1) {
-                            throw new DukeException(topLine
-                                    + "Sorry, I don't know which task you would like to mark :(\n"
-                                    + bottomLine);
                         } else {
-                            int taskNum = Integer.parseInt(inputArr[1]);
-                            Task t = taskArr.get(taskNum - 1);
-
-                            if (command.equals(Command.MARK)) { // mark task as done
-                                System.out.print(topLine + "Nice! You've completed this task:\n  ");
-                                t.markAsDone();
-                            } else { // unmark task
-                                System.out.print(topLine + "Okay, I've marked this task as undone:\n  ");
-                                t.markAsUndone();
+                            String str = input.substring(6);
+                            String[] strArr = str.split(" /at ");
+                            if (strArr.length == 1) {
+                                throw new DukeException(topLine + "Oops, please set a date/time for this task!\n"
+                                        + bottomLine);
+                            } else {
+                                tasks.add(new Event(strArr[0], strArr[1]));
                             }
-                            System.out.print(indent + t + "\n" + bottomLine);
                         }
-                        break;
-
-                    case DELETE: // delete task from list
-                        if (inputArr.length == 1) {
-                            throw new DukeException(topLine
-                                    + "Sorry, I don't know which task you would like to delete :(\n"
-                                    + bottomLine);
-                        } else {
-                            int taskNum = Integer.parseInt(inputArr[1]);
-                            Task t = taskArr.remove(taskNum - 1);
-                            totalTasks--;
-                            System.out.print(topLine + "Okay, I've deleted this task:\n  "
-                                    + indent + t + "\n" + indent + listStatus(totalTasks) + bottomLine);
-                        }
-                        break;
-
-                    case TODO: // add task to list
-                    case DEADLINE:
-                    case EVENT:
-                        if (inputArr.length == 1) {
-                            throw new DukeException(topLine + "Oops, the task needs a description!\n" + bottomLine);
-                        } else {
-                            if (command.equals(Command.TODO)) {
-                                String str = input.substring(5);
-                                taskArr.add(new Todo(str));
-
-                            } else if (command.equals(Command.DEADLINE)) {
-                                String str = input.substring(9);
-                                String[] strArr = str.split(" /by ");
-                                if (strArr.length == 1) {
-                                    throw new DukeException(topLine + "Oops, please set a date/time for this task!\n"
-                                            + bottomLine);
-                                } else {
-                                    taskArr.add(new Deadline(strArr[0], strArr[1]));
-                                }
-
-                            } else if (command.equals(Command.EVENT)) {
-                                String str = input.substring(6);
-                                String[] strArr = str.split(" /at ");
-                                if (strArr.length == 1) {
-                                    throw new DukeException(topLine + "Oops, please set a date/time for this task!\n"
-                                            + bottomLine);
-                                } else {
-                                    taskArr.add(new Event(strArr[0], strArr[1]));
-                                }
-                            }
-                            totalTasks++;
-                            System.out.print(topLine + "Got it! I've added this task:\n  "
-                                    + indent + taskArr.get(totalTasks - 1).toString() + "\n"
-                                    + indent + listStatus(totalTasks) + bottomLine);
-                        }
-                        break;
+                        totalTasks++;
+                        System.out.print(topLine + "Got it! I've added this task:\n  "
+                                + indent + tasks.get(totalTasks - 1).toString() + "\n"
+                                + indent + listStatus(totalTasks) + bottomLine);
+                    }
+                    break;
                 }
-
             } catch (DukeException e) {
                 System.out.print(e.message);
             } catch (IndexOutOfBoundsException e) {
@@ -140,6 +142,8 @@ public class Duke {
                 input = br.readLine();
             }
         }
+
+        // Print closing statement
         System.out.print(topLine + "Bye! Hope to see you again soon!" + "\n" + bottomLine);
     }
 }
