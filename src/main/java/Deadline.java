a8b6ad80c3ed168078c50e896aefcda226185a14
DESKTOP-MDM8KAA\lx <bianyuqi142857@gmail.com>
2022-01-24 23:21:33 +0800

    Add date recognization function if input time is a date

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,28 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
 
     protected String by;
+    protected LocalDate ld;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
 
+    public Deadline(String description, LocalDate ld) {
+        super(description);
+        this.ld = ld;
+    }
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        if(ld == null) {
+            return "[D]" + super.toString() + " (by: " + by + ")";
+        } else {
+            return "[D]" + super.toString() + " (by: " + ld.format(DateTimeFormatter.ofPattern("MMM-dd-yyyy")) + ")";
+        }
+
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
     protected String time;
+    protected LocalDate ld;
 
     public Event(String description, String time) {
         super(description);
         this.time = time;
     }
 
+    public Event(String description, LocalDate ld) {
+        super(description);
+        this.ld = ld;
+    }
+
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (by: " + time + ")";
+        if(ld == null) {
+            return "[E]" + super.toString() + " (by: " + time + ")";
+        } else {
+            return "[E]" + super.toString() + " (by: " + ld.format(DateTimeFormatter.ofPattern("MMM-dd-yyyy")) + ")";
+        }
     }
 }
