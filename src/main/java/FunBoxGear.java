dada23002d11a1a60184abf7b2bed414b993a218
junrong98 <e0544375@u.nus.edu>
2022-01-24 16:49:05 +0800

    Update FunBoxGear.java

--- src/main/java/FunBoxGear.java
+++ src/main/java/FunBoxGear.java
@@ -1,4 +1,8 @@
+import java.sql.Array;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Collection;
 
 /**
  * The FunBoxGear class contains all the functionalities of FunBox.
@@ -8,9 +12,7 @@ public class FunBoxGear {
     // private Task[] tasksList;
     private ArrayList<Task> tasksList;
 
-    /**
-     * Constructor for FunBoxGear
-     */
+
     public FunBoxGear() {
         this.tasksList = new ArrayList<Task>();
     }
@@ -23,7 +25,7 @@ public class FunBoxGear {
     }
 
     /**
-     * Check whether if user's input is a commands
+     * Checks whether if user's input is a commands
      *
      * @param message The user's input to the command prompt
      * @return Return false if message is "bye", otherwise return true
@@ -36,6 +38,9 @@ public class FunBoxGear {
             return false;
         case "list":
             this.showList();
+            return true;
+        case "find":
+            this.getTasksOnThisDate(formattedMsg);
             return true;
         case "mark":
             this.markDone(formattedMsg);
@@ -53,7 +58,7 @@ public class FunBoxGear {
     }
 
     /**
-     * Add user's tasks to the list based on the type of task
+     * Adds user's tasks to the list based on the type of task
      *
      * @param formattedMsg The original message from the users split by " " into an array
      * @param type         The type of task: event, deadline, todo
@@ -67,16 +72,18 @@ public class FunBoxGear {
             case "event":
                 this.isDescriptionError(description, type);
                 resultArr = this.getDescriptionAndDate(description, type);
-                this.tasksList.add(new Event(resultArr[0], resultArr[1]));
+                this.tasksList.add(new Event(resultArr[0], stringToLocalDate(resultArr[1]),
+                        getTime(resultArr[1]), type));
                 break;
             case "deadline":
                 this.isDescriptionError(description, type);
                 resultArr = this.getDescriptionAndDate(description, type);
-                this.tasksList.add(new Deadline(resultArr[0], resultArr[1]));
+                this.tasksList.add(new Deadline(resultArr[0], stringToLocalDate(resultArr[1]),
+                        getTime(resultArr[1]), type));
                 break;
             case "todo":
                 this.isDescriptionError(description, type);
-                this.tasksList.add(new ToDo(description));
+                this.tasksList.add(new ToDo(description, type));
                 break;
             default:
                 throw new FunBoxExceptions("ERROR! I do not know what the commands means :<");
@@ -87,11 +94,13 @@ public class FunBoxGear {
             System.out.println("Now you have " + noOfItems + " tasks in the list!");
         } catch (FunBoxExceptions e) {
             System.out.println(e.getMessage());
+        } catch (DateTimeParseException e) {
+            System.out.println("ERROR! Please ensure date is formatted as followed: yyyy-mm-dd!");
         }
     }
 
     /**
-     * Check whether user input has a description
+     * Checks whether user input has a description
      *
      * @param description The description of the user input which usually comes after the command type
      * @param type        The type of tasks the user used
@@ -104,7 +113,7 @@ public class FunBoxGear {
     }
 
     /**
-     * Loop through a list of items and print out each item
+     * Loops through a list of items and print out each item
      */
     private void showList() {
         System.out.println("Here are the tasks in your list:");
@@ -114,7 +123,7 @@ public class FunBoxGear {
     }
 
     /**
-     * Format the user's message to be able to differentiate between special commands
+     * Formats the user's message to be able to differentiate between special commands
      *
      * @param message The user's message to be formatted
      * @return Return a String array which contains the split message. The first element is used to differentiate
@@ -126,7 +135,7 @@ public class FunBoxGear {
     }
 
     /**
-     * Get the message from the formatted message without the first item of the array which typically contains the
+     * Gets the message from the formatted message without the first item of the array which typically contains the
      * command
      *
      * @param formattedMsg The message sent by the user which has been formatted
@@ -148,7 +157,7 @@ public class FunBoxGear {
     }
 
     /**
-     * Used to split the message, date and time from a message which is needed for certain commands
+     * Splits the message, date and time from a message which is needed for certain commands
      *
      * @param message The message to retrieve the date and time from
      * @return Return a String array of size 2 where the first item on the list contains the message and
@@ -162,7 +171,98 @@ public class FunBoxGear {
     }
 
     /**
-     * Check if commands such as mark, unmark, delete has the correct input
+     * Gets the tasks found on the date requested by user and prints it out.
+     *
+     * @param taskArr The commands provided by the users
+     */
+    private void getTasksOnThisDate(String[] taskArr) {
+        LocalDate date;
+        try {
+            date = LocalDate.parse(taskArr[1]);
+            ArrayList<Task> eventList = new ArrayList<>(this.tasksList);
+            ArrayList<Task> deadlineList = new ArrayList<>(this.tasksList);
+            eventList.removeIf(task -> (task.type.contains("todo") || task.type.contains("deadline")));
+            deadlineList.removeIf(task -> (task.type.contains("todo") || task.type.contains("event")));
+
+            int counter = 0;
+            int eventSize = eventList.size();
+            int deadlineSize = deadlineList.size();
+
+
+            for (int i = 0; i < eventSize; i++) {
+                Event temp = (Event) eventList.get(i);
+                if (temp.date.equals(date)) {
+                    counter++;
+                    System.out.println(counter + "." + temp);
+                }
+            }
+
+            for (int i = 0; i < deadlineSize; i++) {
+                Deadline temp = (Deadline) deadlineList.get(i);
+                if (temp.date.equals(date)) {
+                    counter++;
+                    System.out.println(counter + "." + temp);
+                }
+            }
+
+            if (counter == 0) {
+                System.out.println("No tasks found on this date! You are free!");
+            }
+
+        } catch (DateTimeParseException e) {
+            System.out.println("ERROR! Please ensure date is in the correct format: yyyy-mm-dd");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ERROR! Date not found in command `find`!");
+        }
+    }
+
+    private void filterList() {
+        for (Task task : this.tasksList) {
+
+        }
+    }
+
+    /**
+     * Converts string consisting of a date and time to LocalDate object
+     *
+     * @param dateTime The date and time which the task should be completed by
+     * @return Returns a LocalDate object
+     * @throws FunBoxExceptions If dateTimeArr.length != 2
+     */
+    private LocalDate stringToLocalDate(String dateTime) throws FunBoxExceptions {
+        String[] dateTimeArr = this.formatCommands(dateTime);
+        LocalDate result;
+        if (dateTimeArr.length == 2) {
+            result = LocalDate.parse(dateTimeArr[0]);
+        } else {
+            throw new FunBoxExceptions("ERROR! Please ensure date and time"
+                    + " is in the correct format: yyyy-mm-dd time");
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the time from a string consisting of a date and time
+     *
+     * @param dateTime The date and time which the task should be completed by
+     * @return Returns a time
+     * @throws FunBoxExceptions If dateTimeArr.length != 2
+     */
+    private String getTime(String dateTime) throws FunBoxExceptions {
+        String[] dateTimeArr = this.formatCommands(dateTime);
+        String time;
+        if (dateTimeArr.length == 2) {
+            time = dateTimeArr[1];
+        } else {
+            throw new FunBoxExceptions("ERROR! Please ensure date and time"
+                    + " is in this format: yyyy-mm-dd time");
+        }
+        return time;
+    }
+
+    /**
+     * Checks if commands such as mark, unmark, delete has the correct input
      *
      * @param messageArr The commands to be checked against
      * @throws FunBoxExceptions If messageArr.length != 2
@@ -178,7 +278,7 @@ public class FunBoxGear {
     }
 
     /**
-     * Mark the item on the list as done
+     * Marks the item on the list as done
      *
      * @param messageArr The formatted message of the user, the second item of the array typically contains the
      *                   taskNo to be mark as done
@@ -194,7 +294,7 @@ public class FunBoxGear {
     }
 
     /**
-     * Mark the item on the list as not done
+     * Marks the item on the list as not done
      *
      * @param messageArr The formatted message of the user, the second item of the array typically contains the
      *                   taskNo to be mark as not done
@@ -210,7 +310,7 @@ public class FunBoxGear {
     }
 
     /**
-     * Delete the task requested by the user
+     * Deletes the task requested by the user
      *
      * @param messageArr The formatted message of the user, the second item of the array typically contains the
      *                   taskNo to be deleted
@@ -230,7 +330,7 @@ public class FunBoxGear {
     }
 
     /**
-     * Print out goodbye message to the user
+     * Prints out goodbye message to the user
      */
     public void sayBye() {
         System.out.println("B-b-bbye. Hope to see you again soon [0 n 0]");
