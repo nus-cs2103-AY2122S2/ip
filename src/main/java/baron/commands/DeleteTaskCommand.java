fcc4d0cae07ad2580e0da60009fa460531f03bb9
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-01-24 20:05:33 +0800

    Update DeleteTaskCommand to update the storage file as well

--- src/main/java/baron/commands/DeleteTaskCommand.java
+++ src/main/java/baron/commands/DeleteTaskCommand.java
@@ -4,13 +4,16 @@ import baron.exceptions.BaronException;
 import baron.messages.Messages;
 import baron.tasks.Task;
 import baron.tasks.TaskManager;
+import baron.util.StorageManager;
 
 public class DeleteTaskCommand extends Command {
     private final TaskManager taskManager;
     private final String commandArgs;
+    private final StorageManager storageManager;
 
-    public DeleteTaskCommand(TaskManager taskManager, String commandArgs) {
+    public DeleteTaskCommand(TaskManager taskManager, StorageManager storageManager, String commandArgs) {
         this.taskManager = taskManager;
+        this.storageManager = storageManager;
         this.commandArgs = commandArgs;
     }
 
@@ -19,6 +22,13 @@ public class DeleteTaskCommand extends Command {
         try {
             int index = CommandParser.parseTaskIntArg(this.commandArgs);
             Task deletedTask = this.taskManager.deleteTask(index);
+            try {
+                this.storageManager.save(this.taskManager.getAllTasks());
+            } catch (BaronException e) {
+                this.taskManager.revertChanges();
+                throw e;
+            }
+            this.taskManager.commitChanges();
             return Messages.MESSAGE_DELETE_SUCCESS + deletedTask.toString() + "\n"
                     + Messages.generateNoOfTasksMessage(this.taskManager.getTaskCount());
         } catch (BaronException e) {
