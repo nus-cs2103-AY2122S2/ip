4ff183b8a8a19e45f94c33274c2bc408ad7ab389
Albert Sutiono <albertsutz123@gmail.com>
2022-01-24 22:09:55 +0800

    Refactor Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,119 +1,84 @@
 import java.util.Scanner;
 import java.util.List;
-import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Duke {
     private static final String LINE = "_____________________________________________________________";
+    private static final String GAP = "      ";
     private static final String LOGO =
-            "     ████████▄  ███    █▄     ▄█   ▄█▄    ▄████████ \n" +
-            "     ███   ▀███ ███    ███   ███ ▄███▀   ███    ███ \n" +
-            "     ███    ███ ███    ███   ███▐██▀     ███    █▀  \n" +
-            "     ███    ███ ███    ███  ▄█████▀     ▄███▄▄▄     \n" +
-            "     ███    ███ ███    ███ ▀▀█████▄    ▀▀███▀▀▀     \n" +
-            "     ███    ███ ███    ███   ███▐██▄     ███    █▄  \n" +
-            "     ███   ▄███ ███    ███   ███ ▀███▄   ███    ███ \n" +
-            "     ████████▀  ████████▀    ███   ▀█▀   ██████████";
+            "      ████████▄  ███    █▄     ▄█   ▄█▄    ▄████████ \n" +
+            "      ███   ▀███ ███    ███   ███ ▄███▀   ███    ███ \n" +
+            "      ███    ███ ███    ███   ███▐██▀     ███    █▀  \n" +
+            "      ███    ███ ███    ███  ▄█████▀     ▄███▄▄▄     \n" +
+            "      ███    ███ ███    ███ ▀▀█████▄    ▀▀███▀▀▀     \n" +
+            "      ███    ███ ███    ███   ███▐██▄     ███    █▄  \n" +
+            "      ███   ▄███ ███    ███   ███ ▀███▄   ███    ███ \n" +
+            "      ████████▀  ████████▀    ███   ▀█▀   ██████████";
 
 
     public static void main(String[] args) {
 
         System.out.println(LINE);
-        System.out.println("     Hello from\n" + LOGO);
+        System.out.println(GAP + "Hello from\n" + LOGO);
         System.out.println(LINE);
         System.out.println(
-                "     I am a chat bot and I'm here to help you be productive :)\n" +
-                "     What can I do for you today?"
+                GAP + "I am a chat bot and I'm here to help you be productive :)\n" +
+                GAP + "What can I do for you today?"
         );
+        System.out.println(LINE);
 
         Scanner sc = new Scanner(System.in);
         List<Task> tasks = new ArrayList<Task>();
+        List<String> commands = List.of("list", "bye", "mark", "unmark", "delete", "todo", "event", "deadline");
 
         while (true) {
             try {
                 String[] instruction = sc.nextLine().split(" ");
-                switch(instruction[0]){
-                    case "bye": {
-                        System.out.println("Bye. Hope to see you again soon!\n");
-                        return;
-                    }
-
-                    case "list": {
-                        for (int i = 0; i < tasks.size(); i++) {
-                            System.out.println(i + 1 + ". " + tasks.get(i));
-                        }
-                        break;
-                    }
-
-                    case "mark": {
-                        int taskNum = Integer.parseInt(instruction[1]);
-                        tasks.get(taskNum - 1).markAsDone();
-                        System.out.println(String.format("Nice!, I have marked this task as done: \n %s", tasks.get(taskNum - 1)));
-                        break;
-                    }
-
-                    case "unmark": {
-                        int taskNum = Integer.parseInt(instruction[1]);
-                        tasks.get(taskNum - 1).markAsNotDone();
-                        System.out.println(String.format("Ok, I have marked this task as not done: \n %s", tasks.get(taskNum - 1)));
-                        break;
-                    }
-
-                    case "delete": {
-                        int taskNum = Integer.parseInt(instruction[1]);
-                        System.out.println(String.format("Ok, I will remove this task: \n %s", tasks.get(taskNum - 1)));
-                        tasks.remove(taskNum - 1);
-                        break;
-                    }
-
-                    case "todo": {
-                        if(instruction.length == 1) {
-                            throw new InvalidArgumentException();
-                        }
-                        String instructionName = String.join(" ", Arrays.copyOfRange(instruction, 1, instruction.length));
-                        Task newTodo = new Todo(instructionName);
-                        tasks.add(newTodo);
-                        System.out.println("Got it. I've added this task\n" + newTodo);
-                        System.out.println(String.format("You have %d tasks in the list", tasks.size()));
-                        break;
-                    }
-
-                    case "event": {
-                        if(instruction.length == 1) {
-                            throw new InvalidArgumentException();
-                        }
-                        int index = Arrays.asList(instruction).indexOf("/at");
-                        String instructionName = String.join(" ", Arrays.copyOfRange(instruction, 1, index));
-                        String time = String.join(" ", Arrays.copyOfRange(instruction, index + 1, instruction.length));
-                        Task newEvent = new Event(instructionName, time);
-                        tasks.add(newEvent);
-                        System.out.println("Got it. I've added this task\n" + newEvent);
-                        System.out.println(String.format("You have %d tasks in the list", tasks.size()));
-                        break;
-                    }
-
-                    case "deadline": {
-                        if(instruction.length == 1) {
-                            throw new InvalidArgumentException();
-                        }
-                        int index = Arrays.asList(instruction).indexOf("/by");
-                        String instructionName = String.join(" ", Arrays.copyOfRange(instruction, 1, index));
-                        String time = String.join(" ", Arrays.copyOfRange(instruction, index + 1, instruction.length));
-                        Task newDeadline = new Deadline(instructionName, time);
-                        tasks.add(newDeadline);
-                        System.out.println("Got it. I've added this task\n" + newDeadline);
-                        System.out.println(String.format("You have %d tasks in the list", tasks.size()));
-                        break;
-                    }
-
-                    default: {
-                        throw new InvalidCommandException();
-                    }
+                if (instruction.length == 0 || !commands.contains(instruction[0])) {
+                    throw new InvalidCommandException();
                 }
-                System.out.println("");
+
+                if (instruction[0].equals("bye")) {
+                    System.out.println(GAP + "Bye. Hope to see you again soon!");
+                    System.out.println(LINE);
+                    return;
+
+                } else if (instruction[0].equals("list")) {
+                    for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println(GAP + (i + 1) + ". " + tasks.get(i));
+                    }
+
+                } else if (instruction[0].equals("mark") || instruction[0].equals("unmark")) {
+                    int taskNum = Integer.parseInt(instruction[1]);
+                    String response;
+
+                    if(instruction[0].equals("mark")) response = tasks.get(taskNum - 1).markAsDone();
+                    else response = tasks.get(taskNum - 1).markAsNotDone();
+
+                    System.out.println(GAP + response);
+
+                } else if (instruction[0].equals("delete")) {
+                    int taskNum = Integer.parseInt(instruction[1]);
+                    System.out.println(String.format(GAP + "Ok, I will remove this task: \n %s", tasks.get(taskNum - 1)));
+                    tasks.remove(taskNum - 1);
+
+                } else if (instruction[0].equals("todo") || instruction[0].equals("event") || instruction[0].equals("deadline")) {
+                    Task newTask;
+
+                    if(instruction[0].equals("todo")) newTask = Todo.of(instruction);
+                    else if (instruction[0].equals("event")) newTask = Event.of(instruction);
+                    else newTask = Deadline.of(instruction);
+
+                    tasks.add(newTask);
+                    System.out.println(GAP + "Got it. I've added this task\n" + GAP +  newTask);
+                    System.out.println(String.format(GAP + "You have %d tasks in the list", tasks.size()));
+
+                }
+                System.out.println(LINE);
             }
-            catch (Exception e){
-                System.out.println(e.getMessage());
+            catch (DukeException e){
+                System.out.println(GAP +    e.getMessage());
+                System.out.println(LINE);
             }
         }
     }
