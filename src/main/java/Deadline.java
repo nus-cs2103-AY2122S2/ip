03a2bc4d7c1d1eae1d8a681f03e349b85546eded
LowJiaHao99 <77181204+LowJiaHao99@users.noreply.github.com>
2022-01-24 14:32:28 +0800

    Modify Event,Duke and Deadline classes

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Deadline extends Task{
+    private LocalDate deadline;
+
+    public Deadline(String description, LocalDate deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String identify(){
+        String dateString = deadline.format(DateTimeFormatter.ofPattern("MMM dd yyyy" ));
+        if (super.getIsDone()) {
+            return String.format("[D][X] %s (by: %s)", super.getDescription(), dateString);
+        } else {
+            return String.format("[D][ ] %s (by: %s)", super.getDescription(), dateString);
+        }
+    }
+}
