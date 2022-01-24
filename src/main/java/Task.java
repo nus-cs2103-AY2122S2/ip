2ec51980586e28fda2ef01623592142f3a1983a8
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-01-24 16:20:31 +0800

    Modify to support saving to duke.txt

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -15,8 +15,13 @@ public class Task {
         this.isDone = false;
     }
 
+    public String toStringForSave() {
+        return (isDone ? "| 1 | " : "| 0 | ") + this.description;
+    }
+
     @Override
     public String toString() {
         return (isDone ? "[X] " : "[ ] ") + this.description;
     }
+
 }
