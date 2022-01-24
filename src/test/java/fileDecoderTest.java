817bb1ec2ba5d2e9135bf33ffcfce2f38864bedc
iztanpy <isaactanpy2000@gmail.com>
2022-01-24 15:53:39 +0800

    A-JUnit

---
+++ src/test/java/fileDecoderTest.java
@@ -0,0 +1,15 @@
+import backend.FileDecoder;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class fileDecoderTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2,2);
+    }
+
+    public void decodeTest(){
+        FileDecoder fd = new FileDecoder();
+    }
+}
