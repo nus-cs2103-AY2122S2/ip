7f741ad3fbc8069e85e1319cbdb4aef240f11fbe
sharpstorm <chimchar17@gmail.com>
2022-01-24 16:18:23 +0800

    Refactor UI related functions to a separate class
    See commit 8a43906 for reasoning.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,33 +2,23 @@ import java.util.Scanner;
 import java.util.function.Function;
 
 public class Duke {
-    private static final String SEPARATOR = "\t------------------------------------";
-    private static final String BOT_NAME = "Megumin";
-    private static final String ERROR_INVALID_COMMAND = "I do not understand you!";
-    private static final String ERROR_INVALID_SYNTAX = "There was a problem understanding you:";
-
     private static TaskList taskList;
 
     public static void main(String[] args) {
         init();
-        greet();
+        Ui ui = Ui.getInstance().greet();
 
-        Scanner sc = new Scanner(System.in);
         boolean isRunning = true;
         while (isRunning) {
-            String command = readInput(sc);
-            isRunning = printCommand((linePrinter) -> {
+            String command = ui.readInput();
+            isRunning = ui.printCommand((linePrinter) -> {
                 try {
                     return Parser.parse(command, linePrinter, taskList);
-                } catch (DukeInvalidCommandException ex) {
-                    linePrinter.print(ERROR_INVALID_COMMAND);
-                } catch (DukeIllegalArgumentException ex) {
-                    linePrinter.print(ERROR_INVALID_SYNTAX);
-                    linePrinter.print(ex.getMessage());
+                } catch (DukeException ex) {
+                    ui.printError(linePrinter, ex);
                 }
                 return true;
             });
-
             System.out.println();
         }
     }
@@ -48,29 +38,5 @@ public class Duke {
                     + "Please check that you have write to the directory permissions.\n"
                     + "Will not save any changes!");
         }
-    }
-
-    private static void greet() {
-        printCommand((linePrinter) -> {
-            linePrinter.print("Hi! I'm " + BOT_NAME);
-            linePrinter.print("What do you need?");
-            return true;
-        });
-        System.out.println();
-    }
-
-    private static String readInput(Scanner sc) {
-        System.out.println("Enter a Command or New Task:");
-
-        return sc.nextLine();
-    }
-
-    private static boolean printCommand(Function<IPrintable, Boolean> action) {
-        System.out.println(SEPARATOR);
-        boolean isRunning = action.apply((line) -> {
-            System.out.println("\t" + line);
-        });
-        System.out.println(SEPARATOR);
-        return isRunning;
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class Ui {
+    private static final String SEPARATOR = "\t------------------------------------";
+    private static final String BOT_NAME = "Megumin";
+    private static final String ERROR_INVALID_COMMAND = "I do not understand you!";
+    private static final String ERROR_INVALID_SYNTAX = "There was a problem understanding you:";
+
+    private static Ui instance;
+
+    private final Scanner scanner;
+
+    private Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public Ui greet() {
+        printCommand((linePrinter) -> {
+            linePrinter.print("Hi! I'm " + BOT_NAME);
+            linePrinter.print("What do you need?");
+            return true;
+        });
+        System.out.println();
+
+        return this;
+    }
+
+    public String readInput() {
+        System.out.println("Enter a Command or New Task:");
+
+        return this.scanner.nextLine();
+    }
+
+    public boolean printCommand(Function<IPrintable, Boolean> action) {
+        System.out.println(SEPARATOR);
+        boolean isRunning = action.apply((line) -> {
+            System.out.println("\t" + line);
+        });
+        System.out.println(SEPARATOR);
+        return isRunning;
+    }
+
+    public void printError(IPrintable linePrinter, DukeException ex) {
+        if (ex instanceof DukeInvalidCommandException) {
+            linePrinter.print(ERROR_INVALID_COMMAND);
+        } else if (ex instanceof DukeIllegalArgumentException) {
+            linePrinter.print(ERROR_INVALID_SYNTAX);
+            linePrinter.print(ex.getMessage());
+        }
+    }
+
+    public static Ui getInstance() {
+        if (instance == null) {
+            instance = new Ui();
+        }
+        return instance;
+    }
+}
