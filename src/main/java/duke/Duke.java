1ac9302cbd63c100a45fa4e062ee4f3dcf9fceb7
sharpstorm <chimchar17@gmail.com>
2022-01-24 20:03:17 +0800

    Refactor entry point Duke into object

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -9,7 +9,32 @@ public class Duke {
     private static TaskList taskList;
 
     public static void main(String[] args) {
-        init();
+        new Duke().init().run();
+    }
+
+    private Duke() {
+
+    }
+
+    private Duke init() {
+        try {
+            taskList = Storage.load();
+            taskList.registerListener(store -> {
+                try {
+                    Storage.save(store);
+                } catch (DukeIOException ex) {
+                    System.out.println("Warning: An error occurred while saving Task list");
+                }
+            });
+        } catch (DukeIOException ex) {
+            System.out.println("Cannot write to working directory.\n"
+                    + "Please check that you have write to the directory permissions.\n"
+                    + "Will not save any changes!");
+        }
+        return this;
+    }
+
+    private void run() {
         Ui ui = Ui.getInstance().greet();
 
         boolean isRunning = true;
@@ -24,23 +49,6 @@ public class Duke {
                 return true;
             });
             System.out.println();
-        }
-    }
-
-    private static void init() {
-        try {
-            taskList = Storage.load();
-            taskList.registerListener(store -> {
-                try {
-                    Storage.save(store);
-                } catch (DukeIOException ex) {
-                    System.out.println("Warning: An error occurred while saving Task list");
-                }
-            });
-        } catch (DukeIOException ex) {
-            System.out.println("Cannot write to working directory.\n"
-                    + "Please check that you have write to the directory permissions.\n"
-                    + "Will not save any changes!");
         }
     }
 }
