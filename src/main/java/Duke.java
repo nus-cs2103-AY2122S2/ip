365b3e1281ea660282b8a1ab642cf034009a2a27
Tan Yu Tao <tanyutao913@gmail.com>
2022-01-24 12:57:33 +0800

    Add savefile and loadfile functionality
    Did not save list generated when running program
    
    Add functionality to generate file if not in local folder and saves and loads from the file at start and end of program run

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,8 +10,15 @@ import tasks.*;
 
 public class Duke {
 	private static ArrayList<Task> TASKLIST = new ArrayList<>();
-
 	public static void main(String[] args) {
+
+		try {
+			loadFile();
+		} catch (IOException e) {
+			System.out.println("An error occurred");
+			e.printStackTrace();
+		}
+
 		//intro messages
 		Scanner sc = new Scanner(System.in);
 		String logo = " ____        _        \n"
@@ -33,9 +43,16 @@ public class Duke {
 				e.printError();
 			}
 		}
+
+		try{
+			saveFile();
+		} catch (IOException e) {
+			System.out.println("An error occurred");
+			e.printStackTrace();
+		}
 	}
 
-	public static Command processInput(String input) throws DukeException {
+	private static Command processInput(String input) throws DukeException {
 		Command cmd = null;
 		String[] inputSplit = input.split(" ", 2);
 		String commandType = inputSplit[0].toLowerCase();
@@ -85,7 +102,77 @@ public class Duke {
 		return cmd;
 	}
 
-	public static boolean isInteger(String input) {
+	private static void loadFile() throws IOException {
+		//load files
+		File directory = new File("data");
+		if(!directory.exists()){
+			directory.mkdir();
+		}
+
+		File file = new File("data/duke.txt");
+		if (!file.exists()) {
+			file.createNewFile();
+		} else {
+			Scanner s = new Scanner(file);
+			String type;
+			String mark;
+			String detail;
+			String date;
+			Task toAdd = null;
+			while(s.hasNext()) {
+				String input = s.nextLine();
+				String[] inputSplit = input.split(" \\| ", 3);
+				type = inputSplit[0];
+				mark = inputSplit[1];
+				switch (type){
+				case "T":
+					detail = inputSplit[2];
+					toAdd = new ToDo(detail);
+					break;
+				case "E":
+					inputSplit = inputSplit[2].split(" \\| ");
+					detail = inputSplit[0];
+					date = inputSplit[1];
+					toAdd = new Event(detail, date);
+					break;
+				case "D":
+					inputSplit = inputSplit[2].split(" \\| ");
+					detail = inputSplit[0];
+					date = inputSplit[1];
+					toAdd = new Deadline(detail, date);
+					break;
+				}
+				System.out.println(mark);
+				if(mark.equals("1")){
+					toAdd.mark();
+				}
+				TASKLIST.add(toAdd);
+			}
+		}
+	}
+
+	private static void saveFile() throws IOException {
+		FileWriter fw = new FileWriter("data/duke.txt");
+		for (int i =0; i < TASKLIST.size(); i++){
+			Task t = TASKLIST.get(i);
+			String type = t.getType();
+			String mark = t.getMark();
+			String detail = t.getDetail();
+			switch (type){
+			case"T":
+				fw.write(type + " | " +  mark + " | " + detail + "\n");
+				break;
+			case"E":
+			case"D":
+				String date = t.getDate();
+				fw.write(type + " | "  + mark + " | " + detail + " | " + date + "\n");
+				break;
+			}
+		}
+		fw.close();
+	}
+
+	private static boolean isInteger(String input) {
 		for (int i = 0; i < input.length(); i++) {
 			if (!Character.isDigit(input.charAt(i))) {
 				return false;
