e5eb1ca5296cff49e16c0e205bedded3862593c7
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-01-24 20:13:19 +0800

    Add the capability to update the data file during the command execute()

--- src/main/java/baron/commands/MarkTaskCommand.java
+++ src/main/java/baron/commands/MarkTaskCommand.java
@@ -3,13 +3,16 @@ package baron.commands;
 import baron.exceptions.BaronException;
 import baron.messages.Messages;
 import baron.tasks.TaskManager;
+import baron.util.StorageManager;
 
 public class MarkTaskCommand extends Command {
     private final TaskManager taskManager;
     private final String commandArgs;
+    private final StorageManager storageManager;
 
-    public MarkTaskCommand(TaskManager taskManager, String commandArgs) {
+    public MarkTaskCommand(TaskManager taskManager, StorageManager storageManager, String commandArgs) {
         this.taskManager = taskManager;
+        this.storageManager = storageManager;
         this.commandArgs = commandArgs;
     }
 
@@ -18,7 +21,14 @@ public class MarkTaskCommand extends Command {
         int index;
         try {
             index = CommandParser.parseTaskIntArg(this.commandArgs);
-            this.taskManager.markTask(index);
+            if (this.taskManager.markTask(index)) {
+                try {
+                    this.storageManager.save(this.taskManager.getAllTasks());
+                } catch (BaronException e) {
+                    this.taskManager.unmarkTask(index);
+                    throw e;
+                }
+            }
             return Messages.MESSAGE_MARK_SUCCESS + this.taskManager.getTask(index).toString();
         } catch (BaronException e) {
             return e.toString();
