47aa107e3d2f3054f6c4b9dfd7203579c0f1ab0f
shurvirarora <shurvirarora@gmail.com>
2022-01-24 23:04:50 +0800

    Improve chatbot response when there are saved tasks and no saved tasks

--- src/main/java/FileRead.java
+++ src/main/java/FileRead.java
@@ -7,8 +7,13 @@ public class FileRead {
     public static void printFileContents(String filePath) throws FileNotFoundException {
         File f = new File(filePath); // create a File for the given file path
         Scanner s = new Scanner(f); // create a Scanner using the File as the source
-        while (s.hasNext()) {
-            System.out.println(s.nextLine());
+        if (!s.hasNext()) {
+            System.out.println("You have no saved tasks.");
+        } else {
+            System.out.println("Here are your saved tasks:");
+            while (s.hasNext()) {
+                System.out.println(s.nextLine());
+            }
         }
     }
 }
