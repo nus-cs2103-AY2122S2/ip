4c959f62f1c98a76ad5ea2e204e5c561c8862117
flairekq <e0556291@u.nus.edu>
2022-01-24 22:27:31 +0800

    Add JUnit tests
    Add Junit tests to test the behavior of the following classes' codes:
    - Deadline
    - Event
    - Todo

---
+++ test/task/DeadlineTest.java
@@ -0,0 +1,20 @@
+package task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeadlineTest {
+
+    @Test
+    void testToString() {
+        Deadline d = new Deadline("assignment 1", "2022-02-07");
+        assertEquals("[D][ ] assignment 1 (by: Feb 07 2022)", d.toString());
+    }
+
+    @Test
+    void getSaveFormat() {
+        Deadline d = new Deadline("assignment 1", "2022-02-07");
+        assertEquals("D,0,assignment 1,2022-02-07", d.getSaveFormat());
+    }
+}
---
+++ test/task/EventTest.java
@@ -0,0 +1,20 @@
+package task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+
+    @Test
+    void testToString() {
+        Event e = new Event("project meeting", "Sat 2pm");
+        assertEquals("[E][ ] project meeting (at: Sat 2pm)", e.toString());
+    }
+
+    @Test
+    void getSaveFormat() {
+        Event e = new Event("project meeting", "Sat 2pm");
+        assertEquals("E,0,project meeting,Sat 2pm", e.getSaveFormat());
+    }
+}
