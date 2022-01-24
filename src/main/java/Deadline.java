ba391cd98c5802634c8f0e70cd3425127b21d97d
junrong98 <e0544375@u.nus.edu>
2022-01-24 16:44:18 +0800

    Updated Task.java and its children classes

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,25 +1,32 @@
 //@@author junrong98-reused
 //Reused from https://nus-cs2103-ay2122s2.github.io/website/admin/ip-w2.html
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * The Deadline class which inherits from Task.
  */
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate date;
+    protected String time;
 
     /**
      * The constructors for deadline class
      * @param description The description of task sent by the user
      * @param by The deadline which the task should be completed by
      */
-    public Deadline(String description, String by) {
-        super(description);
-        this.by = by;
+    public Deadline(String description, LocalDate date, String time, String type) {
+        super(description, type);
+        this.date = date;
+        this.time = time;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: "
+                + this.date.format(DateTimeFormatter.ofPattern("MMM dd yyyy"))
+                + " "  + time + ")";
     }
 }
