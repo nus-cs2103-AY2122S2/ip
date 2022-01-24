a79686cf1c0c7751baceefd91ca899495455687a
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-01-24 16:20:57 +0800

    Modify to support saving to duke.txt

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -4,6 +4,11 @@ public class Todo extends Task {
     }
 
     @Override
+    public String toStringForSave() {
+        return "T "+ super.toStringForSave();
+    }
+
+    @Override
     public String toString() {
         return "[T]" + super.toString();
     }
