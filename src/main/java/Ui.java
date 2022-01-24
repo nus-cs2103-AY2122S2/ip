7627811c8b1cc6e84a689d1f099b73d335f171e3
raynerljm <raynerljm@gmail.com>
2022-01-24 15:48:49 +0800

    Create UI class

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Ui {
+public class Ui {
+    Ui() {
+    }
+
+    void printBootUp() {
+        System.out.println("Hello! I am Duck 🐥\nWhat can I do for you?");
+    }
+
+    void printLoadingError() {
+        System.out.println("Since loading the save file has failed, your task list is currently empty");
+    }
+
+    void printLineSeparator() {
+        System.out.println("____________________________________________________________");
+    }
+
+    void printException(DukeException e) {
+        System.out.println(String.format("%s *quack*", e.getMessage()));
+    }
 }
