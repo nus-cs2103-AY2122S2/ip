63b771cc1b97a3d2a48577ce70c516695349bd1d
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2022-01-24 16:19:10 +0800

    Modify Deadline.java to support saving to duke.txt

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,6 +8,11 @@ public class Deadline extends Task {
     }
 
     @Override
+    public String toStringForSave() {
+        return "D "+ super.toStringForSave() + " | " + this.by;
+    }
+
+    @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + this.by + ")";
     }
