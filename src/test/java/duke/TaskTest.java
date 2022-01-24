ef61857f7a7e5b8acd84662391b2910e0d6c84cc
ZF <zifoongl@gmail.com>
2022-01-24 19:48:06 +0800

    Resolved conflicts in TaskTest.java

--- src/test/java/duke/TaskTest.java
+++ src/test/java/duke/TaskTest.java
@@ -5,25 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskTest {
     @Test
-<<<<<<< HEAD
-    public void todoTest_printMessage(){
-        String expectedResult = "[T][ ]buy groceries";
-=======
     public void todoTest(){
         String expectedResult = "[T][ ] buy groceries";
->>>>>>> branch-Level-9
         Task todo = new Todo("buy groceries");
         assertEquals(expectedResult, todo.toString());
     }
 
     @Test
-<<<<<<< HEAD
-    public void eventTestNoTime_printMessage(){
-        String expectedResult = "[E][ ]dinner with family (at: Jan 1 2022)";
-=======
     public void eventTestNoTime(){
         String expectedResult = "[E][ ] dinner with family (at: Jan 1 2022)";
->>>>>>> branch-Level-9
         Task todo = null;
         try {
             todo = new Event("dinner with family", "2022-01-01");
@@ -34,13 +24,9 @@ public class TaskTest {
     }
 
     @Test
-<<<<<<< HEAD
-    public void deadlineTestNoTime_printMessage(){
-        String expectedResult = "[D][ ]Complete Tutorial (by: Jan 2 2022)";
-=======
+
     public void deadlineTestNoTime(){
         String expectedResult = "[D][ ] Complete Tutorial (by: Jan 2 2022)";
->>>>>>> branch-Level-9
         Task todo = null;
         try {
             todo = new Deadline("Complete Tutorial", "2022-01-02");
@@ -51,13 +37,9 @@ public class TaskTest {
     }
 
     @Test
-<<<<<<< HEAD
-    public void eventTestTime_printMessage(){
-        String expectedResult = "[E][ ]dinner with family (at: Jan 1 2022 16:00)";
-=======
+
     public void eventTestTime(){
         String expectedResult = "[E][ ] dinner with family (at: Jan 1 2022 16:00)";
->>>>>>> branch-Level-9
         Task todo = null;
         try {
             todo = new Event("dinner with family", "2022-01-01 16:00");
@@ -68,13 +50,8 @@ public class TaskTest {
     }
 
     @Test
-<<<<<<< HEAD
-    public void deadlineTestTime_printMessage(){
-        String expectedResult = "[D][ ]Complete Tutorial (by: Jan 2 2022 23:00)";
-=======
     public void deadlineTestTime(){
         String expectedResult = "[D][ ] Complete Tutorial (by: Jan 2 2022 23:00)";
->>>>>>> branch-Level-9
         Task todo = null;
         try {
             todo = new Deadline("Complete Tutorial", "2022-01-02 23:00");
