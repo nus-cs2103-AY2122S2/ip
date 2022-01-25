e9ca6c6cb49cc2c901c8582c161a042a193071ac
Yu Qi Tan <83687861+yuqitanyq@users.noreply.github.com>
2022-01-25 11:25:17 +0800

    Refactor Code For Readability
    The code base has become too long in Duke.java.
    
    This makes it hard to read and add code into existing codebase.
    
    Let's refactor and abstract out methods to improve readability.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,80 +2,126 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    private static final String LINE = "_______________________________________________";
+    private static final String INTRO = "\nHello! I'm YQ\n" + "What can I do for you?\n";
+    private static final String LISTING = "Here are the tasks in your list:";
+    private static final String MARKING = "Nice! I've marked this task as done:\n  ";
+    private static final String UNMARKING = "OK, I've marked this task as not done yet:\n  ";
+    private static final String ADDED = "Got it. I've added this task:\n  ";
+    private static final String REMOVED = "Noted. I've removed this task:\n";
+    private static final String ERROR_UNKNOWN = "OOPS, I'm sorry, but I don't know what that means :-(";
+    private static final String ERROR_DESCRIPTION = "OOPS, The description of a command cannot be empty.";
+
+    public static void main(String[] args) throws DukeException {
         ArrayList<Task> database = new ArrayList<>();
-        String line = "_______________________________________________";
+        Duke.introMessage();
+        Scanner sc = new Scanner(System.in);
+        Task input = new Task(sc.nextLine());
+        while (!input.description.equals("bye")) {
+            try {
+                System.out.println(LINE);
+                if (input.description.equals("list")) {
+                    Duke.listAction(database);
+                } else if (input.description.startsWith("mark")) {
+                    String itemNumber = input.description.substring(5);
+                    Duke.markAction(itemNumber, database);
+                } else if (input.description.startsWith("unmark")) {
+                    String itemNumber = input.description.substring(7);
+                    Duke.unmarkAction(itemNumber, database);
+                } else if (input.description.startsWith("todo")) {
+                    String description = input.description.substring(5);
+                    Duke.toDoAction(description, database);
+                } else if (input.description.startsWith("deadline")) {
+                    String[] itemArr = input.description.substring(9).split(" /by ");
+                    Duke.deadlineAction(itemArr, database);
+                } else if (input.description.startsWith("event")) {
+                    String[] itemArr = input.description.substring(6).split(" /at ");
+                    Duke.eventAction(itemArr, database);
+                } else if (input.description.startsWith("delete")) {
+                    String itemNumber = input.description.substring(7);
+                    deleteAction(itemNumber, database);
+                } else {
+                    throw new DukeException(ERROR_UNKNOWN);
+                }
+                System.out.println(LINE);
+                input = new Task(sc.nextLine());
+            } catch (StringIndexOutOfBoundsException e){
+                System.out.println(new DukeException(ERROR_DESCRIPTION).getMessage() + "\n" + LINE);
+            } catch (DukeException errorMessage) {
+                System.out.println(errorMessage.getMessage() + "\n" + LINE);
+            }
+            input = new Task(sc.nextLine());
+        }
+        System.out.println(LINE + "\n" + "Bye. Hope to see you again soon!" + "\n" + LINE);
+    }
+
+    public static void introMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String intro = line + "\nHello! I'm YQ\n" + "What can I do for you?\n" + logo + line;
+        String intro = LINE + INTRO + logo + LINE;
         System.out.println(intro);
+    }
 
-        Scanner sc = new Scanner(System.in);
-        Task input = new Task(sc.nextLine());
-        while (!input.description.equals("bye")) {
-            System.out.println(line);
-            if (input.description.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < database.size(); i++) {
-                    System.out.println(i + 1 + "." + database.get(i));
-                }
-            } else if (input.description.startsWith("mark")) {
-                String itemNumber = input.description.substring(5);
-                int num = Integer.parseInt(itemNumber) - 1;
-                Task taskToMark = database.get(num);
-                if (!taskToMark.getStatusIcon().equals("X")) {
-                    taskToMark.markAsDone();
-                    database.set(num, taskToMark);
-                }
-                System.out.println("Nice! I've marked this task as done:\n  " + taskToMark);
-            } else if (input.description.startsWith("unmark")) {
-                String itemNumber = input.description.substring(7);
-                int num = Integer.parseInt(itemNumber) - 1;
-                Task taskToUnmark = database.get(num);
-                if (taskToUnmark.getStatusIcon().equals("X")) {
-                    taskToUnmark.markAsUndone();
-                    database.set(num, taskToUnmark);
-                }
-                System.out.println("OK, I've marked this task as not done yet:\n  " + taskToUnmark);
-            } else if (input.description.startsWith("todo")) {
-                try{
-                    String description = input.description.substring(5);
-                    Todo todoItem = new Todo(description);
-                    database.add(todoItem);
-                    System.out.println("Got it. I've added this task:\n  " + todoItem);
-                    System.out.println("Now you have " + database.size() + " tasks in the list.");
-                }catch(StringIndexOutOfBoundsException e){
-                    DukeException error = new DukeException(e);
-                    System.out.println(error);
-                }
-            } else if (input.description.startsWith("deadline")) {
-                String[] itemArr = input.description.substring(9).split(" /by ");
-                Deadline deadlineItem = new Deadline(itemArr[0], itemArr[1]);
-                database.add(deadlineItem);
-                System.out.println("Got it. I've added this task:\n  " + deadlineItem);
-                System.out.println("Now you have " + database.size() + " tasks in the list.");
-            } else if (input.description.startsWith("event")) {
-                String[] itemArr = input.description.substring(6).split(" /at ");
-                Event eventItem = new Event(itemArr[0], itemArr[1]);
-                database.add(eventItem);
-                System.out.println("Got it. I've added this task:\n  " + eventItem);
-                System.out.println("Now you have " + database.size() + " tasks in the list.");
-            } else if (input.description.startsWith("delete")) {
-                String itemNumber = input.description.substring(7);
-                int num = Integer.parseInt(itemNumber) - 1;
-                Task itemToDelete = database.get(num);
-                database.remove(num);
-                System.out.println("Noted. I've removed this task:\n" + itemToDelete);
-                System.out.println("Now you have " + database.size() + " tasks in the list.");
-            } else {
-                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-            }
-            System.out.println(line);
-            input = new Task(sc.nextLine());
+    public static void listAction(ArrayList<Task> listOfItems) {
+        System.out.println(LISTING);
+        for (int i = 0; i < listOfItems.size(); i++) {
+            System.out.println(i + 1 + "." + listOfItems.get(i));
         }
-        System.out.println(line + "\n" + "Bye. Hope to see you again soon!" + "\n" + line);
+    }
+
+    public static void markAction(String taskNumber, ArrayList<Task> listOfItems) {
+        int num = Integer.parseInt(taskNumber) - 1;
+        Task taskToMark = listOfItems.get(num);
+        if (!taskToMark.getStatusIcon().equals("X")) {
+            taskToMark.markAsDone();
+            listOfItems.set(num, taskToMark);
+        }
+        System.out.println(MARKING + taskToMark);
+    }
+
+    public static void unmarkAction(String taskNumber, ArrayList<Task> listOfItems) {
+        int num = Integer.parseInt(taskNumber) - 1;
+        Task taskToUnmark = listOfItems.get(num);
+        if (taskToUnmark.getStatusIcon().equals("X")) {
+            taskToUnmark.markAsUndone();
+            listOfItems.set(num, taskToUnmark);
+        }
+        System.out.println(UNMARKING + taskToUnmark);
+    }
+
+    public static void toDoAction(String description, ArrayList<Task> lisOfItems) throws DukeException {
+            Todo todoItem = new Todo(description);
+            lisOfItems.add(todoItem);
+            int numOfItems = lisOfItems.size();
+            System.out.println(ADDED + todoItem);
+            System.out.println("Now you have " + numOfItems + " tasks in the list.");
+    }
+
+    public static void deadlineAction(String[] arr, ArrayList<Task> listOfItems) {
+        Deadline deadlineItem = new Deadline(arr[0], arr[1]);
+        listOfItems.add(deadlineItem);
+        int numOfItems = listOfItems.size();
+        System.out.println(ADDED + deadlineItem);
+        System.out.println("Now you have " + numOfItems + " tasks in the list.");
+    }
+
+    public static void eventAction(String[] arr, ArrayList<Task> listOfItems) {
+        Event eventItem = new Event(arr[0], arr[1]);
+        listOfItems.add(eventItem);
+        int numOfItems = listOfItems.size();
+        System.out.println(ADDED + eventItem);
+        System.out.println("Now you have " + numOfItems + " tasks in the list.");
+    }
+
+    public static void deleteAction(String taskNumber, ArrayList<Task> listOfItems) {
+        int num = Integer.parseInt(taskNumber) - 1;
+        Task itemToDelete = listOfItems.get(num);
+        listOfItems.remove(num);
+        int numOfItems = listOfItems.size();
+        System.out.println(REMOVED + itemToDelete);
+        System.out.println("Now you have " + numOfItems + " tasks in the list.");
     }
 }
--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,15 +1,5 @@
-public class DukeException {
-    protected Exception exception;
-
-    DukeException(Exception exception) {
-        this.exception = exception;
-    }
-
-    public String toString(){
-        if (this.exception instanceof StringIndexOutOfBoundsException) {
-            return "☹ OOPS!!! The description of a todo cannot be empty.";
-        } else {
-            return "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
-        }
+public class DukeException extends Exception {
+    DukeException(String errorMessage) {
+        super(errorMessage);
     }
 }
