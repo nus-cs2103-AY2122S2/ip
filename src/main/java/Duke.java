8e7d18fe35c98dc952be43fabe2bfe76e04e4f0d
RYAN <geetanteng93@gmail.com>
2022-01-25 19:09:41 +0800

    Add save feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,6 +14,18 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         ArrayList<Task> tasks = new ArrayList<Task>();
+
+        try {
+            File f = new File("data/duke.txt");
+            if (f.createNewFile()) {
+                System.out.println("created file duke.txt");
+            } else {
+                System.out.println("file exists");
+                tasks = loadList("data/duke.txt");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         
         Scanner sc = new Scanner(System.in);
         
@@ -66,6 +78,12 @@ public class Duke {
         }
         
         myPrint("Bye. Hope to see you again soon!");
+
+        try {
+            writeToFile("data/duke.txt", convertWriteFormat(tasks));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         
         sc.close();
         
@@ -204,6 +222,57 @@ public class Duke {
         
         tasks.add(tempTask);
         myPrint("Got it. I've added this task:\n      " + tempTask + countList(tasks));
+    }
+
+    public static String convertWriteFormat(ArrayList<Task> tasks) {
+        String returnStr = "";
+
+        for (Task t : tasks) {
+            if (t instanceof ToDo) {
+                returnStr += "T#" + t.isDone + "#" + t.description + "\n";
+            } else if (t instanceof Deadline) {
+                Deadline d = (Deadline) t;
+                returnStr += "D#" + d.isDone + "#" + d.description + "#" + d.deadline + "\n";
+            } else if (t instanceof Event) {
+                Event e = (Event) t;
+                returnStr += "E#" + e.isDone + "#" + e.description + "#" + e.time + "\n";
+            }
+        }
+
+        return returnStr;
+    }
+
+    public static void writeToFile(String filePath, String content) throws Exception {
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(content);
+        fw.close();
+    }
+
+    public static ArrayList<Task> loadList(String filePath) throws Exception {
+        File f = new File(filePath);
+        Scanner s = new Scanner(f);
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        String[] stringArr;
+
+        while(s.hasNext()) {
+            stringArr = s.nextLine().split("#");
+
+            if (stringArr[0].equals("T")) {
+
+                tasks.add(new ToDo(stringArr[2], stringArr[1].equals("true")));
+
+            } else if (stringArr[0].equals("D")) {
+
+                tasks.add(new Deadline(stringArr[2], stringArr[1].equals("true"), stringArr[3]));
+
+            } else if (stringArr[0].equals("E")) {
+
+                tasks.add(new Event(stringArr[2], stringArr[1].equals("true"), stringArr[3]));
+
+            }
+        }
+        
+        return tasks;
     }
 
 }
