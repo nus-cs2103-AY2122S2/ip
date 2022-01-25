a6614d77260944c5243cf75bd89e9ce0d7336e06
hqhqhq1 <e0556362@u.nus.edu>
2022-01-26 00:58:44 +0800

    A-JUnit

---
+++ src/test/java/DeadlineTest.java
@@ -0,0 +1,29 @@
+import Duke.task.Deadline;
+import Duke.task.Task;
+import org.junit.jupiter.api.Test;
+
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+    @Test
+    public void LocalDateFormatTest() {
+        Deadline d = new Deadline("return book", "2019-12-01");
+        assertEquals("Dec 01 2019",
+                d.getDate().format(DateTimeFormatter.ofPattern("MMM dd yyyy")));
+    }
+
+    @Test
+    public void getStatusIconTest1() {
+        Deadline d = new Deadline("return book", "2019-12-01");
+        assertEquals(" ", d.getStatusIcon());
+    }
+
+    @Test
+    public void getStatusIconTest2() {
+        Deadline d = new Deadline("return book", "2019-12-01");
+        d.markAsDone();
+        assertEquals("X", d.getStatusIcon());
+    }
+}
---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,23 @@
+import Duke.task.Task;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTest {
+    @Test
+    public void getDescription_and_getStatusIcon_Test1() {
+        Task t = new Task("testing");
+        assertEquals("testing", t.getDescription());
+        assertEquals(" ", t.getStatusIcon());
+    }
+
+    @Test
+    public void getDescription_and_getStatusIcon_Test2() {
+        Task t = new Task("testing123");
+        t.markAsDone();
+        assertEquals("testing123", t.getDescription());
+        assertEquals("X", t.getStatusIcon());
+    }
+
+
+}
