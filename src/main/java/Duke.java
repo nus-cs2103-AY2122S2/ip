acea60e156aada42c4b98af55f44fb078e116154
waffledood <itshaikalhere@hotmail.sg>
2022-01-25 08:37:01 +0800

    Implement load & saving of data

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Duke {
 
@@ -11,6 +15,7 @@ public class Duke {
     ArrayList<Task> taskList = new ArrayList<>();
     boolean firstUserChat = true;
     String userName = "";
+    String taskFilePath = "data/tasks.txt";
 
     String logo =   indentationText +
                     "    _                  _     \n" + indentationText +
@@ -26,14 +31,78 @@ public class Duke {
         new Duke().run();
     }
 
+    private void loadFileContents(String taskFilePath) {
+        // load tasklist data from save 
+        try {
+            File f = new File(taskFilePath);
+            
+            // checks if the user already has existing save data 
+            if (!f.createNewFile()) {
+                System.out.println(indentationText + "loading previous save data...");
+                Scanner s = new Scanner(f);
+                while(s.hasNext()) {
+                    
+                    String task = s.nextLine();
+                    String[] taskStrings = task.split(" \\| ");
+                    String taskType = taskStrings[0];
+                    boolean taskStatus = taskStrings[1] == "1";
+                    String taskDescription = taskStrings[2];
+
+                    switch (taskType) {
+                        case "T":
+                            taskList.add(new Todo(taskDescription, taskStatus));
+                            break;
+                        case "E":
+                            taskList.add(new Event(taskDescription, taskStrings[3], taskStatus));
+                            break;
+                        case "D":
+                            taskList.add(new Deadline(taskDescription, taskStrings[3], taskStatus));
+                            break;
+                        default:
+                            throw new DukeException("Invalid save data");
+                    }
+                }
+                s.close();
+            // else creates the save data for further use
+            } else {
+                System.out.println(indentationText + "creating save data...");
+            }
+            
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } catch (DukeException e) {
+            System.out.println("DukeException: " + e.getMessage());
+        }
+    }
+
+    private void updateFileContents(String taskFilePath) {
+        // write & update save data with current tasklist 
+        try {
+            FileWriter fw = new FileWriter(taskFilePath);
+            for (Task t: taskList) {
+                fw.write(t.toStringSaveData());
+                fw.write(System.lineSeparator());
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.out.print(e.getMessage());
+        }
+    }
+
     public void run() {
         
+        // Jarvis introduces himself & asks for user's name
         System.out.println(String.format("%sHello, I'm\n%s%syour personal assistant.", indentationText, logo, indentationText));
+        System.out.println(String.format("%sHow should I address you?", indentationText));
 
         Scanner sc = new Scanner(System.in);
-        System.out.println(String.format("%sHow should I address you?", indentationText));
         userName = sc.nextLine();
+
+        // Jarvis addresses user
         System.out.println(String.format("%sSplendid! My pleasure to serve you %s ", indentationText, userName));
+
+        // load TaskList from existing data 
+        loadFileContents(taskFilePath);
 
         boolean active = true;
 
@@ -73,32 +142,38 @@ public class Duke {
                     
                     // Mark, "mark itemIndexNumber", marks an item as done 
                     case MARK:
-                        markTask(description, Command.MARK.getRegex());
+                        markTask(description, commandType.getRegex());
+                        updateFileContents(taskFilePath);
                         break;
                     
                     // Unmark, "unmark itemIndexNumber", marks an item as undone 
                     case UNMARK:
-                        unmarkTask(description, Command.UNMARK.getRegex());
+                        unmarkTask(description, commandType.getRegex());
+                        updateFileContents(taskFilePath);
                         break;
 
                     // Delete - removes Tasks from the list 
                     case DELETE:
                         deleteTask(description);
+                        updateFileContents(taskFilePath);
                         break;
 
                     // Adds a todo task
                     case TODO:
                         addTodo(description);
+                        updateFileContents(taskFilePath);
                         break;
 
                     // Adds an event task 
                     case EVENT:
-                        addEventOrDeadline(description, Command.EVENT.getRegex());
+                        addEventOrDeadline(description, commandType, commandType.getRegex());
+                        updateFileContents(taskFilePath);
                         break;
 
                     // Adds a deadline task 
                     case DEADLINE:
-                        addEventOrDeadline(description, Command.DEADLINE.getRegex());
+                        addEventOrDeadline(description, commandType, commandType.getRegex());
+                        updateFileContents(taskFilePath);
                         break;
                 }
             }
@@ -116,16 +191,16 @@ public class Duke {
         if (taskDescription.trim().length() == 0) {
             throw new DukeException("I'm sorry, your event command is missing a task description. Please try again."); 
         }
-        Task task = new Todo(taskDescription); 
+        Task task = new Todo(taskDescription, false); 
         taskList.add(task);
         System.out.println(String.format("%sNoted. I've added this task:", indentationText));
         System.out.println(String.format("%s%s", indentationTaskStatus, task));
     }
 
-    private void addEventOrDeadline(String taskDescription, String regex) throws DukeException {
+    private void addEventOrDeadline(String taskDescription, Command commandType, String regex) throws DukeException {
         // check if the user's input is correctly formatted
         if (!taskDescription.contains(regex)) {
-            throw new DukeException("Your event command is incorrectly formatted. Please try again.");
+            throw new DukeException("Your " + commandType + " command is incorrectly formatted. Please try again.");
         }
 
         String[] taskDescriptionStrings = taskDescription.split(regex);
@@ -134,12 +209,19 @@ public class Duke {
 
         // check if the user's input is just whitespace 
         if (taskDescriptionText.strip().length() == 0) {
-            throw new DukeException("I'm sorry, your event command is missing a task description. Please try again.");
+            throw new DukeException("I'm sorry, your " + commandType + " command is missing a task description. Please try again.");
         } else if (taskDescriptionTime.strip().length() == 0) {
-            throw new DukeException("I'm sorry, your event command is missing a time. Please try again.");
+            throw new DukeException("I'm sorry, your " + commandType + " command is missing a time. Please try again.");
         }
 
-        Task task = new Event(taskDescriptionText, taskDescriptionTime);
+        Task task;
+
+        if (commandType.equals(Command.EVENT)) {
+            task = new Event(taskDescriptionText, taskDescriptionTime, false);
+        } else {
+            task = new Deadline(taskDescriptionText, taskDescriptionTime, false);
+        }
+        
         taskList.add(task);
         System.out.println(String.format("%sNoted. I've added this task:", indentationText));
         System.out.println(String.format("%s%s", indentationTaskStatus, task));
@@ -182,7 +264,7 @@ public class Duke {
                 }
 
                 Task task = taskList.get(taskIndex);
-                task.markAsDone();
+                task.markAsUndone();
                 System.out.println(String.format("%sOkay, marking this task as not done yet:", indentationText));
                 System.out.println(String.format("%s%s", indentationTaskStatus, task));
                 
@@ -215,7 +297,7 @@ public class Duke {
         int taskIndex = Integer.valueOf(id.trim()) - 1;
 
         // if user-specified task index is out of the list 
-        if (taskIndex >= taskList.size() || taskIndex < 0) {
+        if (taskIndex >= taskList.size()) {
             throw new DukeException("I'm sorry, you're referencing a task that does not exist!");
         }
 
