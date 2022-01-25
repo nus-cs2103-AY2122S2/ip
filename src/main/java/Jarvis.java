d6b099d8f5061158b4bf0b7acd256506f1311bbc
NatalieTanML <nat130613@gmail.com>
2022-01-26 01:15:11 +0800

    Abstract data storage handling into Storage class

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,4 +1,5 @@
 import exception.*;
+import storage.Storage;
 import task.*;
 import enums.*;
 import ui.Ui;
@@ -14,15 +15,16 @@ import java.util.Scanner;
 public class Jarvis {
     private static Ui ui;
     private static TaskList tasks;
-
-    private static final String dataFilePath = "data/data.txt";
+    private static Storage storage;
 
     private static boolean processNext = true;
 
     public static void main(String[] args) throws JarvisException {
+        storage = new Storage("data/data.txt");
         ui = new Ui();
+        tasks = new TaskList(storage.loadData(), ui);
+
         ui.welcome();
-        loadData();
 
         while (processNext) {
             try {
@@ -31,7 +33,7 @@ public class Jarvis {
                 switch (Command.valueOf(tokens[0].trim().toUpperCase())) {
                 case BYE:
                     processNext = false;
-                    saveChanges();
+                    storage.saveChanges(tasks);
                     ui.shutdown();
                     return;
                 case LIST:
@@ -63,60 +65,6 @@ public class Jarvis {
             } catch (JarvisException de) {
                 ui.echo("I'm afraid I wasn't able to fulfill your request.\n" + de.getMessage());
             }
-        }
-    }
-
-    private static void loadData() throws JarvisException {
-        try {
-            List<Task> lst = new ArrayList<>();
-            File dataFile = new File(dataFilePath);
-            if (dataFile.exists()) {
-                Scanner s = new Scanner(dataFile);
-                while (s.hasNext()) {
-                    String[] line = s.nextLine().split("\\|");
-                    Task task;
-                    switch (line[0]) {
-                    case "T":
-                        task = new Todo(line[2]);
-                        break;
-                    case "D":
-                        task = new Deadline(line[2], LocalDateTime.parse(line[3]));
-                        break;
-                    case "E":
-                        task = new Event(line[2], LocalDateTime.parse(line[3]));
-                        break;
-                    default:
-                        throw new JarvisException("Unexpected task type encountered: " + line[0]);
-                    }
-
-                    if (line[1].equals("1")) {
-                        task.markAsDone();
-                    }
-
-                    lst.add(task);
-                }
-            } else {
-                dataFile.getParentFile().mkdirs();
-                dataFile.createNewFile();
-            }
-            tasks = new TaskList(lst, ui);
-        } catch (IOException e) {
-            throw new JarvisException("An error has occurred whilst retrieving your tasks.");
-        }
-    }
-
-    private static void saveChanges() throws JarvisException {
-        try {
-            StringBuilder sb = new StringBuilder();
-            for (Task t : tasks.getTasks()) {
-                sb.append(t.toFileString()).append("\n");
-            }
-            FileWriter fw = new FileWriter(dataFilePath);
-            fw.write(sb.toString());
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new JarvisException("An error has occurred whilst saving your tasks.");
         }
     }
 }
---
+++ src/main/java/storage/Storage.java
@@ -0,0 +1,73 @@
+package storage;
+
+import exception.JarvisException;
+import task.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Storage {
+    private String dataFilePath;
+
+    public Storage(String dataFilePath) {
+        this.dataFilePath = dataFilePath;
+    }
+
+    public List<Task> loadData() throws JarvisException {
+        try {
+            List<Task> lst = new ArrayList<>();
+            File dataFile = new File(dataFilePath);
+            if (dataFile.exists()) {
+                Scanner s = new Scanner(dataFile);
+                while (s.hasNext()) {
+                    String[] line = s.nextLine().split("\\|");
+                    Task task;
+                    switch (line[0]) {
+                    case "T":
+                        task = new Todo(line[2]);
+                        break;
+                    case "D":
+                        task = new Deadline(line[2], LocalDateTime.parse(line[3]));
+                        break;
+                    case "E":
+                        task = new Event(line[2], LocalDateTime.parse(line[3]));
+                        break;
+                    default:
+                        throw new JarvisException("Unexpected task type encountered: " + line[0]);
+                    }
+
+                    if (line[1].equals("1")) {
+                        task.markAsDone();
+                    }
+                    lst.add(task);
+                }
+            } else {
+                dataFile.getParentFile().mkdirs();
+                dataFile.createNewFile();
+            }
+            return lst;
+        } catch (IOException e) {
+            throw new JarvisException("An error has occurred whilst retrieving your tasks.");
+        }
+    }
+
+    public void saveChanges(TaskList tasks) throws JarvisException {
+        try {
+            StringBuilder sb = new StringBuilder();
+            for (Task t : tasks.getTasks()) {
+                sb.append(t.toFileString()).append("\n");
+            }
+            FileWriter fw = new FileWriter(dataFilePath);
+            fw.write(sb.toString());
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new JarvisException("An error has occurred whilst saving your tasks.");
+        }
+    }
+}
