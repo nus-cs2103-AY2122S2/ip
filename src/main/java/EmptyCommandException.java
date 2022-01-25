fac8bde1ed2b3dbaef99a1cfeb4f6829903894ac
Lin Da <0407da.lin@gmail.com>
2022-01-25 16:35:40 +0800

    remove redundant exception class and refactor Dazz.java

--- src/main/java/EmptyCommandException.java
+++
@@ -1,5 +0,0 @@
-public class EmptyCommandException extends DazzException {
-    public EmptyCommandException() {
-        super("\tOOPS!!! You did not provide me with any command.");
-    }
-}
--- src/main/java/EmptyListException.java
+++
@@ -1,5 +0,0 @@
-public class EmptyListException extends DazzException {
-    public EmptyListException() {
-        super("\tOOPS!!! Your list is empty!");
-    }
-}
