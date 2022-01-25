d28906012eb0ce08460d17a38b5cd301020e8625
glennljw <glennljw7@gmail.com>
2022-01-25 16:35:12 +0800

    Add JavaDocs

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,39 @@
+/**
+ * Represents a deadline to be fulfilled. This deadline is a task that possesses a state/status that is by default
+ * not done.
+ */
 public class Deadline extends Task {
 	private final String TYPE = "[D]";
 	private final String date;
 	private final String NAME;
 
+	/**
+	 * Creates a task with a Deadline, that is initially undone.
+	 *
+	 * @param name Name of the Deadline Task.
+	 * @param date Date of the deadline.
+	 */
 	public Deadline(String name, String date) {
 		super(name);
 		this.NAME = name;
 		this.date = date;
 	}
 
+	/**
+	 * Returns the String representation of a Deadline.
+	 *
+	 * @return String representation of Deadline.
+	 */
 	@Override
 	public String toString() {
 		return this.NAME + "(" + date + ")";
 	}
 
+	/**
+	 * Returns the type of Task. In this case, it is a Deadline.
+	 *
+	 * @return Type of Task.
+	 */
 	@Override
 	public String track() {
 		return this.TYPE;
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,19 +1,39 @@
+/**
+ * Represents an upcoming event associated with its time of occurrence. This event is a task that possesses a
+ * state/status that is by default not done.
+ */
 public class Event extends Task {
 	private final String TYPE = "[E]";
 	private final String TIME;
 	private final String NAME;
 
+	/**
+	 * Creates an Event that is initially undone.
+	 *
+	 * @param name Name of the Event.
+	 * @param time Time of the upcoming Event.
+	 */
 	public Event(String name, String time) {
 		super(name);
 		this.NAME = name;
 		this.TIME = time;
 	}
 
+	/**
+	 * Returns the String representation of a Deadline.
+	 *
+	 * @return String representation of Deadline.
+	 */
 	@Override
 	public String toString() {
 		return this.NAME + "(" + this.TIME + ")";
 	}
 
+	/**
+	 * Returns the type of Task. In this case, it is an Event.
+	 *
+	 * @return Type of Task.
+	 */
 	@Override
 	public String track() {
 		return this.TYPE;
