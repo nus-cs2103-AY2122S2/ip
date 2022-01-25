7c73ff20f3bd279575bd906ae8deab8df41b9d04
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-01-25 10:44:05 +0800

    Add javadoc comments
    Adding JavaDoc comments to improve understanding of code

--- src/main/java/duke/EmptyDescriptionException.java
+++ src/main/java/duke/EmptyDescriptionException.java
@@ -1,5 +1,8 @@
 package duke;
 
+/**
+ * Represents exception when description of task is empty
+ */
 public class EmptyDescriptionException extends DukeException {
     EmptyDescriptionException(String message) {
         super(message);
