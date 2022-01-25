566ae6d91920dcc3f86bba013661c260428de122
domlimm <dominiclimkj3448@gmail.com>
2022-01-25 12:40:03 +0800

    Add JUnit test for methods in Task file

---
+++ src/test/java/duke/task/TaskTest.java
@@ -0,0 +1,50 @@
+package duke.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskTest {
+    @Test
+    public void dummyTask() {
+        Task task = new Task("Test Task", 'T');
+
+        assertEquals("Test Task", task.getDescription());
+        assertEquals('T', task.getType());
+        assertFalse(task.getCompleted());
+    }
+
+    @Test
+    public void testUnmarkedStatusIcon() {
+        Task task = new Task("Test Task", 'T');
+
+        assertEquals(" ", task.getStatusIcon());
+    }
+
+    @Test
+    public void testMarkedStatusIcon() {
+        Task task = new Task("Test Task", 'T');
+
+        task.setCompleted(true);
+
+        assertEquals("X", task.getStatusIcon());
+    }
+
+    @Test
+    public void testToggleCompleted() {
+        Task task = new Task("Test Task", 'T');
+
+        assertFalse(task.getCompleted(), "Test Task is not done.");
+
+        task.setCompleted(true);
+
+        assertTrue(task.getCompleted(), "Test Task is done.");
+    }
+
+    @Test
+    public void testToString() {
+        Task task = new Task("Test Task", 'T');
+
+        assertEquals("[ ] Test Task", task.toString());
+    }
+}
