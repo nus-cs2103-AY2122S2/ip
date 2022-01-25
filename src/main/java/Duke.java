3c3e12ed824323c904fc6861b5785fee2d9fcd56
Goel Akshit <77224261+goel-a@users.noreply.github.com>
2022-01-25 21:17:04 +0530

    Fixed merge errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
 
-<<<<<<< .merge_file_hgHZFh
         TaskManager duke = new TaskManager(Storage.getSavedList());
         System.out.println("Hello! I'm Duke!");
         System.out.println("What can I do for you?");
         duke.handleLogic();
 
-
-=======
-        TaskManager duke = new TaskManager();
-        System.out.println("Hello! I'm Duke!");
-        System.out.println("What can I do for you?");
-        duke.handleLogic();
-        System.out.println("Bye! Hope to see you again soon");
->>>>>>> .merge_file_v0MEpf
     }
 
 }
