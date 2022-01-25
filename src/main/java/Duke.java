e8ea32d6393ffa40facd06069004940191f1ae48
Alfred Koh <alfredkohkj@gmail.com>
2022-01-26 00:55:59 +0800

    Completed Level-7: Save (To fix path independency, Non-existant file)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,8 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Duke {
@@ -12,12 +17,13 @@ public class Duke {
         System.out.println("Hello! I'm Duke\r\nWhat can I do for you?");
         ArrayList<Task> taskList = new ArrayList<>(); // init arraylist outside infinite loop.
         DukeException dukeException = new DukeException();
-
+        loadTask(taskList);
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine().trim(); // Can also convert result to lower-case to handle cases.
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
+                    writeToTaskList(taskList);
                 break;
             } else if (command.equals("list")) {
                 for(int i = 0; i < taskList.size(); i++) {
@@ -55,8 +61,10 @@ public class Duke {
                 }
             } else if(command.startsWith("deadline")) {
                 try {
+                    command = command.replace("deadline", "");
                     String[] taskText = command.split(" /by");
                     Deadline deadlineTask = new Deadline(taskText[0], taskText[1]);
+                    System.out.println(Arrays.toString(taskText));
                     taskList.add(deadlineTask);
                     System.out.println("Got it. I've added this task:");
                     System.out.println(deadlineTask);
@@ -66,7 +74,9 @@ public class Duke {
                 }
             } else if(command.startsWith("event")) {
                 try {
+                    command = command.replace("event", "");
                     String[] taskText = command.split(" /at");
+                    System.out.println(Arrays.toString(taskText));
                     Event eventTask = new Event(taskText[0], taskText[1]);
                     taskList.add(eventTask);
                     System.out.println("Got it. I've added this task:");
@@ -90,6 +100,69 @@ public class Duke {
              } else {
                 dukeException.noSuchTaskException();
             }
+        }
+    }
+    // Reusable code for loading task into arrayList
+    public static void loadTask(ArrayList<Task> taskList) {
+        String path = "src\\main\\java\\data\\duke.txt";
+        path = path.replace("\\", "/");
+        try {
+            File taskFile = new File(path);
+            Scanner reader = new Scanner(taskFile);
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                if(data.contains("(by:")) {
+                    data = data.substring(9);
+                    String[] taskText = data.split("\\(by:");
+                    System.out.println("Deadline: " + Arrays.toString(taskText));
+                    Deadline deadlineTask = new Deadline(taskText[0].strip(), taskText[1].strip());
+                      if(data.contains("[X]")) {
+                          deadlineTask.setDone();
+                      }
+                    taskList.add(deadlineTask);
+                } else if(data.contains("(at: ")) {
+                    data = data.substring(9);
+                    String[] taskText = data.split("\\(at:");
+                    System.out.println("Event: " + Arrays.toString(taskText));
+                   Event eventTask = new Event(taskText[0].strip(),taskText[1].strip());
+                    if(data.contains("[X]")) {
+                        eventTask.setDone();
+                    }
+                    taskList.add(eventTask);
+                } else {
+                    data = data.substring(9);
+                    Todo todoTask = new Todo(data);
+                    if(data.contains("[X]")) {
+                        todoTask.setDone();
+                    }
+                    taskList.add(todoTask);
+                }
+            }
+        } catch(NullPointerException fileInvalid) {
+            System.out.println("File is Invalid!");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Reusable code for writing into duke.txt task list
+    public static void writeToTaskList(ArrayList<Task> taskList) {
+        String path = "src\\main\\java\\data\\duke.txt";
+        path = path.replace("\\", "/");
+
+        try {
+            // Remove current file tasks
+            PrintWriter pw = new PrintWriter(path);
+            pw.close();
+
+            File taskFile = new File(path);
+            FileWriter myWriter = new FileWriter(taskFile,true);
+            for(int i = 0; i < taskList.size(); i++) {
+                myWriter.write((i + 1) + "." + taskList.get(i).toString() + "\r\n");
+            }
+            myWriter.close();
+        } catch(NullPointerException | IOException fileInvalid) {
+            System.out.println("File is Invalid!");
         }
     }
 }
