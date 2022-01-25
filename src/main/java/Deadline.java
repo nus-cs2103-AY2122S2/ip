e22c9fe975e512757a133f22df29c8e0cf3c413e
lamwj98 <evanlam@Evans-MacBook-Pro.local>
2022-01-25 16:36:12 +0800

    Complete Level-8 without merge

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,36 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 class Deadline extends Task {
-    private String dateTime;
-    
-    Deadline(String item, String dateTime) {
+    private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-LLL-yyyy");
+    private LocalDate dlDate;
+    private LocalTime dlTime;
+
+    public Deadline(String item, LocalDate dlDate) {
         super(item);
-        this.dateTime = dateTime;
+        this.dlDate = dlDate;
+    }
+
+    public Deadline(String item, LocalDate dlDate, LocalTime dlTime) {
+        super(item);
+        this.dlDate = dlDate;
+        this.dlTime = dlTime;
+    }
+
+    public boolean dateCompare(LocalDate date) {
+        return this.dlDate.equals(date);
     }
 
     @Override
     public String getItemAndStatus() {
-        String returned = "[D]" + super.getItemAndStatus() + " (by: " + this.dateTime + ")";
+        String returned;
+        if (dlTime == null) {
+            returned = "[D]" + super.getItemAndStatus() + " (by: " + this.dlDate.format(Deadline.dtf)+ ")";
+        } else {
+            returned = "[D]" + super.getItemAndStatus() + " (by: " + this.dlDate.format(Deadline.dtf) + " " + this.dlTime + ")";
+        }
+
         return returned;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,14 +1,26 @@
-class Event extends Task {
-    private String dateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    Event(String item, String dateTime) {
+class Event extends Task {
+    
+    private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-LLL-yyyy");
+    private LocalDate eventDate;
+    private LocalTime eventTime;
+
+    public Event(String item, LocalDate eventDate, LocalTime eventTime) {
         super(item);
-        this.dateTime = dateTime;
+        this.eventDate = eventDate;
+        this.eventTime = eventTime;
+    }
+
+    public boolean dateCompare(LocalDate date) {
+        return this.eventDate.equals(date);
     }
 
     @Override
     public String getItemAndStatus() {
-        String returned = "[E]" + super.getItemAndStatus() + " (at: " + this.dateTime + ")";
+        String returned = "[E]" + super.getItemAndStatus() + " (at: " + this.eventDate.format(Event.dtf) + " " + this.eventTime + ")";
         return returned;
     }
 }
