6a815ae23382ae5d643366f56a7d47984a9158f8
Decaxical <e0544599@u.nus.edu>
2022-01-25 20:30:34 +0800

    implement echoes
    Echoes the commands inputted by the user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void echo() {
+        Scanner getUserInput = new Scanner(System.in);
+        String userInput = "";
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = getUserInput.nextLine();
+        }
+        System.out.println("Till we meet again");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +18,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("How may I assist you?");
+        echo();
     }
 }
