3f61a7f0c169875739cbe522c9c628e636bec025
ovidharshini <ovidharshini@gmail.com>
2022-01-25 16:40:29 +0800

    Represent task as class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,35 +1,30 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.println("Hello, traveller! My name in Paimon.\nHow can I help you today?");
         String input = readInput(reader);
-        String userData[] = new String[100]; //100 is an arbitrary constant
-        int pointer = 0;
-        String commands[] = new String[10]; //10 is an arbitrary constant
-        Arrays.fill(commands, "");
-        //commands[0] = "list";
+        //Task tasks[] = new Task[100]; //100 is an arbitrary constant
+        List<Task> tasks = new LinkedList<Task>();
 
         while (!input.equals("bye")){
             System.out.println(input);
 
             if (input.equals("list")) {
-                printList(userData);
+                printList(tasks);
             }
             else {
-                userData[pointer] = input;
-                pointer++;
+                tasks.add(new Task(input));
             }
 
             input = readInput(reader);
         }
 
-        if (input.equals("bye")) {
-            System.out.println("Bye, hope to see you again soon!");
-            reader.close();
-        }
+        System.out.println("Bye, hope to see you again soon!");
+        reader.close();
     }
 
     public static String readInput(Scanner s) {
@@ -52,11 +47,11 @@ public class Duke {
     /* Prints out a given list.
      * Only compatible with Strings at the moment.
      */
-    public static void printList(String[] s) {
+    public static void printList(List<Task> t) {
         System.out.println("Hmm... Paimon keeps a clear record in her diary.");
-        for (int i = 0; i < s.length; i++) {
-            if (s[i] != null) {
-                System.out.println(String.format("  %d. %s", i + 1, s[i]));
+        for (int i = 0; i < t.size(); i++) {
+            if (t.get(i) != null) {
+                System.out.println(String.format("  %d. %s", i + 1, t.get(i)));
             }
             else {
                 return;
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markComplete() {
+        this.isDone = true;
+    }
+
+    public void markIncomplete() {
+        this.isDone = false;
+    }
+
+    public boolean isEqualTo(String s) {
+        return description.equals(s);
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String toString() {
+        return "["+ this.getStatusIcon() + "] " + description;
+    }
+}
