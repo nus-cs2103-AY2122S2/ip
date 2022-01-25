2b7d150c5ab79231da7bd58878d709e771a9a2b1
Ryan-L98 <ryanlow98@hotmail.com>
2022-01-25 16:31:10 +0800

    Implement Dates and Times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -16,7 +16,8 @@ public class Event extends Task {
 
     @Override
     public String printStatus() {
-        return "[E] " + Task.statusSymbols[super.getStatus()] + " " + this.toString() + " (at: " + this.date
+        return "[E] " + Task.statusSymbols[super.getStatus()] + " " + this.toString() + " (at: "
+                + this.date.format(DateTimeFormatter.ofPattern("dd MMM yyyy"))
                 + " from " + this.startTime.format(DateTimeFormatter.ofPattern("HH:mm")) + " to "
                 + this.endTime.format(DateTimeFormatter.ofPattern("HH:mm")) + ")";
     }
