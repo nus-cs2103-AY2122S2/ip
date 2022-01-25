128eaec50818365af89f57890ce66843012053c7
Daniel Lee <minqiang8194@gmail.com>
2022-01-25 15:38:00 +0800

    Convert times to LocalDate + LocalTime

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,20 +1,42 @@
-public class DeadlineTask extends Task{
-    protected String byTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-    public DeadlineTask(String title, String byTime){
+public class DeadlineTask extends Task{
+    protected LocalDate taskDate;
+    protected LocalTime taskTime;
+
+    public DeadlineTask(String title, String taskDate){
         super(title);
         this.type = TaskType.DEADLINE;
-        this.byTime = byTime;
+        this.taskDate = LocalDate.parse(taskDate);
+        this.taskTime = null;
     }
-    public DeadlineTask(String title, Boolean isDone, String byTime){
+
+    public DeadlineTask(String title, String taskDate, String taskTime){
+        super(title);
+        this.type = TaskType.DEADLINE;
+        this.taskDate = LocalDate.parse(taskDate);
+        this.taskTime = LocalTime.parse(taskTime);
+    }
+
+    public DeadlineTask(String title, Boolean isDone, String taskDate){
         super(title, isDone);
         this.type = TaskType.DEADLINE;
-        this.byTime = byTime;
+        this.taskDate = LocalDate.parse(taskDate);
+        this.taskTime = null;
+    }
+
+    public DeadlineTask(String title, Boolean isDone, String taskDate, String taskTime){
+        super(title, isDone);
+        this.type = TaskType.DEADLINE;
+        this.taskDate = LocalDate.parse(taskDate);
+        this.taskTime = LocalTime.parse(taskTime);
     }
 
     public String getByTime(){
-        return "(by:" + byTime + ")";
+        return "(by: " + taskDate + (taskTime != null ? " " + taskTime.toString() : "") + ")";
     }
+    
     @Override
     public String toString(){
         return this.title + " " + getByTime();
--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -1,19 +1,40 @@
-public class EventTask extends Task{
-    protected String eventTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-    public EventTask(String title, String eventTime){
+public class EventTask extends Task{
+    protected LocalDate eventDate;
+    protected LocalTime eventTime;
+
+    public EventTask(String title, String eventDate){
         super(title);
         this.type = TaskType.EVENT;
-        this.eventTime = eventTime;
+        this.eventDate = LocalDate.parse(eventDate);
+        this.eventTime = null;
     }
-    public EventTask(String title, Boolean isDone, String eventTime){
+
+    public EventTask(String title, String eventDate, String eventTime){
+        super(title);
+        this.type = TaskType.EVENT;
+        this.eventDate = LocalDate.parse(eventDate);
+        this.eventTime = LocalTime.parse(eventTime);
+    }
+
+    public EventTask(String title, Boolean isDone, String eventDate){
         super(title, isDone);
         this.type = TaskType.EVENT;
-        this.eventTime = eventTime;
+        this.eventDate = LocalDate.parse(eventDate);
+        this.eventTime = null;
+    }
+
+    public EventTask(String title, Boolean isDone, String eventDate, String eventTime){
+        super(title, isDone);
+        this.type = TaskType.EVENT;
+        this.eventDate = LocalDate.parse(eventDate);
+        this.eventTime = LocalTime.parse(eventTime);
     }
 
     public String getEventTime(){
-        return "(by:" + eventTime + ")";
+        return "(at: " + eventDate + (eventTime != null ? " " + eventTime.toString() : "") + ")";
     }
 
     @Override
