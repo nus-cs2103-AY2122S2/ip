03e5f23ed0c588410191fd73c24540402e81c002
waffledood <itshaikalhere@hotmail.sg>
2022-01-25 09:25:53 +0800

    Format Deadline  & Event to recognize dates as LocalDate instead of String

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
 
-    protected String at;  
+    protected LocalDate at;  
     
-    public Event(String description, String at) {
+    public Event(String description, String at) throws DukeException {
         super(description);
-        this.at = at; 
+        try {
+            this.at = LocalDate.parse(at); 
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Date was incorrectly formatted! Please format it as yyyy-mm-dd");
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + this.at + ")";
+        return "[E]" + super.toString() + " (at: " + this.at.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
 }
