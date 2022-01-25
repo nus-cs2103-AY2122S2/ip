25c8a4cb085fe51a9886935d968a7cc804aecd53
michaelseyo <michael.lcw99@gmail.com>
2022-01-25 22:49:11 +0800

    create parser class part2

--- src/main/java/Bernie.java
+++ src/main/java/Bernie.java
@@ -1,5 +1,3 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 /**
@@ -37,7 +35,7 @@ public class Bernie {
         this.tasks = new TaskList();
         this.ui = new Ui();
         this.storage = new Storage();
-        this.parser = new Parser();
+        this.parser = new Parser(tasks);
         ui.greet();
     }
 
@@ -96,13 +94,13 @@ public class Bernie {
     void add(String input) throws BernieException {
         Task newTask;
         if (Parser.isType(Type.TODO, input)) {
-            String[] inputArr = getParams(Type.TODO, input);
+            String[] inputArr = Parser.getParams(Type.TODO, input);
             newTask = tasks.addTask(inputArr, "todo");
         } else if (Parser.isType(Type.DEADLINE, input)) {
-            String[] inputArr = getParams(Type.DEADLINE, input);
+            String[] inputArr = Parser.getParams(Type.DEADLINE, input);
             newTask = tasks.addTask(inputArr, "deadline");
         } else if (Parser.isType(Type.EVENT, input)) {
-            String[] inputArr = getParams(Type.EVENT, input);
+            String[] inputArr = Parser.getParams(Type.EVENT, input);
             newTask = tasks.addTask(inputArr, "event");
         } else {
             throw new BernieException("Not a valid type of task!");
@@ -118,7 +116,7 @@ public class Bernie {
      */
     void delete(String input) {
         try {
-            String taskNum = getParams(Type.DELETE, input)[0];
+            String taskNum = Parser.getParams(Type.DELETE, input)[0];
             Task deletedTask = tasks.deleteTask(taskNum);
             storage.saveTasks(tasks);
             ui.showDeleteMsg(deletedTask, tasks.numTasksLeft());
@@ -128,54 +126,6 @@ public class Bernie {
             ui.showErrorMsg(e.getMessage());
         }
     }
-    /**
-     * Splits a user input into an array containing parameters for creating Task accordingly,
-     * depending on taskType.
-     * @param taskType Type
-     * @param input String, a user input to be split into an array of parameters
-     * @return String[] inputArr.
-     * For "todo": an array of 1, containing description.
-     * For "deadline": an array of 2: [description, by]
-     * For "event": an array of 2: [description, at]
-     * For "mark": an array of 2: [action, taskNum]
-     * For "delete": an array of 1: [taskNum]
-     */
-    String[] getParams(Type taskType, String input) throws BernieException {
-        String description;
-        String[] inputArr = null;
-        switch (taskType) {
-            case TODO:
-                inputArr = input.split("todo ");
-                description = getDescription(inputArr, Type.TODO);
-                inputArr = new String[]{description};
-                break;
-            case DEADLINE:
-                inputArr = input.split(" /by ");
-                description = getDescription(inputArr, Type.DEADLINE);
-                String by = getTime(inputArr, Type.DEADLINE);
-                inputArr = new String[]{description, by};
-                break;
-            case EVENT:
-                inputArr = input.split(" /at ");
-                description = getDescription(inputArr, Type.EVENT);
-                String at = getTime(inputArr, Type.EVENT);
-                inputArr = new String[]{description, at};
-                break;
-            case MARK:
-                inputArr = input.split(" ");
-                // check valid
-                checkMarkOrDeleteInput(inputArr, Type.MARK);
-                break;
-            case DELETE:
-                inputArr = input.split(" ");
-                checkMarkOrDeleteInput(inputArr, Type.DELETE);
-                inputArr = new String[]{inputArr[1]};
-                break;
-            default:
-                break;
-        }
-        return inputArr;
-    }
 
     /**
      * Mark or unmark a task number depending on the input. Handles error if user
@@ -184,7 +134,7 @@ public class Bernie {
      */
     void mark(String input) {
         try {
-            String[] inputArr = getParams(Type.MARK, input);
+            String[] inputArr = Parser.getParams(Type.MARK, input);
             String action = inputArr[0];
             String taskNum = inputArr[1];
             if (action.equals("mark")) {
@@ -198,83 +148,6 @@ public class Bernie {
             ui.showErrorMsg(e.getMessage());
         } catch (Exception e) {
             ui.showErrorMsg(e.getMessage());
-        }
-    }
-
-    /**
-     * Checks for valid "mark" or "delete" action inputs. Throws error if it is not valid.
-     * @param inputArr String[], the user input String split into an array of String for processing
-     *                 parameters for the action
-     * @throws BernieException for invalid inputs
-     */
-    void checkMarkOrDeleteInput(String[] inputArr, Type action) throws BernieException {
-        if (inputArr.length == 2) {
-            try {
-                String taskNum = inputArr[1];
-                Integer.parseInt(taskNum);
-                tasks.taskExists(taskNum);
-            } catch (NumberFormatException nfe) {
-                throw new BernieException("That's not a task number! Put a number.");
-            }
-        } else {
-            if (action.equals(Type.MARK)) {
-                throw new BernieException("Wrong input. Type this: mark/unmark taskNumber");
-            } else {
-                throw new BernieException("Wrong input. Type this: delete taskNumber");
-            }
-        }
-    }
-
-    /**
-     * Gets the description from the inputArr which contains parameters for creating Task
-     * @param inputArr String[], parameters for creating Task, obtained from user input String.
-     * @param taskType Type
-     * @return String description, for creating of Task
-     * @throws BernieException for invalid descriptions such as empty or number description
-     */
-    String getDescription(String[] inputArr, Type taskType) throws BernieException {
-        String description;
-        try {
-            if (taskType.equals(Type.TODO)) {
-                description = inputArr[1];
-            } else {
-                // can take note of lowercase/uppercase
-                description = inputArr[0].split(taskType.name().toLowerCase() + " ")[1];
-            }
-            checkDescriptionNotNumber(description);
-            return description;
-        } catch (IndexOutOfBoundsException e) {
-            throw new BernieException("Where's the description?");
-        }
-    }
-
-    boolean checkDescriptionNotNumber(String description) throws BernieException {
-        try {
-            Integer.parseInt(description);
-            throw new BernieException("Description can't be numbers? We need String!");
-        } catch (NumberFormatException nfe) {
-            return true;
-        }
-    }
-
-    /**
-     * Gets the time from the inputArr which contains parametrs for creating Task
-     * @param inputArr String[], parameters for creating Task, obtained from user input String.
-     * @param taskType type, if it is DEADLINE we check if the input date format is correct.
-     * @return String time, for creating of Task. It is of format: yyyy-mm-dd for converting
-     * to LocalDate object by TaskList's addTask class
-     * @throws BernieException if there is no time input given
-     */
-    String getTime(String[] inputArr, Type taskType) throws BernieException {
-        try {
-            if (taskType.equals(Type.DEADLINE)) {
-                LocalDate checkValid = LocalDate.parse(inputArr[1]);
-            }
-            return inputArr[1];
-        } catch (IndexOutOfBoundsException e) {
-            throw new BernieException("Where's your time input?");
-        } catch (DateTimeParseException e) {
-            throw new BernieException("Please enter deadline date in: yyyy-mm-dd");
         }
     }
 
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,10 +1,30 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
 /**
- * Parser class serves as the preliminary Parser to determine what
- * type of input has the user keyed in. The possible inputs are:
- * list, bye, mark, empty (the ""), delete, and a general add input
- * which is determined in the program by inputs: todo, deadline, event
+ * Parser Class helps to parse the inputs given by the user.
+ * It has parseType method: a preliminary Parser to determine what type of input
+ * has the user keyed in.
+ * It also has getParams method which help to extract out relevant parts of the input
+ * and return parameters needed accordingly for Bernie to perform actions
  */
 public class Parser {
+    static TaskList tasks;
+
+    /**
+     * Constructs a Parser class
+     * @param tasks
+     */
+    Parser(TaskList tasks) {
+        this.tasks = tasks;
+    }
+    /**
+     * Serves as the preliminary Parser to determine what
+     * type of input has the user keyed in. The possible inputs are:
+     * list, bye, mark, empty (the ""), delete, and a general add input
+     * which is determined in the program by inputs: todo, deadline, event
+     * @params input String
+     */
     static Bernie.Type parseType(String input) {
         if (isType(Bernie.Type.LIST, input)) {
             return Bernie.Type.LIST;
@@ -36,10 +56,130 @@ public class Parser {
         return input.indexOf(taskType.name().toLowerCase()) == 0;
     }
 
-    // getParams
-    // checkMarkOrDeleteInput
-    // getDescription
-    // checkDescriptionNotNumber
-    // getTime
+    /**
+     * Splits a user input into an array containing parameters for creating Task accordingly,
+     * depending on taskType.
+     * @param taskType Type
+     * @param input String, a user input to be split into an array of parameters
+     * @return String[] inputArr.
+     * For "todo": an array of 1, containing description.
+     * For "deadline": an array of 2: [description, by]
+     * For "event": an array of 2: [description, at]
+     * For "mark": an array of 2: [action, taskNum]
+     * For "delete": an array of 1: [taskNum]
+     */
+    static String[] getParams(Bernie.Type taskType, String input) throws BernieException {
+        String description;
+        String[] inputArr = null;
+        switch (taskType) {
+        case TODO:
+            inputArr = input.split("todo ");
+            description = getDescription(inputArr, Bernie.Type.TODO);
+            inputArr = new String[]{description};
+            break;
+        case DEADLINE:
+            inputArr = input.split(" /by ");
+            description = getDescription(inputArr, Bernie.Type.DEADLINE);
+            String by = getTime(inputArr, Bernie.Type.DEADLINE);
+            inputArr = new String[]{description, by};
+            break;
+        case EVENT:
+            inputArr = input.split(" /at ");
+            description = getDescription(inputArr, Bernie.Type.EVENT);
+            String at = getTime(inputArr, Bernie.Type.EVENT);
+            inputArr = new String[]{description, at};
+            break;
+        case MARK:
+            inputArr = input.split(" ");
+            // check valid
+            checkMarkOrDeleteInput(inputArr, Bernie.Type.MARK);
+            break;
+        case DELETE:
+            inputArr = input.split(" ");
+            checkMarkOrDeleteInput(inputArr, Bernie.Type.DELETE);
+            inputArr = new String[]{inputArr[1]};
+            break;
+        default:
+            break;
+        }
+        return inputArr;
+    }
+
+    /**
+     * Checks for valid "mark" or "delete" action inputs. Throws error if it is not valid.
+     * @param inputArr String[], the user input String split into an array of String for processing
+     *                 parameters for the action
+     * @throws BernieException for invalid inputs
+     */
+    static void checkMarkOrDeleteInput(String[] inputArr, Bernie.Type action) throws BernieException {
+        if (inputArr.length == 2) {
+            try {
+                String taskNum = inputArr[1];
+                Integer.parseInt(taskNum);
+                tasks.taskExists(taskNum);
+            } catch (NumberFormatException nfe) {
+                throw new BernieException("That's not a task number! Put a number.");
+            }
+        } else {
+            if (action.equals(Bernie.Type.MARK)) {
+                throw new BernieException("Wrong input. Type this: mark/unmark taskNumber");
+            } else {
+                throw new BernieException("Wrong input. Type this: delete taskNumber");
+            }
+        }
+    }
+
+    /**
+     * Gets the description from the inputArr which contains parameters for creating Task
+     * @param inputArr String[], parameters for creating Task, obtained from user input String.
+     * @param taskType Type
+     * @return String description, for creating of Task
+     * @throws BernieException for invalid descriptions such as empty or number description
+     */
+    static String getDescription(String[] inputArr, Bernie.Type taskType) throws BernieException {
+        String description;
+        try {
+            if (taskType.equals(Bernie.Type.TODO)) {
+                description = inputArr[1];
+            } else {
+                // can take note of lowercase/uppercase
+                description = inputArr[0].split(taskType.name().toLowerCase() + " ")[1];
+            }
+            checkDescriptionNotNumber(description);
+            return description;
+        } catch (IndexOutOfBoundsException e) {
+            throw new BernieException("Where's the description?");
+        }
+    }
+
+    static boolean checkDescriptionNotNumber(String description) throws BernieException {
+        try {
+            Integer.parseInt(description);
+            throw new BernieException("Description can't be numbers? We need String!");
+        } catch (NumberFormatException nfe) {
+            return true;
+        }
+    }
+
+    /**
+     * Gets the time from the inputArr which contains parametrs for creating Task
+     * @param inputArr String[], parameters for creating Task, obtained from user input String.
+     * @param taskType type, if it is DEADLINE we check if the input date format is correct.
+     * @return String time, for creating of Task. It is of format: yyyy-mm-dd for converting
+     * to LocalDate object by TaskList's addTask class
+     * @throws BernieException if there is no time input given
+     */
+    static String getTime(String[] inputArr, Bernie.Type taskType) throws BernieException {
+        try {
+            if (taskType.equals(Bernie.Type.DEADLINE)) {
+                LocalDate checkValid = LocalDate.parse(inputArr[1]);
+            }
+            return inputArr[1];
+        } catch (IndexOutOfBoundsException e) {
+            throw new BernieException("Where's your time input?");
+        } catch (DateTimeParseException e) {
+            throw new BernieException("Please enter deadline date in: yyyy-mm-dd");
+        }
+    }
 }
 
