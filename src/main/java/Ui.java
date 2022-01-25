e918b0d336dcd7fc54e80b57ffa19a7f29426d91
alexteo98 <alexteo98@gmail.com>
2022-01-26 00:56:02 +0800

    Update Ui display methods to non-static

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -7,9 +7,9 @@ public class Ui {
     private static final String doubleLine =    "=====================================================================================";
     private Scanner scanner = new Scanner(System.in);
 
-    public static PrintStream writer = System.out;
+    public PrintStream writer = System.out;
 
-    public static void showHelpMenu() {
+    public void showHelpMenu() {
         String helpMenu = "List of available commands:\n\n" +
                 "\"list\": View Current Task List\n" +
                 "\"todo <name>\": Add a new Task\n" +
@@ -24,7 +24,7 @@ public class Ui {
         writer.println(helpMenu);
         writer.println(line);
     }
-    public static void showBanner() {
+    public void showBanner() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -38,65 +38,65 @@ public class Ui {
         writer.println(intro);
         writer.println(line);
     }
-    public static void showBye() {
+    public void showBye() {
         String bye = "Bye. Hope to see you again soon!";
 
         writer.println(line);
         writer.println(bye);
         writer.println(line);
     }
-    public static void showStarting() {
+    public void showStarting() {
         writer.println("Starting Application...");
     }
 
     // Error Messages
-    public static void showInvalidIntegerError() {
+    public void showInvalidIntegerError() {
         writer.println("Invalid number entered! Please enter an integer");
     }
-    public static void showInvalidDateTimeError() {
+    public void showInvalidDateTimeError() {
         writer.println("Invalid Date/Time!");
     }
-    public static void showInvalidTaskNameError() {
+    public void showInvalidTaskNameError() {
         writer.println("Invalid Task Name!");
     }
 
     // Parser Error Messages
-    public static void showParseDateTimeError() {
+    public void showParseDateTimeError() {
         writer.println("Error parsing Date/Time!");
         writer.println("Please enter Date/Time in the form DD/MM/YYYY HHMM");
     }
 
     // Mark/Unmark Messages
-    public static void showMarked(Task t) {
+    public void showMarked(Task t) {
         writer.printf("+++ Nice! I've marked this task as done:\n" +
                 "+++ %s\n", t);
     }
-    public static void showUnmarked(Task t) {
+    public void showUnmarked(Task t) {
         writer.printf("--- OK, I've marked this task as not done yet:\n" +
                 "--- %s\n", t);
     }
-    public static void showUnmarkNotNeeded(Task t) {
+    public void showUnmarkNotNeeded(Task t) {
         writer.printf("    This task has not been marked as done yet:\n" +
                 "    %s\n", t);
     }
-    public static void showMarkNotNeeded(Task t) {
+    public void showMarkNotNeeded(Task t) {
         writer.printf("    This task is already marked as done:\n" +
                 "    %s\n", t);
     }
-    public static void showMarkOutOfBounds() {
+    public void showMarkOutOfBounds() {
         writer.println("Invalid number entered! No tasks marked.");
     }
-    public static void showUnmarkOutOfBounds() {
+    public void showUnmarkOutOfBounds() {
         writer.println("Invalid number entered! No tasks unmarked.");
     }
-    public static void showMarkEmptyList() {
+    public void showMarkEmptyList() {
         writer.println("There are no tasks in your task list to mark");
     }
-    public static void showUnmarkEmptyList() {
+    public void showUnmarkEmptyList() {
         writer.println("There are no tasks in your task list to unmark");
     }
 
-    public static void showList(TaskManager tm) {
+    public void showList(TaskManager tm) {
         int i = 1;
         String s;
         if (tm.size() == 0){
@@ -116,49 +116,49 @@ public class Ui {
     }
 
     // Add/Delete Messages
-    public static void showAddedTask(Task t,int numberOfTasks) {
+    public void showAddedTask(Task t,int numberOfTasks) {
         writer.printf("Got it. I've added this task:\n" +
                 "\t%s\n" +
                 "Now you have %d tasks in the list.", t.toString(), numberOfTasks);
     }
-    public static void showDeletedTask(Task t, int numberOfTasks) {
+    public void showDeletedTask(Task t, int numberOfTasks) {
         writer.printf(   "Noted. I have removed this task:\n" +
                 "\t%s\n" +
                 "There are now %d tasks in your task list", t, numberOfTasks);
     }
-    public static void showDeleteEmptyList() {
+    public void showDeleteEmptyList() {
         writer.println("There are no tasks in your task list to delete.");
     }
-    public static void showDeleteOutOfBounds(int limit) {
+    public void showDeleteOutOfBounds(int limit) {
         writer.printf("Invalid number entered! The number entered must be between 1 and %s", limit);
     }
 
     // Save/Load Files
-    public static void showInitializeDefaults() {
+    public void showInitializeDefaults() {
         writer.println("...Initializing Defaults...");
     }
-    public static void showFileNotFound() {
+    public void showFileNotFound() {
         writer.println("Data File not found!");
     }
-    public static void showFileReadError() {
+    public void showFileReadError() {
         writer.println("Error Reading from file!");
     }
-    public static void showDirNotFound() {
+    public void showDirNotFound() {
         writer.println("Directory not found!");
     }
-    public static void showDirCreating(String s){
+    public void showDirCreating(String s){
         writer.printf("Creating %s...", s);
     }
-    public static void showDirCreated() {
+    public void showDirCreated() {
         writer.println("Directory Created!");
     }
-    public static void showLoadingComplete() {
+    public void showLoadingComplete() {
         writer.println("Loading Completed!");
     }
-    public static void showSavingComplete() {
+    public void showSavingComplete() {
         writer.println("Saving Completed!");
     }
-    public static void showTaskLoadFail(String input) {
+    public void showTaskLoadFail(String input) {
         writer.println("Task not loaded. Details: " + input);
     }
 
@@ -172,7 +172,6 @@ public class Ui {
 
     // Tests
     public static void main(String[] args) {
-        showBanner();
-        showHelpMenu();
+
     }
 }
