46cc2c7ce6d19305aa75ae49e9afab3c83fc0b3d
jiale-c <cheejiale@gmail.com>
2022-01-26 00:34:37 +0800

    Add dates and times format for deadline and event.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,40 @@
-public class Deadline extends Task {
-    protected String dueDate;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String description, String dueDate) {
+public class Deadline extends Task {
+    protected LocalDate dueDate;
+    protected LocalTime time;
+
+    public Deadline(String description, String dueDate) throws DukeException {
         super(description);
-        this.dueDate = dueDate;
+        try {
+            this.setDueDateTime(dueDate);
+        } catch (Exception e) {
+            throw new DukeException("Wrong Date and Time format. yyyy-mm-dd HH:MM\n");
+        }
+    }
+
+    private void setDueDateTime(String dueDate) {
+        this.dueDate = LocalDate.parse(dueDate.split(" ")[0]);
+        this.time = LocalTime.parse(dueDate.split(" ")[1]);
+    }
+
+    private String dateToString() {
+        return this.dueDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
+
+    private String timeToString() {
+        return this.time.format(DateTimeFormatter.ofPattern("h:mm a"));
+    }
+
+    public String getDate() {
+        return this.dateToString();
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + dueDate + ")";
+        return "[D]" + super.toString() +
+                " (by: " + this.dateToString() + " " + this.timeToString() + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,40 @@
-public class Event extends Task{
-    protected String dueDate;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String description, String dueDate) {
+public class Event extends Task{
+    protected LocalDate dueDate;
+    protected LocalTime time;
+
+    public Event(String description, String dueDate) throws DukeException {
         super(description);
-        this.dueDate = dueDate;
+        try {
+            this.setDueDateTime(dueDate);
+        } catch (Exception e) {
+            throw new DukeException("Wrong Date and Time format. yyyy-mm-dd HH:MM\n");
+        }
+    }
+
+    private void setDueDateTime(String dueDate) {
+        this.dueDate = LocalDate.parse(dueDate.split(" ")[0]);
+        this.time = LocalTime.parse(dueDate.split(" ")[1]);
+    }
+
+    private String dateToString() {
+        return this.dueDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
+
+    private String timeToString() {
+        return this.time.format(DateTimeFormatter.ofPattern("h:mm a"));
+    }
+
+    public String getDate() {
+        return this.dateToString();
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + dueDate + ")";
+        return "[E]" + super.toString() + " (at: " +
+                this.dateToString() + " " + this.timeToString() + ")";
     }
 }
