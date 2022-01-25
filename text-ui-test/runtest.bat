7890078af426dda4a02227f140cb4b7e6055f6fd
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-01-25 09:26:18 +0800

    Add jar file
    Added Jar file for users to easily run application

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -10,7 +10,7 @@ REM delete output from previous run
 if exist data\tasks.csv del data\tasks.csv
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac  -cp ..\src\main\java\duke -Xlint:none -d ..\bin ..\src\main\java\duke\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
