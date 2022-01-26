0f0b79372be7b5fefa101ddaa8fc45fb3e124226
markbiju <markbiju16@gmail.com>
2022-01-26 16:58:18 +0800

    Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DukeException {
         Scanner myObj = new Scanner(System.in);
         int counter = 0;
         Task[] tasks = new Task[100];
@@ -20,66 +20,85 @@ public class Duke {
                 for(int i = 0; i < counter; i++) {
                     System.out.println( Integer.toString(i + 1) + "." + tasks[i]);
                 }
-            } else if(input.substring(0,4).equals("mark")){
-                int toBeMarked = Integer.parseInt(input.substring(5));
-                Task currentTask = tasks[toBeMarked - 1];
-                currentTask.markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(currentTask);
-
-            } else if(input.substring(0,6).equals("unmark")){
-                int toBeUnmarked = Integer.parseInt(input.substring(7));
-                Task currentTask = tasks[toBeUnmarked - 1];
-                currentTask.markAsUndone();
-                System.out.println("OK, I've marked this task as not done yet: ");
-                System.out.println(currentTask);
-
-            } else if(input.substring(0, 4).equals("todo")) {
-                String[] arrOfStr = input.split(" ", 2);
-                tasks[counter] = new Todo(arrOfStr[1]);
-                counter = counter + 1;
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[counter - 1]);
-                if(counter == 1){
-                    System.out.println("Now you have " + String.valueOf(counter) + " task in the list.");
-                } else {
-                    System.out.println("Now you have " + String.valueOf(counter) + " tasks in the list.");
+            } else if(input.length() > 3 && input.substring(0,4).equals("mark")){
+                try {
+                    int toBeMarked = Integer.parseInt(input.substring(5));
+                    Task currentTask = tasks[toBeMarked - 1];
+                    currentTask.markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(currentTask);
+                } catch (StringIndexOutOfBoundsException e) {
+                    throw new DukeException("☹ OOPS!!! Please tell me which task to mark as done!");
                 }
 
-            } else if(input.substring(0, 8).equals("deadline")) {
-                String[] arrOfStr = input.split(" ", 2);
-                String[] arrOfStr2 = arrOfStr[1].split("/", 2);
-                String[] arrOfStr3 = arrOfStr2[1].split(" ", 2);
+            } else if(input.length() > 5 && input.substring(0,6).equals("unmark")){
+                try {
+                    int toBeUnmarked = Integer.parseInt(input.substring(7));
+                    Task currentTask = tasks[toBeUnmarked - 1];
+                    currentTask.markAsUndone();
+                    System.out.println("OK, I've marked this task as not done yet: ");
+                    System.out.println(currentTask);
+                } catch (StringIndexOutOfBoundsException e) {
+                    throw new DukeException("☹ OOPS!!! Please tell me which task you want me to mark as not done!");
 
-                tasks[counter] = new Deadline(arrOfStr2[0], arrOfStr3[1]);
-                counter = counter + 1;
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[counter - 1]);
-                if(counter == 1){
-                    System.out.println("Now you have " + String.valueOf(counter) + " task in the list.");
-                } else {
-                    System.out.println("Now you have " + String.valueOf(counter) + " tasks in the list.");
-                }
-            } else if(input.substring(0, 5).equals("event")) {
-                String[] arrOfStr = input.split(" ", 2);
-                String[] arrOfStr2 = arrOfStr[1].split("/", 2);
-                String[] arrOfStr3 = arrOfStr2[1].split(" ", 2);
-
-                tasks[counter] = new Event(arrOfStr2[0], arrOfStr3[1]);
-                counter = counter + 1;
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[counter - 1]);
-                if(counter == 1){
-                    System.out.println("Now you have " + String.valueOf(counter) + " task in the list.");
-                } else {
-                    System.out.println("Now you have " + String.valueOf(counter) + " tasks in the list.");
                 }
 
+            } else if(input.length() > 3 && input.substring(0, 4).equals("todo")) {
+                try {
+                    String[] arrOfStr = input.split(" ", 2);
+                    tasks[counter] = new Todo(arrOfStr[1]);
+                    counter = counter + 1;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks[counter - 1]);
+                    if (counter == 1) {
+                        System.out.println("Now you have " + String.valueOf(counter) + " task in the list.");
+                    } else {
+                        System.out.println("Now you have " + String.valueOf(counter) + " tasks in the list.");
+                    }
 
+                } catch(ArrayIndexOutOfBoundsException e) {
+                    throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                }
+
+            } else if(input.length() > 7  && (input.substring(0, 8).equals("deadline"))) {
+                try {
+                    String[] arrOfStr = input.split(" ", 2);
+                    String[] arrOfStr2 = arrOfStr[1].split("/", 2);
+                    String[] arrOfStr3 = arrOfStr2[1].split(" ", 2);
+
+                    tasks[counter] = new Deadline(arrOfStr2[0], arrOfStr3[1]);
+                    counter = counter + 1;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks[counter - 1]);
+                    if (counter == 1) {
+                        System.out.println("Now you have " + String.valueOf(counter) + " task in the list.");
+                    } else {
+                        System.out.println("Now you have " + String.valueOf(counter) + " tasks in the list.");
+                    }
+                } catch(ArrayIndexOutOfBoundsException e) {
+                    throw new DukeException("☹ OOPS!!! Please provide a deadline for your task.");
+
+                }
+            } else if(input.length() > 4 && input.substring(0, 5).equals("event")) {
+                try {
+                    String[] arrOfStr = input.split(" ", 2);
+                    String[] arrOfStr2 = arrOfStr[1].split("/", 2);
+                    String[] arrOfStr3 = arrOfStr2[1].split(" ", 2);
+
+                    tasks[counter] = new Event(arrOfStr2[0], arrOfStr3[1]);
+                    counter = counter + 1;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(tasks[counter - 1]);
+                    if (counter == 1) {
+                        System.out.println("Now you have " + String.valueOf(counter) + " task in the list.");
+                    } else {
+                        System.out.println("Now you have " + String.valueOf(counter) + " tasks in the list.");
+                    }
+                } catch(ArrayIndexOutOfBoundsException e) {
+                    throw new DukeException("☹ OOPS!!! Please provide a timing for your event.");
+                }
             } else {
-                tasks[counter] = new Task(input);
-                counter = counter + 1;
-                System.out.println("added: " + input);
+                throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         }
     }
