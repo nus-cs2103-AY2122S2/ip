3c0690cfcb8a58ea958c40729d76ce8625f4b659
clement0010 <clement0010@hotmail.com>
2022-01-27 05:08:06 +0800

    Create tasklist class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,50 @@
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+
+public class TaskList {
+    private final ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void markTask(int taskId, boolean isDone) throws DukeException {
+        try {
+            if (isDone) {
+                tasks.get(taskId).markAsDone();
+            } else {
+                tasks.get(taskId).markAsNotDone();
+            }
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException(e.getMessage());
+        }
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void deleteTask(int taskId) throws DukeException {
+        try {
+            tasks.remove(taskId);
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException(e.getMessage());
+        }
+    }
+
+    public Task getTask(int taskId) throws DukeException {
+        try {
+            return this.tasks.get(taskId);
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException(e.getMessage());
+        }
+    }
+
+    public int getSize() {
+        return tasks.size();
+    }
+}
