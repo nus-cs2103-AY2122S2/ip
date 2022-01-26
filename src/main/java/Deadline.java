975e17b2c0cd277f35771cb8700265257de3da29
Shi Hong <68313416+chuashihong@users.noreply.github.com>
2022-01-27 01:04:34 +0800

    Level-7 Save

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -7,6 +7,22 @@ public class Deadline extends Task {
         this.by = by;
     }
 
+    public Deadline(String description, String by, boolean isDone) {
+        super(description, isDone);
+        this.by = by;
+    }
+
+    @Override
+    public String dataFormatOfTask() {
+        String bool;
+        if(this.isDone) {
+            bool = "1";
+        } else {
+            bool = "0";
+        }
+        return "D | " + bool + " | " + this.description + " | " + this.by;
+    }
+
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> taskList;
+
+    public TaskList() {
+        this.taskList = new ArrayList<Task>();
+    }
+
+    public TaskList(ArrayList<Task> taskList) {
+        this.taskList = taskList;
+    }
+
+    public Task get(int index) {
+        return this.taskList.get(index);
+    }
+
+    public int size() {
+        return this.taskList.size();
+    }
+
+    public void add(Task task) {
+        this.taskList.add(task);
+    }
+
+    public void add(String description) {
+        this.taskList.add(new Task(description));
+    }
+
+    public void remove(int index) {
+        this.taskList.remove(index);
+    }
+
+    public void markAsDone(int index) {
+        this.taskList.get(index).markAsDone();
+    }
+
+    public void markAsUndone(int index) {
+        this.taskList.get(index).markAsUndone();
+    }
+}
