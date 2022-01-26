1e6b55a65da1a621e174aa4ff689bb700270a336
Ryan Chang Jia Jie <>
2022-01-26 19:07:25 +0800

    update Deadline.java

--- Siri/L4/Deadline.java
+++ Siri/L4/Deadline.java
@@ -1,12 +1,16 @@
 public class Deadline extends Task {
 
-    public Deadline(String description) {
+    String deadline;
+
+    public Deadline(String description, String deadline) {
         super(description);
+        this.deadline = deadline;
+        //find a way to scan deadline after description entered by user, maybe use the "/" as delimiter for the scanner?
     }
 
     @Override
     public String toString() {
-        return "[D]" + "[" + this.getStatusIcon() + "] " + this.description;
+        return "[D]" + "[" + this.getStatusIcon() + "] " + this.description + " (by: " + this.deadline + ")";
     }
 
 }
