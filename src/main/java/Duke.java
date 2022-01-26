fa1081106c2a701814f8866ab7cc46f6f3b64622
ovidharshini <ovidharshini@gmail.com>
2022-01-26 21:22:56 +0800

    Add input sanitization

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,11 +7,17 @@ public class Duke {
         Scanner reader = new Scanner(System.in);
         System.out.println("Hello, traveller! My name in Paimon.\nHow can I help you today?");
         String input = readInput(reader);
-        List<Task> tasks = new LinkedList<Task>();
+        List<Task> tasks = new LinkedList<>();
 
         while (!input.equals("bye")){
             String command = input.replaceAll(" .*", "");
-            if (input.indexOf(" ") > 0) {
+
+            if (!isValid(input)) {
+                input = readInput(reader);
+                continue;
+            }
+
+            if (input.contains(" ")) {
                 input = input.substring(input.indexOf(" "));
             }
 
@@ -31,7 +37,7 @@ public class Duke {
                 case "todo":
                     Todo t = new Todo(input);
                     tasks.add(t);
-                    System.out.println("Got it! I have noted down the following task to your list.");
+                    System.out.println("Got it! I have noted down the following task in your list.");
                     System.out.println(t);
                     break;
                 case "deadline":
@@ -39,7 +45,7 @@ public class Duke {
                     input = input.replaceAll(" by .*", "");
                     Deadline d = new Deadline(input, datetime);
                     tasks.add(d);
-                    System.out.println("Got it! I have noted down the following task to your list. " +
+                    System.out.println("Got it! I have noted down the following task in your list. " +
                             "\nRemember the deadline!");
                     System.out.println(d);
                     break;
@@ -48,7 +54,7 @@ public class Duke {
                     input = input.replaceAll(" at .*", "");
                     Event e = new Event(input, time);
                     tasks.add(e);
-                    System.out.println("Got it! I have noted down the following task to your list. " +
+                    System.out.println("Got it! I have noted down the following task in your list. " +
                             "\nDo be there on time!");
                     System.out.println(e);
                     break;
@@ -75,12 +81,57 @@ public class Duke {
         System.out.println("Hmm... Paimon keeps a clear record in her diary.");
         for (int i = 0; i < t.size(); i++) {
             if (t.get(i) != null) {
-                System.out.println(String.format("  %d. %s", i + 1, t.get(i)));
+                System.out.println("  " + i+1 +". " + t.get(i));
             }
             else {
-                System.out.println(String.format("You now have %d tasks on your list.", i));
+                System.out.println("You now have " + i +" tasks on your list.");
                 return;
             }
         }
+    }
+
+    public static boolean isValid(String input) {
+        //commands = "bye", "list", "do", "undo", "todo", "deadline", "event"
+        input = input.trim();
+        if (input.equals("bye") || input.equals("list")) {
+            return true;
+        }
+
+        String firstWord = input.replaceAll(" .*", "");
+        if (firstWord.equals("do") || firstWord.equals("undo")) {
+            input = input.replaceAll(".* ", "");
+            return input.matches("[0-9]+");
+        }
+
+        if (firstWord.equals("todo")) {
+            input = input.substring(4).trim();
+            if (input.equals("")) {
+                System.out.println("Oops, you need to mention what the task is :c");
+                return false;
+            }
+            return true;
+        }
+
+        if (firstWord.equals("deadline")) {
+            input = input.substring(8).trim();
+            if (!input.contains(" by ")) {
+                System.out.println("Oops, you need to format deadline tasks as \"deadline X by Y\" :c");
+                return false;
+            }
+            String lastWord = input.substring(input.lastIndexOf(" ") + 1);
+            return !lastWord.equals("by");
+        }
+
+        if (firstWord.equals("event")) {
+            input = input.substring(5).trim();
+            if (!input.contains(" at ")) {
+                System.out.println("Oops, you need to format event tasks as \"event X at Y\" :c");
+                return false;
+            }
+            String lastWord = input.substring(input.lastIndexOf(" ")+1);
+            return !lastWord.equals("at");
+        }
+
+        return false;
     }
 }
