dc7e86d3ad3baab43d4eeb46f261bba30a627703
xzzz3 <xingzheng@u.nus.edu>
2022-01-26 21:39:26 +0800

    Parser class: Add new Parser class
    Require a Parser class to deal with making sense of the user command.
    
    Let's:
    * Add a Parser class to parse user commands, when the command needs
    to be split into multiple parts.
    
    Additional Remarks: Did not quite get what a parser class is useful
    for, as most parts of the code are rather blended together and cannot
    be shifted to Parser. May require future changes to perfect Parser.

---
+++ src/main/java/Parser.java
@@ -0,0 +1,12 @@
+public class Parser {
+    public Parser() {
+    }
+
+    public String[] parseDeadline(String string) {
+        return string.split("/by");
+    }
+
+    public String[] parseEvent(String string) {
+        return string.split("/at");
+    }
+}
