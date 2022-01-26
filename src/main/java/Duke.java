2686fb0a86905581d1578f32d1ee18e65b2325d7
Alexander Er <aceszhenwei@yahoo.com>
2022-01-26 12:46:33 +0800

    A-Packages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import duke.*;
 import java.util.ArrayList;
 
 public class Duke {
--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -1,10 +1,11 @@
+package duke;
 import java.util.ArrayList;
 
 public class TaskList {
 
     protected ArrayList<Task> arr;
 
-    public TaskList(ArrayList<Task> arr) {
+    public TaskList(ArrayList<duke.Task> arr) {
         this.arr = arr;
     }
 
