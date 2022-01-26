1dc8234a842e83765c3535bdd998ddf5fd8a1f5f
Lin Da <0407da.lin@gmail.com>
2022-01-26 21:10:06 +0800

    Level-8: Minimal implementation

--- src/main/java/Dazz.java
+++ src/main/java/Dazz.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Dazz {
@@ -10,19 +13,31 @@ public class Dazz {
     }
 
     public static void main(String[] args) {
-        Dazz dazz = new Dazz(new Reminder());
-        Scanner scanner = new Scanner(System.in);
-        dazz.design.startUp();
-        while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            System.out.println(Design.HORIZONTAL_LINE);
-            dazz.logic.run(input);
-            System.out.println(Design.HORIZONTAL_LINE);
-            if (input.startsWith("bye")) {
-                break;
-            }
-        }
-        scanner.close();
+//        Dazz dazz = new Dazz(new Reminder());
+//        Scanner scanner = new Scanner(System.in);
+//        dazz.design.startUp();
+//        while (scanner.hasNextLine()) {
+//            String input = scanner.nextLine();
+//            System.out.println(Design.HORIZONTAL_LINE);
+//            dazz.logic.run(input);
+//            System.out.println(Design.HORIZONTAL_LINE);
+//            if (input.startsWith("bye")) {
+//                break;
+//            }
+//        }
+//        scanner.close();
+
+        // Testing of datetime
+//        Task deadline1 = new Deadline("D", "return book", "02/12/2019 1900");
+//        Task deadline2 = new Deadline("D", "return book", "ASAP");
+//        Task deadline3 = new Deadline("D", "return book", "02-12-2019 1900");
+//        System.out.println(deadline1.dateTime);
+//        System.out.println(deadline1.getDateTime());
+//        System.out.println(deadline2.dateTime);
+//        System.out.println(deadline2.getDateTime());
+//        System.out.println(deadline3.dateTime);
+//        System.out.println(deadline3.getDateTime());
+
     }
 }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,21 @@
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Task {
     protected String taskType;
     protected boolean isDone;
     protected String description;
     protected String date;
+    protected LocalDateTime dateTime;
 
     public Task(String taskType, String description, String date) {
         this.description = description;
         this.isDone = false;
         this.taskType = taskType;
         this.date = date;
+        toLocalDateTime();
     }
 
     public Task(String taskType, boolean isDone, String description, String date) {
@@ -16,6 +23,7 @@ public class Task {
         this.isDone = isDone;
         this.taskType = taskType;
         this.date = date;
+        toLocalDateTime();
     }
 
     public String getStatusIcon() {
@@ -33,6 +41,29 @@ public class Task {
             return this.date;
         }
         return "";
+    }
+
+    public String getDateTime() {
+        try {
+            if (!this.taskType.equals(TaskType.TODO.getTaskType()) && this.dateTime != null) {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("[MMM dd yyyy, hh:mma]");
+                return this.dateTime.format(formatter);
+            }
+            return "Not a valid date";
+        } catch (DateTimeException e) {
+            return this.getDate();
+        }
+    }
+
+    private void toLocalDateTime() {
+        try {
+            if (!this.taskType.equals(TaskType.TODO.getTaskType())) {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("[dd-MM-yyyy HHmm]" + "[dd/MM/yyyy HHmm]");
+                this.dateTime = LocalDateTime.parse(this.date, formatter);
+            }
+        } catch (DateTimeParseException e) {
+            this.dateTime = null;
+        }
     }
 
     private boolean hasAlreadyMark() {
