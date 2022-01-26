287e66922e07af471c24964cd8b4e8c3b00f51d5
xzzz3 <xingzheng@u.nus.edu>
2022-01-26 19:21:22 +0800

    Deadline and Event classes: Add date parsing functionality
    The deadline and event classes take in only string that are date
    descriptions. They cannot translate numerical date values in string.
    
    Sometimes this functionality will be needed as it is more machine
    friendly and may be preferred by people.
    
    Let's:
    * Change the constructors to check for parse-able date strings
    * Parse the date strings if applicable
    * Use the parsed date string as the stored date instead

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
     protected String by;
 
     public Deadline(String name, String by) {
         super(name);
-        this.by = by;
+        System.out.println(by);
+        try {
+            LocalDate d1 = LocalDate.parse(by);
+            this.by = d1.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (DateTimeParseException ex) {
+            this.by = by;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by:" + by + ")";
+        return "[D]" + super.toString() + " (by: " + by + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,22 @@
-public class Event extends Task{
-    protected String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-    public Event(String name, String by) {
+public class Event extends Task{
+    protected String at;
+
+    public Event(String name, String at) {
         super(name);
-        this.by = by;
+        try {
+            LocalDate d1 = LocalDate.parse(at);
+            this.at = d1.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (DateTimeParseException ex) {
+            this.at = at;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (by:" + by + ")";
+        return "[E]" + super.toString() + " (at: " + at + ")";
     }
 }
