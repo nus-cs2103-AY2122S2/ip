3f9eb77dc63eea2f8c9682e7347999496282b692
clement0010 <clement0010@hotmail.com>
2022-01-27 05:07:57 +0800

    Create messages class

---
+++ src/main/java/Messages.java
@@ -0,0 +1,15 @@
+public class Messages {
+    public static final String MESSAGE_WELCOME = "HELLO! WELCOME!";
+    public static final String MESSAGE_GOOD_BYE = "Bye. Hope to see you again soon!";
+    public static final String MESSAGE_FILE_PATH = "Current file path: ";
+    public static final String MESSAGE_PROMPT = "What can I do for you";
+    public static final String MESSAGE_MARK = "Nice! I've marked this task as done.";
+    public static final String MESSAGE_UNMARKED = "OK, I've marked this task as not done yet.";
+    public static final String MESSAGE_DELETE = "Noted. I've removed this task.";
+    public static final String MESSAGE_ADD_TODO = "Got it. I've added this todo.";
+    public static final String MESSAGE_ADD_DEADLINE = "Got it. I've added this deadline.";
+    public static final String MESSAGE_ADD_EVENT = "Got it. I've added this event.";
+    public static final String MESSAGE_ERROR_INVALID_COMMAND = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
+    public static final String MESSAGE_ERROR_EMPTY_TITLE = "☹ OOPS!!! The description of a todo cannot be empty.";
+    public static final String MESSAGE_ERROR_INVALID_DATETIME_FORMAT= "Invalid date time format.";
+}
