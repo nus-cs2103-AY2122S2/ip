6a72de62cf7f4a8a953f403fbea0cc32933109c9
yokester98 <yokeyue.loy@u.nus.edu>
2022-01-27 00:07:24 +0800

    Edit code to follow coding standard

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -26,6 +26,7 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + super.by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+        return "[D]" + super.toString() + " (by: "
+                + super.by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -26,6 +26,7 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + super.by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
+        return "[E]" + super.toString() + " (at: "
+                + super.by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
