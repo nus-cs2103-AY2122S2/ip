4a5e5cf65a3355df256b1edd32f9d74a5232019c
lchokhoe <chokhoeefps335f@gmail.com>
2022-01-26 15:32:30 +0800

    Adjust coding style for switch-case apllication in Duke.java
    To adhere to the java coding standard set for this iP.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,160 +22,160 @@ public class Duke {
             String[] tokens = input.split("\\s");
             String keyword = tokens[0];
             switch (keyword) {
-                case "bye":
-                    try {
-                        if (tokens.length != 1) {
-                            throw new DukeException("argument for bye detected");
-                        }
-                        cmd.bye();
-                        break label;
-                    } catch (DukeException ex) {
-                        String err = "_______________________________________________________\n"
-                                + "* Arguments detected *\n"
-                                + "Are you trying to type 'bye' instead? If so, please try again!\n"
-                                + "_______________________________________________________\n";
-                        System.out.println(err);
-                        break;
+            case "bye":
+                try {
+                    if (tokens.length != 1) {
+                        throw new DukeException("argument for bye detected");
                     }
-                case "list":
-                    try {
-                        if (tokens.length != 1) {
-                            throw new DukeException("argument for list detected");
-                        }
-                        cmd.list();
-                        break;
-                    } catch (DukeException ex) {
-                        String err = "_______________________________________________________\n"
-                                + "* Arguments detected *\n"
-                                + "Are you trying to type 'list' instead? If so, please try again!\n"
-                                + "_______________________________________________________\n";
-                        System.out.println(err);
-                        break;
-                    }
-                case "mark": {
-                    try {
-                        int index = Integer.parseInt(tokens[1]);
-                        cmd.mark(index - 1);
-                        break;
-                    } catch (NumberFormatException | NullPointerException | ArrayIndexOutOfBoundsException ex) {
-                        String err = "_______________________________________________________\n"
-                                + "* Invalid entry detected *\n"
-                                + "Please provide a valid entry!\n"
-                                + "_______________________________________________________\n";
-                        System.out.println(err);
-                        break;
-                    }
+                    cmd.bye();
+                    break label;
+                } catch (DukeException ex) {
+                    String err = "_______________________________________________________\n"
+                            + "* Arguments detected *\n"
+                            + "Are you trying to type 'bye' instead? If so, please try again!\n"
+                            + "_______________________________________________________\n";
+                    System.out.println(err);
+                    break;
                 }
-                case "unmark": {
-                    try {
-                        int index = Integer.parseInt(tokens[1]);
-                        cmd.unmark(index - 1);
-                        break;
-                    } catch (NumberFormatException | NullPointerException | ArrayIndexOutOfBoundsException ex) {
-                        String err = "_______________________________________________________\n"
-                                + "* Invalid entry detected *\n"
-                                + "Please provide a valid entry!\n"
-                                + "_______________________________________________________\n";
-                        System.out.println(err);
-                        break;
+            case "list":
+                try {
+                    if (tokens.length != 1) {
+                        throw new DukeException("argument for list detected");
                     }
+                    cmd.list();
+                    break;
+                } catch (DukeException ex) {
+                    String err = "_______________________________________________________\n"
+                            + "* Arguments detected *\n"
+                            + "Are you trying to type 'list' instead? If so, please try again!\n"
+                            + "_______________________________________________________\n";
+                    System.out.println(err);
+                    break;
                 }
-                case "todo": {
-                    try {
-                        if (tokens.length <= 1) {
-                            throw new MissingFormatArgumentException("no argument detected");
-                        }
-                        cmd.todo(tokens);
-                        break;
-                    } catch (MissingFormatArgumentException ex) {
-                        String err = "_______________________________________________________\n"
-                                + "* No arguments detected *\n"
-                                + "Please provide a description for your ToDo!\n"
-                                + "_______________________________________________________\n";
-                        System.out.println(err);
-                        break;
-                    }
+            case "mark": {
+                try {
+                    int index = Integer.parseInt(tokens[1]);
+                    cmd.mark(index - 1);
+                    break;
+                } catch (NumberFormatException | NullPointerException | ArrayIndexOutOfBoundsException ex) {
+                    String err = "_______________________________________________________\n"
+                            + "* Invalid entry detected *\n"
+                            + "Please provide a valid entry!\n"
+                            + "_______________________________________________________\n";
+                    System.out.println(err);
+                    break;
                 }
-                case "deadline": {
-                    try {
-                        if (tokens.length <= 1) {
-                            throw new MissingFormatArgumentException("no argument detected");
-                        }
-                        cmd.deadline(tokens);
-                        break;
-                    } catch (MissingFormatArgumentException ex) {
-                        String err = "_______________________________________________________\n"
-                                + "* No arguments detected *\n"
-                                + "Please provide a description for your Deadline!\n"
-                                + "_______________________________________________________\n";
-                        System.out.println(err);
-                        break;
-                    } catch (DukeException ex) {
-                        String err = "_______________________________________________________\n"
-                                + "* Time frame not detected *\n"
-                                + "Please provide a time frame for your Deadline!\n"
-                                + "Check if you have typed '/by' to indicate the time frame!\n"
-                                + "e.g. deadline return book /by Sunday\n"
-                                + "_______________________________________________________\n";
-                        System.out.println(err);
-                        break;
-                    }
+            }
+            case "unmark": {
+                try {
+                    int index = Integer.parseInt(tokens[1]);
+                    cmd.unmark(index - 1);
+                    break;
+                } catch (NumberFormatException | NullPointerException | ArrayIndexOutOfBoundsException ex) {
+                    String err = "_______________________________________________________\n"
+                            + "* Invalid entry detected *\n"
+                            + "Please provide a valid entry!\n"
+                            + "_______________________________________________________\n";
+                    System.out.println(err);
+                    break;
                 }
-                case "event": {
-                    try {
-                        if (tokens.length <= 1) {
-                            throw new MissingFormatArgumentException("no argument detected");
-                        }
-                        cmd.event(tokens);
-                        break;
-                    } catch (MissingFormatArgumentException ex) {
-                        String err = "_______________________________________________________\n"
-                                + "* No arguments detected *\n"
-                                + "Please provide a description for your Event!\n"
-                                + "_______________________________________________________\n";
-                        System.out.println(err);
-                        break;
-                    } catch (DukeException ex) {
-                        String err = "_______________________________________________________\n"
-                                + "* Time frame not detected *\n"
-                                + "Please provide a time frame for your Event!\n"
-                                + "Check if you have typed '/at' to indicate the time frame!\n"
-                                + "e.g. event project meeting /at Mon 2-4pm\n"
-                                + "_______________________________________________________\n";
-                        System.out.println(err);
-                        break;
+            }
+            case "todo": {
+                try {
+                    if (tokens.length <= 1) {
+                        throw new MissingFormatArgumentException("no argument detected");
                     }
+                    cmd.todo(tokens);
+                    break;
+                } catch (MissingFormatArgumentException ex) {
+                    String err = "_______________________________________________________\n"
+                            + "* No arguments detected *\n"
+                            + "Please provide a description for your ToDo!\n"
+                            + "_______________________________________________________\n";
+                    System.out.println(err);
+                    break;
                 }
-                case "delete": {
-                    try {
-                        int index = Integer.parseInt(tokens[1]);
-                        cmd.delete(index - 1);
-                        break;
-                    } catch (NumberFormatException | NullPointerException | IndexOutOfBoundsException ex) {
-                        String err = "_______________________________________________________\n"
-                                + "* Invalid entry detected *\n"
-                                + "Please provide a valid entry!\n"
-                                + "_______________________________________________________\n";
-                        System.out.println(err);
-                        break;
+            }
+            case "deadline": {
+                try {
+                    if (tokens.length <= 1) {
+                        throw new MissingFormatArgumentException("no argument detected");
                     }
+                    cmd.deadline(tokens);
+                    break;
+                } catch (MissingFormatArgumentException ex) {
+                    String err = "_______________________________________________________\n"
+                            + "* No arguments detected *\n"
+                            + "Please provide a description for your Deadline!\n"
+                            + "_______________________________________________________\n";
+                    System.out.println(err);
+                    break;
+                } catch (DukeException ex) {
+                    String err = "_______________________________________________________\n"
+                            + "* Time frame not detected *\n"
+                            + "Please provide a time frame for your Deadline!\n"
+                            + "Check if you have typed '/by' to indicate the time frame!\n"
+                            + "e.g. deadline return book /by Sunday\n"
+                            + "_______________________________________________________\n";
+                    System.out.println(err);
+                    break;
                 }
-                default:
-                    try {
-                        throw new MissingFormatArgumentException("invalid keywords");
-                    } catch (MissingFormatArgumentException ex) {
-                        String err = "_______________________________________________________\n"
-                                + "* Unrecognised keyword used *\n"
-                                + "Please try the keywords provided below:\n"
-                                + "    1. list\n"
-                                + "    2. todo [arg]\n"
-                                + "    3. deadline [arg] /by [arg]\n"
-                                + "    4. event [arg] /at [arg]\n"
-                                + "    5. bye\n"
-                                + "_______________________________________________________\n";
-                        System.out.println(err);
-                        break;
+            }
+            case "event": {
+                try {
+                    if (tokens.length <= 1) {
+                        throw new MissingFormatArgumentException("no argument detected");
                     }
+                    cmd.event(tokens);
+                    break;
+                } catch (MissingFormatArgumentException ex) {
+                    String err = "_______________________________________________________\n"
+                            + "* No arguments detected *\n"
+                            + "Please provide a description for your Event!\n"
+                            + "_______________________________________________________\n";
+                    System.out.println(err);
+                    break;
+                } catch (DukeException ex) {
+                    String err = "_______________________________________________________\n"
+                            + "* Time frame not detected *\n"
+                            + "Please provide a time frame for your Event!\n"
+                            + "Check if you have typed '/at' to indicate the time frame!\n"
+                            + "e.g. event project meeting /at Mon 2-4pm\n"
+                            + "_______________________________________________________\n";
+                    System.out.println(err);
+                    break;
+                }
+            }
+            case "delete": {
+                try {
+                    int index = Integer.parseInt(tokens[1]);
+                    cmd.delete(index - 1);
+                    break;
+                } catch (NumberFormatException | NullPointerException | IndexOutOfBoundsException ex) {
+                    String err = "_______________________________________________________\n"
+                            + "* Invalid entry detected *\n"
+                            + "Please provide a valid entry!\n"
+                            + "_______________________________________________________\n";
+                    System.out.println(err);
+                    break;
+                }
+            }
+            default:
+                try {
+                    throw new MissingFormatArgumentException("invalid keywords");
+                } catch (MissingFormatArgumentException ex) {
+                    String err = "_______________________________________________________\n"
+                            + "* Unrecognised keyword used *\n"
+                            + "Please try the keywords provided below:\n"
+                            + "    1. list\n"
+                            + "    2. todo [arg]\n"
+                            + "    3. deadline [arg] /by [arg]\n"
+                            + "    4. event [arg] /at [arg]\n"
+                            + "    5. bye\n"
+                            + "_______________________________________________________\n";
+                    System.out.println(err);
+                    break;
+                }
             }
         }
     }
