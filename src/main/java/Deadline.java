36bcb23f00e42ca47fd17e05a2ced0e8a5226245
B1LLP4RK <bd08001@gmail.com>
2022-01-26 15:57:28 +0800

    store date as DateTime class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,16 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Deadline extends Task {
-    private String date;
+    private LocalDate date;
     public Deadline(String content, String date) {
         super(content);
-        this.date = date;
+        this.date = LocalDate.parse(date, DateTimeFormatter.ISO_DATE);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " ( by:" + date + ")";
+        return "[D]" + super.toString() + " ( by:" + date.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Event  extends Task {
-    private String time;
+    private LocalDate time;
 
     public Event(String content, String time) {
         super(content);
-        this.time = time;
+        this.time = LocalDate.parse(time, DateTimeFormatter.ISO_DATE);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (at: " + time + ")" ;
+        return "[E]" + super.toString() + " (at: " + time.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)) + ")" ;
     }
 }
