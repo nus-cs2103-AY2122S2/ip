2477b6b7014efa48030554eedf147c87a18dbff5
Mohamad Shafik <>
2022-01-26 21:53:10 +0800

    Add date and time field for Deadline and Event classes.
    The date field must be filled while time is optional.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,22 @@
-public class Deadline extends Task {
-    protected String deadlineDate;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public Deadline(String taskName, String date) {
+public class Deadline extends Task {
+    protected DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d-MMM-yyyy");
+    protected DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mma");
+    protected LocalDate deadlineDate;
+    protected LocalTime deadlineTime;
+
+    public Deadline(String taskName, LocalDate date) {
         super(taskName);
         this.deadlineDate = date;
+    }
+
+    public Deadline(String taskName, LocalDate deadlineDate, LocalTime deadlineTime) {
+        super(taskName);
+        this.deadlineDate = deadlineDate;
+        this.deadlineTime = deadlineTime;
     }
 
     @Override
@@ -13,6 +26,11 @@ public class Deadline extends Task {
 
     @Override
     public String getFullDetails() {
-        return (super.getFullDetails() + "(by: " + this.deadlineDate + ")");
+        if (deadlineTime != null) {
+            return (super.getFullDetails() + "(by: " + dateFormatter.format(deadlineDate) + " "
+                    + timeFormatter.format(deadlineTime) + ")");
+        } else {
+            return (super.getFullDetails() + "(by: " + dateFormatter.format(deadlineDate) + ")");
+        }
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,9 +1,22 @@
-public class Event extends Task {
-    protected String eventDetails;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-    public Event(String taskName, String eventDetails) {
+public class Event extends Task {
+    protected DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d-MMM-yyyy");
+    protected DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mma");
+    protected LocalDate eventDate;
+    protected LocalTime eventTime;
+
+    public Event(String taskName, LocalDate eventDate) {
         super(taskName);
-        this.eventDetails = eventDetails;
+        this.eventDate = eventDate;
+    }
+    
+    public Event(String taskName, LocalDate eventDate, LocalTime eventTime) {
+        super(taskName);
+        this.eventDate = eventDate;
+        this.eventTime = eventTime;
     }
 
     @Override
@@ -13,6 +26,11 @@ public class Event extends Task {
 
     @Override
     public String getFullDetails() {
-        return (super.getFullDetails() + "(at: " + this.eventDetails + ")");
+        if (eventTime != null) {
+            return (super.getFullDetails() + "(by: " + dateFormatter.format(eventDate) + " "
+                    + timeFormatter.format(eventTime) + ")");
+        } else {
+            return (super.getFullDetails() + "(by: " + dateFormatter.format(eventDate) + ")");
+        }
     }
 }
