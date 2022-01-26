6a746a730c99398e67984a65302aa0a8dc474389
petermonky <wjdgustjr0554@gmail.com>
2022-01-26 14:53:01 +0800

    Implement Level-7
    Duke writes and saves tasks to an external file.
    
    Data is stored locally on the user's device.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,18 @@
 public class Deadline extends Task {
     protected String byTime;
 
-    public Deadline(String description, String byTime) {
-        super(description);
+    public Deadline(String description, boolean isMarked, String byTime) {
+        super(description, isMarked);
         this.byTime = byTime;
     }
 
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + this.byTime + ")";
+    }
+
+    @Override
+    public String toData() {
+        return "D | " + (this.isMarked ? 1 : 0) + " | " + this.description + " | " + this.byTime;
     }
 }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,10 +1,15 @@
 public class Todo extends Task {
-    public Todo(String description) {
-        super(description);
+    public Todo(String description, boolean isMarked) {
+        super(description, isMarked);
     }
 
     @Override
     public String toString() {
         return "[T]" + super.toString();
+    }
+
+    @Override
+    public String toData() {
+        return "T | " + (this.isMarked ? 1 : 0) + " | " + this.description;
     }
 }
