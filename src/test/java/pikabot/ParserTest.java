a1e8756099791dad3e01444e7b22662c129d20ad
dionegoh <e0439283@u.nus.edu>
2022-01-26 19:29:41 +0800

    Add JUnit Tests

--- src/test/java/pikabot/ParserTest.java
+++ src/test/java/pikabot/ParserTest.java
@@ -1,12 +1,40 @@
 package pikabot;
 
+import pikabot.exception.TodoException;
+import pikabot.task.Deadline;
+import pikabot.task.Task;
+import pikabot.task.Todo;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
-public class PikaBotTest {
+public class ParserTest {
+
     @Test
-    public void dummyTest() {
-        assertEquals(2,2);
+    public void todo_withNoDescription_exceptionThrown() {
+        try {
+            String[] strArr = {"todo"};
+            Todo currTodo = Parser.parseTodo(strArr);
+            fail();
+        } catch (Exception e) {
+            assertEquals("☹ OOPS!!! The description of a todo cannot be empty.",
+                    e.getMessage());
+        }
     }
+
+    @Test
+    public void deadline_withInvalidDateFormat_exceptionThrown() {
+        try {
+            String[] strArr = {"deadline", "finish hw /by 09-08-2022"};
+            Deadline currDeadline = Parser.parseDeadline(strArr);
+            fail();
+        } catch (Exception e) {
+            assertEquals("☹ OOPS!!! Invalid deadline! Deadline " +
+                    "has to be a valid date in numerical format YYYY-MM-DD", e.getMessage());
+        }
+    }
+
 }
--- src/test/java/pikabot/TaskListTest.java
+++ src/test/java/pikabot/TaskListTest.java
@@ -1,2 +1,37 @@
-package pikabot;public class TaskListTest {
+package pikabot;
+
+import org.junit.jupiter.api.Test;
+import pikabot.task.Task;
+import pikabot.task.Todo;
+
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskListTest {
+
+    TaskList tasklist;
+
+    public void initialiseDummy() {    //Dummy taskList containing a task
+        ArrayList<Task> arrList = new ArrayList<>();
+        Todo todo = new Todo("Testing!");
+        arrList.add(todo);
+        this.tasklist = new TaskList(arrList);
+    }
+
+    @Test
+    public void addedTaskToList_success() {
+        initialiseDummy();
+        this.tasklist.add(new Todo("Test again!"));
+        String addedTaskStr = this.tasklist.get(1).toString();
+        assertEquals("[T][ ] Test again!", addedTaskStr);
+    }
+
+    @Test
+    public void markTaskInTaskList_success() {
+        initialiseDummy();
+        this.tasklist.markTaskAsDone(1);
+        String todoStr = this.tasklist.get(0).toString();
+        assertEquals("[T][X] Testing!", todoStr);
+    }
+
 }
