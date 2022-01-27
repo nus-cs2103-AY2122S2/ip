3f4e715dfad0ff02b6792fb92a166d8057a06d80
waffledood <itshaikalhere@hotmail.sg>
2022-01-27 09:23:30 +0800

    Implement Command as an asbtract class & extract commands as standalone classes

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,44 +1,11 @@
-import java.util.HashMap;
-import java.util.Map;
+public abstract class Command {
 
-public enum Command {
-    BYE("bye", null),
-    LIST("list", null),
-    MARK("mark", "\\d+"),
-    UNMARK("unmark", "\\d+"),
-    DELETE("delete", "\\d+"),
-    TODO("todo", null),
-    EVENT("event", "/at"),
-    DEADLINE("deadline", "/by");
-
-    private static final Map<String, Command> commandMap = new HashMap<>();
-
-    static {
-        for (Command c : values()) {
-            commandMap.put(c.commandString, c);
-        }
+    CommandType commandType;
+    
+    public Command(CommandType commandType) {
+        this.commandType = commandType;
     }
 
-    private final String commandString;
-    private final String regex;
-
-    private Command(String commandString, String regex) {
-        this.commandString = commandString;
-        this.regex = regex;
-    }
-
-    public static Command getCommand(String commandString) throws DukeException {
-        Command command = commandMap.get(commandString);
-
-        if (command == null) {
-            throw new DukeException("I'm sorry, you've input a command I don't recognize. Please try again.");
-        }
-
-        return command;
-    }
-
-    public String getRegex() {
-        return this.regex;
-    }
-
+    public abstract void execute(Ui ui, Storage storage);
+    
 }
