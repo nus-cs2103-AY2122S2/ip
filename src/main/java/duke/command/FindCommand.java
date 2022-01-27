fa412eddfd863194d87a64a1bc0d57dad6daea10
sharpstorm <chimchar17@gmail.com>
2022-01-27 15:45:03 +0800

    Fix broken level 9 caused by merge

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -2,7 +2,7 @@ package duke.command;
 
 import duke.exception.DukeIllegalArgumentException;
 import duke.task.TaskList;
-import duke.util.IPrintable;
+import duke.util.Printable;
 
 /**
  * Represents a handler for the find command.
@@ -17,13 +17,13 @@ public class FindCommand extends Command {
     }
 
     @Override
-    public boolean execute(IPrintable linePrinter, TaskList taskList) throws DukeIllegalArgumentException {
+    public boolean execute(Printable linePrinter, TaskList taskList) throws DukeIllegalArgumentException {
         if (this.args.equals("")) {
             throw new DukeIllegalArgumentException("Search term cannot be empty");
         }
 
         linePrinter.print("Here are the tasks related to your search:");
-        taskList.forEach((index, task) -> {
+        taskList.doForEach((index, task) -> {
             if (task.getDescription().toLowerCase().contains(this.args.toLowerCase())) {
                 linePrinter.print(String.format("%d. %s", index + 1, task.getReadableString()));
             }
