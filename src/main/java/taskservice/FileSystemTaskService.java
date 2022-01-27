a5cb0e0d1e0f28611d4ad190b3d65aee6cfd142d
Chua Bing Quan <chipneverdies@gmail.com>
2022-01-28 01:31:50 +0800

    Remove unused import

--- src/main/java/taskservice/FileSystemTaskService.java
+++ src/main/java/taskservice/FileSystemTaskService.java
@@ -7,7 +7,6 @@ import tasks.Todo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
