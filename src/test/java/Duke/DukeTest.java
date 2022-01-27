6ecb648f9ae8f2cbb36dced6910c33af18a6fae7
Kevin Kuo <63287696+kevinkuo0320@users.noreply.github.com>
2022-01-28 03:40:27 +0800

    Add unit test for Duke

---
+++ src/test/java/Duke/DukeTest.java
@@ -0,0 +1,35 @@
+package Duke;
+
+
+import org.junit.jupiter.api.Test;
+import task.Task;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DukeTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void mark_unmarked() {
+        Task task = new Task("taskName");
+        task.setMarked(true);
+        assertEquals(task.isMarked(), true);
+    }
+
+    @Test
+    public void toString_marked() {
+        Task task = new Task("taskName");
+        task.setMarked(true);
+        assertEquals(task.toString(), "[X] taskName");
+    }
+
+    @Test
+    public void toString_unmarked() {
+        Task task = new Task("taskName");
+        assertEquals(task.toString(), "[ ] taskName");
+    }
+
+}
