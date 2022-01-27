7127df015f6c84f0b448743d34a1fc6dc99e230b
hsiaojietng <hsiaojietng@gmail.com>
2022-01-27 14:13:29 +0800

    Add readTaskFromFile

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,28 +12,33 @@ import java.util.Scanner;
  */
 
 public class Duke {
-    public static void main(String[] args) throws DukeException, FileNotFoundException {
+    public static void main(String[] args) throws DukeException, IOException {
         String welcomeMsg = "____________________________________________________________\n" +
                 "Hello! I'm Duke\nWhat can I do for you?\n" +
                 "____________________________________________________________";
+        String startingFilePath = "C:\\NUS\\CS2103\\iP\\data\\duke.txt";
         boolean echo = true;
         Scanner sc = new Scanner(System.in);
         DukeList dL = new DukeList();
-        FileAction fA = null;
 
         System.out.println(welcomeMsg);
-        while (echo) {
-            boolean isFileWrong = false;
 
+        FileAction fA = null;
+        boolean isFileWrong = true;
+        while (isFileWrong) {
             try {
-                fA = new FileAction();
+                fA = new FileAction(startingFilePath);
+                dL.setTasks(fA.readTasksFromFile());
+                fA.closeReadFile();
+                isFileWrong = false;
             } catch (IOException e) {
-                isFileWrong = true;
-                System.out.println("\nPlease insert a correct file path to your file!\n");
-                String fileName = sc.nextLine();
-                fA.changeFilePath(fileName);
-                continue;
+                System.out.println("\nPlease insert a correct file path to your file: \n");
+                String newFileName = sc.nextLine();
+                fA.requestChangeFile(newFileName);
             }
+        }
+
+        while (echo) {
             String input = sc.nextLine();
             String[] splitter = input.split(" ");
             Action a = new Action(splitter, dL, fA);
--- src/main/java/FileAction.java
+++ src/main/java/FileAction.java
@@ -1,4 +1,10 @@
+import javax.sound.midi.Soundbank;
 import java.io.*;
+import java.nio.Buffer;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 /**
  * This is an FileAction class that allows the saving of
@@ -10,21 +16,100 @@ import java.io.*;
  */
 
 public class FileAction {
-    protected String filepath = "C:\\NUS\\CS2103\\iP\\data\\duke.txt";
-    protected FileWriter file;
-    protected PrintWriter writer;
+    protected String filepath;
+    protected FileWriter fileW;
+    protected FileReader fileR;
+    protected BufferedWriter writer;
+    protected BufferedReader reader;
 
-    public FileAction() throws IOException {
-        file = new FileWriter(filepath);
-        writer = new PrintWriter(file);
+    public FileAction(String path) throws IOException {
+        fileR = new FileReader(path);
+        reader = new BufferedReader(fileR);
     }
 
-    public void save(String cont) {
-        writer.write("Hello");
-        writer.write(cont);
+    /**
+     * Saves content of Tasks into given file
+     */
+    public void saveFile(String cont) throws IOException {
+        fileW = new FileWriter(filepath);
+        writer = new BufferedWriter(fileW);
+        try {
+            writer.write(cont);
+        } catch (IOException e) {
+            throw new IOException("\nUnexpected error occurred where Tasks cannot be saved in file!\n");
+        }
     }
 
-    public void changeFilePath(String fileName) {
+    /**
+     * Asks for user input to change file read/to be written to what user inputted
+     */
+    public void requestChangeFile(String fileName) {
         filepath = fileName;
+    }
+
+    /**
+     * Closes file after writing into the given file
+     */
+    public void closeWriteFile() throws IOException {
+        try {
+            writer.close();
+        } catch (IOException e) {
+            throw new IOException("\nUnexpected error occurred where file writer cannot be closed!\n");
+        }
+    }
+
+    /**
+     * Closes file after reading the given file
+     */
+    public void closeReadFile() throws IOException {
+        try {
+            reader.close();
+        } catch (IOException e) {
+            throw new IOException("\nUnexpected error occurred where file reader cannot be closed!\n");
+        }
+    }
+
+    /**
+     * Reads tasks saved in the hard drive file provided by the user and
+     * places Task objects into the current DukeList
+     */
+    public ArrayList<Task> readTasksFromFile() {
+        ArrayList<Task> readTasks = new ArrayList<>();
+        String line = "";
+        try {
+            while ((line = reader.readLine()) != null) {
+                String[] arrOfString = line.split(" - ");
+                readTasks.add(taskFromText(arrOfString));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return readTasks;
+    }
+
+    /**
+     * Creates Task based on the text provided in the given text file
+     */
+    public Task taskFromText(String[] line) {
+        String type = line[0];
+        String done = line[1];
+        String desc = line[2];
+        Task newT = null;
+        switch (type) {
+            case "T":
+                newT = new ToDo(desc, Integer.valueOf(done));
+                break;
+            case "D":
+                newT = new Deadline(desc, Integer.valueOf(done), line[3]);
+                break;
+            case "E":
+                newT = new Event(desc, Integer.valueOf(done), line[3]);
+                break;
+            default:
+                System.out.println("Invalid Task type!");
+                break;
+        }
+        System.out.println(newT.toString());
+        return newT;
     }
 }
