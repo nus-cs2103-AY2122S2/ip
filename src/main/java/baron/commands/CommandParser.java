0179a499d4e249a270bebd72128f7221847cfe29
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-01-27 20:37:46 +0800

    Add JavaDoc comments to CommandParser

--- src/main/java/baron/commands/CommandParser.java
+++ src/main/java/baron/commands/CommandParser.java
@@ -2,7 +2,19 @@ package baron.commands;
 
 import baron.exceptions.BaronException;
 
+/**
+ * Assists in the parsing of commands.
+ */
 public class CommandParser {
+
+    /**
+     * Parses the given string into an integer for task index.
+     *
+     * @param arg the argument of {@code String} type.
+     * @return the integer represented by the given string.
+     * @throws BaronException if NumberFormatException is thrown by Integer.parseInt.
+     * @see Integer#parseInt(String) 
+     */
     public static int parseTaskIntArg(String arg) throws BaronException {
         if (arg.isBlank()) {
             throw new BaronException("Please enter the index of the task!");
