c7eb0fbf8d119da7438ddb7ca4cefd32cd032575
mechfrog88 <kelzzin2@gmail.com>
2022-01-27 20:01:47 +0800

    Add A-JavaDoc

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -1,27 +1,48 @@
 package duke;
 
+/**
+ * The abstract class representing tasks.
+ */
 public abstract class Task {
 
     protected final String name;
     protected boolean isDone;
 
+    /**
+     * Constructor of the class.
+     * @param name The name of the task.
+     */
     Task(String name) {
         this.name = name;
         this.isDone = false;
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void mark() {
         isDone = true;
     }
 
+    /**
+     * Unmarks the task.
+     */
     public void unmark() {
         isDone = false;
     }
 
+    /**
+     * Returns the status icon of a task.
+     * @return the status icon of a task.
+     */
     protected String getStatusIcon() {
         return isDone ? "X" : " ";
     }
 
+    /**
+     * Returns the stored data representation.
+     * @return The stored data representation.
+     */
     abstract public String toStore();
 
 }
--- src/main/java/duke/TodoTask.java
+++ src/main/java/duke/TodoTask.java
@@ -1,16 +1,31 @@
 package duke;
 
+/**
+ * Represents the todo task.
+ */
 public class TodoTask extends Task {
 
+    /**
+     * Constructor of the class
+     * @param name Name of the task.
+     */
     public TodoTask(String name) {
         super(name);
     }
 
+    /**
+     * Returns the string representation of the object.
+     * @return The string representation.
+     */
     @Override
     public String toString() {
         return String.format("[T][%s] %s", getStatusIcon(), name);
     }
 
+    /**
+     * Returns the stored data representation of the object.
+     * @return The stored data representation.
+     */
     @Override
     public String toStore() {
         return String.format("T | %d | %s", this.isDone ? 1 : 0, this.name);
