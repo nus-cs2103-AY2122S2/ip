c2aebde4a24a54fd11513bb170850b543570873b
imrajsingh <e0543951@u.nus.edu>
2022-01-27 23:34:54 +0800

    A-JUnit

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -4,7 +4,7 @@ public class Chat {
 
     /**
      * This Chat version is just supposed to take in the commands,
-     * and return them, thats all. Mitran.Di.CHatri.
+     * and return them, thats all. Mitran.Di.CHatri. Ton.
      */
 
     public static String LINE_BREAK = "---------------";
