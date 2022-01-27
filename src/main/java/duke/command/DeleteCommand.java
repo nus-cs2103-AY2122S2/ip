8689f67b98b069ec96a6c3040204d31beac49c93
Lim Bing Sen <bingsenlim0806@gmail.com>
2022-01-27 17:21:34 +0800

    Add JavaDocs

--- src/main/java/duke/command/DeleteCommand.java
+++ src/main/java/duke/command/DeleteCommand.java
@@ -6,15 +6,30 @@ import duke.main.TaskList;
 import duke.main.Ui;
 import duke.task.Task;
 
+/**
+ * Represents a Command which, when executed, deletes a Task from the given TaskList instance.
+ */
 public class DeleteCommand extends Command {
     private static final boolean IS_EXIT = false;
     private int taskIndex;
 
+    /**
+     * Creates a new DeleteCommand instance with the initialized index.
+     *
+     * @param taskIndex Zero based index of the Task to be deleted.
+     */
     public DeleteCommand(int taskIndex) {
         super(IS_EXIT);
         this.taskIndex = taskIndex;
     }
 
+    /**
+     * Deletes the Task object to the TaskList and displays the deleted Task on Ui.
+     * Displays the error message on Ui if the given Task cannot be deleted successfully.
+     *
+     * @param tasks The TaskList instance in which the Task object is deleted from.
+     * @param ui The Ui object used for displaying messages.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         try {
