33950e8fc1335804b51c517188bfc412579ca8fc
Albert Sutiono <albertsutz123@gmail.com>
2022-01-27 21:45:38 +0800

    Implement factory constructor for Deadline, Event, and Todo using List

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class Event extends Task {
     private final String at;
@@ -9,13 +10,23 @@ public class Event extends Task {
     }
 
     public static Event of (String[] description) throws  InvalidArgumentException {
-        if(description.length == 1) {
+        return Event.of(Arrays.asList(description));
+    }
+
+    public static Event of(List<String> description) throws InvalidArgumentException {
+        if(description.size() == 1) {
             throw new InvalidArgumentException();
         }
-        int index = Arrays.asList(description).indexOf("/at");
-        String name = String.join(" ", Arrays.copyOfRange(description, 1, index));
-        String at = String.join(" ", Arrays.copyOfRange(description, index + 1, description.length));
+        int index = description.indexOf("/at");
+        String name = String.join(" ", description.subList(1, index));
+        String at = String.join(" ", description.subList(index + 1, description.size()));
         return new Event(name, at);
+    }
+
+    @Override
+    public String toStorageString() {
+        String status = getStatus()? "X" : ".";
+        return String.format(status + " event " + getName() + " /at " + at);
     }
 
     @Override
