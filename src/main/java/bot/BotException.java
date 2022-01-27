27a4e7a3db0ea82226d8a54f1fe1617f0b6c8b74
Chua Bing Quan <chipneverdies@gmail.com>
2022-01-28 03:58:04 +0800

    Rename prefix to adhere to the global variable naming convention

--- src/main/java/bot/BotException.java
+++ src/main/java/bot/BotException.java
@@ -1,9 +1,9 @@
 package bot;
 
 public class BotException extends Exception {
-    private static final String prefix = "☹ OOPS!!! ";
+    private static final String PREFIX = "☹ OOPS!!! ";
 
     public BotException(String description) {
-        super(BotException.prefix + description);
+        super(BotException.PREFIX + description);
     }
 }
