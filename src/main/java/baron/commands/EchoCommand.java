38cee5c46b8cbacfa76def38b16a0e64dc90926a
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-01-27 20:39:45 +0800

    Add JavaDoc comments to EchoCommand

--- src/main/java/baron/commands/EchoCommand.java
+++ src/main/java/baron/commands/EchoCommand.java
@@ -1,12 +1,25 @@
 package baron.commands;
 
+/**
+ * Represents an echo command that returns the string that was given.
+ */
 public class EchoCommand extends Command {
-    private String echoString = "";
+    private final String echoString;
 
+    /**
+     * Constructs an {@code EchoCommand} object with {code echoString} to be echoed.
+     *
+     * @param echoString string to be echoed.
+     */
     public EchoCommand(String echoString) {
         this.echoString = echoString;
     }
 
+    /**
+     * Returns the string to be echoed that was set in this constructor.
+     *
+     * @return the string to be echoed that was set in this constructor.
+     */
     @Override
     public String execute() {
         return this.echoString;
