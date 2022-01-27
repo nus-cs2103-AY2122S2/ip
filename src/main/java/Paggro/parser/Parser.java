7c35b55aff9dcfeed9967d00184d732ffbf26c3c
justinyjt <e0560296@u.nus.edu>
2022-01-27 16:57:51 +0800

    Add packages, extract Command classes, add JUnit testing

--- src/main/java/Paggro/parser/Parser.java
+++ src/main/java/Paggro/parser/Parser.java
@@ -7,42 +7,39 @@ public class Parser {
         String[] inputArr = input.split(" ", 2);
         String command = inputArr[0];
         if (command.equals("list")) {
-            return new Command(Command.CommandType.LIST);
+            return new ListCommand();
         } else if (command.equals("mark")) {
             try {
                 String parameters = inputArr[1];
-                return new Command(Command.CommandType.MARK, parameters);
+                return new MarkCommand(parameters);
             } catch (ArrayIndexOutOfBoundsException e) { // no parameter given
                 throw new PaggroException("    Really? mark has to be used with a number... =.=");
             }
         } else if (command.equals("unmark")) {
             try {
                 String parameters = inputArr[1];
-                int i = Integer.parseInt(parameters);
-                return new Command(Command.CommandType.UNMARK, parameters);
-            } catch (NumberFormatException e) { // parameter was not a number
-                throw new PaggroException("    Really? Can you input an actual number this time... =.=");
+                return new UnmarkCommand(parameters);
             } catch (ArrayIndexOutOfBoundsException e) { // no parameter given
                 throw new PaggroException("    Really? unmark has to be used with a number... =.=");
             }
         } else if (command.equals("todo")) {
             try {
                 String parameters = inputArr[1];
-                return new Command(Command.CommandType.TODO, parameters);
+                return new ToDoCommand(parameters);
             } catch (ArrayIndexOutOfBoundsException e) { // no description given
                 throw new PaggroException("    Really? The description of a todo cannot be empty... =.=");
             }
         } else if (command.equals("deadline")) {
             try {
                 String parameters = inputArr[1];
-                return new Command(Command.CommandType.DEADLINE, parameters);
+                return new DeadlineCommand(parameters);
             } catch (ArrayIndexOutOfBoundsException e) { // no description given
                 throw new PaggroException("    Really? The description of a deadline cannot be empty... =.=");
             }
         } else if (command.equals("event")) {
             try {
                 String parameters = inputArr[1];
-                return new Command(Command.CommandType.EVENT, parameters);
+                return new EventCommand(parameters);
             } catch (ArrayIndexOutOfBoundsException e) { // no description given
                 throw new PaggroException("    Really? The description of an event cannot be empty... =.=");
             }
@@ -50,7 +47,7 @@ public class Parser {
             try {
                 String parameters = inputArr[1];
                 int i = Integer.parseInt(parameters);
-                return new Command(Command.CommandType.DELETE, parameters);
+                return new DeleteCommand(parameters);
             } catch (NumberFormatException e) { // parameter was not a number
                 throw new PaggroException("    Really? Can you input an actual number this time... =.=");
             } catch (ArrayIndexOutOfBoundsException e) { // no parameter given
@@ -59,12 +56,12 @@ public class Parser {
         } else if (command.equals("listOnDate")) {
             try {
                 String dateString = inputArr[1];
-                return new Command(Command.CommandType.LISTONDATE, dateString);
+                return new ListOnDateCommand(dateString);
             } catch (ArrayIndexOutOfBoundsException e) { // no description given
                 throw new PaggroException("    Really? The date of a listOnDate cannot be empty... =.=");
             }
         } else if (command.equals("bye")) {
-            return new Command(Command.CommandType.BYE);
+            return new ByeCommand();
         } else { // command not recognised
             throw new PaggroException("    Come on... You don't actually expect me to understand that right... =.=");
         }
