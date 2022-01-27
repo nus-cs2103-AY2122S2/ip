e1f503d2fb65b37e73420a45f5d325f6a75344a9
shurvirarora <shurvirarora@gmail.com>
2022-01-27 17:49:49 +0800

    Add 3 test methods for TaskListTest class

--- test/yale/task/TaskListTest.java
+++ test/yale/task/TaskListTest.java
@@ -7,54 +7,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaskListTest {
 
     @Test
-    void addToList() {
-    }
-
-    @Test
-    void listOut() {
-    }
-
-    @Test
-    void exportOut() {
-    }
-
-    @Test
-    void importIn() {
-    }
-
-    @Test
     void getTask() {
-    }
-
-    @Test
-    void getSize() {
+        TaskList list = new TaskList();
+        Task task = new Task("work", false);
+        list.addToList(task);
+        assertEquals(task, list.getTask(0));
     }
 
     @Test
     void deleteTask() {
+        TaskList list = new TaskList();
+        Task task = new Task("work", false);
+        list.addToList(task);
+        list.deleteTask(0);
+        assertEquals(0, (list.getSize()));
     }
 
-    @Test
-    void listFeature() {
-    }
-
-    @Test
-    void deleteFeature() {
-    }
 
     @Test
     void markFeature() {
+        TaskList list = new TaskList();
+        Task task = new Task("work", false);
+        list.addToList(task);
+        list.markFeature("mark 1", list);
+        assertEquals(task.isMarked, list.getTask(0).isMarked);
     }
 
-    @Test
-    void todoFeature() {
-    }
-
-    @Test
-    void deadlineFeature() {
-    }
-
-    @Test
-    void eventFeature() {
-    }
 }
