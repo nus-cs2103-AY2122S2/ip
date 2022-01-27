5a0e1051f00e16a07d47f6a510691ba62aad9d0e
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-01-27 20:37:22 +0800

    Add JavaDoc comments to Command

--- src/main/java/baron/commands/Command.java
+++ src/main/java/baron/commands/Command.java
@@ -1,10 +1,24 @@
 package baron.commands;
 
+/**
+ * Represents a command given by user of Baron application, and it consists of one main
+ * function: execute.
+ */
 public abstract class Command {
 
+    /**
+     * Returns true if this is a bye command.
+     *
+     * @return true if this is a bye command.
+     */
     public boolean isByeCommand() {
         return false;
     }
 
+    /**
+     * Executes the command and returns a string.
+     *
+     * @return the command output of {@code String} type.
+     */
     public abstract String execute();
 }
