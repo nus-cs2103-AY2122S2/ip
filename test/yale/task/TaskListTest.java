4e2147fe578be6b5a8808dc036915cdd1f9addb9
shurvirarora <shurvirarora@gmail.com>
2022-01-27 16:59:38 +0800

    Create test directory Create TaskListTest class

---
+++ test/yale/task/TaskListTest.java
@@ -0,0 +1,60 @@
+package yale.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskListTest {
+
+    @Test
+    void addToList() {
+    }
+
+    @Test
+    void listOut() {
+    }
+
+    @Test
+    void exportOut() {
+    }
+
+    @Test
+    void importIn() {
+    }
+
+    @Test
+    void getTask() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+
+    @Test
+    void listFeature() {
+    }
+
+    @Test
+    void deleteFeature() {
+    }
+
+    @Test
+    void markFeature() {
+    }
+
+    @Test
+    void todoFeature() {
+    }
+
+    @Test
+    void deadlineFeature() {
+    }
+
+    @Test
+    void eventFeature() {
+    }
+}
