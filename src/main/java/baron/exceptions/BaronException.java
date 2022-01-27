caff844066717ebbb1b782672f531f2d7f29de17
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-01-27 20:36:51 +0800

    Add JavaDoc comments to BaronException

--- src/main/java/baron/exceptions/BaronException.java
+++ src/main/java/baron/exceptions/BaronException.java
@@ -1,10 +1,22 @@
 package baron.exceptions;
 
+/**
+ * Deals with all the exceptions in Baron application.
+ */
 public class BaronException extends Exception {
+
+    /**
+     * Constructs a {@code BaronException} with the given message.
+     */
     public BaronException(String message) {
         super(message);
     }
 
+    /**
+     * Returns the string representation of {@code BaronException}.
+     *
+     * @return the string representation of {@code BaronException}.
+     */
     public String toString() {
         return "☹ OOPS!!! " + getMessage();
     }
