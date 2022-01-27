062bed249073549b130239f04024aab047a0c83d
shurvirarora <shurvirarora@gmail.com>
2022-01-27 17:57:03 +0800

    Create TaskTest class

---
+++ test/yale/task/TaskTest.java
@@ -0,0 +1,28 @@
+package yale.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @Test
+    void getStatusIcon() {
+    }
+
+    @Test
+    void markTask() {
+    }
+
+    @Test
+    void unmarkTask() {
+    }
+
+    @Test
+    void export() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
