b8bbf322679ac20231f33b67d4bd6bae666c6f98
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-01-27 20:38:52 +0800

    Add JavaDoc comments to DeleteTaskCommand

--- src/main/java/baron/commands/DeleteTaskCommand.java
+++ src/main/java/baron/commands/DeleteTaskCommand.java
@@ -4,23 +4,43 @@ import baron.exceptions.BaronException;
 import baron.message.Message;
 import baron.tasks.Task;
 import baron.tasks.TaskManager;
+import baron.tasks.TaskType;
 import baron.util.Storage;
 
+/**
+ * Represents a delete task command.
+ */
 public class DeleteTaskCommand extends Command {
     private final TaskManager taskManager;
-    private final String commandArgs;
+    private final String commandArg;
     private final Storage storage;
 
-    public DeleteTaskCommand(TaskManager taskManager, Storage storage, String commandArgs) {
+    /**
+     * Constructs a delete task command with the specified {@code TaskManager}, {@code Storage}
+     * and command argument.
+     *
+     * @param taskManager the {@code TaskManager} for the command execution.
+     * @param storage the {@code Storage} for the command execution.
+     * @param commandArg the command argument to be parsed.
+     * @see TaskManager
+     * @see Storage
+     */
+    public DeleteTaskCommand(TaskManager taskManager, Storage storage, String commandArg) {
         this.taskManager = taskManager;
         this.storage = storage;
-        this.commandArgs = commandArgs;
+        this.commandArg = commandArg;
     }
 
+    /**
+     * Execute the delete task command by deleting the task from the specified {@code TaskManager},
+     * and updating the {@code Storage} if necessary.
+     *
+     * @return the delete task command output.
+     */
     @Override
     public String execute() {
         try {
-            int index = CommandParser.parseTaskIntArg(this.commandArgs);
+            int index = CommandParser.parseTaskIntArg(this.commandArg);
             Task deletedTask = this.taskManager.deleteTask(index);
             try {
                 this.storage.save(this.taskManager.getAllTasks());
