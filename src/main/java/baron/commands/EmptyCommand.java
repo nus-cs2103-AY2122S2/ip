a919b020be27cebd273193e6b835be35451668f9
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-01-27 20:40:10 +0800

    Add JavaDoc comments to EmptyCommand

--- src/main/java/baron/commands/EmptyCommand.java
+++ src/main/java/baron/commands/EmptyCommand.java
@@ -2,7 +2,16 @@ package baron.commands;
 
 import baron.exceptions.BaronException;
 
+/**
+ * Represents an empty command.
+ */
 public class EmptyCommand extends Command {
+
+    /**
+     * Returns the prompt to request a command.
+     *
+     * @return the prompt to request a command.
+     */
     @Override
     public String execute() {
         return (new BaronException("Please enter a command!")).toString();
