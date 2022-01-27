2b302574fcaef5efba251d407f9ae1f9b1faaef3
dionegoh <e0439283@u.nus.edu>
2022-01-28 00:46:28 +0800

    Reformat code to follow coding standard

--- src/main/java/pikabot/command/DeadlineCommand.java
+++ src/main/java/pikabot/command/DeadlineCommand.java
@@ -40,8 +40,10 @@ public class DeadlineCommand extends Command {
             taskList.add(currDeadline);
             Ui.indicateAddedTask(currDeadline, taskList);
             storage.appendToFile(currDeadline);
+
         } catch (DeadlineException | IOException e) {
             Ui.printExceptionMessage(e);
+
         } catch (DateTimeParseException e) {
             Ui.printExceptionCustomisedMessage("Invalid deadline! Deadline has to " +
                 "be a valid date in numerical format YYYY-MM-DD.");
