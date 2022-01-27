a004b45a4c9f45ad73f5deec8573e4aeb42cce76
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-01-27 20:42:15 +0800

    Add JavaDoc comments to Message

--- src/main/java/baron/message/Message.java
+++ src/main/java/baron/message/Message.java
@@ -2,6 +2,9 @@ package baron.message;
 
 import baron.tasks.TaskType;
 
+/**
+ * Contains and generates messages to be printed out as the output/partial output of a command.
+ */
 public class Message {
     public static final String MESSAGE_MARK_SUCCESS = "Nice! I've marked this task as done:\n  ";
     public static final String MESSAGE_UNMARK_SUCCESS = "OK, I've marked this task as not done yet:\n  ";
@@ -15,6 +18,12 @@ public class Message {
     public static final String MESSAGE_FILE_WRITE_FAIL = "Data file cannot be written to.";
     public static final String MESSAGE_DATE_TIME_FORMAT_INVALID = "The date/time should be in the following format: d/M/yyyy HH:mm";
 
+    /**
+     * Generates the message to show the number of tasks.
+     *
+     * @param noOfTasks the number of tasks to be display.
+     * @return the message that shows the number of tasks.
+     */
     public static String generateNoOfTasksMessage(int noOfTasks) {
         if (noOfTasks == 0) {
             return Message.MESSAGE_NO_TASK;
@@ -24,7 +33,23 @@ public class Message {
         return String.format("Now you have %d tasks in your list.", noOfTasks);
     }
 
+    /**
+     * Generates the message to warn that the description of a {@code TaskType} cannot be empty.
+     *
+     * @param taskType the type of task.
+     * @return the message that warns that the description of a {@code TaskType} cannot be empty.
+     */
     public static String generateEmptyDescMessage(TaskType taskType) {
         return "The description of a " + taskType.getCommand() + " cannot be empty.";
+    }
+
+    /**
+     * Generates the message to warn that the task index is out of bound and thus invalid.
+     *
+     * @param taskCount the number of tasks.
+     * @return the message that warns that the task index is out of bound and thus invalid.
+     */
+    public static String generateTaskIndexOutOfBoundMessage(int taskCount) {
+        return "The task index is invalid, only accepts 1 to " + taskCount + ".";
     }
 }
