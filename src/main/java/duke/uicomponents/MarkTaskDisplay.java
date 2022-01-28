da959b3e8bff49cba835d257047a40a88d4431ed
Denniszedead <dennisng2255@gmail.com>
2022-01-29 01:57:52 +0800

    Move all source files to duke folder

--- src/main/java/duke/uicomponents/MarkTaskDisplay.java
+++ src/main/java/duke/uicomponents/MarkTaskDisplay.java
@@ -2,7 +2,7 @@ package duke.uicomponents;
 
 public class MarkTaskDisplay{
     public void run(String task) {
-        System.out.println("Alright, I've marked the task: ");
+        System.out.println("Alright, I've marked the duke.task: ");
         System.out.println(task);
         System.out.println("Good job by the way!");
     }
--- src/main/java/duke/uicomponents/UnmarkedTaskDisplay.java
+++ src/main/java/duke/uicomponents/UnmarkedTaskDisplay.java
@@ -2,7 +2,7 @@ package duke.uicomponents;
 
 public class UnmarkedTaskDisplay{
     public void run(String task) {
-        System.out.println("Alright, I've unmarked the task: ");
+        System.out.println("Alright, I've unmarked the duke.task: ");
         System.out.println(task);
         System.out.println("Hope you complete it soon!");
     }
