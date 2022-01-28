24820cd8fb033e7b1cef20ed11ce9e38b8e2a8b0
Ryan Chang Jia Jie <>
2022-01-28 22:58:28 +0800

    Copy Deadline.java over to L6

---
+++ Siri/L6/Deadline.java
@@ -0,0 +1,16 @@
+public class Deadline extends Task {
+
+    String deadline;
+
+    public Deadline(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+        //find a way to scan deadline after description entered by user, maybe use the "/" as delimiter for the scanner?
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + "[" + this.getStatusIcon() + "]" + this.description + "(by:" + this.deadline + ")";
+    }
+
+}
