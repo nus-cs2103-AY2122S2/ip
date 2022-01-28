96217debf288ee15065d784776e11aac0bd30df3
Zechary <E0530207@u.nus.edu>
2022-01-28 11:22:47 +0800

    Add Date functionality to Duke
    Currently, dates are stored merely as strings in each Task. This means
    that the dates cannot be manipulated in a meaningful way and prevents
    any further improvement to the date functionality (such as filtering
    tasks by date)
    
    To make the date formatting more useful, the date attribute now utilizes
    the LocalDate library to parse dates. When a date is input, the Task()
    constructor will attempt to parse it into a LocalDate format. If the
    date cannot be parsed, it will be treated as a simple string.
    
    Lets
    * Use the LocalDate library to allow dates to be utilized more
      functionally

--- src/main/java/duke/duke/info/task/Event.java
+++ src/main/java/duke/duke/info/task/Event.java
@@ -2,21 +2,17 @@ package duke.info.task;
 
 public class Event extends Task {
 
-    private final String date;
-
     public Event(String event, String date, boolean isComplete) {
-        super(event, "event", isComplete);
-        this.date = date;
+        super(event, "event", isComplete, date);
     }
 
     @Override
     public String saveFormat() {
-        return super.saveFormat() + "|" + this.date;
+        return super.saveFormat() + "|" + this.getDateString();
     }
 
     @Override
     public String toString() {
-        System.out.println(date);
-        return String.format("[E]%s (at: %s)", super.toString(), this.date);
+        return String.format("[E]%s (at: %s)", super.toString(), this.getDateString());
     }
 }
