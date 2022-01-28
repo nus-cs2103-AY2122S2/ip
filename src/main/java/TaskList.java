35e4ff9859555d0e70a813cc08e60319ddb952b0
Oddcorner <90017908+Oddcorner@users.noreply.github.com>
2022-01-28 12:04:22 +0800

    Add TaskList.java

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private List<Task> taskList = new ArrayList<>();
+
+    public TaskList() {
+        taskList = new ArrayList<>();
+    }
+    public TaskList(List<Task> list) {
+        taskList = list;
+    }
+
+    public boolean add(Task task){
+        return taskList.add(task);
+    }
+
+    public Task remove(int index){
+        if(index >= 0 && index < taskList.size()) {
+           return taskList.remove(index);
+        }
+        return null;
+    }
+
+    public List<Task> list(){
+        List<Task> copy = new ArrayList<>();
+        for(Task task : taskList){
+            copy.add(task);
+        }
+        return copy;
+    }
+
+    public int size(){
+        return taskList.size();
+    }
+
+    public Task mark(int index){
+        Task task = taskList.get(index);
+        task.mark();
+        return task;
+    }
+
+    public Task unmark(int index){
+        Task task = taskList.get(index);
+        task.unmark();
+        return task;
+    }
+}
