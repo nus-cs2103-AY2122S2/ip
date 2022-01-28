151461d6eb4426419d1371826317d50ae9f6d5e6
Ria Khaitan <ria@u.nus.edu>
2022-01-28 16:16:59 +0800

    add level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        String line = "*************************************************************************";
+        String lineTwo = "|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|";
+        System.out.println(line);
+        System.out.println("Hi! I'm Halloumi ^_^");
+        System.out.println("What do you need help with today?");
+        System.out.println(line);
+        do {
+            String text = sc.nextLine();
+            if(text.equals("bye")) {
+                num = 1;
+                System.out.println("See you soon! Have a good day ^_^");
+                System.out.println(line);
+            }
+            else {
+                System.out.println(lineTwo);
+                System.out.println(text);
+                System.out.println(lineTwo);
+            }
+        }
+        while(num == 0);
     }
 }
