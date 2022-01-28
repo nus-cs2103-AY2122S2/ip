d88fdaf04e5b85412c1f42c576152b307e079b72
gigituang <72374000+gigituang@users.noreply.github.com>
2022-01-28 17:34:11 +0800

    level-7 completed

---
+++ src/main/Command.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public abstract class Command {
+    public abstract void execute(TaskList taskList, Ui ui, Storage storage) throws Exception_handler, IOException;
+    public abstract boolean isExit();
+}
+
+
---
+++ src/main/DeleteCommand.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+public class DeleteCommand extends Command {
+    private int index;
+    public DeleteCommand(int index) {
+        this.index = index;
+    }
+    @Override
+    public void execute(TaskList taskList, Ui ui, Storage storage) throws Exception_handler, IOException {
+        if (index < 0 || index >= taskList.getSize()) {
+            throw new Exception_handler("Index out of range")
+        }
+        Task temp = taskList.removeTask(index);
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(temp);
+        System.out.println("Now you have " + taskList.getSize() + " tasks in the list.");
+        storage.writeToFile(taskList.getListOfTasks());
+    }
+
+    public boolean isExit() {
+        return false;
+    }
+}
