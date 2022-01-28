bc21466729a6abfc5dce2eb8fd943e07b867408a
gigituang <72374000+gigituang@users.noreply.github.com>
2022-01-28 15:44:23 +0800

    Level-8 updated

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.List;
@@ -6,8 +8,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
 
 public class Duke {
+    public static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -19,9 +23,9 @@ public class Duke {
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
 
-        try {
-            List<Task> taskList = importTask();
-            while (sc.hasNext()) {
+        while (sc.hasNext()) {
+            try {
+                List<Task> taskList = importTask();
                 String[] s = sc.nextLine().split(" ");
                 if (s[0].equals("bye")) {
                     if (s.length > 1) {
@@ -105,7 +109,8 @@ public class Duke {
                     for (int k = index; k < s.length; k++) {
                         time = time + s[k] + " ";
                     }
-                    Deadline deadline = new Deadline(str, time);
+                    LocalDateTime dateTime = LocalDateTime.parse(time.trim(), dtf);
+                    Deadline deadline = new Deadline(str, dateTime);
                     taskList.add(deadline);
                     System.out.println("Got it. I've added this task: ");
                     System.out.println(deadline);
@@ -127,15 +132,15 @@ public class Duke {
                 } else {
                     throw new Exception_handler("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-            }
-        }
-            catch(Exception_handler e){
+            } catch (Exception_handler e) {
                 System.out.println(e);
-            }
-            catch(IOException ie) {
+            } catch (IOException ie) {
                 System.out.println("IOException");
+            } catch (DateTimeParseException dtpe) {
+                System.out.println("Please enter deadline in dd/mm/yyyy HHmm format");
             }
         }
+    }
         public static List<Task> importTask() throws FileNotFoundException {
             List<Task> tempList = new ArrayList<>();
             File temp = new File("/Users/gigi/Desktop/CS2103T/duke.txt");
@@ -149,7 +154,7 @@ public class Duke {
                     if(token[0].equals("T")){
                         tempList.add(new ToDos(token[2], token[1]));
                     } else if(token[0].equals("D")) {
-                        tempList.add(new Deadline(token[2], token[1], token[3]));
+                        tempList.add(new Deadline(token[2], token[1], LocalDateTime.parse(token[3],dtf)));
                     } else if(token[0].equals("E")){
                         tempList.add(new Event(token[2], token[1], token[3]));
                     } else{
@@ -166,7 +171,7 @@ public class Duke {
             }
             return tempList;
         }
-        public static void writeToFile(List<Task> tasks) throws IOException {
+        public static void writeToFile(List<Task> tasks) throws IOException, FileNotFoundException {
             File file = new File("/Users/gigi/Desktop/CS2103T/duke.txt");
             PrintWriter pw = new PrintWriter(file);
             String output = "";
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,9 +1,10 @@
-import java.util.Arrays;
+import java.time.format.DateTimeFormatter;
 
 public class Task {
     protected String d;
     protected String type;
     protected boolean isDone;
+    protected final DateTimeFormatter OUT_DTF = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
 
     public Task(String d) {
         this.d = d;
