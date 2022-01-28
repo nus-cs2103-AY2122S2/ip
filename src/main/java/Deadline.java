1e865f653ebbee2421f00296e2b8980af0b8dd03
bloodofme <awesome.dragon.wizard@gmail.com>
2022-01-28 21:48:32 +0800

    complete level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,37 @@
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
 public class Deadline extends Task {
-    String time;
     String deadline;
+    LocalDate time;
+
+    DateTimeFormatterBuilder dateTimeFormatterBuilder = new DateTimeFormatterBuilder()
+            .append(DateTimeFormatter.ofPattern(
+                    "" +
+                            "[yyyy-MM-dd HH:mm:ss]" +
+                            "[yyyy-MM-dd]" +
+                            "[yyyy/MM/dd]" +
+                            "[yyyy-MM-dd HH:mm a]"
+            ));
+
+    DateTimeFormatter dateTimeFormatter = dateTimeFormatterBuilder.toFormatter();
 
     public Deadline(String deadline, String time) {
         super(deadline);
         this.deadline = deadline;
-        this.time = time;
+
+        try {
+            this.time = LocalDate.parse(time, dateTimeFormatter);
+        } catch (DateTimeException de) {
+            System.out.println("sorry, this is not a valid time");
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + time + ")";
+        String temp = time.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        return "[D]" + super.toString() + "(by: " + temp + ")";
     }
 }
