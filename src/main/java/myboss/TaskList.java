2d41fc4ef1983c907e2391af9c46007402e19557
Seah Zhi Xuan <e0659847@u.nus.edu>
2022-01-28 23:37:31 +0800

    Add JavaDoc comments

--- src/main/java/myboss/TaskList.java
+++ src/main/java/myboss/TaskList.java
@@ -2,32 +2,67 @@ package myboss;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a list of tasks.
+ */
 public class TaskList {
     public ArrayList<Task> taskList;
     public static int size = 0;
 
+    /**
+     * Creates a TaskList Object with the specified taskList.
+     *
+     * @param taskList list of tasks.
+     */
     public TaskList(ArrayList<Task> taskList) {
         this.taskList = taskList;
         this.size = taskList.size();
     }
 
+    /**
+     * Returns the list of tasks.
+     *
+     * @return list of tasks.
+     */
     public ArrayList<Task> getTaskList() {
         return taskList;
     }
 
+    /**
+     * Returns the size of the task list.
+     *
+     * @return size of task list
+     */
     public static int getSize() {
         return size;
     }
 
+    /**
+     * Returns the task at the specified index
+     *
+     * @param index index of task.
+     * @return task at specified index.
+     */
     public Task get(int index) {
         return taskList.get(index);
     }
 
+    /**
+     * Adds the specified task to the task list and increments the size.
+     *
+     * @param task task to be added to the task list.
+     */
     public void addTask(Task task) {
         taskList.add(task);
         size++;
     }
 
+    /**
+     * Returns the deleted task at specified index.
+     *
+     * @param index index of task to be deleted.
+     * @return deleted task.
+     */
     public Task deleteTask(int index) {
         size--;
         return taskList.remove(index);
--- src/main/java/myboss/Ui.java
+++ src/main/java/myboss/Ui.java
@@ -4,29 +4,51 @@ import java.util.Scanner;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a user interface.
+ */
 public class Ui {
     static String indent = "    ";
     private Scanner scanner;
 
+    /**
+     * Creates a Ui Object with a scanner.
+     */
     public Ui() {
         this.scanner = new Scanner(System.in);
     }
 
+    /**
+     * Outputs the greeting message of the chatbot.
+     */
     public void outputGreeting() {
         myBossOutput(" Hello! I'm YourBoss.\n" +
                 indent +
                 " What can you do for me?");
     }
 
+    /**
+     * Outputs the farewell message of the chatbot.
+     */
     public void outputFarewell() {
         myBossOutput(" Bye. Hope I never see you again!");
         scanner.close();
     }
 
+    /**
+     * Returns the next line of user input.
+     *
+     * @return next line of user input.
+     */
     public String getUserCmd() {
         return scanner.nextLine();
     }
 
+    /**
+     * Outputs the given task list.
+     *
+     * @param taskList the specified list of tasks.
+     */
     public void outputTaskList(ArrayList<Task> taskList) {
         StringBuilder tempOut = new StringBuilder("");
         for (int i = 0; i < taskList.size(); i++) {
@@ -38,32 +60,57 @@ public class Ui {
         hLineBreak();
     }
 
+    /**
+     * Output the given task as marked.
+     *
+     * @param task the specified task marked as done.
+     */
     public void outputMarked(Task task) {
         myBossOutput(task.markAsDone(true));
     }
 
+    /**
+     * Output the given task as unmarked.
+     *
+     * @param task the specified task marked as not done.
+     */
     public void outputUnmarked(Task task) {
         myBossOutput(task.markAsDone(false));
     }
 
-
-
-
+    /**
+     * Outputs a formatted line break.
+     */
     static void hLineBreak() {
         System.out.println(indent+"____________________________________________________________");
     }
 
+    /**
+     * Outputs the given input with the predefined indent.
+     *
+     * @param input input to print.
+     */
     static void printlnWithIndent(String input) {
         System.out.print(indent);
         System.out.println(input);
     }
 
+    /**
+     * Output the given string in a standardized format.
+     *
+     * @param output string to be outputted.
+     */
     static void myBossOutput(String output) {
         hLineBreak();
         printlnWithIndent(output);
         hLineBreak();
     }
 
+    /**
+     * Outputs the predefined output when adding a task.
+     *
+     * @param task task added.
+     */
     void addTaskOutput(Task task) {
         hLineBreak();
         printlnWithIndent(" Got it. I've added this task:");
@@ -72,6 +119,11 @@ public class Ui {
         hLineBreak();
     }
 
+    /**
+     * Outputs the predefined output when deleting a task.
+     *
+     * @param task task deleted.
+     */
     void outputDeleteTask(Task task) {
         hLineBreak();
         printlnWithIndent(" Noted. I've removed this task:");
