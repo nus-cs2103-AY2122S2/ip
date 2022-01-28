c5e2b2b9b065886b96f43d0597cea38d4166b3bb
janald99 <e0543657@u.nus.edu>
2022-01-28 14:46:35 +0800

    Clean up Duke class and Update Expected.txt

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,55 +2,36 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    private static final String botName = "Feline";
-    private static ArrayList<Task> list = new ArrayList<>();
+    private static final String BOT_NAME = "Feline";
+    private static ArrayList<Task> tasks = new ArrayList<>();
     //private static final String[] commands = {"bye", "mark", "unmark", "list", "todo", "deadline", "event"};
 
-    private static void performAction(String action, String input) {
-        switch (action) {
-         case "mark":
-                Duke.mark(input);
-                break;
-         case "unmark":
-                Duke.unmark(input);
-                break;
-         case "list":
-                Duke.printList();
-                break;
-         case "delete":
-                Duke.delete(input);
-                break;
-         default:
-                Duke.addTask(input);
-        }
-    }
     private static void greet() {
-        System.out.println(String.format("Yoooo! My name is %s!\nHow can i help you bro?\n", botName));
+        System.out.println(String.format("Yoooo! My name is %s!\nHow can i help you bro?\n", BOT_NAME));
     }
     private static void farewell() {
         System.out.println("See you next time!");
     }
 
     private static void printTaskCount() {
-        System.out.println(String.format("Now you have %d task(s) in your list.", list.size()));
+        System.out.println(String.format("Now you have %d task(s) in your list.", tasks.size()));
     }
-    private static void taskAdded() {
+    private static void printTaskAdded() {
         System.out.println("Got it. I've added this task:");
-        System.out.println(list.get(list.size() - 1).toString());
+        System.out.println(tasks.get(tasks.size() - 1).toString());
         printTaskCount();
     }
 
-    private static void addTask(String str) {    //adds task to list
-        String action = Duke.getFirstWord(str);
+    private static void addTask(String action, String input) {    //adds task to list
         switch (action) {
         case "todo":
                 try {
-                    String[] todoArr = str.split(" ", 2);
+                    String[] todoArr = input.split("\\s", 2);
                     if (todoArr.length <= 1) {
                         throw new InvalidArgumentException("todo.. todo what?");
                     }
-                    list.add(new Todo(todoArr[1].trim()));
-                    taskAdded();
+                    tasks.add(new Todo(todoArr[1].trim()));
+                    printTaskAdded();
                 } catch (InvalidArgumentException e) {
                     System.out.println(e.getMessage());
                 } finally {
@@ -59,8 +40,8 @@ public class Duke {
         case "deadline":
                 //deadline do hw /by no idea :-p
                 try {
-                    String[] deadlineArr = str.split("/by", 2);
-                    String[] deadlineSplit = deadlineArr[0].split(" ", 2);
+                    String[] deadlineArr = input.split("/by", 2);
+                    String[] deadlineSplit = deadlineArr[0].split("\\s", 2);
                     if (deadlineSplit.length <= 1) {    // no description
                         throw new InvalidArgumentException("Sorry but.. deadline of what??");
                     }
@@ -68,8 +49,8 @@ public class Duke {
                         throw new InvalidArgumentException("By when?? ..");
                     }
                     String description = deadlineSplit[1].trim();
-                    list.add(new Deadline(description, deadlineArr[1].trim()));
-                    taskAdded();
+                    tasks.add(new Deadline(description, deadlineArr[1].trim()));
+                    printTaskAdded();
                 } catch (InvalidArgumentException e) {
                     System.out.println(e.getMessage());
                 } finally {
@@ -78,8 +59,8 @@ public class Duke {
         case "event":
                 //event project meeting /at Mon 2-4pm
                 try {
-                    String[] eventArr = str.split("/at", 2);
-                    String[] eventSplit = eventArr[0].split(" ", 2);
+                    String[] eventArr = input.split("/at", 2);
+                    String[] eventSplit = eventArr[0].split("\\s", 2);
                     if (eventSplit.length <= 1) {
                         throw new InvalidArgumentException("What event? No event stated.");
                     }
@@ -87,20 +68,12 @@ public class Duke {
                         throw new InvalidArgumentException("At where? Please specify again");
                     }
                     String description = eventSplit[1].trim();
-                    list.add(new Event(description, eventArr[1].trim()));
-                    taskAdded();
+                    tasks.add(new Event(description, eventArr[1].trim()));
+                    printTaskAdded();
                 } catch (InvalidArgumentException e){
                     System.out.println(e.getMessage());
                 } finally {
                     break;
-                }
-            default:
-                try {
-                    throw new UnknownException(String.format("Sorry.. I don't" +
-                            " know what u talking bout bro. What does \"%s\" mean?", str));
-                }
-                catch (UnknownException e) {
-                System.out.println(e.getMessage());
                 }
         }
 
@@ -108,90 +81,109 @@ public class Duke {
 
     private static String getTaskStatement(int i) {
         //[X] read book
-        return list.get(i).toString();
+        return tasks.get(i).toString();
     }
     private static void printList() {
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < tasks.size(); i++) {
             System.out.println(i + 1 + "." + getTaskStatement(i));
         }
     }
 
-    private static String[] parseInput(String input) {
-        return input.split(" ");
-    }
-    private static String getFirstWord(String input) {
-        String[] arr = parseInput(input);
-        return arr[0].toLowerCase();
-    }
-
-    private static void mark(String input) {
+    private static void mark(String[] inputWords) {
         try {
-            String[] words = input.split(" ", 2);
-            // the second word is expected to be a number for now
-            if (words.length <= 1) {
-                throw new InvalidArgumentException("Excuse me, but mark what?");
+            if (!(inputWords.length == 2)) { //e.g mark 2, cannot be mark 2 2 or just mark
+                throw new InvalidArgumentException("Please specify what to mark clearly.");
             }
-            int taskNumber = Integer.parseInt(words[1]);
-            if (taskNumber > list.size() || taskNumber <= 0) {
+            int taskNumber = Integer.parseInt(inputWords[1]);
+            if (taskNumber > tasks.size() || taskNumber <= 0) {
                 throw new OutOfBoundsException(String.format("The task %d does not exist!", taskNumber));
             }
-            list.get(taskNumber - 1).markAsDone();
+            tasks.get(taskNumber - 1).markAsDone();
             System.out.println("Nice! I've marked this task as done:");
             System.out.println(Duke.getTaskStatement(taskNumber - 1));
-        } catch (OutOfBoundsException e) {
-            System.out.println(e.getMessage());
-        } catch (InvalidArgumentException e) {
+        } catch (OutOfBoundsException | InvalidArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
-
-    private static void unmark(String input) {
+    private static void unmark(String[] inputWords) {
         try {
-            String[] words = input.split(" ", 2);
-            if (words.length <= 1) {
-                throw new InvalidArgumentException("Excuse me, but mark what?");
+            if (!(inputWords.length == 2)) {
+                throw new InvalidArgumentException("Please specify what to unmark clearly.");
             }
-            int taskNumber = Integer.parseInt(words[1]);
-            if (taskNumber > list.size() || taskNumber <= 0) {
+            int taskNumber = Integer.parseInt(inputWords[1]);
+            if (taskNumber > tasks.size() || taskNumber <= 0) {
                 throw new OutOfBoundsException(String.format("The task %d does not exist!", taskNumber));
             }
-            list.get(taskNumber - 1).markAsUndone();
+            tasks.get(taskNumber - 1).markAsUndone();
             System.out.println("OK, I've marked this task as not done yet:");
             System.out.println(getTaskStatement(taskNumber - 1));
         } catch (OutOfBoundsException | InvalidArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
-    private static void delete(String input) {
+    private static void delete(String[] inputWords) {
         try {
-            String[] words = input.split(" ", 2);
-            if (words.length <= 1) {
-                throw new InvalidArgumentException("Excuse me, but delete what?");
+            if (!(inputWords.length == 2)) {
+                throw new InvalidArgumentException("Please specify what to delete clearly.");
             }
-            int taskNumber = Integer.parseInt(words[1]);
-            if (taskNumber > list.size() || taskNumber <= 0) {
+            int taskNumber = Integer.parseInt(inputWords[1]);
+            if (taskNumber > tasks.size() || taskNumber <= 0) {
                 throw new OutOfBoundsException(String.format("Cannot delete" +
                         " as task %d does not exist!", taskNumber));
             }
             System.out.println("Noted. I've removed this task:");
             System.out.println(getTaskStatement(taskNumber - 1));
-            list.remove(taskNumber - 1);
+            tasks.remove(taskNumber - 1);
             printTaskCount();
         } catch (OutOfBoundsException | InvalidArgumentException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    private static String getCommands() {
+        return "list, todo, deadline (using /by), event (using /at), mark, unmark, delete";
+    }
+
     public static void main(String[] args) {
         Duke.greet();
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        while (!input.equals("bye")) {  //terminates system when user says bye
-            String action = Duke.getFirstWord(input);
-            Duke.performAction(action, input);
-            System.out.println("\n");
-            input = sc.nextLine();
+        while (sc.hasNext()) {
+            try {
+                String input = sc.nextLine();
+                String[] inputWords = input.split("\\s", 2);
+                switch (input) {
+                    case "bye":
+                        Duke.farewell();
+                        return;
+                    case "list":
+                        printList();
+                        break;
+                    default: // not the one word commands that we have. check further commands, else throw exception
+                        String firstWord = inputWords[0];
+                        switch (firstWord) {
+                            case "mark":
+                                Duke.mark(inputWords);
+                                break;
+                            case "unmark":
+                                Duke.unmark(inputWords);
+                                break;
+                            case "delete":
+                                Duke.delete(inputWords);
+                                break;
+                            case "todo":
+                            case "deadline":
+                            case "event":
+                                Duke.addTask(firstWord, input);
+                                break;
+                            default:
+                                throw new UnknownException("Unknown command! the follow are the commands: "
+                                        + Duke.getCommands()); //can show help commands
+                        }
+                }
+            } catch (UnknownException e) {
+                System.out.println(e.getMessage());
+            }
         }
         sc.close();
         Duke.farewell();
