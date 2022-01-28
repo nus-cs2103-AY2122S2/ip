74d160030ca3ac4f789fb4be8178cd24ba3b862f
Oddcorner <90017908+Oddcorner@users.noreply.github.com>
2022-01-28 12:00:51 +0800

    Add Parser.java
    This is a new class that handles parsing String input from users and
    returns the appropriate Command object.

---
+++ src/main/java/Parser.java
@@ -0,0 +1,57 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Parser {
+
+    public static Command parse(String input) throws ArrayIndexOutOfBoundsException,
+        NumberFormatException, DateTimeParseException {
+        input = input.trim();
+        // Ignore empty input
+        if (input.equals("")) {
+            return new Command("null");
+        }
+
+        // Check for keywords
+        String[] strArr = input.split(" ", 2);
+        String keyword = strArr[0];
+
+        Command command = new Command("default");
+        switch (keyword) {
+        case "bye":
+            command = new ExitCommand();
+            break;
+        case "list":
+            command = new ListCommand();
+            break;
+        case "mark":
+            int index = Integer.parseInt(strArr[1]) - 1;
+            command = new MarkCommand(index);
+            break;
+        case "unmark":
+            index = Integer.parseInt(strArr[1]) - 1;
+            command = new UnmarkCommand(index);
+            break;
+        case "delete":
+            index = Integer.parseInt(strArr[1]) - 1;
+            command = new DeleteCommand(index);
+            break;
+        case "todo":
+            command = new AddCommand("todo", strArr[1]);
+            break;
+        case "event":
+            String[] desc = strArr[1].split("/at");
+            LocalDate date = LocalDate.parse(desc[1].trim().replace("/", "-"));
+            command = new AddCommand("event", desc[0], date);
+            break;
+        case "deadline":
+            desc = strArr[1].split("/by");
+            date = LocalDate.parse(desc[1].trim().replace("/", "-"));
+            command = new AddCommand("deadline", desc[0], date);
+            break;
+        default:
+            command = new UnknownCommand();
+            break;
+        }
+        return command;
+    }
+}
