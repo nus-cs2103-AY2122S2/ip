8dbf61de9aaf463583f31077cd2a980962b98d2d
charmainehly <65525578+charmainehly@users.noreply.github.com>
2022-01-30 02:00:14 +0800

    Add JUnit tests to test behaviour of toString()
    Currently no JUnit tests to test for correct behaviour of methods.
    
    Errors in toString() methods can lead to the wrong output being printed.
    
    Let's add separate Test classes to check if toString() prints the
    correct output for a specific instance of the class object.

---
+++ src/test/java/DeadlineTest.java
@@ -0,0 +1,13 @@
+import duke.Deadline;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+
+    @Test
+    public void testStringConversion() {
+        assertEquals("[D][X] do this - by: Jan 01 2020",
+                new Deadline("do this", true, "01-01-2020").toString());
+    }
+}
---
+++ src/test/java/EventTest.java
@@ -0,0 +1,13 @@
+import duke.Event;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventTest {
+
+    @Test
+    public void testStringConversion() {
+        assertEquals("[E][X] attend this - at: Jan 01 2020",
+                new Event("attend this", true, "01-01-2020").toString());
+    }
+}
