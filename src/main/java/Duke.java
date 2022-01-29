0688e256092161315bc78b543e222ee01191615c
Edward Alvin <alvinpinter02@gmail.com>
2022-01-29 13:50:00 +0800

    Implement word list into Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     static final String BYE = "bye";
+    static final String LIST = "list";
+    static final ArrayList<String> textHistory = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -11,25 +16,55 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String input = sc.nextLine();
 
         replyWelcomeMessage();
-        while (!isBye(input)) {
-            echo(input);
+        String input = "";
+        while (true) {
             input = sc.nextLine();
+            if (isList(input)) {
+                printHistory();
+                continue;
+            }
+
+            if (isBye(input)) {
+                break;
+            }
+
+            echo(input);
+            storeWord(input);
         }
         replyBye();
     }
 
+    public static void storeWord(String word) {
+        Duke.textHistory.add(word);
+    }
+
+    public static void printHistory() {
+        int i = 1;
+        System.out.println("------------------------------------");
+        for(String word: Duke.textHistory) {
+            System.out.println(i + ". " + word);
+        }
+        System.out.println("------------------------------------");
+        System.out.println("");
+    }
+
+//    public static void displayResult()
+
     public static void echo(String words) {
         System.out.println("------------------------------------");
-        System.out.println(words);
+        System.out.println("added: " + words);
         System.out.println("------------------------------------");
         System.out.println("");
     }
 
     public static boolean isBye(String word) {
         return Duke.BYE.equals(word);
+    }
+
+    public static boolean isList(String word) {
+        return Duke.LIST.equals(word);
     }
 
     public static void replyWelcomeMessage() {
