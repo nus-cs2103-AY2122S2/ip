53de07da3ea55c77598f167ffcfc5e9f1c3404de
myc37 <68676763+myc37@users.noreply.github.com>
2022-01-30 16:14:55 +0800

    Fix compiler path in runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,7 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac -cp ..\src\main\java\chatbot\ChatBot.java -Xlint:none -d ..\bin ..\src\main\java\chatbot\exception\*.java ..\src\main\java\chatbot\datetime\*.java ..\src\main\java\chatbot\task\*.java ..\src\main\java\chatbot\util\*.java
+javac -cp ..\src\main\java\chatbot -Xlint:none -d ..\bin ..\src\main\java\chatbot\*.java ..\src\main\java\chatbot\exception\*.java ..\src\main\java\chatbot\datetime\*.java ..\src\main\java\chatbot\task\*.java ..\src\main\java\chatbot\util\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -15,7 +15,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin ChatBot < input.txt > ACTUAL.TXT
+java -classpath ..\bin chatbot.ChatBot < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
