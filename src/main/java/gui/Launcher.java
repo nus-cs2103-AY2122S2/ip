b5cffca60df7246332b6c5e36197a1b57e79d70d
Ryan Walter Lee <88182652+ryanwalterlee@users.noreply.github.com>
2022-01-30 16:59:33 +0800

    Complete GUI

--- src/main/java/gui/Launcher.java
+++ src/main/java/gui/Launcher.java
@@ -1,6 +1,6 @@
 package gui;
 
-import duke.Duke;
+import duke.Main;
 import javafx.application.Application;
 
 /**
@@ -8,7 +8,7 @@ import javafx.application.Application;
  */
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        Application.launch(Main.class, args);
     }
 }
 
