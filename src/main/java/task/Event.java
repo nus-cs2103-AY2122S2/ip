23cb68840c9fb71f94d4d2bbe5fdc2187b81649b
aditi2313 <52269537+aditi2313@users.noreply.github.com>
2022-01-30 16:15:43 +0800

    Update Event.java

--- src/main/java/task/Event.java
+++ src/main/java/task/Event.java
@@ -2,6 +2,9 @@ package task;
 
 import java.time.LocalDate;
 
+/**
+ * Class that encapsulates event tasks with due dates.
+ */
 public class Event extends Task {
 
     LocalDate date;
@@ -11,15 +14,27 @@ public class Event extends Task {
         this.date = LocalDate.parse(date);
     }
 
+    /**
+     * Returns character symbol that represents event tasks.
+     * @return String 'E'.
+     */
     public String symbol() {
         return "E";
     }
 
+    /**
+     * Returns event details along with full string representation of the date.
+     * @return details of the event.
+     */
     @Override
     public String displayTime() {
         return super.toString() + this.date.getDayOfMonth() + " " + this.date.getMonth() + " " + this.date.getYear();
     }
 
+    /**
+     * Returns details of the event along with date object.
+     * @return String which combines event details with toString method of the date object.
+     */
     @Override
     public String toString() {
         return super.toString() + "/" + this.date.toString();
