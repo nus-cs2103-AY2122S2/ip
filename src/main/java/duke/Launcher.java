1305aa52431d24e357e9d7e04c66f63af5541b45
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-01-30 16:44:47 +0800

    Fix checkstyle

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -7,11 +7,6 @@ import javafx.application.Application;
  */
 public class Launcher {
     public static void main(String[] args) {
-        try {
-            Application.launch(Duke.class, args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        Application.launch(Duke.class, args);
     }
 }
