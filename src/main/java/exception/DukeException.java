9f29d5c2a68934d7210cc237022e86b44591ce17
aditi2313 <guptaaditi2313@gmail.com>
2022-01-30 18:00:13 +0800

    Latest commit Level-9

--- src/main/java/exception/DukeException.java
+++ src/main/java/exception/DukeException.java
@@ -1,11 +1,18 @@
 package exception;
 
+/**
+ * Custom Exception class that defines user related errors.
+ */
 public class DukeException extends Exception{
 
     public DukeException(String message) {
         super(message);
     }
 
+    /**
+     * Returns error message.
+     * @return String version of the error message.
+     */
     @Override
     public String toString() {
         return super.getMessage();
