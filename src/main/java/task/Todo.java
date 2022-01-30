b5104b1bc972386da9cb70f855fe542a1beef126
aditi2313 <52269537+aditi2313@users.noreply.github.com>
2022-01-30 16:16:43 +0800

    Update Todo.java

--- src/main/java/task/Todo.java
+++ src/main/java/task/Todo.java
@@ -1,17 +1,26 @@
 package task;
 
-import task.Task;
-
+/**
+ * Class that encapsulates task types without due date.
+ */
 public class Todo extends Task {
 
     public Todo(String details) {
         super(details);
     }
 
+    /**
+     * Returns character symbol that represents todo tasks.
+     * @return String 'T'.
+     */
     public String symbol() {
         return "T";
     }
 
+    /**
+     * Returns task details since it does not have a due date.
+     * @return details of the todo.
+     */
     @Override
     public String displayTime() {
         return super.toString();
