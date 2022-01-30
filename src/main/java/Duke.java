b17138e153b525e5a0fde494739b9866ae7eac71
Ria Khaitan <ria@u.nus.edu>
2022-01-31 02:10:46 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,8 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = 0;
+        int counter = 0;
+        String[] lists = new String[100];
         String line = "*************************************************************************";
         String lineTwo = "|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|";
         System.out.println(line);
@@ -17,10 +19,17 @@ public class Duke {
                 System.out.println("See you soon! Have a good day ^_^");
                 System.out.println(line);
             }
+            else if(text.equals("list")) {
+                for(int i = 0; i < counter; i++) {
+                    System.out.println(i+1 + ". " + lists[i]);
+                }
+            }
             else {
                 System.out.println(lineTwo);
-                System.out.println(text);
+                System.out.println("added: " + text);
                 System.out.println(lineTwo);
+                lists[counter] = text;
+                counter++;
             }
         }
         while(num == 0);
