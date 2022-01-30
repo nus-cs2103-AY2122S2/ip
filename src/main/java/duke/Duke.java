e0ba4b2823853bb89fba076fd87e9cbc3fb71fd5
HappyPear <ongweixin@hotmail.com>
2022-01-30 14:26:08 +0800

    Revert "Add JavaFX"
    This reverts commit 0828a0b688323953c9b259436888ab62ae6352af.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,98 +6,28 @@ import duke.io.Storage;
 import duke.task.TaskList;
 import duke.ui.Ui;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+public class Duke {
+    public static void main(String[] args) {
+        Ui ui = new Ui();
+        Scanner in = new Scanner(System.in);
+        Parser parser = new Parser();
+        TaskList taskList = new TaskList();
 
-public class Duke extends Application {
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
+        Storage.loadFile(System.getProperty("user.dir") + "\\data\\duke.txt", taskList);
 
-//    public static void main(String[] args) {
-//        Ui ui = new Ui();
-//        Scanner in = new Scanner(System.in);
-//        Parser parser = new Parser();
-//        TaskList taskList = new TaskList();
-//
-//        Storage.loadFile(System.getProperty("user.dir") + "\\data\\duke.txt", taskList);
-//
-//        ui.printLogo();
-//        String userInput = in.nextLine();
-//        int runResult = parser.run(userInput, ui, taskList);
-//        while(runResult != -1) {
-//            if(runResult == 1) {
-//                Storage.saveFile("data", "duke.txt", taskList.getList());
-//            }
-//            userInput = in.nextLine();
-//            runResult = parser.run(userInput, ui, taskList);
-//        }
-//    }
-
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        //Step 2. Formatting the window to look as expected
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
+        ui.printLogo();
+        String userInput = in.nextLine();
+        int runResult = parser.run(userInput, ui, taskList);
+        while(runResult != -1) {
+            if(runResult == 1) {
+                Storage.saveFile("data", "duke.txt", taskList.getList());
+            }
+            userInput = in.nextLine();
+            runResult = parser.run(userInput, ui, taskList);
+        }
     }
 
 }
-
-
 
 
 
