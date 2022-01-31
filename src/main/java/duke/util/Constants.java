65c49ca7bd8bbe38fea28673bdf75373137f6e01
Albert Sutiono <albertsutz123@gmail.com>
2022-01-31 18:21:43 +0800

    Update Constant class to include commands word list

--- src/main/java/duke/util/Constants.java
+++ src/main/java/duke/util/Constants.java
@@ -1,5 +1,7 @@
 package duke.util;
 
+import java.util.List;
+
 public class Constants {
     public static final String LINE = "___________________________________________________________________________";
     public static final String GAP = "      ";
@@ -14,4 +16,5 @@ public class Constants {
                     "████████▀  ████████▀    ███   ▀█▀   ██████████";
     public static final String DIR_PATH = "./data";
     public static final String STORAGE_PATH = "./data/duke.txt";
+    public static final List<String> commands = List.of("list", "bye", "mark", "unmark", "delete", "todo", "event", "deadline", "save");
 }
