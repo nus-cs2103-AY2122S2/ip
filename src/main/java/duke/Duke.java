c1921f605a2a1921cafef9c1848b77515f095b0b
waffledood <itshaikalhere@hotmail.sg>
2022-01-31 10:51:14 +0800

    Fix checkstyle errors in Duke.java & DukeException.java

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,67 +2,66 @@ package duke;
 
 import duke.command.Command;
 import duke.command.Parser;
+import duke.task.Storage;
 import duke.task.TaskList;
 import duke.ui.Ui;
-import duke.task.Storage;
 
-/** 
-* Represents a Duke chatbot. 
-* A Duke chatbot will save or load the user's tasks in a local file. 
-*/
+/**
+ * Represents a Duke chatbot.
+ * A Duke chatbot will save or load the user's tasks in a local file.
+ */
 
 public class Duke {
 
-    TaskList taskList;
-    String userName;
-    String taskFilePath;
-    Ui ui;
-    Storage storage;
-    
-    /**
-     * The main method which intializes Duke & runs it. 
-     * @param args Unused. 
-     * return Nothing. 
-    */
-    public static void main(String[] args) {
-        new Duke("data/tasks.txt").run();
-    }
+    private TaskList taskList;
+    private String userName;
+    private String taskFilePath;
+    private Ui ui;
+    private Storage storage;
 
-    /** 
-     * Constructor to initialize Duke & its various components. 
-     * @param filePath Path of savefile of tasks information 
-     * @return Nothing. 
-    */
+    /**
+     * Constructor to initialize Duke & its various components.
+     *
+     * @param filePath Path of savefile of tasks information
+     * @return Nothing.
+     */
     public Duke(String filePath) {
         taskFilePath = filePath;
         ui = new Ui();
         storage = new Storage(taskFilePath, ui);
-        // load TaskList from existing data 
+        // load TaskList from existing data
         taskList = new TaskList(storage.loadFileContents());
     }
 
-    /** 
-     * Runs the main logic of Duke. 
-     * @param Nothing. 
-     * @return Nothing. 
-    */
+    /**
+     * The main method which intializes Duke & runs it.
+     *
+     * @param args Unused.
+     *             return Nothing.
+     */
+    public static void main(String[] args) {
+        new Duke("data/tasks.txt").run();
+    }
+
+
+    /**
+     * Main logic of Duke to set it up & run it.
+     */
     public void run() {
-        
+
         // Jarvis introduces himself, asks for user's name & greets user
         userName = ui.showWelcome();
 
-        // initialize the chat session as active 
+        // initialize the chat session as active
         boolean active = true;
 
-        while(active) {
+        while (active) {
             try {
                 String userInput = ui.readCommand();
                 Command c = Parser.parse(userInput);
                 c.execute(taskList, ui, storage);
                 active = c.isActive();
-            }
-            
-            catch(DukeException e) {
+            } catch (DukeException e) {
                 ui.showError(e.getMessage());
             }
         }
