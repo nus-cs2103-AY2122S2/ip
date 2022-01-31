3cac93fa22d34bad5cafb911cfea558f1276b18c
Tan Yu Tao <tanyutao913@gmail.com>
2022-01-31 15:17:06 +0800

    Add gradle support for building and testing
    No support for gradle to run unit tests

--- src/test/java/commands/AddCommandTest.java
+++ src/test/java/commands/AddCommandTest.java
@@ -1,16 +1,13 @@
 package commands;
 
-import exceptions.DukeException;
 import org.junit.jupiter.api.Test;
 import storage.Storage;
 import tasks.TaskList;
-import tasks.ToDo;
 import ui.Ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class AddCommandTest {
 	AddCommand cmd;
