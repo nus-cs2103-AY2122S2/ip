b0c8510d30b06df82d98e79243b2bd50cd704958
Charisma Kausar <charisma.kausar@gmail.com>
2022-02-02 04:41:38 +0800

    Add javadocs for DateFormatter class

--- src/main/java/heylo/util/DateFormatter.java
+++ src/main/java/heylo/util/DateFormatter.java
@@ -3,7 +3,15 @@ package heylo.util;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Formats dates.
+ */
 public class DateFormatter {
+    /**
+     * Formats the given date in MMM d yyyy format.
+     * @param date LocalDate date.
+     * @return String date.
+     */
     public static String formatDateInLongForm(LocalDate date) {
         try {
             return date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
