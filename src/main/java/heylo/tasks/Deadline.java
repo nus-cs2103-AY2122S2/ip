b7011b9db8f857d66fd4e1e23a414d4ff163651c
Charisma Kausar <charisma.kausar@gmail.com>
2022-02-02 04:21:33 +0800

    Add javadocs for Deadline class

--- src/main/java/heylo/tasks/Deadline.java
+++ src/main/java/heylo/tasks/Deadline.java
@@ -4,10 +4,18 @@ import heylo.util.DateFormatter;
 
 import java.time.LocalDate;
 
+/**
+ * Represents a task with a deadline.
+ */
 public class Deadline extends Task {
 
     protected LocalDate deadline;
 
+    /**
+     * Create a task with a deadline.
+     * @param description String description of the task.
+     * @param deadline String date of the deadline.
+     */
     public Deadline(String description, String deadline) {
         super(description);
         try {
@@ -17,6 +25,10 @@ public class Deadline extends Task {
         }
     }
 
+    /**
+     * Converts the deadline task and its data to string format.
+     * @return String deadline
+     */
     @Override
     public String toString() {
         return " [D]" + super.toString() + "\t (by " + DateFormatter.formatDateInLongForm(deadline) + ")";
