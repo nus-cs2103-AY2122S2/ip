9ef0705b5706a7639b64023146d9ca021c126897
Charisma Kausar <charisma.kausar@gmail.com>
2022-02-02 03:31:42 +0800

    Test toString method of Event

---
+++ src/test/java/heylo/tasks/EventTest.java
@@ -0,0 +1,19 @@
+package heylo.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventTest {
+    @Test
+    public void toString_validEvent_success() {
+        Event event = new Event("event-1", "2022-02-09");
+        assertEquals(" [E][ ] event-1\t (at Feb 9 2022)", event.toString());
+    }
+
+    @Test
+    public void toString_invalidDuration_handleError() {
+        Event event = new Event("event-1", "not-a-date");
+        assertEquals(" [E][ ] event-1\t (at )", event.toString());
+    }
+}
