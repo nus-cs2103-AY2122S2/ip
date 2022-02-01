5e57db29ada1c19a70bcf9b53821959d23db2633
gz <zhouguanzong@gmail.com>
2022-02-01 21:23:35 +0800

    Fix coding style violations

--- src/main/java/duke/manager/Parser.java
+++ src/main/java/duke/manager/Parser.java
@@ -28,77 +28,127 @@ public class Parser {
      *                      empty task description or invalid command.
      */
     public Command parse(String fullCommand) throws DukeException {
+        fullCommand = fullCommand.trim();
         String keyword = fullCommand.split(" ")[0];
-        try {
-            switch (keyword) {
-            case "list": {
-                return new ListCommand();
-            }
-            case "mark": {
-                if (fullCommand.split(" ").length == 1) {
-                    throw new DukeException("OOPS!!! Please choose a task number");
-                }
-                int taskNo = Integer.parseInt(fullCommand.split(" ")[1]) - 1;
-                return new MarkCommand(true, taskNo);
-            }
-            case "unmark": {
-                if (fullCommand.split(" ").length == 1) {
-                    throw new DukeException("OOPS!!! Please choose a task number");
-                }
-                int taskNo = Integer.parseInt(fullCommand.split(" ")[1]) - 1;
-                return new MarkCommand(false, taskNo);
-            }
-            case "todo": {
-                String task = fullCommand.replaceFirst("todo", "");
-                if (task.length() == 0 || task.trim().length() == 0) {
-                    throw new DukeException("OOPS!!! The description of a todo cannot be empty");
-                }
-                return new AddToDoCommand(task);
-            }
-            case "deadline": {
-                String[] text = fullCommand.replaceFirst("deadline", "").split(" /by ");
-                String task = text[0];
-                if (task.trim().length() == 0) {
-                    throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
-                }
-                if (text.length == 1) {
-                    throw new DukeException("OOPS!!! The date/time of a deadline cannot be empty.");
-                }
-                String by = text[1];
-                return new AddDeadlineCommand(task, by);
-            }
-            case "event": {
-                String[] text = fullCommand.replaceFirst("event", "").split(" /at ");
-                String task = text[0];
-                if (task.trim().length() == 0) {
-                    throw new DukeException("OOPS!!! The description of an event cannot be empty.");
-                }
-                String at = text[1];
-                return new AddEventCommand(task, at);
-            }
-            case "delete": {
-                if (fullCommand.split(" ").length == 1) {
-                    throw new DukeException("OOPS!!! Please choose a task number.");
-                }
-                int taskNo = Integer.parseInt(fullCommand.split(" ")[1]) - 1;
-                return new DeleteTaskCommand(taskNo);
-            }
-            case "bye": {
-                return new ExitCommand();
-            }
-            case "find": {
-                if (fullCommand.split(" ").length == 1) {
-                    throw new DukeException("OOPS!!! Please enter something for me to find!");
-                }
-                String toFind = fullCommand.split(" ")[1];
-                return new FindCommand(toFind);
-            }
-            default: {
-                throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
-            }
-            }
-        } catch (NumberFormatException e) {
-            throw new DukeException("Please enter a task number.");
+        switch (keyword) {
+        case "list":
+            return new ListCommand();
+        case "mark":
+            return parseMarkCommand(fullCommand, true);
+        case "unmark":
+            return parseMarkCommand(fullCommand, false);
+        case "todo":
+            return parseAddToDoCommand(fullCommand);
+        case "deadline":
+            return parseAddDeadlineCommand(fullCommand);
+        case "event":
+            return parseAddEventCommand(fullCommand);
+        case "delete":
+            return parseDeleteTaskCommand(fullCommand);
+        case "bye":
+            return new ExitCommand();
+        case "find":
+            return parseFindCommand(fullCommand);
+        default:
+            throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
+    }
+
+    /**
+     * Returns a mark command.
+     *
+     * @param fullCommand The full command that was given by the user.
+     * @param isMark Whether to mark or unmark the task.
+     * @return The command to be executed.
+     * @throws DukeException If there is an error parsing the input.
+     */
+    private MarkCommand parseMarkCommand(String fullCommand, boolean isMark) throws DukeException {
+        if (fullCommand.split(" ").length == 1) {
+            throw new DukeException("OOPS!!! Please choose a task number");
+        }
+        int taskNo = Integer.parseInt(fullCommand.split(" ")[1]) - 1;
+        return new MarkCommand(isMark, taskNo);
+    }
+
+    /**
+     * Returns a AddToDoCommand.
+     *
+     * @param fullCommand The full command that was given by the user.
+     * @return The command to be executed.
+     * @throws DukeException If there is an error parsing the input.
+     */
+    private AddToDoCommand parseAddToDoCommand(String fullCommand) throws DukeException {
+        String task = fullCommand.replaceFirst("todo", "");
+        if (task.length() == 0 || task.trim().length() == 0) {
+            throw new DukeException("OOPS!!! The description of a todo cannot be empty");
+        }
+        return new AddToDoCommand(task);
+    }
+
+    /**
+     * Returns a AddDeadlineCommand.
+     *
+     * @param fullCommand The full command that was given by the user.
+     * @return The command to be executed.
+     * @throws DukeException If there is an error parsing the input.
+     */
+    private AddDeadlineCommand parseAddDeadlineCommand(String fullCommand) throws DukeException {
+        String[] text = fullCommand.replaceFirst("deadline", "").split(" /by ");
+        String task = text[0];
+        if (task.trim().length() == 0) {
+            throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+        }
+        if (text.length == 1) {
+            throw new DukeException("OOPS!!! The date/time of a deadline cannot be empty.");
+        }
+        String by = text[1];
+        return new AddDeadlineCommand(task, by);
+    }
+
+    /**
+     * Returns a AddEventCommand.
+     *
+     * @param fullCommand The full command that was given by the user.
+     * @return The command to be executed.
+     * @throws DukeException If there is an error parsing the input.
+     */
+    private AddEventCommand parseAddEventCommand(String fullCommand) throws DukeException {
+        String[] text = fullCommand.replaceFirst("event", "").split(" /at ");
+        String task = text[0];
+        if (task.trim().length() == 0) {
+            throw new DukeException("OOPS!!! The description of an event cannot be empty.");
+        }
+        String at = text[1];
+        return new AddEventCommand(task, at);
+    }
+
+    /**
+     * Returns a DeleteTaskCommand.
+     *
+     * @param fullCommand The full command that was given by the user.
+     * @return The command to be executed.
+     * @throws DukeException If there is an error parsing the input.
+     */
+    private DeleteTaskCommand parseDeleteTaskCommand(String fullCommand) throws DukeException {
+        if (fullCommand.split(" ").length == 1) {
+            throw new DukeException("OOPS!!! Please choose a task number.");
+        }
+        int taskNo = Integer.parseInt(fullCommand.split(" ")[1]) - 1;
+        return new DeleteTaskCommand(taskNo);
+    }
+
+    /**
+     * Returns a FindCommand.
+     *
+     * @param fullCommand The full command that was given by the user.
+     * @return The command to be executed.
+     * @throws DukeException If there is an error parsing the input.
+     */
+    private FindCommand parseFindCommand(String fullCommand) throws DukeException {
+        if (fullCommand.split(" ").length == 1) {
+            throw new DukeException("OOPS!!! Please enter something for me to find!");
+        }
+        String toFind = fullCommand.split(" ")[1];
+        return new FindCommand(toFind);
     }
 }
