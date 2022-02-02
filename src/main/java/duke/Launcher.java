68262d3d20280850988560f9aa0736f1662e6f8b
Lai Mei Tin <e0550504@u.nus.edu>
2022-02-03 00:57:21 +0800

    Add small modification to the GUI font

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -1,15 +1,6 @@
 package duke;
 
-import duke.gui.DialogBox;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 
 /**
  * A launcher class to workaround classpath issues.
