a20eeb8fa75b81f3e7a588224d68625dbb4cb67e
Jiaaa-yang <lyejy99@gmail.com>
2022-02-02 15:22:25 +0800

    Use FXML for application
    Change structuring of JavaFX application to use
    FXML for main layout and dialog box.

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -1,13 +1,13 @@
 package duke;
 
-import duke.ui.Duke;
+import duke.ui.Main;
 import javafx.application.Application;
 
 /**
  * Main entry class to run ChatBot application
  */
-public class Main {
+public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Duke.class, args);
+        Application.launch(Main.class, args);
     }
 }
--- src/main/java/duke/ui/Duke.java
+++ src/main/java/duke/ui/Duke.java
@@ -1,20 +1,5 @@
 package duke.ui;
 
-import duke.ui.components.DialogBox;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
 import duke.data.Storage;
 import duke.data.TaskList;
 import duke.chatbot.ChatBot;
@@ -25,138 +10,37 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
- * Duke class representing a JavaFX application
- * to serve as the main user interface.
+ * Duke class serving as interface between
+ * GUI and chatbot logic.
  */
-public class Duke extends Application {
+public class Duke {
     /** Default path for data file: data/duke.txt */
     private static final Path DEFAULT_FILE_PATH = Paths.get("data", "duke.txt");
 
     /** ChatBot instance for running of user commands */
-    private ChatBot chatBot;
-
-    /** UI nodes/components for mainframe of application */
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
-    private void initialiseChatBot() throws IOException {
-        Storage store = Storage.initStorage(DEFAULT_FILE_PATH);
-        TaskList taskList = TaskList.initTaskList(store);
-
-        this.chatBot = new ChatBot(taskList);
-    }
+    private ChatBot chatBot = null;
 
     /**
-     * Initialises required components involved in
-     * the main layout and sets their sizes and positions.
-     *
-     * @return Root node for the layout
+     * Creates a duke instance initialised with default data file path.
      */
-    private AnchorPane initialiseLayout() {
-        // Reused with modifications from JavaFX tutorial at
-        // https://se-education.org/guides/tutorials/javaFx.html
-        // by Jeffrey Lum
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        dialogContainer.setSpacing(15);
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        return mainLayout;
-    }
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        // Reused with modifications from JavaFX tutorial at
-        // https://se-education.org/guides/tutorials/javaFx.html
-        // by Jeffrey Lum
-
-        // Initialise logic for chatbot and main layout
-        initialiseChatBot();
-        AnchorPane mainLayout = this.initialiseLayout();
-
-        // Set fixed height and width for application window
-        stage.setTitle("Duke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
-
-        //Step 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        //Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        scene = new Scene(mainLayout);
-        // Workaround for buggy text display on Mac OS
-        scene.getRoot().setStyle("-fx-font-family: 'serif'");
-
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    /**
-     * Creates two dialog boxes, one echoing user input and
-     * the other containing Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        // Reused from JavaFX tutorial at
-        // https://se-education.org/guides/tutorials/javaFx.html
-        // by Jeffrey Lum
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
-        );
-        userInput.clear();
-
-        if (chatBot.hasTerminated()) {
-            Platform.exit();
+    public Duke() {
+        try {
+            Storage store = Storage.initStorage(DEFAULT_FILE_PATH);
+            TaskList taskList = TaskList.initTaskList(store);
+            this.chatBot = new ChatBot(taskList);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    /**
+     * Returns a boolean on whether the chatbot
+     * has received a termination command.
+     *
+     * @return Whether the chatbot has terminated.
+     */
+    public boolean hasTerminated() {
+        return this.chatBot.hasTerminated();
     }
 
     /**
@@ -166,7 +50,7 @@ public class Duke extends Application {
      * @param input User input command to run.
      * @return String feedback for command ran.
      */
-    private String getResponse(String input) {
+    public String getResponse(String input) {
         ArrayList<String> response = chatBot.runCommand(input);
         // Show each item in response in a new line
         return response.stream().reduce("", (first, second) -> first + "\n" + second);
