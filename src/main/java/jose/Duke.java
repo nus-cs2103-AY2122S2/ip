e1a5fcb0098210397a91e0011673eb2aa7882dd8
Dane Bagaoisan <dane0515@gmail.com>
2022-02-03 00:20:59 +0800

    Add GUI

--- src/main/java/jose/Duke.java
+++ src/main/java/jose/Duke.java
@@ -1,7 +1,6 @@
 package jose;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 import jose.task.Deadline;
 import jose.task.Event;
@@ -15,6 +14,7 @@ public class Duke {
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
+    private Parser parser;
 
     /**
      * Class constructor that also sets up the task list and retrieves/creates the data file.
@@ -26,6 +26,7 @@ public class Duke {
         try {
             storage = new Storage(filePath);
             tasks = new TaskList(storage.load());
+            parser = new Parser();
         } catch (IOException e) {
             System.out.println(e.getMessage());
             tasks = new TaskList();
@@ -33,101 +34,68 @@ public class Duke {
     }
 
     /**
-     * Method containing the main logic of the program.
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
      */
-    public void run() {
-        Scanner scanner = new Scanner(System.in);
-        Parser parser = new Parser();
-        boolean isModified = false;
-        boolean isRunning = true;
+    public String getResponse(String input) {
+        String[] taskInfo;
+        Task tempTask;
 
-        ui.showGreeting();
-
-        while (isRunning) {
-            String input = scanner.nextLine();
-            String[] taskInfo;
-            Task tempTask;
-
-            try {
-                Parser.Command command = parser.parse(input);
-                switch (command) {
-                case BYE:
-                    ui.showExitMessage();
-                    isRunning = false;
-                    break;
-                case LIST:
-                    ui.showList(tasks.getTasks());
-                    break;
-                case MARK:
-                    tempTask = tasks.getTask(parser.getIndex(input));
-                    tempTask.mark();
-                    isModified = true;
-                    ui.showMarkMessage(tempTask);
-                    break;
-                case UNMARK:
-                    tempTask = tasks.getTask(parser.getIndex(input));
-                    tempTask.unmark();
-                    isModified = true;
-                    ui.showUnmarkMessage(tempTask);
-                    break;
-                case DELETE:
-                    tempTask = tasks.getTask(parser.getIndex(input));
-                    ui.showDeleteMessage(tempTask);
-                    tasks.removeTask(tempTask);
-                    isModified = true;
-                    ui.showRemainingTasks(tasks);
-                    break;
-                case FIND:
-                    taskInfo = input.split(" ", 2);
-                    ui.showList(tasks.findTasks(taskInfo[1]));
-                    break;
-                case TODO:
-                    taskInfo = input.split(" ", 2);
-                    tempTask = new ToDo(taskInfo[1]);
-                    tasks.addTask(tempTask);
-                    isModified = true;
-                    ui.showAddMessage(tempTask);
-                    ui.showRemainingTasks(tasks);
-                    break;
-                case DEADLINE:
-                    taskInfo = input.split(" ", 2)[1].split(" /by ");
-                    tempTask = new Deadline(taskInfo[0], taskInfo[1]);
-                    tasks.addTask(tempTask);
-                    isModified = true;
-                    ui.showAddMessage(tempTask);
-                    ui.showRemainingTasks(tasks);
-                    break;
-                case EVENT:
-                    taskInfo = input.split(" ", 2)[1].split(" /at ");
-                    tempTask = new Event(taskInfo[0], taskInfo[1]);
-                    tasks.addTask(tempTask);
-                    isModified = true;
-                    ui.showAddMessage(tempTask);
-                    ui.showRemainingTasks(tasks);
-                    break;
-                default:
-                    System.out.println("?");
-                }
-
-                if (isModified) {
-                    storage.update(tasks);
-                    isModified = false;
-                }
-            } catch (DukeException e) {
-                System.out.println(e.getMessage());
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
+        try {
+            Parser.Command command = parser.parse(input);
+            switch (command) {
+            case BYE:
+                return ui.showExitMessage();
+            case LIST:
+                return ui.showList(tasks.getTasks());
+            case MARK:
+                tempTask = tasks.getTask(parser.getIndex(input));
+                tempTask.mark();
+                updateDataFile();
+                return ui.showMarkMessage(tempTask);
+            case UNMARK:
+                tempTask = tasks.getTask(parser.getIndex(input));
+                tempTask.unmark();
+                updateDataFile();
+                return ui.showUnmarkMessage(tempTask);
+            case DELETE:
+                tempTask = tasks.getTask(parser.getIndex(input));
+                ui.showDeleteMessage(tempTask);
+                tasks.removeTask(tempTask);
+                updateDataFile();
+                return ui.showDeleteMessage(tempTask) + ui.showRemainingTasks(tasks);
+            case FIND:
+                taskInfo = input.split(" ", 2);
+                return ui.showList(tasks.findTasks(taskInfo[1]));
+            case TODO:
+                taskInfo = input.split(" ", 2);
+                tempTask = new ToDo(taskInfo[1]);
+                tasks.addTask(tempTask);
+                updateDataFile();
+                return ui.showAddMessage(tempTask) + ui.showRemainingTasks(tasks);
+            case DEADLINE:
+                taskInfo = input.split(" ", 2)[1].split(" /by ");
+                tempTask = new Deadline(taskInfo[0], taskInfo[1]);
+                tasks.addTask(tempTask);
+                updateDataFile();
+                return ui.showAddMessage(tempTask) + ui.showRemainingTasks(tasks);
+            case EVENT:
+                taskInfo = input.split(" ", 2)[1].split(" /at ");
+                tempTask = new Event(taskInfo[0], taskInfo[1]);
+                tasks.addTask(tempTask);
+                updateDataFile();
+                return ui.showAddMessage(tempTask) + ui.showRemainingTasks(tasks);
+            default:
+                return "?";
             }
+        } catch (DukeException e) {
+            return "Error:" + e.getMessage();
+        } catch (IOException e) {
+            return "Error:" + e.getMessage();
         }
-        scanner.close();
     }
 
-    /**
-     * Main method.
-     *
-     * @param args Arguments to be passed in.
-     */
-    public static void main(String[] args) {
-        new Duke(System.getProperty("user.dir") + "/data/db.txt").run();
+    public void updateDataFile() throws IOException {
+        storage.update(tasks);
     }
 }
