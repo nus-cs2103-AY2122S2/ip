21d225674ef56640c916d645fbe9df0da486d831
Samuel Tee <samuel_tee_gy@hotmail.com>
2022-02-02 19:02:21 +0800

    Fix ParserTest's JUnit tests

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -4,8 +4,10 @@ import duke.command.AddDeadlineCommand;
 import duke.command.AddEventCommand;
 import duke.command.AddTodoCommand;
 import duke.command.Command;
+import duke.command.DeleteCommand;
 import duke.command.ErrorCommand;
 import duke.command.ExitCommand;
+import duke.command.FindCommand;
 import duke.command.ListAllTasksCommand;
 import duke.command.MarkAsDoneCommand;
 import duke.command.MarkAsUndoneCommand;
@@ -13,17 +15,17 @@ import duke.taskobjects.Todo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class ParserTest { // Just need to ensure it is returning the correct Command
     @Test
     public void testParse_list() {
-        Parser parser = new Parser();
         try {
+            Parser parser = new Parser();
             Command resultCommand = parser.parseCommand("list");
-            assertTrue(resultCommand instanceof ListAllTasksCommand);
+            assertInstanceOf(ListAllTasksCommand.class, resultCommand);
         } catch (Exception e) {
             fail(e.getMessage());
         }
@@ -45,13 +47,12 @@ public class ParserTest { // Just need to ensure it is returning the correct Com
 
     @Test
     public void testParse_exit() {
-        Parser parser = new Parser();
-        parser.setTaskList(new TaskList());
-
-        Command resultCommand;
         try {
-            resultCommand = parser.parseCommand("exit");
-            assertTrue(ExitCommand.isExitCommand(resultCommand));
+            Parser parser = new Parser();
+            parser.setTaskList(new TaskList());
+
+            Command resultCommand = parser.parseCommand("exit");
+            assertInstanceOf(ExitCommand.class, resultCommand);
         } catch (Exception e) {
             fail(e.getMessage());
         }
@@ -59,15 +60,14 @@ public class ParserTest { // Just need to ensure it is returning the correct Com
 
     @Test
     public void testParse_mark() {
-        Parser parser = new Parser();
-        TaskList taskList = new TaskList();
-        parser.setTaskList(taskList);
-        taskList.add(new Todo("return book"));
-
-        Command resultCommand;
         try {
-            resultCommand = parser.parseCommand("mark 0");
-            assertTrue(resultCommand instanceof MarkAsDoneCommand);
+            Parser parser = new Parser();
+            TaskList taskList = new TaskList();
+            parser.setTaskList(taskList);
+            taskList.add(new Todo("return book"));
+
+            Command resultCommand = parser.parseCommand("mark 1");
+            assertInstanceOf(MarkAsDoneCommand.class, resultCommand);
         } catch (Exception e) {
             fail(e.getMessage());
         }
@@ -75,15 +75,14 @@ public class ParserTest { // Just need to ensure it is returning the correct Com
 
     @Test
     public void testParse_unmark() {
-        Parser parser = new Parser();
-        TaskList taskList = new TaskList();
-        parser.setTaskList(taskList);
-        taskList.add(new Todo("return book"));
-
-        Command resultCommand;
         try {
-            resultCommand = parser.parseCommand("unmark 0");
-            assertTrue(resultCommand instanceof MarkAsUndoneCommand);
+            Parser parser = new Parser();
+            TaskList taskList = new TaskList();
+            parser.setTaskList(taskList);
+            taskList.add(new Todo("return book"));
+
+            Command resultCommand = parser.parseCommand("unmark 1");
+            assertInstanceOf(MarkAsUndoneCommand.class, resultCommand);
         } catch (Exception e) {
             fail(e.getMessage());
         }
@@ -96,7 +95,7 @@ public class ParserTest { // Just need to ensure it is returning the correct Com
 
         try {
             Command resultCommand = parser.parseCommand("mark 0");
-            assertTrue(resultCommand instanceof ErrorCommand);
+            assertInstanceOf(ErrorCommand.class, resultCommand);
             ErrorCommand errorCommand = (ErrorCommand) resultCommand;
             assertEquals("Invalid number entered, index out of bounds", errorCommand.runCommand().toString());
         } catch (Exception e) {
@@ -106,12 +105,12 @@ public class ParserTest { // Just need to ensure it is returning the correct Com
 
     @Test
     public void testParse_todo() {
-        Parser parser = new Parser();
-        parser.setTaskList(new TaskList());
-
         try {
+            Parser parser = new Parser();
+            parser.setTaskList(new TaskList());
+
             Command resultCommand = parser.parseCommand("todo borrow book");
-            assertTrue(resultCommand instanceof AddTodoCommand);
+            assertInstanceOf(AddTodoCommand.class, resultCommand);
         } catch (Exception e) {
             fail(e.getMessage());
         }
@@ -119,12 +118,12 @@ public class ParserTest { // Just need to ensure it is returning the correct Com
 
     @Test
     public void testParse_event() {
-        Parser parser = new Parser();
-        parser.setTaskList(new TaskList());
-
         try {
+            Parser parser = new Parser();
+            parser.setTaskList(new TaskList());
+
             Command resultCommand = parser.parseCommand("event project meeting /at 2022-01-24");
-            assertTrue(resultCommand instanceof AddEventCommand);
+            assertInstanceOf(AddEventCommand.class, resultCommand);
         } catch (Exception e) {
             fail(e.getMessage());
         }
@@ -132,12 +131,12 @@ public class ParserTest { // Just need to ensure it is returning the correct Com
 
     @Test
     public void testParse_event_incorrectDate() { // No date or wrong /at
-        Parser parser = new Parser();
-        parser.setTaskList(new TaskList());
-
         try {
+            Parser parser = new Parser();
+            parser.setTaskList(new TaskList());
+
             Command resultCommand = parser.parseCommand("event project meeting /not a date");
-            assertTrue(resultCommand instanceof ErrorCommand);
+            assertInstanceOf(ErrorCommand.class, resultCommand);
             ErrorCommand errorCommand = (ErrorCommand) resultCommand;
             assertEquals("Please enter /by followed by a date in this format YYYY-MM-DD", errorCommand.runCommand().toString());
         } catch (Exception e) {
@@ -147,12 +146,12 @@ public class ParserTest { // Just need to ensure it is returning the correct Com
 
     @Test
     public void testParse_deadline() {
-        Parser parser = new Parser();
-        parser.setTaskList(new TaskList());
-
         try {
-            Command resultCommand = parser.parseCommand("deadline project meeting /at 2022-01-24");
-            assertTrue(resultCommand instanceof AddDeadlineCommand);
+            Parser parser = new Parser();
+            parser.setTaskList(new TaskList());
+
+            Command resultCommand = parser.parseCommand("deadline project meeting /by 2022-01-24");
+            assertInstanceOf(AddDeadlineCommand.class, resultCommand);
         } catch (Exception e) {
             fail(e.getMessage());
         }
@@ -160,12 +159,12 @@ public class ParserTest { // Just need to ensure it is returning the correct Com
 
     @Test
     public void testParse_deadline_incorrectDate() {
-        Parser parser = new Parser();
-        parser.setTaskList(new TaskList());
-
         try {
+            Parser parser = new Parser();
+            parser.setTaskList(new TaskList());
+
             Command resultCommand = parser.parseCommand("deadline project meeting /not a date");
-            assertTrue(resultCommand instanceof ErrorCommand);
+            assertInstanceOf(ErrorCommand.class, resultCommand);
             ErrorCommand errorCommand = (ErrorCommand) resultCommand;
             assertEquals("Please enter /by followed by a date in this format YYYY-MM-DD", errorCommand.runCommand().toString());
         } catch (Exception e) {
@@ -175,27 +174,69 @@ public class ParserTest { // Just need to ensure it is returning the correct Com
 
     @Test
     public void testParse_delete() {
-        Parser parser = new Parser();
+        try {
+            Parser parser = new Parser();
+            TaskList taskList = new TaskList();
+            taskList.add(new Todo("test"));
+            parser.setTaskList(taskList);
 
+            Command resultCommand = parser.parseCommand("delete 1");
+            assertInstanceOf(DeleteCommand.class, resultCommand);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 
     @Test
     public void testParse_delete_incorrectInt() { // No int or wrong integer input
+        try {
+            Parser parser = new Parser();
+            TaskList taskList = new TaskList();
+            parser.setTaskList(taskList);
 
+            Command resultCommand = parser.parseCommand("delete 1");
+            assertInstanceOf(ErrorCommand.class, resultCommand);
+            ErrorCommand errorCommand = (ErrorCommand) resultCommand;
+            assertEquals("Invalid number entered, index out of bounds", errorCommand.runCommand().toString());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 
     @Test
     public void testParse_find() {
+        try {
+            Parser parser = new Parser();
+            TaskList taskList = new TaskList();
+            parser.setTaskList(taskList);
 
-    }
-
-    @Test
-    public void testParse_find_incorrectInt() {
-
+            Command resultCommand = parser.parseCommand("find hello");
+            assertInstanceOf(FindCommand.class, resultCommand);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 
     @Test
     public void testParse_incorrectCommand() { // Testing ` and unknown command
+        try {
+            Parser parser = new Parser();
+            TaskList taskList = new TaskList();
+            parser.setTaskList(taskList);
 
+            // Testing for ` detection
+            Command resultCommand = parser.parseCommand("test `");
+            assertInstanceOf(ErrorCommand.class, resultCommand);
+            ErrorCommand errorCommand = (ErrorCommand) resultCommand;
+            assertEquals("\"`\" character is not allowed", errorCommand.runCommand().toString());
+
+            // Testing for unknown command
+            Command resultCommand1 = parser.parseCommand("unknown command");
+            assertInstanceOf(ErrorCommand.class, resultCommand);
+            ErrorCommand errorCommand1 = (ErrorCommand) resultCommand1;
+            assertEquals("Unknown command: unknown", errorCommand1.runCommand().toString());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 }
