8afe10c96bd37585a977341d2f303badedfbc43c
shurvirarora <shurvirarora@gmail.com>
2022-02-03 00:03:35 +0800

    Update build.gradle to include JavaFX Create Launcher class Test Launcher by writing psuedo start() method in Yale.java

--- src/main/java/Yale.java
+++ src/main/java/Yale.java
@@ -3,13 +3,17 @@ import yale.command.Storage;
 import yale.command.Ui;
 import yale.task.TaskList;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import java.util.Scanner;
 
 /**
  * Represents a chatbot that tracks
  * tasks for users.
  */
-public class Yale {
+public class Yale extends Application {
     /**
      * Creates Ui, Storage, Parser
      * Scanner and TaskList objects.
@@ -29,6 +33,15 @@ public class Yale {
         storage = new Storage(FILE_PATH);
         parser = new Parser();
         list = new TaskList();
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 
     /**
