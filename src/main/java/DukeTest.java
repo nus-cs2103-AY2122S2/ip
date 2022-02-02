193b6769427b0771cd1a2181dd1fcf99f8ca11bb
Tham Jin Lin <tham.jinlin@dhs.sg>
2022-02-02 19:54:35 +0800

    Implemented new Save function

--- src/main/java/DukeTest.java
+++ src/main/java/DukeTest.java
@@ -1,6 +1,13 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class DukeTest {
     private static ArrayList<Task> taskList = new ArrayList<>();
 
@@ -32,15 +39,126 @@ public class DukeTest {
         }
     }
 
+    private static void addToSave(File save, String type, boolean mark, String description) throws DukeException {
+        try {
+            int marked = mark ? 1 : 0;
+
+            FileWriter saveWriter = new FileWriter(save, true);
+            saveWriter.write(type + " xxx " + marked + " xxx " + description);
+            saveWriter.close();
+        } catch (IOException e) {
+            System.out.println("Unable to new task [" + type + "] to hard disk due to an IO error.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void modifyFromSave(File save, int idx, boolean mark) {
+        try {
+            int lineCounter = 0;
+            BufferedReader saveReader = new BufferedReader(new FileReader(save));
+            String content = "";
+            String line = saveReader.readLine();
+
+            while (line != null) {
+                if (lineCounter == idx) {
+                    if (line.charAt(6) == '1') {
+                        if (!(mark)) {
+                            line = line.replaceFirst("xxx 1 xxx", "xxx 0 xxx");
+                        }
+                    } else {
+                        if (mark) {
+                            line = line.replaceFirst("xxx 0 xxx", "xxx 1 xxx");
+                        }
+                    }
+                }
+
+                content = content + line + System.lineSeparator();
+                line = saveReader.readLine();
+                lineCounter++;
+            }
+
+            FileWriter saveWriter = new FileWriter(save);
+            saveWriter.write(content);
+
+            saveReader.close();
+            saveWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to find the file to save changes to in the hard disk");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Unable to save changes to hard disk due to IO Error");
+            e.printStackTrace();
+        }
+    }
+
+    private static void deleteFromSave(File save, int idx) {
+        try {
+            int lineCounter = 0;
+            BufferedReader saveReader = new BufferedReader(new FileReader(save));
+            String content = "";
+            String line = saveReader.readLine();
+
+            while (line != null) {
+                if (lineCounter != idx) {
+                    content = content + line + System.lineSeparator();
+                }
+
+                line = saveReader.readLine();
+                lineCounter++;
+            }
+
+            FileWriter saveWriter = new FileWriter(save);
+            saveWriter.write(content);
+
+            saveReader.close();
+            saveWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to find the file to save changes to in the hard disk");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Unable to save changes to hard disk due to IO Error");
+            e.printStackTrace();
+        }
+    }
+
+    private static void load(String data) {
+        String[] dataArr = data.split(" xxx ");
+
+        boolean marked = (Integer.valueOf(dataArr[1]) > 0);
+
+        switch (dataArr[0]) {
+            case "D":
+                taskList.add(new Deadlines(dataArr[2], marked));
+                break;
+            case "T":
+                taskList.add(new ToDos(dataArr[2], marked));
+                break;
+            case "E":
+                taskList.add(new Events(dataArr[2], marked));
+                break;
+            default:
+                break;
+        }
+    }
+
     public static void main(String[] args) {
         try {
+            File save = new File("./duke.txt");
+
+            BufferedReader saveReader = new BufferedReader(new FileReader(save));
+            String dataLine = saveReader.readLine();
+
+            while (dataLine != null) {
+                load(dataLine);
+                dataLine = saveReader.readLine();
+            }
+
             Scanner scanner = new Scanner(System.in);
             System.out.println("Hello! I'm Duke\nWhat can I do for you?");
 
             String input = scanner.nextLine();
 
             String[] splitInput = input.split(" ", 2);
-            ;
             String command = splitInput[0];
 
             while (!(isExit(command))) {
@@ -56,14 +174,18 @@ public class DukeTest {
                         if (splitInput.length < 2) {
                             throw new DukeException("Please indicate which task you want to mark!");
                         } else {
-                            taskList.get(Integer.parseInt(splitInput[1]) - 1).mark();
+                            int idx = Integer.parseInt(splitInput[1]) - 1;
+                            taskList.get(idx).mark();
+                            modifyFromSave(save, idx, true);
                             break;
                         }
                     case "unmark":
                         if (splitInput.length < 2) {
                             throw new DukeException("Please indicate which task you want to unmark!");
                         } else {
-                            taskList.get(Integer.parseInt(splitInput[1]) - 1).unmark();
+                            int idx = Integer.parseInt(splitInput[1]) - 1;
+                            taskList.get(idx).unmark();
+                            modifyFromSave(save, idx, false);
                             break;
                         }
                     case "todo":
@@ -71,6 +193,7 @@ public class DukeTest {
                             throw new DukeException("The description of a todo cannot be empty.");
                         } else {
                             addTask(new ToDos(splitInput[1]));
+                            addToSave(save, "T", false, splitInput[1]);
                             break;
                         }
                     case "deadline":
@@ -78,6 +201,7 @@ public class DukeTest {
                             throw new DukeException("The description of a deadline cannot be empty.");
                         } else {
                             addTask(new Deadlines(splitInput[1]));
+                            addToSave(save, "D", false, splitInput[1]);
                             break;
                         }
                     case "event":
@@ -85,11 +209,18 @@ public class DukeTest {
                             throw new DukeException("The description of an event cannot be empty.");
                         } else {
                             addTask(new Events(splitInput[1]));
+                            addToSave(save, "E", false, splitInput[1]);
                             break;
                         }
                     case "delete":
-                        deleteTask(Integer.parseInt(splitInput[1]));
-                        break;
+                        int idx = Integer.parseInt(splitInput[1]);
+                        if (taskList.size() <= idx) {
+                            throw new DukeException("Invalid index input");
+                        } else {
+                            deleteTask(idx);
+                            deleteFromSave(save, idx);
+                            break;
+                        }
                     default:
                         throw new DukeException("I'm sorry, but I don't know what that means :-(");
                 }
@@ -101,8 +232,13 @@ public class DukeTest {
 
             exit();
             scanner.close();
+            saveReader.close();
         } catch (DukeException e) {
             System.out.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
