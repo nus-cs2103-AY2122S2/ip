d6a045da80d7140e0efaa12b6360f4efbebd9040
Bryan Ong <bryan.o@u.nus.edu>
2022-02-04 02:09:39 +0800

    Add a GUI to Duke

--- src/main/java/PukeMain.java
+++ src/main/java/PukeMain.java
@@ -1,10 +1,37 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import puke.exception.PukeException;
+import puke.ui.MainWindow;
 import puke.Puke;
 
+import java.io.IOException;
+
 /**
- * Driver class for Puke.
+ * Main class for Puke.
  */
-public class PukeMain {
-    public static void main(String[] args) {
-        new Puke("data/puke.txt").run();
+public class PukeMain extends Application {
+    private Puke puke = new Puke("data/puke.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            puke.loadTasksFromFile();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(PukeMain.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setPuke(puke);
+
+            stage.setOnCloseRequest(e -> puke.saveTasksToFile());
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
