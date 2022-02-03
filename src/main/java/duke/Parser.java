73ba75e4122b579462303eab11014c8a46138d21
Gernene <wallie988@gmail.com>
2022-02-03 09:54:59 +0800

    Refactor Parser.java

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -34,40 +34,25 @@ public class Parser {
         String[] fullCommandArray = fullCommand.split(" ");
         String command = fullCommandArray[0];
         if (command.equals("deadline")) {
-            String dataString = fullCommand.replaceFirst("deadline ", "");
-            String[] data = dataString.split("/by ");
-            Task task = new Deadline(data[0], LocalDate.parse(data[1]));
-            return new AddCommand(task);
+            return parseAddDeadlineCommand(fullCommand);
         }
         else if (command.equals("event")) {
-            String dataString = fullCommand.replaceFirst("event ", "");
-            String[] data = dataString.split("/at ");
-            Task task = new Event(data[0], LocalDate.parse(data[1]));
-            return new AddCommand(task);
+            return parseAddEventCommand(fullCommand);
         }
         else if (command.equals("todo")) {
-            if (fullCommandArray.length < 2) throw new DukeException("Todo needs a description");
-            String dataString = fullCommand.replaceFirst("todo ", "");
-            Task task = new Todo(dataString);
-            return new AddCommand(task);
+            return parseTodoCommand(fullCommand);
         }
         else if (command.equals("list")) {
-            return new ListCommand();
+            return new parseListCommand();
         }
         else if (command.equals("delete")) {
-            String dataString = fullCommand.replaceFirst("delete ", "");
-            int taskNumber = Integer.parseInt(dataString);
-            return new DeleteCommand(taskNumber);
+            return parseDeleteCommand(fullCommand);
         }
         else if (command.equals("mark")) {
-            String dataString = fullCommand.replaceFirst("mark ", "");
-            int taskNumber = Integer.parseInt(dataString);
-            return new MarkCommand(taskNumber);
+            return parseMarkCommand(fullCommand);
         }
         else if (command.equals("unmark")) {
-            String dataString = fullCommand.replaceFirst("unmark ", "");
-            int taskNumber = Integer.parseInt(dataString);
-            return new UnmarkCommand(taskNumber);
+            return parseUnmarkCommand(fullCommand);
         }
         else if (command.equals("bye")) {
             return new ExitCommand();
@@ -75,6 +60,104 @@ public class Parser {
         else {
             throw new DukeException("I don't recognize that command.");
         }
+    }
+
+    /**
+     * Parses add deadline command.
+     *
+     * @param fullCommand String to parse.
+     * @return AddCommand instance.
+     */
+    public Command parseAddDeadlineCommand(String fullCommand) {
+        String dataString = fullCommand.replaceFirst("deadline ", "");
+        String[] data = dataString.split("/by ");
+        Task task = new Deadline(data[0], LocalDate.parse(data[1]));
+        return new AddCommand(task);
+    }
+
+    /**
+     * Parses add event command.
+     *
+     * @param fullCommand String to parse.
+     * @return AddCommand instance.
+     */
+    public Command parseAddEventCommand(String fullCommand) {
+        String dataString = fullCommand.replaceFirst("event ", "");
+        String[] data = dataString.split("/at ");
+        Task task = new Event(data[0], LocalDate.parse(data[1]));
+        return new AddCommand(task);
+    }
+
+    /**
+     * Parses add todo command.
+     *
+     * @param fullCommand String to parse.
+     * @throws DukeException If command is not recognized.
+     * @return AddCommand instance.
+     */
+    public Command parseAddTodoCommand(String fullCommand) throws DukeException {
+        String dataString = fullCommand.replaceFirst("todo ", "");
+        if (dataString.trim().equals("")) {
+            throw new DukeException("Todo needs a description");
+        }
+        Task task = new Todo(dataString);
+        return new AddCommand(task);
+    }
+
+    /**
+     * Parses list command.
+     *
+     * @param fullCommand String to parse.
+     * @return ListCommand instance.
+     */
+    public Command parseListCommand() {
+        return new ListCommand();
+    }
+
+    /**
+     * Parses delete command.
+     *
+     * @param fullCommand String to parse.
+     * @return DeleteCommand instance.
+     */
+    public Command parseDeleteCommand(String fullCommand) {
+        String dataString = fullCommand.replaceFirst("delete ", "");
+        int taskNumber = Integer.parseInt(dataString);
+        return new DeleteCommand(taskNumber);
+    }
+
+    /**
+     * Parses mark command.
+     *
+     * @param fullCommand String to parse.
+     * @return MarkCommand instance.
+     */
+    public Command parseMarkCommand(String fullCommand) {
+        String dataString = fullCommand.replaceFirst("mark ", "");
+        int taskNumber = Integer.parseInt(dataString);
+        return new MarkCommand(taskNumber);
+    }
+
+    /**
+     * Parses unmark command.
+     *
+     * @param fullCommand String to parse.
+     * @return UnmarkCommand instance.
+     */
+    public Command parseUnmarkCommand(String fullCommand) {
+        String dataString = fullCommand.replaceFirst("unmark ", "");
+        int taskNumber = Integer.parseInt(dataString);
+        return new UnmarkCommand(taskNumber);
+    }
+
+    /**
+     * Parses exit command.
+     *
+     * @param fullCommand String to parse.
+     * @return ExitCommand instance.
+     */
+    public Command parseExitCommand() {
+        return new ExitCommand();
     }
 
 }
