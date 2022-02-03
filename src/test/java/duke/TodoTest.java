114e6c89ad427aab69c18035cfb0171f0f957529
Ryan Aidan <aidanaden@hotmail.com>
2022-02-04 04:40:15 +0800

    Add checkstyle 8.1

--- src/test/java/duke/TodoTest.java
+++ src/test/java/duke/TodoTest.java
@@ -1,10 +1,12 @@
 package duke;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TodoTest {
     @Test
+    @DisplayName("TODO toString check")
     void todoToString() {
         Todo todo = new Todo("New todo");
         Assertions.assertEquals("[T][ ] New todo ", todo.toString());
