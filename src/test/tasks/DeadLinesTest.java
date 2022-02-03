c0186924c81eac4b107c416fbdcc853feba6b9d1
Tan De Shao <e0544387@u.nus.edu>
2022-02-03 17:04:51 +0800

    some changes

--- src/test/tasks/DeadLinesTest.java
+++ src/test/tasks/DeadLinesTest.java
@@ -1,7 +1,8 @@
 package tasks;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Test class for DeadLines.
