cadbdacc584a1d810e4149836847bc2c8d92b68c
isabelteo <e0563890@u.nus.edu>
2022-02-03 22:57:38 +0800

    Add JavaFx Level 4

--- src/main/java/seedu/duke/chatbot/Duke.java
+++ src/main/java/seedu/duke/chatbot/Duke.java
@@ -6,34 +6,19 @@ import seedu.duke.task.TaskList;
 
 import java.io.IOException;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
 
 /**
  * Functions as the chatbot by taking in inputs.
  * Also helps in giving out specified outputs.
  */
-public class Duke extends Application {
+public class Duke {
     private Storage storage;
     private TaskList taskList;
     private Ui ui;
 
-
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
+    public Duke() {
+        //for JavaFx to run
+    }
     /**
      * Creates the chatbot.
      * @param filePath which provides the path to the database, a txt file storing old tasks
@@ -67,34 +52,16 @@ public class Duke extends Application {
         }
     }
 
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
-
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        scene = new Scene(mainLayout);
-
-        stage.setScene(scene);
-        stage.show();
-
-        // more code to be added here later
-    }
-
     /**
      * Runs the chatbot Duke with a specified path to the database file.
      */
     public static void main(String[] args) {
         String filePath = System.getProperty("user.dir")  + "/data/OldTasks.txt";
         new Duke(filePath).run();
+    }
+
+
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
