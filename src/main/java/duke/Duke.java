4d98d92f8a145ad79f88b056cba92c98fa557e03
Justinnje <e0543778@u.nus.edu>
2022-02-04 15:48:20 +0800

    Change the arrangement of the Main Method

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -8,24 +8,29 @@ package duke;
  */
 public class Duke {
 
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n\n";
-        String start = "Hello! I am Duke.\n"
-                + "Your Personal Assistant.\n\n"
-                + "Input 'help' for the Command Manual!!\n\n"
-                + "What can I do for you?\n"
-                + "__________________________________________";
-        System.out.println(logo + start);
+    public static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n\n";
 
+    public static String start = "Hello! I am Duke.\n"
+            + "Your Personal Assistant.\n\n"
+            + "Input 'help' for the Command Manual!!\n\n"
+            + "What can I do for you?\n"
+            + "__________________________________________";
+
+    private static void run() {
+        System.out.println(logo + start);
         TaskList taskList = new TaskList();
         Storage storage = new Storage(taskList);
         Ui ui = new Ui(taskList, storage);
 
         ui.run();
+    }
+
+    public static void main(String[] args) {
+        run();
     }
 }
 
