dbc41f805f26fafd7305c46061fbfd7f6acfeeea
Seah Zhi Xuan <e0659847@u.nus.edu>
2022-02-04 18:27:54 +0800

    Add simple GUI with JavaFX

---
+++ src/main/java/myboss/DialogBox.java
@@ -0,0 +1,56 @@
+package myboss;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+
+public class DialogBox extends HBox {
+
+    private Label text;
+    private ImageView displayPicture;
+
+    public DialogBox(Label l, ImageView iv) {
+        text = l;
+        displayPicture = iv;
+
+        this.setSpacing(10);
+
+        text.setWrapText(true);
+        Circle clip = new Circle(75,75,75);
+        /*displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);*/
+        displayPicture.setClip(clip);
+
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.getChildren().addAll(text, displayPicture);
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(Label l, ImageView iv) {
+        DialogBox dialogBox = new DialogBox(l, iv);
+        dialogBox.setPadding(new Insets(10));
+        return dialogBox;
+    }
+
+    public static DialogBox getDukeDialog(Label l, ImageView iv) {
+        var db = new DialogBox(l, iv);
+        db.flip();
+        db.setPadding(new Insets(10));
+        return db;
+    }
+}
