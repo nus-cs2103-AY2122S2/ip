5049a11e85be1009d9c63e65d54b25cceec5acd5
Han Jiyao <hanjiyao0@gmail.com>
2022-02-05 18:03:46 +0800

    update find

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -14,14 +14,19 @@ public class FindCommand extends Command {
     public void execute(TaskList tasks, Ui ui, Storage storage) {
         StringBuilder searchText = new StringBuilder();
         searchText.append("Here are matching tasks \n");
+        boolean isFound = false;
         for (int i = 0; i < tasks.getSize(); i++) {
             if (tasks.getByIndex(i).getTask().contains(keyword.trim())) {
+                isFound = true;
                 searchText.append("    ").append(i + 1).append(". ")
-                        .append(tasks.getByIndex(i))
-                        .append("\n");
+                        .append(tasks.getByIndex(i)).append("\n");
             }
         }
-        searchText.delete(searchText.length() - 1, searchText.length());
-        ui.showMessage(searchText.toString());
+        if (isFound) {
+            searchText.delete(searchText.length() - 1, searchText.length());
+            ui.showMessage(searchText.toString());
+        } else {
+            ui.showMessage("Sorry no result found");
+        }
     }
 }
