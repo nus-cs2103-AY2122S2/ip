48dd15eb5f12bd374103ac18b009ffc2cc2ed2c8
medajet <jettaneekiat@gmail.com>
2022-02-05 22:03:37 +0800

    Use assert in code for A-Assertions

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -36,6 +36,7 @@ public class Duke {
      */
     String getResponse(String input) {
         input = input.toLowerCase().trim();
+        assert (!input.equals("")): "Please enter an input.";
         try {
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             PrintStream printStream = new PrintStream(outputStream);
