77d4ca5d69b7977ac9102594c87527af0d4a31a5
fantablack <fantablack@users.noreply.github.com>
2022-02-06 16:53:32 +0800

    Seperate UI functionality into Ui.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,21 +8,8 @@ import java.util.Scanner;
 public class Duke {
     ArrayList<Task> tasks;
 
-    private static final String BAR = "____________________________________________________________";
-
     private Duke() {
         tasks = new ArrayList<>();
-    }
-
-    /**
-     * @param curr Task object to be printed
-     */
-    private void printTask(Task curr) {
-        System.out.println(curr.getTaskIcon() + " [" + curr.isDone() + "]" + curr);
-    }
-
-    private void printNoOfTasks() {
-        System.out.println("Now you have " + tasks.size() + " tasks in the list");
     }
 
     /**
@@ -30,25 +17,15 @@ public class Duke {
      */
     private void processNewTask(Task curr) {
         tasks.add(curr);
-        printTask(curr);
-        printNoOfTasks();
+        Ui.printFeedbackFooter("Got it. I've added this task:", curr, tasks);
     }
 
     private void handleBye() {
-        System.out.println(BAR);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(BAR);
+        Ui.printBye();
     }
 
     private void handleList() {
-        System.out.println(BAR);
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 1; i <= tasks.size(); i++) {
-            Task curr = tasks.get(i - 1);
-            System.out.print(i + ".");
-            printTask(curr);
-        }
-        System.out.println(BAR);
+        Ui.printList(tasks);
     }
 
     /**
@@ -59,10 +36,7 @@ public class Duke {
         Task curr = tasks.get(number - 1);
 
         curr.setDone();
-        System.out.println(BAR);
-        System.out.println("Nice! I've marked this task as done:");
-        printTask(curr);
-        System.out.println(BAR);
+        Ui.printFeedbackFooter("Nice! I've marked this task as done:", curr, tasks);
     }
 
     /**
@@ -73,10 +47,7 @@ public class Duke {
         Task curr = tasks.get(number - 1);
 
         curr.setUndone();
-        System.out.println(BAR);
-        System.out.println("OK, I've marked this task as not done yet:");
-        printTask(curr);
-        System.out.println(BAR);
+        Ui.printFeedbackFooter("OK, I've marked this task as not done yet:", curr, tasks);
     }
 
     /**
@@ -89,53 +60,38 @@ public class Duke {
                 throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
             }
         } catch (DukeException e) {
-            System.err.println(e.getMessage());
-            System.out.println("Please try again:");
-            System.out.println(BAR);
+            Ui.printDukeException(e, "Please try again:");
             return;
         }
 
-        System.out.println(BAR);
-        System.out.println("Got it. I've added this task:");
-        Task curr = new Todo(originalInput.replaceAll(".*\\btodo\\.*", ""));
+        Task curr = new Todo(originalInput.substring(4));
         processNewTask(curr);
-        System.out.println(BAR);
     }
 
     private void handleDeadline(String originalInput) {
         String metaInfo = originalInput.split("/by")[1];
-        String strippedCommand = originalInput.replaceAll(".*\\bdeadline\\.*", "");
-        System.out.println(BAR);
+        String strippedCommand = originalInput.substring(8);
         Task curr = null;
         try {
-            curr = new Deadline(strippedCommand.split("/")[0], metaInfo);
+            curr = new Deadline(" " + strippedCommand.split("/")[0], metaInfo);
         } catch (DateTimeParseException ex) {
-            System.err.println("Kindly input Date and Time in dd/mm/yyyy hhmm format!");
-            System.out.println("Please try again:");
-            System.out.println(BAR);
+            Ui.printMessage("Kindly input Date and Time in dd/mm/yyyy hhmm format!\nPlease try again:");
             return;
         }
-        System.out.println("Got it. I've added this task:");
         processNewTask(curr);
-        System.out.println(BAR);
     }
 
     private void handleEvent(String originalInput) {
         String metaInfo = originalInput.split("/at")[1];
-        String strippedCommand = originalInput.replaceAll(".*\\bevent\\.*", "");
-        System.out.println(BAR);
+        String strippedCommand = originalInput.substring(5);
         Task curr = null;
         try {
-            curr = new Event(strippedCommand.split("/")[0], metaInfo);
+            curr = new Event(" " + strippedCommand.split("/")[0], metaInfo);
         } catch (DateTimeParseException ex) {
-            System.err.println("Kindly input Date and Time in dd/mm/yyyy hhmm format!");
-            System.out.println("Please try again:");
-            System.out.println(BAR);
+            Ui.printMessage("Kindly input Date and Time in dd/mm/yyyy hhmm format!\nPlease try again:");
             return;
         }
-        System.out.println("Got it. I've added this task:");
         processNewTask(curr);
-        System.out.println(BAR);
     }
 
     private void handleDelete(String[] inputArray) {
@@ -146,21 +102,14 @@ public class Duke {
                 throw new DukeException("Hey! That item does not exist!");
             }
         } catch (DukeException e) {
-            System.err.println(e.getMessage());
-            System.out.println("Please try again:");
-            System.out.println(BAR);
+            Ui.printDukeException(e, "Please try again");
             return;
         }
 
         Task curr = tasks.get(number - 1);
-        String message = curr.getTaskIcon() + " [" + curr.isDone() + "]" + curr;
         tasks.remove(curr);
 
-        System.out.println(BAR);
-        System.out.println("Noted. I've removed this task:");
-        System.out.println(message);
-        printNoOfTasks();
-        System.out.println(BAR);
+        Ui.printFeedbackFooter("Noted. I've removed this task:", curr, tasks);
     }
 
     /**
@@ -173,12 +122,10 @@ public class Duke {
         Storage storageHandler = new Storage();
         this.tasks = (ArrayList<Task>) storageHandler.loadTasksFromFile();
 
-        Scanner sc = new Scanner(System.in);
+        String botName = "Kizer";
+        Ui.printGreeting(botName);
 
-        String greeting = "Hello! I'm Kizer\nWhat can I do for you?";
-        System.out.println(BAR);
-        System.out.println(greeting);
-        System.out.println(BAR);
+        Scanner sc = new Scanner(System.in);
 
         boolean canContinue = true;
         while (canContinue) {
@@ -214,8 +161,7 @@ public class Duke {
                     handleDelete(inputArray);
                     break;
                 default:
-                    System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
-                    System.out.println("Please try again:");
+                    Ui.printDontKnowCommand();
                     break;
             }
         }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,86 @@
+import java.util.List;
+
+public final class Ui {
+
+    private static final String BAR = "-".repeat(60);
+
+    private Ui() {
+    }
+
+    /**
+     * Main printing method that formats the output with BAR pre and post output
+     * Most pre-defined printing methods in this class ultimately calls this method
+     * for the actual printing
+     * 
+     * @param message string to be printed
+     */
+    public static void printMessage(String message) {
+        System.out.println(BAR + "\n" + message + "\n" + BAR);
+    }
+
+    public static void printGreeting(String botName) {
+        String greeting = "Hello! I'm " + botName + "\nWhat can I do for you?";
+        printMessage(greeting);
+    }
+
+    public static void printBye() {
+        printMessage("Bye. Hope to see you again soon!");
+    }
+
+    public static void printDontKnowCommand() {
+        String message = "OOPS!!! I'm sorry, but I don't know what that means :-(\nPlease try again:";
+        printMessage(message);
+    }
+
+    public static void printFeedbackFooter(String message, Task targetTask, List<Task> tasks) {
+        System.out.println(BAR);
+        System.out.println(message);
+        System.out.println(targetTask.getTaskIcon() + " [" + targetTask.isDone() + "]" + targetTask);
+        printNoOfTasks(tasks);
+        System.out.println(BAR);
+    }
+
+    /**
+     * Prints details of an Duke Exception, along with a hint to resolve the error
+     * 
+     * @param e    Exception object
+     * @param hint string message containing hint to resolve the error
+     *             e.g. "Please try again" or "Please input in format XXXX"
+     */
+    public static void printDukeException(Exception e, String hint) {
+        System.out.println(BAR);
+        System.err.println(e.getMessage());
+        System.out.println(hint);
+        System.out.println(BAR);
+    }
+
+    public static void printList(List<Task> tasks) {
+        System.out.println(BAR);
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 1; i <= tasks.size(); i++) {
+            Task curr = tasks.get(i - 1);
+            System.out.print(i + ".");
+            printTask(curr);
+        }
+        System.out.println(BAR);
+    }
+
+    /**
+     * @param curr Task object to be printed
+     */
+    private static void printTask(Task curr) {
+        System.out.println(curr.getTaskIcon() + " [" + curr.isDone() + "]" + curr);
+    }
+
+    /**
+     * Prints tasks in custom format without bars
+     * Should be called directly outside of this class as this is intended as an
+     * internal helper class
+     * with no extra BAR formatting pre and post output
+     * 
+     * @param tasks
+     */
+    private static void printNoOfTasks(List<Task> tasks) {
+        System.out.println("Now you have " + tasks.size() + " tasks in the list");
+    }
+}
