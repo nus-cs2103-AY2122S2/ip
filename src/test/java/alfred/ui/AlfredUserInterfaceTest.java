54f39a2ae71def7b981cb460ed48272ff9a68df9
tanyjnaaman <67091469+tanyjnaaman@users.noreply.github.com>
2022-02-07 00:16:27 +0800

    Set up continuous integration
    Set up CI with gradle.yml file. Also fixed relevant formatting issues
    flagged by gradle and added a checkstyle.xml file.

--- src/test/java/alfred/ui/AlfredUserInterfaceTest.java
+++ src/test/java/alfred/ui/AlfredUserInterfaceTest.java
@@ -6,27 +6,30 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Encapsulates tests for Alfred CLI user interface.
+ */
 public class AlfredUserInterfaceTest {
-  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-  @Test
-  public void sandwichAndPrint_test() {
+    @Test
+    public void sandwichAndPrint_test() {
 
-    // generate expected
-    String line = "";
-    for (int i = 0; i < 100; i++) {
-      line += "-";
+        // generate expected
+        String line = "";
+        for (int i = 0; i < 100; i++) {
+            line += "-";
+        }
+        String expected = line + "\n" + "expected" + "\n" + line + "\n" + "\n";
+
+        // set
+        System.setOut(new PrintStream(this.outContent));
+
+        // test
+        AlfredUserInterface ui = new AlfredUserInterface();
+        ui.sandwichAndPrint("expected");
+        assertEquals(expected.trim().replace("\r", ""), outContent.toString().trim().replace("\r", ""));
     }
-    String expected = line + "\n" + "expected" +"\n" + line + "\n" + "\n";
-
-    // set
-    System.setOut(new PrintStream(this.outContent));
-
-    // test
-    AlfredUserInterface ui = new AlfredUserInterface();
-    ui.sandwichAndPrint("expected");
-    assertEquals(expected.trim().replace("\r",""), outContent.toString().trim().replace("\r",""));
-  }
 
 }
 
