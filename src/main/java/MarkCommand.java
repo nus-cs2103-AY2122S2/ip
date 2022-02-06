db98dc60e4e9ba5fef2c97684fd2ea57956a4489
juayhee <e0524722@u.nus.edu>
2022-02-06 12:22:58 +0800

    MarkCommand.java: Deprecate unused code

--- src/main/java/MarkCommand.java
+++ src/main/java/MarkCommand.java
@@ -13,20 +13,6 @@ public class MarkCommand extends Command {
         index = Integer.parseInt(input) - 1; // -1 because list starts at 1 while indexes start at 0
     }
 
-    private String formatOutput() {
-        String output = "";
-        String taskString = taskList.get(index).toString();
-
-        output += Duke.line();
-        output += Duke.newLine(Duke.indent("Task(s) marked as done: ", 1));
-        output += Duke.indent("", 2);
-        output += Duke.newLine(taskString);
-        output += Duke.line();
-
-        return output;
-
-    }
-
     @Override
     public void execute() {
         Task task = taskList.get(index);
