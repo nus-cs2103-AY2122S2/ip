6ed0f74058b6ff694cd412283f9eb94a7453e721
Ria Khaitan <ria@u.nus.edu>
2022-02-06 18:19:35 +0800

    add level 7 to branch-level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,8 @@ import exceptions.DukeExceptions;
 import exceptions.DukeInvalidInput;
 import exceptions.DukeInvalidTodo;
 
+import java.io.*;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -22,12 +24,94 @@ public class Duke {
         return newText.trim();
     }
 
-    public static void main(String[] args) throws DukeExceptions {
+    public static void writeToFile(ArrayList<Task> list) throws IOException {
+        try {
+            File f = new File("/Users/riakhaitan/iP/ip/data/duke.txt");
+            f.createNewFile();
 
+            boolean directory = f.getParentFile().mkdirs();
+            FileWriter writer = new FileWriter(f, false);
+            for (Task task : list) {
+                String fileInput;
+                Task ele = task;
+                if (ele instanceof ToDo) {
+                    fileInput = "[T][" + ele.getStatusIcon() + "]/" + ele.description;
+                } else if (ele instanceof Deadline) {
+                    Deadline deadL = (Deadline) ele;
+                    fileInput = "[D][" + deadL.getStatusIcon() + "]/" + deadL.description + "/" + deadL.by;
+                } else {
+                    Event eve = (Event) ele;
+                    fileInput = "[E][" + eve.getStatusIcon() + "]/" + eve.description + "/" + eve.at;
+                }
+                writer.write(fileInput + "\n");
+                writer.flush();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void loadFromFile(ArrayList<Task> list) throws IOException {
+        File f = new File("/Users/riakhaitan/iP/ip/data/duke.txt");
+        f.createNewFile();
+        String input, desc;
+        Task t;
+        Scanner in = new Scanner(f);
+        while(in.hasNextLine()) {
+            input = in.nextLine();
+            String[] tSplit = input.split("/");
+            String[] splitT = tSplit[0].split("]");
+            switch(splitT[0]) {
+                case "[T":
+                    t = new ToDo(tSplit[1]);
+                    break;
+                case "[E":
+                    t = new Event(tSplit[1], tSplit[2]);
+                    break;
+                case "[D":
+                    t = new Deadline(tSplit[1], tSplit[2]);
+                    break;
+                default:
+                    t = new Task("eee");
+                    break;
+            }
+            if(splitT[1].equals("[X")) {
+                t.done();
+                list.add(t);
+            }
+            else {
+                list.add(t);
+            }
+        }
+        printList(list, list.size());
+        in.close();
+    }
+
+    public static int exitHalloumi() {
+        lineOne();
+        System.out.println("See you soon! Have a good day ^_^");
+        lineOne();
+        return 1;
+    }
+
+    public static void printList(ArrayList<Task> list, int size) {
+        lineOne();
+        System.out.println("List:");
+        if(list.isEmpty()) {
+            System.out.println("No tasks to complete! ^_^");
+            return;
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.println(i + 1 + ". " + list.get(i));
+        }
+        lineOne();
+    }
+
+    public static void main(String[] args) throws DukeExceptions, IOException {
         Scanner sc = new Scanner(System.in);
         int num = 0;
-        int counter = 0;
         ArrayList<Task> lists = new ArrayList<Task>();
+        loadFromFile(lists);
         lineOne();
         System.out.println("Hi! I'm Halloumi ^_^");
         System.out.println("What do you need help with today?");
@@ -40,14 +124,10 @@ public class Duke {
             try {
                 switch (textSplit[0]) {
                     case "bye":
-                        num = 1;
-                        System.out.println("See you soon! Have a good day ^_^");
-                        lineOne();
+                        num = exitHalloumi();
                         break;
                     case "list":
-                        for (int i = 0; i < lists.size(); i++) {
-                            System.out.println(i + 1 + ". " + lists.get(i));
-                        }
+                        printList(lists, lists.size());
                         break;
                     case "mark":
                         lineOne();
@@ -74,7 +154,6 @@ public class Duke {
                         int t2No = Integer.parseInt(textSplit[1]);
                         System.out.println(lists.get(t2No - 1));
                         lists.remove(t2No - 1);
-
                         lineOne();
                         break;
                     case "todo":
@@ -87,22 +166,31 @@ public class Duke {
                             System.err.println(e.getMessage());
                             continue;
                         }
+                        lineTwo();
                         System.out.println("New task added:");
                         Task t = new ToDo(fullDesc);
                         lists.add(t);
                         System.out.println(t);
+                        System.out.println("You have " + lists.size() + " tasks left now! ^_^");
+                        lineTwo();
                         break;
                     case "event":
+                        lineTwo();
                         System.out.println("New task added:");
                         Task t1 = new Event(fullDesc, textSplitOne[1]);
                         lists.add(t1);
                         System.out.println(t1);
+                        System.out.println("You have " + lists.size() + " tasks left now! ^_^");
+                        lineTwo();
                         break;
                     case "deadline":
+                        lineTwo();
                         System.out.println("New task added:");
                         Task t2 = new Deadline(fullDesc, textSplitOne[1]);
                         lists.add(t2);
                         System.out.println(t2);
+                        System.out.println("You have " + lists.size() + " tasks left now! ^_^");
+                        lineTwo();
                         break;
                     default:
                         throw new DukeInvalidInput();
@@ -113,6 +201,7 @@ public class Duke {
             }
         }
             while (num == 0) ;
-
+            writeToFile(lists);
     }
 }
+
