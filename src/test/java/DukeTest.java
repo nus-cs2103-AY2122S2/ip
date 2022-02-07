f5d443da7bbf8b53f53730d129af94551a757a0d
tiewweijian <tiewweijian@u.nus.edu>
2022-02-07 21:25:11 +0800

    Rectify checkstyle violations on test classes

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,23 +1,23 @@
-import duke.*;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import duke.Deadline;
+import duke.Event;
+import duke.ToDos;
+
 
 public class DukeTest {
 
     @Test
-    public void testEventTask() throws IOException, DukeException, ParseException {
+    public void testEventTask() throws ParseException {
         DateFormat formatter = new SimpleDateFormat("dd/MM/yy h:mm a");
-        assertEquals("hello (at: 10/10/21 10:50 PM)", new Event("hello", formatter.parse("10/10/2021 10:50 PM")).getDescription());
+        assertEquals("hello (at: 10/10/21 10:50 PM)",
+                new Event("hello", formatter.parse("10/10/2021 10:50 PM")).getDescription());
     }
 
     @Test
@@ -28,7 +28,8 @@ public class DukeTest {
     @Test
     public void testDeadlineTask() throws ParseException {
         DateFormat formatter = new SimpleDateFormat("dd/MM/yy h:mm a");
-        assertEquals("party (by: 10/10/21 10:50 PM)", new Deadline("party", formatter.parse("10/10/2021 10:50 PM")).getDescription());
+        assertEquals("party (by: 10/10/21 10:50 PM)",
+                new Deadline("party", formatter.parse("10/10/2021 10:50 PM")).getDescription());
     }
 
 
