8019fe1e64ed8622a5328e6d2c909d1253e83621
juayhee <e0524722@u.nus.edu>
2022-02-07 18:54:52 +0800

    Update ParserTest with more tests

--- src/test/java/duke/ParserTest.java
+++ src/test/java/duke/ParserTest.java
@@ -7,10 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ParserTest {
 
     @Test
-    public void makeCommandTest() {
+    public void makeCommand_ByeAsInput_ReturnByeCommand() {
         Parser p = new Parser();
         Command c = p.makeCommand("bye");
         boolean result = c instanceof ByeCommand;
+        assertEquals(result, true);
+    }
+
+    @Test
+    public void makeCommand_ListAsInput_ReturnListCommand() {
+        Parser p = new Parser();
+        Command c = p.makeCommand("list");
+        boolean result = c instanceof ListCommand;
         assertEquals(result, true);
     }
 }
