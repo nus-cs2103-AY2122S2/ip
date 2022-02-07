470ab8a4a0c4a6d636617678eb644c8c9a424a3c
NicsunXnus <e0543645@u.nus.edu>
2022-02-07 20:32:08 +0800

    add help button

--- src/main/java/duke/gui/MainWindow.java
+++ src/main/java/duke/gui/MainWindow.java
@@ -5,11 +5,15 @@ import duke.ui.Duke;
 import duke.ui.DukeException;
 import duke.ui.Parser;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.input.InputEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
@@ -62,5 +66,43 @@ public class MainWindow extends AnchorPane {
         } finally {
             userInput.clear();
         }
+    }
+
+    /**
+     * Handle action related to "About" menu item.
+     *
+     * @param event Event on "About" menu item.
+     */
+    @FXML
+    private void handleAboutAction(final ActionEvent event)
+    {
+        provideAboutFunctionality();
+    }
+
+    /**
+     * Handle action related to input (in this case specifically only responds to
+     * keyboard event CTRL-A).
+     *
+     * @param event Input event.
+     */
+    @FXML
+    private void handleKeyInput(final InputEvent event)
+    {
+        if (event instanceof KeyEvent)
+        {
+            final KeyEvent keyEvent = (KeyEvent) event;
+            if (keyEvent.isControlDown() && keyEvent.getCode() == KeyCode.A)
+            {
+                provideAboutFunctionality();
+            }
+        }
+    }
+
+    /**
+     * Perform functionality associated with "About" menu selection or CTRL-A.
+     */
+    private void provideAboutFunctionality()
+    {
+        System.out.println("You clicked on About!");
     }
 }
