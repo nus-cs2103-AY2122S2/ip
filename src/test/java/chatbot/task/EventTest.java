671cd5a5e5f54fb34ba6f2d4a0dbbd0cfda05bc4
TypeDefinition <lnxterry@gmail.com>
2022-02-08 00:09:55 +0800

    Fix unit test code style issues

--- src/test/java/chatbot/task/EventTest.java
+++ src/test/java/chatbot/task/EventTest.java
@@ -1,8 +1,10 @@
 package chatbot.task;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
+
 import org.junit.jupiter.api.Test;
 
 public class EventTest {
