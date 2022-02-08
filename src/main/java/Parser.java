99a6061d08aa9922feb1bf2b87d676f5cf2c680d
kxshxsh <kashish.v.varshney@gmail.com>
2022-02-08 18:00:17 +0800

    further abstracted out functions into the parser class

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,5 +1,28 @@
 public class Parser {
 
+    public static Task parseCommandFromFile(String[] details) {
+        details[0] = details[0].trim();
+        int status = Integer.parseInt(details[1].trim());
+        Task currTask;
+        if (details[0].equals("T")) {
+            currTask = new Todo(details[2].trim());
+        } else if (details[0].equals("D")) {
+            currTask = new Deadline(details[2].trim(), details[3].trim());
+        } else {
+            String eventInformation = details[3];
+            int indexOfDifferentiator = eventInformation.indexOf("-");
+            String dateAndStartTime = eventInformation.substring(0, indexOfDifferentiator).trim();
+            String endTime = eventInformation.substring(indexOfDifferentiator + 1).trim();
+            currTask = new Event(details[2].trim(), dateAndStartTime, endTime);
+        }
+
+        if (status == 1) {
+            currTask.markDone();
+        }
+
+        return currTask;
+    }
+
     public Boolean isValidCommand(String taskType) {
         return taskType.equals("todo") || taskType.equals("deadline")
                 || taskType.equals("event");
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -21,27 +21,9 @@ public class Storage {
             while (sc.hasNextLine()) {
                 String input = sc.nextLine();
                 String[] details = input.split(";", 4);
-                details[0] = details[0].trim();
-                int status = Integer.parseInt(details[1].trim());
-                Task currTask;
-                if (details[0].equals("T")) {
-                    currTask = new Todo(details[2].trim());
-                } else if (details[0].equals("D")) {
-                    currTask = new Deadline(details[2].trim(), details[3].trim());
-                } else {
-                    String eventInformation = details[3];
-                    int indexOfDifferentiator = eventInformation.indexOf("-");
-                    String dateAndStartTime = eventInformation.substring(0, indexOfDifferentiator).trim();
-                    String endTime = eventInformation.substring(indexOfDifferentiator + 1).trim();
-                    currTask = new Event(details[2].trim(), dateAndStartTime, endTime);
-                }
-
-                if (status == 1) {
-                    currTask.markDone();
-                }
-
+                Task currTask = Parser.parseCommandFromFile(details);
                 TaskList.manager.add(currTask);
-            }
+            }lis
             sc.close();
         } catch (IOException e) {
             throw new CustomException("File cannot be created");
