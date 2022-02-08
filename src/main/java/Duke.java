0d9e8c2ea039eeef060bf67a664ed06a545265c7
Ria Khaitan <ria@u.nus.edu>
2022-02-08 23:46:06 +0800

    coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -35,42 +35,38 @@ public class Duke {
             String fullDesc = Parser.makeDesc(textSplit, textSplit.length);
             try {
                 switch (textSplit[0]) {
-                    case "bye":
-                        num = Ui.exitHalloumi();
-                        break;
-                    case "list":
+                case "bye":
+                    num = Ui.exitHalloumi();
+                    break;
+                case "list":
                         Ui.printList(lists, lists.size());
                         break;
-                    case "mark":
-                        taskList.mark(textSplit[1], lists);
-                        break;
-                    case "unmark":
+                case "unmark":
                         taskList.unmark(textSplit[1], lists);
                         break;
-                    case "delete" :
-                        taskList.delete(textSplit[1], lists);
-                        break;
-                    case "todo":
-                        try {
-                            if(fullDesc.equals(" ") || fullDesc.equals("")) {
-                                throw new DukeInvalidTodo();
-                            }
+                case "delete" :
+                    taskList.delete(textSplit[1], lists);
+                    break;
+                case "todo":
+                    try {
+                        if(fullDesc.equals(" ") || fullDesc.equals("")) {
+                            throw new DukeInvalidTodo();
                         }
-                        catch(DukeInvalidTodo e) {
-                            System.err.println(e.getMessage());
-                            continue;
-                        }
-                        taskList.todo(fullDesc, lists);
-                        break;
-                    case "event":
-                        taskList.event(fullDesc, textSplitOne[1], lists);
-                        break;
-                    case "deadline":
-                        taskList.deadline(fullDesc, textSplitOne[1], lists);
-                        break;
-                    default:
-                        throw new DukeInvalidInput();
-
+                    }
+                    catch(DukeInvalidTodo e) {
+                        System.err.println(e.getMessage());
+                        continue;
+                    }
+                    taskList.todo(fullDesc, lists);
+                    break;
+                case "event":
+                    taskList.event(fullDesc, textSplitOne[1], lists);
+                    break;
+                case "deadline":
+                    taskList.deadline(fullDesc, textSplitOne[1], lists);
+                    break;
+                default:
+                    throw new DukeInvalidInput();
                 }
             } catch (DukeInvalidInput e) {
                 System.err.println(e.getMessage());
