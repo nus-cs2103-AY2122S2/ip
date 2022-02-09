ad330363d3eba179a98e3e3753b76045c550fc83
Tan Jie Wei <>
2022-02-09 16:34:58 +0800

    Improve code quality and remove unnecessary files

--- src/main/java/Duke/Main.java
+++ src/main/java/Duke/Main.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    private TestUi testUi = new TestUi();
+    private Duke duke = new Duke();
 
     @Override
     public void start(Stage stage) {
@@ -22,7 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setTestUi(testUi);
+            fxmlLoader.<MainWindow>getController().setTestUi(duke);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
