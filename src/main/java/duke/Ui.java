6823770e278d819ad6aef640fcf743a8e83d3fb5
manu2002g <gimhan.mg@gmail.com>
2022-02-09 17:03:37 +0800

    Complete JavaDocs

--- src/main/java/duke/Ui.java
+++ src/main/java/duke/Ui.java
@@ -1,5 +1,9 @@
 package duke;
 
+/**
+ * The Ui class handles the elements of the user interface to be
+ * displayed at startup
+ */
 public class Ui {
     private static final String divider = "~~~~~~~~~~~~~~~~~~~~~~~";
     private static final String prince = "______       _\n"
@@ -9,16 +13,28 @@ public class Ui {
             + "| |   | |   | || | | || (__|  __/\n"
             + "\\_|   |_|   |_||_| |_| \\___|\\___|\n";
 
+    /**
+     * Constructor for Ui
+     */
     public Ui() {
         System.out.println("Hello I'm\n" + prince);
         System.out.println("How can I help you today?");
         System.out.println(divider);
     }
 
+    /**
+     * Prints the divider that separates the output from a new command
+     */
     void printDivider() {
         System.out.println(divider);
     }
 
+    /**
+     * Used to display an error if a DukeException occurs
+     * during the execution of Prince.
+     *
+     * @param e The DukeException that was thrown
+     */
     void showError(DukeException e) {
         System.out.println(e.getMessage());
         System.out.println(divider);
