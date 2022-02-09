5a8f1e776fd8483ec9d719bcf4c755baf6e473d6
Kevin <thamooie@yahoo.com.sg>
2022-02-09 14:07:55 +0800

    Fix unused imports

--- src/main/java/com/duke/Main.java
+++ src/main/java/com/duke/Main.java
@@ -8,12 +8,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
 import javafx.stage.Stage;
 
 /**
