657f923e78fbf648175fcd734eb8f6587802a09b
zxgoh <gohzixuan123@gmail.com>
2022-02-10 00:07:42 +0800

    made minor changes

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,3 +1,4 @@
+
 package duke;
 
 import java.util.ArrayList;
@@ -58,16 +59,15 @@ public class Duke extends Application {
     }
 
     /**
-     * You should have your own function to generate a response to user input.
-     * Replace this stub with your completed method.
+     *
+     * @param input
+     * @return Juke's response based on the command
      */
-
     private String getResponse(String input) {
 
             Outputs op = new Outputs();
 
             while(true) {
-
                 String reply = input;
                 String[] splittedString = input.split(" ");
 
@@ -75,9 +75,7 @@ public class Duke extends Application {
                 file.delete();
                 try {
                     file.createNewFile();
-                } catch (IOException e) {
-                    System.out.println(" ");
-                }
+                } catch (IOException e) { }
                 if (splittedString[0].equals("tag") && splittedString.length == 3) { // check for mark tag
                     assert !itemList.isEmpty() : "List is empty";
                     String tag = "#" + splittedString[2];
@@ -94,16 +92,21 @@ public class Duke extends Application {
                 } else if (splittedString[0].equals("mark") && splittedString.length == 2) { // check for mark tag
                     assert !itemList.isEmpty() : "List is empty";
                     try {
-                    int index = Integer.valueOf(reply.split(" ")[1]);
-                    itemList.get(index - 1).markAsDone();
-                    return op.border +
-                            "     Nice! I've marked this task as done: \n" +
-                            "       " + itemList.get(index - 1).getDescription() + "\n" +
-                            op.instructions +
-                            op.border;
-                } catch (NumberFormatException n) {
-                    return "Invalid input, please enter a valid index number instead";
-                }
+                        int index = Integer.valueOf(reply.split(" ")[1]);
+                        itemList.get(index - 1).markAsDone();
+                        return op.border +
+                                "     Nice! I've marked this task as done: \n" +
+                                "       " + itemList.get(index - 1).getDescription() + "\n" +
+                                op.instructions +
+                                op.border;
+
+                    } catch (NumberFormatException n) {
+                        return "Invalid input, please enter a valid index number instead";
+                    }
+
+                /**
+                 * if command is "unmark {index no.}", mark said task as undone
+                 */
                 } else if (splittedString[0].equals("unmark") && splittedString.length == 2) { // check for unmark tag
                     assert !itemList.isEmpty() : "List is empty";
                     try {
@@ -234,80 +237,65 @@ public class Duke extends Application {
             }
         }
 
-    @Override
-    public void start(Stage stage) {
-        //Step 1. Setting up required components
+        @Override
+        public void start(Stage stage) {
 
-        //The container for the content of the chat to scroll.
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
+            scrollPane = new ScrollPane();
+            dialogContainer = new VBox();
+            scrollPane.setContent(dialogContainer);
 
-        userInput = new TextField();
-        sendButton = new Button("Send");
+            userInput = new TextField();
+            sendButton = new Button("Send");
 
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+            AnchorPane mainLayout = new AnchorPane();
+            mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
-        scene = new Scene(mainLayout);
+            scene = new Scene(mainLayout);
 
-        stage.setScene(scene);
-        stage.show();
+            stage.setScene(scene);
+            stage.show();
 
-        stage.setTitle("Juke");
-        stage.setResizable(false);
-        stage.setMinHeight(600.0);
-        stage.setMinWidth(400.0);
+            stage.setTitle("Juke");
+            stage.setResizable(false);
+            stage.setMinHeight(600.0);
+            stage.setMinWidth(400.0);
 
-        mainLayout.setPrefSize(400.0, 600.0);
+            mainLayout.setPrefSize(400.0, 600.0);
 
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+            scrollPane.setPrefSize(385, 535);
+            scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
 
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
+            scrollPane.setVvalue(1.0);
+            scrollPane.setFitToWidth(true);
 
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+            // You will need to import `javafx.scene.layout.Region` for this.
+            dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
 
-        userInput.setPrefWidth(325.0);
+            userInput.setPrefWidth(325.0);
 
-        sendButton.setPrefWidth(55.0);
+            sendButton.setPrefWidth(55.0);
 
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
+            AnchorPane.setTopAnchor(scrollPane, 1.0);
 
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
+            AnchorPane.setBottomAnchor(sendButton, 1.0);
+            AnchorPane.setRightAnchor(sendButton, 1.0);
 
-        AnchorPane.setLeftAnchor(userInput , 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
+            AnchorPane.setLeftAnchor(userInput , 1.0);
+            AnchorPane.setBottomAnchor(userInput, 1.0);
 
-        //event handler
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
+            //event handler
+            sendButton.setOnMouseClicked((event) -> {
+                handleUserInput();
+            });
 
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
+            userInput.setOnAction((event) -> {
+                handleUserInput();
+            });
+            dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
 
-    }
-
-    /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    private Label getDialogLabel(String text) {
-        // You will need to import `javafx.scene.control.Label`.
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
+        }
 
 
-        return textToAdd;
-    }
 }
 
