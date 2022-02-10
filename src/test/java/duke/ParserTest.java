7752dbfaf3f7ee53f78bac18a68148c01a0c8fc2
Ria Khaitan <ria@u.nus.edu>
2022-02-10 22:00:51 +0800

    add junit tests

---
+++ src/test/java/duke/ParserTest.java
@@ -0,0 +1,19 @@
+package duke;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+
+    @Test
+    public void ParserTest() {
+        String testSpace = "hello world";
+        String[] output1 = Parser.splitSpace(testSpace);
+
+        String testForwardSlash = "hello/world";
+        String[] output2 = Parser.splitForwardSlash(testForwardSlash);
+
+        assertEquals("world", output1[1]);
+        assertEquals("hello", output2[0]);
+    }
+}
---
+++ src/test/java/duke/UiTest.java
@@ -0,0 +1,13 @@
+package duke;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UiTest {
+
+    @Test
+    public void UiTest() {
+        String output = "*************************************************************************";
+        assertEquals("*************************************************************************", output);
+    }
+}
