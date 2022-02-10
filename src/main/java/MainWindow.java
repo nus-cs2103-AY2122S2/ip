22def3fbdf2e279ff20413b2371dd9a9d44551ea
aditi2313 <guptaaditi2313@gmail.com>
2022-02-10 15:40:42 +0800

    Updated JavaFx

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -29,7 +29,7 @@ public class MainWindow extends AnchorPane {
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
         dialogContainer.getChildren().addAll(
-                DialogBox.getDukeDialog(new Label("Hello! I'm main.Duke :) \nWhat can I do for you? :D"), new ImageView(dukeImage))
+                DialogBox.getDukeDialog(new Label(greetUser()), new ImageView(dukeImage))
         );
     }
 
