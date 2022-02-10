cbd5b5a241de097af24e042c1f73104976811c4f
aditi2313 <52269537+aditi2313@users.noreply.github.com>
2022-02-10 17:06:37 +0800

    Update Parser.java

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -23,30 +23,31 @@ public class Parser {
      */
     public String userCommand(String[] inputStringsArray, Storage storage) throws DukeException {
         switch (inputStringsArray[0]) {
-            case "list":
-                return tasks.displayList();
-            case "mark":
-                String marked = tasks.mark(inputStringsArray[1]);
-                storage.updateData(tasks);
-                return marked;
-            case "unmark":
-                String unmarked = tasks.unmark(inputStringsArray[1]);
-                storage.updateData(tasks);
-                return unmarked;
-            case "todo":
-            case "deadline":
-            case "event":
-                String added = tasks.addToList(inputStringsArray);
-                storage.updateData(tasks);
-                return added;
-            case "delete":
-                String deleted = tasks.delete(inputStringsArray[1]);
-                storage.updateData(tasks);
-                return deleted;
-            case "find":
-                return tasks.find(inputStringsArray[1]);
-            default:
-                throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means O.o");
+        case "list":
+            return tasks.displayList();
+        case "mark":
+            String marked = tasks.mark(inputStringsArray[1]);
+            storage.updateData(tasks);
+            return marked;
+        case "unmark":
+            String unmarked = tasks.unmark(inputStringsArray[1]);
+            storage.updateData(tasks);
+            return unmarked;
+        // Fallthrough
+        case "todo":
+        case "deadline":
+        case "event":
+            String added = tasks.addToList(inputStringsArray);
+            storage.updateData(tasks);
+            return added;
+        case "delete":
+            String deleted = tasks.delete(inputStringsArray[1]);
+            storage.updateData(tasks);
+            return deleted;
+        case "find":
+            return tasks.find(inputStringsArray[1]);
+        default:
+            throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means O.o");
         }
     }
 }
