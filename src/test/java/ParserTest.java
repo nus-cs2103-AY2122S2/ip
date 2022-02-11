c42078a65f2b4b4890f4c3539d2d4cd98919b6ad
cindyangXQ <yangc2105@gmail.com>
2022-02-11 10:53:10 +0800

    Modify JUnit tests accordingly

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -16,7 +16,7 @@ public class ParserTest {
                 new Parser(taskList, new Storage("")).parse("list"));
 
         taskList.readFromFile(new Todo("read book"));
-        assertEquals("Here are the tasks in your list:" + System.lineSeparator()
+        assertEquals("Here are the tasks in your list:\n"
                         + "1.[T][ ] read book",
                 new Parser(taskList, new Storage("")).parse("list"));
     }
@@ -24,13 +24,13 @@ public class ParserTest {
     @Test
     public void parse_basic_todo() {
         TaskList taskList = new TaskList();
-        assertEquals("Got it. I've added this task:" + System.lineSeparator()
-                        + "  [T][ ] read book" + System.lineSeparator()
+        assertEquals("Got it. I've added this task:\n"
+                        + "  [T][ ] read book\n"
                         + "Now you have 1 task in the list.",
                 new Parser(taskList, new Storage("")).parse("todo read book"));
 
-        assertEquals("Got it. I've added this task:" + System.lineSeparator()
-                        + "  [T][ ] have lunch" + System.lineSeparator()
+        assertEquals("Got it. I've added this task:\n"
+                        + "  [T][ ] have lunch\n"
                         + "Now you have 2 tasks in the list.",
                 new Parser(taskList, new Storage("")).parse("todo have lunch"));
     }
@@ -38,13 +38,13 @@ public class ParserTest {
     @Test
     public void parse_basic_deadline() {
         TaskList taskList = new TaskList();
-        assertEquals("Got it. I've added this task:" + System.lineSeparator()
-                        + "  [D][ ] read book (by: 02 Jan 2022)" + System.lineSeparator()
+        assertEquals("Got it. I've added this task:\n"
+                        + "  [D][ ] read book (by: 02 Jan 2022)\n"
                         + "Now you have 1 task in the list.",
                 new Parser(taskList, new Storage("")).parse("deadline read book /by 2022-01-02"));
 
-        assertEquals("Got it. I've added this task:" + System.lineSeparator()
-                        + "  [D][ ] have lunch (by: 03 Jan 2022)" + System.lineSeparator()
+        assertEquals("Got it. I've added this task:\n"
+                        + "  [D][ ] have lunch (by: 03 Jan 2022)\n"
                         + "Now you have 2 tasks in the list.",
                 new Parser(taskList, new Storage("")).parse("deadline have lunch /by 2022-01-03"));
     }
@@ -52,13 +52,13 @@ public class ParserTest {
     @Test
     public void parse_basic_event() {
         TaskList taskList = new TaskList();
-        assertEquals("Got it. I've added this task:" + System.lineSeparator()
-                        + "  [E][ ] read book (at: 2-4pm)" + System.lineSeparator()
+        assertEquals("Got it. I've added this task:\n"
+                        + "  [E][ ] read book (at: 2-4pm)\n"
                         + "Now you have 1 task in the list.",
                 new Parser(taskList, new Storage("")).parse("event read book /at 2-4pm"));
 
-        assertEquals("Got it. I've added this task:" + System.lineSeparator()
-                        + "  [E][ ] have lunch (at: 4-6pm)" + System.lineSeparator()
+        assertEquals("Got it. I've added this task:\n"
+                        + "  [E][ ] have lunch (at: 4-6pm)\n"
                         + "Now you have 2 tasks in the list.",
                 new Parser(taskList, new Storage("")).parse("event have lunch /at 4-6pm"));
     }
@@ -67,7 +67,7 @@ public class ParserTest {
     public void parse_basic_mark() {
         TaskList taskList = new TaskList();
         taskList.readFromFile(new Todo("read book"));
-        assertEquals("Nice! I've marked this task as done:" + System.lineSeparator()
+        assertEquals("Nice! I've marked this task as done:\n"
                         + "  [T][X] read book",
                 new Parser(taskList, new Storage("")).parse("mark 1"));
     }
@@ -76,7 +76,7 @@ public class ParserTest {
     public void parse_basic_unmark() {
         TaskList taskList = new TaskList();
         taskList.readFromFile(new Todo("read book"));
-        assertEquals("OK, I've marked this task as not done yet:" + System.lineSeparator()
+        assertEquals("OK, I've marked this task as not done yet:\n"
                         + "  [T][ ] read book",
                 new Parser(taskList, new Storage("")).parse("unmark 1"));
     }
@@ -85,8 +85,8 @@ public class ParserTest {
     public void parse_basic_delete() {
         TaskList taskList = new TaskList();
         taskList.readFromFile(new Todo("read book"));
-        assertEquals("Noted. I've removed this task:" + System.lineSeparator()
-                        + "  [T][ ] read book" + System.lineSeparator()
+        assertEquals("Noted. I've removed this task:\n"
+                        + "  [T][ ] read book\n"
                         + "Now you have 0 task in the list.",
                 new Parser(taskList, new Storage("")).parse("delete 1"));
     }
