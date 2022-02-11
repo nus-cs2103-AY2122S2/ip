5f34de97c11d210a8e69e7eafc2c2d245f5dd18a
Chua Bing Quan <chipneverdies@gmail.com>
2022-02-12 05:38:16 +0800

    Add unit tests for Deadline

---
+++ src/test/java/tasks/DeadlineTest.java
@@ -0,0 +1,55 @@
+package tasks;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeadlineTest {
+    @Test
+    public void clone_uncompletedDeadline_success() {
+        final Deadline expected = new Deadline("Return Book", LocalDate.now());
+        assertEqualDeadlines(expected, expected.clone());
+    }
+
+    @Test
+    public void clone_completedDeadline_success() {
+        final Deadline expected = new Deadline("Return Book", LocalDate.now());
+        expected.isDone = true;
+        assertEqualDeadlines(expected, expected.clone());
+    }
+
+    @Test
+    public void toString_uncompletedDeadline_success() {
+        final String description = "Return Book";
+        final int year = 2022;
+        final int month = 3;
+        final int day = 1;
+
+        final String expected = "[D][ ] " + description + " (by: Mar 01 2022)";
+        final Deadline actual = new Deadline(description, LocalDate.of(year, month, day));
+
+        assertEquals(expected, actual.toString());
+    }
+
+    @Test
+    public void toString_completedDeadline_success() {
+        final String description = "Return Book";
+        final int year = 2022;
+        final int month = 3;
+        final int day = 1;
+
+        final String expected = "[D][X] " + description + " (by: Mar 01 2022)";
+        final Deadline actual = new Deadline(description, LocalDate.of(year, month, day));
+        actual.isDone = true;
+
+        assertEquals(expected, actual.toString());
+    }
+
+    private void assertEqualDeadlines(Deadline d1, Deadline d2) {
+        assertEquals(d1.description, d2.description);
+        assertEquals(d1.isDone, d2.isDone);
+        assertEquals(d1.getBy(), d2.getBy());
+    }
+}
