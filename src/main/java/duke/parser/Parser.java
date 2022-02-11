ce579e587490093c722d4dcc730d6c7676ebe10e
Lai Mei Tin <e0550504@u.nus.edu>
2022-02-12 00:57:07 +0800

    Refactor the parse method in Parser.java

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -2,6 +2,7 @@ package duke.parser;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Optional;
@@ -25,32 +26,30 @@ import duke.exception.DukeException;
  */
 public class Parser {
     private static final int ARRAY_INDEX_OFFSET = 1;
-    private final HashMap<String, String> aliases;
+    private static final String[] COMMANDS = new String[]{
+        "alias", "bye", "clear", "list", "find", "mark",
+        "unmark", "delete", "todo", "deadline", "event"};
+    private static final String[] COMMAND_SHORTCUTS = new String[]{
+        "a", "b", "c", "ls", "f", "m", "um", "del", "t", "d", "e"};
     private final HashSet<String> reservedCommands;
+    private final HashMap<String, String> aliases;
 
     /**
      * Class constructor.
      */
     public Parser() {
-        aliases = new HashMap<>();
         reservedCommands = new HashSet<>();
+        reservedCommands.addAll(Arrays.asList(COMMANDS));
 
-        aliases.put("a", "alias");
-        aliases.put("b", "bye");
-        aliases.put("c", "clear");
-        aliases.put("ls", "list");
-        aliases.put("f", "find");
-        aliases.put("m", "mark");
-        aliases.put("um", "unmark");
-        aliases.put("del", "delete");
-        aliases.put("t", "todo");
-        aliases.put("d", "deadline");
-        aliases.put("e", "event");
-        reservedCommands.addAll(aliases.values());
+        aliases = new HashMap<>();
+        for (int i = 0; i < COMMAND_SHORTCUTS.length; i++) {
+            aliases.put(COMMAND_SHORTCUTS[i], COMMANDS[i]);
+        }
     }
 
     /**
-     * Parses the input string entered by user into a command known to the application.
+     * Dispatches the input string entered by user to appropriate helper parser
+     * matching the command type.
      *
      * @param command the command entered by user.
      * @return a command to be executed by the application.
@@ -59,55 +58,204 @@ public class Parser {
     public Command parse(String command) throws DukeException {
         String[] commandTokens = command.split(" ", 2);
         String primaryCommand = Optional.ofNullable(aliases.get(commandTokens[0])).orElse(commandTokens[0]);
+        String arguments = commandTokens.length > 1 ? commandTokens[1] : "";
+
+        switch (primaryCommand) {
+        case "alias":
+            return parseAliasCommand(arguments);
+        case "bye":
+            return parseByeCommand(arguments);
+        case "clear":
+            return parseClearCommand(arguments);
+        case "list":
+            return parseListCommand(arguments);
+        case "find":
+            return parseFindCommand(arguments);
+        case "mark":
+            return parseMarkCommand(arguments);
+        case "unmark":
+            return parseUnmarkCommand(arguments);
+        case "delete":
+            return parseDeleteCommand(arguments);
+        case "todo":
+            return parseToDoCommand(arguments);
+        case "deadline":
+            return parseDeadlineCommand(arguments);
+        case "event":
+            return parseEventCommand(arguments);
+        default:
+            throw new DukeException("I'm sorry, but I don't know what that means");
+        }
+    }
+
+    /**
+     * Parses an alias command.
+     *
+     * @param arguments arguments to this command.
+     * @return an <code>AliasCommand</code> instance.
+     * @throws DukeException when one or more arguments is missing or illegal.
+     */
+    private Command parseAliasCommand(String arguments) throws DukeException {
+        String[] aliasMapping = arguments.split(" ");
+        if (aliasMapping.length != 2) {
+            throw new DukeException("The alias command takes in exactly 2 arguments");
+        }
+
+        String from = aliasMapping[0];
+        String to = aliasMapping[1];
+        if (reservedCommands.contains(from)) {
+            throw new DukeException("Duke's original command cannot be changed");
+        } else if (!reservedCommands.contains(to)) {
+            throw new DukeException("Alias can only refer to Duke's original command");
+        }
+
+        aliases.put(from, to);
+        return new AliasCommand(from, to);
+    }
+
+    /**
+     * Parses a bye command.
+     *
+     * @param arguments not used.
+     * @return a <code>ByeCommand</code> instance.
+     */
+    private Command parseByeCommand(String arguments) {
+        return new ByeCommand();
+    }
+
+    /**
+     * Parses a clear command.
+     *
+     * @param arguments not used.
+     * @return a <code>ClearTaskCommand</code> instance.
+     */
+    private Command parseClearCommand(String arguments) {
+        return new ClearTaskCommand();
+    }
+
+    /**
+     * Parses a list command.
+     *
+     * @param arguments not used.
+     * @return a <code>ListTaskCommand</code> instance.
+     */
+    private Command parseListCommand(String arguments) {
+        return new ListTaskCommand();
+    }
+
+    /**
+     * Parses a find command.
+     *
+     * @param arguments arguments to this command.
+     * @return a <code>FindTaskCommand</code> instance.
+     */
+    private Command parseFindCommand(String arguments) {
+        return new FindTaskCommand(arguments);
+    }
+
+    /**
+     * Parses a mark command.
+     *
+     * @param arguments arguments to this command.
+     * @return a <code>MarkTaskCommand</code> instance.
+     * @throws DukeException when the argument is missing or is not a number.
+     */
+    private Command parseMarkCommand(String arguments) throws DukeException {
+        if (arguments.equals("")) {
+            throw new DukeException("Seems like the command is incomplete");
+        }
 
         try {
-            switch (primaryCommand) {
-            case "alias":
-                String[] aliasMapping = commandTokens[1].split(" ");
-                String from = aliasMapping[0];
-                String to = aliasMapping[1];
-                if (aliasMapping.length != 2) {
-                    throw new DukeException("The alias command takes in exactly 2 arguments");
-                } else if (reservedCommands.contains(from)) {
-                    throw new DukeException("Duke's original command cannot be changed");
-                } else if (!reservedCommands.contains(to)) {
-                    throw new DukeException("Alias can only refer to Duke's original command");
-                }
-                aliases.put(from, to);
-                return new AliasCommand(from, to);
-            case "bye":
-                return new ByeCommand();
-            case "clear":
-                return new ClearTaskCommand();
-            case "list":
-                return new ListTaskCommand();
-            case "find":
-                return new FindTaskCommand(commandTokens[1]);
-            case "mark":
-                int indexOfTaskToMark = Integer.parseInt(commandTokens[1]) - ARRAY_INDEX_OFFSET;
-                return new MarkTaskCommand(indexOfTaskToMark);
-            case "unmark":
-                int indexOfTaskToUnmark = Integer.parseInt(commandTokens[1]) - ARRAY_INDEX_OFFSET;
-                return new UnmarkTaskCommand(indexOfTaskToUnmark);
-            case "delete":
-                int indexOfTaskToDelete = Integer.parseInt(commandTokens[1]) - ARRAY_INDEX_OFFSET;
-                return new DeleteTaskCommand(indexOfTaskToDelete);
-            case "todo":
-                return new AddToDoTaskCommand(commandTokens[1]);
-            case "deadline":
-                String[] deadlineTokens = commandTokens[1].split(" /by ");
-                LocalDate date = LocalDate.parse(deadlineTokens[1]);
-                return new AddDeadlineTaskCommand(deadlineTokens[0], date);
-            case "event":
-                String[] eventTokens = commandTokens[1].split(" /at ");
-                return new AddEventTaskCommand(eventTokens[0], eventTokens[1]);
-            default:
-                throw new DukeException("I'm sorry, but I don't know what that means");
-            }
+            int indexOfTaskToMark = Integer.parseInt(arguments) - ARRAY_INDEX_OFFSET;
+            return new MarkTaskCommand(indexOfTaskToMark);
+        } catch (NumberFormatException e) {
+            throw new DukeException("The index of task to mark is not a valid integer");
+        }
+    }
+
+    /**
+     * Parses an unmark command.
+     *
+     * @param arguments arguments to this command.
+     * @return a <code>UnmarkTaskCommand</code> instance.
+     * @throws DukeException when the argument is missing or is not a number.
+     */
+    private Command parseUnmarkCommand(String arguments) throws DukeException {
+        if (arguments.equals("")) {
+            throw new DukeException("Seems like the command is incomplete");
+        }
+
+        try {
+            int indexOfTaskToMark = Integer.parseInt(arguments) - ARRAY_INDEX_OFFSET;
+            return new UnmarkTaskCommand(indexOfTaskToMark);
+        } catch (NumberFormatException e) {
+            throw new DukeException("The index of task to unmark is not a valid integer");
+        }
+    }
+
+    /**
+     * Parses a delete command.
+     *
+     * @param arguments arguments to this command.
+     * @return a <code>DeleteTaskCommand</code> instance.
+     * @throws DukeException when the argument is missing or is not a number.
+     */
+    private Command parseDeleteCommand(String arguments) throws DukeException {
+        if (arguments.equals("")) {
+            throw new DukeException("Seems like the command is incomplete");
+        }
+
+        try {
+            int indexOfTaskToDelete = Integer.parseInt(arguments) - ARRAY_INDEX_OFFSET;
+            return new DeleteTaskCommand(indexOfTaskToDelete);
+        } catch (NumberFormatException e) {
+            throw new DukeException("The index of task to unmark is not a valid integer");
+        }
+    }
+
+    /**
+     * Parses a todo command.
+     *
+     * @param arguments arguments to this command.
+     * @return a <code>AddToDoTaskCommand</code> instance.
+     */
+    private Command parseToDoCommand(String arguments) {
+        return new AddToDoTaskCommand(arguments);
+    }
+
+    /**
+     * Parses a deadline command.
+     *
+     * @param arguments arguments to this command.
+     * @return a <code>AddDeadlineTaskCommand</code> instance.
+     * @throws DukeException when the arguments are missing or in incorrect format.
+     */
+    private Command parseDeadlineCommand(String arguments) throws DukeException {
+        try {
+            String[] deadlineTokens = arguments.split(" /by ");
+            String description = deadlineTokens[0];
+            LocalDate date = LocalDate.parse(deadlineTokens[1]);
+            return new AddDeadlineTaskCommand(description, date);
         } catch (DateTimeParseException e) {
             throw new DukeException("The date provided is invalid(or in wrong format)");
-        } catch (NumberFormatException e) {
-            throw new DukeException("The index of task to " + commandTokens[0] + " is not a valid integer");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new DukeException("Seems like the command is incomplete");
+        }
+    }
+
+    /**
+     * Parses an event command.
+     *
+     * @param arguments arguments to this command.
+     * @return a <code>AddEventTaskCommand</code> instance.
+     * @throws DukeException when the arguments are missing or in incorrect format.
+     */
+    private Command parseEventCommand(String arguments) throws DukeException {
+        try {
+            String[] eventTokens = arguments.split(" /at ");
+            String description = eventTokens[0];
+            String at = eventTokens[1];
+            return new AddEventTaskCommand(description, at);
         } catch (ArrayIndexOutOfBoundsException e) {
             throw new DukeException("Seems like the command is incomplete");
         }
