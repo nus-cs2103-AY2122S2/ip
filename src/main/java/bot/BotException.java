91cbee888d994604707e04ef3561c159eb189024
Chua Bing Quan <chipneverdies@gmail.com>
2022-02-12 03:05:16 +0800

    Remove message prefix

--- src/main/java/bot/BotException.java
+++ src/main/java/bot/BotException.java
@@ -1,9 +1,7 @@
 package bot;
 
 public class BotException extends Exception {
-    private static final String PREFIX = "☹ OOPS!!! ";
-
     public BotException(String description) {
-        super(BotException.PREFIX + description);
+        super(description);
     }
 }
