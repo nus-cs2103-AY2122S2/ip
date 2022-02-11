f6259200ba7b7fb2750d11cfa10a164629ff6658
Chua Bing Quan <chipneverdies@gmail.com>
2022-02-12 03:04:33 +0800

    Implement parser to validate and route user commands

---
+++ src/main/java/parser/Parser.java
@@ -0,0 +1,110 @@
+package parser;
+
+import bot.BotException;
+
+import commands.Command;
+import commands.ListCommand;
+import commands.MarkCommand;
+import commands.UnmarkCommand;
+import commands.AddCommand;
+import commands.DeleteCommand;
+import commands.ExitCommand;
+
+import tasks.Deadline;
+import tasks.Event;
+import tasks.Todo;
+
+import java.time.LocalDate;
+
+import java.util.Arrays;
+
+public class Parser {
+    private static final BotException UNSUPPORTED_COMMAND_EXCEPTION =
+            new BotException("I'm sorry, but I don't know what that means :-(");
+
+    public Command parse(String query) throws BotException {
+        final String[] tokens = query.split(" ", 2);
+        final String command = tokens[0];
+        final String[] args = Arrays.copyOfRange(tokens, 1, tokens.length);
+
+        switch (command) {
+        case ListCommand.COMMAND:
+            return this.listAction(args);
+        case MarkCommand.COMMAND:
+            return this.markAction(args);
+        case UnmarkCommand.COMMAND:
+            return this.unmarkAction(args);
+        case AddCommand.ADD_TODO_COMMAND:
+            return this.addTodoAction(args);
+        case AddCommand.ADD_DEADLINE_COMMAND:
+            return this.addDeadlineAction(args);
+        case AddCommand.ADD_EVENT_COMMAND:
+            return this.addEventAction(args);
+        case DeleteCommand.COMMAND:
+            return this.deleteAction(args);
+        case ExitCommand.COMMAND:
+            return this.exitAction(args);
+        default:
+            throw Parser.UNSUPPORTED_COMMAND_EXCEPTION;
+        }
+    }
+
+    private Command listAction(String[] args) {
+        return new ListCommand();
+    }
+
+    private Command markAction(String[] args) {
+        final int idToMark = this.parseStringToIndex(args[0]);
+        return new MarkCommand(idToMark);
+    }
+
+    private Command unmarkAction(String[] args) {
+        final int idToUnmark = this.parseStringToIndex(args[0]);
+        return new UnmarkCommand(idToUnmark);
+    }
+
+    private Command addTodoAction(String[] args) throws BotException {
+        final String description = args[0].trim();
+        if (description.isEmpty()) {
+            throw new BotException("The description of a task cannot be empty.");
+        }
+        return new AddCommand(new Todo(description));
+    }
+
+    private Command addDeadlineAction(String[] args) throws BotException {
+        final String[] deadlineArgs = args[0].split(" /by ");
+
+        final String description = deadlineArgs[0].trim();
+        if (description.isEmpty()) {
+            throw new BotException("The description of a task cannot be empty.");
+        }
+        final LocalDate by = LocalDate.parse(deadlineArgs[1].trim(), Deadline.DATE_INPUT_FORMAT);
+
+        return new AddCommand(new Deadline(description, by));
+    }
+
+    private Command addEventAction(String[] args) throws BotException {
+        final String[] eventArgs = args[0].split(" /at ");
+
+        final String description = eventArgs[0].trim();
+        if (description.isEmpty()) {
+            throw new BotException("The description of a task cannot be empty.");
+        }
+        final String at = eventArgs[1].trim();
+
+        return new AddCommand(new Event(description, at));
+    }
+
+    private Command deleteAction(String[] args) {
+        final int idToDelete = this.parseStringToIndex(args[0]);
+        return new DeleteCommand(idToDelete);
+    }
+
+    private Command exitAction(String[] args) {
+        return new ExitCommand();
+    }
+
+    private int parseStringToIndex(String str) {
+        return Integer.parseInt(str) - 1;
+    }
+}
