e5f1278a941f193fb7fad07f0878fe90807e7347
Tan Yu Tao <tanyutao913@gmail.com>
2022-02-12 16:53:40 +0800

    Update GUI with background and text box

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -6,7 +6,6 @@ import duke.tasks.Task;
  * Handles all return messages from commands
  */
 public class Ui {
-    public static final String INDENT = "     ";
     public static final String LINE = "________________________________\n";
     public Ui() {
     }
@@ -16,7 +15,7 @@ public class Ui {
      * @return welcome message
      */
     public String welcomeMessage() {
-        return INDENT + "I'm Mr MeeTrack, look at me!!!";
+        return "I'm Mr MeeTrack, look at me!!!";
     }
 
     /**
@@ -26,9 +25,9 @@ public class Ui {
      * @return message for successful add
      */
     public String addMessage(Task added, int size) {
-        return INDENT + "Yes sireeee!. I've added this task:\n"
-            + INDENT + "  " + added + "\n"
-            + INDENT + "Now you have " + size + " tasks in the list";
+        return  "Yes sireeee!. I've added this task:\n"
+            + "  " + added + "\n"
+            + "Now you have " + size + " tasks in the list";
     }
 
     /**
@@ -36,7 +35,7 @@ public class Ui {
      * @return exit message
      */
     public String byeMessage() {
-        return INDENT + "Bye. Hope to see you again soon!";
+        return "Bye. Hope to see you again soon!";
     }
 
     /**
@@ -46,9 +45,9 @@ public class Ui {
      * @return message for successful deletion
      */
     public String deleteMessage(Task deleted, int size) {
-        return INDENT + "Oooh Ok!. I've removed this task:\n"
-            + INDENT + "  " + deleted + "\n"
-            + INDENT + "Now you have " + size + " tasks in the list";
+        return "Oooh Ok!. I've removed this task:\n"
+            + "  " + deleted + "\n"
+            + "Now you have " + size + " tasks in the list";
     }
 
     /**
@@ -57,8 +56,8 @@ public class Ui {
      * @return message for successful mark
      */
     public String markMessage(String marked) {
-        return INDENT + "Nice! I've marked this task as done:\n"
-            + INDENT + marked;
+        return "Nice! I've marked this task as done:\n"
+            + marked;
     }
 
     /**
@@ -67,8 +66,8 @@ public class Ui {
      * @return message for successful un-mark
      */
     public String unMarkMessage(String unMarked) {
-        return INDENT + "I've marked this task as not done yet:\n"
-            + INDENT + unMarked;
+        return "I've marked this task as not done yet:\n"
+            + unMarked;
     }
 
     /**
@@ -78,7 +77,7 @@ public class Ui {
      */
     public String findMessage(boolean isFound) {
         if(isFound) {
-            return INDENT + "Here are the matching tasks in your list\n";
+            return "Here are the matching tasks in your list\n";
         } else {
             return "There are no tasks matching in your list";
         }
@@ -89,7 +88,7 @@ public class Ui {
      * @return message for successful update of task
      */
     public String updateMessage() {
-        return INDENT + "Ooohhh K! I have updated your task\n";
+        return "Ooohhh K! I have updated your task\n";
     }
 
     /**
@@ -97,6 +96,6 @@ public class Ui {
      * @return message for listing tasks
      */
     public String listMessage() {
-        return INDENT + "Here are the tasks in your list:\n";
+        return "Here are the tasks in your list:\n";
     }
 }
