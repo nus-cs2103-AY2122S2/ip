9b9bbf8f72b72d30cfd62e1eae7f45e2376e9a12
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-02-12 14:13:56 +0800

    Update Main.java

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
+        // Solution adapted from @jonahtanjz on github
         duke.setStage(stage);
 
         try {
