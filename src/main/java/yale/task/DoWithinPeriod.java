535b8b26540a46288f858121b065536d106f82ad
shurvirarora <shurvirarora@gmail.com>
2022-02-13 16:13:33 +0800

    Create DoWithinPeriod class
    In order to add a feature that manages tasks that need to be done within a certain period, a new class DoWithinPeriod needs to be created.
    
    Creating this class as a subclass of Task allows it to have a is-a relationship with Task, thereby inheriting the attributes and methods from the Task class.
    
    As a result, DoWithinPeriod is a type of Task object with its own modification, just like Event, Deadline and Todo.

---
+++ src/main/java/yale/task/DoWithinPeriod.java
@@ -0,0 +1,14 @@
+package yale.task;
+
+public class DoWithinPeriod extends Task {
+
+    /**
+     * Constructor method.
+     *
+     * @param name     Name of task.
+     * @param isMarked Boolean of whether task is marked.
+     */
+    public DoWithinPeriod(String name, boolean isMarked) {
+        super(name, isMarked);
+    }
+}
