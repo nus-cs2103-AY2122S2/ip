df5f5eef6e94d21308ddfd681dc220c8954795ac
Halpfrog <62001923+Halpfrog@users.noreply.github.com>
2022-02-14 17:41:54 +0800

    Added a Gui

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,22 @@
 import Duke.Exception.DukeException;
-import Duke.Processing.Storage;
 import Duke.Processing.Parser;
+import Duke.Processing.Storage;
 import Duke.Processing.TaskList;
 import Duke.UI.Ui;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.layout.Region;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Duke {
 
     private Storage storage;
@@ -18,6 +32,8 @@ public class Duke {
         } catch (DukeException e) {
             ui.errorMessage(e);
         }
+    }
+    public Duke() {
     }
 
     public void run() {
@@ -38,6 +54,30 @@ public class Duke {
             ui.errorMessage(e);
         }
         ui.endmessage();
+    }
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    public String getResponse(String input) {
+        return "I'll add the following to the list\n " + input;
+    }
+
+
+
+
+    /**
+     * Iteration 1:
+     * Creates a label with the specified text and adds it to the dialog container.
+     * @param text String containing text to add
+     * @return a label with the specified text that has word wrap enabled.
+     */
+    private Label getDialogLabel(String text) {
+        // You will need to import `javafx.scene.control.Label`.
+        Label textToAdd = new Label(text);
+        textToAdd.setWrapText(true);
+
+        return textToAdd;
     }
     public static void main(String[] args) {
         new Duke("Previously.txt").run();
