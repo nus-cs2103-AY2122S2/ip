73e1f96fddb2e9ade07e8c87462ac98b39b49dc1
DavidTan0527 <david_weiucheng@hotmail.com>
2022-02-14 20:53:53 +0800

    Remove unused imports

--- src/main/java/nikki/gui/DialogBox.java
+++ src/main/java/nikki/gui/DialogBox.java
@@ -9,13 +9,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 
