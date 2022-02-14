d856fafc7d95558e074e6383a8792d5a33d4a9dc
Brelkh <46317008+brelkh@users.noreply.github.com>
2022-02-15 00:38:48 +0800

    Add A-Gradle components

--- src/main/java/mike/Main.java
+++ src/main/java/mike/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         new Mike().run();
-        //Todo: if have time, make it new Duke("data/tasks.txt").run(); ie. include filepath
+        //Todo: if have time, make it new Mike("data/tasks.txt").run(); ie. include filepath
     }
 }
