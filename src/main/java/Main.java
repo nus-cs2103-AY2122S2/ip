c899aa21cabc247069cd37df96f32c59a3d197b8
Tan De Shao <e0544387@u.nus.edu>
2022-02-14 11:44:15 +0800

    Update Ui

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,7 +23,6 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
-
             stage.setTitle("Nexus");
             // Set the scene of the interface.
             stage.setScene(scene);
