015dfffe7d656011168cfcfbed724a96345d3c81
waffledood <itshaikalhere@hotmail.sg>
2022-02-14 09:42:03 +0800

    Update variables in ReminderPredicates.java to be final

--- src/main/java/duke/reminder/ReminderPredicates.java
+++ src/main/java/duke/reminder/ReminderPredicates.java
@@ -1,12 +1,13 @@
 package duke.reminder;
 
-import duke.task.Task;
-
 import java.time.LocalDate;
 import java.util.function.Predicate;
 
+import duke.task.Task;
+
 public class ReminderPredicates {
-    static Predicate<Task> TODAY = task -> task.getDate() != null && task.getDate().isEqual(LocalDate.now());
-    static Predicate<Task> COMPLETED = task -> (task.getStatusIcon() == "X");
-    static Predicate<Task> DUE_NOW = TODAY.and(COMPLETED.negate());
+    static final Predicate<Task> TODAY = task -> task.getDate() != null && task.getDate().isEqual(LocalDate.now());
+    static final Predicate<Task> COMPLETED = task -> (task.getStatusIcon() == "X");
+    static final Predicate<Task> DUE_NOW = TODAY.and(COMPLETED.negate());
+    static final Predicate<Task> OVERDUE = task -> task.getDate() != null && task.getDate().isBefore(LocalDate.now());
 }
