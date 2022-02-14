9b39237e0b330af657831988d802808742ff3c15
waffledood <itshaikalhere@hotmail.sg>
2022-02-14 11:35:19 +0800

    Include additional Predicates for Task Reminders

--- src/main/java/duke/reminder/ReminderPredicates.java
+++ src/main/java/duke/reminder/ReminderPredicates.java
@@ -1,6 +1,7 @@
 package duke.reminder;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.function.Predicate;
 
 import duke.task.Task;
@@ -10,4 +11,6 @@ public class ReminderPredicates {
     static final Predicate<Task> COMPLETED = task -> (task.getStatusIcon() == "X");
     static final Predicate<Task> DUE_NOW = TODAY.and(COMPLETED.negate());
     static final Predicate<Task> OVERDUE = task -> task.getDate() != null && task.getDate().isBefore(LocalDate.now());
+    static final Predicate<Task> UPCOMING = task -> task.getDate() != null && (ChronoUnit.DAYS.between(LocalDate.now(), task.getDate())) == 1;
+    static final Predicate<Task> OVERDUE_AND_UPCOMING = OVERDUE.or(UPCOMING);
 }
