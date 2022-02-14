b33688f83da76eaf1d0cb82fcfafd4e328597f66
sibinhho99 <sibinhho99@gmail.com>
2022-02-14 10:46:57 +0800

    A-Assertions

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -10,7 +10,8 @@ public class Parser {
      * Returns index to delete from delete command.
      */
     public static int getDeleteIndex(String command) {
-        // Extract name from todo command, eg. todo borrow book
+        assert command.contains("delete");
+        // Gets index to delete from delete command
         return Integer.parseInt(command.split(" ")[1]) - 1;
     }
 
@@ -18,6 +19,7 @@ public class Parser {
      * Returns task name to find from find command.
      */
     public static String getTaskToFindName(String command) {
+        assert command.contains("find");
         return command.substring(5);
     }
 
@@ -25,6 +27,7 @@ public class Parser {
      * Returns the type of the command (bye delete etc).
      */
     public String getType(String command) {
+        assert command.split(" ")[0].length() > 0;
         return command.split(" ")[0];
     }
 
@@ -32,6 +35,8 @@ public class Parser {
      * Returns event Task object from command.
      */
     public Task parseEvent(String command) {
+        assert command.contains("event");
+        assert command.contains("/at");
         // Extracts name and time to be done by from event command,
         // eg. event project meeting /at Mon 2-4pm
         String temp = command.substring(6);
@@ -44,6 +49,9 @@ public class Parser {
      * Returns deadline Task object from command.
      */
     public Task parseDeadlines(String command) {
+        assert command.contains("deadline");
+        assert command.contains("/by");
+
         // Extract name and time to be done by from deadline command,
         // eg. deadline return book /by Sunday.
         String temp = command.substring(9);
@@ -56,6 +64,8 @@ public class Parser {
      * Returns todo Task object from command.
      */
     public Task parseTodos(String command) {
+        assert command.contains("todo");
+
         // Extract name from todo command, eg. todo borrow book
         String name = command.substring(5);
         return new TaskToDos(false, name);
