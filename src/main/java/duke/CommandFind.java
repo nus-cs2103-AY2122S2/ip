c5baa6b158183300773827a16be97f8935eeb02e
garethkoh <e0533656@u.nus.edu>
2022-02-15 22:39:23 +0800

    Add JavaDoc for CommandFind class

--- src/main/java/duke/CommandFind.java
+++ src/main/java/duke/CommandFind.java
@@ -2,17 +2,31 @@ package duke;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a command to find a task in the task list.
+ */
 public class CommandFind extends Command {
     private String commandDescription;
     private ArrayList<Task> tasks;
     private String[] split;
 
+    /**
+     * Constructor for a CommandFind object.
+     *
+     * @param commandDescription The whole user input String.
+     * @param tasks The task list associated with this instance of the chatbot.
+     */
     public CommandFind(String commandDescription, ArrayList<Task> tasks) {
         this.commandDescription = commandDescription;
         this.tasks = tasks;
         split = commandDescription.split(" ");
     }
 
+    /**
+     * Handles the execution of a find command.
+     *
+     * @return A String message regarding the execution status of the find command.
+     */
     @Override
     public String execute() {
         String output = "";
