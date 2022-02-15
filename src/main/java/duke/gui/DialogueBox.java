d949483befc27ee7da62bb0fadf1b249557393e8
petermonky <wjdgustjr0554@gmail.com>
2022-02-16 01:10:47 +0800

    DialogueBox: adjust font and image size

--- src/main/java/duke/gui/DialogueBox.java
+++ src/main/java/duke/gui/DialogueBox.java
@@ -7,9 +7,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
 
+/**
+ * Represents a dialogue box in Duke GUI.
+ */
 public class DialogueBox extends HBox {
     private Label text;
     private ImageView displayPicture;
+    private static final int IMAGE_SIZE = 80;
 
     /**
      * Constructor for a dialogue box.
@@ -20,25 +24,26 @@ public class DialogueBox extends HBox {
     public DialogueBox(Label label, ImageView image, boolean isUser) {
         text = label;
         displayPicture = image;
-        displayPicture.setClip(new Circle(50, 50, 50));
 
+        int halfImageSize = IMAGE_SIZE / 2;
+        displayPicture.setClip(new Circle(halfImageSize, halfImageSize, halfImageSize));
 
         text.setWrapText(true);
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
+        displayPicture.setFitWidth(IMAGE_SIZE);
+        displayPicture.setFitHeight(IMAGE_SIZE);
         setMargin(text, new Insets(10, 10, 10, 10));
         setMargin(displayPicture, new Insets(10, 10, 10, 10));
 
         if (isUser) {
             setStyle("-fx-background-color: #1982FC; -fx-padding: 5 5 5 5; \n");
             text.setStyle("-fx-font-family: 'monospaced'; -fx-font-weight: bold; -fx-font-size: "
-                    + "14; -fx-text-fill: #FFFFFF");
+                    + "12; -fx-text-fill: #FFFFFF");
             this.setAlignment(Pos.TOP_RIGHT);
             this.getChildren().addAll(text, displayPicture);
         } else {
             setStyle("-fx-background-color: #EEEEEE; -fx-padding: 5 5 5 5; \n");
             text.setStyle("-fx-font-family: 'monospaced'; -fx-font-weight: bold; -fx-font-size: "
-                    + "14");
+                    + "12");
             this.setAlignment(Pos.TOP_LEFT);
             this.getChildren().addAll(displayPicture, text);
         }
