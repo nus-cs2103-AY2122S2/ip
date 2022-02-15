428bdc7759568b8aa8172004fd720e09ad8144bd
juslam19 <e0552250@u.nus.edu>
2022-02-15 22:02:00 +0800

    Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,20 +8,48 @@ public class Duke {
 
         // Start scanner
         FastIO sc = new FastIO();
-
-        ArrayList<String> lst = new ArrayList<String>();
         String input = sc.nextLine();
+
+        // List to hold tasks
+        ArrayList<Task> lst = new ArrayList<Task>();
+
         while (!input.equals("bye")) {
+
             if (!input.equals("list")) {
-                lst.add(input);
-                System.out.println("added: " + input);
+
+                // mark/unmark tasks
+                String[] words = input.split("\\s",0);
+                if (words[0].equals("mark")) {
+                    int index = Integer.parseInt(words[1]) - 1;
+                    lst.get(index).mark();
+                    System.out.println("Nice! I've marked this task as done: \n"
+                            + lst.get(index).toString());
+                } else if (words[0].equals("unmark")) {
+                    int index = Integer.parseInt(words[1]) - 1;
+                    lst.get(index).unmark();
+                    System.out.println("OK, I've marked this task as not done yet: \n"
+                            + lst.get(index).toString());
+
+                // add task
+                } else {
+                    Task tsk = new Task(input);
+                    lst.add(tsk);
+                    System.out.println("added: " + tsk.toString());
+                }
+
+            // list
             } else {
+                System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < lst.size(); i++) {
-                    System.out.println(i + 1 + ". " + lst.get(i));
+                    System.out.println(i + 1 + ". " + lst.get(i).toString());
                 }
             }
+
+            // scan next line
             input = sc.nextLine();
         }
+
+        // end
         System.out.println("Bye! It was nice having you!");
         sc.close();
     }
