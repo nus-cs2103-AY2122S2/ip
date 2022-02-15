993204c6f8205fbbd5524cd71b72d2b2bdb78321
zhongfu <me@zhongfu.li>
2022-02-15 21:48:10 +0800

    Reduce complexity of Duke handleInput() method
    It's kind of too complicated, but I'm also a little lazy, so I'm just
    gonna break it up into multiple methods for each command.
    
    They all have similar interfaces though, so if we do end up wanting to
    make it Better^tm, then it shouldn't be too difficult.

--- src/main/java/li/zhongfu/cs2103/chatbot/Duke.java
+++ src/main/java/li/zhongfu/cs2103/chatbot/Duke.java
@@ -111,127 +111,33 @@ public class Duke {
             case "mark":
             case "unmark":
             case "delete":
-                try {
-                    int idx = Integer.parseInt(result.getPosArg()) - 1; // list is 1-indexed
-
-                    if ("delete".equals(cmd)) {
-                        Task task = this.tasks.remove(idx);
-                        messages.add(new ChatMessage(String.format("Task removed:%n %s", task)));
-                    } else {
-                        // set as done if cmd == "mark", else (e.g. cmd == "unmark") set as undone
-                        boolean done = "mark".equals(cmd);
-                        Task task = this.tasks.get(idx);
-                        task.setDone(done);
-                        messages.add(new ChatMessage(String.format(
-                                "Task marked as %sdone:%n %s",
-                                done ? "" : "un",
-                                task)));
-                    }
-                } catch (NumberFormatException e) {
-                    throw new DukeException(
-                            String.format(
-                                    "Hmm, that doesn't seem like a valid task number! Try '%s <number>'.",
-                                    cmd),
-                            e);
-                } catch (IndexOutOfBoundsException e) {
-                    throw new DukeException("That task doesn't exist!", e);
-                }
+                messages.addAll(handleDeleteTask(result));
                 break;
 
             case "list":
-                if (this.tasks.isEmpty()) {
-                    messages.add(new ChatMessage("You have no tasks in your task list!"));
-                } else {
-                    List<String> output = new ArrayList<>();
-                    output.add("Here are the tasks in your list:");
-                    output.addAll(this.tasks.toEnumeratedList());
-                    messages.add(new ChatMessage(output.stream().collect(Collectors.joining("\n"))));
-                }
+                messages.addAll(handleListTasks(result));
                 break;
 
             case "find":
-                if (!result.hasNonblankPosArg()) {
-                    throw new DukeException("No search query provided! Try 'find meeting'.");
-                } else {
-                    TaskList matchingTasks = this.tasks.find(result.getPosArg());
-                    List<String> output = new ArrayList<>();
-                    if (matchingTasks.isEmpty()) {
-                        output.add("No tasks with that query found!");
-                    } else {
-                        output.add(String.format("Tasks containing '%s':", result.getPosArg()));
-                        output.addAll(matchingTasks.toEnumeratedList());
-                    }
-                    messages.add(new ChatMessage(output.stream().collect(Collectors.joining(System.lineSeparator()))));
-                }
+                messages.addAll(handleFindTasks(result));
                 break;
 
             case "todo":
             case "deadline":
             case "event":
-                Task task;
-                if (!result.hasNonblankPosArg()) { // including strings with only whitespaces
-                    throw new DukeException("Task name cannot be empty!");
-                }
-
-                if ("deadline".equals(cmd)) {
-                    if (!result.hasNonblankArg("by")) {
-                        throw new DukeException(String.format(
-                                "You need a time for your deadline! Try: deadline %s /by 1 jan 2050 12:15",
-                                result.getPosArg()));
-                    }
-
-                    try {
-                        task = new Deadline(result.getPosArg(), this.parser.parseDateTime(result.getArg("by")));
-                    } catch (DateTimeParseException e) {
-                        throw new DukeException(String.format(
-                                "Unknown date format! Try: deadline %s /by 1 jan 2050 12:15",
-                                result.getPosArg()));
-                    }
-                } else if ("event".equals(cmd)) {
-                    if (!result.hasNonblankArg("at")) {
-                        throw new DukeException(String.format(
-                                "You need a time for your event! Try: event %s /at 1 jan 2050 12:15",
-                                result.getPosArg()));
-                    }
-
-                    try {
-                        task = new Event(result.getPosArg(), this.parser.parseDateTime(result.getArg("at")));
-                    } catch (DateTimeParseException e) {
-                        throw new DukeException(String.format(
-                                "Unknown date format! Try: event %s /at 1 jan 2050 12:15",
-                                result.getPosArg()));
-                    }
-                } else {
-                    task = new ToDo(result.getPosArg());
-                }
-
-                this.tasks.add(task);
-                messages.add(new ChatMessage(String.format(
-                        "New task added:%n %s%nYou now have %d tasks in your task list.",
-                        task,
-                        this.tasks.size())));
+                messages.addAll(handleAddTask(result));
                 break;
 
             case "reload":
-                try {
-                    this.tasks = TaskList.loadFromStorage(this.storage);
-                    messages.add(new ChatMessage(String.format("Tasks reloaded; %d tasks in list now", this.tasks.size())));
-                } catch (FileNotFoundException e) {
-                    messages.add(new ChatMessage("Saved tasks file not found! Skipping reload."));
-                } catch (StorageException e) {
-                    messages.add(new ChatMessage("Error loading tasks! Skipping reload."));
-                }
+                messages.addAll(handleReloadTasks(result));
                 break;
 
             case "save":
-                this.tasks.save(this.storage);
-                messages.add(new ChatMessage("Tasks saved!"));
+                messages.addAll(handleSaveTasks(result));
                 break;
 
             case "bye":
-                this.tasks.save(this.storage);
-                messages.add(new ChatMessage("Bye. Hope to see you again soon!"));
-                messages.add(new QuitMessage());
+                messages.addAll(handleQuit(result));
                 break;
 
             default:
@@ -242,6 +148,144 @@ public class Duke {
             messages.add(new ChatMessage(e.getMessage()));
         }
 
+        return messages;
+    }
+
+    private List<Message> handleDeleteTask(ParserResult result) throws DukeException {
+        List<Message> messages = new ArrayList<>();
+        String cmd = result.getCmd();
+        try {
+            int idx = Integer.parseInt(result.getPosArg()) - 1; // list is 1-indexed
+
+            if ("delete".equals(cmd)) {
+                Task task = this.tasks.remove(idx);
+                messages.add(new ChatMessage(String.format("Task removed:%n %s", task)));
+            } else {
+                // set as done if cmd == "mark", else (e.g. cmd == "unmark") set as undone
+                boolean done = "mark".equals(cmd);
+                Task task = this.tasks.get(idx);
+                task.setDone(done);
+                messages.add(new ChatMessage(String.format(
+                        "Task marked as %sdone:%n %s",
+                        done ? "" : "un",
+                        task)));
+            }
+        } catch (NumberFormatException e) {
+            throw new DukeException(
+                    String.format(
+                            "Hmm, that doesn't seem like a valid task number! Try '%s <number>'.",
+                            cmd),
+                    e);
+        } catch (IndexOutOfBoundsException e) {
+            throw new DukeException("That task doesn't exist!", e);
+        }
+        return messages;
+    }
+
+    private List<Message> handleListTasks(ParserResult result) {
+        List<Message> messages = new ArrayList<>();
+        if (this.tasks.isEmpty()) {
+            messages.add(new ChatMessage("You have no tasks in your task list!"));
+        } else {
+            List<String> output = new ArrayList<>();
+            output.add("Here are the tasks in your list:");
+            output.addAll(this.tasks.toEnumeratedList());
+            messages.add(new ChatMessage(output.stream().collect(Collectors.joining("\n"))));
+        }
+        return messages;
+    }
+
+    private List<Message> handleFindTasks(ParserResult result) throws DukeException {
+        List<Message> messages = new ArrayList<>();
+        if (!result.hasNonblankPosArg()) {
+            throw new DukeException("No search query provided! Try 'find meeting'.");
+        } else {
+            TaskList matchingTasks = this.tasks.find(result.getPosArg());
+            List<String> output = new ArrayList<>();
+            if (matchingTasks.isEmpty()) {
+                output.add("No tasks with that query found!");
+            } else {
+                output.add(String.format("Tasks containing '%s':", result.getPosArg()));
+                output.addAll(matchingTasks.toEnumeratedList());
+            }
+            messages.add(new ChatMessage(output.stream().collect(Collectors.joining(System.lineSeparator()))));
+        }
+        return messages;
+    }
+
+    private List<Message> handleAddTask(ParserResult result) throws DukeException {
+        List<Message> messages = new ArrayList<>();
+        String cmd = result.getCmd();
+        Task task;
+        if (!result.hasNonblankPosArg()) { // including strings with only whitespaces
+            throw new DukeException("Task name cannot be empty!");
+        }
+
+        if ("deadline".equals(cmd)) {
+            if (!result.hasNonblankArg("by")) {
+                throw new DukeException(String.format(
+                        "You need a time for your deadline! Try: deadline %s /by 1 jan 2050 12:15",
+                        result.getPosArg()));
+            }
+
+            try {
+                task = new Deadline(result.getPosArg(), this.parser.parseDateTime(result.getArg("by")));
+            } catch (DateTimeParseException e) {
+                throw new DukeException(String.format(
+                        "Unknown date format! Try: deadline %s /by 1 jan 2050 12:15",
+                        result.getPosArg()));
+            }
+        } else if ("event".equals(cmd)) {
+            if (!result.hasNonblankArg("at")) {
+                throw new DukeException(String.format(
+                        "You need a time for your event! Try: event %s /at 1 jan 2050 12:15",
+                        result.getPosArg()));
+            }
+
+            try {
+                task = new Event(result.getPosArg(), this.parser.parseDateTime(result.getArg("at")));
+            } catch (DateTimeParseException e) {
+                throw new DukeException(String.format(
+                        "Unknown date format! Try: event %s /at 1 jan 2050 12:15",
+                        result.getPosArg()));
+            }
+        } else {
+            task = new ToDo(result.getPosArg());
+        }
+
+        this.tasks.add(task);
+        messages.add(new ChatMessage(String.format(
+                "New task added:%n %s%nYou now have %d tasks in your task list.",
+                task,
+                this.tasks.size())));
+        return messages;
+    }
+
+    private List<Message> handleReloadTasks(ParserResult result) {
+        List<Message> messages = new ArrayList<>();
+        try {
+            this.tasks = TaskList.loadFromStorage(this.storage);
+            messages.add(new ChatMessage(String.format("Tasks reloaded; %d tasks in list now", this.tasks.size())));
+        } catch (FileNotFoundException e) {
+            messages.add(new ChatMessage("Saved tasks file not found! Skipping reload."));
+        } catch (StorageException e) {
+            messages.add(new ChatMessage("Error loading tasks! Skipping reload."));
+        }
+        return messages;
+    }
+
+    private List<Message> handleSaveTasks(ParserResult result) throws IOException {
+        List<Message> messages = new ArrayList<>();
+        this.tasks.save(this.storage);
+        messages.add(new ChatMessage("Tasks saved!"));
+        return messages;
+    }
+
+    private List<Message> handleQuit(ParserResult result) throws IOException {
+        List<Message> messages = new ArrayList<>();
+        this.tasks.save(this.storage);
+        messages.add(new ChatMessage("Bye. Hope to see you again soon!"));
+        messages.add(new QuitMessage());
         return messages;
     }
 }
