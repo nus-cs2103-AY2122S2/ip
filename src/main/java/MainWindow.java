d23d92b3368a7d5922441c6fab1ceae8bf466f15
Halpfrog <62001923+Halpfrog@users.noreply.github.com>
2022-02-15 23:21:46 +0800

    Add background image and changed duke image

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -23,7 +23,7 @@ public class MainWindow extends AnchorPane {
 
     private Duke duke;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/chief.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/chief.jpg"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/cortana.jpg"));
 
     @FXML
