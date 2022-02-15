e67d8492efbcddf9221c93f8f01a753ec74f90d7
juslam19 <e0552250@u.nus.edu>
2022-02-15 21:34:41 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 import java.lang.String;
 
 public class Duke {
@@ -9,9 +9,17 @@ public class Duke {
         // Start scanner
         FastIO sc = new FastIO();
 
+        ArrayList<String> lst = new ArrayList<String>();
         String input = sc.nextLine();
-        while(!input.equals("bye")) {
-            System.out.println(input);
+        while (!input.equals("bye")) {
+            if (!input.equals("list")) {
+                lst.add(input);
+                System.out.println("added: " + input);
+            } else {
+                for (int i = 0; i < lst.size(); i++) {
+                    System.out.println(i + 1 + ". " + lst.get(i));
+                }
+            }
             input = sc.nextLine();
         }
         System.out.println("Bye! It was nice having you!");
