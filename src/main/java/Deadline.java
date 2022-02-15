832f67b12fecc00d93768568b38fae19e78d7460
SimJM <SimJM@users.noreply.github.com>
2022-02-16 01:47:41 +0800

    implement date format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Deadline extends Task {
     private final String sym = "D";
-    private String day;
+    private LocalDate day;
 
     Deadline (String description, String day) {
         super(description);
-        this.day = day;
-    }
-
-    String getSym() {
-        return this.sym;
+        this.day = LocalDate.parse(day);
     }
 
     @Override
     public String toString() {
-        return String.format("[%s][%s]%s (by:%s)", sym, super.getStatusIcon(), super.getDescription(), this.day);
+        return String.format("[%s][%s]%s (by:%s)", sym, super.getStatusIcon(), super.getDescription(),
+                day.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
     }
 }
