88a205d2ac408937c2f92d351eb66d7299299d97
Halpfrog <62001923+Halpfrog@users.noreply.github.com>
2022-02-16 11:44:14 +0800

    Revert "Fix unnecessary code"
    This reverts commit 1da9f1d3e2b1ee62b24f4f79401623fb988528ab.

--- src/main/java/Duke/UI/Ui.java
+++ src/main/java/Duke/UI/Ui.java
@@ -10,6 +10,7 @@ public class Ui {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
     static final String INTRO = "Wake up, I need you \n";
+    static final String SEPERATOR = "_____________________________________________\n";
     static final String BYE = "Welcome Home John";
     Scanner sc;
 
@@ -27,7 +28,7 @@ public class Ui {
      * @return a String output
      */
     public String startmessage() {
-        String message = logo + INTRO;
+        String message = logo + INTRO + SEPERATOR;
         return message;
     }
     /**
@@ -46,6 +47,10 @@ public class Ui {
      */
     public String errorMessage(DukeException error) {
         return error.getMessage();
+    }
+
+    public void divider() {
+        System.out.println(SEPERATOR);
     }
 
 }
--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -37,7 +37,7 @@ public class MainWindow extends AnchorPane {
 
     /**
      * Creates two dialog boxes, one echoing user input and the other containing Duke.Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing. Closes the Application once the user types bye
+     * the dialog container. Clears the user input after processing.
      */
     @FXML
     private void handleUserInput() {
