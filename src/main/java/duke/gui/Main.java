fe2aeba393f0e405627a32d3e29448eefada3726
Ye Yint Paing Oo <e0543517@u.nus.edu>
2022-02-16 20:02:28 +0800

    Update image size in dialog box

--- src/main/java/duke/gui/Main.java
+++ src/main/java/duke/gui/Main.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BackgroundFill;
 import javafx.stage.Stage;
 
 /**
