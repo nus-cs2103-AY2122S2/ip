da04e7176ac9d7d1a89d6e41bd0086680e349a87
Chua Bing Quan <chipneverdies@gmail.com>
2022-02-17 07:21:21 +0800

    Write javadocs for the bot package

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -8,17 +8,33 @@ import tasklist.TaskList;
 
 import ui.Ui;
 
+/**
+ * Represents a chat bot that listens for user inputs to manipulate their tasks.
+ */
 public class Bot {
     private final Parser parser;
     private final Ui ui;
     private final TaskList taskList;
 
+    /**
+     * Returns a chat bot whose commands, interface, and data operations, are
+     * dependent on the arguments supplied.
+     *
+     * @param parser processes and validates all user inputs.
+     * @param ui provides an interface for users to interact with a chat bot.
+     * @param taskList provides a set of operations to manipulate user tasks.
+     */
     public Bot(Parser parser, Ui ui, TaskList taskList) {
         this.parser = parser;
         this.ui = ui;
         this.taskList = taskList;
     }
 
+    /**
+     * Begins execution of the chat bot by showing an interface that accepts
+     * user inputs, processes them, and reflects the eventual response from
+     * the bot.
+     */
     public void run() {
         this.ui.showWelcome();
         boolean isExit = false;
