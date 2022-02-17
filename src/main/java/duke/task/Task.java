6f6f189e72030f9ace4d646909c5069cb67030e0
Albert Sutiono <albertsutz123@gmail.com>
2022-02-17 20:30:59 +0800

    Improve code quality : deep nesting in Task
    Class Task has a code quality issue with regards to deep nesting.
    
    Using if else statement in this case will confuse reader. There is also an issue of deep nesting within the if else statements which is a code quality violation.
    
    Let's refactor the code and use switch case instead to represent the conditional statements. The implementation of switch case and if statements does not need much change after all.
    
    Switch case will be more intuitive to other developers as it clearly states what the essence of the conditions.

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -72,14 +72,19 @@ public class Task {
      * @throws InvalidArgumentException If the user input format is invalid/unknown.
      */
     public static Task createTask(List<String> description) throws InvalidArgumentException {
-        if (description.get(0).equals("todo")) {
-            return Todo.of(description);
-        } else if (description.get(0).equals("deadline")) {
-            return Deadline.of(description);
-        } else if (description.get(0).equals("event")) {
-            return Event.of(description);
+        switch(description.get(0)) {
+            case "todo":
+                return Todo.of(description);
+
+            case "deadline":
+                return Deadline.of(description);
+
+            case "event":
+                return Event.of(description);
+
+            default:
+                throw new InvalidArgumentException();
         }
-        throw new InvalidArgumentException();
     }
 
     /**
