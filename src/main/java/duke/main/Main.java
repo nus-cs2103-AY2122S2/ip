645da8ee04e972a4de5c93dad712331e81075bfe
AinsleyJ <58112399+AinsleyJ@users.noreply.github.com>
2022-02-17 22:35:21 +0800

    Set title of window

--- src/main/java/duke/main/Main.java
+++ src/main/java/duke/main/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage stage) {
+        stage.setTitle("Duke");
         try {
             URL filePath = Main.class.getResource("/view/MainWindow.fxml");
             assert filePath != null : "Some resource is missing, unable to proceed.";
