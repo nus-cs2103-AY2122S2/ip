70523933b886ba2723b664df4223d07003abf145
Chua Bing Quan <chipneverdies@gmail.com>
2022-02-18 03:21:34 +0800

    Implement the find command

--- src/main/java/commands/FindCommand.java
+++ src/main/java/commands/FindCommand.java
@@ -1,2 +1,30 @@
-package commands;public class FindCommand {
+package commands;
+
+import tasklist.TaskList;
+
+import tasklist.TaskListException;
+import ui.Ui;
+
+public class FindCommand extends Command {
+    public static final String COMMAND = "find";
+
+    private final String keyword;
+
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public void execute(Ui ui, TaskList taskList) {
+        try {
+            ui.showFindTasks(taskList.find(this.keyword));
+        } catch (TaskListException ex) {
+            ui.showError(ex.getMessage());
+        }
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 }
