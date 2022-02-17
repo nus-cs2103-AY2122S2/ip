9f6796cf79567007d5eb9003b13c15d843943810
LapisRaider <angielimenqi7@gmail.com>
2022-02-18 03:52:08 +0800

    Add header comment to storage

--- src/main/java/duke/util/Storage.java
+++ src/main/java/duke/util/Storage.java
@@ -24,6 +24,9 @@ public class Storage {
     private final String fullFilePath;
     private File file;
 
+    /**
+     * Default constructor for Storage.
+     */
     public Storage() {
         fileDirPath = "";
         fullFilePath = "";
