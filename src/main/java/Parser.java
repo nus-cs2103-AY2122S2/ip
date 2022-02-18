e0fc56f8b5c0f832eb7d2e4e05f0f015f10d9e55
Ryan Chang Jia Jie <>
2022-02-19 01:52:32 +0800

    Add Parser.java to handle the logic of the program when interacting with the user
    There are several different valid commands the program can take as
    inputs, in some cases the commands are not inputted in a valid syntax.
    
    Consolidating the majority of the program logic into Parser.java instead
    of lumping them with the main method improves code quality and follows
    coding standards.
    
    As a step towards improving code quality and utilising OOP, let's
    abstract the code that handles the program logic into Parser.java. Doing
    so will make code readability and quality better.

---
+++ src/main/java/Parser.java
@@ -0,0 +1,91 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+//Parser.java reused and edited from Brigette Santoso E0564307
+public class Parser {
+
+    static public Command parse(String fullCommand) throws InvalidInputException {
+        String firstWordOfCommand = getFirstWord(fullCommand);
+        int lengthOfFullCommand = fullCommand.length();
+        int lengthOfFirstWord = firstWordOfCommand.length();
+
+        if (firstWordOfCommand.length() == 0 || isNotValidCommand(firstWordOfCommand)) {
+            throw new InvalidInputException("Please enter a valid task with a description.");
+        } else if (lengthOfFirstWord == lengthOfFullCommand) {
+            switch (firstWordOfCommand) {
+            case "bye":
+                return new ExitCommand();
+            case "list":
+                return new ListCommand();
+            default:
+                throw new InvalidInputException("The description of this command cannot be empty.");
+            }
+        } else {
+            String restOfCommand = getRemainingCommand(fullCommand);
+            switch (firstWordOfCommand) {
+            case "mark":
+                return new MarkCommand(Integer.parseInt(restOfCommand));
+            case "unmark":
+                return new UnmarkCommand(Integer.parseInt(restOfCommand));
+            case "delete":
+                return new DeleteCommand(Integer.parseInt(restOfCommand));
+            case "todo":
+                return new AddTodoCommand(restOfCommand);
+            case "deadline":
+                String[] deadlineDetails = restOfCommand.split(" /by ");
+                String deadlineDescription = deadlineDetails[0];
+                String deadlineDueDate = deadlineDetails[1];
+                if (!isDate(deadlineDueDate)) {
+                    return new AddDeadlineCommand(deadlineDescription, deadlineDueDate);
+                } else {
+                    return new AddDeadlineCommand(deadlineDescription, LocalDate.parse(deadlineDueDate));
+                }
+            case "event":
+                String[] eventDetails = restOfCommand.split(" /at ");
+                String eventDescription = eventDetails[0];
+                String eventDate = eventDetails[1];
+                if (!isDate(eventDate)) {
+                    return new AddEventCommand(eventDescription, eventDate);
+                } else {
+                    return new AddEventCommand(eventDescription, LocalDate.parse(eventDate));
+                }
+            default:
+                throw new InvalidInputException("The description of this command cannot be empty.");
+            }
+        }
+    }
+
+    public static boolean isDate(String s) {
+        try {
+            LocalDate.parse(s);
+        }
+        catch (DateTimeParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static String getFirstWord(String command) {
+        int index = command.indexOf(' ');
+        if (index > -1) { // Check if there is more than one word.
+            return command.substring(0, index).trim(); //Extract first word.
+        } else {
+            return command; //Text is the first word itself.
+        }
+    }
+
+    public static String getRemainingCommand(String command) {
+        int index = command.indexOf(' ');
+        int lastIndexOfCommand = command.length();
+        return command.substring(index, lastIndexOfCommand).trim(); //Extract first word.
+    }
+
+    //isNotValidCommand reused from Tan De Shao
+    public static boolean isNotValidCommand(String command) {
+        return (!command.equals("bye") && !command.equals("list")
+                && !command.equals("delete")  && !command.equals("mark") && !command.equals("unmark")
+                && !command.equals("todo") && !command.equals("deadline") && !command.equals("event"));
+    }
+
+}
+
