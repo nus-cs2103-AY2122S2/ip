9f3f1911c2918ba08f89a65c01b70dc1326e2e5b
Ryan Chang Jia Jie <>
2022-02-19 01:49:07 +0800

    Add Ui.java to handle interactions with the user
    There are several different required outputs to print to ensure
    interactions with the user make sense to them.
    
    Consolidating the majority of the print statements into Ui.java instead
    of lumping them with the main method improves code quality and follows
    coding standards.
    
    As a step towards improving code quality and utilising OOP, let's
    abstract the code that handles print statements into Ui.java. Doing so
    will make code readability and quality better.

---
+++ src/main/java/Ui.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//Ui.java reused and edited from Brigette Santoso E0564307
+public class Ui {
+    protected Scanner scan;
+
+    public Ui() {
+        scan = new Scanner(System.in);
+    }
+
+    public void showWelcome() {
+        showLine();
+        System.out.println("     Hi, I'm Siri, a simple list program. \n"
+                + "     You can add 3 types of tasks to the list:\n"
+                + "     1.todo \n     2.deadline \n     3.event \n\n"
+                + "     To add to the list, specify the type of task before it's description.\n"
+                + "     Only deadlines and events can accept dates and timings\n\n"
+                + "     Examples:\n"
+                + "     todo run a mile\n"
+                + "     deadline return library book /by Sunday 2359\n"
+                + "     event Jack's wedding /at Holiday Inn 1800\n\n"
+                + "     What do you wish to take note of today?");
+        showLine();
+    }
+
+    public void showExit() {
+        System.out.println("     Goodbye.\n");
+    }
+
+    public void showList(ArrayList<Task> tasks) {
+        System.out.println("     Tasks to do:");
+        int numberOftasks = tasks.size();
+        for (int i = 0; i < numberOftasks; i++) {
+            String currTask = tasks.get(i).toString();
+            System.out.println("     " + (i + 1) + "." + currTask);
+        }
+
+    }
+
+    public void showMarkTask(Task task) {
+        System.out.println("     This task has been marked:\n       "
+                + task.toString() + "\n");
+    }
+
+    public void showUnmarkTask(Task task) {
+        System.out.println("     This task has been unmarked:\n       "
+                + task.toString() + "\n");
+    }
+
+    public void showAddTask(Task task, ArrayList<Task> tasks) {
+        System.out.println("     This task has been added to your list:\n       "
+                + task.toString() + "\n" + "\n"
+                + "     Number of task(s) in your list: " + tasks.size() + "\n");
+    }
+
+    public void showDeleteTask(Task task, ArrayList<Task> tasks) {
+        System.out.println("     Sure. I've removed this task from the list:\n       "
+                + task.toString() + "\n" + "\n"
+                + "     Number of task(s) in your list: " + tasks.size() + "\n");
+    }
+
+    public void showLine() {
+        System.out.println("____________________________________________________________" +
+                "__________________________________");
+    }
+
+    public void showLoadingError(String message) {
+        System.out.println(message);
+    }
+
+    public void showError(String message) {
+        System.out.println(message);
+    }
+
+    public String readCommand() {
+        return scan.nextLine();
+    }
+}
