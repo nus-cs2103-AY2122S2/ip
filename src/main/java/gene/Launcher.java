17955e74e07ca68a25a3f143f4547af8f4c72acd
eugenechiaay <eugene.chiay@gmail.com>
2022-02-18 16:57:06 +0800

    Help commad: Registers a help command if user needs help
    Help command is case sensitive
    
    Case sensitive but upon startup, the specific thing to key in for help is specified.

--- src/main/java/gene/Launcher.java
+++ src/main/java/gene/Launcher.java
@@ -2,7 +2,7 @@ package gene;
 import javafx.application.Application;
 
 /**
- * A launcher class to workaround classpath issues.
+ * A launcher class to workaround classpath issues yay!
  */
 public class Launcher {
     public static void main(String[] args) {
