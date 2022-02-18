6d80fda11a0bd59ab0d4e436d114da8a19ce8d4f
Ryan Chang Jia Jie <>
2022-02-19 01:55:03 +0800

    Add Command.java as super class to extract more classes for the different types of commands

---
+++ src/main/java/Command.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+//Command.java reused from Brigette Santoso E0564307
+public class Command {
+
+    public boolean isExit() {
+        return false;
+    }
+
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws InvalidInputException {
+
+    }
+}
