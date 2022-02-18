a550e3c7d584488d41f63715991a26bdf1da6259
Ryan Chang Jia Jie <>
2022-02-19 02:05:42 +0800

    Add Siri.java to call all necessary methods to get Siri program to run

---
+++ src/main/java/Siri.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+
+public class Siri {
+
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
+
+    public Siri(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            tasks = new TaskList(storage.load());
+        } catch (InvalidInputException e) {
+            ui.showLoadingError(e.getMessage());
+            tasks = new TaskList();
+        }
+    }
+
+    public void run() {
+        ui.showWelcome();
+        boolean isExit = false;
+        while (!isExit) {
+            try {
+                String fullCommand = ui.readCommand();
+                ui.showLine(); // show the divider line ("_______")
+                Command c = Parser.parse(fullCommand);
+                c.execute(tasks, ui, storage);
+                isExit = c.isExit();
+            } catch (InvalidInputException e) {
+                ui.showError(e.getMessage());
+            } finally {
+                ui.showLine();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Siri("data/siri.txt").run();
+    }
+}
