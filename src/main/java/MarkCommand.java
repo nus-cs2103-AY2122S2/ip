9deaec73493f0d4488f360edcc0e388e90c5ed50
Ryan Chang Jia Jie <>
2022-02-19 02:00:31 +0800

    Add MarkCommand.java & UnmarkCommand.java to mark/unmark tasks

---
+++ src/main/java/MarkCommand.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+//MarkCommand.java reused from Brigette Santoso E0564307
+public class MarkCommand extends Command {
+    protected int taskIndex;
+
+    public MarkCommand(int taskIndex) {
+        this.taskIndex = taskIndex - 1;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws InvalidInputException {
+        tasks.markTask(taskIndex);
+        ui.showMarkTask(tasks.getTasks().get(taskIndex));
+        storage.save(tasks.getTasks());
+    }
+}
---
+++ src/main/java/UnmarkCommand.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class UnmarkCommand extends Command {
+    protected int taskIndex;
+
+    public UnmarkCommand(int taskIndex) {
+        this.taskIndex = taskIndex - 1;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws InvalidInputException {
+        tasks.unmarkTask(taskIndex);
+        ui.showUnmarkTask(tasks.getTasks().get(taskIndex));
+        storage.save(tasks.getTasks());
+    }
+}
