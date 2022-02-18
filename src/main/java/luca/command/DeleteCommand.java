741562c178484639a1419375ec95d183a29f3621
thikhinab <thikhinab@gmail.com>
2022-02-18 20:25:18 +0800

    Add guard clauses

--- src/main/java/luca/command/DeleteCommand.java
+++ src/main/java/luca/command/DeleteCommand.java
@@ -1,6 +1,7 @@
 package luca.command;
 
 import luca.common.DukeException;
+import luca.parser.exceptions.InvalidArgumentException;
 import luca.storage.Storage;
 import luca.task.Task;
 import luca.task.TaskList;
@@ -30,10 +31,17 @@ public class DeleteCommand extends Command {
      * @param taskList list of tasks.
      * @param storage storage used by chat bot.
      * @return response string.
-     * @throw DukeException if there is a File I/O exception thrown when saving file.
+     * @throw DukeException if there is a File I/O exception or when point is invalid.
      */
     @Override
     public String execute(TaskList taskList, Storage storage) throws DukeException {
+        if (taskList.size() == 0) {
+            throw new InvalidArgumentException("There are no tasks to be deleted.");
+        }
+        if (point > taskList.size() || point <= 0) {
+            throw new InvalidArgumentException("The index given is invalid.");
+        }
+
         Task task = taskList.remove(point - 1);
         storage.saveToFile(taskList);
         return "Noted. I've removed this task:\n " + task
