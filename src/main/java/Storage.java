b54019d44bbabe9638d9ff6a01f78fc7499b6c6e
kavxya <e0550269@u.nus.edu>
2022-02-20 03:43:26 +0800

    Add the function to read the file from the storage

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,12 +1,12 @@
 package duke;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
+
 
 /**
  * deals with loading tasks from the file and saving tasks in the file
@@ -16,39 +16,92 @@ public class Storage {
 
     /**
      * Initializes the location of the file to retrieve data from.
-     * @param filePath
+     * @param filePath is the location of the file, will create a file in this location if it does not already exist.
      */
-    Storage(String filePath){
+    Storage(String filePath) {
         this.filePath = filePath;
-    }
-    /*ArrayList<String> load() {
-        ArrayList<String> lines = Collections.emptyList();
-        try
-        {
-            lines = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
+        try {
+            File myObj = new File(this.filePath);
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
         }
-
-        catch (IOException e)
-        {
-
-            // do something
-            e.printStackTrace();
-        }
-        return lines;
-        return Files.readAllLines(Paths.get("sample.txt"));
     }
 
+    /**
+     * Default constructor for Storage object.
      */
+    public Storage() {
+        try {
+            File myObj = new File("data/tasks.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
+    }
+
+    /**
+     * Loads list of tasks saved from previous execution of program.
+     *
+     * @return a list of Tasks loaded from previous runs
+     */
+    public duke.TaskList load() {
+        duke.TaskList tasks = new duke.TaskList();
+
+        try {
+            FileReader fr = new FileReader(this.filePath);
+            BufferedReader br = new BufferedReader(fr);
+            String currLine;
+
+            while ((currLine = br.readLine()) != null) {
+                int taskInt = currLine.indexOf("[") + 1;
+                char typeOfTask = currLine.charAt(taskInt);
+                int doneInt = taskInt + 3;
+                char taskDone = currLine.charAt(doneInt);
+                String task = currLine.substring(doneInt + 3);
+                if (typeOfTask == 'T') {
+                    duke.ToDo toDo = new duke.ToDo(task);
+                    if (taskDone == 'X') {
+                        toDo.markAsDone();
+                    }
+                    tasks.add(toDo);
+                } else if (typeOfTask == 'D') {
+                    duke.Deadline deadline = new duke.Deadline(task);
+                    if (taskDone == 'X') {
+                        deadline.markAsDone();
+                    }
+                    tasks.add(deadline);
+                } else if (typeOfTask == 'E') {
+                    duke.Event event = new duke.Event(task);
+                    if (taskDone == 'X') {
+                        event.markAsDone();
+                    }
+                    tasks.add(event);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
+        return tasks;
+    }
 
     /**
      * Writes the values stored within the tasklist to the file called data/tasks.txt
-     * @param list
+     * @param list is the list of tasks which are to be saved.
      */
-    void save(TaskList list) {
+    void save(duke.TaskList list) {
         try {
             FileWriter myWriter = new FileWriter("data/tasks.txt");
-            for (int i=0; i<list.size(); i++) {
-                myWriter.write(i+1 + ". " + list.get(i));
+            for (int i = 0; i < list.size(); i++) {
+                myWriter.write(i + 1 + ". " + list.get(i));
                 myWriter.write(String.format("%n"));
 
             }
