796370e861f94952566b73d54f38fdd2e3600ac4
glennljw <glennljw7@gmail.com>
2022-02-19 22:52:29 +0800

    Add Wonka app icon

--- src/main/java/wonka/Main.java
+++ src/main/java/wonka/Main.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     private Wonka wonka = new Wonka();
+    private final Image appIcon = new Image(this.getClass().getResourceAsStream("/images/wonka.png"));
 
     @Override
     public void start(Stage stage) {
@@ -20,6 +22,7 @@ public class Main extends Application {
             stage.setScene(scene);
             fxmlLoader.<wonka.gui.MainWindow>getController().setWonka(wonka);
             fxmlLoader.<wonka.gui.MainWindow>getController().setStage(stage);
+            stage.getIcons().add(appIcon);
             stage.show();
             stage.setTitle("Wonka");
         } catch (IOException e) {
