e934d30095fcc6b3c89526e20b2f700a4e0e637d
LAPTOP-3M0VCNEN\Lik Hern <e0412919@u.nus.edu>
2022-02-20 07:35:29 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static String welcomeMessage() {
+        return "Hello! I'm Duke\nWhat can I do for you?";
+    }
+
+    private static String farewellMessage() {
+        return "Bye. Hope to see you again soon!";
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(welcomeMessage());
+        while(true) {
+            String command = new Scanner(System.in).nextLine();
+            if (command.trim().equalsIgnoreCase("bye")) {
+                System.out.println(farewellMessage());
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
     }
 }
