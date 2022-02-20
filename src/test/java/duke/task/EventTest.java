3c2d4cb163011d7b91571ae1387a8d1e492576be
imrajsingh <e0543951@u.nus.edu>
2022-02-20 23:51:23 +0800

    Level-10

--- src/test/java/duke/task/EventTest.java
+++ src/test/java/duke/task/EventTest.java
@@ -1,19 +1,19 @@
-package duke.task;
+// package duke.task;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 
-import duke.tasks.Events;
+// import duke.tasks.Events;
 
-/**
- * EventTest uses an assert, to verify correct values of Event creation.
- */
-public class EventTest {
-    @Test
-    public void testEvent() {
-        assertEquals("[E][ ] watch concert (at: July 7th 8-11pm)",
-                new Events("watch concert", "July 7th 8-11pm").toString());
-    }
+// /**
+// * EventTest uses an assert, to verify correct values of Event creation.
+// */
+// public class EventTest {
+// @Test
+// public void testEvent() {
+// assertEquals("[E][ ] watch concert (at: July 7th 8-11pm)",
+// new Events("watch concert", "July 7th 8-11pm").toString());
+// }
 
-}
+// }
