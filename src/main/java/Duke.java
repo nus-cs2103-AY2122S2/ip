1afdccda8429484b8a26fb24eb5c9ac15854b5bd
LAPTOP-3M0VCNEN\Lik Hern <e0412919@u.nus.edu>
2022-02-20 09:07:16 +0800

    Update Duke.java
    Add the ability to store whatever text entered by the user and
    display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,40 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Duke {
-    private static String welcomeMessage() {
-        return "Hello! I'm Duke\nWhat can I do for you?";
+    private static final int TURN_ZERO_BASED_INDEXING_TO_ONE_BASED_INDEXING = 1;
+
+    private static final ArrayList<String> taskList = new ArrayList<>(100);
+
+    private static void welcomeMessage() {
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
     }
 
-    private static String farewellMessage() {
-        return "Bye. Hope to see you again soon!";
+    private static void farewellMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void viewTaskList() {
+        ListIterator<String> iterator = taskList.listIterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.nextIndex() + TURN_ZERO_BASED_INDEXING_TO_ONE_BASED_INDEXING
+                    + ". " + iterator.next());
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(welcomeMessage());
+        welcomeMessage();
         while(true) {
             String command = new Scanner(System.in).nextLine();
-            if (command.trim().equalsIgnoreCase("bye")) {
-                System.out.println(farewellMessage());
+            if (command.equalsIgnoreCase("bye")) {
+                farewellMessage();
                 break;
+            } else if (command.equalsIgnoreCase("list")){
+                viewTaskList();
             } else {
-                System.out.println(command);
+                taskList.add(command);
+                System.out.println("added: " + command);
             }
         }
     }
