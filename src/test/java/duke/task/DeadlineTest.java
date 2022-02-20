71685d5d7cf7ae03301cfad9b6644383a54cd898
imrajsingh <e0543951@u.nus.edu>
2022-02-21 04:59:18 +0800

    update tests

--- src/test/java/duke/task/DeadlineTest.java
+++ src/test/java/duke/task/DeadlineTest.java
@@ -1,18 +1,18 @@
-// package duke.task;
+package duke.task;
 
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-// import duke.tasks.Deadlines;
+import duke.tasks.Deadlines;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// /**
-// * DeadlineTest uses an assert, to verify correct values of Deadline creation.
-// */
-// public class DeadlineTest {
-// @Test
-// public void testDeadlines() {
-// assertEquals("[D][ ] finish assignment (by: Jan 1st)",
-// new Deadlines("finish assignemnt", "Jan 1st").toString());
-// }
-// }
+/**
+ * DeadlineTest uses an assert, to verify correct values of Deadline creation.
+ */
+public class DeadlineTest {
+    @Test
+    public void testDeadlines() {
+        assertEquals("[D][ ] finish assignment (by: Jan 1st)",
+                new Deadlines("finish assignment", "Jan 1st").toString());
+    }
+}
--- src/test/java/duke/task/TodoTest.java
+++ src/test/java/duke/task/TodoTest.java
@@ -1,18 +1,18 @@
-// package duke.task;
+package duke.task;
 
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-// import duke.tasks.ToDos;
+import duke.tasks.ToDos;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// /**
-// * TodoTest uses an assert, to verify correct values of Todo creation.
-// */
-// public class TodoTest {
-// @Test
-// public void testStringResult() {
-// assertEquals("[T][ ] go for lunch", new ToDos("go for lunch").toString());
-// }
+/**
+ * TodoTest uses an assert, to verify correct values of Todo creation.
+ */
+public class TodoTest {
+    @Test
+    public void testStringResult() {
+        assertEquals("[T][ ] go for lunch", new ToDos("go for lunch").toString());
+    }
 
-// }
+}
