831f9b6aa98bb0fc5cb2e53effe892a36d142e58
hanqinilnix <ohanqin@gmail.com>
2022-02-20 22:35:25 +0800

    Remove used methods

--- src/main/java/ari/ui/Ui.java
+++ src/main/java/ari/ui/Ui.java
@@ -6,71 +6,13 @@ import java.util.Scanner;
  * Deals with the interactions with the user
  */
 public class Ui {
-    private static final String LINE_BREAK = "____________________________________________________________";
-    private static final String LOGO = "             ___      .______       __         \n"
-            + "            /   \\     |   _  \\     |  |      \n"
-            + "           /  ^  \\    |  |_)  |    |  |      \n"
-            + "          /  /_\\  \\   |      /     |  |      \n"
-            + "         /  _____  \\  |  |\\  \\----.|  |      \n"
-            + "        /__/     \\__\\ | _| `._____||__|      \n"
-            + "\n";
     private static final String WELCOME_MESSAGE = "Dear Master, I am Ari, your Personal Assistant Chatbot";
     private static final String WELCOME_QUESTION = "What can I do for you?";
 
-    private Scanner sc;
-
-    public Ui() {
-        this.sc = new Scanner(System.in);
-    }
-
-    /**
-     * Returns user input
-     *
-     * @return user input
-     */
-    public String getUserInput() {
-        String input = sc.nextLine();
-        printWithTab(LINE_BREAK);
-
-        return input;
-    }
-
-    /**
-     * Prints welcome message
-     */
-    public void printWelcomeMessage() {
-        printWithTab(LINE_BREAK);
-        System.out.println(LOGO);
-        printWithTab(WELCOME_MESSAGE);
-        printWithTab(WELCOME_QUESTION);
-        printWithTab(LINE_BREAK);
-        System.out.println();
-    }
+    public Ui() {}
 
     public static String displayWelcomeMessage() {
         return WELCOME_MESSAGE + "\n" + WELCOME_QUESTION;
     }
-
-    /**
-     * Prints message that other classes want to display to user
-     *
-     * @param message message to display to user
-     */
-    public void displayMessage(String message) {
-        printWithTab(message);
-        printWithTab(LINE_BREAK);
-        System.out.println();
-    }
-
-    /**
-     * Prints the message with tab prepended to it
-     *
-     * @param message message to pad with tab
-     */
-    private void printWithTab(String message) {
-        String[] mesArray = message.split("\n");
-        for (String mes : mesArray) {
-            System.out.println("\t" + mes);
-        }
-    }
 }
+
