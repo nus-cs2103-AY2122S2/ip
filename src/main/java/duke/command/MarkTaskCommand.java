f29b2b3c9ef47f6002764afd05f53b210604ec1f
clement0010 <clement0010@hotmail.com>
2022-02-21 05:57:11 +0800

    Fix unused import styling

--- src/main/java/duke/command/MarkTaskCommand.java
+++ src/main/java/duke/command/MarkTaskCommand.java
@@ -3,7 +3,6 @@ package duke.command;
 import duke.DukeException;
 import duke.common.Messages;
 import duke.storage.Storage;
-import duke.task.Task;
 import duke.task.TaskList;
 import duke.ui.TextUi;
 
