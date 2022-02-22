c5a63248725bef07e81df2d68b6d2d25d8f18dd0
tiewweijian <tiewweijian@u.nus.edu>
2022-02-22 23:12:54 +0800

    Create Launcher to rectify bugs

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -23,10 +23,6 @@ public class Main extends Application {
 
     }
 
-    public static void main(String[] args) {
-        Application.launch(Main.class, args);
-    }
-
     @Override
     public void start(Stage stage) {
         try {
