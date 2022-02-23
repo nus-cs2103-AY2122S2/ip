f9dabd3af7b6427492218c78248f40784da8156d
kaiyichen <chenkaiyi01@gmail.com>
2022-02-23 17:23:01 +0800

    fixed to pass smoke test

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -26,6 +26,11 @@ public class Parser {
      */
     public void checkDir () {
         File dukeDirectory = new File(directory);
+
+//        String home = System.getProperty("user.home");
+//        java.nio.file.Path path = java.nio.file.Paths.get(home, "my", "app", "dir");
+//        boolean directoryExists = java.nio.file.Files.exists(path);
+
         if (!dukeDirectory.exists()) {
             dukeDirectory.mkdir();
         }
