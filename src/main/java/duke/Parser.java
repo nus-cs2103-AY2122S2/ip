d84593ae01440c8cde5e8bb9fb36732a1de514e3
markbiju <markbiju16@gmail.com>
2022-02-24 17:01:48 +0800

    Update Parser.java

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -34,32 +34,32 @@ public class Parser {
                         case "todo":
                             return new TodoCommand(arrOfStr[1]);
                         case "deadline": {
-                                             String[] arrOfStr2 = arrOfStr[1].split("/", 2);
-                                             String[] arrOfStr3 = arrOfStr2[1].split(" ", 3);
-                                             return new DeadlineCommand(arrOfStr2[0], LocalDate.parse(arrOfStr3[1]), arrOfStr3[2]);
+                            String[] arrOfStr2 = arrOfStr[1].split("/", 2);
+                            String[] arrOfStr3 = arrOfStr2[1].split(" ", 3);
+                            return new DeadlineCommand(arrOfStr2[0], LocalDate.parse(arrOfStr3[1]), arrOfStr3[2]);
                         }
                         case "event": {
-                                          String[] arrOfStr2 = arrOfStr[1].split("/", 2);
-                                          String[] arrOfStr3 = arrOfStr2[1].split(" ", 2);
-                                          assert (arrOfStr3[0].equals("at")) : "event command is not entered properly.";
-                                              return new EventCommand(arrOfStr2[0], (" " + arrOfStr3[1]));
+                            String[] arrOfStr2 = arrOfStr[1].split("/", 2);
+                            String[] arrOfStr3 = arrOfStr2[1].split(" ", 2);
+                            assert (arrOfStr3[0].equals("at")) : "event command is not entered properly.";
+                            return new EventCommand(arrOfStr2[0], (" " + arrOfStr3[1]));
                         }
                         case "find":
-                                      return new FindCommand(arrOfStr[1]);
+                            return new FindCommand(arrOfStr[1]);
                         default:
-                                      int number = Integer.parseInt(arrOfStr[1]);
-                                      switch (command) {
-                                          case "mark":
-                                              return new MarkCommand(number);
-                                          case "unmark":
-                                              return new UnmarkCommand(number);
-                                          case "delete":
-                                              return new DeleteCommand(number);
-                                          case "snooze":
-                                              return new SnoozeCommand(number);
-                                          default:
-                                              throw new DukeException("Bud!!! What in the world " +
-                                                      "are you talking about? Are you following the right format bud?");
+                            int number = Integer.parseInt(arrOfStr[1]);
+                            switch (command) {
+                                case "mark":
+                                    return new MarkCommand(number);
+                                case "unmark":
+                                    return new UnmarkCommand(number);
+                                case "delete":
+                                    return new DeleteCommand(number);
+                                case "snooze":
+                                    return new SnoozeCommand(number);
+                                default:
+                                throw new DukeException("Bud!!! What in the world " +
+                                        "are you talking about? Are you following the right format bud?");
                                       }
                     }
                 } else {
