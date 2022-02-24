92364cf1afe4b6d999c4b587eafc39f93a0628cb
imrajsingh <e0543951@u.nus.edu>
2022-02-24 17:14:09 +0800

    Finalised changes to Duke

--- src/main/java/duke/main/Duke.java
+++ src/main/java/duke/main/Duke.java
@@ -7,28 +7,13 @@ import duke.parser.Parser;
 import duke.storage.Storage;
 import duke.tasks.TaskList;
 import duke.ui.Ui;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
-public class Duke extends Application {
+public class Duke {
 
-    private ScrollPane scrollPane;
     private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    // create the images, by getting their directories
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    // create the image, by getting its directory
     private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
     private String filePath = "data/duke.txt";
 
@@ -36,128 +21,19 @@ public class Duke extends Application {
     private TaskList tasks;
     private Ui ui;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        // set up the required components, adapted from main method previously.
-
-        // instantiates the system.in
-        ui = new Ui();
-        storage = new Storage(filePath);
-
-        // scroll content of the chat
-        scrollPane = new ScrollPane();
-        dialogContainer = new VBox();
-        scrollPane.setContent(dialogContainer);
-
-        userInput = new TextField();
-        // place "Send" to make the button's title
-        sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-        scene = new Scene(mainLayout);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-        // style the window to look as expected
-        primaryStage.setTitle("Duke");
-        primaryStage.setResizable(false);
-        primaryStage.setMinWidth(400.0);
-        primaryStage.setMinHeight(600.0);
-
-        // the preferred height & width of the window
-        double prefHeight = 600.0;
-        double prefWidth = 400.0;
-        mainLayout.setPrefSize(prefWidth, prefHeight);
-
-        scrollPane.setPrefSize(385, 550);
-        // setting the ability to scroll horiz and vertically in the window
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-
-        scrollPane.setVvalue(1.0);
-        scrollPane.setFitToWidth(true);
-
-        // You will need to import `javafx.scene.layout.Region` for this.
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-
-        sendButton.setPrefWidth(55.0);
-
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-
-        // Part 3. Add functionality to handle user input.
-        sendButton.setOnMouseClicked((event) -> {
-            try {
-                handleUserInput();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        userInput.setOnAction((event) -> {
-            try {
-                handleUserInput();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        // Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-
-        ui = new Ui();
-        storage = new Storage(filePath);
+    public Duke() {
+        this.ui = new Ui();
+        this.storage = new Storage(filePath);
 
         try {
             tasks = new TaskList(storage.load());
         } catch (DukeException e) {
             ui.showLoadingError(e.getMessage());
-            Label dukeText = new Label("bro what is this");
             dialogContainer.getChildren().addAll(
-                    DialogBox.getDukeDialog(dukeText, new ImageView(duke)));
+                    DialogBox.getDukeDialog("No file found", duke));
             tasks = new TaskList();
         }
-    }
 
-    /**
-     * Iteration 1:
-     * Creates a label with the specified text and adds it to the dialog container.
-     * 
-     * @param text String containing text to add
-     * @return a label with the specified text that has word wrap enabled.
-     */
-    private Label getDialogLabel(String text) {
-        // You will need to import `javafx.scene.control.Label`.
-        Label textToAdd = new Label(text);
-        textToAdd.setWrapText(true);
-
-        return textToAdd;
-    }
-
-    /**
-     * Iteration 2:
-     * Creates two dialog boxes, one echoing user input and the other containing
-     * Duke's reply and then appends them to
-     * the dialog container. Clears the user input after processing.
-     * 
-     * @throws IOException
-     */
-    private void handleUserInput() throws IOException {
-        Label userText = new Label(userInput.getText());
-        Label dukeText = new Label(getResponse(userInput.getText()));
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(userText, new ImageView(user)),
-                DialogBox.getDukeDialog(dukeText, new ImageView(duke)));
-        userInput.clear();
     }
 
     /**
@@ -165,7 +41,7 @@ public class Duke extends Application {
      * 
      * @throws IOException
      */
-    private String getResponse(String input) throws IOException {
+    public String getResponse(String input) throws IOException {
         // return "Duke heard: " + input;
         Parser parser = new Parser(tasks);
 
@@ -183,4 +59,131 @@ public class Duke extends Application {
             return e.getMessage();
         }
     }
+
+    // @Override
+    // public void start(Stage primaryStage) throws Exception {
+    // // set up the required components, adapted from main method previously.
+
+    // // instantiates the system.in
+    // ui = new Ui();
+    // storage = new Storage(filePath);
+
+    // // scroll content of the chat
+    // scrollPane = new ScrollPane();
+    // dialogContainer = new VBox();
+    // scrollPane.setContent(dialogContainer);
+
+    // userInput = new TextField();
+    // // place "Send" to make the button's title
+    // sendButton = new Button("Send");
+
+    // AnchorPane mainLayout = new AnchorPane();
+    // mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+    // scene = new Scene(mainLayout);
+
+    // primaryStage.setScene(scene);
+    // primaryStage.show();
+
+    // // style the window to look as expected
+    // primaryStage.setTitle("Duke");
+    // primaryStage.setResizable(false);
+    // primaryStage.setMinWidth(400.0);
+    // primaryStage.setMinHeight(600.0);
+
+    // // the preferred height & width of the window
+    // double prefHeight = 600.0;
+    // double prefWidth = 400.0;
+    // mainLayout.setPrefSize(prefWidth, prefHeight);
+
+    // scrollPane.setPrefSize(385, 550);
+    // // setting the ability to scroll horiz and vertically in the window
+    // scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+    // scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+    // scrollPane.setVvalue(1.0);
+    // scrollPane.setFitToWidth(true);
+
+    // // You will need to import `javafx.scene.layout.Region` for this.
+    // dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+    // userInput.setPrefWidth(325.0);
+
+    // sendButton.setPrefWidth(55.0);
+
+    // AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+    // AnchorPane.setBottomAnchor(sendButton, 1.0);
+    // AnchorPane.setRightAnchor(sendButton, 1.0);
+
+    // AnchorPane.setLeftAnchor(userInput, 1.0);
+    // AnchorPane.setBottomAnchor(userInput, 1.0);
+
+    // // Part 3. Add functionality to handle user input.
+    // sendButton.setOnMouseClicked((event) -> {
+    // try {
+    // handleUserInput();
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
+    // });
+
+    // userInput.setOnAction((event) -> {
+    // try {
+    // handleUserInput();
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
+    // });
+
+    // // Scroll down to the end every time dialogContainer's height changes.
+    // dialogContainer.heightProperty().addListener((observable) ->
+    // scrollPane.setVvalue(1.0));
+
+    // ui = new Ui();
+    // storage = new Storage(filePath);
+
+    // try {
+    // tasks = new TaskList(storage.load());
+    // } catch (DukeException e) {
+    // ui.showLoadingError(e.getMessage());
+    // Label dukeText = new Label("bro what is this");
+    // dialogContainer.getChildren().addAll(
+    // DialogBox.getDukeDialog(dukeText, new ImageView(duke)));
+    // tasks = new TaskList();
+    // }
+    // }
+
+    // /**
+    // * Iteration 1:
+    // * Creates a label with the specified text and adds it to the dialog
+    // container.
+    // *
+    // * @param text String containing text to add
+    // * @return a label with the specified text that has word wrap enabled.
+    // */
+    // private Label getDialogLabel(String text) {
+    // // You will need to import `javafx.scene.control.Label`.
+    // Label textToAdd = new Label(text);
+    // textToAdd.setWrapText(true);
+
+    // return textToAdd;
+    // }
+
+    // /**
+    // * Iteration 2:
+    // * Creates two dialog boxes, one echoing user input and the other containing
+    // * Duke's reply and then appends them to
+    // * the dialog container. Clears the user input after processing.
+    // *
+    // * @throws IOException
+    // */
+    // private void handleUserInput() throws IOException {
+    // Label userText = new Label(userInput.getText());
+    // Label dukeText = new Label(getResponse(userInput.getText()));
+    // dialogContainer.getChildren().addAll(
+    // DialogBox.getUserDialog(userText, new ImageView(user)),
+    // DialogBox.getDukeDialog(dukeText, new ImageView(duke)));
+    // userInput.clear();
+    // }
+
 }
