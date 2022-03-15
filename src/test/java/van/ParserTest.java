990ca69ead94f182a71e0855a1197576cb4505af
howardwhw2 <howardwhw2@gmail.com>
2022-03-15 21:36:37 +0800

    Fix minor typos

--- src/test/java/van/ParserTest.java
+++ src/test/java/van/ParserTest.java
@@ -9,21 +9,21 @@ public class ParserTest {
   public void deadLineTester() {
     Command deadLine = Parser.parseCommand("deadline eat /by 22-10-2022 18:00");
 
-    assertEquals("[D][ ] eat (by:Oct 22 2022 1800)"
+    assertEquals("[D][ ] eat (by:Oct 22 2022 1800) "
       , deadLine.toString());
   }
 
   @Test
   public void eventTester() {
     Command eventTest = Parser.parseCommand("event sleep /at 12-01-2022 06:00");
-    assertEquals("[E][ ] sleep (at:Jan 12 2022 0600)",
+    assertEquals("[E][ ] sleep (at:Jan 12 2022 0600) ",
       eventTest.toString());
   }
 
   @Test
   public void toDoTester() {
     Command toDoTest = Parser.parseCommand("todo study");
-    assertEquals("[T][ ] study", toDoTest.toString());
+    assertEquals("[T][ ] study ", toDoTest.toString());
   }
 
 
