54af7c2d1dd71e850e9bd3a7e6d2abe4f58cddea
Tan Yu Tao <tanyutao913@gmail.com>
2022-03-17 11:00:30 +0800

    Update test case for addcommand to catch dukeexception

--- src/test/java/commands/AddCommandTest.java
+++ src/test/java/commands/AddCommandTest.java
@@ -3,8 +3,10 @@ package commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import duke.commands.AddCommand;
+import duke.exceptions.DukeException;
 import org.junit.jupiter.api.Test;
 
 import duke.storage.Storage;
@@ -81,20 +83,24 @@ public class AddCommandTest {
     void addsToList() {
         TaskList tasks = new TaskList();
         assertEquals(0, tasks.size());
-        cmd = new AddCommand("todo", "return book");
-        cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
-        tasks = cmd.getList();
-        assertEquals(1, tasks.size());
+        try {
+            cmd = new AddCommand("todo", "return book");
+            cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
+            tasks = cmd.getList();
+            assertEquals(1, tasks.size());
 
-        cmd = new AddCommand("event", "return book /at 2/2/2022 1200");
-        cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
-        tasks = cmd.getList();
-        assertEquals(2, tasks.size());
+            cmd = new AddCommand("event", "return book /at 2/2/2022 1200");
+            cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
+            tasks = cmd.getList();
+            assertEquals(2, tasks.size());
 
-        cmd = new AddCommand("deadline", "return book /by 2/2/2022 1200");
-        cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
-        tasks = cmd.getList();
-        assertEquals(3, tasks.size());
+            cmd = new AddCommand("deadline", "return book /by 2/2/2022 1200");
+            cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
+            tasks = cmd.getList();
+            assertEquals(3, tasks.size());
+        } catch (DukeException e) {
+            fail();
+        }
     }
 
     /**
@@ -104,14 +110,18 @@ public class AddCommandTest {
     void addsCorrectTaskType() {
         TaskList tasks = new TaskList();
         assertEquals(0, tasks.size());
-        cmd = new AddCommand("todo", "return book");
-        cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
-        assertEquals("T", cmd.getList().get(0).getType());
-        cmd = new AddCommand("event", "return book /at 2/2/2022 1200");
-        cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
-        assertEquals("E", cmd.getList().get(1).getType());
-        cmd = new AddCommand("deadline", "return book /by 2/2/2022 1200");
-        cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
-        assertEquals("D", cmd.getList().get(2).getType());
+        try {
+            cmd = new AddCommand("todo", "return book");
+            cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
+            assertEquals("T", cmd.getList().get(0).getType());
+            cmd = new AddCommand("event", "return book /at 2/2/2022 1200");
+            cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
+            assertEquals("E", cmd.getList().get(1).getType());
+            cmd = new AddCommand("deadline", "return book /by 2/2/2022 1200");
+            cmd.execute(tasks, new Ui(), new Storage("data/tasks.txt"));
+            assertEquals("D", cmd.getList().get(2).getType());
+        } catch (DukeException e) {
+            fail();
+        }
     }
 }
