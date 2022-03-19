dc126ca90558abf9436dbb6998aa3b83fe8c39f1
Ryan Chang Jia Jie <>
2022-03-19 21:39:36 +0800

    Add header comments

--- src/main/java/siri/TaskList.java
+++ src/main/java/siri/TaskList.java
@@ -9,33 +9,60 @@ import java.util.ArrayList;
 public class TaskList {
     protected ArrayList<Task> tasks;
 
+    /**
+     * Constructs a new Arraylist of tasks.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Initialises the tasks of this Tasklist to the given tasks.
+     * @param tasks Tasks inside the Tasklist.
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Returns an Arraylist of tasks.
+     * @return Arraylist of tasks.
+     */
     public ArrayList<Task> getTasks() {
         return tasks;
     }
 
+    /**
+     * Marks the task at the specified index as done.
+     * @param index Target index of task to be marked.
+     */
     public void markTask(int index) {
         assert index >= 0 && index <= tasks.size();
         tasks.get(index).markAsDone();
     }
 
+    /**
+     * Marks the task at the specified index as not done.
+     * @param index Target index of task to be unmarked.
+     */
     public void unmarkTask(int index) {
         assert index >= 0 && index <= tasks.size();
         tasks.get(index).markAsNotDone();
     }
 
+    /**
+     * Deletes the task at the specified index.
+     * @param index Target index of task to be deleted.
+     */
     public void deleteTask(int index) {
         assert index >= 0 && index <= tasks.size();
         tasks.remove(index);
     }
 
+    /**
+     * Adds task to the Tasklist.
+     * @param task Task to be added.
+     */
     public void addTask(Task task) {
         assert task != null;
         tasks.add(task);
